a4a4e3784dd07bc1f53387dadd5c74c3180a49bf
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-05-24 12:04:43 -0700

    Implement CLI, version command

---
+++ commands.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/terraform/command"
+	"github.com/mitchellh/cli"
+)
+
+// Commands is the mapping of all the available Terraform commands.
+var Commands map[string]cli.CommandFactory
+
+func init() {
+	ui := &cli.BasicUi{Writer: os.Stdout}
+
+	Commands = map[string]cli.CommandFactory{
+		"version": func() (cli.Command, error) {
+			return &command.VersionCommand{
+				Revision:          GitCommit,
+				Version:           Version,
+				VersionPrerelease: VersionPrerelease,
+				Ui:                ui,
+			}, nil
+		},
+	}
+}
---
+++ terraform/resource_provider.go
@@ -0,0 +1,21 @@
+package terraform
+
+// ResourceProvider is an interface that must be implemented by any
+// resource provider: the thing that creates and manages the resources in
+// a Terraform configuration.
+type ResourceProvider interface {
+	// Configure configures the provider itself with the configuration
+	// given. This is useful for setting things like access keys.
+	//
+	// Configure returns a list of warnings and a potential error.
+	Configure(config map[string]interface{}) ([]string, error)
+
+	// Resources returns all the available resource types that this provider
+	// knows how to manage.
+	Resources() []ResourceType
+}
+
+// ResourceType is a type of resource that a resource provider can manage.
+type ResourceType struct {
+	Name string
+}
