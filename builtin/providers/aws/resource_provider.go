54f3ff33b9242d25d56ad028638805fa9b1dcfb1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-06-06 20:17:38 -0700

    providers/aws: pass tests

--- builtin/providers/aws/resource_provider.go
+++ builtin/providers/aws/resource_provider.go
@@ -7,7 +7,13 @@ import (
 type ResourceProvider struct {
 }
 
-func (p *ResourceProvider) Configure(map[string]interface{}) ([]string, error) {
+func (p *ResourceProvider) Configure(map[string]interface{}) error {
+	return nil
+}
+
+func (p *ResourceProvider) Diff(
+	s *terraform.ResourceState,
+	c map[string]interface{}) (*terraform.ResourceDiff, error) {
 	return nil, nil
 }
 
