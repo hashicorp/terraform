fa6a87e819bf75b01db2b78242894ff64596d195
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-06-10 11:34:14 -0700

    providers/aws: some mock data

--- builtin/providers/aws/resource_provider.go
+++ builtin/providers/aws/resource_provider.go
@@ -14,9 +14,25 @@ func (p *ResourceProvider) Configure(map[string]interface{}) error {
 func (p *ResourceProvider) Diff(
 	s *terraform.ResourceState,
 	c map[string]interface{}) (*terraform.ResourceDiff, error) {
-	return nil, nil
+	return &terraform.ResourceDiff{
+		Attributes: map[string]*terraform.ResourceAttrDiff{
+			"id": &terraform.ResourceAttrDiff{
+				Old:         "",
+				NewComputed: true,
+				RequiresNew: true,
+			},
+			"created": &terraform.ResourceAttrDiff{
+				Old: "false",
+				New: "true",
+			},
+		},
+	}, nil
 }
 
 func (p *ResourceProvider) Resources() []terraform.ResourceType {
-	return nil
+	return []terraform.ResourceType{
+		terraform.ResourceType{
+			Name: "aws_instance",
+		},
+	}
 }
