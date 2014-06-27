4c9a4ad04145745882119aa1f9cc1b8704d26679
Jack Pearkes <jackpearkes@gmail.com>
2014-06-27 12:47:19 -0400

    providers/aws: add elb layout, move instance into it's own namespace

---
+++ builtin/providers/aws/resource_aws_elb.go
@@ -0,0 +1,56 @@
+package aws
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform/helper/diff"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/pearkes/goamz/elb"
+)
+
+func resource_aws_elb_create(
+	s *terraform.ResourceState,
+	d *terraform.ResourceDiff,
+	meta interface{}) (*terraform.ResourceState, error) {
+	p := meta.(*ResourceProvider)
+	elbconn := p.elbconn
+
+	// Merge the diff into the state so that we have all the attributes
+	// properly.
+	rs := s.MergeDiff(d)
+
+	// Create
+
+	return
+}
+
+func resource_aws_elb_destroy(
+	s *terraform.ResourceState,
+	meta interface{}) error {
+	p := meta.(*ResourceProvider)
+	elbconn := p.elbconn
+
+	// destroy
+
+	return
+}
+
+func resource_aws_elb_diff(
+	s *terraform.ResourceState,
+	c *terraform.ResourceConfig,
+	meta interface{}) (*terraform.ResourceDiff, error) {
+
+	// diff
+}
+
+func resource_aws_elb_refresh(
+	s *terraform.ResourceState,
+	meta interface{}) (*terraform.ResourceState, error) {
+	p := meta.(*ResourceProvider)
+	elbconn := p.elbconn
+
+	// retrieve elb health status and describe instances?
+
+	return
+}
