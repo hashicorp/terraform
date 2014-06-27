d3f2547f864b1bbf49ec3a2bc8d28a49b5f20bb1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-06-26 22:01:05 -0700

    command: concurrent UI

--- command/hook_ui.go
+++ command/hook_ui.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/mitchellh/cli"
@@ -11,16 +12,29 @@ type UiHook struct {
 	terraform.NilHook
 
 	Ui cli.Ui
+
+	once sync.Once
+	ui   cli.Ui
 }
 
 func (h *UiHook) PreDiff(
 	id string, s *terraform.ResourceState) (terraform.HookAction, error) {
-	h.Ui.Output(fmt.Sprintf("Calculating diff for %s", id))
+	h.once.Do(h.init)
+
+	h.ui.Output(fmt.Sprintf("%s: Calculating diff", id))
 	return terraform.HookActionContinue, nil
 }
 
 func (h *UiHook) PreRefresh(
 	id string, s *terraform.ResourceState) (terraform.HookAction, error) {
-	h.Ui.Output(fmt.Sprintf("Refreshing state for %s (ID: %s)", id, s.ID))
+	h.once.Do(h.init)
+
+	h.ui.Output(fmt.Sprintf("%s: Refreshing state (ID: %s)", id, s.ID))
 	return terraform.HookActionContinue, nil
+}
+
+func (h *UiHook) init() {
+	// Wrap the ui so that it is safe for concurrency regardless of the
+	// underlying reader/writer that is in place.
+	h.ui = &cli.ConcurrentUi{Ui: h.Ui}
 }
