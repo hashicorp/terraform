20198cb7aa1be3e8c451b067beda493be49ae88e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-07-07 11:25:54 -0700

    providers/aws: ABC, Its as Easy as 123
    https://www.youtube.com/watch?v=I8GvDLDYhNM&feature=kp

--- builtin/providers/aws/resources.go
+++ builtin/providers/aws/resources.go
@@ -11,18 +11,19 @@ var resourceMap *resource.Map
 func init() {
 	resourceMap = &resource.Map{
 		Mapping: map[string]resource.Resource{
-			"aws_instance": resource.Resource{
-				Create:  resource_aws_instance_create,
-				Destroy: resource_aws_instance_destroy,
-				Diff:    resource_aws_instance_diff,
-				Refresh: resource_aws_instance_refresh,
-			},
 			"aws_elb": resource.Resource{
 				Create:  resource_aws_elb_create,
 				Update:  resource_aws_elb_update,
 				Destroy: resource_aws_elb_destroy,
 				Diff:    resource_aws_elb_diff,
 				Refresh: resource_aws_elb_refresh,
+			},
+
+			"aws_instance": resource.Resource{
+				Create:  resource_aws_instance_create,
+				Destroy: resource_aws_instance_destroy,
+				Diff:    resource_aws_instance_diff,
+				Refresh: resource_aws_instance_refresh,
 			},
 		},
 	}
