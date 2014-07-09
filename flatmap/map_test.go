f0ff4fad7401afe1600c631391c647e29d176191
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-07-09 16:43:36 -0700

    flatmap: add Keys() and Merge()

--- flatmap/map_test.go
+++ flatmap/map_test.go
@@ -22,3 +22,61 @@ func TestMapDelete(t *testing.T) {
 		t.Fatalf("bad: %#v", m)
 	}
 }
+
+func TestMapKeys(t *testing.T) {
+	cases := []struct {
+		Input  map[string]string
+		Output []string
+	}{
+		{
+			Input: map[string]string{
+				"foo":       "bar",
+				"bar.#":     "bar",
+				"bar.0.foo": "bar",
+				"bar.0.baz": "bar",
+			},
+			Output: []string{
+				"foo",
+				"bar",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		actual := Map(tc.Input).Keys()
+		if !reflect.DeepEqual(actual, tc.Output) {
+			t.Fatalf("input: %#v\n\nbad: %#v", tc.Input, actual)
+		}
+	}
+}
+
+func TestMapMerge(t *testing.T) {
+	cases := []struct {
+		One    map[string]string
+		Two    map[string]string
+		Result map[string]string
+	}{
+		{
+			One: map[string]string{
+				"foo": "bar",
+				"bar": "nope",
+			},
+			Two: map[string]string{
+				"bar": "baz",
+				"baz": "buz",
+			},
+			Result: map[string]string{
+				"foo": "bar",
+				"bar": "baz",
+				"baz": "buz",
+			},
+		},
+	}
+
+	for i, tc := range cases {
+		Map(tc.One).Merge(Map(tc.Two))
+		if !reflect.DeepEqual(tc.One, tc.Result) {
+			t.Fatalf("case %d bad: %#v", i, tc.One)
+		}
+	}
+}
