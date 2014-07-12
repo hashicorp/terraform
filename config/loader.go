14b29d0ecdf22b8195db11de6d54e6f327b217be
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-07-11 20:15:09 -0700

    config: LoadDir

--- config/loader.go
+++ config/loader.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"path/filepath"
+)
+
 // Load loads the Terraform configuration from a given file.
 //
 // This file can be any format that Terraform recognizes, and import any
@@ -21,4 +25,32 @@ func Load(path string) (*Config, error) {
 	}
 
 	return configTree.Flatten()
+}
+
+// LoadDir loads all the Terraform configuration files in a single
+// directory and merges them together.
+func LoadDir(path string) (*Config, error) {
+	matches, err := filepath.Glob(filepath.Join(path, "*.tf"))
+	if err != nil {
+		return nil, err
+	}
+
+	var result *Config
+	for _, f := range matches {
+		c, err := Load(f)
+		if err != nil {
+			return nil, err
+		}
+
+		if result != nil {
+			result, err = mergeConfig(result, c)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			result = c
+		}
+	}
+
+	return result, nil
 }
