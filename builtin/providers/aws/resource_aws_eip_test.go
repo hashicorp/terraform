dff47bdbbedefe0e0bf4c1522320db55be83d9ec
Jack Pearkes <jackpearkes@gmail.com>
2014-07-16 10:40:48 -0400

    providers/aws: fix eip test

--- builtin/providers/aws/resource_aws_eip_test.go
+++ builtin/providers/aws/resource_aws_eip_test.go
@@ -105,7 +105,5 @@ func testAccCheckAWSEIPExists(n string, res *ec2.Address) resource.TestCheckFunc
 
 const testAccAWSEIPConfig = `
 resource "aws_eip" "bar" {
-  image_id = "ami-fb8e9292"
-  instance_type = "t1.micro"
 }
 `
