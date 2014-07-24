d6d4cf4b39db39edf748580db02c3a694d1b2625
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2014-07-23 19:31:42 -0700

    Grunt file cleanup
    Better indenatation, more readable

--- website/source/Gruntfile.js
+++ website/source/Gruntfile.js
@@ -1,69 +1,67 @@
+// jshint node:true
 module.exports = function(grunt) {
 
-	// Configuration goes here
-	grunt.initConfig({
+// Load plugins here
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-recess');
 
-		less: {
-			development:{
-				files: {
-					"stylesheets/main.css": "stylesheets/main.less"
-				}
+// Configuration goes here
+grunt.initConfig({
+
+	less: {
+		development:{
+			files: {
+				"stylesheets/main.css": "stylesheets/main.less"
 			}
-		},
-
-  	concat: {
-    	options: {
-      		separator: ';'
-    	},
-    	site: {
-    		src: 	[
-                'javascripts/app/app.js',
-                'javascripts/app/util.js',
-                'javascripts/app/homepage.js'
-
-    					],
-
-  			dest:  'javascripts/app/deploy/site.js'
-    	},
-		},
-
-		uglify: {
-      		app: {
-				files: {
-					'javascripts/app/deploy/site.min.js': ['javascripts/app/deploy/site.js']
-				}
-			}
-		},
-
-		watch: {
-			less: {
-				files: 'stylesheets/*.less',
-				tasks: ['less']
-			},
-		  js: {
-		    files: 'javascripts/app/*.js',
-		    tasks: ['concat', 'uglify']
-		  }
 		}
+	},
 
-	});
+	concat: {
+		options: {
+			separator: ';'
+		},
+		site: {
+			src: 	[
+				'javascripts/app/app.js',
+				'javascripts/app/util.js',
+				'javascripts/app/homepage.js'
+			],
+			dest:  'javascripts/app/deploy/site.js'
+		},
+	},
 
-	// Load plugins here
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-recess');
+	uglify: {
+		app: {
+			files: {
+				'javascripts/app/deploy/site.min.js': ['javascripts/app/deploy/site.js']
+			}
+		}
+	},
 
-  	// JS distribution task.
-  	grunt.registerTask('dist-js', ['concat', 'uglify']);
+	watch: {
+		less: {
+			files: 'stylesheets/*.less',
+			tasks: ['less']
+		},
+		js: {
+			files: 'javascripts/app/*.js',
+			tasks: ['concat', 'uglify']
+		}
+	}
 
-  	// Full distribution task.
-  	grunt.registerTask('dist', ['dist-js']);
+});
 
-	grunt.registerTask('default', ['watch']);
+// JS distribution task.
+grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+// Full distribution task.
+grunt.registerTask('dist', ['dist-js']);
+grunt.registerTask('default', ['watch']);
 
 };
