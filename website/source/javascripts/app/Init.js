a4b9e96f7ba1cfb9f8039b5a963313251b2bc93b
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2014-07-27 18:01:48 -0700

    Fixed IE detection

--- website/source/javascripts/app/Init.js
+++ website/source/javascripts/app/Init.js
@@ -2,21 +2,14 @@
 	Engine
 ){
 
+// Quick and dirty IE detection
 var isIE = (function(){
-
-	var undef,
-	v = 3,
-	div = document.createElement('div'),
-	all = div.getElementsByTagName('i');
-
-	while (
-		div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
-			all[0]
-	);
-
-	return v > 4 ? v : undef;
-
-}());
+	if (window.navigator.userAgent.match('Trident')) {
+		return true;
+	} else {
+		return false;
+	}
+})();
 
 // isIE = true;
 
