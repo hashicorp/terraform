fd889083c26a6d68fae646627323e06f5dd81730
Armon Dadgar <armon.dadgar@gmail.com>
2014-07-28 13:43:27 -0400

    Set version 0.1

--- version.go
+++ version.go
@@ -9,4 +9,4 @@ const Version = "0.1.0"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = "dev"
+const VersionPrerelease = ""
