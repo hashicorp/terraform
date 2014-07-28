839ea6070129cfd4d9442e7c2d214663fa1d732b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-07-28 10:59:54 -0700

    scripts: dist

---
+++ scripts/dist.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+# Get the version from the command line
+VERSION=$1
+if [ -z $VERSION ]; then
+    echo "Please specify a version."
+    exit 1
+fi
+
+# Make sure we have a bintray API key
+if [ -z $BINTRAY_API_KEY ]; then
+    echo "Please set your bintray API key in the BINTRAY_API_KEY env var."
+    exit 1
+fi
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that dir because we expect that
+cd $DIR
+
+# Zip all the files
+rm -rf ./pkg/dist
+mkdir -p ./pkg/dist
+for FILENAME in $(find ./pkg -mindepth 1 -maxdepth 1 -type f); do
+    FILENAME=$(basename $FILENAME)
+    cp ./pkg/${FILENAME} ./pkg/dist/${VERSION}_${FILENAME}
+done
+
+# Make the checksums
+pushd ./pkg/dist
+shasum -a256 * > ./${VERSION}_SHA256SUMS
+popd
+
+# Upload
+for ARCHIVE in ./pkg/dist/*; do
+    ARCHIVE_NAME=$(basename ${ARCHIVE})
+
+    echo Uploading: $ARCHIVE_NAME
+    curl \
+        -T ${ARCHIVE} \
+        -umitchellh:${BINTRAY_API_KEY} \
+        "https://api.bintray.com/content/mitchellh/terraform/terraform/${VERSION}/${ARCHIVE_NAME}"
+done
+
+exit 0
