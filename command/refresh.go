f26f432f4c388e57d142ee08aef4a0a500cfeaa0
Armon Dadgar <armon.dadgar@gmail.com>
2014-07-27 22:58:12 -0400

    command/refresh: Testing backup option

--- command/refresh.go
+++ command/refresh.go
@@ -94,7 +94,7 @@ func (c *RefreshCommand) Run(args []string) int {
 	}
 
 	// Create a backup of the state before updating
-	if backupPath != "-" {
+	if backupPath != "-" && c.State != nil {
 		log.Printf("[INFO] Writing backup state to: %s", backupPath)
 		f, err := os.Create(backupPath)
 		if err == nil {
