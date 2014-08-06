b8d0010db9410dd9a10d7d1178840abdcffa3487
Eric Bellemon <eric.bellemon@gmail.com>
2014-08-06 23:44:53 +0200

    Beginning of diff and update router

--- builtin/providers/openstack/resource_openstack_router.go
+++ builtin/providers/openstack/resource_openstack_router.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform/helper/diff"
 	//"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	//"github.com/racker/perigee"
+	"github.com/racker/perigee"
 	//"github.com/rackspace/gophercloud"
 	"log"
 )
@@ -28,6 +28,11 @@ func resource_openstack_router_create(
 		return nil, err
 	}
 
+	newRouter := network.NewRouter{
+		Name:         rs.Attributes["name"],
+		AdminStateUp: true,
+	}
+
 	if _, ok := rs.Attributes["external_gateway"]; ok {
 		if _, ok := rs.Attributes["external_id"]; !ok {
 
@@ -35,25 +40,25 @@ func resource_openstack_router_create(
 			externalId := "first public network available"
 			rs.Attributes["external_id"] = externalId
 		}
+
+		externalGateway := network.ExternalGateway{
+			NetworkId: rs.Attributes["external_id"],
+		}
+
+		newRouter.ExternalGateway = externalGateway
+	} else {
+		rs.Attributes["external_id"] = ""
 	}
 
-	externalGateway := network.ExternalGateway{
-		NetworkId: rs.Attributes["external_id"],
-	}
-
-	newRouter, err := networksApi.CreateRouter(network.NewRouter{
-		Name:            rs.Attributes["name"],
-		AdminStateUp:    true,
-		ExternalGateway: externalGateway,
-	})
+	createdRouter, err := networksApi.CreateRouter(newRouter)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Printf("[DEBUG] New router created: %#v", newRouter)
+	log.Printf("[DEBUG] New router created: %#v", createdRouter)
 
-	rs.Attributes["id"] = newRouter.Id
-	rs.ID = newRouter.Id
+	rs.Attributes["id"] = createdRouter.Id
+	rs.ID = createdRouter.Id
 
 	// TODO wait for router status
 
@@ -82,21 +87,101 @@ func resource_openstack_router_update(
 	d *terraform.ResourceDiff,
 	meta interface{}) (*terraform.ResourceState, error) {
 
-	return nil, nil
+	p := meta.(*ResourceProvider)
+	client := p.client
+
+	// Merge the diff into the state so that we have all the attributes
+	// properly.
+	rs := s.MergeDiff(d)
+
+	networksApi, err := getNetworkApi(client.AccessProvider)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("[DEBUG] Updating router: %#v", s.ID)
+
+	if attr, ok := d.Attributes["name"]; ok {
+		_, err := networksApi.UpdateRouter(network.Router{
+			Id:   rs.ID,
+			Name: attr.New,
+		})
+
+		if err != nil {
+			return nil, err
+		}
+
+		rs.Attributes["name"] = attr.New
+	}
+
+	return rs, nil
 }
 
 func resource_openstack_router_destroy(
 	s *terraform.ResourceState,
 	meta interface{}) error {
 
-	return nil
+	p := meta.(*ResourceProvider)
+	client := p.client
+
+	networksApi, err := getNetworkApi(client.AccessProvider)
+	if err != nil {
+		return err
+	}
+
+	ports, err := networksApi.GetPorts()
+	if err != nil {
+		return err
+	}
+
+	for _, port := range ports {
+		if port.DeviceId == s.ID {
+			err = networksApi.RemoveRouterInterface(s.ID, port.PortId)
+		}
+	}
+
+	err = networksApi.DeleteRouter(s.ID)
+	if err != nil {
+		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
+		if !ok {
+			return err
+		}
+
+		if httpError.Actual == 404 {
+			return nil
+		}
+	}
+	return err
 }
 
 func resource_openstack_router_refresh(
 	s *terraform.ResourceState,
 	meta interface{}) (*terraform.ResourceState, error) {
 
-	// TODO refresh ports
+	p := meta.(*ResourceProvider)
+	client := p.client
+
+	networksApi, err := getNetworkApi(client.AccessProvider)
+	if err != nil {
+		return nil, err
+	}
+
+	router, err := networksApi.GetRouter(s.ID)
+	if err != nil {
+		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
+		if !ok {
+			return nil, err
+		}
+
+		if httpError.Actual == 404 {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	s.Attributes["name"] = router.Name
+	s.Attributes["external_id"] = router.ExternalGateway.NetworkId
 
 	return s, nil
 }
@@ -116,6 +201,7 @@ func resource_openstack_router_diff(
 
 		ComputedAttrs: []string{
 			"id",
+			"external_id",
 		},
 	}
 
