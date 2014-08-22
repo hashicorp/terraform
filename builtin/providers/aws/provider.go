2fd2470c92cf4eb5807ffbc199b87cf686f05ea9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-08-21 18:38:43 -0700

    providers/aws: resource_aws_instance partially to schema.Resource

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -17,6 +17,7 @@ func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 			"aws_eip":            resourceAwsEip(),
+			"aws_instance":       resourceAwsInstance(),
 			"aws_security_group": resourceAwsSecurityGroup(),
 		},
 	}
