0da47bab446fcd644f75d8bb4e764ee203010910
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-08-28 16:47:35 -0700

    up version for dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1 (unreleased)
+
+
+
 ## 0.2.0 (August 28, 2014)
 
 BACKWARDS INCOMPATIBILITIES:
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package main
 var GitCommit string
 
 // The main version number that is being run at the moment.
-const Version = "0.2.0"
+const Version = "0.2.1"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "dev"
