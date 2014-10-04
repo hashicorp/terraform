5aa62ae63594594d12644301c54b4431844c96aa
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-10-04 16:42:43 -0700

    providers/null: null provider

---
+++ builtin/providers/null/provider.go
@@ -0,0 +1,17 @@
+package null
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"null_resource": resource(),
+		},
+	}
+}
