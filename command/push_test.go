7ba0c003f2ca74d90b845e2b5b0a1faec3f9a193
Armon Dadgar <armon.dadgar@gmail.com>
2014-10-08 17:39:08 -0700

    command/push: Allow existing state file to enable remote

--- command/push_test.go
+++ command/push_test.go
@@ -2,6 +2,8 @@ package command
 
 import (
 	"bytes"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/remote"
@@ -50,6 +52,50 @@ func TestPush_cliRemote_noState(t *testing.T) {
 	}
 }
 
+func TestPush_cliRemote_withState(t *testing.T) {
+	tmp, cwd := testCwd(t)
+	defer fixDir(tmp, cwd)
+
+	s := terraform.NewState()
+	conf, srv := testRemoteState(t, s, 200)
+	defer srv.Close()
+
+	s = terraform.NewState()
+	s.Serial = 10
+
+	// Store the local state
+	buf := bytes.NewBuffer(nil)
+	terraform.WriteState(s, buf)
+	err := ioutil.WriteFile(DefaultStateFilename, buf.Bytes(), 0777)
+	if err != nil {
+		t.Fatalf("Err: %v", err)
+	}
+
+	ui := new(cli.MockUi)
+	c := &PushCommand{
+		Meta: Meta{
+			ContextOpts: testCtxConfig(testProvider()),
+			Ui:          ui,
+		},
+	}
+
+	// Remote with default state file
+	args := []string{"-remote", conf.Name, "-remote-server", conf.Server}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: \n%s", ui.ErrorWriter.String())
+	}
+
+	// Should backup state
+	if _, err := os.Stat(DefaultStateFilename + DefaultBackupExtention); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Should enable remote state
+	if _, err := os.Stat(remote.LocalDirectory + "/" + remote.HiddenStateFile); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+}
+
 func TestPush_local(t *testing.T) {
 	tmp, cwd := testCwd(t)
 	defer fixDir(tmp, cwd)
@@ -57,11 +103,11 @@ func TestPush_local(t *testing.T) {
 	s := terraform.NewState()
 	s.Serial = 5
 	conf, srv := testRemoteState(t, s, 200)
+	defer srv.Close()
 
 	s = terraform.NewState()
 	s.Serial = 10
 	s.Remote = conf
-	defer srv.Close()
 
 	// Store the local state
 	buf := bytes.NewBuffer(nil)
