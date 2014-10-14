b8c8f4d63c14d99d67baa2c32b53057e2da828f8
stungtoat <isaac@overttone.com>
2014-10-14 00:00:44 -0700

    providers/google: remove secrets file
    fixes #452

--- builtin/providers/google/config.go
+++ builtin/providers/google/config.go
@@ -17,17 +17,26 @@ const clientScopes string = "https://www.googleapis.com/auth/compute"
 // Config is the configuration structure used to instantiate the Google
 // provider.
 type Config struct {
-	AccountFile       string
-	ClientSecretsFile string
-	Project           string
-	Region            string
+	AccountFile string
+	Project     string
+	Region      string
 
 	clientCompute *compute.Service
 }
 
 func (c *Config) loadAndValidate() error {
 	var account accountFile
-	var secrets clientSecretsFile
+
+	// TODO: validation that it isn't blank
+	if c.AccountFile == "" {
+		c.AccountFile = os.Getenv("GOOGLE_ACCOUNT_FILE")
+	}
+	if c.Project == "" {
+		c.Project = os.Getenv("GOOGLE_PROJECT")
+	}
+	if c.Region == "" {
+		c.Region = os.Getenv("GOOGLE_REGION")
+	}
 
 	if err := loadJSON(&account, c.AccountFile); err != nil {
 		return fmt.Errorf(
@@ -36,24 +45,15 @@ func (c *Config) loadAndValidate() error {
 			err)
 	}
 
-	if err := loadJSON(&secrets, c.ClientSecretsFile); err != nil {
-		return fmt.Errorf(
-			"Error loading client secrets file '%s': %s",
-			c.ClientSecretsFile,
-			err)
-	}
-
 	// Get the token for use in our requests
 	log.Printf("[INFO] Requesting Google token...")
 	log.Printf("[INFO]   -- Email: %s", account.ClientEmail)
 	log.Printf("[INFO]   -- Scopes: %s", clientScopes)
 	log.Printf("[INFO]   -- Private Key Length: %d", len(account.PrivateKey))
-	log.Printf("[INFO]   -- Token URL: %s", secrets.Web.TokenURI)
 	jwtTok := jwt.NewToken(
 		account.ClientEmail,
 		clientScopes,
 		[]byte(account.PrivateKey))
-	jwtTok.ClaimSet.Aud = secrets.Web.TokenURI
 	token, err := jwtTok.Assert(new(http.Client))
 	if err != nil {
 		return fmt.Errorf("Error retrieving auth token: %s", err)
@@ -64,8 +64,6 @@ func (c *Config) loadAndValidate() error {
 		Config: &oauth.Config{
 			ClientId: account.ClientId,
 			Scope:    clientScopes,
-			TokenURL: secrets.Web.TokenURI,
-			AuthURL:  secrets.Web.AuthURI,
 		},
 		Token: token,
 	}
@@ -85,16 +83,6 @@ type accountFile struct {
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientId     string `json:"client_id"`
-}
-
-// clientSecretsFile represents the structure of the client secrets JSON file.
-type clientSecretsFile struct {
-	Web struct {
-		AuthURI     string `json:"auth_uri"`
-		ClientEmail string `json:"client_email"`
-		ClientId    string `json:"client_id"`
-		TokenURI    string `json:"token_uri"`
-	}
 }
 
 func loadJSON(result interface{}, path string) error {
