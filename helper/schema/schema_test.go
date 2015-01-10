34617b337f696de8419417872e173af6a8a11867
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-01-10 15:53:29 -0800

    helper/schema: remove commit not for this branch

--- helper/schema/schema_test.go
+++ helper/schema/schema_test.go
@@ -15,7 +15,6 @@ func TestValueType_Zero(t *testing.T) {
 	}{
 		{TypeBool, false},
 		{TypeInt, 0},
-		{TypeFloat, 0.0},
 		{TypeString, ""},
 		{TypeList, []interface{}{}},
 		{TypeMap, map[string]interface{}{}},
