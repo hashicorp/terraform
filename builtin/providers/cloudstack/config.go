7b16c44cc2a82cc0fb8a51e0078bbfd17864b56b
Sander van Harmelen <sander@xanzy.io>
2015-01-12 15:57:24 +0100

    provider/cloudstack: make timeout configurable
    Seems some platforms need more time, so make it configurable with a
    sane default…

--- builtin/providers/cloudstack/config.go
+++ builtin/providers/cloudstack/config.go
@@ -8,11 +8,12 @@ type Config struct {
 	ApiURL    string
 	ApiKey    string
 	SecretKey string
+	Timeout   int64
 }
 
 // Client() returns a new CloudStack client.
 func (c *Config) NewClient() (*cloudstack.CloudStackClient, error) {
 	cs := cloudstack.NewAsyncClient(c.ApiURL, c.ApiKey, c.SecretKey, false)
-	cs.AsyncTimeout(180)
+	cs.AsyncTimeout(c.Timeout)
 	return cs, nil
 }
