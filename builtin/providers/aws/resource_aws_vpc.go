a514077e0de3c1591ec6fbaaf4c2d0204cbfe737
Josh Frye <joshfng@gmail.com>
2015-01-13 11:26:00 -0500

    Ability to create and link DHCP Option Sets to VPC

--- builtin/providers/aws/resource_aws_vpc.go
+++ builtin/providers/aws/resource_aws_vpc.go
@@ -42,6 +42,39 @@ func resourceAwsVpc() *schema.Resource {
 				Computed: true,
 			},
 
+			"dhcp_options": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"domain_name_servers": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"domain_name": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"ntp_servers": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"netbios_name_servers": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+
+						"netbios_node_type": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+
 			"main_route_table_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
@@ -57,7 +90,6 @@ func resourceAwsVpc() *schema.Resource {
 				Computed: true,
 			},
 
-
 			"tags": tagsSchema(),
 		},
 	}
@@ -68,7 +100,7 @@ func resourceAwsVpcCreate(d *schema.ResourceData, meta interface{}) error {
 
 	// Create the VPC
 	createOpts := &ec2.CreateVpc{
-		CidrBlock: d.Get("cidr_block").(string),
+		CidrBlock:       d.Get("cidr_block").(string),
 		InstanceTenancy: d.Get("instance_tenancy").(string),
 	}
 	log.Printf("[DEBUG] VPC create config: %#v", createOpts)
@@ -100,6 +132,38 @@ func resourceAwsVpcCreate(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf(
 			"Error waiting for VPC (%s) to become available: %s",
 			d.Id(), err)
+	}
+
+	if _, ok := d.GetOk("dhcp_options"); ok {
+		// Create DHCP Options set options
+		createDhcpOpts := &ec2.CreateDhcpOptions{
+			DomainNameServers: d.Get("domain_name_servers").(string),
+			DomainName: d.Get("domain_name").(string),
+			NtpServers: d.Get("ntp_servers").(string),
+			NetbiosNameServers: d.Get("netbios_name_servers").(string),
+			NetbiosNodeType: d.Get("netbios_node_type").(string),
+		}
+
+		// Create the DHCP Options set
+		log.Printf("[DEBUG] DHCP Options create config: %#v", createDhcpOpts)
+		dhcpResp, err := ec2conn.CreateDhcpOptions(createDhcpOpts)
+		if err != nil {
+			return fmt.Errorf("Error creating DHCP Options: %s", err)
+		}
+
+		// Get the ID
+		dhcp := &dhcpResp.DhcpOptions
+		log.Printf("[INFO] DHCP Options ID: %s", dhcp.DhcpOptionsId)
+
+		// Set partial mode and say that we setup the cidr block
+		d.Partial(true)
+		d.SetPartial("dhcp_options")
+
+		// Link DhcpOptionsId and VpcId if we have a dhcp_options_id
+		_, err = ec2conn.AssociateDhcpOptions(dhcp.DhcpOptionsId, vpc.VpcId)
+		if err != nil {
+			return fmt.Errorf("Error associating DHCP Options with VPC: %s", err)
+		}
 	}
 
 	// Update our attributes and return
@@ -243,8 +307,7 @@ func VPCStateRefreshFunc(conn *ec2.EC2, id string) resource.StateRefreshFunc {
 	}
 }
 
-
-func resourceAwsVpcSetDefaultNetworkAcl(conn *ec2.EC2, d *schema.ResourceData) error  {
+func resourceAwsVpcSetDefaultNetworkAcl(conn *ec2.EC2, d *schema.ResourceData) error {
 	filter := ec2.NewFilter()
 	filter.Add("default", "true")
 	filter.Add("vpc-id", d.Id())
@@ -260,7 +323,7 @@ func resourceAwsVpcSetDefaultNetworkAcl(conn *ec2.EC2, d *schema.ResourceData) e
 	return nil
 }
 
-func resourceAwsVpcSetDefaultSecurityGroup(conn *ec2.EC2, d *schema.ResourceData) error  {
+func resourceAwsVpcSetDefaultSecurityGroup(conn *ec2.EC2, d *schema.ResourceData) error {
 	filter := ec2.NewFilter()
 	filter.Add("group-name", "default")
 	filter.Add("vpc-id", d.Id())
