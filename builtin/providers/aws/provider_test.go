9676697e61bfc3ce8bf5f92344f3a5c1aa0620e6
Seth Vargo <sethvargo@gmail.com>
2015-01-22 15:30:27 -0500

    Read the "standard" AWS environment variables
    This is 100% backwards-compatible

--- builtin/providers/aws/provider_test.go
+++ builtin/providers/aws/provider_test.go
@@ -30,14 +30,14 @@ func TestProvider_impl(t *testing.T) {
 }
 
 func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("AWS_ACCESS_KEY"); v == "" {
-		t.Fatal("AWS_ACCESS_KEY must be set for acceptance tests")
+	if v := os.Getenv("AWS_ACCESS_KEY_ID"); v == "" {
+		t.Fatal("AWS_ACCESS_KEY_ID must be set for acceptance tests")
 	}
-	if v := os.Getenv("AWS_SECRET_KEY"); v == "" {
-		t.Fatal("AWS_SECRET_KEY must be set for acceptance tests")
+	if v := os.Getenv("AWS_SECRET_ACCESS_KEY"); v == "" {
+		t.Fatal("AWS_SECRET_ACCESS_KEY must be set for acceptance tests")
 	}
-	if v := os.Getenv("AWS_REGION"); v == "" {
+	if v := os.Getenv("AWS_DEFAULT_REGION"); v == "" {
 		log.Println("[INFO] Test: Using us-west-2 as test region")
-		os.Setenv("AWS_REGION", "us-west-2")
+		os.Setenv("AWS_DEFAULT_REGION", "us-west-2")
 	}
 }
