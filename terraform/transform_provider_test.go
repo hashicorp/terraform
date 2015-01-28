254689389a82d21e41c46474aaa3bf6897213894
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-01-27 22:14:01 -0800

    terraform: missing provider transform

--- terraform/transform_provider_test.go
+++ terraform/transform_provider_test.go
@@ -3,6 +3,8 @@ package terraform
 import (
 	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform/dag"
 )
 
 func TestProviderTransformer(t *testing.T) {
@@ -28,8 +30,50 @@ func TestProviderTransformer(t *testing.T) {
 	}
 }
 
+func TestMissingProviderTransformer(t *testing.T) {
+	mod := testModule(t, "transform-provider-basic")
+
+	g := Graph{Path: RootModulePath}
+	{
+		tf := &ConfigTransformer{Module: mod}
+		if err := tf.Transform(&g); err != nil {
+			t.Fatalf("err: %s", err)
+		}
+	}
+
+	transform := &MissingProviderTransformer{Providers: []string{"foo"}}
+	if err := transform.Transform(&g); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	actual := strings.TrimSpace(g.String())
+	expected := strings.TrimSpace(testTransformMissingProviderBasicStr)
+	if actual != expected {
+		t.Fatalf("bad:\n\n%s", actual)
+	}
+}
+
+func TestGraphNodeMissingProvider_impl(t *testing.T) {
+	var _ dag.Vertex = new(graphNodeMissingProvider)
+	var _ dag.NamedVertex = new(graphNodeMissingProvider)
+	var _ GraphNodeProvider = new(graphNodeMissingProvider)
+}
+
+func TestGraphNodeMissingProvider_ProviderName(t *testing.T) {
+	n := &graphNodeMissingProvider{ProviderNameValue: "foo"}
+	if v := n.ProviderName(); v != "foo" {
+		t.Fatalf("bad: %#v", v)
+	}
+}
+
 const testTransformProviderBasicStr = `
 aws_instance.web
   provider.aws
 provider.aws
+`
+
+const testTransformMissingProviderBasicStr = `
+aws_instance.web
+provider.aws
+provider.foo
 `
