9dfce9c93acbd177270a002eb07c372bd4b09241
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-01-30 12:56:03 -0800

    dag: Remove, RemoveEdge, DownEdges, UpEdges

--- dag/graph.go
+++ dag/graph.go
@@ -9,10 +9,10 @@ import (
 
 // Graph is used to represent a dependency graph.
 type Graph struct {
-	vertices  *set
-	edges     []Edge
-	downEdges map[Vertex]*set
-	upEdges   map[Vertex]*set
+	vertices  *Set
+	edges     *Set
+	downEdges map[Vertex]*Set
+	upEdges   map[Vertex]*Set
 	once      sync.Once
 }
 
@@ -39,7 +39,13 @@ func (g *Graph) Vertices() []Vertex {
 
 // Edges returns the list of all the edges in the graph.
 func (g *Graph) Edges() []Edge {
-	return g.edges
+	list := g.vertices.List()
+	result := make([]Edge, len(list))
+	for i, v := range list {
+		result[i] = v.(Edge)
+	}
+
+	return result
 }
 
 // Add adds a vertex to the graph. This is safe to call multiple time with
@@ -48,6 +54,51 @@ func (g *Graph) Add(v Vertex) Vertex {
 	g.once.Do(g.init)
 	g.vertices.Add(v)
 	return v
+}
+
+// Remove removes a vertex from the graph. This will also remove any
+// edges with this vertex as a source or target.
+func (g *Graph) Remove(v Vertex) Vertex {
+	// Delete the vertex itself
+	g.vertices.Delete(v)
+
+	// Delete the edges to non-existent things
+	for _, target := range g.DownEdges(v).List() {
+		g.RemoveEdge(BasicEdge(v, target))
+	}
+	for _, source := range g.UpEdges(v).List() {
+		g.RemoveEdge(BasicEdge(source, v))
+	}
+
+	return nil
+}
+
+// RemoveEdge removes an edge from the graph.
+func (g *Graph) RemoveEdge(edge Edge) {
+	g.once.Do(g.init)
+
+	// Delete the edge from the set
+	g.edges.Delete(edge)
+
+	// Delete the up/down edges
+	if s, ok := g.downEdges[edge.Source()]; ok {
+		s.Delete(edge.Target())
+	}
+	if s, ok := g.upEdges[edge.Target()]; ok {
+		s.Delete(edge.Source())
+	}
+}
+
+// DownEdges returns the outward edges from the source Vertex v.
+func (g *Graph) DownEdges(v Vertex) *Set {
+	g.once.Do(g.init)
+	return g.downEdges[v]
+}
+
+// UpEdges returns the inward edges to the destination Vertex v.
+func (g *Graph) UpEdges(v Vertex) *Set {
+	g.once.Do(g.init)
+	return g.upEdges[v]
 }
 
 // Connect adds an edge with the given source and target. This is safe to
@@ -65,13 +116,13 @@ func (g *Graph) Connect(edge Edge) {
 		return
 	}
 
-	// TODO: add all edges
-	g.edges = append(g.edges, edge)
+	// Add the edge to the set
+	g.edges.Add(edge)
 
 	// Add the down edge
 	s, ok := g.downEdges[source]
 	if !ok {
-		s = new(set)
+		s = new(Set)
 		g.downEdges[source] = s
 	}
 	s.Add(target)
@@ -79,7 +130,7 @@ func (g *Graph) Connect(edge Edge) {
 	// Add the up edge
 	s, ok = g.upEdges[target]
 	if !ok {
-		s = new(set)
+		s = new(Set)
 		g.upEdges[target] = s
 	}
 	s.Add(source)
@@ -125,10 +176,10 @@ func (g *Graph) String() string {
 }
 
 func (g *Graph) init() {
-	g.vertices = new(set)
-	g.edges = make([]Edge, 0, 2)
-	g.downEdges = make(map[Vertex]*set)
-	g.upEdges = make(map[Vertex]*set)
+	g.vertices = new(Set)
+	g.edges = new(Set)
+	g.downEdges = make(map[Vertex]*Set)
+	g.upEdges = make(map[Vertex]*Set)
 }
 
 // VertexName returns the name of a vertex.
--- dag/graph_test.go
+++ dag/graph_test.go
@@ -32,6 +32,21 @@ func TestGraph_basic(t *testing.T) {
 	}
 }
 
+func TestGraph_remove(t *testing.T) {
+	var g Graph
+	g.Add(1)
+	g.Add(2)
+	g.Add(3)
+	g.Connect(BasicEdge(1, 3))
+	g.Remove(3)
+
+	actual := strings.TrimSpace(g.String())
+	expected := strings.TrimSpace(testGraphRemoveStr)
+	if actual != expected {
+		t.Fatalf("bad: %s", actual)
+	}
+}
+
 const testGraphBasicStr = `
 1
   3
@@ -42,4 +57,9 @@ const testGraphEmptyStr = `
 1
 2
 3
+`
+
+const testGraphRemoveStr = `
+1
+2
 `
