dd795798544d11459eb824d0a3ce07eeab214764
INADA Naoki <songofacandy@gmail.com>
2015-02-04 01:54:14 +0900

    regenerate with new stringer.

--- helper/schema/valuetype_string.go
+++ helper/schema/valuetype_string.go
@@ -6,16 +6,11 @@ import "fmt"
 
 const _ValueType_name = "TypeInvalidTypeBoolTypeIntTypeFloatTypeStringTypeListTypeMapTypeSettypeObject"
 
-var _ValueType_index = [...]uint8{11, 19, 26, 35, 45, 53, 60, 67, 77}
+var _ValueType_index = [...]uint8{0, 11, 19, 26, 35, 45, 53, 60, 67, 77}
 
 func (i ValueType) String() string {
-	if i < 0 || i >= ValueType(len(_ValueType_index)) {
+	if i < 0 || i+1 >= ValueType(len(_ValueType_index)) {
 		return fmt.Sprintf("ValueType(%d)", i)
 	}
-	hi := _ValueType_index[i]
-	lo := uint8(0)
-	if i > 0 {
-		lo = _ValueType_index[i-1]
-	}
-	return _ValueType_name[lo:hi]
+	return _ValueType_name[_ValueType_index[i]:_ValueType_index[i+1]]
 }
