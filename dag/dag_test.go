18cea36698b09c490067ef6f7ba149822cd17f57
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-04 19:38:38 -0500

    dag: fix walk order issue, scc issues

--- dag/dag_test.go
+++ dag/dag_test.go
@@ -95,8 +95,8 @@ func TestAcyclicGraphWalk(t *testing.T) {
 	}
 
 	expected := [][]Vertex{
-		{3, 1, 2},
-		{3, 2, 1},
+		{1, 2, 3},
+		{2, 1, 3},
 	}
 	for _, e := range expected {
 		if reflect.DeepEqual(visits, e) {
