af8d33ddb8021d6d1d31cf02253d87fbadd8b682
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-07 15:37:17 -0800

    dag: Replace

--- dag/graph_test.go
+++ dag/graph_test.go
@@ -47,12 +47,29 @@ func TestGraph_remove(t *testing.T) {
 	}
 }
 
+func TestGraph_replace(t *testing.T) {
+	var g Graph
+	g.Add(1)
+	g.Add(2)
+	g.Add(3)
+	g.Connect(BasicEdge(1, 2))
+	g.Connect(BasicEdge(2, 3))
+	g.Replace(2, 42)
+
+	actual := strings.TrimSpace(g.String())
+	expected := strings.TrimSpace(testGraphReplaceStr)
+	if actual != expected {
+		t.Fatalf("bad: %s", actual)
+	}
+}
+
 const testGraphBasicStr = `
 1
   3
 2
 3
 `
+
 const testGraphEmptyStr = `
 1
 2
@@ -62,4 +79,12 @@ const testGraphEmptyStr = `
 const testGraphRemoveStr = `
 1
 2
+`
+
+const testGraphReplaceStr = `
+1
+  42
+3
+42
+  3
 `
