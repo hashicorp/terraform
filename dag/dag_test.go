d36a6aef69263b136fedc4f3404cc9f08a491c87
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-08 17:06:17 -0800

    dag: walk should be able to be halted

--- dag/dag_test.go
+++ dag/dag_test.go
@@ -1,6 +1,7 @@
 package dag
 
 import (
+	"fmt"
 	"reflect"
 	"sync"
 	"testing"
@@ -96,10 +97,11 @@ func TestAcyclicGraphWalk(t *testing.T) {
 
 	var visits []Vertex
 	var lock sync.Mutex
-	err := g.Walk(func(v Vertex) {
+	err := g.Walk(func(v Vertex) error {
 		lock.Lock()
 		defer lock.Unlock()
 		visits = append(visits, v)
+		return nil
 	})
 	if err != nil {
 		t.Fatalf("err: %s", err)
@@ -108,6 +110,43 @@ func TestAcyclicGraphWalk(t *testing.T) {
 	expected := [][]Vertex{
 		{1, 2, 3},
 		{2, 1, 3},
+	}
+	for _, e := range expected {
+		if reflect.DeepEqual(visits, e) {
+			return
+		}
+	}
+
+	t.Fatalf("bad: %#v", visits)
+}
+
+func TestAcyclicGraphWalk_error(t *testing.T) {
+	var g AcyclicGraph
+	g.Add(1)
+	g.Add(2)
+	g.Add(3)
+	g.Connect(BasicEdge(3, 2))
+	g.Connect(BasicEdge(3, 1))
+
+	var visits []Vertex
+	var lock sync.Mutex
+	err := g.Walk(func(v Vertex) error {
+		lock.Lock()
+		defer lock.Unlock()
+
+		if v == 2 {
+			return fmt.Errorf("error")
+		}
+
+		visits = append(visits, v)
+		return nil
+	})
+	if err == nil {
+		t.Fatal("should error")
+	}
+
+	expected := [][]Vertex{
+		{1},
 	}
 	for _, e := range expected {
 		if reflect.DeepEqual(visits, e) {
