a5040ecc032e314229ca0393dbac9f8affcb28e1
Clint Shryock <clint@ctshryock.com>
2015-02-11 10:59:21 -0800

    Update hashcode to always generate a positive

--- helper/hashcode/hashcode_test.go
+++ helper/hashcode/hashcode_test.go
@@ -1,8 +1,6 @@
 package hashcode
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestString(t *testing.T) {
 	v := "hello, world"
@@ -10,7 +8,17 @@ func TestString(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		actual := String(v)
 		if actual != expected {
-			t.Fatalf("bad: %#v", actual)
+			t.Fatalf("bad: %#v\n\t%#v", actual, expected)
+		}
+	}
+}
+
+func TestString_positiveIndex(t *testing.T) {
+	ips := []string{"192.168.1.3", "192.168.1.5"}
+	for _, ip := range ips {
+		index := String(ip)
+		if index < 0 {
+			t.Fatalf("Bad Index %#v for ip %s", index, ip)
 		}
 	}
 }
