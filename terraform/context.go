aae2d4c7800d7a8fca48f55b7fdbbedf2b4b3170
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-11 15:22:03 -0800

    terraform: starting up the plans

--- terraform/context.go
+++ terraform/context.go
@@ -26,6 +26,7 @@ type ContextOpts struct {
 // perform operations on infrastructure. This structure is built using
 // NewContext. See the documentation for that.
 type Context2 struct {
+	diff         *Diff
 	hooks        []Hook
 	module       *module.Tree
 	providers    map[string]ResourceProviderFactory
@@ -41,12 +42,19 @@ type Context2 struct {
 // should not be mutated in any way, since the pointers are copied, not
 // the values themselves.
 func NewContext2(opts *ContextOpts) *Context2 {
+	state := opts.State
+	if state == nil {
+		state = new(State)
+		state.init()
+	}
+
 	return &Context2{
+		diff:         opts.Diff,
 		hooks:        opts.Hooks,
 		module:       opts.Module,
 		providers:    opts.Providers,
 		provisioners: opts.Provisioners,
-		state:        opts.State,
+		state:        state,
 		variables:    opts.Variables,
 	}
 }
@@ -71,6 +79,51 @@ func (c *Context2) GraphBuilder() GraphBuilder {
 		Provisioners: provisioners,
 		State:        c.state,
 	}
+}
+
+// Plan generates an execution plan for the given context.
+//
+// The execution plan encapsulates the context and can be stored
+// in order to reinstantiate a context later for Apply.
+//
+// Plan also updates the diff of this context to be the diff generated
+// by the plan, so Apply can be called after.
+func (c *Context2) Plan(opts *PlanOpts) (*Plan, error) {
+	p := &Plan{
+		Module: c.module,
+		Vars:   c.variables,
+		State:  c.state,
+	}
+
+	var operation walkOperation
+	if opts != nil && opts.Destroy {
+		operation = walkPlanDestroy
+	} else {
+		// Set our state to be something temporary. We do this so that
+		// the plan can update a fake state so that variables work, then
+		// we replace it back with our old state.
+		old := c.state
+		if old == nil {
+			c.state = &State{}
+			c.state.init()
+		} else {
+			c.state = old.deepcopy()
+		}
+		defer func() {
+			c.state = old
+		}()
+
+		operation = walkPlan
+	}
+
+	// Do the walk
+	walker, err := c.walk(operation)
+	p.Diff = walker.Diff
+
+	// Update the diff so that our context is up-to-date
+	c.diff = p.Diff
+
+	return p, err
 }
 
 // Refresh goes through all the resources in the state and refreshes them
