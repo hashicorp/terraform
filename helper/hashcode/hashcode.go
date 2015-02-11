7f8bb6a7eb61794ac6f891545674c6987b3262ed
Clint Shryock <clint@ctshryock.com>
2015-02-11 10:59:21 -0800

    Update hashcode to always generate a positive

--- helper/hashcode/hashcode.go
+++ helper/hashcode/hashcode.go
@@ -1,10 +1,16 @@
 package hashcode
 
-import (
-	"hash/crc32"
-)
+import "hash/crc32"
 
 // String hashes a string to a unique hashcode.
+//
+// crc32 returns a uint32, but for our use we need
+// and non negative integer. Here we cast to an integer
+// and invert it if the result is negative.
 func String(s string) int {
-	return int(crc32.ChecksumIEEE([]byte(s)))
+	v := int(crc32.ChecksumIEEE([]byte(s)))
+	if v < 0 {
+		return -v
+	}
+	return v
 }
