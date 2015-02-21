ec882c205fc63af879100c5f7a16748898de612c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-21 12:25:10 -0800

    state: cache state test

--- state/cache.go
+++ state/cache.go
@@ -1,6 +1,8 @@
 package state
 
 import (
+	"reflect"
+
 	"github.com/hashicorp/terraform/terraform"
 )
 
@@ -10,7 +12,8 @@ type CacheState struct {
 	Cache   CacheStateCache
 	Durable CacheStateDurable
 
-	state *terraform.State
+	refreshResult CacheRefreshResult
+	state         *terraform.State
 }
 
 // StateReader impl.
@@ -26,6 +29,7 @@ func (s *CacheState) WriteState(state *terraform.State) error {
 		return err
 	}
 
+	s.state = state
 	return s.Cache.PersistState()
 }
 
@@ -38,7 +42,77 @@ func (s *CacheState) WriteState(state *terraform.State) error {
 //
 // StateRefresher impl.
 func (s *CacheState) RefreshState() error {
+	// Refresh the durable state
+	if err := s.Durable.RefreshState(); err != nil {
+		return err
+	}
+
+	// Refresh the cached state
+	if err := s.Cache.RefreshState(); err != nil {
+		return err
+	}
+
+	// Handle the matrix of cases that can happen when comparing these
+	// two states.
+	cached := s.Cache.State()
+	durable := s.Durable.State()
+	switch {
+	case cached == nil && durable == nil:
+		// Initialized
+		s.refreshResult = CacheRefreshInit
+	case cached != nil && durable == nil:
+		// Cache is newer than remote. Not a big deal, user can just
+		// persist to get correct state.
+		s.refreshResult = CacheRefreshLocalNewer
+	case cached == nil && durable != nil:
+		// Cache should be updated since the remote is set but cache isn't
+		s.refreshResult = CacheRefreshUpdateLocal
+	case durable.Serial < cached.Serial:
+		// Cache is newer than remote. Not a big deal, user can just
+		// persist to get correct state.
+		s.refreshResult = CacheRefreshLocalNewer
+	case durable.Serial > cached.Serial:
+		// Cache should be updated since the remote is newer
+		s.refreshResult = CacheRefreshUpdateLocal
+	case durable.Serial == cached.Serial:
+		// They're supposedly equal, verify.
+		if reflect.DeepEqual(cached, durable) {
+			// Hashes are the same, everything is great
+			s.refreshResult = CacheRefreshNoop
+			break
+		}
+
+		// This is very bad. This means we have two state files that
+		// have the same serial but have a different hash. We can't
+		// reconcile this. The most likely cause is parallel apply
+		// operations.
+		s.refreshResult = CacheRefreshConflict
+
+		// Return early so we don't updtae the state
+		return nil
+	default:
+		panic("unhandled cache refresh state")
+	}
+
+	if s.refreshResult == CacheRefreshUpdateLocal {
+		if err := s.Cache.WriteState(durable); err != nil {
+			s.refreshResult = CacheRefreshNoop
+			return err
+		}
+		if err := s.Cache.PersistState(); err != nil {
+			s.refreshResult = CacheRefreshNoop
+			return err
+		}
+	}
+
+	s.state = cached
+
 	return nil
+}
+
+// RefreshResult returns the result of the last refresh.
+func (s *CacheState) RefreshResult() CacheRefreshResult {
+	return s.refreshResult
 }
 
 // PersistState takes the local cache, assuming it is newer than the remote
@@ -61,11 +135,57 @@ type CacheStateCache interface {
 	StateReader
 	StateWriter
 	StatePersister
+	StateRefresher
 }
 
 // CacheStateDurable is the meta-interface that must be implemented for
 // the durable storage for CacheState.
 type CacheStateDurable interface {
+	StateReader
 	StateWriter
 	StatePersister
+	StateRefresher
 }
+
+// CacheRefreshResult is used to explain the result of the previous
+// RefreshState for a CacheState.
+type CacheRefreshResult int
+
+const (
+	// CacheRefreshNoop indicates nothing has happened,
+	// but that does not indicate an error. Everything is
+	// just up to date. (Push/Pull)
+	CacheRefreshNoop CacheRefreshResult = iota
+
+	// CacheRefreshInit indicates that there is no local or
+	// remote state, and that the state was initialized
+	CacheRefreshInit
+
+	// CacheRefreshUpdateLocal indicates the local state
+	// was updated. (Pull)
+	CacheRefreshUpdateLocal
+
+	// CacheRefreshUpdateRemote indicates the remote state
+	// was updated. (Push)
+	CacheRefreshUpdateRemote
+
+	// CacheRefreshLocalNewer means the pull was a no-op
+	// because the local state is newer than that of the
+	// server. This means a Push should take place. (Pull)
+	CacheRefreshLocalNewer
+
+	// CacheRefreshRemoteNewer means the push was a no-op
+	// because the remote state is newer than that of the
+	// local state. This means a Pull should take place.
+	// (Push)
+	CacheRefreshRemoteNewer
+
+	// CacheRefreshConflict means that the push or pull
+	// was a no-op because there is a conflict. This means
+	// there are multiple state definitions at the same
+	// serial number with different contents. This requires
+	// an operator to intervene and resolve the conflict.
+	// Shame on the user for doing concurrent apply.
+	// (Push/Pull)
+	CacheRefreshConflict
+)
