b8836ff279a2f8baebefb9dfb3dcc843950b5d4e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-21 15:13:28 -0800

    state/remote: consul client

--- state/remote/remote.go
+++ state/remote/remote.go
@@ -1,5 +1,9 @@
 package remote
 
+import (
+	"fmt"
+)
+
 // Client is the interface that must be implemented for a remote state
 // driver. It supports dumb put/get/delete, and the higher level structs
 // handle persisting the state properly here.
@@ -17,3 +21,20 @@ type Payload struct {
 
 // Factory is the factory function to create a remote client.
 type Factory func(map[string]string) (Client, error)
+
+// NewClient returns a new Client with the given type and configuration.
+// The client is looked up in the BuiltinClients variable.
+func NewClient(t string, conf map[string]string) (Client, error) {
+	f, ok := BuiltinClients[t]
+	if !ok {
+		return nil, fmt.Errorf("unknown remote client type: %s", t)
+	}
+
+	return f(conf)
+}
+
+// BuiltinClients is the list of built-in clients that can be used with
+// NewClient.
+var BuiltinClients = map[string]Factory{
+	"consul": consulFactory,
+}
