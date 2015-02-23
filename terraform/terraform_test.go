31348c87707f783c9fd852d14e89eba01f5e1aac
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-23 15:02:26 -0800

    terraform: more self tests

--- terraform/terraform_test.go
+++ terraform/terraform_test.go
@@ -366,6 +366,21 @@ aws_instance.foo:
   type = aws_instance
 `
 
+const testTerraformApplyProvisionerMultiSelfRefStr = `
+aws_instance.foo.0:
+  ID = foo
+  foo = number 0
+  type = aws_instance
+aws_instance.foo.1:
+  ID = foo
+  foo = number 1
+  type = aws_instance
+aws_instance.foo.2:
+  ID = foo
+  foo = number 2
+  type = aws_instance
+`
+
 const testTerraformApplyProvisionerDiffStr = `
 aws_instance.bar:
   ID = foo
