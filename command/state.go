8ef840ba7f3814a44e413bd0714d900ca9e85a71
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-02-23 10:20:40 -0800

    command/remote: no more remote package

--- command/state.go
+++ command/state.go
@@ -21,12 +21,37 @@ type StateOpts struct {
 	LocalPathOut string
 
 	// RemotePath is the path where the remote state cache would be.
-	RemotePath string
+	//
+	// RemoteCache, if true, will set the result to only be the cache
+	// and not backed by any real durable storage.
+	RemotePath      string
+	RemoteCacheOnly bool
+	RemoteRefresh   bool
 
 	// BackupPath is the path where the backup will be placed. If not set,
 	// it is assumed to be the path where the state is stored locally
 	// plus the DefaultBackupExtension.
 	BackupPath string
+}
+
+// StateResult is the result of calling State and holds various different
+// State implementations so they can be accessed directly.
+type StateResult struct {
+	// State is the final outer state that should be used for all
+	// _real_ reads/writes.
+	//
+	// StatePath is the local path where the state will be stored or
+	// cached, no matter whether State is local or remote.
+	State     state.State
+	StatePath string
+
+	// Local and Remote are the local/remote state implementations, raw
+	// and unwrapped by any backups. The paths here are the paths where
+	// these state files would be saved.
+	Local      *state.LocalState
+	LocalPath  string
+	Remote     *state.CacheState
+	RemotePath string
 }
 
 // State returns the proper state.State implementation to represent the
@@ -35,19 +60,48 @@ type StateOpts struct {
 // localPath is the path to where state would be if stored locally.
 // dataDir is the path to the local data directory where the remote state
 // cache would be stored.
-func State(opts *StateOpts) (state.State, string, error) {
-	var result state.State
-	var resultPath string
+func State(opts *StateOpts) (*StateResult, error) {
+	result := new(StateResult)
 
 	// Get the remote state cache path
 	if opts.RemotePath != "" {
-		if _, err := os.Stat(opts.RemotePath); err == nil {
-			// We have a remote state, initialize that.
-			result, err = remoteStateFromPath(opts.RemotePath)
-			if err != nil {
-				return nil, "", err
+		result.RemotePath = opts.RemotePath
+
+		var remote *state.CacheState
+		if opts.RemoteCacheOnly {
+			// Setup the in-memory state
+			ls := &state.LocalState{Path: opts.RemotePath}
+			if err := ls.RefreshState(); err != nil {
+				return nil, err
 			}
-			resultPath = opts.RemotePath
+			is := &state.InmemState{}
+			is.WriteState(ls.State())
+
+			// Setupt he remote state, cache-only, and refresh it so that
+			// we have access to the state right away.
+			remote = &state.CacheState{
+				Cache:   ls,
+				Durable: is,
+			}
+			if err := remote.RefreshState(); err != nil {
+				return nil, err
+			}
+		} else {
+			if _, err := os.Stat(opts.RemotePath); err == nil {
+				// We have a remote state, initialize that.
+				remote, err = remoteStateFromPath(
+					opts.RemotePath,
+					opts.RemoteRefresh)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		if remote != nil {
+			result.State = remote
+			result.StatePath = opts.RemotePath
+			result.Remote = remote
 		}
 	}
 
@@ -57,22 +111,20 @@ func State(opts *StateOpts) (state.State, string, error) {
 			Path:    opts.LocalPath,
 			PathOut: opts.LocalPathOut,
 		}
+
+		// Always store it in the result even if we're not using it
+		result.Local = local
+		result.LocalPath = local.Path
+		if local.PathOut != "" {
+			result.LocalPath = local.PathOut
+		}
+
 		err := local.RefreshState()
-		if err != nil {
-			isNotExist := false
-			errwrap.Walk(err, func(e error) {
-				if !isNotExist && os.IsNotExist(e) {
-					isNotExist = true
-				}
-			})
-			if isNotExist {
-				err = nil
-			}
-		} else {
-			if result != nil {
+		if err == nil {
+			if result.State != nil && !result.State.State().Empty() {
 				if !local.State().Empty() {
 					// We already have a remote state... that is an error.
-					return nil, "", fmt.Errorf(
+					return nil, fmt.Errorf(
 						"Remote state found, but state file '%s' also present.",
 						opts.LocalPath)
 				}
@@ -82,34 +134,34 @@ func State(opts *StateOpts) (state.State, string, error) {
 			}
 		}
 		if err != nil {
-			return nil, "", errwrap.Wrapf(
+			return nil, errwrap.Wrapf(
 				"Error reading local state: {{err}}", err)
 		}
 
 		if local != nil {
-			result = local
-			resultPath = opts.LocalPath
+			result.State = local
+			result.StatePath = opts.LocalPath
 			if opts.LocalPathOut != "" {
-				resultPath = opts.LocalPathOut
+				result.StatePath = opts.LocalPathOut
 			}
 		}
 	}
 
 	// If we have a result, make sure to back it up
-	if result != nil {
-		backupPath := resultPath + DefaultBackupExtention
+	if result.State != nil {
+		backupPath := result.StatePath + DefaultBackupExtention
 		if opts.BackupPath != "" {
 			backupPath = opts.BackupPath
 		}
 
-		result = &state.BackupState{
-			Real: result,
+		result.State = &state.BackupState{
+			Real: result.State,
 			Path: backupPath,
 		}
 	}
 
 	// Return whatever state we have
-	return result, resultPath, nil
+	return result, nil
 }
 
 // StateFromPlan gets our state from the plan.
@@ -147,7 +199,7 @@ func StateFromPlan(
 
 func remoteState(
 	local *terraform.State,
-	localPath string, refresh bool) (state.State, error) {
+	localPath string, refresh bool) (*state.CacheState, error) {
 	// If there is no remote settings, it is an error
 	if local.Remote == nil {
 		return nil, fmt.Errorf("Remote state cache has no remote info")
@@ -199,7 +251,7 @@ func remoteState(
 	return cache, nil
 }
 
-func remoteStateFromPath(path string) (state.State, error) {
+func remoteStateFromPath(path string, refresh bool) (*state.CacheState, error) {
 	// First create the local state for the path
 	local := &state.LocalState{Path: path}
 	if err := local.RefreshState(); err != nil {
@@ -207,5 +259,5 @@ func remoteStateFromPath(path string) (state.State, error) {
 	}
 	localState := local.State()
 
-	return remoteState(localState, path, true)
+	return remoteState(localState, path, refresh)
 }
