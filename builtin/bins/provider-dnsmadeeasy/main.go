8f7aa87974c2934398b328bf6ee24a16cc4dedb7
Jack Pearkes <jackpearkes@gmail.com>
2015-02-25 14:45:11 -0800

    dnsmadeeasy: re-organize the bins
    cc/ @soniah

--- builtin/bins/provider-dnsmadeeasy/main.go
+++ builtin/bins/provider-dnsmadeeasy/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/hashicorp/terraform/builtin/providers/dnsmadeeasy"
 	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider,
+		ProviderFunc: dnsmadeeasy.Provider,
 	})
 }
