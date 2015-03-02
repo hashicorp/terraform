21aecbd14ca11d9d792df51fdddb3837a692d786
Radek Simko <radek.simko@timeinc.com>
2015-03-02 11:15:49 +0000

    Save token that came from goamz + use it in go-sdk-aws

--- builtin/providers/aws/config.go
+++ builtin/providers/aws/config.go
@@ -21,6 +21,7 @@ import (
 type Config struct {
 	AccessKey string
 	SecretKey string
+	Token     string
 	Region    string
 }
 
@@ -58,7 +59,7 @@ func (c *Config) Client() (interface{}, error) {
 		// bucket storage in S3
 		client.region = c.Region
 
-		creds := awsGo.Creds(c.AccessKey, c.SecretKey, "")
+		creds := awsGo.Creds(c.AccessKey, c.SecretKey, c.Token)
 
 		log.Println("[INFO] Initializing EC2 connection")
 		client.ec2conn = ec2.New(auth, region)
@@ -95,6 +96,7 @@ func (c *Config) AWSAuth() (aws.Auth, error) {
 		// Store the accesskey and secret that we got...
 		c.AccessKey = auth.AccessKey
 		c.SecretKey = auth.SecretKey
+		c.Token = auth.Token
 	}
 
 	return auth, err
