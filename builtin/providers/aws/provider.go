dda5c5b80be6cdf2455a808f628f6584d934614c
Dan Everton <dan.everton@wotifgroup.com>
2015-03-05 16:24:14 +1000

    Add acceptance test for aws_vpn_gateway resource.

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -67,6 +67,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_subnet":                       resourceAwsSubnet(),
 			"aws_vpc":                          resourceAwsVpc(),
 			"aws_vpc_peering_connection":       resourceAwsVpcPeeringConnection(),
+			"aws_vpn_gateway":                  resourceAwsVpnGateway(),
 		},
 
 		ConfigureFunc: providerConfigure,
