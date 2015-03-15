16067a51987dc9a57506fbb006476f0df46316ec
Tarrant <tarrant@keyneston.com>
2015-03-15 16:12:25 -0700

    Add SSH Agent support

--- builtin/provisioners/remote-exec/resource_provisioner.go
+++ builtin/provisioners/remote-exec/resource_provisioner.go
@@ -178,10 +178,13 @@ func (p *ResourceProvisioner) runScripts(
 			"  Host: %s\n"+
 			"  User: %s\n"+
 			"  Password: %v\n"+
-			"  Private key: %v",
+			"  Private key: %v"+
+			"  SSH Agent: %v",
 		conf.Host, conf.User,
 		conf.Password != "",
-		conf.KeyFile != ""))
+		conf.KeyFile != "",
+		conf.Agent,
+	))
 
 	// Wait and retry until we establish the SSH connection
 	var comm *helper.SSHCommunicator
