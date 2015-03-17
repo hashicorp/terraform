e4214a998380e6872e01d8d6a0d6b9a45b91509d
Peter Beams <peter.beams@wiggle.co.uk>
2015-03-17 13:00:36 +0000

    ran go fmt and made 1 fix after running tests again

--- builtin/providers/aws/resource_aws_network_interface.go
+++ builtin/providers/aws/resource_aws_network_interface.go
@@ -1,17 +1,17 @@
 package aws
 
 import (
-	"bytes"	
+	"bytes"
 	"fmt"
 	"log"
 	"strconv"
 	"time"
-	
+
 	"github.com/hashicorp/aws-sdk-go/aws"
-	"github.com/hashicorp/terraform/helper/hashcode"	
+	"github.com/hashicorp/aws-sdk-go/gen/ec2"
+	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/aws-sdk-go/gen/ec2"
 )
 
 func resourceAwsNetworkInterface() *schema.Resource {
@@ -21,18 +21,18 @@ func resourceAwsNetworkInterface() *schema.Resource {
 		Update: resourceAwsNetworkInterfaceUpdate,
 		Delete: resourceAwsNetworkInterfaceDelete,
 
-		Schema: map[string]*schema.Schema{			
-			
+		Schema: map[string]*schema.Schema{
+
 			"subnet_id": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,				
+				Required: true,
 				ForceNew: true,
 			},
 
 			"private_ips": &schema.Schema{
 				Type:     schema.TypeSet,
 				Optional: true,
-				ForceNew: true,				
+				ForceNew: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Set: func(v interface{}) int {
 					return hashcode.String(v.(string))
@@ -42,7 +42,7 @@ func resourceAwsNetworkInterface() *schema.Resource {
 			"security_groups": &schema.Schema{
 				Type:     schema.TypeSet,
 				Optional: true,
-				Computed: true,				
+				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Set: func(v interface{}) int {
 					return hashcode.String(v.(string))
@@ -63,29 +63,29 @@ func resourceAwsNetworkInterface() *schema.Resource {
 							Required: true,
 						},
 						"attachment_id": &schema.Schema{
-							Type:     schema.TypeString,	
-							Computed: true,						
+							Type:     schema.TypeString,
+							Computed: true,
 						},
 					},
 				},
 				Set: resourceAwsEniAttachmentHash,
 			},
 
-			"tags": tagsSchema(),			
+			"tags": tagsSchema(),
 		},
 	}
 }
 
 func resourceAwsNetworkInterfaceCreate(d *schema.ResourceData, meta interface{}) error {
-	
+
 	ec2conn := meta.(*AWSClient).ec2conn
 
-	request := &ec2.CreateNetworkInterfaceRequest{		
-		Groups: 				expandStringList(d.Get("security_groups").(*schema.Set).List()),
-		SubnetID:				aws.String(d.Get("subnet_id").(string)),
-		PrivateIPAddresses:		expandPrivateIPAddesses(d.Get("private_ips").(*schema.Set).List()),
+	request := &ec2.CreateNetworkInterfaceRequest{
+		Groups:             expandStringList(d.Get("security_groups").(*schema.Set).List()),
+		SubnetID:           aws.String(d.Get("subnet_id").(string)),
+		PrivateIPAddresses: expandPrivateIPAddesses(d.Get("private_ips").(*schema.Set).List()),
 	}
-	
+
 	log.Printf("[DEBUG] Creating network interface")
 	resp, err := ec2conn.CreateNetworkInterface(request)
 	if err != nil {
@@ -95,14 +95,14 @@ func resourceAwsNetworkInterfaceCreate(d *schema.ResourceData, meta interface{})
 	d.SetId(*resp.NetworkInterface.NetworkInterfaceID)
 	log.Printf("[INFO] ENI ID: %s", d.Id())
 
-	return resourceAwsNetworkInterfaceUpdate(d, meta)	
+	return resourceAwsNetworkInterfaceUpdate(d, meta)
 }
 
 func resourceAwsNetworkInterfaceRead(d *schema.ResourceData, meta interface{}) error {
 
 	ec2conn := meta.(*AWSClient).ec2conn
-	describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesRequest{		
-		NetworkInterfaceIDs:	[]string{d.Id()},
+	describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesRequest{
+		NetworkInterfaceIDs: []string{d.Id()},
 	}
 	describeResp, err := ec2conn.DescribeNetworkInterfaces(describe_network_interfaces_request)
 
@@ -112,7 +112,7 @@ func resourceAwsNetworkInterfaceRead(d *schema.ResourceData, meta interface{}) e
 			d.SetId("")
 			return nil
 		}
-		
+
 		return fmt.Errorf("Error retrieving ENI: %s", err)
 	}
 	if len(describeResp.NetworkInterfaces) != 1 {
@@ -125,41 +125,42 @@ func resourceAwsNetworkInterfaceRead(d *schema.ResourceData, meta interface{}) e
 	d.Set("security_groups", flattenGroupIdentifiers(eni.Groups))
 
 	if eni.Attachment != nil {
-		d.Set("attachment", flattenAttachment(eni.Attachment))
+		attachment := []map[string]interface{} { flattenAttachment(eni.Attachment) }
+		d.Set("attachment", attachment)
 	} else {
 		d.Set("attachment", nil)
 	}
-	
+
 	return nil
 }
 
 func networkInterfaceAttachmentRefreshFunc(ec2conn *ec2.EC2, id string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
 
-		describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesRequest{		
-			NetworkInterfaceIDs:	[]string{id},
+		describe_network_interfaces_request := &ec2.DescribeNetworkInterfacesRequest{
+			NetworkInterfaceIDs: []string{id},
 		}
 		describeResp, err := ec2conn.DescribeNetworkInterfaces(describe_network_interfaces_request)
 
-		if err != nil {			
-			log.Printf("[ERROR] Could not find network interface %s. %s", id, err)					
+		if err != nil {
+			log.Printf("[ERROR] Could not find network interface %s. %s", id, err)
 			return nil, "", err
 		}
 
 		eni := describeResp.NetworkInterfaces[0]
-		hasAttachment := strconv.FormatBool(eni.Attachment != nil)		
-		log.Printf("[DEBUG] ENI %s has attachment state %s", id, hasAttachment)					
+		hasAttachment := strconv.FormatBool(eni.Attachment != nil)
+		log.Printf("[DEBUG] ENI %s has attachment state %s", id, hasAttachment)
 		return eni, hasAttachment, nil
 	}
 }
 
 func resourceAwsNetworkInterfaceDetach(oa *schema.Set, meta interface{}, eniId string) error {
 	// if there was an old attachment, remove it
-	if oa != nil && len(oa.List()) > 0 {			
+	if oa != nil && len(oa.List()) > 0 {
 		old_attachment := oa.List()[0].(map[string]interface{})
 		detach_request := &ec2.DetachNetworkInterfaceRequest{
-			AttachmentID: 	aws.String(old_attachment["attachment_id"].(string)),
-			Force:			aws.Boolean(true),
+			AttachmentID: aws.String(old_attachment["attachment_id"].(string)),
+			Force:        aws.Boolean(true),
 		}
 		ec2conn := meta.(*AWSClient).ec2conn
 		detach_err := ec2conn.DetachNetworkInterface(detach_request)
@@ -175,9 +176,9 @@ func resourceAwsNetworkInterfaceDetach(oa *schema.Set, meta interface{}, eniId s
 			Timeout: 10 * time.Minute,
 		}
 		if _, err := stateConf.WaitForState(); err != nil {
-		return fmt.Errorf(
-			"Error waiting for ENI (%s) to become dettached: %s", eniId, err)
-	}
+			return fmt.Errorf(
+				"Error waiting for ENI (%s) to become dettached: %s", eniId, err)
+		}
 	}
 
 	return nil
@@ -189,20 +190,20 @@ func resourceAwsNetworkInterfaceUpdate(d *schema.ResourceData, meta interface{})
 
 	if d.HasChange("attachment") {
 		ec2conn := meta.(*AWSClient).ec2conn
-		oa, na := d.GetChange("attachment")			
-		
+		oa, na := d.GetChange("attachment")
+
 		detach_err := resourceAwsNetworkInterfaceDetach(oa.(*schema.Set), meta, d.Id())
 		if detach_err != nil {
 			return detach_err
 		}
 
 		// if there is a new attachment, attach it
-		if na != nil && len(na.(*schema.Set).List()) > 0 {				
+		if na != nil && len(na.(*schema.Set).List()) > 0 {
 			new_attachment := na.(*schema.Set).List()[0].(map[string]interface{})
 			attach_request := &ec2.AttachNetworkInterfaceRequest{
-				DeviceIndex:		aws.Integer(new_attachment["device_index"].(int)),
-				InstanceID:			aws.String(new_attachment["instance"].(string)),
-				NetworkInterfaceID:	aws.String(d.Id()),
+				DeviceIndex:        aws.Integer(new_attachment["device_index"].(int)),
+				InstanceID:         aws.String(new_attachment["instance"].(string)),
+				NetworkInterfaceID: aws.String(d.Id()),
 			}
 			_, attach_err := ec2conn.AttachNetworkInterface(attach_request)
 			if attach_err != nil {
@@ -215,8 +216,8 @@ func resourceAwsNetworkInterfaceUpdate(d *schema.ResourceData, meta interface{})
 
 	if d.HasChange("security_groups") {
 		request := &ec2.ModifyNetworkInterfaceAttributeRequest{
-			NetworkInterfaceID:		aws.String(d.Id()),
-			Groups:					expandStringList(d.Get("security_groups").(*schema.Set).List()),
+			NetworkInterfaceID: aws.String(d.Id()),
+			Groups:             expandStringList(d.Get("security_groups").(*schema.Set).List()),
 		}
 
 		ec2conn := meta.(*AWSClient).ec2conn
