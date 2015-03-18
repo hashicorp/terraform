e903d59c1827c1d1f36ea5eaae10af0d699ae1b5
Clint Shryock <clint@ctshryock.com>
2015-03-18 10:50:36 -0500

    provider/aws: Fix issue when changing map-public-ip in Subnets

--- builtin/providers/aws/resource_aws_subnet.go
+++ builtin/providers/aws/resource_aws_subnet.go
@@ -133,8 +133,10 @@ func resourceAwsSubnetUpdate(d *schema.ResourceData, meta interface{}) error {
 
 	if d.HasChange("map_public_ip_on_launch") {
 		modifyOpts := &ec2.ModifySubnetAttributeRequest{
-			SubnetID:            aws.String(d.Id()),
-			MapPublicIPOnLaunch: &ec2.AttributeBooleanValue{aws.Boolean(true)},
+			SubnetID: aws.String(d.Id()),
+			MapPublicIPOnLaunch: &ec2.AttributeBooleanValue{
+				aws.Boolean(d.Get("map_public_ip_on_launch").(bool)),
+			},
 		}
 
 		log.Printf("[DEBUG] Subnet modify attributes: %#v", modifyOpts)
