4e19aa05a0c9a3a412905eecfe02ea5e1d9c56d1
Paul Hinze <paul@hashicorp.com>
2015-03-19 11:40:48 -0500

    providers/aws: fix blockdevices acceptance test
    hashcodes just needed updating from latest hash func tweaks

--- builtin/providers/aws/resource_aws_instance_test.go
+++ builtin/providers/aws/resource_aws_instance_test.go
@@ -111,33 +111,33 @@ func TestAccAWSInstance_blockDevices(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"aws_instance.foo", "root_block_device.#", "1"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "root_block_device.3018388612.device_name", "/dev/sda1"),
+						"aws_instance.foo", "root_block_device.1246122048.device_name", "/dev/sda1"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "root_block_device.3018388612.volume_size", "11"),
+						"aws_instance.foo", "root_block_device.1246122048.volume_size", "11"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "root_block_device.3018388612.volume_type", "gp2"),
+						"aws_instance.foo", "root_block_device.1246122048.volume_type", "gp2"),
 					resource.TestCheckResourceAttr(
 						"aws_instance.foo", "ebs_block_device.#", "2"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.418220885.device_name", "/dev/sdb"),
+						"aws_instance.foo", "ebs_block_device.2225977507.device_name", "/dev/sdb"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.418220885.volume_size", "9"),
+						"aws_instance.foo", "ebs_block_device.2225977507.volume_size", "9"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.418220885.volume_type", "standard"),
+						"aws_instance.foo", "ebs_block_device.2225977507.volume_type", "standard"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.1877654467.device_name", "/dev/sdc"),
+						"aws_instance.foo", "ebs_block_device.1977224956.device_name", "/dev/sdc"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.1877654467.volume_size", "10"),
+						"aws_instance.foo", "ebs_block_device.1977224956.volume_size", "10"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.1877654467.volume_type", "io1"),
+						"aws_instance.foo", "ebs_block_device.1977224956.volume_type", "io1"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ebs_block_device.1877654467.iops", "100"),
+						"aws_instance.foo", "ebs_block_device.1977224956.iops", "100"),
 					resource.TestCheckResourceAttr(
 						"aws_instance.foo", "ephemeral_block_device.#", "1"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ephemeral_block_device.2087552357.device_name", "/dev/sde"),
+						"aws_instance.foo", "ephemeral_block_device.1692014856.device_name", "/dev/sde"),
 					resource.TestCheckResourceAttr(
-						"aws_instance.foo", "ephemeral_block_device.2087552357.virtual_name", "ephemeral0"),
+						"aws_instance.foo", "ephemeral_block_device.1692014856.virtual_name", "ephemeral0"),
 					testCheck(),
 				),
 			},
