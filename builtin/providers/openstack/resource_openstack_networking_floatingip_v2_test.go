1388421ada108bcb797f53a51cd7f1fa668ee159
Joe Topjian <joe@topjian.net>
2015-04-01 22:54:09 +0000

    Updating Floating IP acceptance tests

--- builtin/providers/openstack/resource_openstack_networking_floatingip_v2_test.go
+++ builtin/providers/openstack/resource_openstack_networking_floatingip_v2_test.go
@@ -2,11 +2,13 @@ package openstack
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 
+	"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
 	"github.com/rackspace/gophercloud/openstack/networking/v2/extensions/layer3/floatingips"
 )
 
@@ -22,6 +24,40 @@ func TestAccNetworkingV2FloatingIP_basic(t *testing.T) {
 				Config: testAccNetworkingV2FloatingIP_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckNetworkingV2FloatingIPExists(t, "openstack_networking_floatingip_v2.foo", &floatingIP),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNetworkingV2FloatingIP_attach(t *testing.T) {
+	var instance servers.Server
+	var fip floatingips.FloatingIP
+	var testAccNetworkV2FloatingIP_attach = fmt.Sprintf(`
+    resource "openstack_networking_floatingip_v2" "myip" {
+    }
+
+    resource "openstack_compute_instance_v2" "foo" {
+      name = "terraform-test"
+      floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
+
+      network {
+        uuid = "%s"
+      }
+    }`,
+		os.Getenv("OS_NETWORK_ID"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNetworkingV2FloatingIPDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkV2FloatingIP_attach,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkingV2FloatingIPExists(t, "openstack_networking_floatingip_v2.myip", &fip),
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+					testAccCheckNetworkingV2InstanceFloatingIPAttach(&instance, &fip),
 				),
 			},
 		},
@@ -81,11 +117,28 @@ func testAccCheckNetworkingV2FloatingIPExists(t *testing.T, n string, kp *floati
 	}
 }
 
+func testAccCheckNetworkingV2InstanceFloatingIPAttach(
+	instance *servers.Server, fip *floatingips.FloatingIP) resource.TestCheckFunc {
+
+	// When Neutron is used, the Instance sometimes does not know its floating IP until some time
+	// after the attachment happened. This can be anywhere from 2-20 seconds. Because of that delay,
+	// the test usually completes with failure.
+	// However, the Fixed IP is known on both sides immediately, so that can be used as a bridge
+	// to ensure the two are now related.
+	// I think a better option is to introduce some state changing config in the actual resource.
+	return func(s *terraform.State) error {
+		for _, networkAddresses := range instance.Addresses {
+			for _, element := range networkAddresses.([]interface{}) {
+				address := element.(map[string]interface{})
+				if address["OS-EXT-IPS:type"] == "fixed" && address["addr"] == fip.FixedIP {
+					return nil
+				}
+			}
+		}
+		return fmt.Errorf("Floating IP %+v was not attached to instance %+v", fip, instance)
+	}
+}
+
 var testAccNetworkingV2FloatingIP_basic = `
   resource "openstack_networking_floatingip_v2" "foo" {
-  }
-
-  resource "openstack_compute_instance_v2" "bar" {
-	name = "terraform-acc-floating-ip-test"
-	floating_ip = "${openstack_networking_floatingip_v2.foo.address}"
   }`
