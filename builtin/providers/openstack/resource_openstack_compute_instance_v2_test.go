67e33a7ac97dee558b1209b996d59f9c216386fe
Joe Topjian <joe@topjian.net>
2015-04-01 02:56:24 +0000

    Updated compute_instance acceptance tests for floating IPs

--- builtin/providers/openstack/resource_openstack_compute_instance_v2_test.go
+++ builtin/providers/openstack/resource_openstack_compute_instance_v2_test.go
@@ -2,12 +2,14 @@ package openstack
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 
 	"github.com/rackspace/gophercloud/openstack/blockstorage/v1/volumes"
+	"github.com/rackspace/gophercloud/openstack/compute/v2/extensions/floatingip"
 	"github.com/rackspace/gophercloud/openstack/compute/v2/extensions/volumeattach"
 	"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
 	"github.com/rackspace/gophercloud/pagination"
@@ -47,6 +49,40 @@ func TestAccComputeV2Instance_volumeAttach(t *testing.T) {
 					testAccCheckBlockStorageV1VolumeExists(t, "openstack_blockstorage_volume_v1.myvol", &volume),
 					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
 					testAccCheckComputeV2InstanceVolumeAttachment(&instance, &volume),
+				),
+			},
+		},
+	})
+}
+
+func TestAccComputeV2Instance_floatingIPAttach(t *testing.T) {
+	var instance servers.Server
+	var fip floatingip.FloatingIP
+	var testAccComputeV2Instance_floatingIPAttach = fmt.Sprintf(`
+		resource "openstack_compute_floatingip_v2" "myip" {
+		}
+
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			floating_ip = "${openstack_compute_floatingip_v2.myip.address}"
+
+			network {
+				name = "%s"
+			}
+		}`,
+		os.Getenv("OS_NETWORK_NAME"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2InstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Instance_floatingIPAttach,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeV2FloatingIPExists(t, "openstack_compute_floatingip_v2.myip", &fip),
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+					testAccCheckComputeV2InstanceFloatingIPAttach(&instance, &fip),
 				),
 			},
 		},
@@ -156,6 +192,18 @@ func testAccCheckComputeV2InstanceVolumeAttachment(
 		}
 
 		return fmt.Errorf("Volume not found: %s", volume.ID)
+	}
+}
+
+func testAccCheckComputeV2InstanceFloatingIPAttach(
+	instance *servers.Server, fip *floatingip.FloatingIP) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if fip.InstanceID == instance.ID {
+			return nil
+		}
+
+		return fmt.Errorf("Floating IP %s was not attached to instance %s", fip.ID, instance.ID)
+
 	}
 }
 
