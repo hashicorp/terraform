7b9affd984a09622d78b8e8cfb093eaefcfd15b9
Paul Hinze <phinze@phinze.com>
2015-04-15 10:41:56 -0500

    core: protect against count.index in modules
    Modules should get a validation error just like outputs do.
    
    refs #1528

--- config/config_test.go
+++ config/config_test.go
@@ -63,10 +63,17 @@ func TestConfigValidate_countInt(t *testing.T) {
 
 func TestConfigValidate_countBadContext(t *testing.T) {
 	c := testConfig(t, "validate-count-bad-context")
-	expected := "count variables are only valid within resources"
+
 	err := c.Validate()
-	if !strings.Contains(err.Error(), expected) {
-		t.Fatalf("expected: %q,\nto contain: %q", err, expected)
+
+	expected := []string{
+		"no_count_in_output: count variables are only valid within resources",
+		"no_count_in_module: count variables are only valid within resources",
+	}
+	for _, exp := range expected {
+		if !strings.Contains(err.Error(), exp) {
+			t.Fatalf("expected: %q,\nto contain: %q", err, exp)
+		}
 	}
 }
 
