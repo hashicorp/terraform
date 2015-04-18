de8666a5fa5e82a682d556f3acab53972c3a2c25
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-04-18 16:07:11 -0700

    helper/ssh: add random number to script [GH-1545]

--- helper/ssh/provisioner_test.go
+++ helper/ssh/provisioner_test.go
@@ -1,10 +1,40 @@
 package ssh
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform/terraform"
 )
+
+func TestSSHConfig_RemotePath(t *testing.T) {
+	cases := []struct {
+		Input   string
+		Pattern string
+	}{
+		{
+			"/tmp/script.sh",
+			`^/tmp/script\.sh$`,
+		},
+		{
+			"/tmp/script_%RAND%.sh",
+			`^/tmp/script_(\d+)\.sh$`,
+		},
+	}
+
+	for _, tc := range cases {
+		config := &SSHConfig{ScriptPath: tc.Input}
+		output := config.RemotePath()
+
+		match, err := regexp.Match(tc.Pattern, []byte(output))
+		if err != nil {
+			t.Fatalf("bad: %s\n\nerr: %s", tc.Input, err)
+		}
+		if !match {
+			t.Fatalf("bad: %s\n\n%s", tc.Input, output)
+		}
+	}
+}
 
 func TestResourceProvider_verifySSH(t *testing.T) {
 	r := &terraform.InstanceState{
