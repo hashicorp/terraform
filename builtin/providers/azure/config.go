5d065293a1f3211e4e3de729044560b5f013ae94
Sander van Harmelen <sander@xanzy.io>
2015-04-30 17:12:05 +0200

    Little refactoring and fixing some issues
    Starting to look pretty nice…

--- builtin/providers/azure/config.go
+++ builtin/providers/azure/config.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/MSOpenTech/azure-sdk-for-go/management"
+	"github.com/Azure/azure-sdk-for-go/management"
 )
 
 // Config is the configuration structure used to instantiate a
