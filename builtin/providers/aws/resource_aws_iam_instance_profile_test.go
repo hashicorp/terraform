7303568469d71bd0909eca1e490108720f9c5f62
Paul Hinze <phinze@phinze.com>
2015-05-06 09:18:41 -0500

    providers/aws: update test name

--- builtin/providers/aws/resource_aws_iam_instance_profile_test.go
+++ builtin/providers/aws/resource_aws_iam_instance_profile_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAwsIamInstanceProfile(t *testing.T) {
+func TestAccAWSIAMInstanceProfile(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
