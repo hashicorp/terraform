60984b2da207c29321d7d91416eeff28291b8924
Sander van Harmelen <sander@xanzy.io>
2015-05-08 13:45:31 +0200

    This commit adds a Chef Client provisioner
    The commit is pretty complete and has a tested/working provisioner for
    both SSH and WinRM. There are a few tests, but we maybe need another
    few to have better coverage. Docs are also included…

---
+++ builtin/provisioners/chef-client/resource_provisioner_test.go
@@ -0,0 +1,56 @@
+package chefclient
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/config"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestResourceProvisioner_impl(t *testing.T) {
+	var _ terraform.ResourceProvisioner = new(ResourceProvisioner)
+}
+
+func TestResourceProvider_Validate_good(t *testing.T) {
+	c := testConfig(t, map[string]interface{}{
+		"package":                "https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.18.6-1.el6.x86_64.rpm",
+		"run_list":               []interface{}{"cookbook::recipe"},
+		"node_name":              "nodename1",
+		"environment":            "_default",
+		"server_url":             "https://chef.local",
+		"validation_client_name": "validator",
+		"validation_key_path":    "validator.pem",
+		"attributes":             []interface{}{"key1 { subkey1 = value1 }"},
+	})
+	p := new(ResourceProvisioner)
+	warn, errs := p.Validate(c)
+	if len(warn) > 0 {
+		t.Fatalf("Warnings: %v", warn)
+	}
+	if len(errs) > 0 {
+		t.Fatalf("Errors: %v", errs)
+	}
+}
+
+func TestResourceProvider_Validate_bad(t *testing.T) {
+	c := testConfig(t, map[string]interface{}{
+		"package": "nope",
+	})
+	p := new(ResourceProvisioner)
+	warn, errs := p.Validate(c)
+	if len(warn) > 0 {
+		t.Fatalf("Warnings: %v", warn)
+	}
+	if len(errs) == 0 {
+		t.Fatalf("Should have errors")
+	}
+}
+
+func testConfig(t *testing.T, c map[string]interface{}) *terraform.ResourceConfig {
+	r, err := config.NewRawConfig(c)
+	if err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+
+	return terraform.NewResourceConfig(r)
+}
