4932aa327cce913cd779e3407acda36dc53a0d31
Martin Atkins <mart@degeneration.co.uk>
2015-05-10 20:21:03 -0700

    aws_opswork_stack resource type.
    "Stack" is the root concept in OpsWorks, and acts as a container for a number
    of different "layers" that each provide some service for an application.
    A stack isn't very interesting on its own, but it needs to be created before
    any layers can be created.

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -207,6 +207,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_main_route_table_association": resourceAwsMainRouteTableAssociation(),
 			"aws_network_acl":                  resourceAwsNetworkAcl(),
 			"aws_network_interface":            resourceAwsNetworkInterface(),
+			"aws_opsworks_stack":               resourceAwsOpsworksStack(),
 			"aws_proxy_protocol_policy":        resourceAwsProxyProtocolPolicy(),
 			"aws_route53_delegation_set":       resourceAwsRoute53DelegationSet(),
 			"aws_route53_record":               resourceAwsRoute53Record(),
