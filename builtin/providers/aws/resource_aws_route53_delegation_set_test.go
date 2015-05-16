bfd8226b895e33cc6c6a7de4134ab9f9c7fae600
Radek Simko <radek.simko@timeinc.com>
2015-05-16 18:12:53 +0100

    provider/aws: Add route53_delegation_set

---
+++ builtin/providers/aws/resource_aws_route53_delegation_set_test.go
@@ -0,0 +1,79 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/service/route53"
+)
+
+func TestAccRoute53DelegationSet_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRoute53ZoneDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccRoute53DelegationSetConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckRoute53DelegationSetExists("aws_route53_delegation_set.test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckRoute53DelegationSetDestroy(s *terraform.State, provider *schema.Provider) error {
+	conn := provider.Meta().(*AWSClient).r53conn
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_route53_delegation_set" {
+			continue
+		}
+
+		_, err := conn.GetReusableDelegationSet(&route53.GetReusableDelegationSetInput{ID: aws.String(rs.Primary.ID)})
+		if err == nil {
+			return fmt.Errorf("Delegation set still exists")
+		}
+	}
+	return nil
+}
+
+func testAccCheckRoute53DelegationSetExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*AWSClient).r53conn
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No delegation set ID is set")
+		}
+
+		out, err := conn.GetReusableDelegationSet(&route53.GetReusableDelegationSetInput{
+			ID: aws.String(rs.Primary.ID),
+		})
+
+		if err != nil {
+			return fmt.Errorf("Delegation set does not exist: %#v", rs.Primary.ID)
+		}
+
+		setID := cleanDelegationSetId(*out.DelegationSet.ID)
+		if setID != rs.Primary.ID {
+			return fmt.Errorf("Delegation set ID does not match:\nExpected: %#v\nReturned: %#v", rs.Primary.ID, setID)
+		}
+
+		return nil
+	}
+}
+
+const testAccRoute53DelegationSetConfig = `
+resource "aws_route53_delegation_set" "test" {
+	reference_name = "test"
+}
+`
