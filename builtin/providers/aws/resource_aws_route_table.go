798a063596c6e71fb4ac1432b26ccff63a46c239
Otto Jongerius <ojongerius@atlassian.com>
2015-05-25 16:23:44 +1000

    Ignore S3 VPC endpoint routes.

--- builtin/providers/aws/resource_aws_route_table.go
+++ builtin/providers/aws/resource_aws_route_table.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"regexp"
 
 	"github.com/awslabs/aws-sdk-go/aws"
 	"github.com/awslabs/aws-sdk-go/aws/awserr"
@@ -139,9 +140,20 @@ func resourceAwsRouteTableRead(d *schema.ResourceData, meta interface{}) error {
 	route := &schema.Set{F: resourceAwsRouteTableHash}
 
 	// Loop through the routes and add them to the set
+	s3VpcEndpointRegExp := regexp.MustCompile("^vpce-.*")
+
 	for _, r := range rt.Routes {
-		if r.GatewayID != nil && *r.GatewayID == "local" {
-			continue
+		if r.GatewayID != nil {
+			if *r.GatewayID == "local" {
+				continue
+			}
+
+			// VPC endpoints are special;  They will not have a DestinationCidrBlock,
+			// The result is an object that will choke aws.resourceAwsRouteTableHash
+			// skip it is supported.
+			if s3VpcEndpointRegExp.MatchString(*r.GatewayID) {
+				continue
+			}
 		}
 
 		if r.Origin != nil && *r.Origin == "EnableVgwRoutePropagation" {
