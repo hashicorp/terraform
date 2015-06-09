074f1ed6258208f4f4f57509c2181d987c72cad1
Sathiya Shunmugasundaram <sathiya.shunmugasundaram@capitalone.com>
2015-06-09 12:37:22 -0400

    Added postgres option

--- examples/aws-rds/outputs.tf
+++ examples/aws-rds/outputs.tf
@@ -1,6 +1,9 @@
 output "subnet_group" {
     value = "${aws_db_subnet_group.default.name}"
 }
-output "subnet_group" {
-    value = "${aws_db_subnet_group.default.name}"
+output "db_instance_id" {
+    value = "${aws_db_instance.default.id}"
+}
+output "db_instance_address" {
+    value = "${aws_db_instance.default.address}"
 }
