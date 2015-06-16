f4b98ed8c985ababcfc3ae96a8a858c33a9d9550
Sander van Harmelen <sander@xanzy.io>
2015-06-16 17:45:28 +0200

    Fixing annoying git diffs
    As these files are recreated/updated every time when calling `make dev`
    of `make bin`, git show these diffs every time which is a little
    annoying. So I thought to just update them in and get it over with
    :wink:

--- terraform/graphnodeconfigtype_string.go
+++ terraform/graphnodeconfigtype_string.go
@@ -9,7 +9,7 @@ const _GraphNodeConfigType_name = "GraphNodeConfigTypeInvalidGraphNodeConfigType
 var _GraphNodeConfigType_index = [...]uint8{0, 26, 53, 80, 105, 130, 157}
 
 func (i GraphNodeConfigType) String() string {
-	if i < 0 || i >= GraphNodeConfigType(len(_GraphNodeConfigType_index)-1) {
+	if i < 0 || i+1 >= GraphNodeConfigType(len(_GraphNodeConfigType_index)) {
 		return fmt.Sprintf("GraphNodeConfigType(%d)", i)
 	}
 	return _GraphNodeConfigType_name[_GraphNodeConfigType_index[i]:_GraphNodeConfigType_index[i+1]]
