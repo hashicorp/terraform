f5e22d0675c862a92c20b8047c7265107c3b0ffb
Radek Simko <radek.simko@timeinc.com>
2015-07-05 18:39:01 +0200

    Add new resource - google_container_cluster

--- builtin/providers/google/provider.go
+++ builtin/providers/google/provider.go
@@ -39,6 +39,7 @@ func Provider() terraform.ResourceProvider {
 			"google_compute_network":           resourceComputeNetwork(),
 			"google_compute_route":             resourceComputeRoute(),
 			"google_compute_target_pool":       resourceComputeTargetPool(),
+			"google_container_cluster":         resourceContainerCluster(),
 			"google_dns_managed_zone":          resourceDnsManagedZone(),
 			"google_dns_record_set":            resourceDnsRecordSet(),
 			"google_storage_bucket":            resourceStorageBucket(),
