0a738078b186de4271776c8d467e5ff8185e3869
Joshua Seidel <joshua.seidel@blackboard.com>
2015-07-09 09:05:12 -0400

    ran gofmt removed required, added IF

--- builtin/provisioners/chef/resource_provisioner_test.go
+++ builtin/provisioners/chef/resource_provisioner_test.go
@@ -21,7 +21,7 @@ func TestResourceProvider_Validate_good(t *testing.T) {
 		"server_url":             "https://chef.local",
 		"validation_client_name": "validator",
 		"validation_key_path":    "validator.pem",
-		"secret_key_path":    	  "encrypted_data_bag_secret",
+		"secret_key_path":        "encrypted_data_bag_secret",
 	})
 	r := new(ResourceProvisioner)
 	warn, errs := r.Validate(c)
@@ -69,7 +69,7 @@ func TestResourceProvider_runChefClient(t *testing.T) {
 				"server_url":             "https://chef.local",
 				"validation_client_name": "validator",
 				"validation_key_path":    "test-fixtures/validator.pem",
-				"secret_key_path":	  "test-fixtures/encrypted_data_bag_secret",
+				"secret_key_path":        "test-fixtures/encrypted_data_bag_secret",
 			}),
 
 			ConfDir: linuxConfDir,
@@ -87,7 +87,7 @@ func TestResourceProvider_runChefClient(t *testing.T) {
 				"server_url":             "https://chef.local",
 				"validation_client_name": "validator",
 				"validation_key_path":    "test-fixtures/validator.pem",
-				"secret_key_path":	  "test-fixtures/encrypted_data_bag_secret",
+				"secret_key_path":        "test-fixtures/encrypted_data_bag_secret",
 			}),
 
 			ConfDir: linuxConfDir,
@@ -106,7 +106,7 @@ func TestResourceProvider_runChefClient(t *testing.T) {
 				"server_url":             "https://chef.local",
 				"validation_client_name": "validator",
 				"validation_key_path":    "test-fixtures/validator.pem",
-				"secret_key_path":	  "test-fixtures/encrypted_data_bag_secret",
+				"secret_key_path":        "test-fixtures/encrypted_data_bag_secret",
 			}),
 
 			ConfDir: windowsConfDir,
