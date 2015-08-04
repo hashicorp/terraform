5739c4869ca586e4ebf878e2687ef81ce488c327
Clint Shryock <clint@ctshryock.com>
2015-08-04 16:24:55 -0500

    provider/aws: Docs for RDS Cluster, Cluster Instance

---
+++ builtin/providers/aws/resource_aws_rds_cluster_instance.go
@@ -0,0 +1,210 @@
+package aws
+
+import (
+        "fmt"
+        "log"
+        "time"
+
+        "github.com/aws/aws-sdk-go/aws"
+        "github.com/aws/aws-sdk-go/service/rds"
+        "github.com/hashicorp/terraform/helper/resource"
+        "github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAwsRDSClusterInstance() *schema.Resource {
+        return &schema.Resource{
+                Create: resourceAwsRDSClusterInstanceCreate,
+                Read:   resourceAwsRDSClusterInstanceRead,
+                Update: resourceAwsRDSClusterInstanceUpdate,
+                Delete: resourceAwsRDSClusterInstanceDelete,
+
+                Schema: map[string]*schema.Schema{
+                        "identifier": &schema.Schema{
+                                Type:         schema.TypeString,
+                                Optional:     true,
+                                ForceNew:     true,
+                                ValidateFunc: validateRdsId,
+                        },
+
+                        "db_subnet_group_name": &schema.Schema{
+                                Type:     schema.TypeString,
+                                Optional: true,
+                                ForceNew: true,
+                                Computed: true,
+                        },
+
+                        "writer": &schema.Schema{
+                                Type:     schema.TypeBool,
+                                Computed: true,
+                        },
+
+                        "cluster_identifier": &schema.Schema{
+                                Type:     schema.TypeString,
+                                Required: true,
+                                ForceNew: true,
+                        },
+
+                        "endpoint": &schema.Schema{
+                                Type:     schema.TypeString,
+                                Computed: true,
+                        },
+
+                        "port": &schema.Schema{
+                                Type:     schema.TypeInt,
+                                Computed: true,
+                        },
+
+                        "instance_class": &schema.Schema{
+                                Type:     schema.TypeString,
+                                Required: true,
+                                ForceNew: true,
+                        },
+
+                        "tags": tagsSchema(),
+                },
+        }
+}
+
+func resourceAwsRDSClusterInstanceCreate(d *schema.ResourceData, meta interface{}) error {
+        conn := meta.(*AWSClient).rdsconn
+        tags := tagsFromMapRDS(d.Get("tags").(map[string]interface{}))
+
+        createOpts := &rds.CreateDBInstanceInput{
+                DBInstanceClass:     aws.String(d.Get("instance_class").(string)),
+                DBClusterIdentifier: aws.String(d.Get("cluster_identifier").(string)),
+                Engine:              aws.String("aurora"),
+                Tags:                tags,
+        }
+
+        if v := d.Get("identifier").(string); v != "" {
+                createOpts.DBInstanceIdentifier = aws.String(v)
+        } else {
+                createOpts.DBInstanceIdentifier = aws.String(resource.UniqueId())
+        }
+
+        if attr, ok := d.GetOk("db_subnet_group_name"); ok {
+                createOpts.DBSubnetGroupName = aws.String(attr.(string))
+        }
+
+        log.Printf("[DEBUG] Creating RDS DB Instance opts: %s", createOpts)
+        resp, err := conn.CreateDBInstance(createOpts)
+        if err != nil {
+                return err
+        }
+
+        d.SetId(*resp.DBInstance.DBInstanceIdentifier)
+
+        // reuse db_instance refresh func
+        stateConf := &resource.StateChangeConf{
+                Pending:    []string{"creating", "backing-up", "modifying"},
+                Target:     "available",
+                Refresh:    resourceAwsDbInstanceStateRefreshFunc(d, meta),
+                Timeout:    40 * time.Minute,
+                MinTimeout: 10 * time.Second,
+                Delay:      10 * time.Second,
+        }
+
+        // Wait, catching any errors
+        _, err = stateConf.WaitForState()
+        if err != nil {
+                return err
+        }
+
+        return resourceAwsRDSClusterInstanceRead(d, meta)
+}
+
+func resourceAwsRDSClusterInstanceRead(d *schema.ResourceData, meta interface{}) error {
+        db, err := resourceAwsDbInstanceRetrieve(d, meta)
+        if err != nil {
+                log.Printf("[WARN] Error on retrieving RDS Cluster Instance (%s): %s", d.Id(), err)
+                d.SetId("")
+                return nil
+        }
+
+        // Retreive DB Cluster information, to determine if this Instance is a writer
+        conn := meta.(*AWSClient).rdsconn
+        resp, err := conn.DescribeDBClusters(&rds.DescribeDBClustersInput{
+                DBClusterIdentifier: db.DBClusterIdentifier,
+        })
+
+        var dbc *rds.DBCluster
+        for _, c := range resp.DBClusters {
+                if *c.DBClusterIdentifier == *db.DBClusterIdentifier {
+                        dbc = c
+                }
+        }
+
+        if dbc == nil {
+                return fmt.Errorf("[WARN] Error finding RDS Cluster (%s) for Cluster Instance (%s): %s",
+                        *db.DBClusterIdentifier, *db.DBInstanceIdentifier, err)
+        }
+
+        for _, m := range dbc.DBClusterMembers {
+                if *db.DBInstanceIdentifier == *m.DBInstanceIdentifier {
+                        if *m.IsClusterWriter == true {
+                                d.Set("writer", true)
+                        } else {
+                                d.Set("writer", false)
+                        }
+                }
+        }
+
+        if db.Endpoint != nil {
+                d.Set("endpoint", db.Endpoint.Address)
+                d.Set("port", db.Endpoint.Port)
+        }
+
+        // Fetch and save tags
+        arn, err := buildRDSARN(d, meta)
+        if err != nil {
+                log.Printf("[DEBUG] Error building ARN for RDS Cluster Instance (%s), not setting Tags", *db.DBInstanceIdentifier)
+        } else {
+                if err := saveTagsRDS(conn, d, arn); err != nil {
+                        log.Printf("[WARN] Failed to save tags for RDS Cluster Instance (%s): %s", *db.DBClusterIdentifier, err)
+                }
+        }
+
+        return nil
+}
+
+func resourceAwsRDSClusterInstanceUpdate(d *schema.ResourceData, meta interface{}) error {
+        conn := meta.(*AWSClient).rdsconn
+
+        if arn, err := buildRDSARN(d, meta); err == nil {
+                if err := setTagsRDS(conn, d, arn); err != nil {
+                        return err
+                }
+        }
+
+        return resourceAwsRDSClusterInstanceRead(d, meta)
+}
+
+func resourceAwsRDSClusterInstanceDelete(d *schema.ResourceData, meta interface{}) error {
+        conn := meta.(*AWSClient).rdsconn
+
+        log.Printf("[DEBUG] RDS Cluster Instance destroy: %v", d.Id())
+
+        opts := rds.DeleteDBInstanceInput{DBInstanceIdentifier: aws.String(d.Id())}
+
+        log.Printf("[DEBUG] RDS Cluster Instance destroy configuration: %s", opts)
+        if _, err := conn.DeleteDBInstance(&opts); err != nil {
+                return err
+        }
+
+        // re-uses db_instance refresh func
+        log.Println("[INFO] Waiting for RDS Cluster Instance to be destroyed")
+        stateConf := &resource.StateChangeConf{
+                Pending:    []string{"modifying", "deleting"},
+                Target:     "",
+                Refresh:    resourceAwsDbInstanceStateRefreshFunc(d, meta),
+                Timeout:    40 * time.Minute,
+                MinTimeout: 10 * time.Second,
+        }
+
+        if _, err := stateConf.WaitForState(); err != nil {
+                return err
+        }
+
+        return nil
+
+}
---
+++ website/source/docs/providers/aws/r/rds_cluster.html.markdown
@@ -0,0 +1,85 @@
+---
+layout: "aws"
+page_title: "AWS: aws_rds_cluster"
+sidebar_current: "docs-aws-resource-rds-cluster"
+description: |-
+  Provides an RDS Cluster Resource
+---
+
+# aws\_rds\_cluster
+
+Provides an RDS Cluster Resource. A Cluster Resource defines attributes that are
+applied to the entire cluster of [RDS Cluster Instances][3]. Use the RDS Cluster
+resource and RDS Cluster Instances to create and use Amazon Aurora, a MySQL-compatible
+database engine.
+
+For more information on Amazon Aurora, see [Aurora on Amazon RDS][2] in the Amazon RDS User Guide.
+
+## Example Usage
+
+```
+resource "aws_rds_cluster" "default" {
+  cluster_identifier = "aurora-cluster-demo"
+  availability_zones = ["us-west-2a","us-west-2b","us-west-2c"]
+  database_name = "mydb"
+  master_username = "foo"
+  master_password = "bar"
+}
+```
+
+~> **NOTE:** RDS Clusters resources that are created without any matching
+RDS Cluster Instances do not currently display in the AWS Console.
+
+## Argument Reference
+
+For more detailed documentation about each argument, refer to
+the [AWS official documentation](http://docs.aws.amazon.com/AmazonRDS/latest/CommandLineReference/CLIReference-cmd-ModifyDBInstance.html).
+
+The following arguments are supported:
+
+* `cluster_identifier` - (Required) The Cluster Identifier. Must be a lower case
+string.
+* `database_name` - (Optional) The name for your database of up to 8 alpha-numeric
+  characters. If you do not provide a name, Amazon RDS will not create a
+  database in the DB cluster you are creating
+* `master_password` - (Required) Password for the master DB user. Note that this may
+    show up in logs, and it will be stored in the state file
+* `master_username` - (Required) Username for the master DB user
+* `availability_zones` - (Optional) A list of EC2 Availability Zones that
+  instances in the DB cluster can be created in
+* `backup_retention_period` - (Optional) The days to retain backups for. Default
+1
+* `port` - (Optional) The port on which the DB accepts connections
+* `vpc_security_group_ids` - (Optional) List of VPC security groups to associate
+  with the Cluster
+* `apply_immediately` - (Optional) Specifies whether any cluster modifications
+     are applied immediately, or during the next maintenance window. Default is
+     `false`. See [Amazon RDS Documentation for more information.](http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Overview.DBInstance.Modifying.html)
+* `vpc_security_group_ids` - (Optional) List of VPC security groups to associate.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The RDS Cluster Identifier
+* `cluster_identifier` - The RDS Cluster Identifier
+* `cluster_members` – List of RDS Instances that are a part of this cluster
+* `address` - The address of the RDS instance.
+* `allocated_storage` - The amount of allocated storage
+* `availability_zones` - The availability zone of the instance
+* `backup_retention_period` - The backup retention period
+* `backup_window` - The backup window
+* `endpoint` - The primary, writeable connection endpoint
+* `engine` - The database engine
+* `engine_version` - The database engine version
+* `maintenance_window` - The instance maintenance window
+* `database_name` - The database name
+* `port` - The database port
+* `status` - The RDS instance status
+* `username` - The master username for the database
+* `storage_encrypted` - Specifies whether the DB instance is encrypted
+
+[1]: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Overview.Replication.html
+
+[2]: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Aurora.html
+[3]: /docs/providers/aws/r/rds_cluster_instance.html
