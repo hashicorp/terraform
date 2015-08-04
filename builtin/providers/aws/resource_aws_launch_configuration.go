519b0deaa639e0196a0931626096a709060313d8
Otto Jongerius <ojongerius@atlassian.com>
2015-08-04 13:29:04 +1000

    StringValue we used was renamed to awsutil.Prettify.

--- builtin/providers/aws/resource_aws_launch_configuration.go
+++ builtin/providers/aws/resource_aws_launch_configuration.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -392,7 +393,7 @@ func resourceAwsLaunchConfigurationCreate(d *schema.ResourceData, meta interface
 	createLaunchConfigurationOpts.LaunchConfigurationName = aws.String(lcName)
 
 	log.Printf(
-		"[DEBUG] autoscaling create launch configuration: %s", aws.StringValue(&createLaunchConfigurationOpts))
+		"[DEBUG] autoscaling create launch configuration: %s", awsutil.Prettify(createLaunchConfigurationOpts))
 
 	// IAM profiles can take ~10 seconds to propagate in AWS:
 	// http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html#launch-instance-with-role-console
@@ -433,7 +434,7 @@ func resourceAwsLaunchConfigurationRead(d *schema.ResourceData, meta interface{}
 		LaunchConfigurationNames: []*string{aws.String(d.Id())},
 	}
 
-	log.Printf("[DEBUG] launch configuration describe configuration: %s", aws.StringValue(&describeOpts))
+	log.Printf("[DEBUG] launch configuration describe configuration: %s", awsutil.Prettify(describeOpts))
 	describConfs, err := autoscalingconn.DescribeLaunchConfigurations(&describeOpts)
 	if err != nil {
 		return fmt.Errorf("Error retrieving launch configuration: %s", err)
