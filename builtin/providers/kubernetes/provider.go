72f9860fd52e4f2dc1062624fa8e7dedbbfca860
Radek Simko <radek.simko@gmail.com>
2015-08-14 16:14:28 +0100

    kubernetes: Add provider + kubernetes_pod resource

---
+++ builtin/providers/kubernetes/provider.go
@@ -0,0 +1,136 @@
+package kubernetes
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"k8s.io/kubernetes/pkg/client"
+)
+
+func Provider() terraform.ResourceProvider {
+
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"endpoint": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KUBERNETES_ENDPOINT", nil),
+				Description: descriptions["endpoint"],
+			},
+
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KUBERNETES_USERNAME", nil),
+				Description: descriptions["username"],
+			},
+
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KUBERNETES_PASSWORD", nil),
+				Description: descriptions["password"],
+			},
+
+			"insecure": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				DefaultFunc: schema.EnvDefaultFunc("KUBERNETES_INSECURE", nil),
+				Description: descriptions["insecure"],
+			},
+
+			"client_certificate": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: descriptions["client_certificate"],
+				// TODO: KUBERNETES_CLIENT_CERTIFICATE or KUBERNETES_CLIENT_CERTIFICATE_PATH ?
+			},
+
+			"client_key": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: descriptions["client_key"],
+				// TODO: KUBERNETES_CLIENT_KEY or KUBERNETES_CLIENT_KEY_PATH ?
+			},
+
+			"cluster_ca_certificate": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: descriptions["cluster_ca_certificate"],
+				// TODO: KUBERNETES_CLUSTER_CA_CERTIFICATE or KUBERNETES_CLUSTER_CA_CERTIFICATE_PATH ?
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"kubernetes_pod": resourceKubernetesPod(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+var descriptions map[string]string
+
+func init() {
+	descriptions = map[string]string{
+		"endpoint": "The hostname (in form of URI) of Kubernetes master.",
+
+		"username": "The username to use for HTTP basic authentication\n" +
+			"when accessing the Kubernetes master endpoint.",
+
+		"password": "The password to use for HTTP basic authentication\n" +
+			"when accessing the Kubernetes master endpoint.",
+
+		"insecure": "Whether server should be accessed without verifying " +
+			"the TLS certificate.",
+
+		"client_certificate": "PEM-encoded client certificate for " +
+			"TLS authentication.",
+
+		"client_key": "PEM-encoded client certificate key for TLS " +
+			"authentication.",
+
+		"cluster_ca_certificate": "PEM-encoded root certificates bundle " +
+			"for TLS authentication.",
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	insecure := d.Get("insecure").(bool)
+
+	config := client.Config{
+		Host:     d.Get("endpoint").(string),
+		Username: d.Get("username").(string),
+		Password: d.Get("password").(string),
+		Insecure: insecure,
+	}
+
+	tlsConfig := client.TLSClientConfig{}
+	addTlsConfig := false
+
+	if v, ok := d.GetOk("client_certificate"); ok {
+		tlsConfig.CertData = []byte(v.(string))
+		addTlsConfig = true
+	}
+	if v, ok := d.GetOk("client_key"); ok {
+		tlsConfig.KeyData = []byte(v.(string))
+		addTlsConfig = true
+	}
+	if v, ok := d.GetOk("cluster_ca_certificate"); ok {
+		tlsConfig.CAData = []byte(v.(string))
+		addTlsConfig = true
+	}
+
+	if insecure && addTlsConfig {
+		return nil, fmt.Errorf("You can either specify 'insecure' " +
+			"or provide TLS configuration, not both")
+	}
+
+	if addTlsConfig {
+		config.TLSClientConfig = tlsConfig
+	}
+
+	return client.New(&config)
+}
---
+++ builtin/providers/kubernetes/resource_pod.go
@@ -0,0 +1,181 @@
+package kubernetes
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client"
+	"k8s.io/kubernetes/pkg/util/yaml"
+)
+
+func resourceKubernetesPod() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKubernetesPodCreate,
+		Read:   resourceKubernetesPodRead,
+		Update: resourceKubernetesPodUpdate,
+		Delete: resourceKubernetesPodDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"namespace": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Default:  api.NamespaceDefault,
+			},
+
+			"labels": &schema.Schema{
+				Type:     schema.TypeMap,
+				Optional: true,
+			},
+
+			"spec": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				StateFunc: func(input interface{}) string {
+					s, err := normalizePodSpec(input.(string))
+					if err != nil {
+						log.Printf("[ERROR] Normalising spec failed: %q", err.Error())
+					}
+					return s
+				},
+			},
+		},
+	}
+}
+
+func resourceKubernetesPodCreate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*client.Client)
+
+	spec, err := expandPodSpec(d.Get("spec").(string))
+	if err != nil {
+		return err
+	}
+
+	l := d.Get("labels").(map[string]interface{})
+	labels := make(map[string]string, len(l))
+	for k, v := range l {
+		labels[k] = v.(string)
+	}
+
+	req := api.Pod{
+		ObjectMeta: api.ObjectMeta{
+			Name:   d.Get("name").(string),
+			Labels: labels,
+		},
+		Spec: spec,
+	}
+
+	ns := d.Get("namespace").(string)
+
+	pod, err := c.Pods(ns).Create(&req)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(string(pod.UID))
+
+	return resourceKubernetesPodRead(d, meta)
+}
+
+func resourceKubernetesPodRead(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*client.Client)
+	pod, err := c.Pods(d.Get("namespace").(string)).Get(d.Get("name").(string))
+	if err != nil {
+		return err
+	}
+
+	spec, err := flattenPodSpec(pod.Spec)
+	if err != nil {
+		return err
+	}
+	d.Set("spec", spec)
+	d.Set("labels", pod.Labels)
+	d.Set("spec", pod.Spec)
+
+	return nil
+}
+
+func resourceKubernetesPodUpdate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*client.Client)
+
+	spec, err := expandPodSpec(d.Get("spec").(string))
+	if err != nil {
+		return err
+	}
+
+	l := d.Get("labels").(map[string]interface{})
+	labels := make(map[string]string, len(l))
+	for k, v := range l {
+		labels[k] = v.(string)
+	}
+
+	req := api.Pod{
+		ObjectMeta: api.ObjectMeta{
+			Name:   d.Get("name").(string),
+			Labels: labels,
+		},
+		Spec: spec,
+	}
+
+	_, err = c.Pods(d.Get("namespace").(string)).Update(&req)
+	if err != nil {
+		return err
+	}
+
+	return resourceKubernetesPodRead(d, meta)
+}
+
+func resourceKubernetesPodDelete(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*client.Client)
+	err := c.Pods(d.Get("namespace").(string)).Delete(d.Get("name").(string), nil)
+	return err
+}
+
+func expandPodSpec(input string) (spec api.PodSpec, err error) {
+	r := strings.NewReader(input)
+	y := yaml.NewYAMLOrJSONDecoder(r, 4096)
+
+	err = y.Decode(&spec)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func flattenPodSpec(spec api.PodSpec) (string, error) {
+	b, err := json.Marshal(spec)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
+
+func normalizePodSpec(input string) (string, error) {
+	r := strings.NewReader(input)
+	y := yaml.NewYAMLOrJSONDecoder(r, 4096)
+	spec := api.PodSpec{}
+
+	err := y.Decode(&spec)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: Add/ignore default structures, e.g. resources.limits.cpu = 100m
+
+	b, err := json.Marshal(spec)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
