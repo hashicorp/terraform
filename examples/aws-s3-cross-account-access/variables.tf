0a637be9b374004cd5d605c85af1fc55dbefddcb
Radek Simko <radek.simko@timeinc.com>
2015-08-19 10:15:43 +0100

    aws: Add example w/ S3 & cross-account access

---
+++ examples/aws-s3-cross-account-access/variables.tf
@@ -0,0 +1,8 @@
+variable "prod_access_key" {}
+variable "prod_secret_key" {}
+
+variable "test_account_id" {}
+variable "test_access_key" {}
+variable "test_secret_key" {}
+
+variable "bucket_name" {}
