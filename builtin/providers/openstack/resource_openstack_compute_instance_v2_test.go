5d17f2dfe9de70edb857c583dfda9cde3910f2e9
Joe Topjian <joe@topjian.net>
2015-09-12 17:56:38 +0000

    provider/openstack: Volume Cleanup
    This commit cleans up the volume and block device handling in the instance
    resource. It also adds more acceptance tests to deal with different workflows
    of attaching and detaching a volume through the instance's lifecycle.
    
    No new functionality has been added.

--- builtin/providers/openstack/resource_openstack_compute_instance_v2_test.go
+++ builtin/providers/openstack/resource_openstack_compute_instance_v2_test.go
@@ -50,6 +50,20 @@ func TestAccComputeV2Instance_volumeAttach(t *testing.T) {
 	var instance servers.Server
 	var volume volumes.Volume
 
+	var testAccComputeV2Instance_volumeAttach = fmt.Sprintf(`
+		resource "openstack_blockstorage_volume_v1" "myvol" {
+			name = "myvol"
+			size = 1
+		}
+
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			security_groups = ["default"]
+			volume {
+				volume_id = "${openstack_blockstorage_volume_v1.myvol.id}"
+			}
+		}`)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -61,6 +75,102 @@ func TestAccComputeV2Instance_volumeAttach(t *testing.T) {
 					testAccCheckBlockStorageV1VolumeExists(t, "openstack_blockstorage_volume_v1.myvol", &volume),
 					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
 					testAccCheckComputeV2InstanceVolumeAttachment(&instance, &volume),
+				),
+			},
+		},
+	})
+}
+
+func TestAccComputeV2Instance_volumeAttachPostCreation(t *testing.T) {
+	var instance servers.Server
+	var volume volumes.Volume
+
+	var testAccComputeV2Instance_volumeAttachPostCreationInstance = fmt.Sprintf(`
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			security_groups = ["default"]
+		}`)
+
+	var testAccComputeV2Instance_volumeAttachPostCreationInstanceAndVolume = fmt.Sprintf(`
+		resource "openstack_blockstorage_volume_v1" "myvol" {
+			name = "myvol"
+			size = 1
+		}
+
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			security_groups = ["default"]
+			volume {
+				volume_id = "${openstack_blockstorage_volume_v1.myvol.id}"
+			}
+		}`)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2InstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Instance_volumeAttachPostCreationInstance,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+				),
+			},
+			resource.TestStep{
+				Config: testAccComputeV2Instance_volumeAttachPostCreationInstanceAndVolume,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBlockStorageV1VolumeExists(t, "openstack_blockstorage_volume_v1.myvol", &volume),
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+					testAccCheckComputeV2InstanceVolumeAttachment(&instance, &volume),
+				),
+			},
+		},
+	})
+}
+
+func TestAccComputeV2Instance_volumeDetachPostCreation(t *testing.T) {
+	var instance servers.Server
+	var volume volumes.Volume
+
+	var testAccComputeV2Instance_volumeDetachPostCreationInstanceAndVolume = fmt.Sprintf(`
+		resource "openstack_blockstorage_volume_v1" "myvol" {
+			name = "myvol"
+			size = 1
+		}
+
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			security_groups = ["default"]
+			volume {
+				volume_id = "${openstack_blockstorage_volume_v1.myvol.id}"
+			}
+		}`)
+
+	var testAccComputeV2Instance_volumeDetachPostCreationInstance = fmt.Sprintf(`
+		resource "openstack_compute_instance_v2" "foo" {
+			name = "terraform-test"
+			security_groups = ["default"]
+		}`)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2InstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2Instance_volumeDetachPostCreationInstanceAndVolume,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBlockStorageV1VolumeExists(t, "openstack_blockstorage_volume_v1.myvol", &volume),
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+					testAccCheckComputeV2InstanceVolumeAttachment(&instance, &volume),
+				),
+			},
+			resource.TestStep{
+				Config: testAccComputeV2Instance_volumeDetachPostCreationInstance,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBlockStorageV1VolumeDoesNotExist(t, "openstack_blockstorage_volume_v1.myvol", &volume),
+					testAccCheckComputeV2InstanceExists(t, "openstack_compute_instance_v2.foo", &instance),
+					testAccCheckComputeV2InstanceVolumesDetached(&instance),
 				),
 			},
 		},
@@ -241,6 +351,33 @@ func testAccCheckComputeV2InstanceVolumeAttachment(
 	}
 }
 
+func testAccCheckComputeV2InstanceVolumesDetached(instance *servers.Server) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		var attachments []volumeattach.VolumeAttachment
+
+		config := testAccProvider.Meta().(*Config)
+		computeClient, err := config.computeV2Client(OS_REGION_NAME)
+		if err != nil {
+			return err
+		}
+		err = volumeattach.List(computeClient, instance.ID).EachPage(func(page pagination.Page) (bool, error) {
+			actual, err := volumeattach.ExtractVolumeAttachments(page)
+			if err != nil {
+				return false, fmt.Errorf("Unable to lookup attachment: %s", err)
+			}
+
+			attachments = actual
+			return true, nil
+		})
+
+		if len(attachments) > 0 {
+			return fmt.Errorf("Volumes are still attached.")
+		}
+
+		return nil
+	}
+}
+
 func testAccCheckComputeV2InstanceBootVolumeAttachment(
 	instance *servers.Server) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
@@ -280,19 +417,3 @@ func testAccCheckComputeV2InstanceFloatingIPAttach(
 
 	}
 }
-
-var testAccComputeV2Instance_volumeAttach = fmt.Sprintf(`
-  resource "openstack_blockstorage_volume_v1" "myvol" {
-    name = "myvol"
-    size = 1
-  }
-
-  resource "openstack_compute_instance_v2" "foo" {
-    region = "%s"
-    name = "terraform-test"
-    security_groups = ["default"]
-    volume {
-      volume_id = "${openstack_blockstorage_volume_v1.myvol.id}"
-    }
-  }`,
-	OS_REGION_NAME)
