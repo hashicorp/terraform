bf8f103ca15535ec2a7cc47815d030e3c3bfbe2f
Radek Simko <radek.simko@gmail.com>
2015-09-15 23:11:53 +0100

    provider/aws: Add efs_mount_target

--- builtin/providers/aws/structure.go
+++ builtin/providers/aws/structure.go
@@ -368,11 +368,22 @@ func flattenElastiCacheParameters(list []*elasticache.Parameter) []map[string]in
 }
 
 // Takes the result of flatmap.Expand for an array of strings
-// and returns a []string
+// and returns a []*string
 func expandStringList(configured []interface{}) []*string {
 	vs := make([]*string, 0, len(configured))
 	for _, v := range configured {
 		vs = append(vs, aws.String(v.(string)))
+	}
+	return vs
+}
+
+// Takes list of pointers to strings. Expand to an array
+// of raw strings and returns a []interface{}
+// to keep compatibility w/ schema.NewSetschema.NewSet
+func flattenStringList(list []*string) []interface{} {
+	vs := make([]interface{}, 0, len(list))
+	for _, v := range list {
+		vs = append(vs, *v)
 	}
 	return vs
 }
