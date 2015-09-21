79f2afbc21da73a1c1ba3202c5eaef7d128b7b33
stack72 <public@paulstack.co.uk>
2015-09-21 18:40:08 +0100

    Fleshing out the Create and Read functions for the aws-configservice resource

--- builtin/providers/aws/resource_aws_configservice_inventory.go
+++ builtin/providers/aws/resource_aws_configservice_inventory.go
@@ -1,6 +1,14 @@
 package aws
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"fmt"
+
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/configservice"
+	"github.com/hashicorp/terraform/helper/schema"
+)
 
 func resourceAwsConfigServiceInventory() *schema.Resource {
 	return &schema.Resource{
@@ -14,15 +22,67 @@ func resourceAwsConfigServiceInventory() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+
+			"role_arn": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"configuration_recorder": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"resource_types": &schema.Schema{
+							Type:     schema.TypeSet,
+							Required: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set:      schema.HashString,
+						},
+						"all_supported": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
 
 func resourceAwsConfigServiceInventoryCreate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	configserviceconn := meta.(*AWSClient).configserviceconn
+
+	configurationRecorder := &configservice.ConfigurationRecorder{
+		Name:    aws.String("name"),
+		RoleARN: aws.String("role_arn"),
+		RecordingGroup: &configservice.RecordingGroup{
+			AllSupported: aws.Bool(true),
+		},
+	}
+
+	input := &configservice.PutConfigurationRecorderInput{
+		ConfigurationRecorder: configurationRecorder,
+	}
+
+	_, err := configserviceconn.PutConfigurationRecorder(input)
+	if err != nil {
+		return fmt.Errorf("Error Creating ConfigService Configuration Recorder: %s", err.Error())
+	}
+
+	return resourceAwsConfigServiceInventoryUpdate(d, meta)
 }
 
 func resourceAwsConfigServiceInventoryRead(d *schema.ResourceData, meta interface{}) error {
+	configserviceconn := meta.(*AWSClient).configserviceconn
+
+	out, err := configserviceconn.DescribeConfigurationRecorders(nil)
+	if err != nil {
+		log.Printf("Error")
+	}
+	log.Printf("Got the ARN %s", out.ConfigurationRecorders[0].RoleARN)
+	log.Printf("Got the Name %s", out.ConfigurationRecorders[0].Name)
+
 	return nil
 }
 
