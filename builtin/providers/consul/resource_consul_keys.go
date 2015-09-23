63a045f4ff09b1d5097c84ecac325998795de734
Max Englander <max.englander@gmail.com>
2015-09-23 03:43:48 -0400

    #2087: add consul agent_service and catalog_entry resources; create resource works; TODO support more fields and add tests

--- builtin/providers/consul/resource_consul_keys.go
+++ builtin/providers/consul/resource_consul_keys.go
@@ -283,13 +283,3 @@ func attributeValue(sub map[string]interface{}, key string, pair *consulapi.KVPa
 	// No value
 	return ""
 }
-
-// getDC is used to get the datacenter of the local agent
-func getDC(client *consulapi.Client) (string, error) {
-	info, err := client.Agent().Self()
-	if err != nil {
-		return "", fmt.Errorf("Failed to get datacenter from Consul agent: %v", err)
-	}
-	dc := info["Config"]["Datacenter"].(string)
-	return dc, nil
-}
