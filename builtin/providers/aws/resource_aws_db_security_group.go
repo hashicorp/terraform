2ba8dc38fae1e885b6ce79fda27463b51470d852
Sander van Harmelen <sander@xanzy.io>
2015-09-27 18:58:48 -0700

    Switch to go-multierror
    It seems there are 4 locations left that use the `helper/multierror`
    package, where the rest is TF settled on the `hashicorp/go-multierror`
    package.
    
    Functionally this doesn’t change anything, so I suggest to delete the
    builtin version as it can only cause confusion (both packages have the
    same name, but are still different types according to Go’s type system.

--- builtin/providers/aws/resource_aws_db_security_group.go
+++ builtin/providers/aws/resource_aws_db_security_group.go
@@ -9,8 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform/helper/hashcode"
-	"github.com/hashicorp/terraform/helper/multierror"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 )
