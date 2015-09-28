83c2d19acecf1e6087681562fae7ca011121b0be
Hany Fahim <hany@vmfarms.com>
2015-09-28 13:31:08 -0400

    Check for proper empty response instead of UnlimitedResourceID

--- builtin/providers/cloudstack/resource_cloudstack_template.go
+++ builtin/providers/cloudstack/resource_cloudstack_template.go
@@ -238,7 +238,7 @@ func resourceCloudStackTemplateRead(d *schema.ResourceData, meta interface{}) er
 
 	setValueOrUUID(d, "project", t.Project, t.Projectid)
 
-	if t.Zoneid == UnlimitedResourceID {
+	if t.Zoneid == "" {
 		setValueOrUUID(d, "zone", t.Zonename, UnlimitedResourceID)
 	} else {
 		setValueOrUUID(d, "zone", t.Zonename, t.Zoneid)
