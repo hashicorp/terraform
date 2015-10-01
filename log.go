135224e056e37a0ea372b17de52a164a45d8097a
Fatih Arslan <ftharsln@gmail.com>
2015-10-01 22:45:12 +0300

    log: support for user defined log levels
    This PR brings support for better log level handling. Terraform logs
    are already written in the form which
    https://github.com/hashicorp/logutils can understand easily. Based on
    this fact we know have the followings:
    
    * A new `TF_LOG_LEVEL` environment variable which the user can control
    * Users can pass levels in the form of "info", "Info" or "INFO"
    * If an invalid log level is passed, we print a warning but still
      continue
    * All changes are backwards compatible with the current logs

--- log.go
+++ log.go
@@ -2,28 +2,68 @@ package main
 
 import (
 	"io"
+	"log"
 	"os"
+	"strings"
+
+	"github.com/hashicorp/logutils"
 )
 
 // These are the environmental variables that determine if we log, and if
 // we log whether or not the log should go to a file.
-const EnvLog = "TF_LOG"          //Set to True
-const EnvLogFile = "TF_LOG_PATH" //Set to a file
+const (
+	EnvLog      = "TF_LOG"       // Set to True
+	EnvLogLevel = "TF_LOG_LEVEL" // Set to a log level
+	EnvLogFile  = "TF_LOG_PATH"  // Set to a file
+)
 
-// logOutput determines where we should send logs (if anywhere).
+var validLevels = []logutils.LogLevel{"TRACE", "DEBUG", "INFO", "WARN", "ERROR"}
+
+// logOutput determines where we should send logs (if anywhere) and the log level.
 func logOutput() (logOutput io.Writer, err error) {
 	logOutput = nil
-	if os.Getenv(EnvLog) != "" {
-		logOutput = os.Stderr
+	if os.Getenv(EnvLog) == "" {
+		return
+	}
 
-		if logPath := os.Getenv(EnvLogFile); logPath != "" {
-			var err error
-			logOutput, err = os.Create(logPath)
-			if err != nil {
-				return nil, err
-			}
+	logOutput = os.Stderr
+
+	if logPath := os.Getenv(EnvLogFile); logPath != "" {
+		var err error
+		logOutput, err = os.Create(logPath)
+		if err != nil {
+			return nil, err
 		}
 	}
 
+	// This was the default since the beginning
+	logLevel := logutils.LogLevel("TRACE")
+
+	if level := os.Getenv(EnvLogLevel); level != "" {
+		if isValidLevel(level) {
+			// allow following for better ux: info, Info or INFO
+			logLevel = logutils.LogLevel(strings.ToUpper(level))
+		} else {
+			log.Printf("[WARN] Invalid log level: %q. Valid levels are: %+v",
+				level, validLevels)
+		}
+	}
+
+	logOutput = &logutils.LevelFilter{
+		Levels:   validLevels,
+		MinLevel: logLevel,
+		Writer:   logOutput,
+	}
+
 	return
+}
+
+func isValidLevel(level string) bool {
+	for _, l := range validLevels {
+		if strings.ToUpper(level) == string(l) {
+			return true
+		}
+	}
+
+	return false
 }
