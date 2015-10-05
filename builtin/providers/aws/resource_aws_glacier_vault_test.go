9f01efae6f027ce6cd646ebb7c018f95a410104d
stack72 <public@paulstack.co.uk>
2015-10-05 11:24:09 +0100

    Adding a test to make sure that the diffGlacierVaultTags func works as expected

--- builtin/providers/aws/resource_aws_glacier_vault_test.go
+++ builtin/providers/aws/resource_aws_glacier_vault_test.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -65,6 +66,57 @@ func TestAccAWSGlacierVault_RemoveNotifications(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestDiffGlacierVaultTags(t *testing.T) {
+	cases := []struct {
+		Old, New map[string]interface{}
+		Create   map[string]string
+		Remove   []string
+	}{
+		// Basic add/remove
+		{
+			Old: map[string]interface{}{
+				"foo": "bar",
+			},
+			New: map[string]interface{}{
+				"bar": "baz",
+			},
+			Create: map[string]string{
+				"bar": "baz",
+			},
+			Remove: []string{
+				"foo",
+			},
+		},
+
+		// Modify
+		{
+			Old: map[string]interface{}{
+				"foo": "bar",
+			},
+			New: map[string]interface{}{
+				"foo": "baz",
+			},
+			Create: map[string]string{
+				"foo": "baz",
+			},
+			Remove: []string{
+				"foo",
+			},
+		},
+	}
+
+	for i, tc := range cases {
+		c, r := diffGlacierVaultTags(mapGlacierVaultTags(tc.Old), mapGlacierVaultTags(tc.New))
+
+		if !reflect.DeepEqual(c, tc.Create) {
+			t.Fatalf("%d: bad create: %#v", i, c)
+		}
+		if !reflect.DeepEqual(r, tc.Remove) {
+			t.Fatalf("%d: bad remove: %#v", i, r)
+		}
+	}
 }
 
 func testAccCheckGlacierVaultExists(name string) resource.TestCheckFunc {
