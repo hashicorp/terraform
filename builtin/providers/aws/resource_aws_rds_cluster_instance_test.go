17e264756c2f63220062013f08bcf638a8f88e47
Clint Shryock <clint@ctshryock.com>
2015-10-07 11:27:24 -0500

    go fmt after rebase

--- builtin/providers/aws/resource_aws_rds_cluster_instance_test.go
+++ builtin/providers/aws/resource_aws_rds_cluster_instance_test.go
@@ -1,118 +1,118 @@
 package aws
 
 import (
-        "fmt"
-        "math/rand"
-        "strings"
-        "testing"
-        "time"
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
 
-        "github.com/hashicorp/terraform/helper/resource"
-        "github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 
-        "github.com/aws/aws-sdk-go/aws"
-        "github.com/aws/aws-sdk-go/aws/awserr"
-        "github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/rds"
 )
 
 func TestAccAWSRDSClusterInstance_basic(t *testing.T) {
-        var v rds.DBInstance
+	var v rds.DBInstance
 
-        resource.Test(t, resource.TestCase{
-                PreCheck:     func() { testAccPreCheck(t) },
-                Providers:    testAccProviders,
-                CheckDestroy: testAccCheckAWSClusterDestroy,
-                Steps: []resource.TestStep{
-                        resource.TestStep{
-                                Config: testAccAWSClusterInstanceConfig,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testAccCheckAWSClusterInstanceExists("aws_rds_cluster_instance.cluster_instances", &v),
-                                        testAccCheckAWSDBClusterInstanceAttributes(&v),
-                                ),
-                        },
-                },
-        })
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSClusterDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSClusterInstanceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSClusterInstanceExists("aws_rds_cluster_instance.cluster_instances", &v),
+					testAccCheckAWSDBClusterInstanceAttributes(&v),
+				),
+			},
+		},
+	})
 }
 
 func testAccCheckAWSClusterInstanceDestroy(s *terraform.State) error {
-        for _, rs := range s.RootModule().Resources {
-                if rs.Type != "aws_rds_cluster" {
-                        continue
-                }
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_rds_cluster" {
+			continue
+		}
 
-                // Try to find the Group
-                conn := testAccProvider.Meta().(*AWSClient).rdsconn
-                var err error
-                resp, err := conn.DescribeDBInstances(
-                        &rds.DescribeDBInstancesInput{
-                                DBInstanceIdentifier: aws.String(rs.Primary.ID),
-                        })
+		// Try to find the Group
+		conn := testAccProvider.Meta().(*AWSClient).rdsconn
+		var err error
+		resp, err := conn.DescribeDBInstances(
+			&rds.DescribeDBInstancesInput{
+				DBInstanceIdentifier: aws.String(rs.Primary.ID),
+			})
 
-                if err == nil {
-                        if len(resp.DBInstances) != 0 &&
-                                *resp.DBInstances[0].DBInstanceIdentifier == rs.Primary.ID {
-                                return fmt.Errorf("DB Cluster Instance %s still exists", rs.Primary.ID)
-                        }
-                }
+		if err == nil {
+			if len(resp.DBInstances) != 0 &&
+				*resp.DBInstances[0].DBInstanceIdentifier == rs.Primary.ID {
+				return fmt.Errorf("DB Cluster Instance %s still exists", rs.Primary.ID)
+			}
+		}
 
-                // Return nil if the Cluster Instance is already destroyed
-                if awsErr, ok := err.(awserr.Error); ok {
-                        if awsErr.Code() == "DBInstanceNotFound" {
-                                return nil
-                        }
-                }
+		// Return nil if the Cluster Instance is already destroyed
+		if awsErr, ok := err.(awserr.Error); ok {
+			if awsErr.Code() == "DBInstanceNotFound" {
+				return nil
+			}
+		}
 
-                return err
+		return err
 
-        }
+	}
 
-        return nil
+	return nil
 }
 
 func testAccCheckAWSDBClusterInstanceAttributes(v *rds.DBInstance) resource.TestCheckFunc {
-        return func(s *terraform.State) error {
+	return func(s *terraform.State) error {
 
-                if *v.Engine != "aurora" {
-                        return fmt.Errorf("bad engine, expected \"aurora\": %#v", *v.Engine)
-                }
+		if *v.Engine != "aurora" {
+			return fmt.Errorf("bad engine, expected \"aurora\": %#v", *v.Engine)
+		}
 
-                if !strings.HasPrefix(*v.DBClusterIdentifier, "tf-aurora-cluster") {
-                        return fmt.Errorf("Bad Cluster Identifier prefix:\nexpected: %s\ngot: %s", "tf-aurora-cluster", *v.DBClusterIdentifier)
-                }
+		if !strings.HasPrefix(*v.DBClusterIdentifier, "tf-aurora-cluster") {
+			return fmt.Errorf("Bad Cluster Identifier prefix:\nexpected: %s\ngot: %s", "tf-aurora-cluster", *v.DBClusterIdentifier)
+		}
 
-                return nil
-        }
+		return nil
+	}
 }
 
 func testAccCheckAWSClusterInstanceExists(n string, v *rds.DBInstance) resource.TestCheckFunc {
-        return func(s *terraform.State) error {
-                rs, ok := s.RootModule().Resources[n]
-                if !ok {
-                        return fmt.Errorf("Not found: %s", n)
-                }
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
 
-                if rs.Primary.ID == "" {
-                        return fmt.Errorf("No DB Instance ID is set")
-                }
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No DB Instance ID is set")
+		}
 
-                conn := testAccProvider.Meta().(*AWSClient).rdsconn
-                resp, err := conn.DescribeDBInstances(&rds.DescribeDBInstancesInput{
-                        DBInstanceIdentifier: aws.String(rs.Primary.ID),
-                })
+		conn := testAccProvider.Meta().(*AWSClient).rdsconn
+		resp, err := conn.DescribeDBInstances(&rds.DescribeDBInstancesInput{
+			DBInstanceIdentifier: aws.String(rs.Primary.ID),
+		})
 
-                if err != nil {
-                        return err
-                }
+		if err != nil {
+			return err
+		}
 
-                for _, d := range resp.DBInstances {
-                        if *d.DBInstanceIdentifier == rs.Primary.ID {
-                                *v = *d
-                                return nil
-                        }
-                }
+		for _, d := range resp.DBInstances {
+			if *d.DBInstanceIdentifier == rs.Primary.ID {
+				*v = *d
+				return nil
+			}
+		}
 
-                return fmt.Errorf("DB Cluster (%s) not found", rs.Primary.ID)
-        }
+		return fmt.Errorf("DB Cluster (%s) not found", rs.Primary.ID)
+	}
 }
 
 // Add some random to the name, to avoid collision
