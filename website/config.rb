ef99a433cb595cd24bc2fc7e4335dec37bf0b1ce
Seth Vargo <sethvargo@gmail.com>
2015-10-25 23:58:36 -0400

    Use releases for releases

--- website/config.rb
+++ website/config.rb
@@ -1,14 +1,7 @@
-#-------------------------------------------------------------------------
-# Configure Middleman
-#-------------------------------------------------------------------------
-
 set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
-  h.version         = ENV["TERRAFORM_VERSION"]
-  h.bintray_enabled = ENV["BINTRAY_ENABLED"]
-  h.bintray_repo    = "mitchellh/terraform"
-  h.bintray_user    = "mitchellh"
-  h.bintray_key     = ENV["BINTRAY_API_KEY"]
-  h.github_slug     = "hashicorp/terraform"
+  h.name        = "terraform"
+  h.version     = "0.6.6"
+  h.github_slug = "hashicorp/terraform"
 end
