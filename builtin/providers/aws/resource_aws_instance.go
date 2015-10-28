37677ee2f4fbfd0226c0b6c79ee7fea1c563614e
Joshua Semar <jsemar@underarmour.com>
2015-10-27 21:30:11 -0500

    get profile name even if profile path exists

--- builtin/providers/aws/resource_aws_instance.go
+++ builtin/providers/aws/resource_aws_instance.go
@@ -1082,5 +1082,6 @@ func iamInstanceProfileArnToName(ip *ec2.IamInstanceProfile) string {
 	if ip == nil || ip.Arn == nil {
 		return ""
 	}
-	return strings.Split(*ip.Arn, "/")[1]
+	parts := strings.Split(*ip.Arn, "/")
+	return parts[len(parts)-1]
 }
