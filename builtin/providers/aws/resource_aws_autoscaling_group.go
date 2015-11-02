c7b02d9fdb8aa7b219fb814f45c55ddb5588c19b
Sunil K Chopra <schopra@misdepartment.com>
2015-11-02 09:33:46 -0600

    handling updates

--- builtin/providers/aws/resource_aws_autoscaling_group.go
+++ builtin/providers/aws/resource_aws_autoscaling_group.go
@@ -231,6 +231,7 @@ func resourceAwsAutoscalingGroupRead(d *schema.ResourceData, meta interface{}) e
 	d.Set("load_balancers", g.LoadBalancerNames)
 	d.Set("min_size", g.MinSize)
 	d.Set("max_size", g.MaxSize)
+	d.Set("placement_group", g.PlacementGroup)
 	d.Set("name", g.AutoScalingGroupName)
 	d.Set("tag", g.Tags)
 	d.Set("vpc_zone_identifier", strings.Split(*g.VPCZoneIdentifier, ","))
@@ -283,6 +284,10 @@ func resourceAwsAutoscalingGroupUpdate(d *schema.ResourceData, meta interface{})
 		if v, ok := d.GetOk("availability_zones"); ok && v.(*schema.Set).Len() > 0 {
 			opts.AvailabilityZones = expandStringList(d.Get("availability_zones").(*schema.Set).List())
 		}
+	}
+
+	if d.HasChange("placement_group") {
+		opts.PlacementGroup = aws.String(d.Get("placement_group").(string))
 	}
 
 	if d.HasChange("termination_policies") {
