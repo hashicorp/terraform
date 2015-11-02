f70d4079c1522d58291abe9dc0cc4544f7cf78d1
Lars Wander <lars.wander@gmail.com>
2015-11-02 10:15:19 -0500

    provider/google: SSL Certificates resource + tests & documentation

--- builtin/providers/google/provider.go
+++ builtin/providers/google/provider.go
@@ -46,6 +46,7 @@ func Provider() terraform.ResourceProvider {
 			"google_compute_network":                resourceComputeNetwork(),
 			"google_compute_project_metadata":       resourceComputeProjectMetadata(),
 			"google_compute_route":                  resourceComputeRoute(),
+			"google_compute_ssl_certificate":        resourceComputeSslCertificate(),
 			"google_compute_target_pool":            resourceComputeTargetPool(),
 			"google_compute_vpn_gateway":            resourceComputeVpnGateway(),
 			"google_compute_vpn_tunnel":             resourceComputeVpnTunnel(),
