79102f84bc73888e41cfa46167d76380a2f1d0db
captainill <jonthomas0@gmail.com>
2015-11-06 21:01:14 -0800

    whitespace

--- website/source/assets/javascripts/app/Sidebar.js
+++ website/source/assets/javascripts/app/Sidebar.js
@@ -1,50 +1,50 @@
 (function(){
 
-Sidebar = Base.extend({
+  Sidebar = Base.extend({
 
-	$body: null,
-	$overlay: null,
-	$sidebar: null,
-	$sidebarHeader: null,
-	$sidebarImg: null,
-	$toggleButton: null,
+    $body: null,
+    $overlay: null,
+    $sidebar: null,
+    $sidebarHeader: null,
+    $sidebarImg: null,
+    $toggleButton: null,
 
-	constructor: function(){
-		this.$body = $('body');
-		this.$overlay = $('.sidebar-overlay');
-    this.$sidebar = $('#sidebar');
-    this.$sidebarHeader = $('#sidebar .sidebar-header');
-    this.$toggleButton = $('.navbar-toggle');
-    this.sidebarImg = this.$sidebarHeader.css('background-image');
+    constructor: function(){
+      this.$body = $('body');
+      this.$overlay = $('.sidebar-overlay');
+      this.$sidebar = $('#sidebar');
+      this.$sidebarHeader = $('#sidebar .sidebar-header');
+      this.$toggleButton = $('.navbar-toggle');
+      this.sidebarImg = this.$sidebarHeader.css('background-image');
 
-		this.addEventListeners();
-	},
+      this.addEventListeners();
+    },
 
-	addEventListeners: function(){
-		var _this = this;
+    addEventListeners: function(){
+      var _this = this;
 
-	    _this.$toggleButton.on('click', function() {
-	        _this.$sidebar.toggleClass('open');
-	        if ((_this.$sidebar.hasClass('sidebar-fixed-left') || _this.$sidebar.hasClass('sidebar-fixed-right')) && _this.$sidebar.hasClass('open')) {
-	            _this.$overlay.addClass('active');
-	            _this.$body.css('overflow', 'hidden');
-	        } else {
-	            _this.$overlay.removeClass('active');
-	            _this.$body.css('overflow', 'auto');
-	        }
+      _this.$toggleButton.on('click', function() {
+        _this.$sidebar.toggleClass('open');
+        if ((_this.$sidebar.hasClass('sidebar-fixed-left') || _this.$sidebar.hasClass('sidebar-fixed-right')) && _this.$sidebar.hasClass('open')) {
+          _this.$overlay.addClass('active');
+          _this.$body.css('overflow', 'hidden');
+        } else {
+          _this.$overlay.removeClass('active');
+          _this.$body.css('overflow', 'auto');
+        }
 
-	        return false;
-	    });
+        return false;
+      });
 
-	    _this.$overlay.on('click', function() {
-	        $(this).removeClass('active');
-	        _this.$body.css('overflow', 'auto');
-	        _this.$sidebar.removeClass('open');
-	    });
-	}
+      _this.$overlay.on('click', function() {
+        $(this).removeClass('active');
+        _this.$body.css('overflow', 'auto');
+        _this.$sidebar.removeClass('open');
+      });
+    }
 
-});
+  });
 
-window.Sidebar = Sidebar;
+  window.Sidebar = Sidebar;
 
 })();
