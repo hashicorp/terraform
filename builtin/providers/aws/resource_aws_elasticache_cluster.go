73475edceb835d9463f1958f9e23ea67046c2fe1
clint shryock <clint@ctshryock.com>
2015-11-19 16:27:56 -0600

    update TestAccAWSFlowLog_subnet to use new cloudwatch resource, not needing env var anymore

--- builtin/providers/aws/resource_aws_elasticache_cluster.go
+++ builtin/providers/aws/resource_aws_elasticache_cluster.go
@@ -141,7 +141,7 @@ func resourceAwsElasticacheCluster() *schema.Resource {
 			"snapshot_window": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
-                                Computed: true,
+				Computed: true,
 			},
 
 			"snapshot_retention_limit": &schema.Schema{
