d06ce54c0879d03b715ae84df96bae9efcf588b6
David Harris <dharris@dharris.io>
2015-11-18 19:51:58 -0700

    Updated resourceAwsElasticBeanstalkEnviornmentCreate to treat version_label as optional.

--- builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
+++ builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
@@ -108,7 +108,6 @@ func resourceAwsElasticBeanstalkEnvironmentCreate(d *schema.ResourceData, meta i
 	createOpts := elasticbeanstalk.CreateEnvironmentInput{
 		EnvironmentName: aws.String(name),
 		ApplicationName: aws.String(app),
-		VersionLabel:    aws.String(version),
 		OptionSettings:  extractOptionSettings(settings),
 		Tags:            tagsFromMapBeanstalk(d.Get("tags").(map[string]interface{})),
 	}
@@ -127,6 +126,10 @@ func resourceAwsElasticBeanstalkEnvironmentCreate(d *schema.ResourceData, meta i
 
 	if templateName != "" {
 		createOpts.TemplateName = aws.String(templateName)
+	}
+
+	if version != "" {
+		createOpts.VersionLabel = aws.String(version)
 	}
 
 	log.Printf("[DEBUG] Elastic Beanstalk Environment create opts: %s", createOpts)
