4e72f444766ebe3c038f3ef89d984f390627137e
Patrick Cadelina <patrick.cadelina@freelancer.com>
2015-12-01 14:19:32 +0800

    Add `executionEnabled` schema attribute to rundeck job

--- builtin/providers/rundeck/resource_job.go
+++ builtin/providers/rundeck/resource_job.go
@@ -44,6 +44,11 @@ func resourceRundeckJob() *schema.Resource {
 				Required: true,
 			},
 
+			"execution_enabled": &schema.Schema{
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+
 			"log_level": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
@@ -337,6 +342,7 @@ func jobFromResourceData(d *schema.ResourceData) (*rundeck.JobDetail, error) {
 		GroupName:                 d.Get("group_name").(string),
 		ProjectName:               d.Get("project_name").(string),
 		Description:               d.Get("description").(string),
+		ExecutionEnabled:          d.Get("execution_enabled").(bool),
 		LogLevel:                  d.Get("log_level").(string),
 		AllowConcurrentExecutions: d.Get("allow_concurrent_executions").(bool),
 		Dispatch: &rundeck.JobDispatch{
@@ -465,6 +471,7 @@ func jobToResourceData(job *rundeck.JobDetail, d *schema.ResourceData) error {
 	d.Set("group_name", job.GroupName)
 	d.Set("project_name", job.ProjectName)
 	d.Set("description", job.Description)
+	d.Set("execution_enabled", job.ExecutionEnabled)
 	d.Set("log_level", job.LogLevel)
 	d.Set("allow_concurrent_executions", job.AllowConcurrentExecutions)
 	if job.Dispatch != nil {
