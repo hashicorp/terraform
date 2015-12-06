02cc9e1ebdc9d60ce483dda5995d4b2681c8ff43
Dmytro Aleksandrov <alkersan@gmail.com>
2015-12-06 20:21:33 +0200

    provider/powerdns: Add acceptance tests for common record types

---
+++ builtin/providers/powerdns/provider_test.go
@@ -0,0 +1,39 @@
+package powerdns
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"powerdns": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProviderImpl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("PDNS_API_KEY"); v == "" {
+		t.Fatal("PDNS_API_KEY must be set for acceptance tests")
+	}
+
+	if v := os.Getenv("PDNS_SERVER_URL"); v == "" {
+		t.Fatal("PDNS_SERVER_URL must be set for acceptance tests")
+	}
+}
