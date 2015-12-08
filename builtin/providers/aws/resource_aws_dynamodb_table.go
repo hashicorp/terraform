5bea1d30237945671b8b1913217baff79fe19818
stack72 <public@paulstack.co.uk>
2015-12-08 20:58:06 +0000

    Adding a validation function for the DynamoDb Table StreamViewType

--- builtin/providers/aws/resource_aws_dynamodb_table.go
+++ builtin/providers/aws/resource_aws_dynamodb_table.go
@@ -172,6 +172,7 @@ func resourceAwsDynamoDbTable() *schema.Resource {
 					value := v.(string)
 					return strings.ToUpper(value)
 				},
+				ValidateFunc: validateStreamViewType,
 			},
 		},
 	}
@@ -799,4 +800,19 @@ func waitForTableToBeActive(tableName string, meta interface{}) error {
 
 	return nil
 
+}
+
+func validateStreamViewType(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	viewTypes := map[string]bool {
+		"KEYS_ONLY": true,
+		"NEW_IMAGE": true,
+		"OLD_IMAGE": true,
+		"NEW_AND_OLD_IMAGES": true,
+	}
+
+	if !viewTypes[value] {
+		errors = append(errors, fmt.Errorf("%q be a valid DynamoDB StreamViewType", k))
+	}
+	return
 }
