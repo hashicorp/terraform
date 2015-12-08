66c54837b83170c239873cbcfc73284dc07e2a54
Paul Hinze <phinze@phinze.com>
2015-12-08 17:48:57 -0600

    provider/aws: fix capitalization in acctest names

--- builtin/providers/aws/resource_aws_lb_cookie_stickiness_policy_test.go
+++ builtin/providers/aws/resource_aws_lb_cookie_stickiness_policy_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAccAwsLBCookieStickinessPolicy_basic(t *testing.T) {
+func TestAccAWSLBCookieStickinessPolicy_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
