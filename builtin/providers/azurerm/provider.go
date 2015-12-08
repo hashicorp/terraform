65a366695845dcceb9306669d25cbee46287bb99
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2015-12-08 17:30:12 -0500

    provider/azurerm: Initial commit.
    This commit brings some of the work over from #3808, but rearchitects to
    use a separate provider for Azure Resource Manager. This is in line with
    the decisions made by the Azure Powershell Cmdlets, and is important for
    usability since the sets of required fields change between the ASM and
    ARM APIs.
    
    Currently `azurerm_resource_group` and `azurerm_virtual_network` are
    implemented, more resources will follow.

---
+++ builtin/providers/azurerm/provider.go
@@ -0,0 +1,108 @@
+package azurerm
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"arm_config_file": &schema.Schema{
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "",
+				DefaultFunc:  schema.EnvDefaultFunc("ARM_CONFIG_FILE", nil),
+				ValidateFunc: validateArmConfigFile,
+			},
+
+			"subscription_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("AZURE_SUBSCRIPTION_ID", ""),
+			},
+
+			"client_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_CLIENT_ID", ""),
+			},
+
+			"client_secret": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_CLIENT_SECRET", ""),
+			},
+
+			"tenant_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_TENANT_ID", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"azurerm_resource_group":  resourceArmResourceGroup(),
+			"azurerm_virtual_network": resourceArmVirtualNetwork(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+// Config is the configuration structure used to instantiate a
+// new Azure management client.
+type Config struct {
+	ManagementURL string
+
+	ArmConfig string
+
+	SubscriptionID string
+	ClientID       string
+	ClientSecret   string
+	TenantID       string
+}
+
+const noConfigError = `Credentials must be provided either via arm_config_file, or via
+subscription_id, client_id, client_secret and tenant_id. Please see
+the provider documentation for more information on how to obtain these
+credentials.`
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		SubscriptionID: d.Get("subscription_id").(string),
+		ClientID:       d.Get("client_id").(string),
+		ClientSecret:   d.Get("client_secret").(string),
+		TenantID:       d.Get("tenant_id").(string),
+	}
+
+	// check if credentials file is provided:
+	armConfig := d.Get("arm_config_file").(string)
+	if armConfig != "" {
+		// then, load the settings from that:
+		if err := config.readArmSettings(armConfig); err != nil {
+			return nil, err
+		}
+	}
+
+	// then; check whether the ARM credentials were provided:
+	if !config.armCredentialsProvided() {
+		return nil, fmt.Errorf(noConfigError)
+	}
+
+	client, err := config.getArmClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func azureRMNormalizeLocation(location interface{}) string {
+	input := location.(string)
+	return strings.Replace(strings.ToLower(input), " ", "", -1)
+}
---
+++ builtin/providers/azurerm/resourceArmResourceGroup.go
@@ -0,0 +1,140 @@
+package azurerm
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/arm/resources"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+// resourceArmResourceGroup returns the *schema.Resource
+// associated to resource group resources on ARM.
+func resourceArmResourceGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceArmResourceGroupCreate,
+		Read:   resourceArmResourceGroupRead,
+		Exists: resourceArmResourceGroupExists,
+		Delete: resourceArmResourceGroupDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				//TODO(jen20) - implement validation func: {resource-group-name} must uniquely identify the resource group within the subscription. It must be no longer than 80 characters long. It can only contain alphanumeric characters, dash, underscore, opening parenthesis, closing parenthesis or period. The name cannot end with a period.
+			},
+			"location": &schema.Schema{
+				Type:      schema.TypeString,
+				Required:  true,
+				ForceNew:  true,
+				StateFunc: azureRMNormalizeLocation,
+			},
+		},
+	}
+}
+
+// resourceArmResourceGroupCreate goes ahead and creates the specified ARM resource group.
+func resourceArmResourceGroupCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ArmClient)
+	resGroupClient := client.resourceGroupClient
+
+	name := d.Get("name").(string)
+	location := d.Get("location").(string)
+
+	log.Printf("[INFO] Issuing Azure ARM creation request for resource group '%s'.", name)
+
+	rg := resources.ResourceGroup{
+		Name:     &name,
+		Location: &location,
+	}
+
+	_, err := resGroupClient.CreateOrUpdate(name, rg)
+	if err != nil {
+		return fmt.Errorf("Error issuing Azure ARM create request for resource group '%s': %s", name, err)
+	}
+
+	d.SetId(*rg.Name)
+
+	// Wait for the resource group to become available
+	// TODO(jen20): Is there any need for this?
+	log.Printf("[DEBUG] Waiting for Resource Group (%s) to become available", d.Id())
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"Accepted"},
+		Target:  "Succeeded",
+		Refresh: resourceGroupStateRefreshFunc(client, d.Id()),
+		Timeout: 10 * time.Minute,
+	}
+	if _, err := stateConf.WaitForState(); err != nil {
+		return fmt.Errorf("Error waiting for Resource Group (%s) to become available: %s", d.Id(), err)
+	}
+
+	return resourceArmResourceGroupRead(d, meta)
+}
+
+// resourceArmResourceGroupRead goes ahead and reads the state of the corresponding ARM resource group.
+func resourceArmResourceGroupRead(d *schema.ResourceData, meta interface{}) error {
+	resGroupClient := meta.(*ArmClient).resourceGroupClient
+
+	name := d.Id()
+	log.Printf("[INFO] Issuing read request to Azure ARM for resource group '%s'.", name)
+
+	res, err := resGroupClient.Get(name)
+	if err != nil {
+		return fmt.Errorf("Error issuing read request to Azure ARM for resource group '%s': %s", name, err)
+	}
+
+	d.Set("name", *res.Name)
+	d.Set("location", *res.Location)
+
+	return nil
+}
+
+// resourceArmResourceGroupExists goes ahead and checks for the existence of the correspoding ARM resource group.
+func resourceArmResourceGroupExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	resGroupClient := meta.(*ArmClient).resourceGroupClient
+
+	name := d.Id()
+
+	resp, err := resGroupClient.CheckExistence(name)
+	if err != nil {
+		// TODO(aznashwan): implement some error switching helpers in the SDK
+		// to avoid HTTP error checks such as the below:
+		if resp.StatusCode != 200 {
+			return false, err
+		}
+
+		return true, nil
+	}
+
+	return true, nil
+}
+
+// resourceArmResourceGroupDelete deletes the specified ARM resource group.
+func resourceArmResourceGroupDelete(d *schema.ResourceData, meta interface{}) error {
+	resGroupClient := meta.(*ArmClient).resourceGroupClient
+
+	name := d.Id()
+
+	_, err := resGroupClient.Delete(name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// resourceGroupStateRefreshFunc returns a resource.StateRefreshFunc that is used to watch
+// a resource group.
+func resourceGroupStateRefreshFunc(client *ArmClient, id string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		res, err := client.resourceGroupClient.Get(id)
+		if err != nil {
+			return nil, "", fmt.Errorf("Error issuing read request in resourceGroupStateRefreshFunc to Azure ARM for resource group '%s': %s", id, err)
+		}
+
+		return res, *res.Properties.ProvisioningState, nil
+	}
+}
