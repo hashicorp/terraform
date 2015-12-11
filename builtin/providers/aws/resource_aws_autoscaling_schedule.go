c965d2278ee35268ca0b22add624a24a24c71dcd
stack72 <public@paulstack.co.uk>
2015-12-11 18:13:52 +0000

    Adding a resource for aws_autoscaling_schedule

--- builtin/providers/aws/resource_aws_autoscaling_schedule.go
+++ builtin/providers/aws/resource_aws_autoscaling_schedule.go
@@ -3,11 +3,14 @@ package aws
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/hashicorp/terraform/helper/schema"
 )
+
+const awsAutoscalingScheduleTimeLayout = "2006-01-02T15:04:05Z"
 
 func resourceAwsAutoscalingSchedule() *schema.Resource {
 	return &schema.Resource{
@@ -17,6 +20,10 @@ func resourceAwsAutoscalingSchedule() *schema.Resource {
 		Delete: resourceAwsAutoscalingScheduleDelete,
 
 		Schema: map[string]*schema.Schema{
+			"arn": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"scheduled_action_name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
@@ -28,14 +35,16 @@ func resourceAwsAutoscalingSchedule() *schema.Resource {
 				ForceNew: true,
 			},
 			"start_time": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validateASGScheduleTimestamp,
 			},
 			"end_time": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validateASGScheduleTimestamp,
 			},
 			"recurrence": &schema.Schema{
 				Type:     schema.TypeString,
@@ -63,25 +72,41 @@ func resourceAwsAutoscalingSchedule() *schema.Resource {
 
 func resourceAwsAutoscalingScheduleCreate(d *schema.ResourceData, meta interface{}) error {
 	autoscalingconn := meta.(*AWSClient).autoscalingconn
-	params := autoscaling.PutScheduledUpdateGroupActionInput{
+	params := &autoscaling.PutScheduledUpdateGroupActionInput{
 		AutoScalingGroupName: aws.String(d.Get("autoscaling_group_name").(string)),
 		ScheduledActionName:  aws.String(d.Get("scheduled_action_name").(string)),
 	}
 
 	if attr, ok := d.GetOk("start_time"); ok {
-		params.StartTime = aws.Time()
+		t, err := time.Parse(awsAutoscalingScheduleTimeLayout, attr.(string))
+		if err != nil {
+			return fmt.Errorf("Error Parsing AWS Autoscaling Group Schedule Start Time: %s", err.Error())
+		}
+		params.StartTime = aws.Time(t)
+	}
+
+	if attr, ok := d.GetOk("end_time"); ok {
+		t, err := time.Parse(awsAutoscalingScheduleTimeLayout, attr.(string))
+		if err != nil {
+			return fmt.Errorf("Error Parsing AWS Autoscaling Group Schedule End Time: %s", err.Error())
+		}
+		params.EndTime = aws.Time(t)
+	}
+
+	if attr, ok := d.GetOk("recurrance"); ok {
+		params.Recurrence = aws.String(attr.(string))
 	}
 
 	if attr, ok := d.GetOk("min_size"); ok {
-		params.MinSize = aws.Int(int64(attr.(int)))
+		params.MinSize = aws.Int64(int64(attr.(int)))
 	}
 
 	if attr, ok := d.GetOk("max_size"); ok {
-		params.MaxSize = aws.Int(int64(attr.(int)))
+		params.MaxSize = aws.Int64(int64(attr.(int)))
 	}
 
 	if attr, ok := d.GetOk("desired_capacity"); ok {
-		params.DesiredCapacity = aws.Int(int64(attr.(int)))
+		params.DesiredCapacity = aws.Int64(int64(attr.(int)))
 	}
 
 	log.Printf("[INFO] Creating Autoscaling Scheduled Action: %s", d.Get("scheduled_action_name").(string))
@@ -96,22 +121,69 @@ func resourceAwsAutoscalingScheduleCreate(d *schema.ResourceData, meta interface
 }
 
 func resourceAwsAutoscalingScheduleRead(d *schema.ResourceData, meta interface{}) error {
+	sa, err := resourceAwsASGScheduledActionRetrieve(d, meta)
+	if err != nil {
+		return err
+	}
+
+	d.Set("autoscaling_group_name", sa.AutoScalingGroupName)
+	d.Set("arn", sa.ScheduledActionARN)
+	d.Set("desired_capacity", sa.DesiredCapacity)
+	d.Set("min_size", sa.MinSize)
+	d.Set("max_size", sa.MaxSize)
+	d.Set("recurrance", sa.Recurrence)
+	d.Set("start_time", sa.StartTime.Format(awsAutoscalingScheduleTimeLayout))
+	d.Set("end_time", sa.EndTime.Format(awsAutoscalingScheduleTimeLayout))
+
 	return nil
 }
 
 func resourceAwsAutoscalingScheduleDelete(d *schema.ResourceData, meta interface{}) error {
 	autoscalingconn := meta.(*AWSClient).autoscalingconn
 
-	params := autoscaling.DeleteScheduledActionInput{
+	params := &autoscaling.DeleteScheduledActionInput{
 		AutoScalingGroupName: aws.String(d.Get("autoscaling_group_name").(string)),
-		ScheduledActionName:  aws.String(d.Get("scheduled_action_name").(string)),
+		ScheduledActionName:  aws.String(d.Id()),
 	}
 
-	log.Printf("[INFO] Deleting Autoscaling Scheduled Action: %s", d.Get("scheduled_action_name").(string))
+	log.Printf("[INFO] Deleting Autoscaling Scheduled Action: %s", d.Id())
 	_, err := autoscalingconn.DeleteScheduledAction(params)
 	if err != nil {
 		return fmt.Errorf("Error deleting Autoscaling Scheduled Action: %s", err.Error())
 	}
 
 	return nil
+}
+
+func resourceAwsASGScheduledActionRetrieve(d *schema.ResourceData, meta interface{}) (*autoscaling.ScheduledUpdateGroupAction, error) {
+	autoscalingconn := meta.(*AWSClient).autoscalingconn
+
+	params := &autoscaling.DescribeScheduledActionsInput{
+		AutoScalingGroupName: aws.String(d.Get("autoscaling_group_name").(string)),
+		ScheduledActionNames: []*string{aws.String(d.Id())},
+	}
+
+	log.Printf("[INFO] Describing Autoscaling Scheduled Action: %+v", params)
+	actions, err := autoscalingconn.DescribeScheduledActions(params)
+	if err != nil {
+		return nil, fmt.Errorf("Error retrieving Autoscaling Scheduled Actions: %s", err)
+	}
+
+	if len(actions.ScheduledUpdateGroupActions) != 1 ||
+		*actions.ScheduledUpdateGroupActions[0].ScheduledActionName != d.Id() {
+		return nil, fmt.Errorf("Unable to find Autoscaling Scheduled Action: %#v", actions.ScheduledUpdateGroupActions)
+	}
+
+	return actions.ScheduledUpdateGroupActions[0], nil
+}
+
+func validateASGScheduleTimestamp(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	_, err := time.Parse(awsAutoscalingScheduleTimeLayout, value)
+	if err != nil {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be parsed as iso8601 Timestamp Format", value))
+	}
+
+	return
 }
---
+++ website/source/docs/providers/aws/r/autoscaling_schedule.html.markdown
@@ -0,0 +1,55 @@
+---
+layout: "aws"
+page_title: "AWS: aws_autoscaling_schedule"
+sidebar_current: "docs-aws-resource-autoscaling-schedule"
+description: |-
+  Provides an AutoScaling Schedule resource.
+---
+
+# aws\_autoscaling\_schedule
+
+Provides an AutoScaling Schedule resource.
+
+## Example Usage
+```
+resource "aws_autoscaling_group" "foobar" {
+    availability_zones = ["us-west-2a"]
+    name = "terraform-test-foobar5"
+    max_size = 1
+    min_size = 1
+    health_check_grace_period = 300
+    health_check_type = "ELB"
+    force_delete = true
+    termination_policies = ["OldestInstance"]
+}
+
+resource "aws_autoscaling_schedule" "foobar" {
+    scheduled_action_name = "foobar"
+    min_size = 0
+    max_size = 1
+    desired_capacity = 0
+    start_time = "2016-12-11T18:00:00Z"
+    end_time = "2016-12-12T06:00:00Z"
+    autoscaling_group_name = "${aws_autoscaling_group.foobar.name}"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `autoscaling_group_name` - (Required) The name or Amazon Resource Name (ARN) of the Auto Scaling group.
+* `scheduled_action_name` - (Required) The name of this scaling action.
+* `start_time` - (Optional) The time for this action to start, in "YYYY-MM-DDThh:mm:ssZ" format in UTC/GMT only (for example, 2014-06-01T00:00:00Z ).
+                            If you try to schedule your action in the past, Auto Scaling returns an error message.
+* `end_time` - (Optional) The time for this action to end, in "YYYY-MM-DDThh:mm:ssZ" format in UTC/GMT only (for example, 2014-06-01T00:00:00Z ).
+                          If you try to schedule your action in the past, Auto Scaling returns an error messag
+* `recurrence` - (Optional) The time when recurring future actions will start. Start time is specified by the user following the Unix cron syntax format. 
+* `min_size` - (Optional) The minimum size for the Auto Scaling group.
+* `max_size` - (Optional) The maximum size for the Auto Scaling group.
+* `desired_capacity` - (Optional) The number of EC2 instances that should be running in the group.
+
+~> **NOTE:** When `start_time` and `end_time` are specified with `recurrence` , they form the boundaries of when the recurring action will start and stop.
+
+## Attribute Reference
+* `arn` - The ARN assigned by AWS to the autoscaling schedule.
