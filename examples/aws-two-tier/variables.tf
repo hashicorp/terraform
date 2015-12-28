ca8b82034016ffaa74e3f82876c4cbed9b574e80
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2015-12-28 12:52:52 +0000

    Correct README.md and add missing variable. Correct README.md execution instructions var name and use 'key_name' var as intended in 'aws_key_pair' resource

--- examples/aws-two-tier/variables.tf
+++ examples/aws-two-tier/variables.tf
@@ -4,8 +4,12 @@ Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
 
-Example: ~/.ssh/id_rsa.pub
+Example: ~/.ssh/terraform.pub
 DESCRIPTION
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
 }
 
 variable "aws_region" {
