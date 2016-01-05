aae741032c7e149df64667fe51b55514086e4c9f
pat <pat@22acacia.com>
2016-01-04 23:05:56 -0800

    hello table schema
    the table schema is an arbitrarily deep structure which I'm not
    sure how to represent in terraform's schema syntax.  failing that
    I have introduced a work around:
    - one level deep of table structure in the standard terraform
    schema manner (as many fields as you want, no sub fields or fields
    of type 'record')
    - arbitrary depth but specify a file that has the layout in json
    
    I really like the idea of including an auxilarly file to represent
    the table structure but I'm open to it being spcified via the file
    built-in or something.  I don't really want to let there be
    arbitrary depth configs in the terraform config file.  that seems
    a bit heinous

--- builtin/providers/google/resource_bigquery_table.go
+++ builtin/providers/google/resource_bigquery_table.go
@@ -40,108 +40,41 @@ func resourceBigQueryTable() *schema.Resource {
 				Optional: true,
 			},
 			
+			"schemaFile": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			
 			"schema": &schema.Schema{
 				Type:     schema.TypeList,
 				Optional: true,
 				Elem:     schema.Resource{
-						Schema: map[string]*schema.Schema{
-								"fields": &schema.Schema{
-										Type:	 schema.TypeList,
-										Optiona: true,
-										Elem:	 schema.Resource{
-												Schema: map[string]*schema.Schema{
-														"description": &schema.Schema{
-																Type:     schema.TypeString,
-																Optional: true,
-														},
-														"mode": &schema.Schema{
-																Type:     schema.TypeString,
-																Optional: true,
-														},
-														"name": &schema.Schema{
-																Type:     schema.TypeString,
-																Required: true,
-														},
-														"type": &schema.Schema{
-																Type:     schema.TypeString,
-																Required: true,
-														},
-														"fields": &schema.Schema{
-																Type:	 schema.TypeList,
-																Optiona: true,
-																Elem:	 schema.Resource{
-																		Schema: map[string]*schema.Schema{
-																				"description": &schema.Schema{
-																						Type:     schema.TypeString,
-																						Optional: true,
-																				},
-																				"mode": &schema.Schema{
-																						Type:     schema.TypeString,
-																						Optional: true,
-																				},
-																				"name": &schema.Schema{
-																						Type:     schema.TypeString,
-																						Required: true,
-																				},
-																				"type": &schema.Schema{
-																						Type:     schema.TypeString,
-																						Required: true,
-																				},
-																				"fields": &schema.Schema{
-																						Type:	 schema.TypeList,
-																						Optiona: true,
-																						Elem:	 schema.Resource{
-																								Schema: map[string]*schema.Schema{
-																										"description": &schema.Schema{
-																												Type:     schema.TypeString,
-																												Optional: true,
-																										},
-																										"mode": &schema.Schema{
-																												Type:     schema.TypeString,
-																												Optional: true,
-																										},
-																										"name": &schema.Schema{
-																												Type:     schema.TypeString,
-																												Required: true,
-																										},
-																										"type": &schema.Schema{
-																												Type:     schema.TypeString,
-																												Required: true,
-																										},
-																										"fields": &schema.Schema{
-																												Type:	 schema.TypeList,
-																												Optiona: true,
-																												Elem:	 schema.Resource{
-																														Schema: map[string]*schema.Schema{
-																																"description": &schema.Schema{
-																																		Type:     schema.TypeString,
-																																		Optional: true,
-																																},
-																																"mode": &schema.Schema{
-																																		Type:     schema.TypeString,
-																																		Optional: true,
-																																},
-																																"name": &schema.Schema{
-																																		Type:     schema.TypeString,
-																																		Required: true,
-																																},
-																																"type": &schema.Schema{
-																																		Type:     schema.TypeString,
-																																		Required: true,
-																																},
-																														},
-																												},
-																										},
-																								},
-																						},
-																				},
-																		},
-																},
-														},
-												},
-										},
+					Schema: map[string]*schema.Schema{
+						"fields": &schema.Schema{
+							Type:	 schema.TypeList,
+							Optiona: true,
+							Elem:	 schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"description": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"mode": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"name": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"type": &schema.Schema{
+										Type:     schema.TypeString,
+										Required: true,
+									},
 								},
+							},								
 						},
+					},
 				},
 			},
 			
@@ -149,20 +82,122 @@ func resourceBigQueryTable() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			
+			"id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"kind": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"lastModifiedTime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
 
 		},
 	},
 }
 
+// convert raw field config into TableFieldSchema ref
+func parseField(fieldDef map[string]interface{}) (*bigquery.TableFieldSchema, error) {
+	fieldParsed := &bigquery.TableFieldSchema{}
+	if description, ok := fieldDef["description"]; ok {
+		fieldParsed.Description = description.(string)
+	}
+		
+	if mode, ok := fieldDef["mode"]; ok {
+		fieldParsed.Mode = mode.(string)
+	}
+	
+	if name, ok := fieldDef["name"]; ok {
+		fieldParsed.Name = name.(string)
+	} else {
+		return fmt.Errorf("All fields must have 'name' defined.  The following field did not:  %q\n", field_def)
+	}
+		
+	if type, ok := fieldDef["type"]; ok {
+		fieldParsed.Type = type.(string)
+	} else {
+		return fmt.Errorf("All fields must have 'type' defined.  The following field did not:  %q\n", field_def)
+	}
+		
+	if tableFieldSchema, ok := fieldDef["fields"]; ok {
+		fieldList, err := parseFieldList(tableFieldSchema)
+		if err != nil {
+			return err
+		}
+		fieldParse.TableFieldSchema = fieldList
+	}
+		
+	return fieldParsed
+}
+
+//  convert list of raw field data into list of TableFieldSchema refs
+func parseFieldList(schema []interface{}) ([]*bigquery.TableFieldSchema, error) {
+	tableFieldList := make([]*bigquery.TableFieldSchema, 0)
+	for _, fieldInterface := range schema.([]interface{}) {
+		fieldParsed, err := parseField(fieldInterface.(map[string]interface{}))
+		if err != nil {
+			return err
+		}
+		tableFieldList = append(tableFieldList, fieldParsed)
+	}
+	return tableFieldList
+}
+
 func resourceBigQueryTableCreate(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
+	// build tableRef
 	datasetId := d.Get("datasetId").(string)
 	tableId := d.Get("tableId").(string)
 	tRef := &bigquery.TableReference{DatasetId: datasetId, ProjectId: config.Project, TableId: tableName}
 	
+	// build the table
 	table := &bigquery.Table{TableReference: tRef}
-
+	
+	if description, ok := d.GetOk("description"); ok {
+		table.Description = description.(string)
+	}
+	
+	if expirationTime, ok := d.GetOk("expirationTime"); ok {
+		table.ExpirationTime = expirationTime.(int64)
+	}
+	
+	if friendlyName, ok := d.GetOk("friendlyName"); ok {
+		table.FriendlyName = friendlyName.(string)
+	}
+	
+	// build arbitrarily deep table schema
+	//   first check that didn't specify both schema and schemaFile
+	schema, schemaOk := d.GetOk("schema")
+	schemaFile, schemaFileOk := d.GetOk("schemaFile")
+	if schemaOk && schemaFileOk {
+			return fmt.Errorf("Config contains both schema and schemaFile.  Specify at most one\n");
+	} else if schemaOk {
+		fieldList, err := parse_field_list(schema.([]interface{}))
+		if err != nil {
+			return err
+		}
+		table.Schema = &bigquery.TableSchema{Fields: fieldList}
+	} else if schemaFileOk {
+		schemaJson, err := ioutil.ReadFile(schemaFile)
+		if err != nil {
+			return err
+		}
+		fieldList, err := parse_field_list(schemaJson)
+		if err != nil {
+			return err
+		}
+		table.Schema = &bigquery.TableSchema{Fields: fieldList}
+	}
+	
+	
+	
 	call := config.clientBigQuery.Tables.Insert(config.Project, datasetId, table)
 	_, err := call.Do()
 	if err != nil {
@@ -187,6 +222,10 @@ func resourceBigQueryTableRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	d.SetId(res.Id)
+	d.Set("self_link", res.SelfLink)
+	d.Set("lastModifiedTime", res.LastModifiedTime)
+	d.Set("id", res.Id)
+	d.Set("kind", res.Kind)
 	return nil
 }
 
