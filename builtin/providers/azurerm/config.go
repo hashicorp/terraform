5912bf25b486957ca50149b1a19d148f28de5772
Paul Hinze <phinze@phinze.com>
2016-01-08 14:06:09 -0600

    Update azurerm imports to fix build
    Catches us up with breaking change introduced here:
    
    https://github.com/Azure/azure-sdk-for-go/pull/258

--- builtin/providers/azurerm/config.go
+++ builtin/providers/azurerm/config.go
@@ -9,7 +9,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/Godeps/_workspace/src/github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/azure-sdk-for-go/arm/compute"
 	"github.com/Azure/azure-sdk-for-go/arm/network"
-	"github.com/Azure/azure-sdk-for-go/arm/resources"
+	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
 	"github.com/Azure/azure-sdk-for-go/arm/scheduler"
 	"github.com/Azure/azure-sdk-for-go/arm/storage"
 	"github.com/hashicorp/terraform/terraform"
