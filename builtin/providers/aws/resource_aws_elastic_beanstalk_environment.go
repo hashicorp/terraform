a9f1a505910973fba5550a446c097c2dcb08507c
David Harris <dharris@dharris.io>
2016-01-12 19:11:11 -0700

    Update optionSettingKeyHash function to hash setting key, not setting key and value. Added debug statements for all option names.

--- builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
+++ builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
@@ -356,7 +356,7 @@ func resourceAwsElasticBeanstalkEnvironmentSettingsRead(d *schema.ResourceData, 
 		return err
 	}
 
-	if err := d.Set("setting", updatedSettingsKeySet.List()); err != nil {
+	if err := d.Set("setting", updatedSettings.List()); err != nil {
 		return err
 	}
 
@@ -438,9 +438,7 @@ func optionSettingValueHash(v interface{}) int {
 	optionName := rd["name"].(string)
 	value, _ := rd["value"].(string)
 	hk := fmt.Sprintf("%s:%s=%s", namespace, optionName, sortValues(value))
-	if optionName == "Subnets" {
-		log.Printf("[DEBUG] Elastic Beanstalk optionSettingValueHash(%#v): hk=%s,hc=%d", v, hk, hashcode.String(hk))
-	}
+	log.Printf("[DEBUG] Elastic Beanstalk optionSettingValueHash(%#v): %s: hk=%s,hc=%d", v, optionName,hk, hashcode.String(hk))
 	return hashcode.String(hk)
 }
 
@@ -448,8 +446,9 @@ func optionSettingKeyHash(v interface{}) int {
 	rd := v.(map[string]interface{})
 	namespace := rd["namespace"].(string)
 	optionName := rd["name"].(string)
-	value, _ := rd["value"].(string)
-	return hashcode.String(fmt.Sprintf("%s:%s=%s", namespace, optionName, sortValues(value)))
+	hk := fmt.Sprintf("%s:%s", namespace, optionName)
+	log.Printf("[DEBUG] Elastic Beanstalk optionSettingKeyHash(%#v): %s: hk=%s,hc=%d", v, optionName,hk, hashcode.String(hk))
+	return hashcode.String(hk)
 }
 
 func sortValues(v string) string {
