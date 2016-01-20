23a54464291a6ed8c4cb8c9fa997297a0d00ef1d
Ryan Roberts <ryansroberts@gmail.com>
2016-01-20 15:40:40 +0000

    Resources, no docs

---
+++ builtin/providers/aws/resource_aws_sns_platform_application.go
@@ -0,0 +1,196 @@
+package aws
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAwsSnsPlatformApplicationAPNS() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsSnsPlatformApplicationAPNSCreate,
+		Read:   resourceAwsSnsPlatformApplicationAPNSRead,
+		Update: resourceAwsSnsPlatformApplicationAPNSUpdate,
+		Delete: resourceAwsSnsPlatformApplicationDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: func(v interface{}, k string) (ws []string, errors []error) {
+					value := v.(string)
+					if value != "SANDBOX" && value != "VOIP" {
+						errors = append(errors, fmt.Errorf(
+							"%q has unsupported value %q", k, value))
+					}
+					return
+				},
+			},
+			"platform_credential": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"platform_principal": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceAwsSnsPlatformApplicationAPNSCreate(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	platform := "APNS"
+	if d.Get("type") != nil {
+		platform += "_" + d.Get("type").(string)
+	}
+
+	resp, err := snsconn.CreatePlatformApplication(&sns.CreatePlatformApplicationInput{
+		Platform: aws.String("APNS"),
+		Attributes: aws.StringMap(map[string]string{
+			"PlatformCredential": d.Get("platform_credential").(string),
+			"PlatformPrincipal":  d.Get("platform_principal").(string),
+		}),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error reading platform application %s", err)
+	}
+
+	d.SetId(*resp.PlatformApplicationArn)
+
+	return resourceAwsSnsPlatformApplicationAPNSRead(d, meta)
+}
+
+func resourceAwsSnsPlatformApplicationAPNSRead(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	resp, err := snsconn.GetPlatformApplicationAttributes(&sns.GetPlatformApplicationAttributesInput{
+		PlatformApplicationArn: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error reading plaform application %s", err)
+	}
+
+	d.Set("platform_credential", resp.Attributes["PlatformCredential"])
+	d.Set("platform_principal", resp.Attributes["PlatformPrincipal"])
+
+	return nil
+}
+
+func resourceAwsSnsPlatformApplicationAPNSUpdate(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	resp, err := snsconn.SetPlatformApplicationAttributes(&sns.SetPlatformApplicationAttributesInput{
+		PlatformApplicationArn: aws.String(d.Id()),
+		Attributes: aws.StringMap(map[string]string{
+			"PlatformCredential": d.Get("platform_credential").(string),
+			"PlatformPrincipal":  d.Get("platform_principal").(string),
+		}),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error reading platform application %s", err)
+	}
+
+	log.Printf("[DEBUG] Received GCM platform application: %s", resp)
+
+	return nil
+}
+
+func resourceAwsSnsPlatformApplicationDelete(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	_, err := snsconn.DeletePlatformApplication(&sns.DeletePlatformApplicationInput{
+		PlatformApplicationArn: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error deleting plaform application %s", err)
+	}
+
+	return nil
+}
+
+func resourceAwsSnsPlatformApplicationGCM() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsSnsPlatformApplicationGCMCreate,
+		Read:   resourceAwsSnsPlatformApplicationGCMRead,
+		Update: resourceAwsSnsPlatformApplicationGCMUpdate,
+		Delete: resourceAwsSnsPlatformApplicationDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"platform_credential": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceAwsSnsPlatformApplicationGCMCreate(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	resp, err := snsconn.CreatePlatformApplication(&sns.CreatePlatformApplicationInput{
+		Name:     aws.String(d.Get("name").(string)),
+		Platform: aws.String("GCM"),
+		Attributes: aws.StringMap(map[string]string{
+			"PlatformCredential": d.Get("platform_credential").(string),
+		}),
+	})
+	if err != nil {
+		return fmt.Errorf("Error reading platform application %s", err)
+	}
+
+	d.SetId(*resp.PlatformApplicationArn)
+
+	return resourceAwsSnsPlatformApplicationGCMRead(d, meta)
+}
+
+func resourceAwsSnsPlatformApplicationGCMRead(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	resp, err := snsconn.GetPlatformApplicationAttributes(&sns.GetPlatformApplicationAttributesInput{
+		PlatformApplicationArn: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error reading platform application %s", err)
+	}
+
+	log.Printf("[DEBUG] Received GCM platform application: %s", resp)
+
+	d.Set("platform_credential", resp.Attributes["PlatformCredential"])
+
+	return nil
+}
+
+func resourceAwsSnsPlatformApplicationGCMUpdate(d *schema.ResourceData, meta interface{}) error {
+	snsconn := meta.(*AWSClient).snsconn
+
+	_, err := snsconn.SetPlatformApplicationAttributes(&sns.SetPlatformApplicationAttributesInput{
+		PlatformApplicationArn: aws.String(d.Id()),
+		Attributes: aws.StringMap(map[string]string{
+			"PlatformCredential": d.Get("platform_credential").(string),
+		}),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error updating platform application %s", err)
+	}
+
+	return nil
+}
---
+++ website/source/docs/providers/aws/r/sns_platform_application_gcm.markdown
@@ -0,0 +1,8 @@
++---
+ +layout: "aws"
+ +page_title: "AWS: aws_platform_application_gcm"
+ +sidebar_current: "docs-aws-platform-application-gcm"
+ +description: |-
+ +  Provides a Google Cloud Messaging Platform Application
+ +---
+ +
