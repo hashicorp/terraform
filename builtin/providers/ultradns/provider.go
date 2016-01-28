1b84bea79835a1fceb7669f6d372558da21d9c94
Josh Masseo <josh.masseo@ensighten.com>
2016-01-28 16:49:41 -0600

    ultradns_dirpool: add

--- builtin/providers/ultradns/provider.go
+++ builtin/providers/ultradns/provider.go
@@ -35,6 +35,7 @@ func Provider() terraform.ResourceProvider {
 		ResourcesMap: map[string]*schema.Resource{
 			"ultradns_record": resourceUltraDNSRecord(),
 			"ultradns_tcpool": resourceUltradnsTcpool(),
+			"ultradns_dirpool": resourceUltradnsDirpool(),
 		},
 
 		ConfigureFunc: providerConfigure,
