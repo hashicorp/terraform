b488541ee9960cc4b636c135b11d5f543de18c9b
Joe Topjian <joe@topjian.net>
2016-01-31 22:02:25 +0000

    provider/openstack Add Acceptance Test for No Port IP

--- builtin/providers/openstack/resource_openstack_networking_port_v2_test.go
+++ builtin/providers/openstack/resource_openstack_networking_port_v2_test.go
@@ -63,6 +63,53 @@ func TestAccNetworkingV2Port_basic(t *testing.T) {
 	})
 }
 
+func TestAccNetworkingV2Port_noip(t *testing.T) {
+	region := os.Getenv(OS_REGION_NAME)
+
+	var network networks.Network
+	var port ports.Port
+	var subnet subnets.Subnet
+
+	var testAccNetworkingV2Port_noip = fmt.Sprintf(`
+		resource "openstack_networking_network_v2" "foo" {
+			region = "%s"
+			name = "network_1"
+			admin_state_up = "true"
+		}
+		resource "openstack_networking_subnet_v2" "foo" {
+			region = "%s"
+			name = "subnet_1"
+			network_id = "${openstack_networking_network_v2.foo.id}"
+			cidr = "192.168.199.0/24"
+			ip_version = 4
+		}
+		resource "openstack_networking_port_v2" "foo" {
+			region = "%s"
+			name = "port_1"
+			network_id = "${openstack_networking_network_v2.foo.id}"
+			admin_state_up = "true"
+			fixed_ip {
+				subnet_id =  "${openstack_networking_subnet_v2.foo.id}"
+			}
+		}`, region, region, region)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNetworkingV2PortDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkingV2Port_noip,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNetworkingV2SubnetExists(t, "openstack_networking_subnet_v2.foo", &subnet),
+					testAccCheckNetworkingV2NetworkExists(t, "openstack_networking_network_v2.foo", &network),
+					testAccCheckNetworkingV2PortExists(t, "openstack_networking_port_v2.foo", &port),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckNetworkingV2PortDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
 	networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
