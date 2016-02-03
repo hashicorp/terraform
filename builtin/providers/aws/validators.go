140a2f141c1adf65fd019fda3a0c5b7f9989b29f
Radek Simko <radek.simko@timeinc.com>
2016-02-03 17:55:59 +0000

    provider/aws: Add CloudWatch Event Target

--- builtin/providers/aws/validators.go
+++ builtin/providers/aws/validators.go
@@ -165,3 +165,21 @@ func validateMaxLength(length int) schema.SchemaValidateFunc {
 		return
 	}
 }
+
+func validateCloudWatchEventTargetId(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 64 {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be longer than 64 characters: %q", k, value))
+	}
+
+	// http://docs.aws.amazon.com/AmazonCloudWatchEvents/latest/APIReference/API_Target.html
+	pattern := `^[\.\-_A-Za-z0-9]+$`
+	if !regexp.MustCompile(pattern).MatchString(value) {
+		errors = append(errors, fmt.Errorf(
+			"%q doesn't comply with restrictions (%q): %q",
+			k, pattern, value))
+	}
+
+	return
+}
