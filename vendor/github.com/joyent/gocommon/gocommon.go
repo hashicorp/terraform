7b2c93d15d5e4df8c71aa95f29302b451383c011
James Nugent <james@jen20.com>
2016-02-10 15:30:32 -0500

    provider/triton: New provider for Joyent Triton
    This brings across the following resources for Triton from the
    joyent/triton-terraform repository, and converts them to the canonical
    Terraform style, introducing Terraform-style documentation and
    acceptance tests which run against the live API rather than the local
    APIs:
    
    - triton_firewall_rule
    - triton_machine
    - triton_key

---
+++ vendor/github.com/joyent/gocommon/gocommon.go
@@ -0,0 +1,18 @@
+/*
+The gocommon package collects common packages to interact with the Joyent Public Cloud and Joyent Manta services.
+
+The gocommon package is structured as follow:
+
+	- gocommon/client. Client for sending requests.
+	- gocommon/errors. Joyent specific errors.
+	- gocommon/http. HTTP client for sending requests.
+	- gocommon/jpc. This package provides common structures and functions across packages.
+	- gocommon/testing. Testing Suite for local testing.
+
+Licensed under LGPL v3.
+
+Copyright (c) 2013 Joyent Inc.
+Written by Daniele Stroppa <daniele.stroppa@joyent.com>
+
+*/
+package gocommon
