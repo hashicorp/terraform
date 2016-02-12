b495df7d8408d68209fb636c379a8c62d6d7eb38
David Harris <dharris@dharris.io>
2016-02-11 21:00:08 -0700

    Adding TestAccAWSBeanstalkEnv_version_label test.

--- builtin/providers/aws/resource_aws_elastic_beanstalk_environment_test.go
+++ builtin/providers/aws/resource_aws_elastic_beanstalk_environment_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -24,6 +25,24 @@ func TestAccAWSBeanstalkEnv_basic(t *testing.T) {
 				Config: testAccBeanstalkEnvConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBeanstalkEnvExists("aws_elastic_beanstalk_environment.tfenvtest", &app),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAWSBeanstalkEnv_version_label(t *testing.T) {
+	var app elasticbeanstalk.EnvironmentDescription
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBeanstalkEnvDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccBeanstalkEnvApplicationVersionConfig(acctest.RandInt()),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBeanstalkApplicationVersionDeployed("aws_elastic_beanstalk_environment.default", &app),
 				),
 			},
 		},
@@ -102,6 +121,42 @@ func testAccCheckBeanstalkEnvExists(n string, app *elasticbeanstalk.EnvironmentD
 	}
 }
 
+func testAccCheckBeanstalkApplicationVersionDeployed(n string, env *elasticbeanstalk.EnvironmentDescription) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Elastic Beanstalk ENV is not set")
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).elasticbeanstalkconn
+		describeBeanstalkEnvOpts := &elasticbeanstalk.DescribeEnvironmentsInput{
+			EnvironmentIds: []*string{aws.String(rs.Primary.ID)},
+		}
+
+		log.Printf("[DEBUG] Elastic Beanstalk Environment TEST describe opts: %s", describeBeanstalkEnvOpts)
+
+		resp, err := conn.DescribeEnvironments(describeBeanstalkEnvOpts)
+		if err != nil {
+			return err
+		}
+		if len(resp.Environments) == 0 {
+			return fmt.Errorf("Elastic Beanstalk ENV not found.")
+		}
+
+		if *resp.Environments[0].VersionLabel != rs.Primary.Attributes["version_label"] {
+			return fmt.Errorf("Elastic Beanstalk version deployed %s. Expected %s", resp.Environments[0].VersionLabel, rs.Primary.Attributes["version_label"])
+		}
+
+		*env = *resp.Environments[0]
+
+		return nil
+	}
+}
+
 const testAccBeanstalkEnvConfig = `
 resource "aws_elastic_beanstalk_application" "tftest" {
   name = "tf-test-name"
@@ -115,3 +170,36 @@ resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   #solution_stack_name =
 }
 `
+
+func testAccBeanstalkEnvApplicationVersionConfig(randInt int) string {
+	return fmt.Sprintf(`
+resource "aws_s3_bucket" "default" {
+  bucket = "tftest.applicationversion.buckets-%d"
+}
+
+resource "aws_s3_bucket_object" "default" {
+  bucket = "${aws_s3_bucket.default.id}"
+  key = "beanstalk/go-v1.zip"
+  source = "test-fixtures/beanstalk-go-v1.zip"
+}
+
+resource "aws_elastic_beanstalk_application" "default" {
+  name = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+  application = "tf-test-name"
+  name = "tf-test-version-label"
+  bucket = "${aws_s3_bucket.default.id}"
+  key = "${aws_s3_bucket_object.default.id}"
+}
+
+resource "aws_elastic_beanstalk_environment" "default" {
+  name = "tf-test-name"
+  application = "${aws_elastic_beanstalk_application.default.name}"
+  version_label = "${aws_elastic_beanstalk_application_version.default.name}"
+  solution_stack_name = "64bit Amazon Linux 2015.09 v2.0.4 running Go 1.4"
+}
+`, randInt)
+}
