f8d8324c0f8064fc07c69e47b105efb0665617a3
James Nugent <james@jen20.com>
2016-02-18 13:33:01 -0800

    provider/azurerm: Add generic state refresh func

--- builtin/providers/azurerm/resource_arm_search_service.go
+++ builtin/providers/azurerm/resource_arm_search_service.go
@@ -3,7 +3,9 @@ package azurerm
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/jen20/riviera/azure"
 	"github.com/jen20/riviera/search"
@@ -94,11 +96,13 @@ func resourceArmSearchServiceCreate(d *schema.ResourceData, meta interface{}) er
 		return fmt.Errorf("Error creating Search Service: %s", createResponse.Error)
 	}
 
-	readRequest := rivieraClient.NewRequest()
-	readRequest.Command = &search.GetSearchService{
+	getSearchServiceCommand := &search.GetSearchService{
 		Name:              d.Get("name").(string),
 		ResourceGroupName: d.Get("resource_group_name").(string),
 	}
+
+	readRequest := rivieraClient.NewRequest()
+	readRequest.Command = getSearchServiceCommand
 
 	readResponse, err := readRequest.Execute()
 	if err != nil {
@@ -107,8 +111,20 @@ func resourceArmSearchServiceCreate(d *schema.ResourceData, meta interface{}) er
 	if !readResponse.IsSuccessful() {
 		return fmt.Errorf("Error reading Search Service: %s", readResponse.Error)
 	}
-
 	resp := readResponse.Parsed.(*search.GetSearchServiceResponse)
+
+	log.Printf("[DEBUG] Waiting for Search Service (%s) to become available", d.Get("name"))
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"provisioning"},
+		Target:  []string{"succeeded"},
+		Refresh: azureStateRefreshFunc(*resp.ID, client, getSearchServiceCommand),
+		// ¯\_(ツ)_/¯
+		Timeout: 30 * time.Minute,
+	}
+	if _, err := stateConf.WaitForState(); err != nil {
+		return fmt.Errorf("Error waiting for Search Service (%s) to become available: %s", d.Get("name"), err)
+	}
+
 	d.SetId(*resp.ID)
 
 	return resourceArmSearchServiceRead(d, meta)
