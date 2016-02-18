dbc5464a197fedb12f3edf23b46192ca90aaf1a4
James Nugent <james@jen20.com>
2016-02-18 13:34:38 -0800

    Update jen20/riviera dependency

--- vendor/github.com/jen20/riviera/azure/request.go
+++ vendor/github.com/jen20/riviera/azure/request.go
@@ -37,7 +37,6 @@ func readTaggedFields(command interface{}) map[string]interface{} {
 	result := make(map[string]interface{})
 
 	for i := 0; i < value.NumField(); i++ { // iterates through every struct type field
-		fmt.Println(i, value.Type().Field(i).Name)
 		tag := value.Type().Field(i).Tag // returns the tag string
 		tagValue := tag.Get("riviera")
 		if tagValue != "" {
--- vendor/github.com/jen20/riviera/search/get_search_service.go
+++ vendor/github.com/jen20/riviera/search/get_search_service.go
@@ -3,17 +3,17 @@ package search
 import "github.com/jen20/riviera/azure"
 
 type GetSearchServiceResponse struct {
-	ID                 *string            `mapstructure:"id"`
-	Name               string             `json:"-"`
-	ResourceGroupName  string             `json:"-"`
-	Location           string             `json:"-" riviera:"location"`
-	Tags               map[string]*string `json:"-" riviera:"tags"`
-	Sku                *Sku               `json:"sku,omitempty"`
-	ReplicaCount       *string            `json:"replicaCount,omitempty"`
-	PartitionCount     *string            `json:"partitionCount,omitempty"`
-	Status             *string            `mapstructure:"status"`
-	StatusDetails      *string            `mapstructure:"statusDetails"`
-	ProvisioningStatus *string            `mapstructure:"provisioningStatus"`
+	ID                *string            `mapstructure:"id"`
+	Name              string             `json:"-"`
+	ResourceGroupName string             `json:"-"`
+	Location          string             `json:"-" riviera:"location"`
+	Tags              map[string]*string `json:"-" riviera:"tags"`
+	Sku               *Sku               `json:"sku,omitempty"`
+	ReplicaCount      *string            `json:"replicaCount,omitempty"`
+	PartitionCount    *string            `json:"partitionCount,omitempty"`
+	Status            *string            `mapstructure:"status"`
+	StatusDetails     *string            `mapstructure:"statusDetails"`
+	ProvisioningState *string            `mapstructure:"provisioningState"`
 }
 
 type GetSearchService struct {
