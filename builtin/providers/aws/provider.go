032e6081cbc61418a44ccc8ce4415bedb6c27eb5
Raphael Randschau <nicolai86@me.com>
2016-03-05 23:15:22 +0100

    Add aws_api_gateway_method resource

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -116,6 +116,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_ami_from_instance":                resourceAwsAmiFromInstance(),
 			"aws_api_gateway_rest_api":             resourceAwsApiGatewayRestApi(),
 			"aws_api_gateway_resource":             resourceAwsApiGatewayResource(),
+			"aws_api_gateway_method":               resourceAwsApiGatewayMethod(),
 			"aws_app_cookie_stickiness_policy":     resourceAwsAppCookieStickinessPolicy(),
 			"aws_autoscaling_group":                resourceAwsAutoscalingGroup(),
 			"aws_autoscaling_notification":         resourceAwsAutoscalingNotification(),
