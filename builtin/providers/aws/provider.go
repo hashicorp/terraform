a73721d248d81808f898155cc7a969c13257e682
Raphael Randschau <nicolai86@me.com>
2016-03-05 23:13:38 +0100

    Add aws_api_gateway_rest_api resource

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -114,6 +114,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_ami":                              resourceAwsAmi(),
 			"aws_ami_copy":                         resourceAwsAmiCopy(),
 			"aws_ami_from_instance":                resourceAwsAmiFromInstance(),
+			"aws_api_gateway_rest_api":             resourceAwsApiGatewayRestApi(),
 			"aws_app_cookie_stickiness_policy":     resourceAwsAppCookieStickinessPolicy(),
 			"aws_autoscaling_group":                resourceAwsAutoscalingGroup(),
 			"aws_autoscaling_notification":         resourceAwsAutoscalingNotification(),
