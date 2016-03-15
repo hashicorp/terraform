3ee3c18e93842721c65a792d0b0d588937012c27
Paul Hinze <phinze@phinze.com>
2016-03-15 10:03:01 -0500

    core: Fix issues with ignore_changes
    The ignore_changes diff filter was stripping out attributes on Create
    but the diff was still making it down to the provider, so Create would
    end up missing attributes, causing a full failure if any required
    attributes were being ignored.
    
    In addition, any changes that required a replacement of the resource
    were causing problems with `ignore_chages`, which didn't properly filter
    out the replacement when the triggering attributes were filtered out.
    
    Refs #5627

--- builtin/providers/test/resource_test.go
+++ builtin/providers/test/resource_test.go
@@ -27,6 +27,145 @@ resource "test_resource" "foo" {
 	})
 }
 
+// Targeted test in TestContext2Apply_ignoreChangesCreate
+func TestResource_ignoreChangesRequired(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResourceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+       required = "yep"
+       lifecycle {
+               ignore_changes = ["required"]
+       }
+}
+                               `),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+		},
+	})
+}
+
+func TestResource_ignoreChangesEmpty(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResourceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_force_new = "one"
+	lifecycle {
+		ignore_changes = []
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_force_new = "two"
+	lifecycle {
+		ignore_changes = []
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+		},
+	})
+}
+
+func TestResource_ignoreChangesForceNew(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResourceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_force_new = "one"
+	lifecycle {
+		ignore_changes = ["optional_force_new"]
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_force_new = "two"
+	lifecycle {
+		ignore_changes = ["optional_force_new"]
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+		},
+	})
+}
+
+func TestResource_ignoreChangesMap(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResourceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_computed_map {
+		foo = "bar"
+	}
+	lifecycle {
+		ignore_changes = ["optional_computed_map"]
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+			resource.TestStep{
+				Config: strings.TrimSpace(`
+resource "test_resource" "foo" {
+	required           = "yep"
+	optional_computed_map {
+		foo = "bar"
+		no  = "update"
+	}
+	lifecycle {
+		ignore_changes = ["optional_computed_map"]
+	}
+}
+				`),
+				Check: func(s *terraform.State) error {
+					return nil
+				},
+			},
+		},
+	})
+}
+
 func testAccCheckResourceDestroy(s *terraform.State) error {
 	return nil
 }
