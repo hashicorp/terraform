8d54df95e1c18ff98d6799ba50192fd1a3bdc992
James Nugent <james@jen20.com>
2016-03-16 11:52:40 +0000

    release: clean up after v0.6.13

--- terraform/version.go
+++ terraform/version.go
@@ -1,9 +1,9 @@
 package terraform
 
 // The main version number that is being run at the moment.
-const Version = "0.6.13"
+const Version = "0.6.14"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "dev"
