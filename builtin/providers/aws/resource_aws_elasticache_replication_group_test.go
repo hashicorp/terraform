357d3df799acd230333e2cd614907af975bcadbe
Alex Gray <agray@clearcareonline.com>
2016-03-16 13:44:20 -0400

    Trying to get gofrmt to work...

--- builtin/providers/aws/resource_aws_elasticache_replication_group_test.go
+++ builtin/providers/aws/resource_aws_elasticache_replication_group_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/hashicorp/terraform/helper/acctest"
+
 )
 
 func TestAccAWSEcacheReplicationGroup(t *testing.T) {
