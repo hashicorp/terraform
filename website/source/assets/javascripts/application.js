87d6078cffeec15803c844efa64805e561ff6175
Seth Vargo <sethvargo@gmail.com>
2016-03-22 23:33:40 +0200

    Do not build supporting JS files
    These files are already included in the bundle

---
+++ website/source/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+//= require jquery
+//= require bootstrap
+
+//= require lib/_String.substitute
+//= require lib/_Vector
+//= require lib/_Function.prototype.bind
+//= require lib/_Base
+//= require lib/_Chainable
+//= require lib/_dbg
+
+//= require app/_Docs
+//= require app/_Logo
+//= require app/_Grid
+//= require app/_Engine
+//= require app/_Engine.Particle
+//= require app/_Engine.Particle.Fixed
+//= require app/_Engine.Point
+//= require app/_Engine.Point.Puller
+//= require app/_Engine.Polygon
+//= require app/_Engine.Polygon.Puller
+//= require app/_Engine.Shape
+//= require app/_Engine.Shape.Puller
+//= require app/_Engine.Typewriter
+//= require app/_Sidebar
+//= require app/_Init
