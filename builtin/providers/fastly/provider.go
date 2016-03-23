2ad37bba4a65a18891586389919acb1006e304d6
clint shryock <clint@ctshryock.com>
2016-03-23 14:53:50 -0500

    provider/fastly: Add Fastly Provider, ServiceV1 resource

---
+++ builtin/providers/fastly/provider.go
@@ -0,0 +1,34 @@
+package fastly
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"FASTLY_API_KEY",
+				}, nil),
+				Description: "Fastly API Key from https://app.fastly.com/#account",
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"fastly_service_v1": resourceServiceV1(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		ApiKey: d.Get("api_key").(string),
+	}
+	return config.Client()
+}
