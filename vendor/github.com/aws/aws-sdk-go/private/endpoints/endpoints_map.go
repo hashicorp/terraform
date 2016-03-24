e9c976fed7b331ea5f28e9781e162d925ec9bed4
James Nugent <james@jen20.com>
2016-03-24 15:52:51 -0700

    deps: Update github.com/aws/aws-sdk-go

--- vendor/github.com/aws/aws-sdk-go/private/endpoints/endpoints_map.go
+++ vendor/github.com/aws/aws-sdk-go/private/endpoints/endpoints_map.go
@@ -31,8 +31,7 @@ var endpointsMap = endpointStruct{
 			SigningRegion: "us-east-1",
 		},
 		"*/ec2metadata": {
-			Endpoint:      "http://169.254.169.254/latest",
-			SigningRegion: "us-east-1",
+			Endpoint: "http://169.254.169.254/latest",
 		},
 		"*/iam": {
 			Endpoint:      "iam.amazonaws.com",
@@ -60,6 +59,9 @@ var endpointsMap = endpointStruct{
 		"cn-north-1/*": {
 			Endpoint: "{service}.{region}.amazonaws.com.cn",
 		},
+		"cn-north-1/ec2metadata": {
+			Endpoint: "http://169.254.169.254/latest",
+		},
 		"eu-central-1/s3": {
 			Endpoint: "{service}.{region}.amazonaws.com",
 		},
@@ -69,6 +71,9 @@ var endpointsMap = endpointStruct{
 		"us-east-1/sdb": {
 			Endpoint:      "sdb.amazonaws.com",
 			SigningRegion: "us-east-1",
+		},
+		"us-gov-west-1/ec2metadata": {
+			Endpoint: "http://169.254.169.254/latest",
 		},
 		"us-gov-west-1/iam": {
 			Endpoint: "iam.us-gov.amazonaws.com",
