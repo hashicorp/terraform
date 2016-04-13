02376e1e0e8519b4f02e50d5bbeb1030e01df4a0
James Nugent <james@jen20.com>
2016-04-13 10:18:20 -0500

    core: Add test demonstrating module input types
    This test demonstrates a problem where the types to a module input are
    not checked. For example, if a module - inner - defines a variable
    "should_be_a_map" as a map, or with a default variable of map, we do not
    fail if the user sets the variable value in the outer module to a string
    value.

--- terraform/context_plan_test.go
+++ terraform/context_plan_test.go
@@ -626,6 +626,23 @@ func TestContext2Plan_moduleVar(t *testing.T) {
 	}
 }
 
+func TestContext2Plan_moduleVarWrongType(t *testing.T) {
+	m := testModule(t, "plan-module-wrong-var-type")
+	p := testProvider("aws")
+	p.DiffFn = testDiffFn
+	ctx := testContext2(t, &ContextOpts{
+		Module: m,
+		Providers: map[string]ResourceProviderFactory{
+			"aws": testProviderFuncFixed(p),
+		},
+	})
+
+	_, err := ctx.Plan()
+	if err == nil {
+		t.Fatalf("should error")
+	}
+}
+
 func TestContext2Plan_moduleVarComputed(t *testing.T) {
 	m := testModule(t, "plan-module-var-computed")
 	p := testProvider("aws")
@@ -2035,6 +2052,7 @@ func TestContext2Plan_varListErr(t *testing.T) {
 	})
 
 	_, err := ctx.Plan()
+
 	if err == nil {
 		t.Fatal("should error")
 	}
