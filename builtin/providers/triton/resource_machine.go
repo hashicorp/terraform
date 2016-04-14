7940babad34295f7f2f8030eccb63866b1ac50e3
Patrick Sodré <sodre@sodre.co>
2016-04-13 22:28:40 -0400

    Added Triton CNS domain names

--- builtin/providers/triton/resource_machine.go
+++ builtin/providers/triton/resource_machine.go
@@ -174,6 +174,14 @@ func resourceMachine() *schema.Resource {
 				Optional:    true,
 				Default:     "up",
 			},
+			"domain_names": {
+				Description: "list of domain names from Triton's CNS",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
 
 			// computed resources from metadata
 			"root_authorized_keys": {
@@ -300,6 +308,7 @@ func resourceMachineRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("image", machine.Image)
 	d.Set("primaryip", machine.PrimaryIP)
 	d.Set("firewall_enabled", machine.FirewallEnabled)
+	d.Set("domain_names", machine.DomainNames)
 
 	// create and update NICs
 	var (
