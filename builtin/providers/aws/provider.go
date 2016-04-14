363012332c79a527cfb4b79480316d9bf6dba80b
Chris Marchesi <chrism@vancluevertech.com>
2016-04-14 12:55:11 -0700

    CloudFront distribution and origin access identity support (#5221)
    * CloudFront implementation v3
    
    * Update tests
    
    * Refactor - new resource: aws_cloudfront_distribution
    
     * Includes a complete re-write of the old aws_cloudfront_web_distribution
       resource to bring it to feature parity with API and CloudFormation.
     * Also includes the aws_cloudfront_origin_access_identity resource to generate
       origin access identities for use with S3.

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -129,6 +129,8 @@ func Provider() terraform.ResourceProvider {
 			"aws_autoscaling_policy":                       resourceAwsAutoscalingPolicy(),
 			"aws_autoscaling_schedule":                     resourceAwsAutoscalingSchedule(),
 			"aws_cloudformation_stack":                     resourceAwsCloudFormationStack(),
+			"aws_cloudfront_distribution":                  resourceAwsCloudFrontDistribution(),
+			"aws_cloudfront_origin_access_identity":        resourceAwsCloudFrontOriginAccessIdentity(),
 			"aws_cloudtrail":                               resourceAwsCloudTrail(),
 			"aws_cloudwatch_event_rule":                    resourceAwsCloudWatchEventRule(),
 			"aws_cloudwatch_event_target":                  resourceAwsCloudWatchEventTarget(),
