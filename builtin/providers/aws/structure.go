a38ccbe0748f527463f7396b6fd3b8b575865356
Chris Marchesi <chrism@vancluevertech.com>
2016-04-14 12:55:11 -0700

    CloudFront distribution and origin access identity support (#5221)
    * CloudFront implementation v3
    
    * Update tests
    
    * Refactor - new resource: aws_cloudfront_distribution
    
     * Includes a complete re-write of the old aws_cloudfront_web_distribution
       resource to bring it to feature parity with API and CloudFormation.
     * Also includes the aws_cloudfront_origin_access_identity resource to generate
       origin access identities for use with S3.

--- builtin/providers/aws/structure.go
+++ builtin/providers/aws/structure.go
@@ -1077,3 +1077,23 @@ func flattenBeanstalkTrigger(list []*elasticbeanstalk.Trigger) []string {
 	}
 	return strs
 }
+
+// There are several parts of the AWS API that will sort lists of strings,
+// causing diffs inbetweeen resources that use lists. This avoids a bit of
+// code duplication for pre-sorts that can be used for things like hash
+// functions, etc.
+func sortInterfaceSlice(in []interface{}) []interface{} {
+	a := []string{}
+	b := []interface{}{}
+	for _, v := range in {
+		a = append(a, v.(string))
+	}
+
+	sort.Strings(a)
+
+	for _, v := range a {
+		b = append(b, v)
+	}
+
+	return b
+}
