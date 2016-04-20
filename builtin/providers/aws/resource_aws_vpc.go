12ffdce95412a99c14c034692b9e0987a771b129
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-04-20 10:36:28 -0700

    providers/aws: vpc refresh sets instance_tenancy

--- builtin/providers/aws/resource_aws_vpc.go
+++ builtin/providers/aws/resource_aws_vpc.go
@@ -140,6 +140,7 @@ func resourceAwsVpcRead(d *schema.ResourceData, meta interface{}) error {
 	vpcid := d.Id()
 	d.Set("cidr_block", vpc.CidrBlock)
 	d.Set("dhcp_options_id", vpc.DhcpOptionsId)
+	d.Set("instance_tenancy", vpc.InstanceTenancy)
 
 	// Tags
 	d.Set("tags", tagsToMap(vpc.Tags))
