f0511631bfa621518f6e81282568ec5addd9e835
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-04-21 21:36:14 -0700

    provider/aws: id-only refresh for autoscaling groups

--- builtin/providers/aws/resource_aws_autoscaling_group_test.go
+++ builtin/providers/aws/resource_aws_autoscaling_group_test.go
@@ -22,9 +22,11 @@ func TestAccAWSAutoScalingGroup_basic(t *testing.T) {
 	randName := fmt.Sprintf("terraform-test-%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckAWSAutoScalingGroupDestroy,
+		PreCheck:        func() { testAccPreCheck(t) },
+		IDRefreshName:   "aws_autoscaling_group.bar",
+		IDRefreshIgnore: []string{"force_delete", "metrics_granularity", "wait_for_capacity_timeout"},
+		Providers:       testAccProviders,
+		CheckDestroy:    testAccCheckAWSAutoScalingGroupDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccAWSAutoScalingGroupConfig(randName),
