e9d8b8e9a2fcf63bae0911c5cbfd7cc7b4012946
Cameron Stokes <cameron@cameronstokes.com>
2016-04-22 08:42:22 -0700

    provider/aws: increase timeout for aws_redshift_cluster (#6305)
    * provider/aws: increase timeout for aws_redshift_cluster
    
    * provider/aws: increase timeout for redshift updates too

--- builtin/providers/aws/resource_aws_redshift_cluster.go
+++ builtin/providers/aws/resource_aws_redshift_cluster.go
@@ -265,8 +265,8 @@ func resourceAwsRedshiftClusterCreate(d *schema.ResourceData, meta interface{}) 
 		Pending:    []string{"creating", "backing-up", "modifying"},
 		Target:     []string{"available"},
 		Refresh:    resourceAwsRedshiftClusterStateRefreshFunc(d, meta),
-		Timeout:    5 * time.Minute,
-		MinTimeout: 3 * time.Second,
+		Timeout:    40 * time.Minute,
+		MinTimeout: 10 * time.Second,
 	}
 
 	_, err = stateConf.WaitForState()
@@ -424,8 +424,8 @@ func resourceAwsRedshiftClusterUpdate(d *schema.ResourceData, meta interface{}) 
 		Pending:    []string{"creating", "deleting", "rebooting", "resizing", "renaming", "modifying"},
 		Target:     []string{"available"},
 		Refresh:    resourceAwsRedshiftClusterStateRefreshFunc(d, meta),
-		Timeout:    10 * time.Minute,
-		MinTimeout: 5 * time.Second,
+		Timeout:    40 * time.Minute,
+		MinTimeout: 10 * time.Second,
 	}
 
 	// Wait, catching any errors
