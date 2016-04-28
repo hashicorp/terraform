7a46ba839b5af52d23d630388cf9e1d750bcadb2
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-04-28 19:28:23 +0200

    terraform: verify refresh is called on import

--- terraform/context_import_test.go
+++ terraform/context_import_test.go
@@ -39,8 +39,56 @@ func TestContextImport_basic(t *testing.T) {
 	}
 }
 
+func TestContextImport_refresh(t *testing.T) {
+	p := testProvider("aws")
+	ctx := testContext2(t, &ContextOpts{
+		Providers: map[string]ResourceProviderFactory{
+			"aws": testProviderFuncFixed(p),
+		},
+	})
+
+	p.ImportStateReturn = []*InstanceState{
+		&InstanceState{
+			ID:        "foo",
+			Ephemeral: EphemeralState{Type: "aws_instance"},
+		},
+	}
+
+	p.RefreshFn = func(info *InstanceInfo, s *InstanceState) (*InstanceState, error) {
+		return &InstanceState{
+			ID:         "foo",
+			Attributes: map[string]string{"foo": "bar"},
+		}, nil
+	}
+
+	state, err := ctx.Import(&ImportOpts{
+		Targets: []*ImportTarget{
+			&ImportTarget{
+				Addr: "aws_instance.foo",
+				ID:   "bar",
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	actual := strings.TrimSpace(state.String())
+	expected := strings.TrimSpace(testImportRefreshStr)
+	if actual != expected {
+		t.Fatalf("bad: \n%s", actual)
+	}
+}
+
 const testImportStr = `
 aws_instance.foo:
   ID = foo
   provider = aws
+`
+
+const testImportRefreshStr = `
+aws_instance.foo:
+  ID = foo
+  provider = aws
+  foo = bar
 `
