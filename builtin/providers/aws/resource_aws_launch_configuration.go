0ff9837e168c731b33e85dde7d0cbcb80b458681
Sarguru Nathan <r00t@sarguru.me>
2016-04-30 14:09:09 -0400

    Fix launch config snapshot usage

--- builtin/providers/aws/resource_aws_launch_configuration.go
+++ builtin/providers/aws/resource_aws_launch_configuration.go
@@ -334,11 +334,14 @@ func resourceAwsLaunchConfigurationCreate(d *schema.ResourceData, meta interface
 			bd := v.(map[string]interface{})
 			ebs := &autoscaling.Ebs{
 				DeleteOnTermination: aws.Bool(bd["delete_on_termination"].(bool)),
-				Encrypted:           aws.Bool(bd["encrypted"].(bool)),
 			}
 
 			if v, ok := bd["snapshot_id"].(string); ok && v != "" {
 				ebs.SnapshotId = aws.String(v)
+			}
+
+			if v, ok := bd["encrypted"].(bool); ok && v {
+				ebs.Encrypted = aws.Bool(v)
 			}
 
 			if v, ok := bd["volume_size"].(int); ok && v != 0 {
