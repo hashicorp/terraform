0725e7a6b52bbcc2dce73f0fea194df6820ddcf0
Felivel Camilo <felivel.camilo@gmail.com>
2016-05-08 12:27:04 -0400

    Adding private ip address reference
    * adding private ip address reference
    * Updating the docs.

--- builtin/providers/azurerm/resource_arm_network_interface_card.go
+++ builtin/providers/azurerm/resource_arm_network_interface_card.go
@@ -53,6 +53,12 @@ func resourceArmNetworkInterface() *schema.Resource {
 				Computed: true,
 			},
 
+			"private_ip_address": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
 			"virtual_machine_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
@@ -241,7 +247,7 @@ func resourceArmNetworkInterfaceRead(d *schema.ResourceData, meta interface{}) e
 		return nil
 	}
 	if err != nil {
-		return fmt.Errorf("Error making Read request on Azure Netowkr Interface %s: %s", name, err)
+		return fmt.Errorf("Error making Read request on Azure Network Interface %s: %s", name, err)
 	}
 
 	iface := *resp.Properties
@@ -249,6 +255,17 @@ func resourceArmNetworkInterfaceRead(d *schema.ResourceData, meta interface{}) e
 	if iface.MacAddress != nil {
 		if *iface.MacAddress != "" {
 			d.Set("mac_address", iface.MacAddress)
+		}
+	}
+
+	if iface.IPConfigurations != nil && len(*iface.IPConfigurations) > 0 {
+		var privateIPAddress *string
+		for _, ipConfig := range *iface.IPConfigurations {
+			privateIPAddress = ipConfig.Properties.PrivateIPAddress
+		}
+
+		if *privateIPAddress != "" {
+			d.Set("private_ip_address", *privateIPAddress)
 		}
 	}
 
