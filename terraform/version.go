5a72cc310245c1dde269a728ef0949429d3dcb5d
James Nugent <james@jen20.com>
2016-05-09 20:31:07 +0000

    v0.6.16

--- terraform/version.go
+++ terraform/version.go
@@ -6,4 +6,4 @@ const Version = "0.6.16"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = "dev"
+const VersionPrerelease = ""
