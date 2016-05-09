b971f1a06b11567e77c2db3970265e154be65af9
Daniel <daniel.possmann@boerse-go.de>
2016-05-09 18:30:06 +0200

    added nil checks

--- builtin/providers/vsphere/resource_vsphere_virtual_machine.go
+++ builtin/providers/vsphere/resource_vsphere_virtual_machine.go
@@ -795,15 +795,19 @@ func resourceVSphereVirtualMachineRead(d *schema.ResourceData, meta interface{})
 			networkInterfaces = append(networkInterfaces, networkInterface)
 		}
 	}
-	for _, v := range mvm.Guest.IpStack {
-		for _, route := range v.IpRouteConfig.IpRoute {
-			if route.Network == "0.0.0.0" {
-				deviceID, err := strconv.Atoi(route.Gateway.Device)
-				if err != nil {
-					log.Printf("[DEBUG] error at processing device id %#v: %#v", route.Gateway.Device, err)
-				} else {
-					log.Printf("[DEBUG] gateway of device id %d: %s", deviceID, route.Gateway.IpAddress)
-					networkInterfaces[deviceID]["ipv4_gateway"] = route.Gateway.IpAddress
+	if mvm.Guest.IpStack != nil {
+		for _, v := range mvm.Guest.IpStack {
+			if v.IpRouteConfig != nil && v.IpRouteConfig.IpRoute != nil {
+				for _, route := range v.IpRouteConfig.IpRoute {
+					if route.Network == "0.0.0.0" && route.Gateway.Device != nil {
+						deviceID, err := strconv.Atoi(route.Gateway.Device)
+						if err != nil {
+							log.Printf("[DEBUG] error at processing device id %#v: %#v", route.Gateway.Device, err)
+						} else {
+							log.Printf("[DEBUG] gateway of device id %d: %s", deviceID, route.Gateway.IpAddress)
+							networkInterfaces[deviceID]["ipv4_gateway"] = route.Gateway.IpAddress
+						}
+					}
 				}
 			}
 		}
