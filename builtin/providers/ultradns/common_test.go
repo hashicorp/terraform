57fa72fc74d590dc09a1ca7f81230732fe7d1857
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2016-05-09 23:17:45 -0700

    ultradns_record: cleanup testing

---
+++ builtin/providers/ultradns/common_test.go
@@ -0,0 +1,44 @@
+package ultradns
+
+import (
+	"fmt"
+
+	"github.com/Ensighten/udnssdk"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func testAccCheckUltradnsRecordExists(n string, record *udnssdk.RRSet) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		client := testAccProvider.Meta().(*udnssdk.Client)
+		k := udnssdk.RRSetKey{
+			Zone: rs.Primary.Attributes["zone"],
+			Name: rs.Primary.Attributes["name"],
+			Type: rs.Primary.Attributes["type"],
+		}
+
+		foundRecord, err := client.RRSets.Select(k)
+
+		if err != nil {
+			return err
+		}
+
+		if foundRecord[0].OwnerName != rs.Primary.Attributes["hostname"] {
+			return fmt.Errorf("Record not found: %+v,\n %+v\n", foundRecord, rs.Primary.Attributes)
+		}
+
+		*record = foundRecord[0]
+
+		return nil
+	}
+}
