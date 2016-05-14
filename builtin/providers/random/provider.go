b1de477691fa92c53dfaaadd0787a40cfa1ae142
Martin Atkins <mart@degeneration.co.uk>
2016-05-14 14:00:58 -0700

    provider/random: random_id resource
    This resource generates a cryptographically-strong set of bytes and
    provides them as base64, hexadecimal and decimal string representations.
    It is intended to be used for generating unique ids for resources
    elsewhere in the configuration, and thus the "keepers" would be set to
    any ForceNew attributes of the target resources, so that a new id is
    generated each time a new resource is generated.

--- builtin/providers/random/provider.go
+++ builtin/providers/random/provider.go
@@ -11,7 +11,7 @@ func Provider() terraform.ResourceProvider {
 		Schema: map[string]*schema.Schema{},
 
 		ResourcesMap: map[string]*schema.Resource{
-			//"random_id":      resourceId(),
+			"random_id":      resourceId(),
 			//"random_shuffle": resourceShuffle(),
 		},
 	}
