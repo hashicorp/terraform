1715c539841436e8c7f6fff558341ca1512a612e
John Engelman <john.r.engelman@gmail.com>
2016-05-20 14:58:44 -0500

    Make policy attachment resources manage only 1 entity<->policy attachment

--- builtin/providers/aws/resource_aws_iam_role_policy_attachment.go
+++ builtin/providers/aws/resource_aws_iam_role_policy_attachment.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -14,53 +15,50 @@ func resourceAwsIamRolePolicyAttachment() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAwsIamRolePolicyAttachmentCreate,
 		Read:   resourceAwsIamRolePolicyAttachmentRead,
-		Update: resourceAwsIamRolePolicyAttachmentUpdate,
 		Delete: resourceAwsIamRolePolicyAttachmentDelete,
 
 		Schema: map[string]*schema.Schema{
-			"name": &schema.Schema{
+			"role": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"role": &schema.Schema{
+			"policy_arn": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
-			},
-			"policy_arns": &schema.Schema{
-				Type:     schema.TypeSet,
-				Required: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Set:      schema.HashString,
+				ForceNew: true,
 			},
 		},
 	}
 }
 
+func getRolePolicyAttachmentId(role, policyArn string) string {
+	return fmt.Sprintf("%s:%s", role, policyArn)
+}
+
+func getRoleAndPolicyFromAttachment(id string) (string, string) {
+	tokens := strings.Split(id, ":")
+	return tokens[0], tokens[1]
+}
+
 func resourceAwsIamRolePolicyAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
 
-	name := d.Get("name").(string)
 	role := d.Get("role").(string)
-	arns := expandStringList(d.Get("policy_arns").(*schema.Set).List())
+	arn := d.Get("policy_arn").(string)
 
-	if len(arns) == 0 {
-		return fmt.Errorf("[WARN] No Policies specified for IAM Role Policy Attachment %s", name)
-	}
-
-	err := attachPoliciesToRole(conn, role, arns)
+	err := attachPolicyToRole(conn, role, arn)
 	if err != nil {
-		return fmt.Errorf("[WARN] Error attaching policy with IAM Role Policy Attachment %s: %v", name, err)
+		return fmt.Errorf("[WARN] Error attaching policy %s to IAM Role %s: %v", arn, role, err)
 	}
 
-	d.SetId(d.Get("name").(string))
+	d.SetId(getRolePolicyAttachmentId(role, arn))
 	return resourceAwsIamRolePolicyAttachmentRead(d, meta)
 }
 
 func resourceAwsIamRolePolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
-	role := d.Get("role").(string)
+	role, arn := getRoleAndPolicyFromAttachment(d.Id())
 
 	_, err := conn.GetRole(&iam.GetRoleInput{
 		RoleName: aws.String(role),
@@ -69,7 +67,7 @@ func resourceAwsIamRolePolicyAttachmentRead(d *schema.ResourceData, meta interfa
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			if awsErr.Code() == "NoSuchEntity" {
-				log.Printf("[WARN] No such entity found for Policy Attachment (%s)", d.Id())
+				log.Printf("[WARN] No such entity found for Policy Attachment (%s)", role)
 				d.SetId("")
 				return nil
 			}
@@ -84,91 +82,50 @@ func resourceAwsIamRolePolicyAttachmentRead(d *schema.ResourceData, meta interfa
 		return err
 	}
 
-	policies := make([]string, 0, len(attachedPolicies.AttachedPolicies))
-
+	var policy string
 	for _, p := range attachedPolicies.AttachedPolicies {
-		policies = append(policies, *p.PolicyArn)
+		if *p.PolicyArn == arn {
+			policy = *p.PolicyArn
+		}
 	}
 
-	err = d.Set("policy_arns", policies)
-	if err != nil {
-		return fmt.Errorf("[WARN} Error setting policy list from IAM Role Policy Attachment %s: %v", name, err)
+	if policy == "" {
+		d.SetId("")
 	}
 
 	return nil
-}
-
-func resourceAwsIamRolePolicyAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
-
-	if d.HasChange("policy_arns") {
-		err := updateRolePolicies(conn, d, meta)
-		if err != nil {
-			return fmt.Errorf("[WARN] Error updating policy list from IAM Role Policy Attachment %s: %v", name, err)
-		}
-	}
-	return resourceAwsIamRolePolicyAttachmentRead(d, meta)
 }
 
 func resourceAwsIamRolePolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
 	role := d.Get("role").(string)
-	arns := expandStringList(d.Get("policy_arns").(*schema.Set).List())
+	arn := d.Get("policy_arn").(string)
 
-	err := detachPoliciesFromRole(conn, role, arns)
+	err := detachPolicyFromRole(conn, role, arn)
 	if err != nil {
-		return fmt.Errorf("[WARN] Error removing policies from role IAM Role Policy Detach %s: %v", name, err)
+		return fmt.Errorf("[WARN] Error removing policy %s from IAM Role %s: %v", arn, role, err)
 	}
 	return nil
 }
 
-func attachPoliciesToRole(conn *iam.IAM, role string, arns []*string) error {
-	for _, a := range arns {
-		_, err := conn.AttachRolePolicy(&iam.AttachRolePolicyInput{
-			RoleName:  aws.String(role),
-			PolicyArn: a,
-		})
-		if err != nil {
-			return err
-		}
+func attachPolicyToRole(conn *iam.IAM, role string, arn string) error {
+	_, err := conn.AttachRolePolicy(&iam.AttachRolePolicyInput{
+		RoleName:  aws.String(role),
+		PolicyArn: aws.String(arn),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-func updateRolePolicies(conn *iam.IAM, d *schema.ResourceData, meta interface{}) error {
-	role := d.Get("role").(string)
-	o, n := d.GetChange("policy_arns")
-	if o == nil {
-		o = new(schema.Set)
-	}
-	if n == nil {
-		n = new(schema.Set)
-	}
-	os := o.(*schema.Set)
-	ns := n.(*schema.Set)
-	remove := expandStringList(os.Difference(ns).List())
-	add := expandStringList(ns.Difference(os).List())
-
-	if rErr := detachPoliciesFromRole(conn, role, remove); rErr != nil {
-		return rErr
-	}
-	if aErr := attachPoliciesToRole(conn, role, add); aErr != nil {
-		return aErr
-	}
-	return nil
-}
-
-func detachPoliciesFromRole(conn *iam.IAM, role string, arns []*string) error {
-	for _, a := range arns {
-		_, err := conn.DetachRolePolicy(&iam.DetachRolePolicyInput{
-			RoleName:  aws.String(role),
-			PolicyArn: a,
-		})
-		if err != nil {
-			return err
-		}
+func detachPolicyFromRole(conn *iam.IAM, role string, arn string) error {
+	_, err := conn.DetachRolePolicy(&iam.DetachRolePolicyInput{
+		RoleName:  aws.String(role),
+		PolicyArn: aws.String(arn),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
--- builtin/providers/aws/resource_aws_iam_user_policy_attachment.go
+++ builtin/providers/aws/resource_aws_iam_user_policy_attachment.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -14,53 +15,50 @@ func resourceAwsIamUserPolicyAttachment() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAwsIamUserPolicyAttachmentCreate,
 		Read:   resourceAwsIamUserPolicyAttachmentRead,
-		Update: resourceAwsIamUserPolicyAttachmentUpdate,
 		Delete: resourceAwsIamUserPolicyAttachmentDelete,
 
 		Schema: map[string]*schema.Schema{
-			"name": &schema.Schema{
+			"user": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"policy_arn": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-			},
-			"user": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"policy_arns": &schema.Schema{
-				Type:     schema.TypeSet,
-				Required: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Set:      schema.HashString,
 			},
 		},
 	}
 }
 
+func getUserPolicyAttachmentId(user, policyArn string) string {
+	return fmt.Sprintf("%s:%s", user, policyArn)
+}
+
+func getUserAndPolicyFromAttachment(id string) (string, string) {
+	tokens := strings.Split(id, ":")
+	return tokens[0], tokens[1]
+}
+
 func resourceAwsIamUserPolicyAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
 
-	name := d.Get("name").(string)
 	user := d.Get("user").(string)
-	arns := expandStringList(d.Get("policy_arns").(*schema.Set).List())
+	arn := d.Get("policy_arn").(string)
 
-	if len(arns) == 0 {
-		return fmt.Errorf("[WARN] No Policies specified for IAM User Policy Attachment %s", name)
-	}
-
-	err := attachPoliciesToUser(conn, user, arns)
+	err := attachPolicyToUser(conn, user, arn)
 	if err != nil {
-		return fmt.Errorf("[WARN] Error attaching policy with IAM User Policy Attachment %s: %v", name, err)
+		return fmt.Errorf("[WARN] Error attaching policy %s to IAM User %s: %v", arn, user, err)
 	}
 
-	d.SetId(d.Get("name").(string))
+	d.SetId(getUserPolicyAttachmentId(user, arn))
 	return resourceAwsIamUserPolicyAttachmentRead(d, meta)
 }
 
 func resourceAwsIamUserPolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
-	user := d.Get("user").(string)
+	user, arn := getUserAndPolicyFromAttachment(d.Id())
 
 	_, err := conn.GetUser(&iam.GetUserInput{
 		UserName: aws.String(user),
@@ -69,7 +67,7 @@ func resourceAwsIamUserPolicyAttachmentRead(d *schema.ResourceData, meta interfa
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			if awsErr.Code() == "NoSuchEntity" {
-				log.Printf("[WARN] No such entity found for Policy Attachment (%s)", d.Id())
+				log.Printf("[WARN] No such entity found for Policy Attachment (%s)", user)
 				d.SetId("")
 				return nil
 			}
@@ -84,91 +82,49 @@ func resourceAwsIamUserPolicyAttachmentRead(d *schema.ResourceData, meta interfa
 		return err
 	}
 
-	policies := make([]string, 0, len(attachedPolicies.AttachedPolicies))
-
+	var policy string
 	for _, p := range attachedPolicies.AttachedPolicies {
-		policies = append(policies, *p.PolicyArn)
-	}
-
-	err = d.Set("policy_arns", policies)
-	if err != nil {
-		return fmt.Errorf("[WARN} Error setting policy list from IAM User Policy Attachment %s: %v", name, err)
-	}
-
-	return nil
-}
-
-func resourceAwsIamUserPolicyAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
-
-	if d.HasChange("policy_arns") {
-		err := updateUserPolicies(conn, d, meta)
-		if err != nil {
-			return fmt.Errorf("[WARN] Error updating policy list from IAM User Policy Attachment %s: %v", name, err)
+		if *p.PolicyArn == arn {
+			policy = *p.PolicyArn
 		}
 	}
-	return resourceAwsIamUserPolicyAttachmentRead(d, meta)
+
+	if policy == "" {
+		d.SetId("")
+	}
+	return nil
 }
 
 func resourceAwsIamUserPolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).iamconn
-	name := d.Get("name").(string)
 	user := d.Get("user").(string)
-	arns := expandStringList(d.Get("policy_arns").(*schema.Set).List())
+	arn := d.Get("policy_arn").(string)
 
-	err := detachPoliciesFromUser(conn, user, arns)
+	err := detachPolicyFromUser(conn, user, arn)
 	if err != nil {
-		return fmt.Errorf("[WARN] Error removing policies from user IAM User Policy Detach %s: %v", name, err)
+		return fmt.Errorf("[WARN] Error removing policy %s from IAM User %s: %v", arn, user, err)
 	}
 	return nil
 }
 
-func attachPoliciesToUser(conn *iam.IAM, user string, arns []*string) error {
-	for _, a := range arns {
-		_, err := conn.AttachUserPolicy(&iam.AttachUserPolicyInput{
-			UserName:  aws.String(user),
-			PolicyArn: a,
-		})
-		if err != nil {
-			return err
-		}
+func attachPolicyToUser(conn *iam.IAM, user string, arn string) error {
+	_, err := conn.AttachUserPolicy(&iam.AttachUserPolicyInput{
+		UserName:  aws.String(user),
+		PolicyArn: aws.String(arn),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-func updateUserPolicies(conn *iam.IAM, d *schema.ResourceData, meta interface{}) error {
-	user := d.Get("user").(string)
-	o, n := d.GetChange("policy_arns")
-	if o == nil {
-		o = new(schema.Set)
-	}
-	if n == nil {
-		n = new(schema.Set)
-	}
-	os := o.(*schema.Set)
-	ns := n.(*schema.Set)
-	remove := expandStringList(os.Difference(ns).List())
-	add := expandStringList(ns.Difference(os).List())
-
-	if rErr := detachPoliciesFromUser(conn, user, remove); rErr != nil {
-		return rErr
-	}
-	if aErr := attachPoliciesToUser(conn, user, add); aErr != nil {
-		return aErr
-	}
-	return nil
-}
-
-func detachPoliciesFromUser(conn *iam.IAM, user string, arns []*string) error {
-	for _, a := range arns {
-		_, err := conn.DetachUserPolicy(&iam.DetachUserPolicyInput{
-			UserName:  aws.String(user),
-			PolicyArn: a,
-		})
-		if err != nil {
-			return err
-		}
+func detachPolicyFromUser(conn *iam.IAM, user string, arn string) error {
+	_, err := conn.DetachUserPolicy(&iam.DetachUserPolicyInput{
+		UserName:  aws.String(user),
+		PolicyArn: aws.String(arn),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
