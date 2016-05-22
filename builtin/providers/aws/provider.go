7524f06abcc0c34c2c06359ccdddfbbaa779cc9f
Martin Atkins <mart@degeneration.co.uk>
2016-05-22 10:22:09 -0700

    provider/aws: aws_region data source
    The primary purpose of this data source is to ask the question "what is
    my current region?", but it can also be used to retrieve the endpoint
    hostname for a particular (possibly non-current) region, should that be
    useful for some esoteric case.

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -153,6 +153,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_iam_policy_document":      dataSourceAwsIamPolicyDocument(),
 			"aws_ip_ranges":                dataSourceAwsIPRanges(),
 			"aws_redshift_service_account": dataSourceAwsRedshiftServiceAccount(),
+			"aws_region":                   dataSourceAwsRegion(),
 			"aws_s3_bucket_object":         dataSourceAwsS3BucketObject(),
 			"aws_subnet":                   dataSourceAwsSubnet(),
 			"aws_vpc":                      dataSourceAwsVpc(),
