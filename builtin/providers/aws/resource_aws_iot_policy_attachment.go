fbab5876d586b02daa0499e8292b4aa1d5fac822
Joel Hermanns <joel.hermanns@gmail.com>
2016-05-28 18:13:43 +0200

    Add some implementation for IoT policy attachment

--- builtin/providers/aws/resource_aws_iot_policy_attachment.go
+++ builtin/providers/aws/resource_aws_iot_policy_attachment.go
@@ -1,6 +1,10 @@
 package aws
 
 import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iot"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -18,23 +22,73 @@ func resourceAwsIotPolicyAttachment() *schema.Resource {
 			"policies": &schema.Schema{
 				Type:     schema.TypeList,
 				Required: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 		},
 	}
 }
 
 func resourceAwsIotPolicyAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	for _, p := range d.Get("policies").([]string) {
+		_, err := conn.AttachPrincipalPolicy(&iot.AttachPrincipalPolicyInput{
+			Principal:  aws.String(d.Get("principal").(string)),
+			PolicyName: aws.String(p),
+		})
+
+		if err != nil {
+			log.Printf("[ERROR] %s", err)
+			return err
+		}
+	}
+
 	return nil
 }
 
 func resourceAwsIotPolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	out, err := conn.ListPrincipalPolicies(&iot.ListPrincipalPoliciesInput{
+		Principal: aws.String(d.Get("principal").(string)),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
+	length := len(out.Policies)
+	policies := make([]string, length)
+	for i, p := range out.Policies {
+		policies[i] = *p.PolicyName
+	}
+
+	d.Set("policies", policies)
+
 	return nil
 }
 
 func resourceAwsIotPolicyAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
+	//TODO: implement
 	return nil
 }
 
 func resourceAwsIotPolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	for _, p := range d.Get("policies").([]string) {
+		_, err := conn.DetachPrincipalPolicy(&iot.DetachPrincipalPolicyInput{
+			Principal:  aws.String(d.Get("principal").(string)),
+			PolicyName: aws.String(p),
+		})
+
+		if err != nil {
+			log.Printf("[ERROR] %s", err)
+			return err
+		}
+	}
 	return nil
 }
