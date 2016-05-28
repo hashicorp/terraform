d49b60b5345f372d52af4d0e8a37891b0b1626bb
Joel Hermanns <joel.hermanns@gmail.com>
2016-05-28 17:12:24 +0200

    Implement IoT certificate functionality

--- builtin/providers/aws/resource_aws_iot_certificate.go
+++ builtin/providers/aws/resource_aws_iot_certificate.go
@@ -1,6 +1,10 @@
 package aws
 
 import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iot"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -24,17 +28,95 @@ func resourceAwsIotCertificate() *schema.Resource {
 }
 
 func resourceAwsIotCertificateCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	log.Printf("[DEBUG] Creating certificate from csr")
+	out, err := conn.CreateCertificateFromCsr(&iot.CreateCertificateFromCsrInput{
+		CertificateSigningRequest: aws.String(d.Get("csr").(string)),
+		SetAsActive:               aws.Bool(d.Get("active").(bool)),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+	log.Printf("[DEBUG] Created certificate from csr")
+
+	d.SetId(*out.CertificateId)
+	d.Set("arn", *out.CertificateArn)
+	d.Set("pem", *out.CertificatePem)
+
 	return nil
 }
 
 func resourceAwsIotCertificateRead(d *schema.ResourceData, meta interface{}) error {
+
+	conn := meta.(*AWSClient).iotconn
+
+	out, err := conn.DescribeCertificate(&iot.DescribeCertificateInput{
+		CertificateId: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
+	d.SetId(*out.CertificateDescription.CertificateId)
+	d.Set("arn", *out.CertificateDescription.CertificateArn)
+	d.Set("pem", *out.CertificateDescription.CertificatePem)
+
 	return nil
 }
 
 func resourceAwsIotCertificateUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	if d.HasChange("csr") {
+		//TODO: Create new certificate
+	} else {
+
+		// What about a status like REVOKED etc. ??
+		status := "INACTIVE"
+		if d.Get("active").(bool) {
+			status = "ACTIVE"
+		}
+
+		_, err := conn.UpdateCertificate(&iot.UpdateCertificateInput{
+			CertificateId: aws.String(d.Id()),
+			NewStatus:     aws.String(status),
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 func resourceAwsIotCertificateDelete(d *schema.ResourceData, meta interface{}) error {
+
+	conn := meta.(*AWSClient).iotconn
+
+	_, err := conn.UpdateCertificate(&iot.UpdateCertificateInput{
+		CertificateId: aws.String(d.Id()),
+		NewStatus:     aws.String("INACTIVE"),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR], %s", err)
+		return err
+	}
+
+	_, err = conn.DeleteCertificate(&iot.DeleteCertificateInput{
+		CertificateId: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
 	return nil
 }
