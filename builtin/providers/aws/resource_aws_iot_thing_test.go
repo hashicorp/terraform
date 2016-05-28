5320b3b0cbb44fbd8306920509c3dc4384344c6c
Joel Hermanns <joel.hermanns@gmail.com>
2016-05-28 18:20:34 +0200

    Add tests for AWS IoT thing

---
+++ builtin/providers/aws/resource_aws_iot_thing_test.go
@@ -0,0 +1,130 @@
+package aws
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/iot"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAWSIoTThing_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSIoTThingDestroy_basic,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSIoTThing_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSIotThingExists_basic("aws_iot_thing.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSIoTThingDestroy_basic(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).iotconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_iot_thing" {
+			continue
+		}
+
+		out, err := conn.ListThings(&iot.ListThingsInput{})
+
+		if err != nil {
+			return err
+		}
+
+		for _, t := range out.Things {
+			if *t.ThingName == rs.Primary.ID {
+				return fmt.Errorf("IoT thing still exists:\n%s", t)
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckAWSIotThingExists_basic(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		return nil
+	}
+}
+
+func TestAccAWSIoTThing_attributes(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSIoTThingDestroy_attributes,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSIoTThing_attributes,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSIotThingExists_attributes("aws_iot_thing.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSIoTThingDestroy_attributes(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).iotconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_iot_thing" {
+			continue
+		}
+
+		out, err := conn.ListThings(&iot.ListThingsInput{})
+
+		if err != nil {
+			return err
+		}
+
+		for _, t := range out.Things {
+			if *t.ThingName == rs.Primary.ID {
+				return fmt.Errorf("IoT thing still exists:\n%s", t)
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckAWSIotThingExists_attributes(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		return nil
+	}
+}
+
+var testAccAWSIoTThing_basic = `
+resource "aws_iot_thing" "foo" {
+	name = "foo-thing"
+}
+`
+var testAccAWSIoTThing_attributes = `
+resource "aws_iot_thing" "foo" {
+	name = "foo-thing"
+
+  attributes {
+    key1 = "val1"
+    key2 = "val2"
+ }
+}
+`
