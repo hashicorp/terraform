9bcc40e326869982902bf53d7d648fa75112d1ff
Joel Hermanns <joel.hermanns@gmail.com>
2016-05-28 17:46:09 +0200

    Implement IoT policy functionality

--- builtin/providers/aws/resource_aws_iot_policy.go
+++ builtin/providers/aws/resource_aws_iot_policy.go
@@ -1,6 +1,10 @@
 package aws
 
 import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iot"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -24,17 +28,104 @@ func resourceAwsIotPolicy() *schema.Resource {
 }
 
 func resourceAwsIotPolicyCreate(d *schema.ResourceData, meta interface{}) error {
-	return nil
-}
 
-func resourceAwsIotPolicyUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	_, err := conn.CreatePolicy(&iot.CreatePolicyInput{
+		PolicyName:     aws.String(d.Get("name")),
+		PolicyDocument: aws.String(d.Get("policy")),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
+	d.SetId(*out.PolicyName)
+	d.Set("arn", *out.PolicyArn)
+	d.Set("defaultVersionId", *out.PolicyVersionId)
+
 	return nil
 }
 
 func resourceAwsIotPolicyRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	out, err := conn.GetPolicy(&iot.GetPolicyInput{
+		PolicyName: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
+	d.SetId(*out.PolicyName)
+	d.Set("arn", *out.PolicyArn)
+	d.Set("defaultVersionId", *out.DefaultVersionId)
+
+	return nil
+}
+
+func resourceAwsIotPolicyUpdate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).iotconn
+
+	//TODO: prune old versions
+
+	if d.HasChange("policy") {
+		out, err := conn.CreatePolicyVersion(&iot.CreatePolicyVersionInput{
+			PolicyName:     aws.String(d.Id()),
+			PolicyDocument: aws.String(d.Get("policy")),
+			SetAsDefault:   true,
+		})
+
+		if err != nil {
+			log.Printf("[ERROR] %s", err)
+			return err
+		}
+
+		d.Set("arn", *out.PolicyArn)
+		d.Set("defaultVersionId", *out.PolicyVersionId)
+	}
+
 	return nil
 }
 
 func resourceAwsIotPolicyDelete(d *schema.ResourceData, meta interface{}) error {
+
+	conn := meta.(*AWSClient).iotconn
+
+	out, err := conn.ListPolicyVersions(&iot.ListPolicyVersionsInput{
+		PolicyName: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// Delete all non-default versions of the policy
+	for _, ver := range out.PolicyVersions {
+		if !ver.IsDefaultVersion {
+			_, err = conn.DeletePolicyVersion(&iot.DeletePolicyVersionInput{
+				PolicyName:      aws.String(d.Id()),
+				PolicyVersionId: ver.VersionId,
+			})
+			if err != nil {
+				log.Printf("[ERROR] %s", err)
+				return err
+			}
+		}
+	}
+
+	//Delete default policy version
+	_, err = conn.DeletePolicy(&iot.DeletePolicyInput{
+		PolicyName: aws.String(d.Id()),
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return err
+	}
+
 	return nil
 }
