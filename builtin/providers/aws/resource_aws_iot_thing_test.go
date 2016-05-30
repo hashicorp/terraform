902637a42e8a3897c041ceed76a4daf5d72f018f
Joel Hermanns <joel.hermanns@gmail.com>
2016-05-30 12:09:28 +0200

    More tests for aws iot thing

--- builtin/providers/aws/resource_aws_iot_thing_test.go
+++ builtin/providers/aws/resource_aws_iot_thing_test.go
@@ -113,6 +113,58 @@ func testAccCheckAWSIotThingExists_attributes(name string) resource.TestCheckFun
 	}
 }
 
+func TestAccAWSIoTThing_principal(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSIoTThingDestroy_principal,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSIoTThing_principal,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSIotThingExists_principal("aws_iot_thing.device3"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAWSIoTThingDestroy_principal(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*AWSClient).iotconn
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_iot_thing" {
+			continue
+		}
+
+		out, err := conn.ListThings(&iot.ListThingsInput{})
+
+		if err != nil {
+			return err
+		}
+
+		for _, t := range out.Things {
+			if *t.ThingName == rs.Primary.ID {
+				return fmt.Errorf("IoT thing still exists:\n%s", t)
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckAWSIotThingExists_principal(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		return nil
+	}
+}
+
 var testAccAWSIoTThing_basic = `
 resource "aws_iot_thing" "foo" {
 	name = "foo-thing"
@@ -126,5 +178,23 @@ resource "aws_iot_thing" "foo" {
     key1 = "val1"
     key2 = "val2"
  }
+}
+`
+
+var testAccAWSIoTThing_principal = `
+resource "aws_iot_thing" "device3" {
+  name = "MyDevice3"
+  principals = ["${aws_iot_certificate.cert.arn}"]
+
+  attributes {
+    Manufacturer = "Amazon"
+    Type = "IoT Device A"
+    SerialNumber = "10293847562912"
+  }
+}
+
+resource "aws_iot_certificate" "cert" {
+  csr = "${file("./csr.pem")}"
+  active = true
 }
 `
