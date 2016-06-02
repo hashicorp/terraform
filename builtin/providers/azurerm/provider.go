6bd46387b3dd371dbfb5dcfa6e9ae6a2bd4075f5
James Nugent <james@jen20.com>
2016-06-01 19:51:57 -0500

    provider/azurerm: Fix azurerm_virtual_machine

--- builtin/providers/azurerm/provider.go
+++ builtin/providers/azurerm/provider.go
@@ -61,8 +61,8 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_storage_queue":          resourceArmStorageQueue(),
 			"azurerm_subnet":                 resourceArmSubnet(),
 			"azurerm_template_deployment":    resourceArmTemplateDeployment(),
-			//"azurerm_virtual_machine":        resourceArmVirtualMachine(),
-			"azurerm_virtual_network": resourceArmVirtualNetwork(),
+			"azurerm_virtual_machine":        resourceArmVirtualMachine(),
+			"azurerm_virtual_network":        resourceArmVirtualNetwork(),
 
 			// These resources use the Riviera SDK
 			"azurerm_dns_a_record":      resourceArmDnsARecord(),
