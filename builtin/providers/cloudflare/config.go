15dec5b646ef949a400c82bd10e2dced7919cb0c
clint shryock <clint@ctshryock.com>
2016-06-06 14:57:26 -0500

    provider/cloudfront: Minor adjustments for new cloudflare-go lib

--- builtin/providers/cloudflare/config.go
+++ builtin/providers/cloudflare/config.go
@@ -1,6 +1,7 @@
 package cloudflare
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/cloudflare/cloudflare-go"
@@ -13,7 +14,10 @@ type Config struct {
 
 // Client() returns a new client for accessing cloudflare.
 func (c *Config) Client() (*cloudflare.API, error) {
-	client := cloudflare.New(c.Token, c.Email)
+	client, err := cloudflare.New(c.Token, c.Email)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating new CloudFlare client: %s", err)
+	}
 	log.Printf("[INFO] CloudFlare Client configured for user: %s", c.Email)
 	return client, nil
 }
