0f92161f82b5df0bd193c6ce0a94c2fe3eec4f52
Paul Hinze <phinze@phinze.com>
2016-06-12 19:21:46 +0000

    release: clean up after v0.7.0-rc2

--- terraform/version.go
+++ terraform/version.go
@@ -10,7 +10,7 @@ const Version = "0.7.0"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = "rc2"
+const VersionPrerelease = "dev"
 
 // SemVersion is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
