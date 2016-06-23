b2f0b2c9124de89474296c23bdbbd039c14f0ce1
stack72 <public@paulstack.co.uk>
2016-06-23 10:18:24 +0100

    provider/aws: Support Import for `aws_cloudtrail`
    ```
    make testacc TEST=./builtin/providers/aws
    TESTARGS='-run=TestAccAWSCloudTrail_'
    ==> Checking that code complies with gofmt requirements...
    go generate $(go list ./... | grep -v /vendor/)
    TF_ACC=1 go test ./builtin/providers/aws -v -run=TestAccAWSCloudTrail_
    -timeout 120m
    === RUN   TestAccAWSCloudTrail_importBasic
    --- PASS: TestAccAWSCloudTrail_importBasic (64.64s)
    === RUN   TestAccAWSCloudTrail_basic
    --- PASS: TestAccAWSCloudTrail_basic (97.96s)
    === RUN   TestAccAWSCloudTrail_enable_logging
    --- PASS: TestAccAWSCloudTrail_enable_logging (137.07s)
    === RUN   TestAccAWSCloudTrail_is_multi_region
    --- PASS: TestAccAWSCloudTrail_is_multi_region (138.51s)
    === RUN   TestAccAWSCloudTrail_logValidation
    --- PASS: TestAccAWSCloudTrail_logValidation (102.61s)
    === RUN   TestAccAWSCloudTrail_tags
    --- PASS: TestAccAWSCloudTrail_tags (135.66s)
    PASS
    ok      github.com/hashicorp/terraform/builtin/providers/aws    676.484s
    ```

---
+++ builtin/providers/aws/import_aws_cloudtrail_test.go
@@ -0,0 +1,31 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAWSCloudTrail_importBasic(t *testing.T) {
+	resourceName := "aws_cloudtrail.foobar"
+	cloudTrailRandInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSCloudTrailDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSCloudTrailConfig(cloudTrailRandInt),
+			},
+
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"enable_log_file_validation", "is_multi_region_trail", "include_global_service_events", "enable_logging"},
+			},
+		},
+	})
+}
