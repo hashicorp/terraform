18ca06cae9b42770d01489789752ad49f4ec098f
Joe Topjian <joe@topjian.net>
2016-07-03 09:37:35 -0600

    provider/openstack: Support Import of OpenStack FWaaS Resources (#7471)

---
+++ builtin/providers/openstack/import_openstack_fw_firewall_v1_test.go
@@ -0,0 +1,29 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccOpenStackFWFirewallV1_importBasic(t *testing.T) {
+	resourceName := "openstack_fw_firewall_v1.accept_test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckFWFirewallV1Destroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testFirewallConfig,
+			},
+
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region"},
+			},
+		},
+	})
+}
