44ac0ce032a671d9fd17abda1e9283a0fbb0831f
Joe Topjian <joe@topjian.net>
2016-07-03 09:37:58 -0600

    provider/openstack: Support Import openstack_compute_secgroup_v2 (#7350)

---
+++ builtin/providers/openstack/import_openstack_compute_secgroup_v2_test.go
@@ -0,0 +1,29 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccComputeV2SecGroup_importBasic(t *testing.T) {
+	resourceName := "openstack_compute_secgroup_v2.foo"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckComputeV2SecGroupDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccComputeV2SecGroup_basic_orig,
+			},
+
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region"},
+			},
+		},
+	})
+}
