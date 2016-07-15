f7da2612946fed4a8a593d1bafc368a6c0d87a2f
Joe Topjian <joe@topjian.net>
2016-07-15 02:49:31 -0600

    provider/openstack: Support Import of OpenStack LBaaS V1 Resources (#7660)

---
+++ builtin/providers/openstack/import_openstack_lb_vip_v1_test.go
@@ -0,0 +1,29 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccOpenStackLBVIPV1_importBasic(t *testing.T) {
+	resourceName := "openstack_lb_vip_v1.vip_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV1VIPDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccLBV1VIP_basic,
+			},
+
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region"},
+			},
+		},
+	})
+}
