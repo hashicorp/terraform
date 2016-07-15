2dfdf4ff5c489f8af3bf873df744ab4879c44554
Joe Topjian <joe@topjian.net>
2016-07-15 05:17:21 +0000

    provider/openstack: Support Import of OpenStack Networking Resources
    Router-based resources are not included. They will be added later.

---
+++ builtin/providers/openstack/import_openstack_networking_floatingip_v2_test.go
@@ -0,0 +1,29 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccOpenStackNetworkingFloatingIPV2_importBasic(t *testing.T) {
+	resourceName := "openstack_networking_floatingip_v2.foo"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNetworkingV2FloatingIPDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNetworkingV2FloatingIP_basic,
+			},
+
+			resource.TestStep{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region"},
+			},
+		},
+	})
+}
