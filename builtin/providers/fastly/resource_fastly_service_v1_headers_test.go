b0022f57de5ab7f324150ac1dc88355c722917f7
clint shryock <clint@ctshryock.com>
2016-07-21 10:48:46 -0500

    update test to check regression

--- builtin/providers/fastly/resource_fastly_service_v1_headers_test.go
+++ builtin/providers/fastly/resource_fastly_service_v1_headers_test.go
@@ -181,10 +181,11 @@ resource "fastly_service_v1" "foo" {
   }
 
   header {
-    destination = "http.Server"
-    type        = "cache"
-    action      = "delete"
-    name        = "remove s3 server"
+    destination   = "http.Server"
+    type          = "cache"
+    action        = "delete"
+    name          = "remove s3 server"
+    ignore_if_set = "true"
   }
 
   force_destroy = true
