0e908be07e243cbd49475a4ec72385c13cfd44bf
clint shryock <clint@ctshryock.com>
2016-07-22 16:15:07 -0500

    provider/aws: Fix issue updating ElasticBeanstalk Environment Settings
    Fixes the logic that updated settings for Elastic Beanstalk Environments.
    Because the update is done in the same API call, we need to split removals /
    additions.
    
    Fixes #6890

--- builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
+++ builtin/providers/aws/resource_aws_elastic_beanstalk_environment.go
@@ -97,7 +97,6 @@ func resourceAwsElasticBeanstalkEnvironment() *schema.Resource {
 			"setting": &schema.Schema{
 				Type:     schema.TypeSet,
 				Optional: true,
-				Computed: true,
 				Elem:     resourceAwsElasticBeanstalkOptionSetting(),
 				Set:      optionSettingValueHash,
 			},
@@ -329,7 +328,39 @@ func resourceAwsElasticBeanstalkEnvironmentUpdate(d *schema.ResourceData, meta i
 		os := o.(*schema.Set)
 		ns := n.(*schema.Set)
 
-		updateOpts.OptionSettings = extractOptionSettings(ns.Difference(os))
+                rm := extractOptionSettings(os.Difference(ns))
+                add := extractOptionSettings(ns.Difference(os))
+
+                // Additions and removals of options are done in a single API call, so we
+                // can't do our normal "remove these" and then later "add these", re-adding
+                // any updated settings.
+                // Because of this, we need to remove any settings in the "removable"
+                // settings that are also found in the "add" settings, otherwise they
+                // conflict. Here we loop through all the initial removables from the set
+                // difference, and we build up a slice of settings not found in the "add"
+                // set
+                var remove []*elasticbeanstalk.ConfigurationOptionSetting
+                if len(add) > 0 {
+                        for _, r := range rm {
+                                for _, a := range add {
+                                        if *r.Namespace == *a.Namespace && *r.OptionName == *a.OptionName {
+                                                continue
+                                        }
+                                        remove = append(remove, r)
+                                }
+                        }
+                } else {
+                        remove = rm
+                }
+
+                for _, elem := range remove {
+                        updateOpts.OptionsToRemove = append(updateOpts.OptionsToRemove, &elasticbeanstalk.OptionSpecification{
+                                Namespace:  elem.Namespace,
+                                OptionName: elem.OptionName,
+                        })
+                }
+
+                updateOpts.OptionSettings = add
 	}
 
 	if d.HasChange("template_name") {
