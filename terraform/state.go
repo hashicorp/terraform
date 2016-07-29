be46904cb3deaf93ce00f5263e3f77cb9b3bab38
James Bardin <j.bardin@gmail.com>
2016-07-29 13:17:48 -0400

    Make all terraform package tests pass under -race
    This isn't a pretty refactor, but fixes the race issues in this package
    for now.
    
    Fix race on RawConfig.Config()
    
    fix command package races

--- terraform/state.go
+++ terraform/state.go
@@ -1331,7 +1331,7 @@ func (s *InstanceState) MergeDiff(d *InstanceDiff) *InstanceState {
 		}
 	}
 	if d != nil {
-		for k, diff := range d.Attributes {
+		for k, diff := range d.CopyAttributes() {
 			if diff.NewRemoved {
 				delete(result.Attributes, k)
 				continue
