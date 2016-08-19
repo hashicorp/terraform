01cfeb806213d8d4e0530687990dd2e6735a4c03
AMeng <alexbmeng@gmail.com>
2016-08-19 12:32:38 -0600

    provider/aws: Set AWS region in elasticache cluster import test

--- builtin/providers/aws/import_aws_elasticache_cluster_test.go
+++ builtin/providers/aws/import_aws_elasticache_cluster_test.go
@@ -1,12 +1,17 @@
 package aws
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccAWSElasticacheCluster_importBasic(t *testing.T) {
+	oldvar := os.Getenv("AWS_DEFAULT_REGION")
+	os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
+	defer os.Setenv("AWS_DEFAULT_REGION", oldvar)
+
 	resourceName := "aws_elasticache_cluster.bar"
 
 	resource.Test(t, resource.TestCase{
