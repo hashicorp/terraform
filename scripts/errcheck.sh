7e7872e036df5c0b48c752a6e2ef651cb1239cc4
clint shryock <clint@ctshryock.com>
2016-08-30 16:47:30 -0500

    provider/aws: Add errcheck to Makefile, error on unchecked errors

---
+++ scripts/errcheck.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check gofmt
+echo "==> Checking AWS provider for unchecked errors..."
+echo "==> NOTE: at this time we only look for uncheck errors in the AWS package"
+
+if ! which errcheck > /dev/null; then
+    echo "==> Installing errcheck..."
+    go get -u github.com/kisielk/errcheck
+fi
+
+err_files=$(errcheck -ignore 'github.com/hashicorp/terraform/helper/schema:Set,bytes:.*' ./builtin/providers/aws/...)
+if [[ -n ${err_files} ]]; then
+    echo 'Unchecked errors found in the following places:'
+    echo "${err_files}"
+    echo "Please handle returned errors. You can check directly with \`make errcheck\`"
+    exit 1
+fi
+
+exit 0
