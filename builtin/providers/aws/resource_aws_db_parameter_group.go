3c71783d0764ba8af6b67eed35684e5708796d3f
stack72 <public@paulstack.co.uk>
2016-09-01 13:08:30 +0100

    provider/aws: Set `apply_method` to state in `aws_db_parameter_group`
    Fixes #8593
    
    ```
    % make testacc TEST=./builtin/providers/aws TESTARGS='-run=TestAccAWSDBParameterGroup_'
    ==> Checking that code complies with gofmt requirements...
    go generate $(go list ./... | grep -v /terraform/vendor/)
    2016/09/01 13:04:22 Generated command/internal_plugin_list.go
    TF_ACC=1 go test ./builtin/providers/aws -v
    -run=TestAccAWSDBParameterGroup_ -timeout 120m
    === RUN   TestAccAWSDBParameterGroup_importBasic
    --- PASS: TestAccAWSDBParameterGroup_importBasic (27.03s)
    === RUN   TestAccAWSDBParameterGroup_limit
    --- PASS: TestAccAWSDBParameterGroup_limit (48.54s)
    === RUN   TestAccAWSDBParameterGroup_basic
    --- PASS: TestAccAWSDBParameterGroup_basic (46.29s)
    === RUN   TestAccAWSDBParameterGroup_Only
    --- PASS: TestAccAWSDBParameterGroup_Only (23.57s)
    PASS
    ok      github.com/hashicorp/terraform/builtin/providers/aws    145.445s
    ```

--- builtin/providers/aws/resource_aws_db_parameter_group.go
+++ builtin/providers/aws/resource_aws_db_parameter_group.go
@@ -66,14 +66,6 @@ func resourceAwsDbParameterGroup() *schema.Resource {
 							Type:     schema.TypeString,
 							Optional: true,
 							Default:  "immediate",
-							// this parameter is not actually state, but a
-							// meta-parameter describing how the RDS API call
-							// to modify the parameter group should be made.
-							// Future reads of the resource from AWS don't tell
-							// us what we used for apply_method previously, so
-							// by squashing state to an empty string we avoid
-							// needing to do an update for every future run.
-							StateFunc: func(interface{}) string { return "" },
 						},
 					},
 				},
