c78bb66ef9ae08820a801b3f4d60b7c80ab463fa
Joe Topjian <joe@topjian.net>
2016-09-04 02:51:22 +0000

    provider/openstack: gophercloud migration: util

--- builtin/providers/openstack/util.go
+++ builtin/providers/openstack/util.go
@@ -3,14 +3,14 @@ package openstack
 import (
 	"fmt"
 
+	"github.com/gophercloud/gophercloud"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/rackspace/gophercloud"
 )
 
 // CheckDeleted checks the error to see if it's a 404 (Not Found) and, if so,
 // sets the resource ID to the empty string instead of throwing an error.
 func CheckDeleted(d *schema.ResourceData, err error, msg string) error {
-	errCode, ok := err.(*gophercloud.UnexpectedResponseCodeError)
+	errCode, ok := err.(*gophercloud.ErrUnexpectedResponseCode)
 	if !ok {
 		return fmt.Errorf("%s: %s", msg, err)
 	}
