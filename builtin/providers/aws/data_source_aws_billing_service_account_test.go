29db48e91f18015683f70eab990c7a1e7d656394
Steve Hoeksema <steve@kotiri.com>
2016-09-08 15:51:31 +1200

    Format with tabs, add missing comma

--- builtin/providers/aws/data_source_aws_billing_service_account_test.go
+++ builtin/providers/aws/data_source_aws_billing_service_account_test.go
@@ -1,25 +1,25 @@
 package aws
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccAWSBillingServiceAccount_basic(t *testing.T) {
-  resource.Test(t, resource.TestCase{
-    PreCheck:  func() { testAccPreCheck(t) },
-    Providers: testAccProviders,
-    Steps: []resource.TestStep{
-      resource.TestStep{
-        Config: testAccCheckAwsBillingServiceAccountConfig,
-        Check: resource.ComposeTestCheckFunc(
-          resource.TestCheckResourceAttr("data.aws_billing_service_account.main", "id", "386209384616"),
-          resource.TestCheckResourceAttr("data.aws_billing_service_account.main", "arn", "arn:aws:iam::386209384616:root"),
-        ),
-      }
-    },
-  })
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckAwsBillingServiceAccountConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.aws_billing_service_account.main", "id", "386209384616"),
+					resource.TestCheckResourceAttr("data.aws_billing_service_account.main", "arn", "arn:aws:iam::386209384616:root"),
+				),
+			},
+		},
+	})
 }
 
 const testAccCheckAwsBillingServiceAccountConfig = `
