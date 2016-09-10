43ed1e1ba56aaa71347e31a62d845f8a54f72d34
Martin Atkins <mart@degeneration.co.uk>
2016-09-10 15:28:33 -0700

    Unit test for the "resourceProvider" utility function
    This small function determines the dependable name of a provider for
    a given resource name and optional provider alias. It's simple but it's
    a key part of how resource nodes get connected to provider nodes so
    worth specifying the intended behavior in the form of a test.

--- terraform/util_test.go
+++ terraform/util_test.go
@@ -46,3 +46,51 @@ func TestStrSliceContains(t *testing.T) {
 		t.Fatalf("Bad")
 	}
 }
+
+func TestUtilResourceProvider(t *testing.T) {
+	type testCase struct {
+		ResourceName string
+		Alias        string
+		Expected     string
+	}
+
+	tests := []testCase{
+		{
+			// If no alias is provided, the first underscore-separated segment
+			// is assumed to be the provider name.
+			ResourceName: "aws_thing",
+			Alias:        "",
+			Expected:     "aws",
+		},
+		{
+			// If we have more than one underscore then it's the first one that we'll use.
+			ResourceName: "aws_thingy_thing",
+			Alias:        "",
+			Expected:     "aws",
+		},
+		{
+			// Resource name without enough parts is invalid
+			ResourceName: "aws",
+			Alias:        "",
+			Expected:     "", // intentionally not a valid provider name
+		},
+		{
+			// Alias always overrides the default extraction of the name
+			ResourceName: "aws_thing",
+			Alias:        "tls.baz",
+			Expected:     "tls.baz",
+		},
+	}
+
+	for _, test := range tests {
+		got := resourceProvider(test.ResourceName, test.Alias)
+		if got != test.Expected {
+			t.Errorf(
+				"(%q, %q) produced %q; want %q",
+				test.ResourceName, test.Alias,
+				got,
+				test.Expected,
+			)
+		}
+	}
+}
