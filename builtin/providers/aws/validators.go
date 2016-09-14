e0b51e48e73c94b4a39057eec51574b280361044
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-09-14 08:55:34 +0100

    Remove surplus type assertion.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- builtin/providers/aws/validators.go
+++ builtin/providers/aws/validators.go
@@ -462,8 +462,7 @@ func validateApiGatewayIntegrationPassthroughBehavior(v interface{}, k string) (
 }
 
 func validateJsonString(v interface{}, k string) (ws []string, errors []error) {
-	value := v.(string)
-	if _, err := normalizeJsonString(value); err != nil {
+	if _, err := normalizeJsonString(v); err != nil {
 		errors = append(errors, fmt.Errorf("%q contains an invalid JSON: %s", k, err))
 	}
 	return
