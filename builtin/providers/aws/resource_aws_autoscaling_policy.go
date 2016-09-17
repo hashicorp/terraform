925fc116a814931546fec2ed23d791b4acf6231d
Dennis Webb <dennis@bluesentryit.com>
2016-09-16 21:00:57 -0500

    always set scaling_adjustment when policy_type = SimpleScaling

--- builtin/providers/aws/resource_aws_autoscaling_policy.go
+++ builtin/providers/aws/resource_aws_autoscaling_policy.go
@@ -218,8 +218,11 @@ func getAwsAutoscalingPutScalingPolicyInput(d *schema.ResourceData) (autoscaling
 		params.PolicyType = aws.String(v.(string))
 	}
 
-	if v, ok := d.GetOk("scaling_adjustment"); ok {
-		params.ScalingAdjustment = aws.Int64(int64(v.(int)))
+	//if policy_type=="SimpleScaling" then scaling_adjustment is required and 0 is allowed
+	if *params.PolicyType == "SimpleScaling" {
+		params.ScalingAdjustment = aws.Int64(int64(d.Get("scaling_adjustment").(int)))
+	} else if v, ok := d.GetOk("scaling_adjustment"); ok {
+		params.EstimatedInstanceWarmup = aws.Int64(int64(v.(int)))
 	}
 
 	if v, ok := d.GetOk("step_adjustment"); ok {
