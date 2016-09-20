7b2bd9309450e56db5c0b192b5cfafa82c7e3809
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-09-20 10:16:49 -0700

    terraform: test the destroy edge transform

---
+++ terraform/transform_destroy_edge_test.go
@@ -0,0 +1,44 @@
+package terraform
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestDestroyEdgeTransformer(t *testing.T) {
+	g := Graph{Path: RootModulePath}
+	g.Add(&graphNodeDestroyerTest{AddrString: "test.A"})
+	g.Add(&graphNodeDestroyerTest{AddrString: "test.B"})
+	tf := &DestroyEdgeTransformer{
+		Module: testModule(t, "transform-destroy-edge-basic"),
+	}
+	if err := tf.Transform(&g); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	actual := strings.TrimSpace(g.String())
+	expected := strings.TrimSpace(testTransformDestroyEdgeBasicStr)
+	if actual != expected {
+		t.Fatalf("bad:\n\n%s", actual)
+	}
+}
+
+type graphNodeDestroyerTest struct {
+	AddrString string
+}
+
+func (n *graphNodeDestroyerTest) Name() string { return n.DestroyAddr().String() + " (destroy)" }
+func (n *graphNodeDestroyerTest) DestroyAddr() *ResourceAddress {
+	addr, err := ParseResourceAddress(n.AddrString)
+	if err != nil {
+		panic(err)
+	}
+
+	return addr
+}
+
+const testTransformDestroyEdgeBasicStr = `
+test.A (destroy)
+  test.B (destroy)
+test.B (destroy)
+`
