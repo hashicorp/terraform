f73dc844c7a9e279bb47552f37fb63d4b532ac81
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-09-26 18:01:02 -0700

    wip

--- terraform/resource_test.go
+++ terraform/resource_test.go
@@ -239,6 +239,19 @@ func TestResourceConfigGet(t *testing.T) {
 	}
 }
 
+func TestResourceConfigEqual_nil(t *testing.T) {
+	var nilRc *ResourceConfig
+	notNil := NewResourceConfig(nil)
+
+	if nilRc.Equal(notNil) {
+		t.Fatal("should not be equal")
+	}
+
+	if notNil.Equal(nilRc) {
+		t.Fatal("should not be equal")
+	}
+}
+
 func testResourceConfig(
 	t *testing.T, c map[string]interface{}) *ResourceConfig {
 	raw, err := config.NewRawConfig(c)
