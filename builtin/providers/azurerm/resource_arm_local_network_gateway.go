48055158b628c348e5e52f61022f5fe3c9853220
Andreas Kyrris <carinadigital@gmail.com>
2016-09-28 10:58:27 +0100

    Fix import of resource group name for azurerm_local_network_gateway.

--- builtin/providers/azurerm/resource_arm_local_network_gateway.go
+++ builtin/providers/azurerm/resource_arm_local_network_gateway.go
@@ -117,6 +117,7 @@ func resourceArmLocalNetworkGatewayRead(d *schema.ResourceData, meta interface{}
 		return nil
 	}
 
+	d.Set("resource_group_name", resGroup)
 	d.Set("name", resp.Name)
 	d.Set("location", resp.Location)
 	d.Set("gateway_address", resp.Properties.GatewayIPAddress)
