3c191f0528e2042b9bed915e298586e9475aa698
stack72 <public@paulstack.co.uk>
2016-10-03 16:45:02 +0100

    provider/aws: Rename the Import aws_opsworks_stack import test
    The casing on the test name was causing it not to run with the entire
    test suite
    
    ```
    % make testacc TEST=./builtin/providers/aws
    % TESTARGS='-run=TestAccAWSOpsworksStack'             2 ↵ ✹
    ==> Checking that code complies with gofmt requirements...
    go generate $(go list ./... | grep -v /terraform/vendor/)
    2016/10/03 16:43:07 Generated command/internal_plugin_list.go
    TF_ACC=1 go test ./builtin/providers/aws -v -run=TestAccAWSOpsworksStack
    -timeout 120m
    === RUN   TestAccAWSOpsworksStackImportBasic
    --- PASS: TestAccAWSOpsworksStackImportBasic (49.00s)
    === RUN   TestAccAWSOpsworksStackNoVpc
    --- PASS: TestAccAWSOpsworksStackNoVpc (36.10s)
    === RUN   TestAccAWSOpsworksStackVpc
    --- PASS: TestAccAWSOpsworksStackVpc (73.27s)
    PASS
    ok      github.com/hashicorp/terraform/builtin/providers/aws158.385s
    ```

--- builtin/providers/aws/import_aws_opsworks_stack_test.go
+++ builtin/providers/aws/import_aws_opsworks_stack_test.go
@@ -1,18 +1,13 @@
 package aws
 
 import (
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAWSOpsWorksStack_importBasic(t *testing.T) {
-	oldvar := os.Getenv("AWS_DEFAULT_REGION")
-	os.Setenv("AWS_DEFAULT_REGION", "us-west-2")
-	defer os.Setenv("AWS_DEFAULT_REGION", oldvar)
-
+func TestAccAWSOpsworksStackImportBasic(t *testing.T) {
 	name := acctest.RandString(10)
 
 	resourceName := "aws_opsworks_stack.tf-acc"
