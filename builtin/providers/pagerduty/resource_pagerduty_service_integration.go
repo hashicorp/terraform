effec42278a5738675c889a88da89c823b063921
Alexander Hellbom <alexander.hellbom@gmail.com>
2016-10-17 23:20:03 +0200

    Add service type validation

--- builtin/providers/pagerduty/resource_pagerduty_service_integration.go
+++ builtin/providers/pagerduty/resource_pagerduty_service_integration.go
@@ -28,6 +28,17 @@ func resourcePagerDutyServiceIntegration() *schema.Resource {
 			"type": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
+				ValidateFunc: validateValueFunc([]string{
+					"aws_cloudwatch_inbound_integration",
+					"cloudkick_inbound_integration",
+					"event_transformer_api_inbound_integration",
+					"generic_email_inbound_integration",
+					"generic_events_api_inbound_integration",
+					"keynote_inbound_integration",
+					"nagios_inbound_integration",
+					"pingdom_inbound_integration",
+					"sql_monitor_inbound_integration",
+				}),
 			},
 			"integration_key": &schema.Schema{
 				Type:     schema.TypeString,
