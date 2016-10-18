e1d6eab83ccf85fdd7aa0cc7aeca09f4f1efbf00
James Nugent <james@jen20.com>
2016-10-18 13:36:31 +0000

    v0.7.7

--- website/config.rb
+++ website/config.rb
@@ -2,6 +2,6 @@ set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
   h.name        = "terraform"
-  h.version     = "0.7.6"
+  h.version     = "0.7.7"
   h.github_slug = "hashicorp/terraform"
 end
