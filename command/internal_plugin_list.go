9bfa48ea9329b9f996e9e2773ac07cc9f4b10aab
Richard Clamp <richardc@unixbeard.net>
2016-10-19 15:25:12 +0100

    provider/gitlab: add gitlab provider and `gitlab_project` resource
    Here we add a basic provider with a single resource type.
    
    It's copied heavily from the `github` provider and `github_repository`
    resource, as there is some overlap in those types/apis.
    
    ~~~
    resource "gitlab_project" "test1" {
      name = "test1"
      visibility_level = "public"
    }
    ~~~
    
    We implement in terms of the
    [go-gitlab](https://github.com/xanzy/go-gitlab) library, which provides
    a wrapping of the [gitlab api](https://docs.gitlab.com/ee/api/)
    
    We have been a little selective in the properties we surface for the
    project resource, as not all properties are very instructive.
    Notable is the removal of the `public` bool as the `visibility_level`
    will take precedent if both are supplied which leads to confusing
    interactions if they disagree.

--- command/internal_plugin_list.go
+++ command/internal_plugin_list.go
@@ -31,6 +31,7 @@ import (
 	externalprovider "github.com/hashicorp/terraform/builtin/providers/external"
 	fastlyprovider "github.com/hashicorp/terraform/builtin/providers/fastly"
 	githubprovider "github.com/hashicorp/terraform/builtin/providers/github"
+	gitlabprovider "github.com/hashicorp/terraform/builtin/providers/gitlab"
 	googleprovider "github.com/hashicorp/terraform/builtin/providers/google"
 	grafanaprovider "github.com/hashicorp/terraform/builtin/providers/grafana"
 	herokuprovider "github.com/hashicorp/terraform/builtin/providers/heroku"
@@ -107,6 +108,7 @@ var InternalProviders = map[string]plugin.ProviderFunc{
 	"external":     externalprovider.Provider,
 	"fastly":       fastlyprovider.Provider,
 	"github":       githubprovider.Provider,
+	"gitlab":       gitlabprovider.Provider,
 	"google":       googleprovider.Provider,
 	"grafana":      grafanaprovider.Provider,
 	"heroku":       herokuprovider.Provider,
