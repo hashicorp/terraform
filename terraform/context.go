daee0bca8e96cba2a15d1d6e03185b6cd559aed5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-10-20 21:36:43 -0700

    terraform: don't set destroy module on diff
    This is something that should be determined and done during an apply. It
    doesn't make a lot of sense that the plan is doing it (in its current
    form at least).

--- terraform/context.go
+++ terraform/context.go
@@ -438,11 +438,6 @@ func (c *Context) Apply() (*State, error) {
 		log.Printf("[WARN] terraform: real graph is experiment, shadow is experiment")
 		real = shadow
 	} else {
-		// TODO: remove before branch is done, we're just not ready for this yet
-		if c.destroy {
-			shadow = nil
-		}
-
 		log.Printf("[WARN] terraform: real graph is original, shadow is experiment")
 	}
 
@@ -450,6 +445,11 @@ func (c *Context) Apply() (*State, error) {
 	// want to shadow yet with the new graphs.
 	if !contextTestShadow {
 		shadow = real
+	}
+
+	// TODO: remove before branch is done, we're just not ready for this yet
+	if c.destroy {
+		shadow = nil
 	}
 
 	// Determine the operation
