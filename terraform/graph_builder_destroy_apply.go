269aa280c5e012c2db17a2f67887da03864defcb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-10-20 22:52:42 -0700

    terraform: destroy apply graph builder should disable providers

--- terraform/graph_builder_destroy_apply.go
+++ terraform/graph_builder_destroy_apply.go
@@ -77,6 +77,7 @@ func (b *DestroyApplyGraphBuilder) Steps() []GraphTransformer {
 		// Create all the providers
 		&MissingProviderTransformer{Providers: b.Providers, Factory: providerFactory},
 		&ProviderTransformer{},
+		&DisableProviderTransformer{},
 		&ParentProviderTransformer{},
 		&AttachProviderConfigTransformer{Module: b.Module},
 
