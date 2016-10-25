e84880193a13150ac452e0c113d66ea0aab7203e
tombuildsstuff <git@ibuildstuff.co.uk>
2016-10-25 17:38:11 +0200

    Adding the Microsoft.Cache provider

--- builtin/providers/azurerm/provider.go
+++ builtin/providers/azurerm/provider.go
@@ -196,7 +196,18 @@ func registerAzureResourceProvidersWithSubscription(client *riviera.Client) erro
 	var err error
 	providerRegistrationOnce.Do(func() {
 		// We register Microsoft.Compute during client initialization
-		providers := []string{"Microsoft.Network", "Microsoft.Cdn", "Microsoft.Storage", "Microsoft.Sql", "Microsoft.Search", "Microsoft.Resources", "Microsoft.ServiceBus", "Microsoft.KeyVault", "Microsoft.EventHub"}
+		providers := []string{
+			"Microsoft.Network",
+			"Microsoft.Cdn",
+			"Microsoft.Storage",
+			"Microsoft.Sql",
+			"Microsoft.Search",
+			"Microsoft.Resources",
+			"Microsoft.ServiceBus",
+			"Microsoft.KeyVault",
+			"Microsoft.EventHub",
+			"Microsoft.Cache",
+		}
 
 		var wg sync.WaitGroup
 		wg.Add(len(providers))
