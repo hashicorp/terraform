5d8a3200a957921f1f99e8e189d3533629b4c7ef
Thomas Boerger <thomas@webhippie.de>
2016-10-29 17:22:38 +0200

    provider/cloudflare: Updated github.com/cloudflare/cloudflare-go (#9715)
    To avoid the issue #8011 I have updated the used client library, with
    this update I don't get the mentioned issues like `unexpected EOF`
    anymore.
    
    Fixes #8011

--- vendor/github.com/cloudflare/cloudflare-go/dns.go
+++ vendor/github.com/cloudflare/cloudflare-go/dns.go
@@ -3,9 +3,41 @@ package cloudflare
 import (
 	"encoding/json"
 	"net/url"
+	"time"
 
 	"github.com/pkg/errors"
 )
+
+// DNSRecord represents a DNS record in a zone.
+type DNSRecord struct {
+	ID         string      `json:"id,omitempty"`
+	Type       string      `json:"type,omitempty"`
+	Name       string      `json:"name,omitempty"`
+	Content    string      `json:"content,omitempty"`
+	Proxiable  bool        `json:"proxiable,omitempty"`
+	Proxied    bool        `json:"proxied,omitempty"`
+	TTL        int         `json:"ttl,omitempty"`
+	Locked     bool        `json:"locked,omitempty"`
+	ZoneID     string      `json:"zone_id,omitempty"`
+	ZoneName   string      `json:"zone_name,omitempty"`
+	CreatedOn  time.Time   `json:"created_on,omitempty"`
+	ModifiedOn time.Time   `json:"modified_on,omitempty"`
+	Data       interface{} `json:"data,omitempty"` // data returned by: SRV, LOC
+	Meta       interface{} `json:"meta,omitempty"`
+	Priority   int         `json:"priority,omitempty"`
+}
+
+// DNSRecordResponse represents the response from the DNS endpoint.
+type DNSRecordResponse struct {
+	Response
+	Result DNSRecord `json:"result"`
+}
+
+// DNSListResponse represents the response from the list DNS records endpoint.
+type DNSListResponse struct {
+	Response
+	Result []DNSRecord `json:"result"`
+}
 
 // CreateDNSRecord creates a DNS record for the zone identifier.
 // API reference:
