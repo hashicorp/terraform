6c5bbb890eea01637e2df035f6ba542294e6a7b7
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-10-31 19:49:53 +0000

    Allow `active` state while waiting for the VPC Peering Connection.
    This commit adds `active` as one of the valid states in which the VPC Peering
    Connection can be when it being created.
    
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- builtin/providers/aws/resource_aws_vpc_peering_connection.go
+++ builtin/providers/aws/resource_aws_vpc_peering_connection.go
@@ -85,7 +85,7 @@ func resourceAwsVPCPeeringCreate(d *schema.ResourceData, meta interface{}) error
 	log.Printf("[DEBUG] Waiting for VPC Peering Connection (%s) to become available.", d.Id())
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{"pending"},
-		Target:  []string{"pending-acceptance"},
+		Target:  []string{"pending-acceptance", "active"},
 		Refresh: resourceAwsVPCPeeringConnectionStateRefreshFunc(conn, d.Id()),
 		Timeout: 1 * time.Minute,
 	}
