2b882ef1012edb0c561031b6c226d9d81a4e0201
Paul Stack <public@paulstack.co.uk>
2016-10-31 10:40:35 +0000

    provider/aws: Update aws_appautoscaling_target_test (#9736)
    The update of the test was causing a test failure - it was setting
    desired_count to 1 when miz_size was set to 2 - this was causing a
    perpetual diff in the test

--- builtin/providers/aws/resource_aws_appautoscaling_target_test.go
+++ builtin/providers/aws/resource_aws_appautoscaling_target_test.go
@@ -284,7 +284,7 @@ resource "aws_ecs_service" "service" {
 	name = "foobar"
 	cluster = "${aws_ecs_cluster.foo.id}"
 	task_definition = "${aws_ecs_task_definition.task.arn}"
-	desired_count = 1
+	desired_count = 2
 
 	deployment_maximum_percent = 200
 	deployment_minimum_healthy_percent = 50
