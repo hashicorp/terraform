c31a91c4fecf398a38c4ad7af4e029c80bb0346f
Paul Stack <public@paulstack.co.uk>
2016-10-31 17:05:06 +0000

    provider/aws: Expose ARN suffix on ALB Target Group (#9734)
    When creating a CloudWatch Metric for an Application Load Balancer Target Group  it is
    neccessary to use the suffix of the ARN as the reference to the load
    balancer TG . This commit exposes that as an attribute on the `aws_alb_target_group`
    resource to prevent the need to use regular expression substitution to
    make the reference.

--- builtin/providers/aws/resource_aws_alb_target_group_test.go
+++ builtin/providers/aws/resource_aws_alb_target_group_test.go
@@ -13,6 +13,37 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+func TestALBTargetGroupCloudwatchSuffixFromARN(t *testing.T) {
+	cases := []struct {
+		name   string
+		arn    *string
+		suffix string
+	}{
+		{
+			name:   "valid suffix",
+			arn:    aws.String(`arn:aws:elasticloadbalancing:us-east-1:123456:targetgroup/my-targets/73e2d6bc24d8a067`),
+			suffix: `my-targets/73e2d6bc24d8a067`,
+		},
+		{
+			name:   "no suffix",
+			arn:    aws.String(`arn:aws:elasticloadbalancing:us-east-1:123456:targetgroup`),
+			suffix: ``,
+		},
+		{
+			name:   "nil ARN",
+			arn:    nil,
+			suffix: ``,
+		},
+	}
+
+	for _, tc := range cases {
+		actual := albTargetGroupSuffixFromARN(tc.arn)
+		if actual != tc.suffix {
+			t.Fatalf("bad suffix: %q\nExpected: %s\n     Got: %s", tc.name, tc.suffix, actual)
+		}
+	}
+}
+
 func TestAccAWSALBTargetGroup_basic(t *testing.T) {
 	var conf elbv2.TargetGroup
 	targetGroupName := fmt.Sprintf("test-target-group-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
