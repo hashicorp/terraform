97f1b366af3478142047e5816b204bc530004d66
Paul Stack <public@paulstack.co.uk>
2016-11-01 10:15:37 +0000

    provider/azurerm: Bump jen20/riviera SDK (#9765)
    Fixes #9400

--- vendor/github.com/jen20/riviera/azure/request.go
+++ vendor/github.com/jen20/riviera/azure/request.go
@@ -75,7 +75,8 @@ func (request *Request) pollForAsynchronousResponse(acceptedResponse *http.Respo
 			return nil, err
 		}
 
-		err = request.client.tokenRequester.addAuthorizationToRequest(req)
+		endpoints := GetEndpointsForCommand(request.Command)
+		err = request.client.tokenRequester.addAuthorizationToRequest(req, endpoints)
 		if err != nil {
 			return nil, err
 		}
@@ -115,10 +116,11 @@ func defaultARMRequestSerialize(body interface{}) (io.ReadSeeker, error) {
 func (request *Request) Execute() (*Response, error) {
 	apiInfo := request.Command.APIInfo()
 
+	endpoints := GetEndpointsForCommand(request.Command)
+
 	var urlString string
 
-	// Base URL should already be validated by now so Parse is safe without error handling
-	urlObj, _ := url.Parse(request.client.BaseURL)
+	urlObj, _ := url.Parse(endpoints.resourceManagerEndpointUrl)
 
 	// Determine whether to use the URLPathFunc or the URI explicitly set in the request
 	if request.URI == nil {
@@ -164,7 +166,7 @@ func (request *Request) Execute() (*Response, error) {
 		req.Header.Add("Content-Type", "application/json")
 	}
 
-	err = request.client.tokenRequester.addAuthorizationToRequest(req)
+	err = request.client.tokenRequester.addAuthorizationToRequest(req, endpoints)
 	if err != nil {
 		return nil, err
 	}
