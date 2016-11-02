b03cbdffc714bafaa5a648bbed6d9bad4e2d782c
Sam Dunne <sam@sam-dunne.com>
2016-11-02 14:36:39 +0000

    Update based off review

--- builtin/providers/postgresql/resource_postgresql_extension.go
+++ builtin/providers/postgresql/resource_postgresql_extension.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/hashicorp/errwrap"
@@ -11,6 +12,7 @@ import (
 func resourcePostgreSQLExtension() *schema.Resource {
 	return &schema.Resource{
 		Create: resourcePostgreSQLExtensionCreate,
+		Read:   resourcePostgreSQLExtensionRead,
 		Delete: resourcePostgreSQLExtensionDelete,
 
 		Schema: map[string]*schema.Schema{
@@ -41,7 +43,31 @@ func resourcePostgreSQLExtensionCreate(d *schema.ResourceData, meta interface{})
 
 	d.SetId(extensionName)
 
-	return resourcePostgreSQLRoleRead(d, meta)
+	return resourcePostgreSQLExtensionRead(d, meta)
+}
+
+func resourcePostgreSQLExtensionRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client)
+	conn, err := client.Connect()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	extensionName := d.Get("name").(string)
+
+	var hasExtension bool
+	err = conn.QueryRow("SELECT 1 from pg_extension d WHERE extname=$1", extensionName).Scan(&hasExtension)
+	switch {
+	case err == sql.ErrNoRows:
+		d.SetId("")
+		return nil
+	case err != nil:
+		return errwrap.Wrapf("Error reading extension: {{err}}", err)
+	default:
+		d.Set("extension", hasExtension)
+		return nil
+	}
 }
 
 func resourcePostgreSQLExtensionDelete(d *schema.ResourceData, meta interface{}) error {
