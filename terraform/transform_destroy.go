d4834e12ff34fe93b92c1c47080f4e14b4e88d71
James Bardin <j.bardin@gmail.com>
2016-11-03 15:43:34 -0400

    fix CreateBeforeDestroy with datasources
    The graph transformation we implement around create_before_destroy
    need to re-order all resources that depend on the create_before_destroy
    resource. Up until now, we've requires that users mark all of these
    resources as create_before_destroy. Data soruces however don't have a
    lifecycle block for create_before_destroy, and could not be marked this
    way.
    
    This PR checks each DestroyNode that doesn't implement CreateBeforeDestroy
    for any ancestors that do implement CreateBeforeDestroy. If there are
    any, we inherit the behavior and re-order the graph as such.

--- terraform/transform_destroy.go
+++ terraform/transform_destroy.go
@@ -1,8 +1,6 @@
 package terraform
 
-import (
-	"github.com/hashicorp/terraform/dag"
-)
+import "github.com/hashicorp/terraform/dag"
 
 // GraphNodeDestroyable is the interface that nodes that can be destroyed
 // must implement. This is used to automatically handle the creation of
@@ -153,7 +151,7 @@ func (t *CreateBeforeDestroyTransformer) Transform(g *Graph) error {
 		}
 
 		// If the node doesn't need to create before destroy, then continue
-		if !dn.CreateBeforeDestroy() {
+		if !dn.CreateBeforeDestroy() && noCreateBeforeDestroyAncestors(g, dn) {
 			continue
 		}
 
@@ -198,6 +196,30 @@ func (t *CreateBeforeDestroyTransformer) Transform(g *Graph) error {
 	}
 
 	return nil
+}
+
+// noCreateBeforeDestroyAncestors verifies that a vertex has no ancestors that
+// are CreateBeforeDestroy.
+// If this vertex has an ancestor with CreateBeforeDestroy, we will need to
+// inherit that behavior and re-order the edges even if this node type doesn't
+// directly implement CreateBeforeDestroy.
+func noCreateBeforeDestroyAncestors(g *Graph, v dag.Vertex) bool {
+	s, _ := g.Ancestors(v)
+	if s == nil {
+		return true
+	}
+	for _, v := range s.List() {
+		dn, ok := v.(GraphNodeDestroy)
+		if !ok {
+			continue
+		}
+
+		if dn.CreateBeforeDestroy() {
+			// some ancestor is CreateBeforeDestroy, so we need to follow suit
+			return false
+		}
+	}
+	return true
 }
 
 // PruneDestroyTransformer is a GraphTransformer that removes the destroy
