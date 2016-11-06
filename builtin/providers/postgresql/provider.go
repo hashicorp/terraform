59f4ad6fd158a642d6662da9e577059753fec11f
Sean Chittenden <sean@chittenden.org>
2016-11-06 01:50:28 -0700

    Alpha sort the resources in the PostgreSQL map.

--- builtin/providers/postgresql/provider.go
+++ builtin/providers/postgresql/provider.go
@@ -75,8 +75,8 @@ func Provider() terraform.ResourceProvider {
 
 		ResourcesMap: map[string]*schema.Resource{
 			"postgresql_database":  resourcePostgreSQLDatabase(),
-			"postgresql_role":      resourcePostgreSQLRole(),
 			"postgresql_extension": resourcePostgreSQLExtension(),
+			"postgresql_role":      resourcePostgreSQLRole(),
 		},
 
 		ConfigureFunc: providerConfigure,
