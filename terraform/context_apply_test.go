a99b29e5e80b603114ac61883254d3f653248e71
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-11-08 13:21:50 -0800

    terraform: prune orphan outputs in old graph
    This makes the old graph also prune orphan outputs in modules.
    This will fix shadow graph errors such as #9905 since the old graph will
    also behave correctly in these scenarios.
    
    Luckily, because orphan outputs don't rely on anything, we were able to
    simply use the same transformer!

--- terraform/context_apply_test.go
+++ terraform/context_apply_test.go
@@ -12,7 +12,6 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform/config/module"
-	"github.com/hashicorp/terraform/helper/experiment"
 )
 
 func TestContext2Apply_basic(t *testing.T) {
@@ -2291,10 +2290,6 @@ func TestContext2Apply_outputOrphan(t *testing.T) {
 }
 
 func TestContext2Apply_outputOrphanModule(t *testing.T) {
-	if !experiment.Enabled(experiment.X_newApply) {
-		t.SkipNow()
-	}
-
 	m := testModule(t, "apply-output-orphan-module")
 	p := testProvider("aws")
 	p.ApplyFn = testApplyFn
