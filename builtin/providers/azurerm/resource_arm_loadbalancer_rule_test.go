0e0112000941ea0d18681ede386a5f4459818a2b
Peter McAtominey <peter.mcatominey@bedegaming.com>
2016-11-09 16:30:31 +0000

    provider/azurerm: allow updating of lb_rule resource
    * check if rule is being updated rather than assuming created
    * added test to cover guard against multiple rules with the same name
    
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run "TestAccAzureRMLoadBalancerRule_" -timeout 120m
    === RUN   TestAccAzureRMLoadBalancerRule_basic
    --- PASS: TestAccAzureRMLoadBalancerRule_basic (157.45s)
    === RUN   TestAccAzureRMLoadBalancerRule_removal
    --- PASS: TestAccAzureRMLoadBalancerRule_removal (163.67s)
    === RUN   TestAccAzureRMLoadBalancerRule_inconsistentReads
    --- PASS: TestAccAzureRMLoadBalancerRule_inconsistentReads (150.00s)
    === RUN   TestAccAzureRMLoadBalancerRule_update
    --- PASS: TestAccAzureRMLoadBalancerRule_update (164.20s)
    === RUN   TestAccAzureRMLoadBalancerRule_duplicateRules
    --- PASS: TestAccAzureRMLoadBalancerRule_duplicateRules (137.51s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	772.846s

--- builtin/providers/azurerm/resource_arm_loadbalancer_rule_test.go
+++ builtin/providers/azurerm/resource_arm_loadbalancer_rule_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 
+	"regexp"
+
 	"github.com/Azure/azure-sdk-for-go/arm/network"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
@@ -144,6 +146,82 @@ func TestAccAzureRMLoadBalancerRule_inconsistentReads(t *testing.T) {
 					testCheckAzureRMLoadBalancerRuleExists(lbRuleName, &lb),
 					testCheckAzureRMLoadBalancerProbeExists(probeName, &lb),
 				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLoadBalancerRule_update(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	lbRuleName := fmt.Sprintf("LbRule-%s", acctest.RandStringFromCharSet(8, acctest.CharSetAlpha))
+	lbRule2Name := fmt.Sprintf("LbRule-%s", acctest.RandStringFromCharSet(8, acctest.CharSetAlpha))
+
+	subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	lbRuleID := fmt.Sprintf(
+		"/subscriptions/%s/resourceGroups/acctestrg-%d/providers/Microsoft.Network/loadBalancers/arm-test-loadbalancer-%d/loadBalancingRules/%s",
+		subscriptionID, ri, ri, lbRuleName)
+
+	lbRule2ID := fmt.Sprintf(
+		"/subscriptions/%s/resourceGroups/acctestrg-%d/providers/Microsoft.Network/loadBalancers/arm-test-loadbalancer-%d/loadBalancingRules/%s",
+		subscriptionID, ri, ri, lbRule2Name)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLoadBalancerRule_multipleRules(ri, lbRuleName, lbRule2Name),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerRuleExists(lbRuleName, &lb),
+					testCheckAzureRMLoadBalancerRuleExists(lbRule2Name, &lb),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test", "id", lbRuleID),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "id", lbRule2ID),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "frontend_port", "3390"),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "backend_port", "3390"),
+				),
+			},
+			{
+				Config: testAccAzureRMLoadBalancerRule_multipleRulesUpdate(ri, lbRuleName, lbRule2Name),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerRuleExists(lbRuleName, &lb),
+					testCheckAzureRMLoadBalancerRuleExists(lbRule2Name, &lb),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test", "id", lbRuleID),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "id", lbRule2ID),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "frontend_port", "3391"),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test2", "backend_port", "3391"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLoadBalancerRule_duplicateRules(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	lbRuleName := fmt.Sprintf("LbRule-%s", acctest.RandStringFromCharSet(8, acctest.CharSetAlpha))
+
+	subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	lbRuleID := fmt.Sprintf(
+		"/subscriptions/%s/resourceGroups/acctestrg-%d/providers/Microsoft.Network/loadBalancers/arm-test-loadbalancer-%d/loadBalancingRules/%s",
+		subscriptionID, ri, ri, lbRuleName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLoadBalancerRule_multipleRules(ri, lbRuleName, lbRuleName),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerRuleExists(lbRuleName, &lb),
+					resource.TestCheckResourceAttr("azurerm_lb_rule.test", "id", lbRuleID),
+				),
+				ExpectError: regexp.MustCompile(fmt.Sprintf("A LoadBalancer Rule with name %q already exists.", lbRuleName)),
 			},
 		},
 	})
@@ -290,4 +368,104 @@ resource "azurerm_lb_rule" "test" {
   frontend_ip_configuration_name = "one-%d"
 }
 `, rInt, rInt, rInt, rInt, backendPoolName, probeName, lbRuleName, rInt)
+}
+
+func testAccAzureRMLoadBalancerRule_multipleRules(rInt int, lbRuleName, lbRule2Name string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+    name = "acctestrg-%d"
+    location = "West US"
+}
+
+resource "azurerm_public_ip" "test" {
+    name = "test-ip-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    public_ip_address_allocation = "static"
+}
+
+resource "azurerm_lb" "test" {
+    name = "arm-test-loadbalancer-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    frontend_ip_configuration {
+      name = "one-%d"
+      public_ip_address_id = "${azurerm_public_ip.test.id}"
+    }
+}
+
+resource "azurerm_lb_rule" "test" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Udp"
+  frontend_port = 3389
+  backend_port = 3389
+  frontend_ip_configuration_name = "one-%d"
+}
+
+resource "azurerm_lb_rule" "test2" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Udp"
+  frontend_port = 3390
+  backend_port = 3390
+  frontend_ip_configuration_name = "one-%d"
+}
+
+`, rInt, rInt, rInt, rInt, lbRuleName, rInt, lbRule2Name, rInt)
+}
+
+func testAccAzureRMLoadBalancerRule_multipleRulesUpdate(rInt int, lbRuleName, lbRule2Name string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+    name = "acctestrg-%d"
+    location = "West US"
+}
+
+resource "azurerm_public_ip" "test" {
+    name = "test-ip-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    public_ip_address_allocation = "static"
+}
+
+resource "azurerm_lb" "test" {
+    name = "arm-test-loadbalancer-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    frontend_ip_configuration {
+      name = "one-%d"
+      public_ip_address_id = "${azurerm_public_ip.test.id}"
+    }
+}
+
+resource "azurerm_lb_rule" "test" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Udp"
+  frontend_port = 3389
+  backend_port = 3389
+  frontend_ip_configuration_name = "one-%d"
+}
+
+resource "azurerm_lb_rule" "test2" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Udp"
+  frontend_port = 3391
+  backend_port = 3391
+  frontend_ip_configuration_name = "one-%d"
+}
+
+`, rInt, rInt, rInt, rInt, lbRuleName, rInt, lbRule2Name, rInt)
 }
