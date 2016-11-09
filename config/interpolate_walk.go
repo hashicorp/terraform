a8fbf3420c231d7c4762c463118cd452fb2d4081
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-11-09 14:23:28 -0800

    config: clean up unused fucntions

--- config/interpolate_walk.go
+++ config/interpolate_walk.go
@@ -176,11 +176,7 @@ func (w *interpolationWalker) Primitive(v reflect.Value) error {
 		}
 
 		if remove {
-			// Append the key to the unknown keys
 			w.unknownKeys = append(w.unknownKeys, strings.Join(w.key, "."))
-
-			//w.removeCurrent()
-			//return nil
 		}
 
 		resultVal := reflect.ValueOf(replaceVal)
@@ -210,27 +206,6 @@ func (w *interpolationWalker) Primitive(v reflect.Value) error {
 	}
 
 	return nil
-}
-
-func (w *interpolationWalker) removeCurrent() {
-	// Append the key to the unknown keys
-	w.unknownKeys = append(w.unknownKeys, strings.Join(w.key, "."))
-
-	for i := 1; i <= len(w.cs); i++ {
-		c := w.cs[len(w.cs)-i]
-		switch c.Kind() {
-		case reflect.Map:
-			// Zero value so that we delete the map key
-			var val reflect.Value
-
-			// Get the key and delete it
-			k := w.csData.(reflect.Value)
-			c.SetMapIndex(k, val)
-			return
-		}
-	}
-
-	panic("No container found for removeCurrent")
 }
 
 func (w *interpolationWalker) replaceCurrent(v reflect.Value) {
