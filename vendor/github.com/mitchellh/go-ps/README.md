a23ea646f3fb7b200758cc44e2b9ea15a57ba0bb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-11-10 08:52:01 -0800

    vendor: update go-ps to not require cgo on darwin

--- vendor/github.com/mitchellh/go-ps/README.md
+++ vendor/github.com/mitchellh/go-ps/README.md
@@ -10,8 +10,7 @@ DLL methods for Windows, cgo for Darwin, etc.
 
 How it works:
 
-  * **Darwin** uses the `sysctl` syscall to retrieve the process table, via
-    cgo.
+  * **Darwin** uses the `sysctl` syscall to retrieve the process table.
   * **Unix** uses the procfs at `/proc` to inspect the process tree.
   * **Windows** uses the Windows API, and methods such as
     `CreateToolhelp32Snapshot` to get a point-in-time snapshot of
@@ -33,4 +32,3 @@ implemented for this library that would be nice:
 
   * FreeBSD support
   * Plan9 support
-  * Eliminate the need for cgo with Darwin
--- vendor/github.com/mitchellh/go-ps/process_darwin.go
+++ vendor/github.com/mitchellh/go-ps/process_darwin.go
@@ -2,17 +2,12 @@
 
 package ps
 
-// #include "process_darwin.h"
-import "C"
-
 import (
-	"sync"
+	"bytes"
+	"encoding/binary"
+	"syscall"
+	"unsafe"
 )
-
-// This lock is what verifies that C calling back into Go is only
-// modifying data once at a time.
-var darwinLock sync.Mutex
-var darwinProcs []Process
 
 type DarwinProcess struct {
 	pid    int
@@ -32,17 +27,6 @@ func (p *DarwinProcess) Executable() string {
 	return p.binary
 }
 
-//export go_darwin_append_proc
-func go_darwin_append_proc(pid C.pid_t, ppid C.pid_t, comm *C.char) {
-	proc := &DarwinProcess{
-		pid:    int(pid),
-		ppid:   int(ppid),
-		binary: C.GoString(comm),
-	}
-
-	darwinProcs = append(darwinProcs, proc)
-}
-
 func findProcess(pid int) (Process, error) {
 	ps, err := processes()
 	if err != nil {
@@ -59,14 +43,96 @@ func findProcess(pid int) (Process, error) {
 }
 
 func processes() ([]Process, error) {
-	darwinLock.Lock()
-	defer darwinLock.Unlock()
-	darwinProcs = make([]Process, 0, 50)
-
-	_, err := C.darwinProcesses()
+	buf, err := darwinSyscall()
 	if err != nil {
 		return nil, err
 	}
 
+	procs := make([]*kinfoProc, 0, 50)
+	k := 0
+	for i := _KINFO_STRUCT_SIZE; i < buf.Len(); i += _KINFO_STRUCT_SIZE {
+		proc := &kinfoProc{}
+		err = binary.Read(bytes.NewBuffer(buf.Bytes()[k:i]), binary.LittleEndian, proc)
+		if err != nil {
+			return nil, err
+		}
+
+		k = i
+		procs = append(procs, proc)
+	}
+
+	darwinProcs := make([]Process, len(procs))
+	for i, p := range procs {
+		darwinProcs[i] = &DarwinProcess{
+			pid:    int(p.Pid),
+			ppid:   int(p.PPid),
+			binary: darwinCstring(p.Comm),
+		}
+	}
+
 	return darwinProcs, nil
+}
+
+func darwinCstring(s [16]byte) string {
+	i := 0
+	for _, b := range s {
+		if b != 0 {
+			i++
+		} else {
+			break
+		}
+	}
+
+	return string(s[:i])
+}
+
+func darwinSyscall() (*bytes.Buffer, error) {
+	mib := [4]int32{_CTRL_KERN, _KERN_PROC, _KERN_PROC_ALL, 0}
+	size := uintptr(0)
+
+	_, _, errno := syscall.Syscall6(
+		syscall.SYS___SYSCTL,
+		uintptr(unsafe.Pointer(&mib[0])),
+		4,
+		0,
+		uintptr(unsafe.Pointer(&size)),
+		0,
+		0)
+
+	if errno != 0 {
+		return nil, errno
+	}
+
+	bs := make([]byte, size)
+	_, _, errno = syscall.Syscall6(
+		syscall.SYS___SYSCTL,
+		uintptr(unsafe.Pointer(&mib[0])),
+		4,
+		uintptr(unsafe.Pointer(&bs[0])),
+		uintptr(unsafe.Pointer(&size)),
+		0,
+		0)
+
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return bytes.NewBuffer(bs[0:size]), nil
+}
+
+const (
+	_CTRL_KERN         = 1
+	_KERN_PROC         = 14
+	_KERN_PROC_ALL     = 0
+	_KINFO_STRUCT_SIZE = 648
+)
+
+type kinfoProc struct {
+	_    [40]byte
+	Pid  int32
+	_    [199]byte
+	Comm [16]byte
+	_    [301]byte
+	PPid int32
+	_    [84]byte
 }
