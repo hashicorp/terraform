4996f3e289a8c821070070f5270415bc19bbba01
Peter McAtominey <mcatominey08@gmail.com>
2016-11-11 11:09:00 +0000

    provider/azurerm: allow updating load balancer sub-resources (#10016)
    * provider/azurerm: allow updating of lb_rule resource
    
    * check if rule is being updated rather than assuming created
    * added test to cover guard against multiple rules with the same name
    
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run "TestAccAzureRMLoadBalancerRule_" -timeout 120m
    === RUN   TestAccAzureRMLoadBalancerRule_basic
    --- PASS: TestAccAzureRMLoadBalancerRule_basic (157.45s)
    === RUN   TestAccAzureRMLoadBalancerRule_removal
    --- PASS: TestAccAzureRMLoadBalancerRule_removal (163.67s)
    === RUN   TestAccAzureRMLoadBalancerRule_inconsistentReads
    --- PASS: TestAccAzureRMLoadBalancerRule_inconsistentReads (150.00s)
    === RUN   TestAccAzureRMLoadBalancerRule_update
    --- PASS: TestAccAzureRMLoadBalancerRule_update (164.20s)
    === RUN   TestAccAzureRMLoadBalancerRule_duplicateRules
    --- PASS: TestAccAzureRMLoadBalancerRule_duplicateRules (137.51s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	772.846s
    
    * provider/azurerm: allow updating of lb_nat_rule resource
    
    * check if NAT rule is being updated rather than assuming created
    * added test to cover guard against multiple NAT rules with the same name
    
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run "TestAccAzureRMLoadBalancerNatRule" -timeout 120m
    === RUN   TestAccAzureRMLoadBalancerNatRule_basic
    --- PASS: TestAccAzureRMLoadBalancerNatRule_basic (148.48s)
    === RUN   TestAccAzureRMLoadBalancerNatRule_removal
    --- PASS: TestAccAzureRMLoadBalancerNatRule_removal (163.48s)
    === RUN   TestAccAzureRMLoadBalancerNatRule_update
    --- PASS: TestAccAzureRMLoadBalancerNatRule_update (176.97s)
    === RUN   TestAccAzureRMLoadBalancerNatRule_duplicate
    --- PASS: TestAccAzureRMLoadBalancerNatRule_duplicate (136.36s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	625.301s
    
    * provider/azurerm: allow updating of lb_probe resource
    
    * check if probe is being updated rather than assuming created
    * added test to cover guard against multiple probes with the same name
    
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run "TestAccAzureRMLoadBalancerProbe" -timeout 120m
    === RUN   TestAccAzureRMLoadBalancerProbe_basic
    --- PASS: TestAccAzureRMLoadBalancerProbe_basic (134.53s)
    === RUN   TestAccAzureRMLoadBalancerProbe_removal
    --- PASS: TestAccAzureRMLoadBalancerProbe_removal (168.06s)
    === RUN   TestAccAzureRMLoadBalancerProbe_update
    --- PASS: TestAccAzureRMLoadBalancerProbe_update (175.99s)
    === RUN   TestAccAzureRMLoadBalancerProbe_duplicate
    --- PASS: TestAccAzureRMLoadBalancerProbe_duplicate (139.01s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	617.598s
    
    * provider/azurerm: allow updating of lb_nat_pool resource
    
    * check if NAT pool is being updated rather than assuming created
    * added test to cover guard against multiple NAT pools with the same name
    
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run "TestAccAzureRMLoadBalancerNatPool" -timeout 120m
    === RUN   TestAccAzureRMLoadBalancerNatPool_basic
    --- PASS: TestAccAzureRMLoadBalancerNatPool_basic (146.52s)
    === RUN   TestAccAzureRMLoadBalancerNatPool_removal
    --- PASS: TestAccAzureRMLoadBalancerNatPool_removal (158.34s)
    === RUN   TestAccAzureRMLoadBalancerNatPool_update
    --- PASS: TestAccAzureRMLoadBalancerNatPool_update (174.05s)
    === RUN   TestAccAzureRMLoadBalancerNatPool_duplicate
    --- PASS: TestAccAzureRMLoadBalancerNatPool_duplicate (136.94s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	615.866s

--- builtin/providers/azurerm/resource_arm_loadbalancer_nat_pool_test.go
+++ builtin/providers/azurerm/resource_arm_loadbalancer_nat_pool_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 
+	"regexp"
+
 	"github.com/Azure/azure-sdk-for-go/arm/network"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
@@ -62,6 +64,61 @@ func TestAccAzureRMLoadBalancerNatPool_removal(t *testing.T) {
 					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
 					testCheckAzureRMLoadBalancerNatPoolNotExists(natPoolName, &lb),
 				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLoadBalancerNatPool_update(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	natPoolName := fmt.Sprintf("NatPool-%d", ri)
+	natPool2Name := fmt.Sprintf("NatPool-%d", acctest.RandInt())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLoadBalancerNatPool_multiplePools(ri, natPoolName, natPool2Name),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerNatPoolExists(natPoolName, &lb),
+					testCheckAzureRMLoadBalancerNatPoolExists(natPool2Name, &lb),
+					resource.TestCheckResourceAttr("azurerm_lb_nat_pool.test2", "backend_port", "3390"),
+				),
+			},
+			{
+				Config: testAccAzureRMLoadBalancerNatPool_multiplePoolsUpdate(ri, natPoolName, natPool2Name),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerNatPoolExists(natPoolName, &lb),
+					testCheckAzureRMLoadBalancerNatPoolExists(natPool2Name, &lb),
+					resource.TestCheckResourceAttr("azurerm_lb_nat_pool.test2", "backend_port", "3391"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMLoadBalancerNatPool_duplicate(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	natPoolName := fmt.Sprintf("NatPool-%d", ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureRMLoadBalancerNatPool_multiplePools(ri, natPoolName, natPoolName),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMLoadBalancerExists("azurerm_lb.test", &lb),
+					testCheckAzureRMLoadBalancerNatPoolExists(natPoolName, &lb),
+				),
+				ExpectError: regexp.MustCompile(fmt.Sprintf("A NAT Pool with name %q already exists.", natPoolName)),
 			},
 		},
 	})
@@ -154,4 +211,108 @@ resource "azurerm_lb" "test" {
     }
 }
 `, rInt, rInt, rInt, rInt)
+}
+
+func testAccAzureRMLoadBalancerNatPool_multiplePools(rInt int, natPoolName, natPool2Name string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+    name = "acctestrg-%d"
+    location = "West US"
+}
+
+resource "azurerm_public_ip" "test" {
+    name = "test-ip-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    public_ip_address_allocation = "static"
+}
+
+resource "azurerm_lb" "test" {
+    name = "arm-test-loadbalancer-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    frontend_ip_configuration {
+      name = "one-%d"
+      public_ip_address_id = "${azurerm_public_ip.test.id}"
+    }
+}
+
+resource "azurerm_lb_nat_pool" "test" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Tcp"
+  frontend_port_start = 80
+  frontend_port_end = 81
+  backend_port = 3389
+  frontend_ip_configuration_name = "one-%d"
+}
+
+resource "azurerm_lb_nat_pool" "test2" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Tcp"
+  frontend_port_start = 82
+  frontend_port_end = 83
+  backend_port = 3390
+  frontend_ip_configuration_name = "one-%d"
+}
+
+`, rInt, rInt, rInt, rInt, natPoolName, rInt, natPool2Name, rInt)
+}
+
+func testAccAzureRMLoadBalancerNatPool_multiplePoolsUpdate(rInt int, natPoolName, natPool2Name string) string {
+	return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+    name = "acctestrg-%d"
+    location = "West US"
+}
+
+resource "azurerm_public_ip" "test" {
+    name = "test-ip-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    public_ip_address_allocation = "static"
+}
+
+resource "azurerm_lb" "test" {
+    name = "arm-test-loadbalancer-%d"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    frontend_ip_configuration {
+      name = "one-%d"
+      public_ip_address_id = "${azurerm_public_ip.test.id}"
+    }
+}
+
+resource "azurerm_lb_nat_pool" "test" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Tcp"
+  frontend_port_start = 80
+  frontend_port_end = 81
+  backend_port = 3389
+  frontend_ip_configuration_name = "one-%d"
+}
+
+resource "azurerm_lb_nat_pool" "test2" {
+  location = "West US"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id = "${azurerm_lb.test.id}"
+  name = "%s"
+  protocol = "Tcp"
+  frontend_port_start = 82
+  frontend_port_end = 83
+  backend_port = 3391
+  frontend_ip_configuration_name = "one-%d"
+}
+
+`, rInt, rInt, rInt, rInt, natPoolName, rInt, natPool2Name, rInt)
 }
