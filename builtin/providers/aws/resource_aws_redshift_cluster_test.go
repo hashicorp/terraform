e1919edbb3a2afb0265005fc90809c53db24facb
Anshul Sharma <optimisticanshul@gmail.com>
2016-11-11 15:29:00 +0530

    Allow underscore in database_name [redshift] (#10019)
    * allow underscore in database_name [redshift]
    
    Fixes #10009
    
    * Added Test Cases To Validate Redshift DBName
    
    * Remove Old Test Cases Regarding Redshift DBName Validation
    
    * Added More Test Cases For Redshift DBName

--- builtin/providers/aws/resource_aws_redshift_cluster_test.go
+++ builtin/providers/aws/resource_aws_redshift_cluster_test.go
@@ -13,6 +13,42 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+func TestValidateRedshiftClusterDbName(t *testing.T) {
+	validNames := []string{
+		"testdbname",
+		"test_dbname",
+		"testdbname123",
+		"TestDBname",
+		"testdbname$hashicorp",
+		"_dbname",
+	}
+	for _, v := range validNames {
+		_, errors := validateRedshiftClusterDbName(v, "name")
+		if len(errors) != 0 {
+			t.Fatalf("%q should be a valid Redshift DBName: %q", v, errors)
+		}
+	}
+
+	invalidNames := []string{
+		"!",
+		"/",
+		" ",
+		":",
+		";",
+		"test name",
+		"/slash-at-the-beginning",
+		"slash-at-the-end/",
+		"",
+		randomString(100),
+	}
+	for _, v := range invalidNames {
+		_, errors := validateRedshiftClusterDbName(v, "name")
+		if len(errors) == 0 {
+			t.Fatalf("%q should be an invalid Redshift DBName", v)
+		}
+	}
+}
+
 func TestAccAWSRedshiftCluster_basic(t *testing.T) {
 	var v redshift.Cluster
 
@@ -336,42 +372,6 @@ func TestResourceAWSRedshiftClusterIdentifierValidation(t *testing.T) {
 
 		if len(errors) != tc.ErrCount {
 			t.Fatalf("Expected the Redshift Cluster cluster_identifier to trigger a validation error")
-		}
-	}
-}
-
-func TestResourceAWSRedshiftClusterDbNameValidation(t *testing.T) {
-	cases := []struct {
-		Value    string
-		ErrCount int
-	}{
-		{
-			Value:    "tEsting",
-			ErrCount: 1,
-		},
-		{
-			Value:    "testing1",
-			ErrCount: 0,
-		},
-		{
-			Value:    "testing-",
-			ErrCount: 1,
-		},
-		{
-			Value:    "",
-			ErrCount: 2,
-		},
-		{
-			Value:    randomString(65),
-			ErrCount: 1,
-		},
-	}
-
-	for _, tc := range cases {
-		_, errors := validateRedshiftClusterDbName(tc.Value, "aws_redshift_cluster_database_name")
-
-		if len(errors) != tc.ErrCount {
-			t.Fatalf("Expected the Redshift Cluster database_name to trigger a validation error")
 		}
 	}
 }
