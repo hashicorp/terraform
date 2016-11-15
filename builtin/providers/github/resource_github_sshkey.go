b22ff3f6863bf28ec875f4abfd8647adc1728820
mehmetalisavas <mehmetali.savas@yandex.com>
2016-11-15 17:32:25 +0300

    github/sshkey: add sshkey creation and deletion operations

---
+++ builtin/providers/github/resource_github_sshkey.go
@@ -0,0 +1,96 @@
+package github
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceGithubRepositorySSHKey() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGithubRepositorySSHKeyCreate,
+		Read:   resourceGithubRepositorySSHKeyRead,
+		Delete: resourceGithubRepositorySSHKeyDelete,
+
+		Schema: map[string]*schema.Schema{
+			// owner specifies the owner of the repository
+			"title": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"sshkey": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+// isErr422ValidationFailed return true if error contains the string:
+// '422 Validation Failed'. This error is special cased so we can ignore it on
+// when it occurs during rebuilding of stack template.
+func isErr422ValidationFailed(err error) bool {
+	return err != nil && strings.Contains(err.Error(), "422 Validation Failed")
+}
+
+func resourceGithubRepositorySSHKeyCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Organization).client
+	title := d.Get("title").(string)
+	keySSH := d.Get("sshkey").(string)
+
+	key := &github.Key{
+		Title: &title,
+		Key:   &keySSH,
+	}
+
+	// CreateKey creates a public key. Requires that you are authenticated via Basic Auth,
+	// or OAuth with at least `write:public_key` scope.
+	//
+	// If SSH key is already set up, when u try to add same SSHKEY then
+	//you are gonna get 422: Validation error.
+	responseKey, _, err := client.Users.CreateKey(key)
+	if err != nil && !isErr422ValidationFailed(err) {
+		return err
+	}
+
+	d.SetId(strconv.Itoa(*responseKey.ID))
+
+	return resourceGithubRepositorySSHKeyRead(d, meta)
+}
+
+func resourceGithubRepositorySSHKeyRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Organization).client
+	id := d.Id()
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+
+	key, _, err := client.Users.GetKey(i)
+	if err != nil || key == nil {
+		d.SetId("")
+		return nil
+	}
+
+	return nil
+}
+
+func resourceGithubRepositorySSHKeyDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Organization).client
+	id := d.Id()
+	if id == "" {
+		return nil
+	}
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Users.DeleteKey(i)
+
+	return err
+}
---
+++ builtin/providers/github/resource_github_sshkey_test.go
@@ -0,0 +1,111 @@
+package github
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const (
+	testTitleSSHKey = "sshkey title"
+)
+
+func TestAccGithubRepositorySSHKey_basic(t *testing.T) {
+	testSSHKey := os.Getenv("GITHUB_TEST_SSHKEY")
+	testAccGithubRepositorySSHKeyConfig := fmt.Sprintf(`
+		resource "github_repository_sshkey" "test_repo_sshkey" {
+			title = "%s"
+			sshkey = "%s"
+		}
+	`, testTitleSSHKey, testSSHKey)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGithubRepositorySSHKeyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccGithubRepositorySSHKeyConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGithubRepositorySSHKeyExists("github_repository_sshkey.test_repo_sshkey"),
+					testAccCheckGithubRepositorySSHKeyPermission("github_repository_sshkey.test_repo_sshkey"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGithubRepositorySSHKeyDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*Organization).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "github_repository_sshkey" {
+			continue
+		}
+
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		key, _, err := conn.Users.GetKey(id)
+		if err != nil {
+			return err
+		}
+
+		if key != nil {
+			return fmt.Errorf("Ssh key still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckGithubRepositorySSHKeyExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not Found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("ID is not set")
+		}
+
+		conn := testAccProvider.Meta().(*Organization).client
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		key, _, err := conn.Users.GetKey(id)
+		if err != nil {
+			return err
+		}
+
+		if key == nil {
+			return fmt.Errorf("ssh key does not exist")
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckGithubRepositorySSHKeyPermission(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		return nil
+	}
+}
+
+var testSSHKey = `
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCCXIfYjGfxeB1XGMD8rtcC8BKl
+s/PScavn2szY04//P0hD+hnrsyFreixT7GwG3AOac6qR7rUr6/w7Pe/bypBUXWNp
+Ff4bqLrHezaVbDnMiSMA2m62/o4nX5cHoRqQyIObKfRu8tPwdHKO1pFuZE32Bgyq
+qHmOHG4aIsOqSHZfVwIDAQAB
+-----END PUBLIC KEY-----
+`
