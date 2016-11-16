19103b9fe3ec6f846da70e59f57c4db45bb53fec
Mike Ball <mikedball@gmail.com>
2016-11-16 14:10:39 -0500

    add GTM property resource

---
+++ builtin/providers/akamai/resource_gtm_property.go
@@ -0,0 +1,343 @@
+package akamai
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Comcast/go-edgegrid/edgegrid"
+	"github.com/hashicorp/terraform/helper/hashcode"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAkamaiGtmProperty() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGTMPropertyCreate,
+		Read:   resourceGTMPropertyRead,
+		Update: resourceGTMPropertyUpdate,
+		Delete: resourceGTMPropertyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"balance_by_download_score": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"domain": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"cname": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"dynamic_ttl": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"failover_delay": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"failback_delay": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"handout_mode": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"health_max": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"health_multiplier": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"health_threshold": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"load_imbalance_percentage": &schema.Schema{
+				Type:     schema.TypeFloat,
+				Optional: true,
+			},
+			"ipv6": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"score_aggregation_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"static_ttl": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"stickiness_bonus_percentage": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"stickiness_bonus_constant": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"use_computed_targets": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"liveness_test": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"http_error_3xx": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"http_error_4xx": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"http_error_5xx": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"test_interval": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"test_object": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"test_object_port": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"test_object_protocol": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"test_object_username": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"test_object_password": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"test_timeout": &schema.Schema{
+							Type:     schema.TypeFloat,
+							Optional: true,
+						},
+						"disable_nonstandard_port_warning": &schema.Schema{
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"request_string": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"response_string": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"ssl_client_private_key": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"ssl_certificate": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"host_header": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"traffic_target": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"datacenter_id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+						"enabled": &schema.Schema{
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+						"weight": &schema.Schema{
+							Type:     schema.TypeFloat,
+							Required: true,
+						},
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"servers": &schema.Schema{
+							Type:     schema.TypeSet,
+							Required: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set: func(v interface{}) int {
+								return hashcode.String(v.(string))
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceGTMPropertyCreate(d *schema.ResourceData, meta interface{}) error {
+	name := d.Get("name").(string)
+	property := property(d)
+	log.Printf("[INFO] Creating GTM property: %s", name)
+	created, err := meta.(*Clients).GTM.PropertyCreate(d.Get("domain").(string), property)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(created.Property.Name)
+
+	err = resourceGTMWaitUntilDeployed(d, meta)
+	if err != nil {
+		return err
+	}
+
+	return resourceGTMPropertyRead(d, meta)
+}
+
+func resourceGTMPropertyRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] Reading GTM property: %s", d.Id())
+	prop, err := meta.(*Clients).GTM.Property(d.Get("domain").(string), d.Get("name").(string))
+	if err != nil {
+		return err
+	}
+
+	d.Set("name", prop.Name)
+
+	return nil
+}
+
+func resourceGTMPropertyUpdate(d *schema.ResourceData, meta interface{}) error {
+	name := d.Get("name").(string)
+	log.Printf("[INFO] Updating GTM property: %s", name)
+	updated, err := meta.(*AkamaiClients).GTM.PropertyCreate(d.Get("domain").(string), property(d))
+	if err != nil {
+		return err
+	}
+
+	d.SetId(updated.Property.Name)
+
+	err = resourceGTMWaitUntilDeployed(d, meta)
+	if err != nil {
+		return err
+	}
+
+	return resourceGTMPropertyRead(d, meta)
+}
+
+func resourceGtmPropertyDelete(d *schema.ResourceData, meta interface{}) error {
+	name := d.Get("name").(string)
+	log.Printf("[INFO] Deleting property: %s", name)
+	_, err := meta.(*Clients).GTM.PropertyDelete(d.Get("domain").(string), name)
+	if err != nil {
+		return nil
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func property(d *schema.ResourceData) *edgegrid.Property {
+	return &edgegrid.Property{
+		Cname:                     d.Get("cname").(string),
+		Name:                      d.Get("name").(string),
+		Type:                      d.Get("type").(string),
+		Ipv6:                      d.Get("ipv6").(bool),
+		DynamicTTL:                d.Get("dynamic_ttl").(int),
+		StaticTTL:                 d.Get("static_ttl").(int),
+		HandoutMode:               d.Get("handout_mode").(string),
+		FailbackDelay:             d.Get("failback_delay").(int),
+		FailoverDelay:             d.Get("failover_delay").(int),
+		ScoreAggregationType:      d.Get("score_aggregation_type").(string),
+		LoadImbalancePercentage:   d.Get("load_imbalance_percentage").(float64),
+		StickinessBonusPercentage: d.Get("stickiness_bonus_percentage").(int),
+		TrafficTargets:            trafficTargets(d),
+		LivenessTests:             livenessTests(d),
+	}
+}
+
+func trafficTargets(d *schema.ResourceData) []edgegrid.TrafficTarget {
+	targets := []edgegrid.TrafficTarget{}
+	targetsCount := d.Get("traffic_target.#").(int)
+
+	for i := 0; i < targetsCount; i++ {
+		prefix := fmt.Sprintf("traffic_target.%d", i)
+
+		targets = append(targets, edgegrid.TrafficTarget{
+			Name:         d.Get(prefix + ".name").(string),
+			Weight:       d.Get(prefix + ".weight").(float64),
+			Enabled:      d.Get(prefix + ".enabled").(bool),
+			Servers:      stringSetToStringSlice(d.Get(prefix + ".servers").(*schema.Set)),
+			DataCenterID: d.Get(prefix + ".datacenter_id").(int),
+		})
+	}
+
+	return targets
+}
+
+func livenessTests(d *schema.ResourceData) []edgegrid.LivenessTest {
+	tests := []edgegrid.LivenessTest{}
+	testsCount := d.Get("liveness_test.#").(int)
+
+	for i := 0; i < testsCount; i++ {
+		prefix := fmt.Sprintf("liveness_test.%d", i)
+
+		tests = append(tests, edgegrid.LivenessTest{
+			Name:                          d.Get(prefix + ".name").(string),
+			TestInterval:                  int64(d.Get(prefix + ".test_interval").(int)),
+			HTTPError3xx:                  d.Get(prefix + ".http_error_3xx").(bool),
+			HTTPError4xx:                  d.Get(prefix + ".http_error_4xx").(bool),
+			HTTPError5xx:                  d.Get(prefix + ".http_error_5xx").(bool),
+			TestObjectPort:                int64(d.Get(prefix + ".test_object_port").(int)),
+			TestTimeout:                   d.Get(prefix + ".test_timeout").(float64),
+			TestObject:                    d.Get(prefix + ".test_object").(string),
+			TestObjectProtocol:            d.Get(prefix + ".test_object_protocol").(string),
+			DisableNonstandardPortWarning: d.Get(prefix + ".disable_nonstandard_port_warning").(bool),
+		})
+	}
+
+	return tests
+}
+
+func getServers(prefix string, d *schema.ResourceData) []string {
+	servers := []string{}
+	serversPrefix := d.Get(prefix + ".servers")
+	serversCount := d.Get(fmt.Sprintf("%d.#", serversPrefix)).(int)
+
+	for i := 0; i < serversCount; i++ {
+		serverPrefix := fmt.Sprintf("%d.%s", serversPrefix, i)
+		servers = append(servers, d.Get(serverPrefix).(string))
+	}
+
+	return servers
+}
---
+++ builtin/providers/akamai/resource_gtm_property_test.go
@@ -0,0 +1,161 @@
+package akamai
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAkamaiGTMPropertyBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccAkamaiGTMPropertyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckAkamaiGTMPropertyConfigBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAkamaiGTMPropertyExists("akamai_gtm_property.test_property"),
+					resource.TestCheckResourceAttr("akamai_gtm_property.test_property", "domain", "terraform-test.akadns.net"),
+					resource.TestCheckResourceAttr("akamai_gtm_property.test_property", "name", "test_property"),
+					resource.TestCheckResourceAttr("akamai_gtm_property.test_property", "cname", "example.com"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAkamaiGTMPropertyDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*Clients).GTM
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "akamai_gtm_property" {
+			continue
+		}
+		name := rs.Primary.ID
+
+		// Try to find the property
+		_, err := client.Property("terraform-test.akadns.net", name)
+		if err == nil {
+			fmt.Errorf("Property still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckAkamaiGTMPropertyExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found %s", rs)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		client := testAccProvider.Meta().(*Clients).GTM
+
+		readProp, err := client.Property("terraform-test.akadns.net", rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if readProp.Name != rs.Primary.ID {
+			return fmt.Errorf("Record not found")
+		}
+
+		return nil
+	}
+}
+
+const testAccCheckAkamaiGTMPropertyConfigBasic = `
+resource "akamai_gtm_domain" "property_test_domain" {
+	name = "terraform-test.akadns.net"
+	type = "basic"
+}
+
+resource "akamai_gtm_datacenter" "property_test_dc1" {
+	name = "property_test_dc1"
+	domain = "${akamai_gtm_domain.property_test_domain.name}"
+	country = "GB"
+	continent = "EU"
+	city = "Downpatrick"
+	longitude = -5.582
+	latitude = 54.367
+	depends_on = [
+		"akamai_gtm_domain.property_test_domain"
+	]
+}
+
+resource "akamai_gtm_datacenter" "property_test_dc2" {
+	name = "property_test_dc2"
+	domain = "${akamai_gtm_domain.property_test_domain.name}"
+	country = "IS"
+	continent = "EU"
+	city = "Snæfellsjökull"
+	longitude = -23.776
+	latitude = 64.808
+	depends_on = [
+		"akamai_gtm_datacenter.property_test_dc1"
+	]
+}
+
+resource "akamai_gtm_property" "test_property" {
+	cname = "example.com"
+	domain = "${akamai_gtm_domain.property_test_domain.name}"
+	type = "weighted-round-robin"
+	name = "test_property"
+	balanceByDownloadScore = false
+	dynamicTTL = 300
+	failoverDelay = 0
+	failbackDelay = 0
+	handoutMode = "normal"
+	healthThreshold = 0
+	healthMax = 0
+	healthMultiplier = 0
+	loadImbalancePercentage = 10
+	ipv6 = false
+	scoreAggregationType = "mean"
+	staticTTL = 600
+	stickinessBonusPercentage = 50
+	stickinessBonusConstant = 0
+	useComputedTargets = false
+  livenessTest {
+    name = "terraform-provider-akamai automated acceptance tests"
+    testObject = "/status"
+    testObjectProtocol = "HTTP"
+    testInterval = 60
+    disableNonstandardPortWarning = false
+    httpError4xx = true
+    httpError3xx = true
+    httpError5xx = true
+    testObjectPort = 80
+    testTimeout = 25
+  }
+	trafficTarget {
+		enabled = true
+		dataCenterId = "${akamai_gtm_datacenter.property_test_dc1.id}"
+		weight = 50.0
+		name = "${akamai_gtm_datacenter.property_test_dc1.name}"
+		servers = [
+			"1.2.3.4",
+			"1.2.3.5"
+		]
+	}
+	trafficTarget {
+		enabled = true
+		dataCenterId = "${akamai_gtm_datacenter.property_test_dc2.id}"
+		weight = 50.0
+		name = "${akamai_gtm_datacenter.property_test_dc2.name}"
+		servers = [
+			"1.2.3.6",
+			"1.2.3.7"
+		]
+	}
+}
+`
