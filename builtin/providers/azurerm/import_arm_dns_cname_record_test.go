02c51cd4df239896ccc35e1152d1c6ed993fb263
Peter McAtominey <peter.mcatominey@bedegaming.com>
2016-11-18 00:32:51 +0000

    provider/azurerm: support import of dns record resources
    TF_ACC=1 go test ./builtin/providers/azurerm -v -run TestAccAzureRMDns[A-z]+Record_importBasic -timeout 120m
    === RUN   TestAccAzureRMDnsARecord_importBasic
    --- PASS: TestAccAzureRMDnsARecord_importBasic (102.84s)
    === RUN   TestAccAzureRMDnsAAAARecord_importBasic
    --- PASS: TestAccAzureRMDnsAAAARecord_importBasic (100.59s)
    === RUN   TestAccAzureRMDnsCNameRecord_importBasic
    --- PASS: TestAccAzureRMDnsCNameRecord_importBasic (98.94s)
    === RUN   TestAccAzureRMDnsMxRecord_importBasic
    --- PASS: TestAccAzureRMDnsMxRecord_importBasic (107.30s)
    === RUN   TestAccAzureRMDnsNsRecord_importBasic
    --- PASS: TestAccAzureRMDnsNsRecord_importBasic (98.55s)
    === RUN   TestAccAzureRMDnsSrvRecord_importBasic
    --- PASS: TestAccAzureRMDnsSrvRecord_importBasic (100.19s)
    === RUN   TestAccAzureRMDnsTxtRecord_importBasic
    --- PASS: TestAccAzureRMDnsTxtRecord_importBasic (97.49s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/azurerm	706.000s

---
+++ builtin/providers/azurerm/import_arm_dns_cname_record_test.go
@@ -0,0 +1,33 @@
+package azurerm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAzureRMDnsCNameRecord_importBasic(t *testing.T) {
+	resourceName := "azurerm_dns_cname_record.test"
+
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMDnsCNameRecord_basic, ri, ri, ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDnsCNameRecordDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: config,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
