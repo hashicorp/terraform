d23608620a2086a66e70c270004ccba588261cbb
Paul Tyng <ptyng@underarmour.com>
2016-11-23 12:28:30 -0500

    Add initial provider and alert channel resource

---
+++ builtin/providers/newrelic/provider_test.go
@@ -0,0 +1,36 @@
+package newrelic
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"newrelic": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProviderImpl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("NEWRELIC_API_KEY"); v == "" {
+		t.Log(v)
+		t.Fatal("NEWRELIC_API_KEY must be set for acceptance tests")
+	}
+}
