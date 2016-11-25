e0b94e4dc4f004c270da94792b774af57953ef5a
Paul Stack <public@paulstack.co.uk>
2016-11-25 15:55:00 +0000

    provider/azurerm: Removing the commented out acceptance tests as they were killing the test runner (#10368)

--- builtin/providers/azurerm/resource_arm_network_interface_card_test.go
+++ builtin/providers/azurerm/resource_arm_network_interface_card_test.go
@@ -98,35 +98,6 @@ func TestAccAzureRMNetworkInterface_withTags(t *testing.T) {
 	})
 }
 
-///TODO: Re-enable this test when https://github.com/Azure/azure-sdk-for-go/issues/259 is fixed
-//func TestAccAzureRMNetworkInterface_addingIpConfigurations(t *testing.T) {
-//
-//	resource.Test(t, resource.TestCase{
-//		PreCheck:     func() { testAccPreCheck(t) },
-//		Providers:    testAccProviders,
-//		CheckDestroy: testCheckAzureRMNetworkInterfaceDestroy,
-//		Steps: []resource.TestStep{
-//			resource.TestStep{
-//				Config: testAccAzureRMNetworkInterface_basic,
-//				Check: resource.ComposeTestCheckFunc(
-//					testCheckAzureRMNetworkInterfaceExists("azurerm_network_interface.test"),
-//					resource.TestCheckResourceAttr(
-//						"azurerm_network_interface.test", "ip_configuration.#", "1"),
-//				),
-//			},
-//
-//			resource.TestStep{
-//				Config: testAccAzureRMNetworkInterface_extraIpConfiguration,
-//				Check: resource.ComposeTestCheckFunc(
-//					testCheckAzureRMNetworkInterfaceExists("azurerm_network_interface.test"),
-//					resource.TestCheckResourceAttr(
-//						"azurerm_network_interface.test", "ip_configuration.#", "2"),
-//				),
-//			},
-//		},
-//	})
-//}
-
 func testCheckAzureRMNetworkInterfaceExists(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		// Ensure we have enough information in state to look up in API
@@ -355,51 +326,3 @@ resource "azurerm_network_interface" "test" {
 }
 `, rInt)
 }
-
-//TODO: Re-enable this test when https://github.com/Azure/azure-sdk-for-go/issues/259 is fixed
-//var testAccAzureRMNetworkInterface_extraIpConfiguration = `
-//resource "azurerm_resource_group" "test" {
-//    name = "acceptanceTestResourceGroup1"
-//    location = "West US"
-//}
-//
-//resource "azurerm_virtual_network" "test" {
-//    name = "acceptanceTestVirtualNetwork1"
-//    address_space = ["10.0.0.0/16"]
-//    location = "West US"
-//    resource_group_name = "${azurerm_resource_group.test.name}"
-//}
-//
-//resource "azurerm_subnet" "test" {
-//    name = "testsubnet"
-//    resource_group_name = "${azurerm_resource_group.test.name}"
-//    virtual_network_name = "${azurerm_virtual_network.test.name}"
-//    address_prefix = "10.0.2.0/24"
-//}
-//
-//resource "azurerm_subnet" "test1" {
-//    name = "testsubnet1"
-//    resource_group_name = "${azurerm_resource_group.test.name}"
-//    virtual_network_name = "${azurerm_virtual_network.test.name}"
-//    address_prefix = "10.0.1.0/24"
-//}
-//
-//resource "azurerm_network_interface" "test" {
-//    name = "acceptanceTestNetworkInterface1"
-//    location = "West US"
-//    resource_group_name = "${azurerm_resource_group.test.name}"
-//
-//    ip_configuration {
-//    	name = "testconfiguration1"
-//    	subnet_id = "${azurerm_subnet.test.id}"
-//    	private_ip_address_allocation = "dynamic"
-//    }
-//
-//    ip_configuration {
-//    	name = "testconfiguration2"
-//    	subnet_id = "${azurerm_subnet.test1.id}"
-//    	private_ip_address_allocation = "dynamic"
-//    	primary = true
-//    }
-//}
-//`
