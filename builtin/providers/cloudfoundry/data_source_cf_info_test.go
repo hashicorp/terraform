4bdd76cf3a842856bfc5a3ca56dc1e949c9239cf
Mevan Samaratunga <mevansam@gmail.com>
2016-11-27 17:15:15 +0400

    terraform provider for cloudfoundry - mvp implementation

---
+++ builtin/providers/cloudfoundry/data_source_cf_info_test.go
@@ -0,0 +1,50 @@
+package cloudfoundry
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+const infoDataResource = `
+
+data "cf_info" "info" {}
+`
+
+func TestAccDataSourceInfo_normal(t *testing.T) {
+
+	_, filename, _, _ := runtime.Caller(0)
+	ut := os.Getenv("UNIT_TEST")
+	if !testAccEnvironmentSet() || (ut != "" && ut != filepath.Base(filename)) {
+		fmt.Printf("Skipping tests in '%s'.\n", filepath.Base(filename))
+		return
+	}
+
+	ref := "data.cf_info.info"
+
+	resource.Test(t,
+		resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+
+				resource.TestStep{
+					Config: infoDataResource,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr(
+							ref, "auth_endpoint", "https://login.local.pcfdev.io"),
+						resource.TestCheckResourceAttr(
+							ref, "uaa_endpoint", "https://uaa.local.pcfdev.io"),
+						resource.TestCheckResourceAttr(
+							ref, "logging_endpoint", "wss://loggregator.local.pcfdev.io:443"),
+						resource.TestCheckResourceAttr(
+							ref, "doppler_endpoint", "wss://doppler.local.pcfdev.io:443"),
+					),
+				},
+			},
+		})
+}
