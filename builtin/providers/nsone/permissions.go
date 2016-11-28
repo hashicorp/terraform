e64ad0f28247743dee08638a9c269bc909afad8b
Pasha Palangpour <ppalangpour@nsone.net>
2016-11-27 21:42:00 -0500

    NS1: Move remaining permissions logic to permissions.go

--- builtin/providers/nsone/permissions.go
+++ builtin/providers/nsone/permissions.go
@@ -108,3 +108,77 @@ func permissionsToResourceData(d *schema.ResourceData, permissions account.Permi
 	d.Set("monitoring_manage_jobs", permissions.Monitoring.ManageJobs)
 	d.Set("monitoring_view_jobs", permissions.Monitoring.ViewJobs)
 }
+
+func resourceDataToPermissions(d *schema.ResourceData) account.PermissionsMap {
+	var p account.PermissionsMap
+	if v, ok := d.GetOk("dns_view_zones"); ok {
+		p.DNS.ViewZones = v.(bool)
+	}
+	if v, ok := d.GetOk("dns_manage_zones"); ok {
+		p.DNS.ManageZones = v.(bool)
+	}
+	if v, ok := d.GetOk("dns_zones_allow_by_default"); ok {
+		p.DNS.ZonesAllowByDefault = v.(bool)
+	}
+	if v, ok := d.GetOk("dns_zones_deny"); ok {
+		denyRaw := v.([]interface{})
+		p.DNS.ZonesDeny = make([]string, len(denyRaw))
+		for i, deny := range denyRaw {
+			p.DNS.ZonesDeny[i] = deny.(string)
+		}
+	} else {
+		p.DNS.ZonesDeny = make([]string, 0)
+	}
+	if v, ok := d.GetOk("dns_zones_allow"); ok {
+		allowRaw := v.([]interface{})
+		p.DNS.ZonesAllow = make([]string, len(allowRaw))
+		for i, allow := range allowRaw {
+			p.DNS.ZonesAllow[i] = allow.(string)
+		}
+	} else {
+		p.DNS.ZonesAllow = make([]string, 0)
+	}
+	if v, ok := d.GetOk("data_push_to_datafeeds"); ok {
+		p.Data.PushToDatafeeds = v.(bool)
+	}
+	if v, ok := d.GetOk("data_manage_datasources"); ok {
+		p.Data.ManageDatasources = v.(bool)
+	}
+	if v, ok := d.GetOk("data_manage_datafeeds"); ok {
+		p.Data.ManageDatafeeds = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_users"); ok {
+		p.Account.ManageUsers = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_payment_methods"); ok {
+		p.Account.ManagePaymentMethods = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_plan"); ok {
+		p.Account.ManagePlan = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_teams"); ok {
+		p.Account.ManageTeams = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_apikeys"); ok {
+		p.Account.ManageApikeys = v.(bool)
+	}
+	if v, ok := d.GetOk("account_manage_account_settings"); ok {
+		p.Account.ManageAccountSettings = v.(bool)
+	}
+	if v, ok := d.GetOk("account_view_activity_log"); ok {
+		p.Account.ViewActivityLog = v.(bool)
+	}
+	if v, ok := d.GetOk("account_view_invoices"); ok {
+		p.Account.ViewInvoices = v.(bool)
+	}
+	if v, ok := d.GetOk("monitoring_manage_lists"); ok {
+		p.Monitoring.ManageLists = v.(bool)
+	}
+	if v, ok := d.GetOk("monitoring_manage_jobs"); ok {
+		p.Monitoring.ManageJobs = v.(bool)
+	}
+	if v, ok := d.GetOk("monitoring_view_jobs"); ok {
+		p.Monitoring.ViewJobs = v.(bool)
+	}
+	return p
+}
