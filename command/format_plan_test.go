62b71f7e537c06975ba66db907d9ddd419200fc3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-11-28 13:49:10 -0800

    command: plan formatting for deposed destroy

--- command/format_plan_test.go
+++ command/format_plan_test.go
@@ -9,6 +9,41 @@ import (
 )
 
 // Test that a root level data source gets a special plan output on create
+func TestFormatPlan_destroyDeposed(t *testing.T) {
+	plan := &terraform.Plan{
+		Diff: &terraform.Diff{
+			Modules: []*terraform.ModuleDiff{
+				&terraform.ModuleDiff{
+					Path: []string{"root"},
+					Resources: map[string]*terraform.InstanceDiff{
+						"aws_instance.foo": &terraform.InstanceDiff{
+							DestroyDeposed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+	opts := &FormatPlanOpts{
+		Plan: plan,
+		Color: &colorstring.Colorize{
+			Colors:  colorstring.DefaultColors,
+			Disable: true,
+		},
+		ModuleDepth: 1,
+	}
+
+	actual := FormatPlan(opts)
+
+	expected := strings.TrimSpace(`
+- aws_instance.foo (deposed)
+	`)
+	if actual != expected {
+		t.Fatalf("expected:\n\n%s\n\ngot:\n\n%s", expected, actual)
+	}
+}
+
+// Test that a root level data source gets a special plan output on create
 func TestFormatPlan_rootDataSource(t *testing.T) {
 	plan := &terraform.Plan{
 		Diff: &terraform.Diff{
