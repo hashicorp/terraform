857d446fd6910864c7e5f7467f140e5e094cbc51
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-12-01 08:47:27 -0500

    Remove test files that snuck in here

--- test/foo/main.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "foo" {
-  count = 2
-
-  provisioner "local-exec" { command = "sleep ${count.index*3}" }
-
-  //provisioner "local-exec" { command = "exit 1" }
-
-  lifecycle { create_before_destroy = true }
-}
