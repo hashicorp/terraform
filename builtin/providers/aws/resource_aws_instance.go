97b0b70e1f22a0c7d223057e2d53f90800e6a78f
Hunter Morgan <hunter.morgan@capitalone.com>
2016-12-04 22:10:53 -0500

    add NetworkInterfaceId to opts

--- builtin/providers/aws/resource_aws_instance.go
+++ builtin/providers/aws/resource_aws_instance.go
@@ -1024,6 +1024,7 @@ func buildAwsInstanceOpts(
 		EBSOptimized:          aws.Bool(d.Get("ebs_optimized").(bool)),
 		ImageID:               aws.String(d.Get("ami").(string)),
 		InstanceType:          aws.String(d.Get("instance_type").(string)),
+		NetworkInterfaceId:    aws.String(d.Get("network_interface_id").(string)),
 	}
 
 	if v := d.Get("instance_initiated_shutdown_behavior").(string); v != "" {
