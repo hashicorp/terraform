4679001b8acaeda4ac304c4050ae59d68c76fb97
Jan Schumann <js@schumann-it.com>
2016-12-05 15:21:20 +0100

    add test that forces new resource

--- builtin/providers/aws/resource_aws_opsworks_rds_db_instance_test.go
+++ builtin/providers/aws/resource_aws_opsworks_rds_db_instance_test.go
@@ -38,6 +38,14 @@ func TestAccAWSOpsworksRdsDbInstance(t *testing.T) {
 					),
 				),
 			},
+			resource.TestStep{
+				Config: testAccAwsOpsworksRdsDbInstanceForceNew(sName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"aws_opsworks_rds_db_instance.tf-acc-opsworks-db", "db_user", "foo",
+					),
+				),
+			},
 		},
 	})
 }
@@ -48,12 +56,37 @@ resource "aws_opsworks_rds_db_instance" "tf-acc-opsworks-db" {
   stack_id = "${aws_opsworks_stack.tf-acc.id}"
 
   rds_db_instance_arn = "${aws_db_instance.bar.arn}"
-  db_user = "%s"
-  db_password = "%s"
+  db_user             = "%s"
+  db_password         = "%s"
 }
 
 %s
 
 %s
 `, userName, password, testAccAwsOpsworksStackConfigVpcCreate(name), testAccAWSDBInstanceConfig)
+}
+
+func testAccAwsOpsworksRdsDbInstanceForceNew(name string) string {
+	return fmt.Sprintf(`
+resource "aws_opsworks_rds_db_instance" "tf-acc-opsworks-db" {
+  stack_id = "${aws_opsworks_stack.tf-acc.id}"
+
+  rds_db_instance_arn = "${aws_db_instance.foo.arn}"
+  db_user             = "foo"
+  db_password         = "foofoofoofoo"
+}
+
+%s
+
+resource "aws_db_instance" "foo" {
+  allocated_storage    = 10
+  engine               = "MySQL"
+  engine_version       = "5.6.21"
+  instance_class       = "db.t1.micro"
+  name                 = "baz"
+  password             = "foofoofoofoo"
+  username             = "foo"
+  parameter_group_name = "default.mysql5.6"
+}
+`, testAccAwsOpsworksStackConfigVpcCreate(name))
 }
