b0b17afd1708d98762c16c19ba06e490f24f4862
Ninir <gauthier.wallet@gmail.com>
2016-12-11 22:50:55 +0100

    provider/aws: Added Lambda function guard when needed attributes are not set

--- builtin/providers/aws/resource_aws_lambda_function_test.go
+++ builtin/providers/aws/resource_aws_lambda_function_test.go
@@ -39,6 +39,22 @@ func TestAccAWSLambdaFunction_basic(t *testing.T) {
 	})
 }
 
+func TestAccAWSLambdaFunction_expectFilenameAndS3Attributes(t *testing.T) {
+	rName := fmt.Sprintf("tf_test_%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLambdaFunctionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccAWSLambdaConfigWithoutFilenameAndS3Attributes(rName),
+				ExpectError: regexp.MustCompile(`filename or s3_\* attributes must be set`),
+			},
+		},
+	})
+}
+
 func TestAccAWSLambdaFunction_envVariables(t *testing.T) {
 	var conf lambda.GetFunctionOutput
 
@@ -618,6 +634,16 @@ func testAccAWSLambdaConfigBasic(rName string) string {
 	return fmt.Sprintf(baseAccAWSLambdaConfig+`
 resource "aws_lambda_function" "lambda_function_test" {
     filename = "test-fixtures/lambdatest.zip"
+    function_name = "%s"
+    role = "${aws_iam_role.iam_for_lambda.arn}"
+    handler = "exports.example"
+}
+`, rName)
+}
+
+func testAccAWSLambdaConfigWithoutFilenameAndS3Attributes(rName string) string {
+	return fmt.Sprintf(baseAccAWSLambdaConfig+`
+resource "aws_lambda_function" "lambda_function_test" {
     function_name = "%s"
     role = "${aws_iam_role.iam_for_lambda.arn}"
     handler = "exports.example"
