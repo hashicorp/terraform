6ccd70f959c114803f594990cca22f73e401c7d5
Sean Chittenden <sean@chittenden.org>
2016-12-12 01:36:14 -0800

    Nuke some whitespace.

--- builtin/providers/postgresql/resource_postgresql_extension_test.go
+++ builtin/providers/postgresql/resource_postgresql_extension_test.go
@@ -10,7 +10,6 @@ import (
 )
 
 func TestAccPostgresqlExtension_Basic(t *testing.T) {
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
