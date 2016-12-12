9f24c5921e84a52831632e02c3aef60f272871bb
tstraub-rms <tstraub@rms.com>
2016-12-12 16:07:59 -0500

    Only set the LicenseType property if the "license_type" string is not empty.

--- builtin/providers/azurerm/resource_arm_virtual_machine.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine.go
@@ -488,14 +488,16 @@ func resourceArmVirtualMachineCreate(d *schema.ResourceData, meta interface{}) e
 
 	networkProfile := expandAzureRmVirtualMachineNetworkProfile(d)
 	vmSize := d.Get("vm_size").(string)
-	licenseType := d.Get("license_type").(string)
 	properties := compute.VirtualMachineProperties{
 		NetworkProfile: &networkProfile,
 		HardwareProfile: &compute.HardwareProfile{
 			VMSize: compute.VirtualMachineSizeTypes(vmSize),
 		},
 		StorageProfile: &storageProfile,
-		LicenseType:    &licenseType,
+	}
+	licenseType := d.Get("license_type").(string)
+	if licenseType != "" {
+		properties.LicenseType = &licenseType
 	}
 
 	if _, ok := d.GetOk("boot_diagnostics"); ok {
