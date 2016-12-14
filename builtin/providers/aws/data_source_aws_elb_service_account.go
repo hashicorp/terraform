a2e6ab2fc0194717f3e2d7e6714b78fa5b167257
stack72 <public@paulstack.co.uk>
2016-12-14 09:29:26 +0000

    provider/aws: Support eu-west-2 region

--- builtin/providers/aws/data_source_aws_elb_service_account.go
+++ builtin/providers/aws/data_source_aws_elb_service_account.go
@@ -17,6 +17,7 @@ var elbAccountIdPerRegionMap = map[string]string{
 	"cn-north-1":     "638102146993",
 	"eu-central-1":   "054676820928",
 	"eu-west-1":      "156460612806",
+	"eu-west-2":      "652711504416",
 	"sa-east-1":      "507241528517",
 	"us-east-1":      "127311923021",
 	"us-east-2":      "033677994240",
