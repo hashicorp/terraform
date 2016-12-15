a54c3478b456502c19be675c17eb58309808ebc1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-12-15 06:10:22 +0000

    v0.8.1

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
   h.name        = "terraform"
-  h.version     = "0.8.0"
+  h.version     = "0.8.1"
   h.github_slug = "hashicorp/terraform"
 end
 
