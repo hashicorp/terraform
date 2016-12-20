ad565974acd0a1100cc9f037cfd780c0f1ecfad1
Curtis Allen <curtis.allen@robotsandpencils.com>
2016-12-20 14:37:50 -0700

    fixes double base64 encode Add a utility that ensures a byte array is not doubly base64 encoded Fixes #10786

---
+++ builtin/providers/aws/utils.go
@@ -0,0 +1,19 @@
+package aws
+
+import "encoding/base64"
+
+// Base64Encode encodes data if the input isn't already encoded using base64.StdEncoding.EncodeToString.
+// If the input is already base64 encoded, return the original input unchanged.
+func base64Encode(data []byte) string {
+	// Check whether the data is already Base64 encoded; don't double-encode
+	if isBase64Encoded(data) {
+		return string(data)
+	}
+	// data has not been encoded encode and return
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func isBase64Encoded(data []byte) bool {
+	_, err := base64.StdEncoding.DecodeString(string(data))
+	return err == nil
+}
