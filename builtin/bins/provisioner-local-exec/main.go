e36171761942128120a9f017a629e6e75f04cf5b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-12-22 14:11:41 -0800

    provisioners/local-exec: switch to helper/schema

--- builtin/bins/provisioner-local-exec/main.go
+++ builtin/bins/provisioner-local-exec/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/hashicorp/terraform/builtin/provisioners/local-exec"
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/hashicorp/terraform/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProvisionerFunc: func() terraform.ResourceProvisioner {
-			return new(localexec.ResourceProvisioner)
-		},
+		ProvisionerFunc: localexec.Provisioner,
 	})
 }
