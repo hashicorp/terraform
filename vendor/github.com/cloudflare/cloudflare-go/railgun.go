325dd020657b9dcaeeebe91b9ae6941823bcbc2e
Michael Warkentin <mwarkentin@gmail.com>
2016-12-24 09:37:11 -0500

    Fix #7690: Update vendored cloudflare-go library (#10910)
    There was an upstream fix to allow updating of DNS record name fields.
    
    A few other comment updates, but that's it.

--- vendor/github.com/cloudflare/cloudflare-go/railgun.go
+++ vendor/github.com/cloudflare/cloudflare-go/railgun.go
@@ -238,7 +238,7 @@ func (api *API) ZoneRailguns(zoneID string) ([]ZoneRailgun, error) {
 	return r.Result, nil
 }
 
-// Railgun returns the configuration for a given Railgun.
+// ZoneRailgunDetails returns the configuration for a given Railgun.
 // API reference:
 // 	https://api.cloudflare.com/#railguns-for-a-zone-get-railgun-details
 // 	GET /zones/:zone_identifier/railguns/:identifier
@@ -255,7 +255,7 @@ func (api *API) ZoneRailgunDetails(zoneID, railgunID string) (ZoneRailgun, error
 	return r.Result, nil
 }
 
-// TestRailgunResponse tests a Railgun connection for a given zone.
+// TestRailgunConnection tests a Railgun connection for a given zone.
 // API reference:
 //  https://api.cloudflare.com/#railgun-connections-for-a-zone-test-railgun-connection
 //  GET /zones/:zone_identifier/railguns/:identifier/diagnose
@@ -294,7 +294,7 @@ func (api *API) connectZoneRailgun(zoneID, railgunID string, connect bool) (Zone
 	return r.Result, nil
 }
 
-// ZoneRailgun connects a Railgun for a given zone.
+// ConnectZoneRailgun connects a Railgun for a given zone.
 // API reference:
 // 	https://api.cloudflare.com/#railguns-for-a-zone-connect-or-disconnect-a-railgun
 // 	PATCH /zones/:zone_identifier/railguns/:identifier
@@ -302,7 +302,7 @@ func (api *API) ConnectZoneRailgun(zoneID, railgunID string) (ZoneRailgun, error
 	return api.connectZoneRailgun(zoneID, railgunID, true)
 }
 
-// ZoneRailgun disconnects a Railgun for a given zone.
+// DisconnectZoneRailgun disconnects a Railgun for a given zone.
 // API reference:
 //  https://api.cloudflare.com/#railguns-for-a-zone-connect-or-disconnect-a-railgun
 //  PATCH /zones/:zone_identifier/railguns/:identifier
