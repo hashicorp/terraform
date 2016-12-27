5dbc66012e1e94aca6c6a3998589aeed287b162a
Ninir <gauthier.wallet@gmail.com>
2016-12-27 21:52:22 +0100

    provider/aws: Reworked validateArn function to handle empty values (#10833)

--- builtin/providers/aws/validators_test.go
+++ builtin/providers/aws/validators_test.go
@@ -189,6 +189,12 @@ func TestValidateAwsAccountId(t *testing.T) {
 }
 
 func TestValidateArn(t *testing.T) {
+	v := ""
+	_, errors := validateArn(v, "arn")
+	if len(errors) != 0 {
+		t.Fatalf("%q should not be validated as an ARN: %q", v, errors)
+	}
+
 	validNames := []string{
 		"arn:aws:elasticbeanstalk:us-east-1:123456789012:environment/My App/MyEnvironment", // Beanstalk
 		"arn:aws:iam::123456789012:user/David",                                             // IAM User
