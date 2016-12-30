aa26a1017f0cacc105d0d39256390bef7f01a40c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2016-12-29 19:30:51 -0500

    provider/aws: Add 'aws_vpc_peering_connection' data source (#10913)
    * Add 'aws_vpc_peering_connection' data source.
    
    * Changes after code review.
    
    * Add 'accepter' and 'requester' blocks to aws_vpc_peering_connection data source output attributes.

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -171,6 +171,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_security_group":           dataSourceAwsSecurityGroup(),
 			"aws_vpc":                      dataSourceAwsVpc(),
 			"aws_vpc_endpoint_service":     dataSourceAwsVpcEndpointService(),
+			"aws_vpc_peering_connection":   dataSourceAwsVpcPeeringConnection(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
