81c8d7baddab2dac4f24dbf0250cbe706cd42659
tombuildsstuff <git@ibuildstuff.co.uk>
2017-01-03 21:30:46 +0000

    Running makefmt

--- builtin/bins/provider-opsgenie/main.go
+++ builtin/bins/provider-opsgenie/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/builtin/providers/opsgenie"
+	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
