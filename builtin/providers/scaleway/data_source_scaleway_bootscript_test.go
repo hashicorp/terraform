5dc44f611455068d0cb13cedff235ea8efb99309
Raphael Randschau <nicolai86@me.com>
2017-01-12 22:09:23 +0100

    provider/scaleway: fix bootscript tests
    the bootscript tests where failing because the referenced bootscript is no
    longer available.
    
    for now this just makes the tests pass again, next step should be to lookup a
    bootscript so we don't have to update the tests all the time

--- builtin/providers/scaleway/data_source_scaleway_bootscript_test.go
+++ builtin/providers/scaleway/data_source_scaleway_bootscript_test.go
@@ -20,7 +20,7 @@ func TestAccScalewayDataSourceBootscript_Basic(t *testing.T) {
 					testAccCheckBootscriptID("data.scaleway_bootscript.debug"),
 					resource.TestCheckResourceAttr("data.scaleway_bootscript.debug", "architecture", "x86_64"),
 					resource.TestCheckResourceAttr("data.scaleway_bootscript.debug", "public", "true"),
-					resource.TestMatchResourceAttr("data.scaleway_bootscript.debug", "kernel", regexp.MustCompile("4.8.3")),
+					resource.TestMatchResourceAttr("data.scaleway_bootscript.debug", "kernel", regexp.MustCompile("4.8.14")),
 				),
 			},
 		},
@@ -67,7 +67,7 @@ func testAccCheckBootscriptID(n string) resource.TestCheckFunc {
 
 const testAccCheckScalewayBootscriptConfig = `
 data "scaleway_bootscript" "debug" {
-  name = "x86_64 4.8.3 debug #1"
+  name = "x86_64 4.8.14 debug #2"
 }
 `
 
