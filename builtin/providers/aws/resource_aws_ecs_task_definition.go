a353a12a8245342bb1372c70f8746635a0cd003c
Paul Stack <public@paulstack.co.uk>
2017-01-17 18:43:34 +0000

    provider/aws: Add support for placement_constraint to aws_ecs_service (#11242)
    ```
    % make testacc TEST=./builtin/providers/aws TESTARGS='-run=TestAccAWSEcsServiceWithPlacementConstraints'
    ==> Checking that code complies with gofmt requirements...
    go generate $(go list ./... | grep -v /terraform/vendor/)
    2017/01/17 18:25:27 Generated command/internal_plugin_list.go
    TF_ACC=1 go test ./builtin/providers/aws -v -run=TestAccAWSEcsServiceWithPlacementConstraints -timeout 120m
    === RUN   TestAccAWSEcsServiceWithPlacementConstraints
    --- PASS: TestAccAWSEcsServiceWithPlacementConstraints (113.18s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/aws	113.208s
    ```
    
    //cc @catsby

--- builtin/providers/aws/resource_aws_ecs_task_definition.go
+++ builtin/providers/aws/resource_aws_ecs_task_definition.go
@@ -21,23 +21,23 @@ func resourceAwsEcsTaskDefinition() *schema.Resource {
 		Delete: resourceAwsEcsTaskDefinitionDelete,
 
 		Schema: map[string]*schema.Schema{
-			"arn": &schema.Schema{
+			"arn": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
 
-			"family": &schema.Schema{
+			"family": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
 
-			"revision": &schema.Schema{
+			"revision": {
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
 
-			"container_definitions": &schema.Schema{
+			"container_definitions": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -47,13 +47,13 @@ func resourceAwsEcsTaskDefinition() *schema.Resource {
 				},
 			},
 
-			"task_role_arn": &schema.Schema{
+			"task_role_arn": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
 
-			"network_mode": &schema.Schema{
+			"network_mode": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				Computed:     true,
@@ -61,18 +61,18 @@ func resourceAwsEcsTaskDefinition() *schema.Resource {
 				ValidateFunc: validateAwsEcsTaskDefinitionNetworkMode,
 			},
 
-			"volume": &schema.Schema{
+			"volume": {
 				Type:     schema.TypeSet,
 				Optional: true,
 				ForceNew: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"name": &schema.Schema{
+						"name": {
 							Type:     schema.TypeString,
 							Required: true,
 						},
 
-						"host_path": &schema.Schema{
+						"host_path": {
 							Type:     schema.TypeString,
 							Optional: true,
 						},
@@ -81,19 +81,19 @@ func resourceAwsEcsTaskDefinition() *schema.Resource {
 				Set: resourceAwsEcsTaskDefinitionVolumeHash,
 			},
 
-			"placement_constraints": &schema.Schema{
+			"placement_constraints": {
 				Type:     schema.TypeSet,
 				Optional: true,
 				ForceNew: true,
 				MaxItems: 10,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"type": &schema.Schema{
+						"type": {
 							Type:     schema.TypeString,
 							ForceNew: true,
 							Required: true,
 						},
-						"expression": &schema.Schema{
+						"expression": {
 							Type:     schema.TypeString,
 							ForceNew: true,
 							Required: true,
@@ -108,9 +108,9 @@ func resourceAwsEcsTaskDefinition() *schema.Resource {
 func validateAwsEcsTaskDefinitionNetworkMode(v interface{}, k string) (ws []string, errors []error) {
 	value := strings.ToLower(v.(string))
 	validTypes := map[string]struct{}{
-		"bridge": struct{}{},
-		"host":   struct{}{},
-		"none":   struct{}{},
+		"bridge": {},
+		"host":   {},
+		"none":   {},
 	}
 
 	if _, ok := validTypes[value]; !ok {
