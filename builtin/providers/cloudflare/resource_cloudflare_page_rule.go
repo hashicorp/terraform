0ba9b33db8684e82b4d59a818d0daa8b730fe821
Ollie Ford <me@ojford.com>
2017-01-18 23:17:37 +0000

    Implement Read/Update/Delete Page Rules

--- builtin/providers/cloudflare/resource_cloudflare_page_rule.go
+++ builtin/providers/cloudflare/resource_cloudflare_page_rule.go
@@ -106,13 +106,91 @@ func resourceCloudFlarePageRuleCreate(d *schema.ResourceData, meta interface{}) 
 }
 
 func resourceCloudFlarePageRuleRead(d *schema.ResourceData, meta interface{}) error {
-	return fmt.Errorf("Page Rule Read not implemented.")
+	client := meta.(*cloudflare.API)
+	domain := d.Get("domain").(string)
+
+	zoneId, err := client.ZoneIDByName(domain)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", domain, err)
+	}
+
+	pageRule, err := client.PageRule(zoneId, d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.SetId(pageRule.ID)
+	d.Set("targets", pageRule.Targets)
+	d.Set("actions", pageRule.Actions)
+	d.Set("priority", pageRule.Priority)
+	d.Set("status", pageRule.Status)
+
+	return nil
 }
 
 func resourceCloudFlarePageRuleUpdate(d *schema.ResourceData, meta interface{}) error {
-	return fmt.Errorf("Page Rule Update not implemented.")
+	client := meta.(*cloudflare.API)
+	domain := d.Get("domain").(string)
+
+	updatePageRule := cloudflare.PageRule{
+		ID: d.Id(),
+	}
+
+	if target, ok := d.GetOk("target"); ok {
+		updatePageRule.Targets = []cloudflare.PageRuleTarget{
+			cloudflare.PageRuleTarget{
+				Target: "url",
+				Constraint: struct {
+					Operator string `json:"operator"`
+					Value    string `json:"value"`
+				}{
+					Operator: "matches",
+					Value:    target.(string),
+				},
+			},
+		}
+	}
+
+	if actions, ok := d.GetOk("actions"); ok {
+		updatePageRule.Actions = actions.([]cloudflare.PageRuleAction)
+	}
+
+	if priority, ok := d.GetOk("priority"); ok {
+		updatePageRule.Priority = priority.(int)
+	}
+
+	if status, ok := d.GetOk("status"); ok {
+		updatePageRule.Status = status.(string)
+	}
+
+	zoneId, err := client.ZoneIDByName(domain)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", domain, err)
+	}
+
+	log.Printf("[DEBUG] Cloudflare Page Rule update configuration: %#v", updatePageRule)
+
+	if err := client.UpdatePageRule(zoneId, d.Id(), updatePageRule); err != nil {
+		return fmt.Errorf("Failed to update Cloudflare Page Rule: %s", err)
+	}
+
+	return resourceCloudFlarePageRuleRead(d, meta)
 }
 
 func resourceCloudFlarePageRuleDelete(d *schema.ResourceData, meta interface{}) error {
-	return fmt.Errorf("Page Rule Delete not implemented.")
+	client := meta.(*cloudflare.API)
+	domain := d.Get("domain").(string)
+
+	zoneId, err := client.ZoneIDByName(domain)
+	if err != nil {
+		return fmt.Errorf("Error finding zone %q: %s", domain, err)
+	}
+
+	log.Printf("[INFO] Deleting Cloudflare Page Rule: %s, %s", domain, d.Id())
+
+	if err := client.DeletePageRule(zoneId, d.Id()); err != nil {
+		return fmt.Errorf("Error deleting Cloudflare Page Rule: %s", err)
+	}
+
+	return nil
 }
