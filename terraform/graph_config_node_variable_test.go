5590afc91601ff7d83aff3344b183aa490774cea
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-01-26 19:31:32 -0800

    terraform: delete proxy transformer

--- terraform/graph_config_node_variable_test.go
+++ terraform/graph_config_node_variable_test.go
@@ -10,12 +10,10 @@ func TestGraphNodeConfigVariable_impl(t *testing.T) {
 	var _ dag.Vertex = new(GraphNodeConfigVariable)
 	var _ dag.NamedVertex = new(GraphNodeConfigVariable)
 	var _ graphNodeConfig = new(GraphNodeConfigVariable)
-	var _ GraphNodeProxy = new(GraphNodeConfigVariable)
 }
 
 func TestGraphNodeConfigVariableFlat_impl(t *testing.T) {
 	var _ dag.Vertex = new(GraphNodeConfigVariableFlat)
 	var _ dag.NamedVertex = new(GraphNodeConfigVariableFlat)
 	var _ graphNodeConfig = new(GraphNodeConfigVariableFlat)
-	var _ GraphNodeProxy = new(GraphNodeConfigVariableFlat)
 }
