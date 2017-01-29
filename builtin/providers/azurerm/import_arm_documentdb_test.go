7db560cbe88a33898b1019ae297f094b30cd4923
tombuildsstuff <git@ibuildstuff.co.uk>
2017-01-29 15:43:15 +0100

    Import tests for the super happy path

---
+++ builtin/providers/azurerm/import_arm_documentdb_test.go
@@ -0,0 +1,34 @@
+package azurerm
+
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAzureRMDocumentDb_importStandard(t *testing.T) {
+	resourceName := "azurerm_documentdb.test"
+
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMDocumentDb_standard, ri, ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDocumentDbDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
---
+++ builtin/providers/azurerm/resource_arm_documentdb_test.go
@@ -0,0 +1,111 @@
+package azurerm
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// TODO: validation
+
+func TestAccAzureRMDocumentDb_standard(t *testing.T) {
+
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMDocumentDb_standard, ri, ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMDocumentDbDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMDocumentDbExists("azurerm_documentdb.test"),
+				),
+			},
+		},
+	})
+}
+
+func testCheckAzureRMDocumentDbDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*ArmClient).documentDbClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azurerm_documentdb" {
+			continue
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup := rs.Primary.Attributes["resource_group_name"]
+
+		resp, err := conn.Get(resourceGroup, name)
+
+		if err != nil {
+			return nil
+		}
+
+		if resp.StatusCode != http.StatusNotFound {
+			return fmt.Errorf("DocumentDB instance still exists:\n%#v", resp)
+		}
+	}
+
+	return nil
+}
+
+func testCheckAzureRMDocumentDbExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for DocumentDB instance: %s", name)
+		}
+
+		conn := testAccProvider.Meta().(*ArmClient).documentDbClient
+
+		resp, err := conn.Get(resourceGroup, name)
+		if err != nil {
+			return fmt.Errorf("Bad: Get on documentDbClient: %s", err)
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return fmt.Errorf("Bad: DocumentDB instance %q (resource group: %q) does not exist", name, resourceGroup)
+		}
+
+		return nil
+	}
+}
+
+var testAccAzureRMDocumentDb_standard = `
+resource "azurerm_resource_group" "test" {
+    name = "acctestRG-%d"
+    location = "West US"
+}
+resource "azurerm_documentdb" "test" {
+  name                = "acctestDDB-%d"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  offer_type          = "Standard"
+
+  consistency_policy {
+    consistency_level       = "Eventual"
+    max_interval_in_seconds = 100
+    max_staleness           = 30
+  }
+
+  failover_policy {
+    name     = "${azurerm_resource_group.test.location}"
+    priority = 0
+  }
+}
+`
