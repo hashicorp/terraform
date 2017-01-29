547017a9954703df82e0bd96e87211520e952514
Marcial Rosales <mrosales@pivotal.io>
2017-01-29 09:42:34 +0100

    Complete user story - Create service instance + Docs

--- builtin/providers/cf/resource_cf_service_instance.go
+++ builtin/providers/cf/resource_cf_service_instance.go
@@ -1,6 +1,13 @@
 package cloudfoundry
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"fmt"
+
+	"encoding/json"
+
+	"github.com/hashicorp/terraform/builtin/providers/cf/cfapi"
+	"github.com/hashicorp/terraform/helper/schema"
+)
 
 func resourceServiceInstance() *schema.Resource {
 
@@ -27,65 +34,151 @@ func resourceServiceInstance() *schema.Resource {
 			},
 			"jsonParameters": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: false,
+				Optional: true,
+				Default:  "",
 			},
 			"tags": &schema.Schema{
-				Type:     schema.TypeSet,
+				Type:     schema.TypeList,
 				Optional: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
-				Set:      resourceStringHash,
 			},
 		},
 	}
 }
 
 func resourceServiceInstanceCreate(d *schema.ResourceData, meta interface{}) (err error) {
-	/*
-		session := meta.(*cfapi.Session)
-		if session == nil {
-			return fmt.Errorf("client is nil")
-		}
 
-		name := d.Get("name").(string)
-		servicePlan := d.Get("servicePlan").(string)
-		space := d.Get("space").(string)
-		jsonParameters := d.Get("jsonParameters").(string)
-		tags := d.Get("tags").(string)
+	session := meta.(*cfapi.Session)
+	if session == nil {
+		return fmt.Errorf("client is nil")
+	}
 
-		sm := session.ServiceManager()
+	var (
+		id     string
+		tags   []string
+		params map[string]interface{}
+	)
+	name := d.Get("name").(string)
+	servicePlan := d.Get("servicePlan").(string)
+	space := d.Get("space").(string)
+	jsonParameters := d.Get("jsonParameters").(string)
 
-		var (
-			serviceInstance cfapi.CCServiceInstance
-		)
+	for _, v := range d.Get("tags").([]interface{}) {
+		tags = append(tags, v.(string))
+	}
 
-		if serviceInstance, err = sm.CreateServiceInstance(name, servicePlan, space); err != nil {
+	if len(jsonParameters) > 0 {
+		if err = json.Unmarshal([]byte(jsonParameters), &params); err != nil {
 			return
 		}
+	}
 
-		d.SetId(serviceInstance.ID)
-	*/
+	sm := session.ServiceManager()
+
+	if id, err = sm.CreateServiceInstance(name, servicePlan, space, params, tags); err != nil {
+		return
+	}
+	session.Log.DebugMessage("New Service Instance : %# v", id)
+
+	// TODO deal with asynchronous responses
+
+	d.SetId(id)
+
 	return
 }
 
 func resourceServiceInstanceRead(d *schema.ResourceData, meta interface{}) (err error) {
-	/*
-		session := meta.(*cfapi.Session)
-		if session == nil {
-			return fmt.Errorf("client is nil")
-		}
-		sm := session.ServiceManager()
 
-		var serviceInstance cfapi.CCServiceInstance
-	*/
+	session := meta.(*cfapi.Session)
+	if session == nil {
+		return fmt.Errorf("client is nil")
+	}
+	session.Log.DebugMessage("Reading Service Instance : %s", d.Id())
+
+	sm := session.ServiceManager()
+	var serviceInstance cfapi.CCServiceInstance
+
+	serviceInstance, err = sm.ReadServiceInstance(d.Id())
+	if err != nil {
+		return
+	}
+
+	d.Set("name", serviceInstance.Name)
+	d.Set("servicePlan", serviceInstance.ServicePlanGUID)
+	d.Set("space", serviceInstance.SpaceGUID)
+
+	if serviceInstance.Tags != nil {
+		tags := make([]interface{}, len(serviceInstance.Tags))
+		for i, v := range serviceInstance.Tags {
+			tags[i] = v
+		}
+		d.Set("tags", tags)
+	} else {
+		d.Set("tags", nil)
+	}
+
+	session.Log.DebugMessage("Read Service Instance : %# v", serviceInstance)
+
 	return
 }
 
-func resourceServiceInstanceUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceServiceInstanceUpdate(d *schema.ResourceData, meta interface{}) (err error) {
 
-	return nil
+	session := meta.(*cfapi.Session)
+	if session == nil {
+		return fmt.Errorf("client is nil")
+	}
+	sm := session.ServiceManager()
+
+	session.Log.DebugMessage("begin resourceServiceInstanceUpdate")
+
+	var (
+		id, name string
+		tags     []string
+		params   map[string]interface{}
+	)
+
+	id = d.Id()
+	name = d.Get("name").(string)
+	servicePlan := d.Get("servicePlan").(string)
+	jsonParameters := d.Get("jsonParameters").(string)
+
+	if len(jsonParameters) > 0 {
+		if err = json.Unmarshal([]byte(jsonParameters), &params); err != nil {
+			return
+		}
+	}
+
+	for _, v := range d.Get("tags").([]interface{}) {
+		tags = append(tags, v.(string))
+	}
+
+	if _, err = sm.UpdateServiceInstance(id, name, servicePlan, params, tags); err != nil {
+		return
+	}
+	if err != nil {
+		return
+	}
+
+	return
 }
 
-func resourceServiceInstanceDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceServiceInstanceDelete(d *schema.ResourceData, meta interface{}) (err error) {
 
-	return nil
+	session := meta.(*cfapi.Session)
+	if session == nil {
+		return fmt.Errorf("client is nil")
+	}
+	session.Log.DebugMessage("begin resourceServiceInstanceDelete")
+
+	sm := session.ServiceManager()
+
+	err = sm.DeleteServiceInstance(d.Id())
+	if err != nil {
+		return
+	}
+
+	session.Log.DebugMessage("Deleted Service Instance : %s", d.Id())
+
+	return
 }
