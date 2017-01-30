dcfe870cd8b5c45509ffad70f034c9525d9e35f5
Rob Coward <rob@jive-videos.net>
2017-01-30 23:31:32 +0000

    OS-131 Updated dependencies to use ukcloud/govcloudair instead of hmrc/vmware-govcd

--- builtin/providers/vcd/resource_vcd_firewall_rules_test.go
+++ builtin/providers/vcd/resource_vcd_firewall_rules_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/hmrc/vmware-govcd"
+	"github.com/ukcloud/govcloudair"
 )
 
 func TestAccVcdFirewallRules_basic(t *testing.T) {
