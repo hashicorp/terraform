72dadd9f5b4b200a77e160f0c87fd2b7e0dd7423
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-01-30 08:41:38 -0800

    terraform: acquireRun during test to avoid special case logic

--- terraform/context_validate_test.go
+++ terraform/context_validate_test.go
@@ -906,6 +906,7 @@ func TestContext2Validate_PlanGraphBuilder(t *testing.T) {
 		Targets:   c.targets,
 	}).Build(RootModulePath)
 
+	defer c.acquireRun("validate-test")()
 	walker, err := c.walk(graph, graph, walkValidate)
 	if err != nil {
 		t.Fatal(err)
