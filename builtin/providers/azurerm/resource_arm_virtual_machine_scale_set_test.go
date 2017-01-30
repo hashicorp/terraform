31fb9d2d39375d2fda6a8c1feac3133f24e1cea0
Robert Rudduck <rrudduck@ambitenergy.com>
2017-01-30 16:07:19 -0600

    Create acceptance test for scale set overprovisioning.

--- builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -48,9 +48,9 @@ func TestAccAzureRMVirtualMachineScaleSet_basicLinux_disappears(t *testing.T) {
 	})
 }
 
-func TestAccAzureRMVirtualMachineScaleSet_linxuLoadBalancer(t *testing.T) {
+func TestAccAzureRMVirtualMachineScaleSet_loadBalancer(t *testing.T) {
 	ri := acctest.RandInt()
-	config := fmt.Sprintf(testAccAzureRMVirtualMachineScaleSet_linuxLoadbalancer, ri, ri, ri, ri, ri, ri, ri)
+	config := fmt.Sprintf(testAccAzureRMVirtualMachineScaleSetLoadbalancerTemplate, ri, ri, ri, ri, ri, ri, ri)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -61,6 +61,25 @@ func TestAccAzureRMVirtualMachineScaleSet_linxuLoadBalancer(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testCheckAzureRMVirtualMachineScaleSetExists("azurerm_virtual_machine_scale_set.test"),
 					testCheckAzureRMVirtualMachineScaleSetHasLoadbalancer("azurerm_virtual_machine_scale_set.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureRMVirtualMachineScaleSet_overprovision(t *testing.T) {
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMVirtualMachineScaleSetOverprovisionTemplate, ri, ri, ri, ri, ri, ri)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMVirtualMachineScaleSetDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMVirtualMachineScaleSetExists("azurerm_virtual_machine_scale_set.test"),
+					testCheckAzureRMVirtualMachineScaleSetOverprovision("azurerm_virtual_machine_scale_set.test"),
 				),
 			},
 		},
@@ -189,6 +208,38 @@ func testCheckAzureRMVirtualMachineScaleSetHasLoadbalancer(name string) resource
 	}
 }
 
+func testCheckAzureRMVirtualMachineScaleSetOverprovision(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Ensure we have enough information in state to look up in API
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		name := rs.Primary.Attributes["name"]
+		resourceGroup, hasResourceGroup := rs.Primary.Attributes["resource_group_name"]
+		if !hasResourceGroup {
+			return fmt.Errorf("Bad: no resource group found in state for virtual machine: scale set %s", name)
+		}
+
+		conn := testAccProvider.Meta().(*ArmClient).vmScaleSetClient
+		resp, err := conn.Get(resourceGroup, name)
+		if err != nil {
+			return fmt.Errorf("Bad: Get on vmScaleSetClient: %s", err)
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return fmt.Errorf("Bad: VirtualMachineScaleSet %q (resource group: %q) does not exist", name, resourceGroup)
+		}
+
+		if *resp.Overprovision {
+			return fmt.Errorf("Bad: Overprovision should have been false for scale set %v", name)
+		}
+
+		return nil
+	}
+}
+
 var testAccAzureRMVirtualMachineScaleSet_basicLinux = `
 resource "azurerm_resource_group" "test" {
     name = "acctestRG-%d"
@@ -282,7 +333,7 @@ resource "azurerm_virtual_machine_scale_set" "test" {
 }
 `
 
-var testAccAzureRMVirtualMachineScaleSet_linuxLoadbalancer = `
+var testAccAzureRMVirtualMachineScaleSetLoadbalancerTemplate = `
 resource "azurerm_resource_group" "test" {
     name 	 = "acctestrg-%d"
     location = "southcentralus"
@@ -344,7 +395,7 @@ resource "azurerm_virtual_machine_scale_set" "test" {
   	sku {
 		name     = "Standard_A0"
     	tier     = "Standard"
-    	capacity = 2
+    	capacity = 1
 	}
 
   	os_profile {
@@ -360,6 +411,84 @@ resource "azurerm_virtual_machine_scale_set" "test" {
         	name                                   = "TestIPConfiguration"
         	subnet_id                              = "${azurerm_subnet.test.id}"
 			load_balancer_backend_address_pool_ids = [ "${azurerm_lb_backend_address_pool.test.id}" ]
+      	}
+  	}
+
+  	storage_profile_os_disk {
+    	name 		   = "os-disk"
+    	caching        = "ReadWrite"
+    	create_option  = "FromImage"
+    	vhd_containers = [ "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}" ]
+  	}
+
+  	storage_profile_image_reference {
+    	publisher = "Canonical"
+    	offer     = "UbuntuServer"
+    	sku       = "14.04.2-LTS"
+    	version   = "latest"
+  	}
+}
+`
+
+var testAccAzureRMVirtualMachineScaleSetOverprovisionTemplate = `
+resource "azurerm_resource_group" "test" {
+    name 	 = "acctestrg-%d"
+    location = "southcentralus"
+}
+
+resource "azurerm_virtual_network" "test" {
+    name 		        = "acctvn-%d"
+    address_space       = ["10.0.0.0/16"]
+    location            = "southcentralus"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+    name                 = "acctsub-%d"
+    resource_group_name  = "${azurerm_resource_group.test.name}"
+    virtual_network_name = "${azurerm_virtual_network.test.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_storage_account" "test" {
+    name                = "accsa%d"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    location            = "southcentralus"
+    account_type        = "Standard_LRS"
+}
+
+resource "azurerm_storage_container" "test" {
+    name                  = "vhds"
+    resource_group_name   = "${azurerm_resource_group.test.name}"
+    storage_account_name  = "${azurerm_storage_account.test.name}"
+    container_access_type = "private"
+}
+
+resource "azurerm_virtual_machine_scale_set" "test" {
+  	name                = "acctvmss-%d"
+  	location            = "southcentralus"
+  	resource_group_name = "${azurerm_resource_group.test.name}"
+  	upgrade_policy_mode = "Manual"
+	overprovision       = false
+
+  	sku {
+		name     = "Standard_A0"
+    	tier     = "Standard"
+    	capacity = 1
+	}
+
+  	os_profile {
+    	computer_name_prefix = "testvm-%d"
+    	admin_username = "myadmin"
+    	admin_password = "Passwword1234"
+  	}
+
+  	network_profile {
+      	name    = "TestNetworkProfile"
+      	primary = true
+      	ip_configuration {
+        	name	  = "TestIPConfiguration"
+        	subnet_id = "${azurerm_subnet.test.id}"
       	}
   	}
 
