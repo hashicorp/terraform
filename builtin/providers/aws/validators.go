4d0048fa75607766d6833298a9a3bd78b6a64dc9
Gauthier Wallet <gauthier.wallet@gmail.com>
2017-01-31 21:17:38 +0100

    Added Step Function resources (State Machine & Activity) (#11420)
    * Added Step Function Activity & Step Function State Machine
    
    * Added SFN State Machine documentation
    
    * Added aws_sfn_activity & documentation
    
    * Allowed import of sfn resources
    
    * Added more checks on tests, fixed documentation
    
    * Handled the update case of a SFN function (might be already deleting)
    
    * Removed the State Machine import test file
    
    * Fixed the eventual consistency of the read after delete for SFN functions

--- builtin/providers/aws/validators.go
+++ builtin/providers/aws/validators.go
@@ -743,3 +743,33 @@ func validateAwsEmrEbsVolumeType(v interface{}, k string) (ws []string, errors [
 	}
 	return
 }
+
+func validateSfnActivityName(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 80 {
+		errors = append(errors, fmt.Errorf("%q cannot be longer than 80 characters", k))
+	}
+
+	return
+}
+
+func validateSfnStateMachineDefinition(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 1048576 {
+		errors = append(errors, fmt.Errorf("%q cannot be longer than 1048576 characters", k))
+	}
+	return
+}
+
+func validateSfnStateMachineName(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 80 {
+		errors = append(errors, fmt.Errorf("%q cannot be longer than 80 characters", k))
+	}
+
+	if !regexp.MustCompile(`^[a-zA-Z0-9-_]+$`).MatchString(value) {
+		errors = append(errors, fmt.Errorf(
+			"%q must be composed with only these characters [a-zA-Z0-9-_]: %v", k, value))
+	}
+	return
+}
