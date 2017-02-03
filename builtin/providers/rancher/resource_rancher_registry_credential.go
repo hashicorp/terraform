7f3e62c23b1c2c416b71a78559997ea685056829
Raphaël Pinson <github+aem1eeshi1@raphink.net>
2017-02-03 13:34:26 +0100

    rancher: set environment_id on import (#11594)

--- builtin/providers/rancher/resource_rancher_registry_credential.go
+++ builtin/providers/rancher/resource_rancher_registry_credential.go
@@ -17,7 +17,7 @@ func resourceRancherRegistryCredential() *schema.Resource {
 		Update: resourceRancherRegistryCredentialUpdate,
 		Delete: resourceRancherRegistryCredentialDelete,
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			State: resourceRancherRegistryCredentialImport,
 		},
 
 		Schema: map[string]*schema.Schema{
@@ -230,6 +230,16 @@ func resourceRancherRegistryCredentialDelete(d *schema.ResourceData, meta interf
 
 	d.SetId("")
 	return nil
+}
+
+func resourceRancherRegistryCredentialImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	client := meta.(*Config)
+	regC, err := client.RegistryCredential.ById(d.Id())
+	if err != nil {
+		return []*schema.ResourceData{}, err
+	}
+	d.Set("environment_id", regC.AccountId)
+	return []*schema.ResourceData{d}, nil
 }
 
 // RegistryCredentialStateRefreshFunc returns a resource.StateRefreshFunc that is used to watch
