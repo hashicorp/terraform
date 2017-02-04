dd0167ff673923aecf0cd1e315284b72bacf0e79
ValFadeev <ValFadeev@users.noreply.github.com>
2017-02-04 11:54:45 +0000

    provider/aws: New DataSource: aws_partition (#11675)

--- builtin/providers/aws/provider.go
+++ builtin/providers/aws/provider.go
@@ -174,6 +174,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_iam_server_certificate":   dataSourceAwsIAMServerCertificate(),
 			"aws_instance":                 dataSourceAwsInstance(),
 			"aws_ip_ranges":                dataSourceAwsIPRanges(),
+			"aws_partition":                dataSourceAwsPartition(),
 			"aws_prefix_list":              dataSourceAwsPrefixList(),
 			"aws_redshift_service_account": dataSourceAwsRedshiftServiceAccount(),
 			"aws_region":                   dataSourceAwsRegion(),
