721d1b74ab0a48de27f86d89e470e2fcdd4a1465
Erik Jansson <me@erikjansson.se>
2017-02-07 11:25:29 +0100

    Fix nil pointer in resource_aws_codebuild_project (#11740)

--- builtin/providers/aws/resource_aws_codebuild_project.go
+++ builtin/providers/aws/resource_aws_codebuild_project.go
@@ -588,7 +588,7 @@ func sourceAuthToMap(sourceAuth *codebuild.SourceAuth) map[string]interface{} {
 	auth := map[string]interface{}{}
 	auth["type"] = *sourceAuth.Type
 
-	if sourceAuth.Type != nil {
+	if sourceAuth.Resource != nil {
 		auth["resource"] = *sourceAuth.Resource
 	}
 
