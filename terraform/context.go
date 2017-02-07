864c79396d6d8e5a1255a7a8a33128a74d196a11
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-02-06 18:02:32 -0800

    terraform: default shadow to false
    To avoid chasing down issues like #11635 I'm proposing we disable the
    shadow graph for end users now that we have merged in all the new
    graphs. I've kept it around and default-on for tests so that we can use
    it to test new features as we build them. I think it'll still have value
    going forward but I don't want to hold us for making it work 100% with
    all of Terraform at all times.
    
    I propose backporting this to 0-8-stable, too.

--- terraform/context.go
+++ terraform/context.go
@@ -748,19 +748,20 @@ func (c *Context) walk(
 		shadow = nil
 	}
 
+	// Just log this so we can see it in a debug log
+	if !c.shadow {
+		log.Printf("[WARN] terraform: shadow graph disabled")
+		shadow = nil
+	}
+
 	// If we have a shadow graph, walk that as well
 	var shadowCtx *Context
 	var shadowCloser Shadow
-	if c.shadow && shadow != nil {
+	if shadow != nil {
 		// Build the shadow context. In the process, override the real context
 		// with the one that is wrapped so that the shadow context can verify
 		// the results of the real.
 		realCtx, shadowCtx, shadowCloser = newShadowContext(c)
-	}
-
-	// Just log this so we can see it in a debug log
-	if !c.shadow {
-		log.Printf("[WARN] terraform: shadow graph disabled")
 	}
 
 	log.Printf("[DEBUG] Starting graph walk: %s", operation.String())
