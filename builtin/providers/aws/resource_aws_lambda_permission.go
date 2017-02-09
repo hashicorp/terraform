5db09991c06f7d7c31a60318c0135ff9020adaf7
clint shryock <clint@ctshryock.com>
2017-02-09 10:39:20 -0600

    provider/aws: Set the qualifier to an empty string if the parsing fails, to attempt to detect drift

--- builtin/providers/aws/resource_aws_lambda_permission.go
+++ builtin/providers/aws/resource_aws_lambda_permission.go
@@ -211,9 +211,10 @@ func resourceAwsLambdaPermissionRead(d *schema.ResourceData, meta interface{}) e
 	}
 
 	qualifier, err := getQualifierFromLambdaAliasOrVersionArn(statement.Resource)
-	if err == nil {
-		d.Set("qualifier", qualifier)
+	if err != nil {
+		log.Printf("[ERR] Error getting Lambda Qualifier: %s", err)
 	}
+	d.Set("qualifier", qualifier)
 
 	// Save Lambda function name in the same format
 	if strings.HasPrefix(d.Get("function_name").(string), "arn:"+meta.(*AWSClient).partition+":lambda:") {
