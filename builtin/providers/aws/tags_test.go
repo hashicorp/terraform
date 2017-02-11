d304212c4a37c27b24a9362e748e417163e484a5
Eric Westfall <eawestfall@gmail.com>
2017-02-11 17:04:24 -0500

    Add ability to opt-out of internal AWS tag filtering.

--- builtin/providers/aws/tags_test.go
+++ builtin/providers/aws/tags_test.go
@@ -50,15 +50,71 @@ func TestDiffTags(t *testing.T) {
 	}
 
 	for i, tc := range cases {
-		c, r := diffTags(tagsFromMap(tc.Old), tagsFromMap(tc.New))
-		cm := tagsToMap(c)
-		rm := tagsToMap(r)
+		c, r := diffTags(tagsFromMap(tc.Old, false), tagsFromMap(tc.New, false))
+		cm := tagsToMap(c, false)
+		rm := tagsToMap(r, false)
 		if !reflect.DeepEqual(cm, tc.Create) {
 			t.Fatalf("%d: bad create: %#v", i, cm)
 		}
 		if !reflect.DeepEqual(rm, tc.Remove) {
 			t.Fatalf("%d: bad remove: %#v", i, rm)
 		}
+	}
+}
+
+// Test the ability to opt-out of internal AWS tag filtering when calling the
+// tagsFromMap and tagsToMap functions.
+func TestAllowInternalTags(t *testing.T) {
+	var ignoredTags []*ec2.Tag
+	var ignoredTagsMap map[string]interface{}
+
+	const (
+		tagKey   string = "aws:cloudformation:logical-id"
+		tagValue string = "foo"
+	)
+
+	ignoredTags = append(ignoredTags,
+		&ec2.Tag{
+			Key:   aws.String(tagKey),
+			Value: aws.String(tagValue),
+		})
+
+	ignoredTagsMap = make(map[string]interface{})
+	ignoredTagsMap[tagKey] = tagValue
+
+	// Make two calls, one that should allow internal AWS tags and one that should not.
+	failFromMap := tagsFromMap(ignoredTagsMap, false)
+	successFromMap := tagsFromMap(ignoredTagsMap, true)
+	if len(failFromMap) != 0 {
+		t.Fatalf("Test[tagsFromMap]: Tag %v with value %v was not ignored and should have been.", tagKey, tagValue)
+	}
+
+	if len(successFromMap) != 0 {
+		for _, tag := range successFromMap {
+			if (*tag.Key != tagKey) || (*tag.Value != tagValue) {
+				t.Fatalf("Test[tagsFromMap]: Tag %v with value %v does not match the expected tag %v with value %v.", *tag.Key, *tag.Value, tagKey, tagValue)
+			}
+		}
+	} else {
+		t.Fatalf("Test[tagsFromMap]: Tag %v with value %v was ignored and should not have been.", tagKey, tagValue)
+	}
+
+	// Make two calls, one that should allow internal AWS tags and one that should not.
+	successToMap := tagsToMap(ignoredTags, true)
+	failToMap := tagsToMap(ignoredTags, false)
+
+	if len(successToMap) != 0 {
+		for tag, value := range successToMap {
+			if (tag != tagKey) || (value != tagValue) {
+				t.Fatalf("Test[tagsToMap]: Tag %v with value %v does not match the expected tag %v with value %v.", tag, value, tagKey, tagValue)
+			}
+		}
+	} else {
+		t.Fatalf("Test[tagsToMap]: Tag %v with value %v was ignored and should not have been.", tagKey, tagValue)
+	}
+
+	if len(failToMap) != 0 {
+		t.Fatalf("Test[tagsToMap]: Tag %v with value %v was not ignored and should have been.", tagKey, tagValue)
 	}
 }
 
@@ -84,7 +140,7 @@ func TestIgnoringTags(t *testing.T) {
 func testAccCheckTags(
 	ts *[]*ec2.Tag, key string, value string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		m := tagsToMap(*ts)
+		m := tagsToMap(*ts, false)
 		v, ok := m[key]
 		if value != "" && !ok {
 			return fmt.Errorf("Missing tag: %s", key)
