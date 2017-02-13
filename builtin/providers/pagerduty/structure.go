9b2439b0277523e1080e9b566be94f4362f9d369
Jahn Saito <jahn.saito@gmail.com>
2017-02-13 12:41:24 +0100

    Add support for missing attributes for PagerDuty service resource  (#11856)
    * Add urgencies for PagerDuty services
    
    * Improve naming, comments, handle unexpected urgency rules
    
    * Document urgency rules for PagerDuty service

--- builtin/providers/pagerduty/structure.go
+++ builtin/providers/pagerduty/structure.go
@@ -184,3 +184,207 @@ func expandStringList(configured []interface{}) []string {
 	}
 	return vs
 }
+
+// Expands attribute slice to incident urgency rule, returns it and true if successful
+func expandIncidentUrgencyRule(incidentUrgencyList interface{}) (*pagerduty.IncidentUrgencyRule, bool) {
+	i := incidentUrgencyList.([]interface{})
+
+	i, ok := incidentUrgencyList.([]interface{})
+	if !ok {
+		return nil, false
+	}
+
+	m, ok := i[0].(map[string]interface{})
+	if !ok || len(m) == 0 {
+		return nil, false
+	}
+
+	iur := pagerduty.IncidentUrgencyRule{}
+	if val, ok := m["type"]; ok {
+		iur.Type = val.(string)
+	}
+	if val, ok := m["urgency"]; ok {
+		iur.Urgency = val.(string)
+	}
+	if val, ok := m["during_support_hours"]; ok {
+		iur.DuringSupportHours = expandIncidentUrgencyType(val)
+	}
+	if val, ok := m["outside_support_hours"]; ok {
+		iur.OutsideSupportHours = expandIncidentUrgencyType(val)
+	}
+
+	return &iur, true
+}
+
+// Expands attribute to inline model
+func expandActionInlineModel(inlineModelVal interface{}) *pagerduty.InlineModel {
+	inlineModel := pagerduty.InlineModel{}
+
+	if slice, ok := inlineModelVal.([]interface{}); ok && len(slice) == 1 {
+		m := slice[0].(map[string]interface{})
+
+		if val, ok := m["type"]; ok {
+			inlineModel.Type = val.(string)
+		}
+		if val, ok := m["name"]; ok {
+			inlineModel.Name = val.(string)
+		}
+	}
+
+	return &inlineModel
+}
+
+// Expands attribute into incident urgency type
+func expandIncidentUrgencyType(attribute interface{}) *pagerduty.IncidentUrgencyType {
+	ict := pagerduty.IncidentUrgencyType{}
+
+	slice := attribute.([]interface{})
+	if len(slice) != 1 {
+		return &ict
+	}
+
+	m := slice[0].(map[string]interface{})
+
+	if val, ok := m["type"]; ok {
+		ict.Type = val.(string)
+	}
+	if val, ok := m["urgency"]; ok {
+		ict.Urgency = val.(string)
+	}
+
+	return &ict
+}
+
+// Returns service's incident urgency rule as slice of length one and bool indicating success
+func flattenIncidentUrgencyRule(service *pagerduty.Service) ([]interface{}, bool) {
+	if service.IncidentUrgencyRule.Type == "" && service.IncidentUrgencyRule.Urgency == "" {
+		return nil, false
+	}
+
+	m := map[string]interface{}{
+		"type":    service.IncidentUrgencyRule.Type,
+		"urgency": service.IncidentUrgencyRule.Urgency,
+	}
+
+	if dsh := service.IncidentUrgencyRule.DuringSupportHours; dsh != nil {
+		m["during_support_hours"] = flattenIncidentUrgencyType(dsh)
+	}
+	if osh := service.IncidentUrgencyRule.OutsideSupportHours; osh != nil {
+		m["outside_support_hours"] = flattenIncidentUrgencyType(osh)
+	}
+
+	return []interface{}{m}, true
+}
+
+func flattenIncidentUrgencyType(iut *pagerduty.IncidentUrgencyType) []interface{} {
+	incidenUrgencyType := map[string]interface{}{
+		"type":    iut.Type,
+		"urgency": iut.Urgency,
+	}
+	return []interface{}{incidenUrgencyType}
+}
+
+// Expands attribute to support hours
+func expandSupportHours(attribute interface{}) (sh *pagerduty.SupportHours) {
+	if slice, ok := attribute.([]interface{}); ok && len(slice) >= 1 {
+		m := slice[0].(map[string]interface{})
+		sh = &pagerduty.SupportHours{}
+
+		if val, ok := m["type"]; ok {
+			sh.Type = val.(string)
+		}
+		if val, ok := m["time_zone"]; ok {
+			sh.Timezone = val.(string)
+		}
+		if val, ok := m["start_time"]; ok {
+			sh.StartTime = val.(string)
+		}
+		if val, ok := m["end_time"]; ok {
+			sh.EndTime = val.(string)
+		}
+		if val, ok := m["days_of_week"]; ok {
+			daysOfWeekInt := val.([]interface{})
+			var daysOfWeek []uint
+
+			for _, i := range daysOfWeekInt {
+				daysOfWeek = append(daysOfWeek, uint(i.(int)))
+			}
+
+			sh.DaysOfWeek = daysOfWeek
+		}
+	}
+
+	return
+}
+
+// Returns service's support hours as slice of length one
+func flattenSupportHours(service *pagerduty.Service) []interface{} {
+	if service.SupportHours == nil {
+		return nil
+	}
+
+	m := map[string]interface{}{}
+
+	if s := service.SupportHours; s != nil {
+		m["type"] = s.Type
+		m["time_zone"] = s.Timezone
+		m["start_time"] = s.StartTime
+		m["end_time"] = s.EndTime
+		m["days_of_week"] = s.DaysOfWeek
+	}
+
+	return []interface{}{m}
+}
+
+// Expands attribute to scheduled action
+func expandScheduledActions(input interface{}) (scheduledActions []pagerduty.ScheduledAction) {
+	inputs := input.([]interface{})
+
+	for _, i := range inputs {
+		m := i.(map[string]interface{})
+		sa := pagerduty.ScheduledAction{}
+
+		if val, ok := m["type"]; ok {
+			sa.Type = val.(string)
+		}
+		if val, ok := m["to_urgency"]; ok {
+			sa.ToUrgency = val.(string)
+		}
+		if val, ok := m["at"]; ok {
+			sa.At = *expandActionInlineModel(val)
+		}
+
+		scheduledActions = append(scheduledActions, sa)
+	}
+
+	return scheduledActions
+}
+
+// Returns service's scheduled actions
+func flattenScheduledActions(service *pagerduty.Service) []interface{} {
+	scheduledActions := []interface{}{}
+
+	if sas := service.ScheduledActions; sas != nil {
+		for _, sa := range sas {
+			m := map[string]interface{}{}
+			m["to_urgency"] = sa.ToUrgency
+			m["type"] = sa.Type
+			if at, ok := scheduledActionsAt(sa.At); ok {
+				m["at"] = at
+			}
+			scheduledActions = append(scheduledActions, m)
+		}
+	}
+
+	return scheduledActions
+}
+
+// Returns service's scheduled action's at attribute as slice of length one
+func scheduledActionsAt(inlineModel pagerduty.InlineModel) ([]interface{}, bool) {
+	if inlineModel.Type == "" || inlineModel.Name == "" {
+		return nil, false
+	}
+
+	m := map[string]interface{}{"type": inlineModel.Type, "name": inlineModel.Name}
+	return []interface{}{m}, true
+}
