ab4ed4c008463885997bf5b9501d212a527a5c02
Jake Champlin <jake.champlin.27@gmail.com>
2017-02-13 12:26:06 -0500

    provider/aws: Fix Diff Suppress function for aws_db_instance
    Introduced in #11369, this fixes an issue with the diff suppress function when creating a new `aws_db_instance` resource, while using the default `engine_version`.
    
    ```
    $ make testacc TEST=./builtin/providers/aws TESTARGS='-run=TestAccAWSDBInstance_diffSuppressInitialState'
    ==> Checking that code complies with gofmt requirements...
    go generate $(go list ./... | grep -v /terraform/vendor/)
    2017/02/13 11:52:12 Generated command/internal_plugin_list.go
    TF_ACC=1 go test ./builtin/providers/aws -v -run=TestAccAWSDBInstance_diffSuppressInitialState -timeout 120m
    === RUN   TestAccAWSDBInstance_diffSuppressInitialState
    --- PASS: TestAccAWSDBInstance_diffSuppressInitialState (480.78s)
    PASS
    ok      github.com/hashicorp/terraform/builtin/providers/aws    480.793s
    ```

--- builtin/providers/aws/diff_suppress_funcs.go
+++ builtin/providers/aws/diff_suppress_funcs.go
@@ -19,12 +19,23 @@ func suppressEquivalentAwsPolicyDiffs(k, old, new string, d *schema.ResourceData
 
 // Suppresses minor version changes to the db_instance engine_version attribute
 func suppressAwsDbEngineVersionDiffs(k, old, new string, d *schema.ResourceData) bool {
-	if d.Get("auto_minor_version_upgrade").(bool) {
-		// If we're set to auto upgrade minor versions
-		// ignore a minor version diff between versions
-		if strings.HasPrefix(old, new) {
-			log.Printf("[DEBUG] Ignoring minor version diff")
-			return true
+	// First check if the old/new values are nil.
+	// If both are nil, we have no state to compare the values with, so register a diff.
+	// This populates the attribute field during a plan/apply with fresh state, allowing
+	// the attribute to still be used in future resources.
+	// See https://github.com/hashicorp/terraform/issues/11881
+	if old == "" && new == "" {
+		return false
+	}
+
+	if v, ok := d.GetOk("auto_minor_version_upgrade"); ok {
+		if v.(bool) {
+			// If we're set to auto upgrade minor versions
+			// ignore a minor version diff between versions
+			if strings.HasPrefix(old, new) {
+				log.Printf("[DEBUG] Ignoring minor version diff")
+				return true
+			}
 		}
 	}
 
