2af2aaa51485fa6c3c2df6e252e42564a16265b7
Traver Tischio <travertischio@gmail.com>
2017-02-17 11:36:05 -0500

    provider/fastly Adds fastly response object (#12032)
    * Adds basic schema for response object
    
    * Updates based on differences in fastly response objects
    
    * Refreshes and flattens fastly response object
    
    * Tests fastly response object
    
    * Adds documentation for a fastly response object

--- builtin/providers/fastly/resource_fastly_service_v1.go
+++ builtin/providers/fastly/resource_fastly_service_v1.go
@@ -582,6 +582,58 @@ func resourceServiceV1() *schema.Resource {
 				},
 			},
 
+			"response_object": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required
+						"name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Unique name to refer to this request object",
+						},
+						// Optional fields
+						"status": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     200,
+							Description: "The HTTP Status Code of the object",
+						},
+						"response": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "OK",
+							Description: "The HTTP Response of the object",
+						},
+						"content": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "",
+							Description: "The content to deliver for the response object",
+						},
+						"content_type": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "",
+							Description: "The MIME type of the content",
+						},
+						"request_condition": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "",
+							Description: "Name of the condition to be checked during the request phase to see if the object should be delivered",
+						},
+						"cache_condition": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "",
+							Description: "Name of the condition checked after we have retrieved an object. If the condition passes then deliver this Request Object instead.",
+						},
+					},
+				},
+			},
+
 			"request_setting": {
 				Type:     schema.TypeSet,
 				Optional: true,
@@ -743,6 +795,7 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 		"healthcheck",
 		"s3logging",
 		"papertrail",
+		"response_object",
 		"condition",
 		"request_setting",
 		"cache_setting",
@@ -1276,6 +1329,61 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 			}
 		}
 
+		// find difference in Response Object
+		if d.HasChange("response_object") {
+			or, nr := d.GetChange("response_object")
+			if or == nil {
+				or = new(schema.Set)
+			}
+			if nr == nil {
+				nr = new(schema.Set)
+			}
+
+			ors := or.(*schema.Set)
+			nrs := nr.(*schema.Set)
+			removeResponseObject := ors.Difference(nrs).List()
+			addResponseObject := nrs.Difference(ors).List()
+
+			// DELETE old response object configurations
+			for _, rRaw := range removeResponseObject {
+				rf := rRaw.(map[string]interface{})
+				opts := gofastly.DeleteResponseObjectInput{
+					Service: d.Id(),
+					Version: latestVersion,
+					Name:    rf["name"].(string),
+				}
+
+				log.Printf("[DEBUG] Fastly Response Object removal opts: %#v", opts)
+				err := conn.DeleteResponseObject(&opts)
+				if err != nil {
+					return err
+				}
+			}
+
+			// POST new/updated Response Object
+			for _, rRaw := range addResponseObject {
+				rf := rRaw.(map[string]interface{})
+
+				opts := gofastly.CreateResponseObjectInput{
+					Service:          d.Id(),
+					Version:          latestVersion,
+					Name:             rf["name"].(string),
+					Status:           uint(rf["status"].(int)),
+					Response:         rf["response"].(string),
+					Content:          rf["content"].(string),
+					ContentType:      rf["content_type"].(string),
+					RequestCondition: rf["request_condition"].(string),
+					CacheCondition:   rf["cache_condition"].(string),
+				}
+
+				log.Printf("[DEBUG] Create Response Object Opts: %#v", opts)
+				_, err := conn.CreateResponseObject(&opts)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
 		// find difference in request settings
 		if d.HasChange("request_setting") {
 			os, ns := d.GetChange("request_setting")
@@ -1636,6 +1744,23 @@ func resourceServiceV1Read(d *schema.ResourceData, meta interface{}) error {
 
 		if err := d.Set("papertrail", pl); err != nil {
 			log.Printf("[WARN] Error setting Papertrail for (%s): %s", d.Id(), err)
+		}
+
+		// refresh Response Objects
+		log.Printf("[DEBUG] Refreshing Response Object for (%s)", d.Id())
+		responseObjectList, err := conn.ListResponseObjects(&gofastly.ListResponseObjectsInput{
+			Service: d.Id(),
+			Version: s.ActiveVersion.Number,
+		})
+
+		if err != nil {
+			return fmt.Errorf("[ERR] Error looking up Response Object for (%s), version (%s): %s", d.Id(), s.ActiveVersion.Number, err)
+		}
+
+		rol := flattenResponseObjects(responseObjectList)
+
+		if err := d.Set("response_object", rol); err != nil {
+			log.Printf("[WARN] Error setting Response Object for (%s): %s", d.Id(), err)
 		}
 
 		// refresh Conditions
@@ -2057,6 +2182,33 @@ func flattenPapertrails(papertrailList []*gofastly.Papertrail) []map[string]inte
 	}
 
 	return pl
+}
+
+func flattenResponseObjects(responseObjectList []*gofastly.ResponseObject) []map[string]interface{} {
+	var rol []map[string]interface{}
+	for _, ro := range responseObjectList {
+		// Convert ResponseObjects to a map for saving to state.
+		nro := map[string]interface{}{
+			"name":              ro.Name,
+			"status":            ro.Status,
+			"response":          ro.Response,
+			"content":           ro.Content,
+			"content_type":      ro.ContentType,
+			"request_condition": ro.RequestCondition,
+			"cache_condition":   ro.CacheCondition,
+		}
+
+		// prune any empty values that come from the default string value in structs
+		for k, v := range nro {
+			if v == "" {
+				delete(nro, k)
+			}
+		}
+
+		rol = append(rol, nro)
+	}
+
+	return rol
 }
 
 func flattenConditions(conditionList []*gofastly.Condition) []map[string]interface{} {
