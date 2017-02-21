0e39b057fbc391b68df935c2e4f06f7245f53cb6
Ryon Coleman <ryon55@gmail.com>
2017-02-21 12:49:34 -0500

    Use JS to build a <select> for docs navigation on small screens

--- website/source/assets/javascripts/app/_Docs.js
+++ website/source/assets/javascripts/app/_Docs.js
@@ -10,6 +10,7 @@ var Init = {
     }
 
     this.makeNavSticky();
+    this.buildNavSelect();
   },
 
   hasClass: function (elem, className) {
@@ -22,6 +23,66 @@ var Init = {
     window.addEventListener('resize', _this.onResize, false);
 
     this.resizeImage();
+  },
+
+  buildNavSelect: function(){
+    var $sidebar = $('.docs-sidebar'),
+        $formGroup = $('<div class="form-group docs-nav-mobile">').append('<label>Navigation</label>'),
+        $select = $('<select class="form-control visible-xs visible-sm">'),
+        $options = $([]);
+
+    // hide sidebar
+    // TODO use a better selector? or write this into templates instead of js
+    $sidebar.closest('.col-md-4').wrap($('<div class="hidden-xs hidden-sm"></div>'));
+
+    // kick off recursive search/build for nav <option>s
+    $options = $options.add(this.buildNavOptions($sidebar.find('ul').first(), 0));
+
+    $select
+      .append($options)
+      .appendTo($formGroup)
+      .change(this.onNavSelectChange);
+
+    $formGroup.insertBefore('#main-content');
+  },
+
+  onNavSelectChange: function(event){
+    location = this.value;
+  },
+
+  buildNavOptions: function($list, level){
+    var _this = this,
+        $options = $([]);
+
+    // look for <a>s and new <ul> inside list item
+    $list.children('li').each(function(index, item){
+      var $item = $(item),
+          $link = $item.children('a').first(),
+          $sublist = $item.children('ul').first(),
+          $option,
+          space = '',
+          i = 0;
+
+      // add indentation to indicate current level
+      for (; i<level; i++) {
+        space += '&nbsp;&nbsp;&nbsp;&nbsp;';
+      }
+
+      $option = $('<option>',{
+        value: $link.attr('href'),
+        html: space + $link.text(),
+        selected: $item.hasClass('active')
+      });
+
+      $options = $options.add($option);
+
+      // if there's a sub <ul>, re-run this method one level deeper
+      if ($sublist.length > 0) {
+        $options = $options.add( _this.buildNavOptions($sublist, (level+1)) );
+      }
+    });
+
+    return $options;
   },
 
   onResize: function() {
