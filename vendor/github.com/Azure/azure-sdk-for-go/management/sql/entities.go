c2665d62e2bd1db4425bd4bc5c7cdf12b4e4b8f5
Paul Stack <public@paulstack.co.uk>
2017-02-23 18:14:55 +0200

    provider/azurerm: Bump AzureRM SDK to v8.0.1-beta (#11866)
    * provider/azurerm: Bump AzureRM SDK to v8.0.1-beta
    
    * provider/azurerm: Renaming SDK packages as per MSFT updates
    
    * Bump azurerm sdk 8.0.1 (#12076)
    
    * Updating the constructors to match the updated types
    
    * Updating the Redis Client name
    
    * ObjectID is now a string
    
    * Updating to match the new Storage API specs

--- vendor/github.com/Azure/azure-sdk-for-go/management/sql/entities.go
+++ vendor/github.com/Azure/azure-sdk-for-go/management/sql/entities.go
@@ -27,7 +27,7 @@ const (
 	DatabaseServerVersion12 = "12.0"
 )
 
-// DatabaseServer represents the set of data recieved from
+// DatabaseServer represents the set of data received from
 // a database server list operation.
 //
 // https://msdn.microsoft.com/en-us/library/azure/dn505702.aspx
@@ -45,7 +45,7 @@ type ListServersResponse struct {
 }
 
 // FirewallRuleCreateParams represents the set of possible
-// paramaters when creating a firewall rule on an Azure database server.
+// parameters when creating a firewall rule on an Azure database server.
 //
 // https://msdn.microsoft.com/en-us/library/azure/dn505712.aspx
 type FirewallRuleCreateParams struct {
@@ -55,7 +55,7 @@ type FirewallRuleCreateParams struct {
 	EndIPAddress   string
 }
 
-// FirewallRuleResponse represents the set of data recieved from
+// FirewallRuleResponse represents the set of data received from
 // an Azure database server firewall rule get response.
 //
 // https://msdn.microsoft.com/en-us/library/azure/dn505698.aspx
