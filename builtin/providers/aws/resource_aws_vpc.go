ad884f19426522c06e53f1d98e6a1b49fb431479
clint shryock <clint@ctshryock.com>
2017-02-24 16:04:21 -0600

    update vpc but it probably wont last anyway

--- builtin/providers/aws/resource_aws_vpc.go
+++ builtin/providers/aws/resource_aws_vpc.go
@@ -23,8 +23,9 @@ func resourceAwsVpc() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(5 * time.Minute),
+			Create:  schema.DefaultTimeout(10 * time.Minute),
+			Update:  schema.DefaultTimeout(5 * time.Minute),
+			Default: schema.DefaultTimeout(5 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
