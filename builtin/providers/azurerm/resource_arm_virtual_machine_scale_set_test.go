0ca483f82c9a4af0554f4906ae531590c64c7b0c
Robert Rudduck <rrudduck@ambitenergy.com>
2017-02-26 20:42:46 -0600

    Add multi extension test.

--- builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -105,6 +105,25 @@ func TestAccAzureRMVirtualMachineScaleSet_extension(t *testing.T) {
 	})
 }
 
+func TestAccAzureRMVirtualMachineScaleSet_multipleExtensions(t *testing.T) {
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccAzureRMVirtualMachineScaleSetMultipleExtensionsTemplate, ri, ri, ri, ri, ri, ri)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureRMVirtualMachineScaleSetDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureRMVirtualMachineScaleSetExists("azurerm_virtual_machine_scale_set.test"),
+					testCheckAzureRMVirtualMachineScaleSetExtension("azurerm_virtual_machine_scale_set.test"),
+				),
+			},
+		},
+	})
+}
+
 func testCheckAzureRMVirtualMachineScaleSetExists(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		// Ensure we have enough information in state to look up in API
@@ -654,6 +673,112 @@ SETTINGS
 			"storageAccountKey": "${azurerm_storage_account.test.primary_access_key}"
 		}
 SETTINGS
+	}
+}
+`
+
+var testAccAzureRMVirtualMachineScaleSetMultipleExtensionsTemplate = `
+resource "azurerm_resource_group" "test" {
+    name 	 = "acctestrg-%d"
+    location = "southcentralus"
+}
+
+resource "azurerm_virtual_network" "test" {
+    name 		        = "acctvn-%d"
+    address_space       = ["10.0.0.0/16"]
+    location            = "southcentralus"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+    name                 = "acctsub-%d"
+    resource_group_name  = "${azurerm_resource_group.test.name}"
+    virtual_network_name = "${azurerm_virtual_network.test.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_storage_account" "test" {
+    name                = "accsa%d"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+    location            = "southcentralus"
+    account_type        = "Standard_LRS"
+}
+
+resource "azurerm_storage_container" "test" {
+    name                  = "vhds"
+    resource_group_name   = "${azurerm_resource_group.test.name}"
+    storage_account_name  = "${azurerm_storage_account.test.name}"
+    container_access_type = "private"
+}
+
+resource "azurerm_virtual_machine_scale_set" "test" {
+  	name                = "acctvmss-%d"
+  	location            = "southcentralus"
+  	resource_group_name = "${azurerm_resource_group.test.name}"
+  	upgrade_policy_mode = "Manual"
+	overprovision       = false
+
+  	sku {
+		name     = "Standard_A0"
+    	tier     = "Standard"
+    	capacity = 1
+	}
+
+  	os_profile {
+    	computer_name_prefix = "testvm-%d"
+    	admin_username = "myadmin"
+    	admin_password = "Passwword1234"
+  	}
+
+  	network_profile {
+      	name    = "TestNetworkProfile"
+      	primary = true
+      	ip_configuration {
+        	name	  = "TestIPConfiguration"
+        	subnet_id = "${azurerm_subnet.test.id}"
+      	}
+  	}
+
+  	storage_profile_os_disk {
+    	name 		   = "os-disk"
+    	caching        = "ReadWrite"
+    	create_option  = "FromImage"
+    	vhd_containers = [ "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}" ]
+  	}
+
+  	storage_profile_image_reference {
+    	publisher = "Canonical"
+    	offer     = "UbuntuServer"
+    	sku       = "14.04.2-LTS"
+    	version   = "latest"
+  	}
+
+	extension {
+		name                       = "CustomScript"
+		publisher                  = "Microsoft.Azure.Extensions"
+		type                       = "CustomScript"
+		type_handler_version       = "2.0"
+		auto_upgrade_minor_version = true
+		settings                   = <<SETTINGS
+		{
+			"commandToExecute": "echo $HOSTNAME"
+		}
+SETTINGS
+
+		protected_settings         = <<SETTINGS
+		{
+			"storageAccountName": "${azurerm_storage_account.test.name}",
+			"storageAccountKey": "${azurerm_storage_account.test.primary_access_key}"
+		}
+SETTINGS
+	}
+
+	extension {
+		name                       = "Docker"
+		publisher                  = "Microsoft.Azure.Extensions"
+		type                       = "DockerExtension"
+		type_handler_version       = "1.0"
+		auto_upgrade_minor_version = true
 	}
 }
 `
