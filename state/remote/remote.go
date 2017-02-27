8a4167b133793b452aa069892bc03bc909619c8a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-02-27 13:37:09 -0800

    backend/init: add atlas, remove legacy atlas remote state

--- state/remote/remote.go
+++ state/remote/remote.go
@@ -46,7 +46,6 @@ func NewClient(t string, conf map[string]string) (Client, error) {
 // NewClient.
 var BuiltinClients = map[string]Factory{
 	"artifactory": artifactoryFactory,
-	"atlas":       atlasFactory,
 	"azure":       azureFactory,
 	"etcd":        etcdFactory,
 	"gcs":         gcsFactory,
