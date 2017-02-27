98144fc2ee877bbecf8a00b522993fd8f90a7248
Sean Chittenden <sean@chittenden.org>
2017-02-26 17:17:43 -0800

    Remove stale comment in types.

--- builtin/providers/circonus/types.go
+++ builtin/providers/circonus/types.go
@@ -1,18 +1,12 @@
 package circonus
 
-// NOTE(sean): (ab)use the fact that _ is considered a letter type but not
-// exported.  This allows us to create an intrapackage type system that won't
-// leak to the outside world, but retains most of the style conventions of
-// types, structs, and consts having upper case identifiers.  It's not perfect,
-// but it helps ensure that the compiler is working for us down the road in
-// terms of maintenance.
-//
-// One objectives of this was to ensure that based on aesthetics alone there
-// should be very few locations where type assertions or casting in the main
-// resource files is required (mainly when interacting with the external API
-// structs).  As a rule of thumb, all type assertions should happen in the utils
-// file and casting is only done at assignment time when storing a result to a
-// struct.
+// NOTE(sean): One of the objectives of the use of types is to ensure that based
+// on aesthetics alone are very few locations where type assertions or casting
+// in the main resource files is required (mainly when interacting with the
+// external API structs).  As a rule of thumb, all type assertions should happen
+// in the utils file and casting is only done at assignment time when storing a
+// result to a struct.  Said differently, contained tedium should enable
+// compiler enforcement of types and easy verification.
 
 type apiCheckType string
 
