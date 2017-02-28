03a8e32fedf0ccaee20f82543f5ee80bc4904625
Seth Vargo <seth@sethvargo.com>
2017-02-28 15:55:39 -0500

    Cache in Fastly for a long time (#12309)

--- website/scripts/deploy.sh
+++ website/scripts/deploy.sh
@@ -48,6 +48,9 @@ if [ -z "$NO_UPLOAD" ]; then
     exit 1
   fi
 
+  # Set browser-side cache-control to ~4h, but tell Fastly to cache for much
+  # longer. We manually purge the Fastly cache, so setting it to a year is more
+  # than fine.
   s3cmd \
     --quiet \
     --delete-removed \
@@ -56,6 +59,7 @@ if [ -z "$NO_UPLOAD" ]; then
     --acl-public \
     --recursive \
     --add-header="Cache-Control: max-age=14400" \
+    --add-header="x-amz-meta-surrogate-control: max-age=31536000" \
     --add-header="x-amz-meta-surrogate-key: site-$PROJECT" \
     sync "$DIR/build/" "s3://hc-sites/$PROJECT/latest/"
 
@@ -89,7 +93,7 @@ if [ -z "$NO_UPLOAD" ]; then
     modify "s3://hc-sites/$PROJECT/latest/"
 fi
 
-# Perform a soft-purge of the surrogate key.
+# Perform a purge of the surrogate key.
 if [ -z "$NO_PURGE" ]; then
   echo "Purging Fastly cache..."
   curl \
@@ -99,7 +103,6 @@ if [ -z "$NO_PURGE" ]; then
     --request "POST" \
     --header "Accept: application/json" \
     --header "Fastly-Key: $FASTLY_API_KEY" \
-    --header "Fastly-Soft-Purge: 1" \
     "https://api.fastly.com/service/$FASTLY_SERVICE_ID/purge/site-$PROJECT"
 fi
 
