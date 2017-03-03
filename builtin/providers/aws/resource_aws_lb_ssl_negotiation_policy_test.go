0ba25cf98dd4366d423fba5fcdd08c0f86914805
bclodius <b_clodius@yahoo.com>
2017-03-03 10:18:07 -0500

    Adds back https properties for test elb

--- builtin/providers/aws/resource_aws_lb_ssl_negotiation_policy_test.go
+++ builtin/providers/aws/resource_aws_lb_ssl_negotiation_policy_test.go
@@ -258,9 +258,10 @@ resource "aws_elb" "lb" {
 	availability_zones = ["us-west-2a"]
 	listener {
 		instance_port = 8000
-		instance_protocol = "http"
-		lb_port = 80
-		lb_protocol = "http"
+		instance_protocol = "https"
+		lb_port = 443
+		lb_protocol = "https"
+		ssl_certificate_id = "${aws_iam_server_certificate.test_cert.arn}"
 	}
 }
 resource "aws_lb_ssl_negotiation_policy" "foo" {
