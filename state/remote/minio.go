e49e3e2216928b09ef64528c3fdd1d3cc42d0f3d
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-03-03 14:12:46 +0000

    Add copyright statement and license notice to minio code

--- state/remote/minio.go
+++ state/remote/minio.go
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Genome Research Ltd.
+ *
+ * Author: Joshua C. Randall <jcrandall@alum.mit.edu>
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v.
+ * 2.0. If a copy of the MPL was not
+ * distributed with this file, You can
+ * obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
 package remote
 
 import (
