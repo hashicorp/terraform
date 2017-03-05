36006e4d7c6fb6fb5ef0c292a2f882c43bb6f325
Andre Silva <andre.beat@gmail.com>
2017-03-05 13:44:22 +0000

    provider/statuscake: use default status code list when updating test (#12375)

--- builtin/providers/statuscake/resource_statuscaketest.go
+++ builtin/providers/statuscake/resource_statuscaketest.go
@@ -193,5 +193,13 @@ func getStatusCakeTestInput(d *schema.ResourceData) *statuscake.Test {
 	if v, ok := d.GetOk("port"); ok {
 		test.Port = v.(int)
 	}
+
+	defaultStatusCodes := "204, 205, 206, 303, 400, 401, 403, 404, 405, 406, " +
+		"408, 410, 413, 444, 429, 494, 495, 496, 499, 500, 501, 502, 503, " +
+		"504, 505, 506, 507, 508, 509, 510, 511, 521, 522, 523, 524, 520, " +
+		"598, 599"
+
+	test.StatusCodes = defaultStatusCodes
+
 	return test
 }
