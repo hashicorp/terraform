f6ac200acae872d4a0ff243a5ed947638f7468a9
Clint <catsby@users.noreply.github.com>
2017-03-08 09:29:54 -0600

    provider/aws: Rename 'timeout' to 'build_timeout' for Codebuild (#12503)

--- builtin/providers/aws/resource_aws_codebuild_project_test.go
+++ builtin/providers/aws/resource_aws_codebuild_project_test.go
@@ -342,7 +342,7 @@ resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
 resource "aws_codebuild_project" "foo" {
   name         = "test-project-%s"
   description  = "test_codebuild_project"
-  timeout      = "5"
+  build_timeout      = "5"
 	service_role = "${aws_iam_role.codebuild_role.arn}"
 
 	artifacts {
@@ -429,7 +429,7 @@ resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
 resource "aws_codebuild_project" "foo" {
   name         = "test-project-%s"
   description  = "test_codebuild_project"
-  timeout      = "5"
+  build_timeout      = "5"
 	service_role = "${aws_iam_role.codebuild_role.arn}"
 
 	artifacts {
