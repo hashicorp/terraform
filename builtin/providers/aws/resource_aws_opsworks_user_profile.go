294eb861a888d01a9bf4a0303b5e9512b56eca80
Harry Hull <harry.hull1@gmail.com>
2017-03-13 02:05:22 -0700

    Fixes #11258 (#12595)
    If the user_arn changes on the user_profile resource we should delete it and recreate it
    instead of attempting to modify it incorrectly and causing the terraform apply to fail.
    
    I also added an acceptance test that will trigger this case.

--- builtin/providers/aws/resource_aws_opsworks_user_profile.go
+++ builtin/providers/aws/resource_aws_opsworks_user_profile.go
@@ -26,6 +26,7 @@ func resourceAwsOpsworksUserProfile() *schema.Resource {
 			"user_arn": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 
 			"allow_self_management": &schema.Schema{
