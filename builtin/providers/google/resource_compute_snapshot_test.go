2d1e1741bbb5e19a1ef6e234e00b849695d2cc58
Thomas Poindessous <thomas@skale-5.com>
2017-03-13 12:27:39 +0100

    Test encrypted snapshot of a encrypted disk

--- builtin/providers/google/resource_compute_snapshot_test.go
+++ builtin/providers/google/resource_compute_snapshot_test.go
@@ -146,11 +146,13 @@ resource "google_compute_disk" "foobar" {
 	size = 10
 	type = "pd-ssd"
 	zone = "us-central1-a"
+	disk_encryption_key_raw = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
 }
 resource "google_compute_snapshot" "foobar" {
 	name = "%s"
-	disk = "%s"
+	disk = "${google_compute_disk.foobar.name}"
 	zone = "us-central1-a"
+	sourcedisk_encryption_key_raw = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
 	snapshot_encryption_key_raw = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
-}`, diskName, snapshotName, diskName)
+}`, diskName, snapshotName)
 }
