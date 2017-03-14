6a469a018fc4905b41c90cdf7eeb8230d2873e72
Joshua Spence <josh@freelancer.com>
2017-03-15 07:14:12 +1100

    Allow `name_prefix` to be used with various IAM resources
    Adds the `name_prefix` to `aws_iam_group_policy`, `aws_iam_role_policy` and `aws_iam_user_policy`.

--- builtin/providers/aws/validators.go
+++ builtin/providers/aws/validators.go
@@ -949,3 +949,28 @@ func validateAccountAlias(v interface{}, k string) (ws []string, es []error) {
 
 	return
 }
+
+func validateIamRolePolicyName(v interface{}, k string) (ws []string, errors []error) {
+	// https://github.com/boto/botocore/blob/2485f5c/botocore/data/iam/2010-05-08/service-2.json#L8291-L8296
+	value := v.(string)
+	if len(value) > 128 {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be longer than 128 characters", k))
+	}
+	if !regexp.MustCompile("^[\\w+=,.@-]+$").MatchString(value) {
+		errors = append(errors, fmt.Errorf("%q must match [\\w+=,.@-]", k))
+	}
+	return
+}
+
+func validateIamRolePolicyNamePrefix(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 100 {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be longer than 100 characters", k))
+	}
+	if !regexp.MustCompile("^[\\w+=,.@-]+$").MatchString(value) {
+		errors = append(errors, fmt.Errorf("%q must match [\\w+=,.@-]", k))
+	}
+	return
+}
--- website/source/docs/providers/aws/r/iam_user_policy.html.markdown
+++ website/source/docs/providers/aws/r/iam_user_policy.html.markdown
@@ -49,7 +49,8 @@ The following arguments are supported:
 
 * `policy` - (Required) The policy document. This is a JSON formatted string.
 	The heredoc syntax or `file` function is helpful here.
-* `name` - (Required) Name of the policy.
+* `name` - (Optional) The name of the policy. If omitted, Terraform will assign a random, unique name.
+* `name_prefix` - (Optional, Forces new resource) Creates a unique name beginning with the specified prefix. Conflicts with `name`.
 * `user` - (Required) IAM user to which to attach this policy.
 
 ## Attributes Reference
