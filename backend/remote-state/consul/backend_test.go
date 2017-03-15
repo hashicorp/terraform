eb8736ba1a726eefe3ef1e0d055b6a889ed107b7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-03-14 17:28:42 -0700

    backend/consul: support "lock" option to disable locking
    This adds a "lock" config (default true) to allow users to optionally
    disable state locking with Consul. This is necessary if the token given
    doesn't have session permission and is necessary for backwards
    compatibility.

--- backend/remote-state/consul/backend_test.go
+++ backend/remote-state/consul/backend_test.go
@@ -38,14 +38,44 @@ func TestBackend(t *testing.T) {
 	srv := newConsulTestServer(t)
 	defer srv.Stop()
 
-	// Get the backend
-	b := backend.TestBackendConfig(t, New(), map[string]interface{}{
+	path := fmt.Sprintf("tf-unit/%s", time.Now().String())
+
+	// Get the backend. We need two to test locking.
+	b1 := backend.TestBackendConfig(t, New(), map[string]interface{}{
 		"address": srv.HTTPAddr,
-		"path":    fmt.Sprintf("tf-unit/%s", time.Now().String()),
+		"path":    path,
+	})
+
+	b2 := backend.TestBackendConfig(t, New(), map[string]interface{}{
+		"address": srv.HTTPAddr,
+		"path":    path,
 	})
 
 	// Test
-	backend.TestBackend(t, b)
+	backend.TestBackend(t, b1, b2)
+}
+
+func TestBackend_lockDisabled(t *testing.T) {
+	srv := newConsulTestServer(t)
+	defer srv.Stop()
+
+	path := fmt.Sprintf("tf-unit/%s", time.Now().String())
+
+	// Get the backend. We need two to test locking.
+	b1 := backend.TestBackendConfig(t, New(), map[string]interface{}{
+		"address": srv.HTTPAddr,
+		"path":    path,
+		"lock":    false,
+	})
+
+	b2 := backend.TestBackendConfig(t, New(), map[string]interface{}{
+		"address": srv.HTTPAddr,
+		"path":    path + "different", // Diff so locking test would fail if it was locking
+		"lock":    false,
+	})
+
+	// Test
+	backend.TestBackend(t, b1, b2)
 }
 
 func TestBackend_gzip(t *testing.T) {
