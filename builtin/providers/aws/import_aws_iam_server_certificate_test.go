c26a98f524ead3807c35707d0d8c23f7a0214299
Matt Copper <Matt.Copper@charter.com>
2017-03-20 10:19:43 -0600

    Adding new test for server certificate import.

---
+++ builtin/providers/aws/import_aws_iam_server_certificate_test.go
@@ -0,0 +1,34 @@
+package aws
+
+import (
+	//	"fmt"
+	"testing"
+
+	//	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAWSIAMServerCertificate_importBasic(t *testing.T) {
+	resourceName := "aws_iam_server_certificate.cert"
+
+	//	n := fmt.Sprintf("test-cert-%d", acctest.RandInt())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIAMServerCertificateDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccIAMServerCertConfig,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"force_destroy"},
+			},
+		},
+	})
+}
