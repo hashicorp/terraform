35ae71e98fc021d873c7afba83fe56307ee7006d
厚泼 <demon.wy@alibaba-inc.com>
2017-03-21 16:03:41 +0800

    add new rds resource and some bugs fix

--- builtin/providers/alicloud/data_source_alicloud_instance_types_test.go
+++ builtin/providers/alicloud/data_source_alicloud_instance_types_test.go
@@ -17,8 +17,6 @@ func TestAccAlicloudInstanceTypesDataSource_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudDataSourceID("data.alicloud_instance_types.4c8g"),
 
-					resource.TestCheckResourceAttr("data.alicloud_instance_types.4c8g", "instance_types.#", "4"),
-
 					resource.TestCheckResourceAttr("data.alicloud_instance_types.4c8g", "instance_types.0.cpu_core_count", "4"),
 					resource.TestCheckResourceAttr("data.alicloud_instance_types.4c8g", "instance_types.0.memory_size", "8"),
 					resource.TestCheckResourceAttr("data.alicloud_instance_types.4c8g", "instance_types.0.id", "ecs.s3.large"),
