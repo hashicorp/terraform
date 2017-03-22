7640718ed7cb99b8a3846a85a211f85a36975434
Radek Simko <radek.simko@gmail.com>
2017-03-22 14:46:52 +0000

    provider/aws: Lower metadata log msg from WARN to INFO

--- builtin/providers/aws/auth_helpers.go
+++ builtin/providers/aws/auth_helpers.go
@@ -134,7 +134,7 @@ func GetCredentials(c *Config) (*awsCredentials.Credentials, error) {
 			if usedEndpoint == "" {
 				usedEndpoint = "default location"
 			}
-			log.Printf("[WARN] Ignoring AWS metadata API endpoint at %s "+
+			log.Printf("[INFO] Ignoring AWS metadata API endpoint at %s "+
 				"as it doesn't return any instance-id", usedEndpoint)
 		}
 	}
