808a9f04f6d386a6c8774ba4ef72303a65712bf8
Brandon Tosch <brandontosch@gmail.com>
2017-03-23 12:14:16 -0700

    removed commented import

--- builtin/providers/azurerm/resource_arm_virtual_machine_test.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	//"regexp"
 	"regexp"
 )
 
