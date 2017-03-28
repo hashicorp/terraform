15d59f069d50f1d09cdc10a7986e6c31144f91dc
Paul Stack <public@paulstack.co.uk>
2017-03-28 09:56:35 +0000

    v0.9.2

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
   h.name        = "terraform"
-  h.version     = "0.9.1"
+  h.version     = "0.9.2"
   h.github_slug = "hashicorp/terraform"
 end
 
