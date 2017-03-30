255b01828b7f746971d0dba548d5a54dd60288ca
James Nugent <james@jen20.com>
2017-03-30 15:25:27 -0700

    provider/triton: Move to joyent/triton-go (#13225)
    * provider/triton: Move to joyent/triton-go
    
    This commit moves the Triton provider to the new joyent/triton-go
    library from gosdc. This has a number of advantages - not least that
    requests can be signed using an SSH agent without having to keep
    unencrypted key material in memory.
    
    Schema has been maintained for all resources, and several tests have
    been added and acceptance tests repaired - in some cases by fixing bugs
    in the underlying resources.
    
    After applying this patch, all acceptance tests pass:
    
    ```
    go generate $(go list ./... | grep -v /terraform/vendor/)
    2017/03/30 13:48:33 Generated command/internal_plugin_list.go
    TF_ACC=1 go test ./builtin/providers/triton -v  -timeout 120m
    === RUN   TestProvider
    --- PASS: TestProvider (0.00s)
    === RUN   TestProvider_impl
    --- PASS: TestProvider_impl (0.00s)
    === RUN   TestAccTritonFabric_basic
    --- PASS: TestAccTritonFabric_basic (15.11s)
    === RUN   TestAccTritonFirewallRule_basic
    --- PASS: TestAccTritonFirewallRule_basic (1.48s)
    === RUN   TestAccTritonFirewallRule_update
    --- PASS: TestAccTritonFirewallRule_update (1.55s)
    === RUN   TestAccTritonFirewallRule_enable
    --- PASS: TestAccTritonFirewallRule_enable (1.52s)
    === RUN   TestAccTritonKey_basic
    --- PASS: TestAccTritonKey_basic (11.76s)
    === RUN   TestAccTritonKey_noKeyName
    --- PASS: TestAccTritonKey_noKeyName (11.20s)
    === RUN   TestAccTritonMachine_basic
    --- PASS: TestAccTritonMachine_basic (82.19s)
    === RUN   TestAccTritonMachine_dns
    --- PASS: TestAccTritonMachine_dns (173.36s)
    === RUN   TestAccTritonMachine_nic
    --- PASS: TestAccTritonMachine_nic (167.82s)
    === RUN   TestAccTritonMachine_addNIC
    --- PASS: TestAccTritonMachine_addNIC (192.11s)
    === RUN   TestAccTritonMachine_firewall
    --- PASS: TestAccTritonMachine_firewall (188.53s)
    === RUN   TestAccTritonMachine_metadata
    --- PASS: TestAccTritonMachine_metadata (614.57s)
    === RUN   TestAccTritonVLAN_basic
    --- PASS: TestAccTritonVLAN_basic (0.93s)
    === RUN   TestAccTritonVLAN_update
    --- PASS: TestAccTritonVLAN_update (1.50s)
    PASS
    ok  	github.com/hashicorp/terraform/builtin/providers/triton	1463.621s
    ```
    
    * provider/triton: Update docs for provider config
    
    * deps: Vendor github.com/joyent/triton-go/...
    
    * deps: Remove github.com/joyent/gosdc

--- builtin/providers/triton/resource_fabric_test.go
+++ builtin/providers/triton/resource_fabric_test.go
@@ -9,19 +9,19 @@ import (
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/joyent/gosdc/cloudapi"
+	"github.com/joyent/triton-go"
 )
 
 func TestAccTritonFabric_basic(t *testing.T) {
 	fabricName := fmt.Sprintf("acctest-%d", acctest.RandInt())
-	config := fmt.Sprintf(testAccTritonFabric_basic, fabricName)
+	config := fmt.Sprintf(testAccTritonFabric_basic, acctest.RandIntRange(3, 2049), fabricName, fabricName)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testCheckTritonFabricDestroy,
 		Steps: []resource.TestStep{
-			resource.TestStep{
+			{
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					testCheckTritonFabricExists("triton_fabric.test"),
@@ -37,62 +37,75 @@ func TestAccTritonFabric_basic(t *testing.T) {
 
 func testCheckTritonFabricExists(name string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		// Ensure we have enough information in state to look up in API
 		rs, ok := s.RootModule().Resources[name]
 		if !ok {
 			return fmt.Errorf("Not found: %s", name)
 		}
-		conn := testAccProvider.Meta().(*cloudapi.Client)
+		conn := testAccProvider.Meta().(*triton.Client)
 
-		id, err := strconv.ParseInt(rs.Primary.Attributes["vlan_id"], 10, 16)
+		vlanID, err := strconv.Atoi(rs.Primary.Attributes["vlan_id"])
 		if err != nil {
 			return err
 		}
 
-		fabric, err := conn.GetFabricNetwork(int16(id), rs.Primary.ID)
+		exists, err := resourceExists(conn.Fabrics().GetFabricNetwork(&triton.GetFabricNetworkInput{
+			FabricVLANID: vlanID,
+			NetworkID:    rs.Primary.ID,
+		}))
 		if err != nil {
-			return fmt.Errorf("Bad: Check Fabric Exists: %s", err)
+			return fmt.Errorf("Error: Check Fabric Exists: %s", err)
 		}
 
-		if fabric == nil {
-			return fmt.Errorf("Bad: Fabric %q does not exist", rs.Primary.ID)
+		if exists {
+			return nil
 		}
 
-		return nil
+		return fmt.Errorf("Error: Fabric %q (VLAN %d) Does Not Exist", rs.Primary.ID, vlanID)
 	}
 }
 
 func testCheckTritonFabricDestroy(s *terraform.State) error {
-	conn := testAccProvider.Meta().(*cloudapi.Client)
+	conn := testAccProvider.Meta().(*triton.Client)
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "triton_fabric" {
 			continue
 		}
 
-		id, err := strconv.ParseInt(rs.Primary.Attributes["vlan_id"], 10, 16)
+		vlanID, err := strconv.Atoi(rs.Primary.Attributes["vlan_id"])
 		if err != nil {
 			return err
 		}
 
-		fabric, err := conn.GetFabricNetwork(int16(id), rs.Primary.ID)
+		exists, err := resourceExists(conn.Fabrics().GetFabricNetwork(&triton.GetFabricNetworkInput{
+			FabricVLANID: vlanID,
+			NetworkID:    rs.Primary.ID,
+		}))
 		if err != nil {
 			return nil
 		}
 
-		if fabric != nil {
-			return fmt.Errorf("Bad: Fabric %q still exists", rs.Primary.ID)
+		if exists {
+			return fmt.Errorf("Error: Fabric %q (VLAN %d) Still Exists", rs.Primary.ID, vlanID)
 		}
+
+		return nil
 	}
 
 	return nil
 }
 
 var testAccTritonFabric_basic = `
+resource "triton_vlan" "test" {
+  vlan_id = "%d"
+  name = "%s"
+  description = "testAccTritonFabric_basic"
+}
+
 resource "triton_fabric" "test" {
   name = "%s"
   description = "test network"
-  vlan_id = 2 # every DC seems to have a vlan 2 available
+  vlan_id = "${triton_vlan.test.id}"
 
   subnet = "10.0.0.0/22"
   gateway = "10.0.0.1"
