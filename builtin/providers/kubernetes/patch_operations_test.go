ded6e1f4f7a13d76b8dbbf65a7e27bacb15a5f33
zimbatm <zimbatm@zimbatm.com>
2017-03-31 14:59:57 +0100

    kubernetes PatchOperations tests reliability
    This fixes a flakey test where a map is turned into a list but doesn't
    always produce the same result because go maps don't guarantee
    predictable ordering.
    
    PatchOperations.Equal sorts the left-hand side before applying the
    comparison.

--- builtin/providers/kubernetes/patch_operations_test.go
+++ builtin/providers/kubernetes/patch_operations_test.go
@@ -117,7 +117,7 @@ func TestDiffStringMap(t *testing.T) {
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			ops := diffStringMap(tc.Path, tc.Old, tc.New)
-			if !tc.ExpectedOps.Equal(ops) {
+			if !ops.Equal(tc.ExpectedOps) {
 				t.Fatalf("Operations don't match.\nExpected: %v\nGiven:    %v\n", tc.ExpectedOps, ops)
 			}
 		})
