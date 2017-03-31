49a2b8c85d927a952ce30e4c2f2ded41b6c17880
Ryon Coleman <ryon55@gmail.com>
2017-03-31 00:31:36 -0400

    Implement chevron icons, reset button; refine filter/collapse behavior

--- website/source/assets/javascripts/app/_Docs.js
+++ website/source/assets/javascripts/app/_Docs.js
@@ -23,10 +23,19 @@ var Init = {
   addEventListeners: function(){
     var _this = this;
 
-    $('.nav-filter-input').on('input', $.proxy(_this.onFilterChange, _this));
-    window.addEventListener('resize', _this.onResize, false);
+    // ignore filter submit
+    $('.nav-filter')
+      .on('submit', function(event){ event.preventDefault(); })
+      .on('reset', function(event){ _this.clearFilter() });
 
-    this.resizeImage();
+    $('.nav-filter-input')
+      .on('input', $.proxy(_this.onFilterInput, _this))
+      .on('keyup', $.proxy(_this.onFilterKeyDown, _this));
+
+    $(window)
+      .on('resize', $.proxy(_this.onResize, _this));
+
+    _this.resizeImage();
   },
 
   buildNavSelect: function(){
@@ -46,56 +55,116 @@ var Init = {
     $formGroup.insertBefore('#main-content');
   },
 
+  clearFilter: function() {
+    var $input = $('.nav-filter-input'),
+      fakeEvent = {
+        target: $input
+      };
+
+    $input.val('');
+    this.onFilterInput(fakeEvent);
+  },
+
   initCollapsing: function() {
+    var _this = this,
+      $activeEl = $('.active');
+
     $('.docs-sidenav ul')
       .removeClass('nav-visible')
       .parents('li')
         .addClass('has-sublist')
-        .click(this.onCollapseClick);
+          .children('a')
+            .click(this.onCollapseClick);
+
+    _this.$superLists = $('.has-sublist');
+    _this.$superLists.filter('.active.has-sublist').addClass('is-expanded');
+
+    if ($activeEl.offset().top + 50 > $(window).height()) {
+      $activeEl[0].scrollIntoView();
+      window.scrollTo(0,0);
+    }
   },
 
   initFilter: function() {
     var _this = this;
 
-    _this.$unfilterableItems = $('.docs-sidenav > li');
-    _this.$filterableLinks = $('.docs-sidenav li a');
+    _this.$filterResetButton = $('.nav-filter button[type="reset"]');
 
+    _this.$filterableLinks = $('.docs-sidenav li a');
     _this.$filterableLinks.each(function(i, el) {
       var $el = $(el);
-
       $el.data('text', $el.text().trim());
     });
+
+    $('nav-visible').removeClass('nav-visible');
   },
 
-  onCollapseClick: function() {
-    $(this).find('ul').toggleClass('nav-visible');
+  onCollapseClick: function(event) {
+    event.preventDefault();
+    $(this).closest('.has-sublist').toggleClass('is-expanded');
   },
 
-  onFilterChange: function(event) {
+  onFilterInput: function(event) {
     var _this = this,
         $input = $(event.target),
         filterString = $input.val().trim().toLowerCase(),
         matchLength = filterString.length;
 
+    // reset collapses
+    _this.$superLists.show()
+      .addClass('is-expanded');
+
+    // collapse everything except active, if blank search
+    if (filterString.length == 0) {
+      _this.$filterResetButton.hide();
+      _this.$superLists.show()
+        .filter(':not(.active)')
+          .removeClass('is-expanded');
+    } else {
+      _this.$filterResetButton.show();
+    }
+
+    // run search
     _this.$filterableLinks.each(function(i, el) {
-      var $link = $(el).first(),
+      var $link = $(el),
           $item = $link.closest('li'),
           linkText = $link.data('text'),
           matchIndex = linkText.toLowerCase().indexOf(filterString),
           matchEnd = matchIndex + matchLength;
 
-      if (matchIndex == -1) {
-        $item.hide();
-        $link.html(linkText);
+      if (matchIndex == -1) { // hide nonmatches
+        if (!$item.hasClass('has-sublist')) { // exclude superLists from hide
+          $item.hide();
+        }
+        $link.html(linkText); // reset text to have no highlight
       } else {
-        $link.parents('li').show()
-        $link.html(
+        $link.html( // highlight search term
           linkText.substr(0, matchIndex)
           + '<mark>' + linkText.substr(matchIndex, matchLength) + '</mark>'
           + linkText.substr(matchEnd)
         );
+        $item.show();
       }
     });
+
+    // make sure non-empty superlists are visible, empty ones hidden
+    if (filterString.length > 0) {
+      _this.$superLists.each(function(index, list) {
+        var $list = $(list);
+        if ($list.find('li').filter(':visible').length > 0) {
+          $list.show().addClass('is-expanded');
+        } else {
+          $list.hide().removeClass('is-expanded');
+        }
+      });
+    }
+  },
+
+  // clear filter on esc
+  onFilterKeyDown: function(event) {
+    if (event.which == 27) {
+      this.clearFilter();
+    }
   },
 
   onNavSelectChange: function(event){
