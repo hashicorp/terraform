146fb00d24add1c6190f8a24e1ac0b7acd907b19
Seth Vargo <sethvargo@gmail.com>
2017-04-05 11:28:34 -0400

    Update basic scaffolding

--- website/config.rb
+++ website/config.rb
@@ -26,17 +26,66 @@ helpers do
   #
   # @return [String]
   def description_for(page)
-    return escape_html(page.data.description || "")
+    description = (page.data.description || "")
+      .gsub('"', '')
+      .gsub(/\n+/, ' ')
+      .squeeze(' ')
+
+    return escape_html(description)
   end
 
   # This helps by setting the "active" class for sidebar nav elements
   # if the YAML frontmatter matches the expected value.
   def sidebar_current(expected)
     current = current_page.data.sidebar_current || ""
-    if current == expected or (expected.is_a?(Regexp) and expected.match(current))
+    if current.start_with?(expected)
       return " class=\"active\""
     else
       return ""
     end
+  end
+
+  # Returns the id for this page.
+  # @return [String]
+  def body_id_for(page)
+    if !(name = page.data.sidebar_current).blank?
+      return "page-#{name.strip}"
+    end
+    if page.url == "/" || page.url == "/index.html"
+      return "page-home"
+    end
+    if !(title = page.data.page_title).blank?
+      return title
+        .downcase
+        .gsub('"', '')
+        .gsub(/[^\w]+/, '-')
+        .gsub(/_+/, '-')
+        .squeeze('-')
+        .squeeze(' ')
+    end
+    return ""
+  end
+
+  # Returns the list of classes for this page.
+  # @return [String]
+  def body_classes_for(page)
+    classes = []
+
+    if !(layout = page.data.layout).blank?
+      classes << "layout-#{page.data.layout}"
+    end
+
+    if !(title = page.data.page_title).blank?
+      title = title
+        .downcase
+        .gsub('"', '')
+        .gsub(/[^\w]+/, '-')
+        .gsub(/_+/, '-')
+        .squeeze('-')
+        .squeeze(' ')
+      classes << "page-#{title}"
+    end
+
+    return classes.join(" ")
   end
 end
