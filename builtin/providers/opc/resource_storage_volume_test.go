4ae439bc785ea77d103c62bb5c6e58c78e0e649c
tombuildsstuff <git@ibuildstuff.co.uk>
2017-04-06 19:12:20 +0100

    Refactoring Bootable Storage Volumes

--- builtin/providers/opc/resource_storage_volume_test.go
+++ builtin/providers/opc/resource_storage_volume_test.go
@@ -116,9 +116,10 @@ func TestAccOPCStorageVolume_Bootable(t *testing.T) {
 	})
 }
 
-func TestAccOPCStorageVolume_FromSnapshot(t *testing.T) {
+func TestAccOPCStorageVolume_ImageListEntry(t *testing.T) {
 	volumeResourceName := "opc_compute_storage_volume.test"
-	rInt := acctest.RandInt()
+	ri := acctest.RandInt()
+	config := fmt.Sprintf(testAccStorageVolumeImageListEntry, ri, ri)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -126,13 +127,9 @@ func TestAccOPCStorageVolume_FromSnapshot(t *testing.T) {
 		CheckDestroy: opcResourceCheck(volumeResourceName, testAccCheckStorageVolumeDestroyed),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccStorageVolumeFromSnapshot(rInt),
+				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					opcResourceCheck(volumeResourceName, testAccCheckStorageVolumeExists),
-					resource.TestCheckResourceAttr(volumeResourceName, "name", fmt.Sprintf("test-acc-stor-vol-final-%d", rInt)),
-					resource.TestCheckResourceAttrSet(volumeResourceName, "snapshot"),
-					resource.TestCheckResourceAttrSet(volumeResourceName, "snapshot_id"),
-					resource.TestCheckResourceAttr(volumeResourceName, "size", "5"),
 				),
 			},
 		},
@@ -206,17 +203,44 @@ resource "opc_compute_storage_volume" "test" {
 const testAccStorageVolumeBootable = `
 resource "opc_compute_image_list" "test" {
   name        = "test-acc-stor-vol-bootable-image-list-%d"
-  description = "Provider Acceptance Tests Storage Volume"
+  description = "Provider Acceptance Tests Storage Volume Bootable"
+}
+
+resource "opc_compute_image_list_entry" "test" {
+  name           = "${opc_compute_image_list.test.name}"
+  machine_images = [ "/oracle/public/oel_6.7_apaas_16.4.5_1610211300" ]
+  version        = 1
 }
 
 resource "opc_compute_storage_volume" "test" {
-  name        = "test-acc-stor-vol-bootable-%d"
-  description = "Provider Acceptance Tests Storage Volume"
-  size        = 2
-  tags        = ["bar", "foo"]
-  bootable {
-  	image_list = "${opc_compute_image_list.test.name}"
-  }
+  name             = "test-acc-stor-vol-bootable-%d"
+  description      = "Provider Acceptance Tests Storage Volume Bootable"
+  size             = 20
+  tags             = ["bar", "foo"]
+  bootable         = true
+  image_list       = "${opc_compute_image_list.test.name}"
+  image_list_entry = "${opc_compute_image_list_entry.test.version}"
+}
+`
+
+const testAccStorageVolumeImageListEntry = `
+resource "opc_compute_image_list" "test" {
+  name        = "test-acc-stor-vol-bootable-image-list-%d"
+  description = "Provider Acceptance Tests Storage Volume Image List Entry"
+}
+
+resource "opc_compute_image_list_entry" "test" {
+  name           = "${opc_compute_image_list.test.name}"
+  machine_images = [ "/oracle/public/oel_6.7_apaas_16.4.5_1610211300" ]
+  version        = 1
+}
+
+resource "opc_compute_storage_volume" "test" {
+  name             = "test-acc-stor-vol-bootable-%d"
+  description      = "Provider Acceptance Tests Storage Volume Image List Entry"
+  size             = 20
+  tags             = ["bar", "foo"]
+  image_list_entry = "${opc_compute_image_list_entry.test.version}"
 }
 `
 
@@ -227,29 +251,3 @@ resource "opc_compute_storage_volume" "test" {
   size = 2048
 }
 `
-
-func testAccStorageVolumeFromSnapshot(rInt int) string {
-	return fmt.Sprintf(`
-// Initial Storage Volume to create snapshot with
-resource "opc_compute_storage_volume" "foo" {
-  name = "test-acc-stor-vol-%d"
-  description = "Acc Test intermediary storage volume for snapshot"
-  size = 5
-}
-
-resource "opc_compute_storage_volume_snapshot" "foo" {
-  description = "testing-acc"
-  name = "test-acc-stor-snapshot-%d"
-  collocated = true
-  volume_name = "${opc_compute_storage_volume.foo.name}"
-}
-
-// Create storage volume from snapshot
-resource "opc_compute_storage_volume" "test" {
-  name = "test-acc-stor-vol-final-%d"
-  description = "storage volume from snapshot"
-  size = 5
-  snapshot_id = "${opc_compute_storage_volume_snapshot.foo.snapshot_id}"
-}
-`, rInt, rInt, rInt)
-}
