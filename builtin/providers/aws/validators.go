05d7b1e3b050d7c346bbc2e6edbe5c4a6edf7c1d
Joshua Spence <josh@joshuaspence.com>
2017-04-07 11:15:58 +1000

    Add `name_prefix` to `aws_alb_target_group`
    Adds the `name_prefix` parameter to the `aws_alb_target_group` resource.

--- builtin/providers/aws/validators.go
+++ builtin/providers/aws/validators.go
@@ -1154,3 +1154,19 @@ func validateDbOptionGroupNamePrefix(v interface{}, k string) (ws []string, erro
 	}
 	return
 }
+
+func validateAwsAlbTargetGroupName(v interface{}, k string) (ws []string, errors []error) {
+	name := v.(string)
+	if len(name) > 32 {
+		errors = append(errors, fmt.Errorf("%q (%q) cannot be longer than '32' characters", k, name))
+	}
+	return
+}
+
+func validateAwsAlbTargetGroupNamePrefix(v interface{}, k string) (ws []string, errors []error) {
+	name := v.(string)
+	if len(name) > 32 {
+		errors = append(errors, fmt.Errorf("%q (%q) cannot be longer than '6' characters", k, name))
+	}
+	return
+}
