9622b49c457ed3cf72689c78ab94352e1e8e0e89
Joern Barthel <joern.barthel@kreuzwerker.de>
2017-04-07 10:41:55 +0200

    Support for Windows newlines.

--- config/interpolate_funcs_test.go
+++ config/interpolate_funcs_test.go
@@ -386,10 +386,38 @@ func TestInterpolateFuncChomp(t *testing.T) {
 			},
 
 			{
-				`${chomp("goodbye\ncruel\nworld\n")}`,
-				`goodbye
-cruel
-world`,
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\ncruel\nworld"),
+				"goodbye\ncruel\nworld",
+				false,
+			},
+
+			{
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\r\nwindows\r\nworld"),
+				"goodbye\r\nwindows\r\nworld",
+				false,
+			},
+
+			{
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\ncruel\nworld\n"),
+				"goodbye\ncruel\nworld",
+				false,
+			},
+
+			{
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\ncruel\nworld\n\n\n\n"),
+				"goodbye\ncruel\nworld",
+				false,
+			},
+
+			{
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\r\nwindows\r\nworld\r\n"),
+				"goodbye\r\nwindows\r\nworld",
+				false,
+			},
+
+			{
+				fmt.Sprintf(`${chomp("%s")}`, "goodbye\r\nwindows\r\nworld\r\n\r\n\r\n\r\n"),
+				"goodbye\r\nwindows\r\nworld",
 				false,
 			},
 		},
