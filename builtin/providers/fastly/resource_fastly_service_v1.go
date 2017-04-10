f900f464a4205a197d555e607c9f4e7bfbd44e3d
Marcin Suterski <msuterski@users.noreply.github.com>
2017-04-10 07:04:14 -0400

    Add support for Sumologic logging to Fastly provider (#12541)

--- builtin/providers/fastly/resource_fastly_service_v1.go
+++ builtin/providers/fastly/resource_fastly_service_v1.go
@@ -539,7 +539,7 @@ func resourceServiceV1() *schema.Resource {
 							Optional:     true,
 							Default:      1,
 							Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
-							ValidateFunc: validateS3FormatVersion,
+							ValidateFunc: validateLoggingFormatVersion,
 						},
 						"timestamp_format": {
 							Type:        schema.TypeString,
@@ -590,6 +590,52 @@ func resourceServiceV1() *schema.Resource {
 							Optional:    true,
 							Default:     "",
 							Description: "Name of a condition to apply this logging",
+						},
+					},
+				},
+			},
+			"sumologic": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Required fields
+						"name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Unique name to refer to this logging setup",
+						},
+						"url": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "The URL to POST to.",
+						},
+						// Optional fields
+						"format": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "%h %l %u %t %r %>s",
+							Description: "Apache-style string or VCL variables to use for log formatting",
+						},
+						"format_version": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      1,
+							Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
+							ValidateFunc: validateLoggingFormatVersion,
+						},
+						"response_condition": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "",
+							Description: "Name of a condition to apply this logging.",
+						},
+						"message_type": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      "classic",
+							Description:  "How the message should be formatted.",
+							ValidateFunc: validateLoggingMessageType,
 						},
 					},
 				},
@@ -1344,6 +1390,59 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 			}
 		}
 
+		// find difference in Sumologic
+		if d.HasChange("sumologic") {
+			os, ns := d.GetChange("sumologic")
+			if os == nil {
+				os = new(schema.Set)
+			}
+			if ns == nil {
+				ns = new(schema.Set)
+			}
+
+			oss := os.(*schema.Set)
+			nss := ns.(*schema.Set)
+			removeSumologic := oss.Difference(nss).List()
+			addSumologic := nss.Difference(oss).List()
+
+			// DELETE old sumologic configurations
+			for _, pRaw := range removeSumologic {
+				sf := pRaw.(map[string]interface{})
+				opts := gofastly.DeleteSumologicInput{
+					Service: d.Id(),
+					Version: latestVersion,
+					Name:    sf["name"].(string),
+				}
+
+				log.Printf("[DEBUG] Fastly Sumologic removal opts: %#v", opts)
+				err := conn.DeleteSumologic(&opts)
+				if err != nil {
+					return err
+				}
+			}
+
+			// POST new/updated Sumologic
+			for _, pRaw := range addSumologic {
+				sf := pRaw.(map[string]interface{})
+				opts := gofastly.CreateSumologicInput{
+					Service:           d.Id(),
+					Version:           latestVersion,
+					Name:              sf["name"].(string),
+					URL:               sf["url"].(string),
+					Format:            sf["format"].(string),
+					FormatVersion:     sf["format_version"].(int),
+					ResponseCondition: sf["response_condition"].(string),
+					MessageType:       sf["message_type"].(string),
+				}
+
+				log.Printf("[DEBUG] Create Sumologic Opts: %#v", opts)
+				_, err := conn.CreateSumologic(&opts)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
 		// find difference in Response Object
 		if d.HasChange("response_object") {
 			or, nr := d.GetChange("response_object")
@@ -1759,6 +1858,22 @@ func resourceServiceV1Read(d *schema.ResourceData, meta interface{}) error {
 
 		if err := d.Set("papertrail", pl); err != nil {
 			log.Printf("[WARN] Error setting Papertrail for (%s): %s", d.Id(), err)
+		}
+
+		// refresh Sumologic Logging
+		log.Printf("[DEBUG] Refreshing Sumologic for (%s)", d.Id())
+		sumologicList, err := conn.ListSumologics(&gofastly.ListSumologicsInput{
+			Service: d.Id(),
+			Version: s.ActiveVersion.Number,
+		})
+
+		if err != nil {
+			return fmt.Errorf("[ERR] Error looking up Sumologic for (%s), version (%s): %s", d.Id(), s.ActiveVersion.Number, err)
+		}
+
+		sul := flattenSumologics(sumologicList)
+		if err := d.Set("sumologic", sul); err != nil {
+			log.Printf("[WARN] Error setting Sumologic for (%s): %s", d.Id(), err)
 		}
 
 		// refresh Response Objects
@@ -2179,7 +2294,7 @@ func flattenS3s(s3List []*gofastly.S3) []map[string]interface{} {
 func flattenPapertrails(papertrailList []*gofastly.Papertrail) []map[string]interface{} {
 	var pl []map[string]interface{}
 	for _, p := range papertrailList {
-		// Convert S3s to a map for saving to state.
+		// Convert Papertrails to a map for saving to state.
 		ns := map[string]interface{}{
 			"name":               p.Name,
 			"address":            p.Address,
@@ -2199,6 +2314,32 @@ func flattenPapertrails(papertrailList []*gofastly.Papertrail) []map[string]inte
 	}
 
 	return pl
+}
+
+func flattenSumologics(sumologicList []*gofastly.Sumologic) []map[string]interface{} {
+	var l []map[string]interface{}
+	for _, p := range sumologicList {
+		// Convert Sumologic to a map for saving to state.
+		ns := map[string]interface{}{
+			"name":               p.Name,
+			"url":                p.URL,
+			"format":             p.Format,
+			"response_condition": p.ResponseCondition,
+			"message_type":       p.MessageType,
+			"format_version":     int(p.FormatVersion),
+		}
+
+		// prune any empty values that come from the default string value in structs
+		for k, v := range ns {
+			if v == "" {
+				delete(ns, k)
+			}
+		}
+
+		l = append(l, ns)
+	}
+
+	return l
 }
 
 func flattenResponseObjects(responseObjectList []*gofastly.ResponseObject) []map[string]interface{} {
