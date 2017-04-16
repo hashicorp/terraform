9e3c063efb6a9acba80dfcbb87554474ab8e4956
Radek Simko <radek.simko@gmail.com>
2017-04-16 17:45:44 +0100

    provider/kubernetes: Add support for limit_range

--- builtin/providers/kubernetes/provider.go
+++ builtin/providers/kubernetes/provider.go
@@ -87,6 +87,7 @@ func Provider() terraform.ResourceProvider {
 
 		ResourcesMap: map[string]*schema.Resource{
 			"kubernetes_config_map":              resourceKubernetesConfigMap(),
+			"kubernetes_limit_range":             resourceKubernetesLimitRange(),
 			"kubernetes_namespace":               resourceKubernetesNamespace(),
 			"kubernetes_persistent_volume":       resourceKubernetesPersistentVolume(),
 			"kubernetes_persistent_volume_claim": resourceKubernetesPersistentVolumeClaim(),
