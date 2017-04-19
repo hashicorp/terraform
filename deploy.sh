82a0b6baa09c3a52b250ee11921db38472b2bcd4
Scott Nowicki <scott@sgn-mbp>
2017-04-18 19:09:15 -0500

    added deploy script, updated travis.yml to build topic- branches

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+terraform get
+terraform plan
+terraform apply
+
+# echo "Setting git user name"
+# git config user.name $GH_USER_NAME
+#
+# echo "Setting git user email"
+# git config user.email $GH_USER_EMAIL
+#
+# echo "Adding git upstream remote"
+# git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
+#
+# git checkout master
+
+
+#
+# NOW=$(TZ=America/Chicago date)
+#
+# git commit -m "tfstate: $NOW [ci skip]"
+#
+# echo "Pushing changes to upstream master"
+# git push upstream master
