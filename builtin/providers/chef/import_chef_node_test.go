0303f2ab966d7d45acf3fe9c8c3e7edcce4322a5
Anthony Petitbois <anthony@petitbois.net>
2017-04-20 16:20:54 -0400

    acceptance testing for import.

---
+++ builtin/providers/chef/import_chef_node_test.go
@@ -0,0 +1,32 @@
+package chef
+
+import (
+	"testing"
+
+	chefc "github.com/go-chef/chef"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccChefNode_importBasic(t *testing.T) {
+	var node chefc.Node
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNodeConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccNodeCheckExists("chef_node.test", &node),
+				),
+			},
+			resource.TestStep{
+				ResourceName:            "chef_node.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
