e26ab38f41f377a4ed33533bbc8c2fc6e9517f29
tombuildsstuff <git@ibuildstuff.co.uk>
2017-04-20 16:49:29 +0100

    Provision VM Agent / Enable Automatic Upgrades need to be ForceNew

--- builtin/providers/azurerm/resource_arm_virtual_machine.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine.go
@@ -354,11 +354,13 @@ func resourceArmVirtualMachine() *schema.Resource {
 						"provision_vm_agent": {
 							Type:     schema.TypeBool,
 							Optional: true,
+							ForceNew: true,
 							Default:  true,
 						},
 						"enable_automatic_upgrades": {
 							Type:     schema.TypeBool,
 							Optional: true,
+							ForceNew: true,
 							Default:  false,
 						},
 						"winrm": {
