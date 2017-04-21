fbd00594a3be2fc8eadccb4213ad3699c764dff9
Scott Nowicki <scott@sgn-mbp>
2017-04-20 19:46:42 -0500

    simplified deploy.sh, fixed after_deploy delete via cli

--- examples/azure-vm-simple-linux/deploy.sh
+++ examples/azure-vm-simple-linux/deploy.sh
@@ -7,33 +7,14 @@ set -o errexit -o nounset
 # PASSWORD=$KEY$(cat /dev/urandom | tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | tr -cd '0-9' | head -c 2)
 
 docker run --rm -it \
-  -v $(pwd):/data -w /data \
-  hashicorp/terraform:light \
-  get
-
-docker run --rm -it \
   -e ARM_CLIENT_ID \
   -e ARM_CLIENT_SECRET \
   -e ARM_SUBSCRIPTION_ID \
   -e ARM_TENANT_ID \
-  -v $(pwd):/data -w /data \
+  -v $(pwd):/data \
+  --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
-  plan -var dns_name=$KEY -var resource_group=$KEY -var admin_username=$KEY -var admin_password=$PASSWORD -out=out.tfplan
-
-docker run --rm -it \
-  -e ARM_CLIENT_ID \
-  -e ARM_CLIENT_SECRET \
-  -e ARM_SUBSCRIPTION_ID \
-  -e ARM_TENANT_ID \
-  -v $(pwd):/data -w /data \
-  hashicorp/terraform:light \
-  apply out.tfplan
-
-# terraform get
-#
-# terraform plan -var 'dns_name='$KEY -var 'admin_password='$PASSWORD -var 'admin_username='$KEY -var 'resource_group='$KEY -out=out.tfplan
-#
-# terraform apply out.tfplan
+  -c "cd /data; /bin/terraform get; /bin/terraform plan -var dns_name=$KEY -var resource_group=$KEY -var admin_username=$KEY -var admin_password=$PASSWORD -out=out.tfplan; /bin/terraform apply out.tfplan"
 
 # TODO: determine external validation, possibly Azure CLI
 
