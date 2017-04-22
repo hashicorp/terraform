34d0c450ff3c0efdb2b75b0e93664110bdeed65e
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-22 17:27:49 -0500

    updated to best practices/conventions

--- examples/azure-vm-simple-linux/variables.tf
+++ examples/azure-vm-simple-linux/variables.tf
@@ -4,12 +4,18 @@ variable "resource_group" {
 
 variable "rg_prefix" {
   description = "The shortened abbreviation to represent your resource group that will go on the front of some resources."
-  default = "rg"
+}
+
+variable "hostname" {
+  description = "VM name referenced also in storage-related names."
+}
+
+variable "dns_name" {
+  description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
 }
 
 variable "location" {
   description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
-  default     = "southcentralus"
 }
 
 variable "virtual_network_name" {
@@ -55,15 +61,6 @@ variable "image_sku" {
 variable "image_version" {
   description = "version of the image to apply (az vm image list)"
   default     = "latest"
-}
-
-variable "hostname" {
-  description = "VM name referenced also in storage-related names."
-  default     = "myvm"
-}
-
-variable "dns_name" {
-  description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
 }
 
 variable "admin_username" {
