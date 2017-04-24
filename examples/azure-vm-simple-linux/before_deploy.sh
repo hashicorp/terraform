8fbbdfd2629691acb84e62e199abb32220df24b1
Scott Nowicki <scott@sgn-mbp>
2017-04-24 13:27:00 -0500

    easier to export in before_deploy block

--- examples/azure-vm-simple-linux/before_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -o errexit -o nounset
-
-export KEY=$(cat /dev/urandom | tr -cd 'a-z' | head -c 12)
-export PASSWORD=$KEY$(cat /dev/urandom | tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | tr -cd '0-9' | head -c 2)
