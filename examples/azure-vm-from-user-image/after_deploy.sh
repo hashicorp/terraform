f9bf6baaf3724acc7bbc72cc015a7c40f79db2ab
Scott Nowicki <scott@sgn-mbp>
2017-04-25 18:07:09 -0500

    prep for PR

--- examples/azure-vm-from-user-image/after_deploy.sh
+++ examples/azure-vm-from-user-image/after_deploy.sh
@@ -2,8 +2,10 @@
 
 set -o errexit -o nounset
 
-# cleanup deployed azure resources
 docker run --rm -it \
   azuresdk/azure-cli-python \
   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
-         az group delete -y -n $KEY"
+         az vm delete                 --name $KEY --resource-group permanent -y; \
+         az network nic delete       --name $KEY'nic' --resource-group permanent; \
+         az network vnet delete       --name $KEY'vnet' --resource-group permanent; \
+         az network public-ip delete --name $KEY'-ip' --resource-group permanent;"
--- examples/azure-vm-from-user-image/deploy.mac.sh
+++ examples/azure-vm-from-user-image/deploy.mac.sh
@@ -5,13 +5,8 @@ set -o errexit -o nounset
 # generate a unique string for CI deployment
 export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
 export PASSWORD=$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
-export IMAGE_URI=https://myrgdisks640.blob.core.windows.net/vhds/original-vm20170424164303.vhd
-export STORAGE_ACCOUNT_NAME=myrgdisks640
-export RG=myrg
+export EXISTING_IMAGE_URI=https://permanentstor.blob.core.windows.net/permanent-vhds/permanent-osdisk1.vhd
+export EXISTING_STORAGE_ACCOUNT_NAME=permanentstor
+export EXISTING_RESOURCE_GROUP=permanent
 
-/bin/sh ./deploy.sh
-
-# docker run --rm -it \
-#     azuresdk/azure-cli-python \
-#     sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
-#            az group delete -y -n $KEY"
+/bin/sh ./after_deploy.sh
