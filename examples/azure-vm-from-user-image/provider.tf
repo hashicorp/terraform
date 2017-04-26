d78e8bd4bdad292eff2a0dacc92299d5798e15d3
Scott Nowicki <scott@sgn-mbp>
2017-04-26 10:52:16 -0500

    more PR prep

---
+++ examples/azure-vm-from-user-image/provider.tf
@@ -0,0 +1,6 @@
+# provider "azurerm" {
+#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
+#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
+#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
+#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
+# }
