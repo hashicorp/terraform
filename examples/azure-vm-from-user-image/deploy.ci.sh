c98b1d11780976d1bba35b3537831102f5ef475d
Scott Nowicki <scott@sgn-mbp>
2017-04-27 16:29:02 -0500

    consolidated deploy and after_deploy into a single script; simplified ci process; added os_profile_linux_config

--- examples/azure-vm-from-user-image/deploy.ci.sh
+++ examples/azure-vm-from-user-image/deploy.ci.sh
@@ -16,22 +16,24 @@ docker run --rm -it \
       /bin/terraform plan -out=out.tfplan -var hostname=$KEY -var resource_group=$EXISTING_RESOURCE_GROUP -var admin_username=$KEY -var admin_password=$PASSWORD -var image_uri=$EXISTING_IMAGE_URI -var storage_account_name=$EXISTING_STORAGE_ACCOUNT_NAME; \
       /bin/terraform apply out.tfplan"
 
-# echo "Setting git user name"
-# git config user.name $GH_USER_NAME
-#
-# echo "Setting git user email"
-# git config user.email $GH_USER_EMAIL
-#
-# echo "Adding git upstream remote"
-# git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
-#
-# git checkout master
+# cleanup deployed azure resources via terraform
+docker run --rm -it \
+  -e ARM_CLIENT_ID \
+  -e ARM_CLIENT_SECRET \
+  -e ARM_SUBSCRIPTION_ID \
+  -e ARM_TENANT_ID \
+  -v $(pwd):/data \
+  --workdir=/data \
+  --entrypoint "/bin/sh" \
+  hashicorp/terraform:light \
+  -c "/bin/terraform destroy -force -var hostname=$KEY -var resource_group=$EXISTING_RESOURCE_GROUP -var admin_username=$KEY -var admin_password=$PASSWORD -var image_uri=$EXISTING_IMAGE_URI -var storage_account_name=$EXISTING_STORAGE_ACCOUNT_NAME -target=azurerm_virtual_machine.vm -target=azurerm_network_interface.nic -target=azurerm_virtual_network.vnet -target=azurerm_public_ip.pip;"
 
 
-#
-# NOW=$(TZ=America/Chicago date)
-#
-# git commit -m "tfstate: $NOW [ci skip]"
-#
-# echo "Pushing changes to upstream master"
-# git push upstream master
+## cleanup deployed azure resources via azure-cli
+# docker run --rm -it \
+#   azuresdk/azure-cli-python \
+#   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
+#          az vm delete                 --name $KEY --resource-group permanent -y; \
+#          az network nic delete       --name $KEY'nic' --resource-group permanent; \
+#          az network vnet delete       --name $KEY'vnet' --resource-group permanent; \
+#          az network public-ip delete --name $KEY'-ip' --resource-group permanent;"
--- examples/azure-vm-from-user-image/main.tf
+++ examples/azure-vm-from-user-image/main.tf
@@ -59,4 +59,8 @@ resource "azurerm_virtual_machine" "vm" {
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
   }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
