66b74413034b49764f8aa6f6f620f79a6c7e8b9c
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-27 16:27:21 -0500

    updated deploy

--- examples/azure-cdn-with-storage-account/deploy.ci.sh
+++ examples/azure-cdn-with-storage-account/deploy.ci.sh
@@ -15,3 +15,22 @@ docker run --rm -it \
       /bin/terraform validate; \
       /bin/terraform plan -out=out.tfplan -var resource_group=$KEY; \
       /bin/terraform apply out.tfplan"
+
+# cleanup deployed azure resources via terraform
+docker run --rm -it \
+  -e ARM_CLIENT_ID \
+  -e ARM_CLIENT_SECRET \
+  -e ARM_SUBSCRIPTION_ID \
+  -e ARM_TENANT_ID \
+  -v $(pwd):/data \
+  --workdir=/data \
+  --entrypoint "/bin/sh" \
+  hashicorp/terraform:light \
+  -c "/bin/terraform destroy -force -var resource_group=$KEY;"
+
+
+# cleanup deployed azure resources via azure-cli
+# docker run --rm -it \
+#   azuresdk/azure-cli-python \
+#   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
+#          az group delete -y -n $KEY"
