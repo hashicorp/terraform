56f6aa16c4148b097c5ad8edac9ca3034554cdcc
Scott Nowicki <scott@sgn-mbp>
2017-04-26 21:53:25 -0500

    renamed scripts to be more intuitive; added check for docker

--- examples/azure-vm-simple-linux-managed-disk/deploy.ci.sh
+++ examples/azure-vm-simple-linux-managed-disk/deploy.ci.sh
@@ -8,10 +8,10 @@ docker run --rm -it \
   -e ARM_SUBSCRIPTION_ID \
   -e ARM_TENANT_ID \
   -v $(pwd):/data \
+  --workdir=/data \
   --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
-  -c "cd /data; \
-      /bin/terraform get; \
+  -c "/bin/terraform get; \
       /bin/terraform validate; \
       /bin/terraform plan -out=out.tfplan -var dns_name=$KEY -var hostname=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD; \
       /bin/terraform apply out.tfplan"
