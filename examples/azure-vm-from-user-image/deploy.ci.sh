5e99f8166ea6961a69eddc33bcd7b16eeb4ceb1e
Scott Nowicki <scott@sgn-mbp>
2017-04-27 19:09:55 -0500

    added az cli check

--- examples/azure-vm-from-user-image/deploy.ci.sh
+++ examples/azure-vm-from-user-image/deploy.ci.sh
@@ -14,8 +14,13 @@ docker run --rm -it \
   -c "/bin/terraform get; \
       /bin/terraform validate; \
       /bin/terraform plan -out=out.tfplan -var hostname=$KEY -var resource_group=$EXISTING_RESOURCE_GROUP -var admin_username=$KEY -var admin_password=$PASSWORD -var image_uri=$EXISTING_IMAGE_URI -var storage_account_name=$EXISTING_STORAGE_ACCOUNT_NAME; \
-      /bin/terraform apply out.tfplan \
+      /bin/terraform apply out.tfplan; \
       /bin/terraform show;"
+
+docker run --rm -it \
+  azuresdk/azure-cli-python \
+  sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
+         az vm show --name $KEY --resource-group permanent"
 
 # cleanup deployed azure resources via terraform
 docker run --rm -it \
@@ -28,13 +33,3 @@ docker run --rm -it \
   --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
   -c "/bin/terraform destroy -force -var hostname=$KEY -var resource_group=$EXISTING_RESOURCE_GROUP -var admin_username=$KEY -var admin_password=$PASSWORD -var image_uri=$EXISTING_IMAGE_URI -var storage_account_name=$EXISTING_STORAGE_ACCOUNT_NAME -target=azurerm_virtual_machine.vm -target=azurerm_network_interface.nic -target=azurerm_virtual_network.vnet -target=azurerm_public_ip.pip;"
-
-
-## cleanup deployed azure resources via azure-cli
-# docker run --rm -it \
-#   azuresdk/azure-cli-python \
-#   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
-#          az vm delete                 --name $KEY --resource-group permanent -y; \
-#          az network nic delete       --name $KEY'nic' --resource-group permanent; \
-#          az network vnet delete       --name $KEY'vnet' --resource-group permanent; \
-#          az network public-ip delete --name $KEY'-ip' --resource-group permanent;"
--- examples/azure-vm-from-user-image/deploy.mac.sh
+++ examples/azure-vm-from-user-image/deploy.mac.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset
 if docker -v; then
 
   # generate a unique string for CI deployment
-  export KEY=jcnkmmscfkrw
+  export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
   export PASSWORD=$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
   export EXISTING_IMAGE_URI=https://permanentstor.blob.core.windows.net/permanent-vhds/permanent-osdisk1.vhd
   export EXISTING_STORAGE_ACCOUNT_NAME=permanentstor
