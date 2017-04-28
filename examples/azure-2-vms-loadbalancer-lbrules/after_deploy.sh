2a43a3614b13088acd57c3fb1d67dc520c83c89b
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-28 15:16:48 -0500

    updated travis and deploy scripts for Hari's repo

--- examples/azure-2-vms-loadbalancer-lbrules/after_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit -o nounset
-
-# cleanup deployed azure resources
-docker run --rm -it \
-  azuresdk/azure-cli-python \
-  sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID; \
-         az group delete -y -n $KEY"
--- examples/azure-2-vms-loadbalancer-lbrules/deploy.sh
+++ examples/azure-2-vms-loadbalancer-lbrules/deploy.sh
@@ -15,3 +15,15 @@ docker run --rm -it \
       /bin/terraform validate; \
       /bin/terraform plan -out=out.tfplan -var dns_name=$KEY -var hostname=$KEY -var lb_ip_dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD; \
       /bin/terraform apply out.tfplan"
+
+# cleanup deployed azure resources via terraform
+docker run --rm -it \
+  -e ARM_CLIENT_ID \
+  -e ARM_CLIENT_SECRET \
+  -e ARM_SUBSCRIPTION_ID \
+  -e ARM_TENANT_ID \
+  -v $(pwd):/data \
+  --workdir=/data \
+  --entrypoint "/bin/sh" \
+  hashicorp/terraform:light \
+  -c "/bin/terraform destroy -force -var dns_name=$KEY -var hostname=$KEY -var lb_ip_dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD;"
