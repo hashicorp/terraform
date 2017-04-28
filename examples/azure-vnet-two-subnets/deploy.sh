2675c55d5c22abb71bdd859a830f789e5f3b0eb9
Scott Nowicki <scott@sgn-mbp>
2017-04-28 10:16:01 -0500

    merge master

---
+++ examples/azure-vnet-two-subnets/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+# generate a unique string for CI deployment
+# KEY=$(cat /dev/urandom | tr -cd 'a-z' | head -c 12)
+# PASSWORD=$KEY$(cat /dev/urandom | tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | tr -cd '0-9' | head -c 2)
+
+docker run --rm -it \
+  -e ARM_CLIENT_ID \
+  -e ARM_CLIENT_SECRET \
+  -e ARM_SUBSCRIPTION_ID \
+  -e ARM_TENANT_ID \
+  -v $(pwd):/data \
+  --entrypoint "/bin/sh" \
+  hashicorp/terraform:light \
+  -c "cd /data; \
+      /bin/terraform get; \
+      /bin/terraform validate; \
+      /bin/terraform plan -out=out.tfplan; \
+      /bin/terraform apply out.tfplan"
+
+# TODO: determine external validation, possibly Azure CLI
+
+# echo "Setting git user name"
+# git config user.name $GH_USER_NAME
+#
+# echo "Setting git user email"
+# git config user.email $GH_USER_EMAIL
+#
+# echo "Adding git upstream remote"
+# git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
+#
+# git checkout master
+
+
+#
+# NOW=$(TZ=America/Chicago date)
+#
+# git commit -m "tfstate: $NOW [ci skip]"
+#
+# echo "Pushing changes to upstream master"
+# git push upstream master
---
+++ examples/azure-vnet-two-subnets/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "The name of the resource group in which to create the virtual network."
+  default     = "rg2"
+}
+
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default     = "southcentralus"
+}
