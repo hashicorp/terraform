e4c732b34c225678a9f027a0a20a128137578096
Gareth Oakley <gaz@gazoakley.com>
2017-04-29 20:32:21 +0100

    Add support for IAMDatabaseAuthenticationEnabled

--- vendor/github.com/aws/aws-sdk-go/service/rds/service.go
+++ vendor/github.com/aws/aws-sdk-go/service/rds/service.go
@@ -14,9 +14,9 @@ import (
 //
 // Amazon Relational Database Service (Amazon RDS) is a web service that makes
 // it easier to set up, operate, and scale a relational database in the cloud.
-// It provides cost-efficient, resizeable capacity for an industry-standard
-// relational database and manages common database administration tasks, freeing
-// up developers to focus on what makes their applications and businesses unique.
+// It provides cost-efficient, resizable capacity for an industry-standard relational
+// database and manages common database administration tasks, freeing up developers
+// to focus on what makes their applications and businesses unique.
 //
 // Amazon RDS gives you access to the capabilities of a MySQL, MariaDB, PostgreSQL,
 // Microsoft SQL Server, Oracle, or Amazon Aurora database server. These capabilities
