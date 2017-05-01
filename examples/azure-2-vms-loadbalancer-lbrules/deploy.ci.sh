aadfa659e78756937fc6c3d8f8914b3506841f40
Scott Nowicki <scott@sgn-mbp>
2017-04-30 22:42:51 -0500

    prep for PR

--- examples/azure-2-vms-loadbalancer-lbrules/deploy.ci.sh
+++ examples/azure-2-vms-loadbalancer-lbrules/deploy.ci.sh
@@ -16,6 +16,13 @@ docker run --rm -it \
       /bin/terraform plan -out=out.tfplan -var dns_name=$KEY -var hostname=$KEY -var lb_ip_dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD; \
       /bin/terraform apply out.tfplan"
 
+# cleanup deployed azure resources via azure-cli
+docker run --rm -it \
+  azuresdk/azure-cli-python \
+  sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null; \
+         az network lb show -g $KEY -n rglb; \
+         az network lb rule list -g $KEY --lb-name rglb;"
+
 # cleanup deployed azure resources via terraform
 docker run --rm -it \
   -e ARM_CLIENT_ID \
