1fe050db5e7e233cedf7eb72ee51c3c5b9b42d33
Eugene Chuvyrov <eugene.chuvyrov@microsoft.com>
2017-05-01 09:50:52 -0700

    Managed Disks with Custom Image - Ready for review for VMs

--- builtin/providers/azurerm/provider.go
+++ builtin/providers/azurerm/provider.go
@@ -87,7 +87,8 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_lb_probe":                resourceArmLoadBalancerProbe(),
 			"azurerm_lb_rule":                 resourceArmLoadBalancerRule(),
 
-			"azurerm_managed_disk": resourceArmManagedDisk(),
+			"azurerm_managed_disk":  resourceArmManagedDisk(),
+			"azurerm_managed_image": resourceArmManagedImage(),
 
 			"azurerm_key_vault":                 resourceArmKeyVault(),
 			"azurerm_local_network_gateway":     resourceArmLocalNetworkGateway(),
