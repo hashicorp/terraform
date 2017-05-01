c1f9fe6d46d34cebca5adeeaf6a59e49db8d1a79
Bill Maxwell <bill@rancher.com>
2017-05-01 10:30:59 -0700

    add host label support to registration command

--- builtin/providers/rancher/resource_rancher_environment.go
+++ builtin/providers/rancher/resource_rancher_environment.go
@@ -329,6 +329,14 @@ func EnvironmentStateRefreshFunc(client *rancherClient.RancherClient, environmen
 			return nil, "", err
 		}
 
+		// Env not returned, or State not set...
+		if env == nil || env.State == "" {
+			// This makes it so user level API keys can be used instead of just admin
+			env = &rancherClient.Project{
+				State: "removed",
+			}
+		}
+
 		return env, env.State, nil
 	}
 }
