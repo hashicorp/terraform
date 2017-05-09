672e347f3bbcd6e1546c904a47708b363994a4cd
Sertaç Özercan <sozercan@gmail.com>
2017-05-09 16:05:12 -0700

    update vmss test to include a blank name

--- builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
+++ builtin/providers/azurerm/resource_arm_virtual_machine_scale_set_test.go
@@ -717,7 +717,7 @@ resource "azurerm_virtual_machine_scale_set" "test" {
   }
 
   storage_profile_os_disk {
-	name 		  = "os-disk"
+	name 		  = ""
     caching       = "ReadWrite"
     create_option = "FromImage"
     managed_disk_type = "Standard_LRS"
