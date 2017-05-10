235f6da1406682218c67079ec01d16e19ddab7d1
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-09 22:12:13 -0500

    merging Hashicorp master into this branch

--- examples/azure-search-create/variables.tf
+++ examples/azure-search-create/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group" {
-  description = "The name of the resource group in which to create the virtual network."
+  description = "The name of the resource group in which to create search service"
 }
 
 variable "location" {
-  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  description = "The location/region where the search service is created. Changing this forces a new resource to be created."
   default     = "southcentralus"
 }
 
