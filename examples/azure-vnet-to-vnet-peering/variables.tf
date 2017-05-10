297f5448cdf1f774aac041c8db605183ac112a15
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-09 19:20:15 -0500

    updating variable descriptions

--- examples/azure-vnet-to-vnet-peering/variables.tf
+++ examples/azure-vnet-to-vnet-peering/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group" {
-  description = "The name of the resource group in which the image to clone resides."
+  description = "The name of the resource group in which the virtual networks are created"
   default     = "myrg"
 }
 
 variable "location" {
-  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  description = "The location/region where the virtual networks are created. Changing this forces a new resource to be created."
   default     = "southcentralus"
 }
