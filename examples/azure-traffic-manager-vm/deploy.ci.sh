1d37c43a22d71f84f2efefcb525f4ecb5e06c6e1
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2017-05-10 16:19:52 -0500

    provider/azurerm: Add example of traffic manager (#14337)
    * initial commit - 101-vm-from-user-image
    
    * changed branch name
    
    * not deploying - storage problems
    
    * provisions vm but image not properly prepared
    
    * storage not correct
    
    * provisions properly
    
    * changed main.tf to azuredeploy.tf
    
    * added tfvars and info for README
    
    * tfvars ignored and corrected file ext
    
    * added CI config; added sane defaults for variables; updated deployment script, added mac specific deployment for local testing
    
    * deploy.sh to be executable
    
    * executable deploy files
    
    * added CI files; changed vars
    
    * prep for PR
    
    * removal of old folder
    
    * prep for PR
    
    * wrong args for travis
    
    * more PR prep
    
    * updated README
    
    * commented out variables in terraform.tfvars
    
    * Topic 101 vm from user image (#2)
    
    * initial commit - 101-vm-from-user-image
    * added tfvars and info for README
    * added CI config; added sane defaults for variables; updated deployment script, added mac specific deployment for local testing
    * prep for PR
    
    * added new template
    
    * oops, left off master
    
    * prep for PR
    
    * correct repository for destination
    
    * renamed scripts to be more intuitive; added check for docker
    
    * merge vm simple; vm from image
    
    * initial commit
    
    * deploys locally
    
    * updated deploy
    
    * consolidated deploy and after_deploy into a single script; simplified ci process; added os_profile_linux_config
    
    * added terraform show
    
    * changed to allow http & https (like ARM tmplt)
    
    * changed host_name & host_name variable desc
    
    * added az cli check
    
    * on this branch, only build test_dir; master will aggregate all the examples
    
    * merge master
    
    * added new constructs/naming for deploy scripts, etc.
    
    * suppress az login output
    
    * suppress az login output
    
    * forgot about line breaks
    
    * breaking build as an example
    
    * fixing broken build example
    
    * work in progress
    
    * merge of CI config
    
    * fixed grammar in readme
    
    * prep for PR
    
    * deploys locally
    
    * deploys locally
    
    * small README change
    
    * took out armviz button and minor README changes
    
    * changed host_name
    
    * fixed merge conflicts
    
    * changed host_name variable
    
    * updating Hashicorp's changes to merged simple linux branch
    
    * updating files to merge w/master and prep for Hashicorp pr
    
    * Revert "updating files to merge w/master and prep for Hashicorp pr"
    
    This reverts commit b850cd5d2a858eff073fc5a1097a6813d0f8b362.
    
    * Revert "updating Hashicorp's changes to merged simple linux branch"
    
    This reverts commit dbaf8d14a9cdfcef0281919671357f6171ebd4e6.
    
    * added provider to main.tf
    
    * took out armviz button; resolved conflicts in .travis.yml
    
    * removing vm from user image example from this branch
    
    * removed old branch
    
    * azure-2-vms-loadbalancer-lbrules (#13)
    
    * initial commit
    
    * need to change lb_rule & nic
    
    * deploys locally
    
    * updated README
    
    * updated travis and deploy scripts for Hari's repo
    
    * renamed deploy script
    
    * clean up
    
    * prep for PR
    
    * updated readme
    
    * fixing conflict in .travis.yml
    
    * add CI build tag
    
    * azure-traffic-manager-vm (#17)
    
    adding example of azure traffic manager
    
    * prepped for Hashicorp PR
    
    * merging with hashicorp master

--- examples/azure-traffic-manager-vm/deploy.ci.sh
+++ examples/azure-traffic-manager-vm/deploy.ci.sh
@@ -13,8 +13,15 @@ docker run --rm -it \
   hashicorp/terraform:light \
   -c "/bin/terraform get; \
       /bin/terraform validate; \
-      /bin/terraform plan -out=out.tfplan -var resource_group=$KEY; \
-      /bin/terraform apply out.tfplan"
+      /bin/terraform plan -out=out.tfplan -var dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD; \
+      /bin/terraform apply out.tfplan; \
+      /bin/terraform show;"
+
+# cleanup deployed azure resources via azure-cli
+docker run --rm -it \
+  azuresdk/azure-cli-python \
+  sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null; \
+         az vm show -g $KEY -n rgvm"
 
 # cleanup deployed azure resources via terraform
 docker run --rm -it \
@@ -26,4 +33,4 @@ docker run --rm -it \
   --workdir=/data \
   --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
-  -c "/bin/terraform destroy -force -var resource_group=$KEY;"
+  -c "/bin/terraform destroy -force -var dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD;"
