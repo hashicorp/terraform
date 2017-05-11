689095f244e47bca66d6de3fd8d5089b85424d07
joel.ringuette <joel.ringuette@ticketmaster.com>
2017-05-11 10:19:48 -0400

    Support nil state

--- helper/schema/resource.go
+++ helper/schema/resource.go
@@ -142,9 +142,11 @@ func (r *Resource) Apply(
 		if err := rt.DiffDecode(d); err != nil {
 			log.Printf("[ERR] Error decoding ResourceTimeout: %s", err)
 		}
-	} else if _, ok := s.Meta[TimeoutKey]; ok {
-		if err := rt.StateDecode(s); err != nil {
-			log.Printf("[ERR] Error decoding ResourceTimeout: %s", err)
+	} else if s != nil {
+		if _, ok := s.Meta[TimeoutKey]; ok {
+			if err := rt.StateDecode(s); err != nil {
+				log.Printf("[ERR] Error decoding ResourceTimeout: %s", err)
+			}
 		}
 	} else {
 		log.Printf("[DEBUG] No meta timeoutkey found in Apply()")
