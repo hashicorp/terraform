7110f86c3c11fea2ded8575e27779df428c3bc71
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-18 17:40:29 -0500

    deploy troubleshooting

--- examples/azure-sql-database/deploy.mac.sh
+++ examples/azure-sql-database/deploy.mac.sh
@@ -6,7 +6,7 @@ if docker -v; then
 
   # generate a unique string for CI deployment
   export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
-  export PASSWORD=$#@abcd9753w0w@h@
+  export PASSWORD=$a@abcd9753w0w@h@12
   # =$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
 
   /bin/sh ./deploy.ci.sh
