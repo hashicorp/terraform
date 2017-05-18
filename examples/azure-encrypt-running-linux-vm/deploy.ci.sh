9119b76baec5c72e41b920f2a4b6eafa42fd990f
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-18 09:18:07 -0500

    deploys successfully locally

--- examples/azure-encrypt-running-linux-vm/deploy.ci.sh
+++ examples/azure-encrypt-running-linux-vm/deploy.ci.sh
@@ -7,21 +7,33 @@ docker run --rm -it \
   -e ARM_CLIENT_SECRET \
   -e ARM_SUBSCRIPTION_ID \
   -e ARM_TENANT_ID \
+  -e AAD_CLIENT_ID \
+  -e AAD_CLIENT_SECRET \
+  -e KEY_ENCRYPTION_KEY_URL \
+  -e KEY_VAULT_RESOURCE_ID \
   -v $(pwd):/data \
   --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
   -c "cd /data; \
       /bin/terraform get; \
       /bin/terraform validate; \
-      /bin/terraform plan -out=out.tfplan -var dns_name=$KEY -var hostname=$KEY -var lb_ip_dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD; \
-      /bin/terraform apply out.tfplan"
+      /bin/terraform plan -out=out.tfplan \
+        -var resource_group=$KEY \
+        -var hostname=$KEY \
+        -var admin_username=$KEY \
+        -var admin_password=$PASSWORD \
+        -var passphrase=$PASSWORD \
+        -var key_vault_name=$KEY_VAULT_NAME \
+        -var key_encryption_key_url=$KEY_ENCRYPTION_KEY_URL \
+        -var key_vault_resource_id=$KEY_VAULT_RESOURCE_ID > /dev/null; \
+      /bin/terraform apply out.tfplan > /dev/null"
 
 # cleanup deployed azure resources via azure-cli
 docker run --rm -it \
   azuresdk/azure-cli-python \
   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null; \
-         az network lb show -g $KEY -n rglb; \
-         az network lb rule list -g $KEY --lb-name rglb;"
+         az vm show -g $KEY -n $KEY; \
+         az vm encryption show -g $KEY -n $KEY;"
 
 # cleanup deployed azure resources via terraform
 docker run --rm -it \
@@ -33,4 +45,13 @@ docker run --rm -it \
   --workdir=/data \
   --entrypoint "/bin/sh" \
   hashicorp/terraform:light \
-  -c "/bin/terraform destroy -force -var dns_name=$KEY -var hostname=$KEY -var lb_ip_dns_name=$KEY -var resource_group=$KEY -var admin_password=$PASSWORD;"
+  -c "/bin/terraform destroy -force \
+    -var resource_group=$KEY \
+    -var hostname=$KEY \
+    -var admin_username=$KEY \
+    -var admin_password=$PASSWORD \
+    -var passphrase=$PASSWORD \
+    -var key_vault_name=$KEY_VAULT_NAME \
+    -var key_encryption_key_url=$KEY_ENCRYPTION_KEY_URL \
+    -var key_vault_resource_id=$KEY_VAULT_RESOURCE_ID \
+    -target=azurerm_resource_group.rg > /dev/null;"
--- examples/azure-encrypt-running-linux-vm/variables.tf
+++ examples/azure-encrypt-running-linux-vm/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group" {
-  description = "Resource group name."
+  description = "Resource group name into which your new virtual machine will go."
 }
 
 variable "location" {
@@ -9,34 +9,6 @@ variable "location" {
 
 variable "hostname" {
   description = "Used to form various names including the key vault, vm, and storage. Must be unique."
-}
-
-variable "keyvault_tenant_id" {
-  description = "The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. Get using 'az account show'."
-}
-
-variable "keyvault_object_id" {
-  description = "The object ID of a service principal in the Azure Active Directory tenant for the key vault. Get using 'az ad sp show'."
-}
-
-variable "keys_permissions" {
-  description = "Permissions to keys in the vault. Valid values are: all, create, import, update, get, list, delete, backup, restore, encrypt, decrypt, wrapkey, unwrapkey, sign, and verify."
-  default     = "all"
-}
-
-variable "secrets_permissions" {
-  description = "Permissions to secrets in the vault. Valid values are: all, get, set, list, and delete."
-  default     = "all"
-}
-
-variable "vault_sku" {
-  description = "SKU for the vault. Valid values are: standard, premium."
-  default     = "standard"
-}
-
-variable "os_type" {
-  description = "Specifies the operating system Type, valid values are windows, linux."
-  default     = "linux"
 }
 
 variable "address_space" {
@@ -108,41 +80,45 @@ variable "encryption_operation" {
 
 variable "volume_type" {
   description = "Defines which drives should be encrypted. OS encryption is supported on RHEL 7.2, CentOS 7.2 & Ubuntu 16.04. Allowed values: OS, Data, All"
-  default     = "Data"
+  default     = "All"
 }
 
 variable "key_encryption_key_url" {
   description = "URL of the KeyEncryptionKey used to encrypt the volume encryption key"
 }
 
-variable "vault_name" {
-  description = "name of the vault"
+variable "key_vault_resource_id" {
+  description = "uri of Azure key vault resource"
 }
 
-# variable "passphrase" {
-#   description = "The passphrase for the disks"
-# }
+variable "key_vault_name" {
+  description = "name of Azure key vault resource"
+}
 
+variable "passphrase" {
+  description = "The passphrase for the disks"
+}
 
-variable "sequenceVersion" {
+variable "extension_name" {
+  description = "the name of the vm extension"
+  default     = "AzureDiskEncryptionForLinux"
+}
+variable "sequence_version" {
   description = "sequence version of the bitlocker operation. Increment this everytime an operation is performed on the same VM"
   default     = 1
 }
 
+variable "use_kek" {
+  description = "Select kek if the secret should be encrypted with a key encryption key. Allowed values: kek, nokek"
+  default = "kek"
+}
 
-# variable "useKek" {
-#   description = "Select kek if the secret should be encrypted with a key encryption key. Allowed values: kek, nokek"
-#   default = "nokek"
-# }
+variable "artifacts_location" {
+  description = "The base URI where artifacts required by this template are located. When the template is deployed using the accompanying scripts, a private location in the subscription will be used and this value will be automatically generated."
+  default = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master"
+}
 
-
-# variable "_artifactsLocation" {
-#   description = "The base URI where artifacts required by this template are located. When the template is deployed using the accompanying scripts, a private location in the subscription will be used and this value will be automatically generated."
-#   default = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master"
-# }
-
-
-# variable "_artifactsLocationSasToken" {
-#   description = "The sasToken required to access _artifactsLocation.  When the template is deployed using the accompanying scripts, a sasToken will be automatically generated."
-# }
-
+variable "artifacts_location_sas_token" {
+  description = "The sasToken required to access _artifactsLocation.  When the template is deployed using the accompanying scripts, a sasToken will be automatically generated."
+  default = ""
+}
