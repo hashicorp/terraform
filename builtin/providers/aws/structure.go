5c33d93db8d4eff7d0cf040f5281cbaef69f98ed
Jake Champlin <jake.champlin.27@gmail.com>
2017-05-19 14:36:39 -0400

    add docstring on ignored error

--- builtin/providers/aws/structure.go
+++ builtin/providers/aws/structure.go
@@ -1886,10 +1886,9 @@ func normalizeJsonString(jsonString interface{}) (string, error) {
 		return s, err
 	}
 
-	bytes, err := json.Marshal(j)
-	if err != nil {
-		return s, err
-	}
+	// The error is intentionally ignored here to allow empty policies to passthrough validation.
+	// This covers any interpolated values
+	bytes, _ := json.Marshal(j)
 
 	return string(bytes[:]), nil
 }
