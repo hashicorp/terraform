fc352ab093130ebce41800bf3d6c2c5dfd56cd5d
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-29 02:10:34 -0500

    commenting out validation

--- examples/azure-openshift-origin/deploy.ci.sh
+++ examples/azure-openshift-origin/deploy.ci.sh
@@ -10,7 +10,6 @@ docker run --rm -it \
   -e AAD_CLIENT_ID \
   -e AAD_CLIENT_SECRET \
   -e KEY_ENCRYPTION_KEY_URL \
-  -e KEY_VAULT_RESOURCE_ID \
   -e SSH_PUBLIC_KEY \
   -v $(pwd):/data \
   --workdir=/data \
@@ -21,7 +20,6 @@ docker run --rm -it \
       /bin/terraform plan -out=out.tfplan \
         -var resource_group_name=$KEY \
         -var subscription_id=$ARM_SUBSCRIPTION_ID \
-        -var hostname=$KEY \
         -var openshift_cluster_prefix=$KEY \
         -var key_vault_secret=$KEY_VAULT_SECRET \
         -var admin_username=$KEY \
@@ -31,16 +29,15 @@ docker run --rm -it \
         -var aad_client_id=$AAD_CLIENT_ID \
         -var aad_client_secret=$AAD_CLIENT_SECRET \
         -var key_encryption_key_url=$KEY_ENCRYPTION_KEY_URL \
-        -var key_vault_resource_id=$KEY_VAULT_RESOURCE_ID \
         -var ssh_public_key=$SSH_PUBLIC_KEY; \
       /bin/terraform apply out.tfplan"
 
 # cleanup deployed azure resources via azure-cli
-docker run --rm -it \
-  azuresdk/azure-cli-python \
-  sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null; \
-         az vm show -g $KEY -n $KEY; \
-         az vm encryption show -g $KEY -n $KEY"
+# docker run --rm -it \
+#   azuresdk/azure-cli-python \
+#   sh -c "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null; \
+#          az vm show -g $KEY -n $KEY; \
+#          az vm encryption show -g $KEY -n $KEY"
 
 # cleanup deployed azure resources via terraform
 docker run --rm -it \
@@ -55,7 +52,6 @@ docker run --rm -it \
   -c "/bin/terraform destroy -force \
         -var resource_group_name=$KEY \
         -var subscription_id=$ARM_SUBSCRIPTION_ID \
-        -var hostname=$KEY \
         -var openshift_cluster_prefix=$KEY \
         -var key_vault_secret=$KEY_VAULT_SECRET \
         -var admin_username=$KEY \
@@ -65,5 +61,4 @@ docker run --rm -it \
         -var aad_client_id=$AAD_CLIENT_ID \
         -var aad_client_secret=$AAD_CLIENT_SECRET \
         -var key_encryption_key_url=$KEY_ENCRYPTION_KEY_URL \
-        -var key_vault_resource_id=$KEY_VAULT_RESOURCE_ID \
         -var ssh_public_key=$SSH_PUBLIC_KEY;"
