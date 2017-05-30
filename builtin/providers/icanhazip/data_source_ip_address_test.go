ee924ffd1b14e0b464e1f17758c914c72784ecaf
Dean Wilson <dean.wilson@gmail.com>
2017-05-30 16:50:43 +0100

    Add basic tests
    This confirms that an IP address can be retrieved and looks like
    a valid IP.

---
+++ builtin/providers/icanhazip/data_source_ip_address_test.go
@@ -0,0 +1,45 @@
+package icanhazip
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccIcanhazipIPAddress_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccIcanhazipIPAddressConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccIcanhazipIPAddress("data.icanhazip_ipaddress.localip"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIcanhazipIPAddress(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		r := s.RootModule().Resources[n]
+		a := r.Primary.Attributes
+
+		ipaddress := a["ip_address"]
+
+		if ip := net.ParseIP(ipaddress); ip == nil {
+			return fmt.Errorf("Not a valid IP address: %s", ipaddress)
+		}
+
+		return nil
+	}
+}
+
+const testAccIcanhazipIPAddressConfig = `
+data "icanhazip_ipaddress" "localip" { }
+`
