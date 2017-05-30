cf7255033db905ccc22283c6431bf40df1d30bee
Jay Wang <jiewan@microsoft.com>
2017-05-30 14:04:37 -0700

    Reuse existing read code

--- builtin/providers/azurerm/data_source_arm_resource_group.go
+++ builtin/providers/azurerm/data_source_arm_resource_group.go
@@ -3,7 +3,6 @@ package azurerm
 import (
 	"fmt"
 
-	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -25,27 +24,26 @@ func dataSourceArmResourceGroup() *schema.Resource {
 
 func dataSourceArmResourceGroupRead(d *schema.ResourceData, meta interface{}) error {
 	armClient := meta.(*ArmClient)
-	resourceGroupClient := armClient.resourceGroupClient
 
 	resourceGroupName := d.Get("name").(string)
-	result, err := resourceGroupClient.Get(resourceGroupName)
-	if err != nil {
-		return errwrap.Wrapf("Error reading Resource Group {{err}}", err)
+	location, getLocationOk := d.GetOk("location")
+	resourceId := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s", armClient.subscriptionId, resourceGroupName)
+
+	d.SetId(resourceId)
+
+	if err := resourceArmResourceGroupRead(d, meta); err != nil {
+		return err
 	}
 
-	if v, ok := d.GetOk("location"); ok {
-		location := azureRMNormalizeLocation(v.(string))
-		actualLocation := azureRMNormalizeLocation(*result.Location)
+	if getLocationOk {
+		actualLocation := azureRMNormalizeLocation(d.Get("location").(string))
+		location := azureRMNormalizeLocation(location)
 
 		if location != actualLocation {
 			return fmt.Errorf(`The location specified in Data Source (%s) doesn't match the actual location of the Resource Group "%s (%s)"`,
 				location, resourceGroupName, actualLocation)
 		}
 	}
-
-	d.Set("location", *result.Location)
-	flattenAndSetTags(d, result.Tags)
-	d.SetId(*result.ID)
 
 	return nil
 }
