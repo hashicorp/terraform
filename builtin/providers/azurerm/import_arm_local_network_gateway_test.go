991972770280024a5c27812062bc52255df97eb7
tombuildsstuff <git@ibuildstuff.co.uk>
2017-06-01 11:46:37 +0100

    Randomizing the Local Network Gateway tests

--- builtin/providers/azurerm/import_arm_local_network_gateway_test.go
+++ builtin/providers/azurerm/import_arm_local_network_gateway_test.go
@@ -3,22 +3,24 @@ package azurerm
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccAzureRMLocalNetworkGateway_importBasic(t *testing.T) {
 	resourceName := "azurerm_local_network_gateway.test"
+	rInt := acctest.RandInt()
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testCheckAzureRMLocalNetworkGatewayDestroy,
 		Steps: []resource.TestStep{
-			resource.TestStep{
-				Config: testAccAzureRMLocalNetworkGatewayConfig_basic,
+			{
+				Config: testAccAzureRMLocalNetworkGatewayConfig_basic(rInt),
 			},
 
-			resource.TestStep{
+			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
