b30220135d11ac94c1da6d0f90638ab57a4b29fc
Riley Karson <rileykarson@google.com>
2017-06-05 17:05:27 -0700

    Updated google_compute_autoscaler tests so that update fails as expected.

--- builtin/providers/google/import_compute_autoscaler_test.go
+++ builtin/providers/google/import_compute_autoscaler_test.go
@@ -3,19 +3,26 @@ package google
 import (
 	"testing"
 
+	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAutoscaler_importBasic(t *testing.T) {
+func TestAccComputeAutoscaler_importBasic(t *testing.T) {
 	resourceName := "google_compute_autoscaler.foobar"
+
+	var it_name = fmt.Sprintf("autoscaler-test-%s", acctest.RandString(10))
+	var tp_name = fmt.Sprintf("autoscaler-test-%s", acctest.RandString(10))
+	var igm_name = fmt.Sprintf("autoscaler-test-%s", acctest.RandString(10))
+	var autoscaler_name = fmt.Sprintf("autoscaler-test-%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckAutoscalerDestroy,
+		CheckDestroy: testAccCheckComputeAutoscalerDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccAutoscaler_basic,
+				Config: testAccComputeAutoscaler_basic(it_name, tp_name, igm_name, autoscaler_name),
 			},
 
 			resource.TestStep{
