d91819f79e6693eb2d826546f183a3914f723a33
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-06-07 17:34:58 -0500

    commented out provider

--- examples/azure-wordpress-mysql-replication/main.tf
+++ examples/azure-wordpress-mysql-replication/main.tf
@@ -1,9 +1,9 @@
-provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
-}
+# provider "azurerm" {
+#   subscription_id = "${var.subscription_id}"
+#   client_id       = "${var.client_id}"
+#   client_secret   = "${var.client_secret}"
+#   tenant_id       = "${var.tenant_id}"
+# }
 
 # ********************** MYSQL REPLICATION ********************** #
 
