965e9883b341ebbebfc6c358ca158080fd9cf149
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-06-08 16:03:54 -0500

    removed ssh from output (no nsg)

--- examples/azure-wordpress-mysql-replication/outputs.tf
+++ examples/azure-wordpress-mysql-replication/outputs.tf
@@ -9,7 +9,3 @@ output "fqdn" {
 output "ip_address" {
   value = "${azurerm_public_ip.pip.ip_address}"
 }
-
-output "ssh_command" {
-  value = "ssh ${var.vm_admin_username}@${azurerm_public_ip.pip.ip_address} -p 64001"
-}
