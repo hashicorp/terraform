4f219220ac0c28fcb23fd0d54dc3b670e995f517
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-06-09 15:56:16 -0500

    in progress; new branch for updating CI's permanent resources

--- examples/azure-vm-custom-image-new-storage-account/deploy.mac.sh
+++ examples/azure-vm-custom-image-new-storage-account/deploy.mac.sh
@@ -8,9 +8,9 @@ if docker -v; then
   export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
   export PASSWORD=$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
   export EXISTING_RESOURCE_GROUP=donotdelete
-  export EXISTING_IMAGE_URI=https://donotdeletedisks636.blob.core.windows.net/vhds/mywindowsimage20170510184809.vhd
+  export EXISTING_IMAGE_URI=https://donotdeletedisks636.blob.core.windows.net/vhds/WindowsImage.vhd
   export EXISTING_STORAGE_ACCOUNT_NAME=donotdeletedisks636
-  export CUSTOM_IMAGE_NAME=mywindowsimage20170510184809
+  export CUSTOM_IMAGE_NAME=WindowsImage
 
   /bin/sh ./deploy.ci.sh
 
