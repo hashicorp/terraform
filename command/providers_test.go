f5ff67c5302887f3f8b3c194ae92809ee6eaaa87
Radek Simko <radek.simko@gmail.com>
2017-06-10 12:02:01 +0100

    command/providers: Add regression test

--- command/providers_test.go
+++ command/providers_test.go
@@ -41,3 +41,33 @@ func TestProviders(t *testing.T) {
 		t.Errorf("output missing provider.baz\n\n%s", output)
 	}
 }
+
+func TestProviders_noConfigs(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if err := os.Chdir(testFixturePath("")); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Chdir(cwd)
+
+	ui := new(cli.MockUi)
+	c := &ProvidersCommand{
+		Meta: Meta{
+			Ui: ui,
+		},
+	}
+
+	args := []string{}
+	if code := c.Run(args); code == 0 {
+		t.Fatal("expected command to return non-zero exit code" +
+			" when no configs are available")
+	}
+
+	output := ui.ErrorWriter.String()
+	expectedErrMsg := "No configuration files found"
+	if !strings.Contains(output, expectedErrMsg) {
+		t.Errorf("Expected error message: %s\nGiven output: %s", expectedErrMsg, output)
+	}
+}
