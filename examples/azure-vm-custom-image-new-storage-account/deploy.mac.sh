3abe2cb28616188bd9bdc5a12f57be8ad3f216ca
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-06-13 17:55:46 -0500

    testing vm-custom-image WindowsImage

--- examples/azure-vm-custom-image-new-storage-account/deploy.mac.sh
+++ examples/azure-vm-custom-image-new-storage-account/deploy.mac.sh
@@ -8,7 +8,7 @@ if docker -v; then
   export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
   export PASSWORD=$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
   export EXISTING_RESOURCE_GROUP=donotdelete
-  export EXISTING_WINDOWS_IMAGE_URI=https://tfpermstor.blob.core.windows.net/permanent-vhds/osdisk_sqCadTgBlS.vhd
+  export EXISTING_WINDOWS_IMAGE_URI=https://tfpermstor.blob.core.windows.net/vhds/osdisk_alBZrO4OlX.vhd
   export EXISTING_STORAGE_ACCOUNT_NAME=donotdeletedisks636
   export CUSTOM_WINDOWS_IMAGE_NAME=WindowsImage
 
