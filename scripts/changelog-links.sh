4d0260fbe24c16877fce7ac46884ce10a34e7404
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-26 14:00:32 -0400

    Fix Changelog Links Script for softlayer provider

--- scripts/changelog-links.sh
+++ scripts/changelog-links.sh
@@ -24,7 +24,7 @@ else
   SED="sed -i.bak -r -e"
 fi
 
-PROVIDER_URL="https:\/\/github.com\/terraform-providers\/terraform-provider-softlayer"
+PROVIDER_URL="https:\/\/github.com\/terraform-providers\/terraform-provider-softlayer\/issues"
 
 $SED "s/GH-([0-9]+)/\[#\1\]\($PROVIDER_URL\/\1\)/g" -e 's/\[\[#(.+)([0-9])\)]$/(\[#\1\2))/g' CHANGELOG.md
 
