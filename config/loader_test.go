5825d1f5811fd603b5767a122d080f7631636b16
Martin Atkins <mart@degeneration.co.uk>
2017-07-01 09:12:31 -0700

    config: parsing of "locals" blocks in configuration

--- config/loader_test.go
+++ config/loader_test.go
@@ -180,17 +180,17 @@ func TestLoadFileBasic(t *testing.T) {
 	}
 
 	if c.Dir != "" {
-		t.Fatalf("bad: %#v", c.Dir)
+		t.Fatalf("wrong dir %#v; want %#v", c.Dir, "")
 	}
 
 	expectedTF := &Terraform{RequiredVersion: "foo"}
 	if !reflect.DeepEqual(c.Terraform, expectedTF) {
-		t.Fatalf("bad: %#v", c.Terraform)
+		t.Fatalf("wrong terraform block %#v; want %#v", c.Terraform, expectedTF)
 	}
 
 	expectedAtlas := &AtlasConfig{Name: "mitchellh/foo"}
 	if !reflect.DeepEqual(c.Atlas, expectedAtlas) {
-		t.Fatalf("bad: %#v", c.Atlas)
+		t.Fatalf("wrong atlas config %#v; want %#v", c.Atlas, expectedAtlas)
 	}
 
 	actual := variablesStr(c.Variables)
@@ -206,6 +206,10 @@ func TestLoadFileBasic(t *testing.T) {
 	actual = resourcesStr(c.Resources)
 	if actual != strings.TrimSpace(basicResourcesStr) {
 		t.Fatalf("bad:\n%s", actual)
+	}
+
+	if actual, want := localsStr(c.Locals), strings.TrimSpace(basicLocalsStr); actual != want {
+		t.Fatalf("wrong locals:\n%s\nwant:\n%s", actual, want)
 	}
 
 	actual = outputsStr(c.Outputs)
@@ -286,6 +290,10 @@ func TestLoadFileBasic_json(t *testing.T) {
 	actual = resourcesStr(c.Resources)
 	if actual != strings.TrimSpace(basicResourcesStr) {
 		t.Fatalf("bad:\n%s", actual)
+	}
+
+	if actual, want := localsStr(c.Locals), strings.TrimSpace(basicLocalsStr); actual != want {
+		t.Fatalf("wrong locals:\n%s\nwant:\n%s", actual, want)
 	}
 
 	actual = outputsStr(c.Outputs)
@@ -1050,6 +1058,18 @@ aws_instance.test (x1)
 `
 
 const basicOutputsStr = `
+web_ip
+  vars
+    resource: aws_instance.web.private_ip
+`
+
+const basicLocalsStr = `
+literal
+literal_list
+literal_map
+security_group_ids
+  vars
+    resource: aws_security_group.firewall.*.id
 web_ip
   vars
     resource: aws_instance.web.private_ip
--- config/test-fixtures/basic.tf
+++ config/test-fixtures/basic.tf
@@ -58,6 +58,17 @@ resource aws_instance "web" {
     }
 }
 
+locals {
+  security_group_ids = "${aws_security_group.firewall.*.id}"
+  web_ip = "${aws_instance.web.private_ip}"
+}
+
+locals {
+  literal = 2
+  literal_list = ["foo"]
+  literal_map = {"foo" = "bar"}
+}
+
 resource "aws_instance" "db" {
     security_groups = "${aws_security_group.firewall.*.id}"
     VPC = "foo"
