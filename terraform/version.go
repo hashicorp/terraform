515fc74da18ae642513996e979d467c284baa941
Martin Atkins <mart@degeneration.co.uk>
2017-07-19 14:05:43 -0700

    v0.10.0-rc1

--- terraform/version.go
+++ terraform/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // The main version number that is being run at the moment.
-const Version = "0.10.0"
+const Version = "0.10.0-rc1"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
