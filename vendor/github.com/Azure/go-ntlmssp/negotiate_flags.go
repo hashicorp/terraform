313086a12b3f1a5fda82124b6d7cb72b39c1e489
Jake Champlin <jake.champlin.27@gmail.com>
2017-08-20 14:12:48 -0400

    add missing dependency: azure/go-ntlmssp

--- vendor/github.com/Azure/go-ntlmssp/negotiate_flags.go
+++ vendor/github.com/Azure/go-ntlmssp/negotiate_flags.go
@@ -7,38 +7,30 @@ const (
 	/*B*/ negotiateFlagNTLMNEGOTIATEOEM = 1 << 1
 	/*C*/ negotiateFlagNTLMSSPREQUESTTARGET = 1 << 2
 
-	/*D*/
-	negotiateFlagNTLMSSPNEGOTIATESIGN = 1 << 4
+	/*D*/ negotiateFlagNTLMSSPNEGOTIATESIGN = 1 << 4
 	/*E*/ negotiateFlagNTLMSSPNEGOTIATESEAL = 1 << 5
 	/*F*/ negotiateFlagNTLMSSPNEGOTIATEDATAGRAM = 1 << 6
 	/*G*/ negotiateFlagNTLMSSPNEGOTIATELMKEY = 1 << 7
 
-	/*H*/
-	negotiateFlagNTLMSSPNEGOTIATENTLM = 1 << 9
+	/*H*/ negotiateFlagNTLMSSPNEGOTIATENTLM = 1 << 9
 
-	/*J*/
-	negotiateFlagANONYMOUS = 1 << 11
+	/*J*/ negotiateFlagANONYMOUS = 1 << 11
 	/*K*/ negotiateFlagNTLMSSPNEGOTIATEOEMDOMAINSUPPLIED = 1 << 12
 	/*L*/ negotiateFlagNTLMSSPNEGOTIATEOEMWORKSTATIONSUPPLIED = 1 << 13
 
-	/*M*/
-	negotiateFlagNTLMSSPNEGOTIATEALWAYSSIGN = 1 << 15
+	/*M*/ negotiateFlagNTLMSSPNEGOTIATEALWAYSSIGN = 1 << 15
 	/*N*/ negotiateFlagNTLMSSPTARGETTYPEDOMAIN = 1 << 16
 	/*O*/ negotiateFlagNTLMSSPTARGETTYPESERVER = 1 << 17
 
-	/*P*/
-	negotiateFlagNTLMSSPNEGOTIATEEXTENDEDSESSIONSECURITY = 1 << 19
+	/*P*/ negotiateFlagNTLMSSPNEGOTIATEEXTENDEDSESSIONSECURITY = 1 << 19
 	/*Q*/ negotiateFlagNTLMSSPNEGOTIATEIDENTIFY = 1 << 20
 
-	/*R*/
-	negotiateFlagNTLMSSPREQUESTNONNTSESSIONKEY = 1 << 22
+	/*R*/ negotiateFlagNTLMSSPREQUESTNONNTSESSIONKEY = 1 << 22
 	/*S*/ negotiateFlagNTLMSSPNEGOTIATETARGETINFO = 1 << 23
 
-	/*T*/
-	negotiateFlagNTLMSSPNEGOTIATEVERSION = 1 << 25
+	/*T*/ negotiateFlagNTLMSSPNEGOTIATEVERSION = 1 << 25
 
-	/*U*/
-	negotiateFlagNTLMSSPNEGOTIATE128 = 1 << 29
+	/*U*/ negotiateFlagNTLMSSPNEGOTIATE128 = 1 << 29
 	/*V*/ negotiateFlagNTLMSSPNEGOTIATEKEYEXCH = 1 << 30
 	/*W*/ negotiateFlagNTLMSSPNEGOTIATE56 = 1 << 31
 )
