78741877d94bbf4ea0ab1b77d6efa3276ff71f18
James Bardin <j.bardin@gmail.com>
2017-09-06 13:44:37 -0400

    change the FolderStorage key
    updating the key will cause the FolderStorage hash to change forcing
    modules to be re-fetched. This is required because any configurations
    using the subDir notation will have the configuration in the wrong
    directory.

--- config/module/tree.go
+++ config/module/tree.go
@@ -182,7 +182,7 @@ func (t *Tree) Load(s getter.Storage, mode GetMode) error {
 		}
 		// Get the directory where this module is so we can load it
 		key := strings.Join(path, ".")
-		key = fmt.Sprintf("root.%s-%s", key, m.Source)
+		key = fmt.Sprintf("module.%s-%s", key, m.Source)
 
 		dir, ok, err := getStorage(s, key, source, mode)
 		if err != nil {
