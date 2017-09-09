c31aaaffd3d727bb444c7a6f57d2948187be8f4b
Bruno Miguel Custodio <brunomcustodio@gmail.com>
2017-09-09 08:03:59 +0100

    Add TLS support.

--- backend/remote-state/etcdv3/backend.go
+++ backend/remote-state/etcdv3/backend.go
@@ -4,14 +4,28 @@ import (
 	"context"
 
 	etcdv3 "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/pkg/transport"
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/helper/schema"
+)
+
+const (
+	endpointsKey       = "endpoints"
+	usernameKey        = "username"
+	usernameEnvVarName = "ETCDV3_USERNAME"
+	passwordKey        = "password"
+	passwordEnvVarName = "ETCDV3_PASSWORD"
+	prefixKey          = "prefix"
+	lockKey            = "lock"
+	cacertPathKey      = "cacert_path"
+	certPathKey        = "cert_path"
+	keyPathKey         = "key_path"
 )
 
 func New() backend.Backend {
 	s := &schema.Backend{
 		Schema: map[string]*schema.Schema{
-			"endpoints": &schema.Schema{
+			endpointsKey: &schema.Schema{
 				Type: schema.TypeList,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
@@ -21,32 +35,53 @@ func New() backend.Backend {
 				Description: "Endpoints for the etcd cluster.",
 			},
 
-			"username": &schema.Schema{
+			usernameKey: &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "Username used to connect to the etcd cluster.",
-				DefaultFunc: schema.EnvDefaultFunc("ETCDV3_USERNAME", ""),
+				DefaultFunc: schema.EnvDefaultFunc(usernameEnvVarName, ""),
 			},
 
-			"password": &schema.Schema{
+			passwordKey: &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "Password used to connect to the etcd cluster.",
-				DefaultFunc: schema.EnvDefaultFunc("ETCDV3_PASSWORD", ""),
+				DefaultFunc: schema.EnvDefaultFunc(passwordEnvVarName, ""),
 			},
 
-			"prefix": &schema.Schema{
+			prefixKey: &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "An optional prefix to be added to keys when to storing state in etcd.",
 				Default:     "",
 			},
 
-			"lock": &schema.Schema{
+			lockKey: &schema.Schema{
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Description: "Whether to lock state access.",
 				Default:     true,
+			},
+
+			cacertPathKey: &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The path to a PEM-encoded CA bundle with which to verify certificates of TLS-enabled etcd servers.",
+				Default:     "",
+			},
+
+			certPathKey: &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The path to a PEM-encoded certificate to provide to etcd for secure client identification.",
+				Default:     "",
+			},
+
+			keyPathKey: &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The path to a PEM-encoded key to provide to etcd for secure client identification.",
+				Default:     "",
 			},
 		},
 	}
@@ -71,9 +106,9 @@ func (b *Backend) configure(ctx context.Context) error {
 	// Grab the resource data.
 	b.data = schema.FromContextBackendConfig(ctx)
 	// Store the lock information.
-	b.lock = b.data.Get("lock").(bool)
+	b.lock = b.data.Get(lockKey).(bool)
 	// Store the prefix information.
-	b.prefix = b.data.Get("prefix").(string)
+	b.prefix = b.data.Get(prefixKey).(string)
 	// Initialize a client to test config.
 	b.client, err = b.rawClient()
 	// Return err, if any.
@@ -82,15 +117,31 @@ func (b *Backend) configure(ctx context.Context) error {
 
 func (b *Backend) rawClient() (*etcdv3.Client, error) {
 	config := etcdv3.Config{}
+	tlsInfo := transport.TLSInfo{}
 
-	if v, ok := b.data.GetOk("endpoints"); ok {
+	if v, ok := b.data.GetOk(endpointsKey); ok {
 		config.Endpoints = retrieveEndpoints(v)
 	}
-	if v, ok := b.data.GetOk("username"); ok && v.(string) != "" {
+	if v, ok := b.data.GetOk(usernameKey); ok && v.(string) != "" {
 		config.Username = v.(string)
 	}
-	if v, ok := b.data.GetOk("password"); ok && v.(string) != "" {
+	if v, ok := b.data.GetOk(passwordKey); ok && v.(string) != "" {
 		config.Password = v.(string)
+	}
+	if v, ok := b.data.GetOk(cacertPathKey); ok && v.(string) != "" {
+		tlsInfo.TrustedCAFile = v.(string)
+	}
+	if v, ok := b.data.GetOk(certPathKey); ok && v.(string) != "" {
+		tlsInfo.CertFile = v.(string)
+	}
+	if v, ok := b.data.GetOk(keyPathKey); ok && v.(string) != "" {
+		tlsInfo.KeyFile = v.(string)
+	}
+
+	if tlsCfg, err := tlsInfo.ClientConfig(); err != nil {
+		return nil, err
+	} else if !tlsInfo.Empty() {
+		config.TLS = tlsCfg // Assign TLS configuration only if it valid and non-empty.
 	}
 
 	return etcdv3.New(config)
