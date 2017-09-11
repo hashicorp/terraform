9855d9645290944f612308aec438e543196de69e
Daniel Compton <desk@danielcompton.net>
2017-09-11 21:50:02 +1200

    Fix init help text for upgrade
    -upgrade=true will upgrade plugins to the latest allowable version.

--- command/init.go
+++ command/init.go
@@ -505,7 +505,7 @@ Options:
   -reconfigure         Reconfigure the backend, ignoring any saved
                        configuration.
 
-  -upgrade=false       If installing modules (-get) or plugins (-get-plugins),
+  -upgrade=true        If installing modules (-get) or plugins (-get-plugins),
                        ignore previously-downloaded objects and install the
                        latest version allowed within configured constraints.
 
