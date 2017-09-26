c4dd31e62bfec81c26c2d97f424072d91f261cf5
James Bardin <j.bardin@gmail.com>
2017-09-26 18:58:43 -0400

    "destroy" outputs too
    Ouptuts don't need to be re-evaluated during destroy, since everything
    is already in the state, so we can simply remove them.

--- terraform/node_output.go
+++ terraform/node_output.go
@@ -69,15 +69,21 @@ func (n *NodeApplyableOutput) References() []string {
 
 // GraphNodeEvalable
 func (n *NodeApplyableOutput) EvalTree() EvalNode {
-	return &EvalOpFilter{
-		Ops: []walkOperation{walkRefresh, walkPlan, walkApply,
-			walkDestroy, walkInput, walkValidate},
-		Node: &EvalSequence{
-			Nodes: []EvalNode{
-				&EvalWriteOutput{
+	return &EvalSequence{
+		Nodes: []EvalNode{
+			&EvalOpFilter{
+				Ops: []walkOperation{walkRefresh, walkPlan, walkApply,
+					walkInput, walkValidate},
+				Node: &EvalWriteOutput{
 					Name:      n.Config.Name,
 					Sensitive: n.Config.Sensitive,
 					Value:     n.Config.RawConfig,
+				},
+			},
+			&EvalOpFilter{
+				Ops: []walkOperation{walkDestroy, walkPlanDestroy},
+				Node: &EvalDeleteOutput{
+					Name: n.Config.Name,
 				},
 			},
 		},
