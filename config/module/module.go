29e5a355b937bd30d8474ff6e7e331743460a3f5
James Bardin <j.bardin@gmail.com>
2017-10-05 14:46:20 -0400

    add "version" and "provider" to module config
    Add the Version and Providers fields to the module config.
    
    Add ProviderConfig.Scope, which will be used to record the original
    path of a ProviderConfig for interpolation.

--- config/module/module.go
+++ config/module/module.go
@@ -1,7 +1,11 @@
 package module
 
+import "github.com/hashicorp/terraform/config"
+
 // Module represents the metadata for a single module.
 type Module struct {
-	Name   string
-	Source string
+	Name      string
+	Source    string
+	Version   string
+	Providers []*config.ProviderConfig
 }
