a50a3839460af999ffe4a4be92f4eaf6b621919b
Nolan Davidson <ndavidson@chef.io>
2017-10-06 12:20:52 -0400

    Additional work on the habitat provisioner.
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

---
+++ builtin/provisioners/habitat/resource_provisioner_test.go
@@ -0,0 +1,74 @@
+package habitat
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/config"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestResourceProvisioner_impl(t *testing.T) {
+	var _ terraform.ResourceProvisioner = Provisioner()
+}
+
+func TestProvisioner(t *testing.T) {
+	if err := Provisioner().(*schema.Provisioner).InternalValidate(); err != nil {
+		t.Fatalf("error: %s", err)
+	}
+}
+
+func TestResourceProvisioner_Validate_good(t *testing.T) {
+	c := testConfig(t, map[string]interface{}{
+		"peer":         "1.2.3.4",
+		"version":      "0.32.0",
+		"service_type": "systemd",
+	})
+
+	warn, errs := Provisioner().Validate(c)
+	if len(warn) > 0 {
+		t.Fatalf("Warnings: %v", warn)
+	}
+	if len(errs) > 0 {
+		t.Fatalf("Errors: %v", errs)
+	}
+}
+
+func TestResourceProvisioner_Validate_bad(t *testing.T) {
+	c := testConfig(t, map[string]interface{}{
+		"service_type": "invalidtype",
+	})
+
+	warn, errs := Provisioner().Validate(c)
+	if len(warn) > 0 {
+		t.Fatalf("Warnings: %v", warn)
+	}
+	if len(errs) != 1 {
+		t.Fatalf("Should have one error")
+	}
+}
+
+func TestResourceProvisioner_Validate_bad_service_config(t *testing.T) {
+	c := testConfig(t, map[string]interface{}{
+		"service": []map[string]interface{}{
+			map[string]interface{}{"name": "core/foo", "strategy": "bar", "topology": "baz", "url": "badurl"},
+		},
+	})
+
+	warn, errs := Provisioner().Validate(c)
+	if len(warn) > 0 {
+		t.Fatalf("Warnings: %v", warn)
+	}
+	if len(errs) != 3 {
+		t.Fatalf("Should have three errors")
+	}
+}
+
+func testConfig(t *testing.T, c map[string]interface{}) *terraform.ResourceConfig {
+	r, err := config.NewRawConfig(c)
+	if err != nil {
+		t.Fatalf("config error: %s", err)
+	}
+
+	return terraform.NewResourceConfig(r)
+}
