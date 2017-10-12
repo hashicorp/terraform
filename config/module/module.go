1136c74bb8758922915cd10d62b3fe7e257a1605
James Bardin <j.bardin@gmail.com>
2017-10-12 13:43:04 -0400

    implement provider inheritence during loading
    This implements provider inheritance during config loading, rather than
    during graph evaluation. At this point it's much simpler to find the
    desired configuration, and once all providers are declared, all the
    inheritance code in the graph can be removed.
    
    The inheritance is dome by simply copying the RawConfig from the parent
    ProviderConfig into the module. Since this happens before any
    evaluation, we record the original interpolation scope in the
    ProviderConfig so that it can be properly resolved later on.

--- config/module/module.go
+++ config/module/module.go
@@ -1,11 +1,9 @@
 package module
 
-import "github.com/hashicorp/terraform/config"
-
 // Module represents the metadata for a single module.
 type Module struct {
 	Name      string
 	Source    string
 	Version   string
-	Providers []*config.ProviderConfig
+	Providers map[string]string
 }
