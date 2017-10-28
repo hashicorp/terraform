9e16fbdcddd19ad5ad9dacbc59f8f4b865e13f5a
James Bardin <j.bardin@gmail.com>
2017-10-28 19:45:10 -0400

    update tests for new consul packages
    Reuse the running consul server for all tests.
    
    Update the lostLockConnection package, since the api client should no
    longer lose a lock immediately on network errors.

--- backend/remote-state/consul/backend_test.go
+++ backend/remote-state/consul/backend_test.go
@@ -15,14 +15,28 @@ func TestBackend_impl(t *testing.T) {
 	var _ backend.Backend = new(Backend)
 }
 
-func newConsulTestServer(t *testing.T) *testutil.TestServer {
-	skip := os.Getenv("TF_ACC") == "" && os.Getenv("TF_CONSUL_TEST") == ""
-	if skip {
-		t.Log("consul server tests require setting TF_ACC or TF_CONSUL_TEST")
-		t.Skip()
+var srv *testutil.TestServer
+
+func TestMain(m *testing.M) {
+	if os.Getenv("TF_ACC") == "" && os.Getenv("TF_CONSUL_TEST") == "" {
+		fmt.Println("consul server tests require setting TF_ACC or TF_CONSUL_TEST")
+		return
 	}
 
-	srv, err := testutil.NewTestServerConfigT(t, func(c *testutil.TestServerConfig) {
+	var err error
+	srv, err = newConsulTestServer()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	rc := m.Run()
+	srv.Stop()
+	os.Exit(rc)
+}
+
+func newConsulTestServer() (*testutil.TestServer, error) {
+	srv, err := testutil.NewTestServerConfig(func(c *testutil.TestServerConfig) {
 		c.LogLevel = "warn"
 
 		if !testing.Verbose() {
@@ -31,17 +45,10 @@ func newConsulTestServer(t *testing.T) *testutil.TestServer {
 		}
 	})
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return srv
+	return srv, err
 }
 
 func TestBackend(t *testing.T) {
-	srv := newConsulTestServer(t)
-	defer srv.Stop()
-
 	path := fmt.Sprintf("tf-unit/%s", time.Now().String())
 
 	// Get the backend. We need two to test locking.
@@ -60,9 +67,6 @@ func TestBackend(t *testing.T) {
 }
 
 func TestBackend_lockDisabled(t *testing.T) {
-	srv := newConsulTestServer(t)
-	defer srv.Stop()
-
 	path := fmt.Sprintf("tf-unit/%s", time.Now().String())
 
 	// Get the backend. We need two to test locking.
@@ -83,9 +87,6 @@ func TestBackend_lockDisabled(t *testing.T) {
 }
 
 func TestBackend_gzip(t *testing.T) {
-	srv := newConsulTestServer(t)
-	defer srv.Stop()
-
 	// Get the backend
 	b := backend.TestBackendConfig(t, New(), map[string]interface{}{
 		"address": srv.HTTPAddr,
