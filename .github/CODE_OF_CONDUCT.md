a3ced1a367857e0be1fd72df6627de4b89284be7
Martin Atkins <mart@degeneration.co.uk>
2017-11-02 10:46:31 -0700

    provider/terraform: reorganize for merge into core
    The "terraform" provider was previously split out into its own repository,
    but that turned out to be a mistake due to how tightly it depends on
    aspects of Terraform Core.
    
    Here we prepare to bring it back into the core repository by reorganizing
    the directory layout to conform with what's expected there.

--- .github/CODE_OF_CONDUCT.md
+++
@@ -1,5 +0,0 @@
-# Code of Conduct
-
-HashiCorp Community Guidelines apply to you when interacting with the community here on GitHub and contributing code.
-
-Please read the full text at https://www.hashicorp.com/community-guidelines
--- vendor/github.com/gophercloud/gophercloud/openstack/dns/v2/recordsets/doc.go
+++
@@ -1,6 +0,0 @@
-// Package recordsets provides information and interaction with the zone API
-// resource for the OpenStack DNS service.
-//
-// For more information, see:
-// http://developer.openstack.org/api-ref/dns/#recordsets
-package recordsets
