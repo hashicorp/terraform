d4ee58ce596bca08a807b3d98cdf2276013d7f3a
Martin Atkins <mart@degeneration.co.uk>
2017-11-03 11:36:31 -0700

    Re-integrate the "terraform" provider into the main binary
    As part of the 0.10 core/provider split we moved this provider, along with
    all the others, out into its own repository.
    
    In retrospect, the "terraform" provider doesn't really make sense to be
    separated since it's just a thin wrapper around some core code anyway,
    and so re-integrating it into core avoids the confusion that results when
    Terraform Core and the terraform provider have inconsistent versions of
    the backend code and dependencies.
    
    There is no good reason to use a different version of the backend code
    in the provider than in core, so this new "internal provider" mechanism
    is stricter than the old one: it's not possible to use an external build
    of this provider at all, and version constraints for it are rejected as
    a result.
    
    This provider is also run in-process rather than in a child process, since
    again it's just a very thin wrapper around code that's already running
    in Terraform core anyway, and so the process barrier between the two does
    not create enough advantage to warrant the additional complexity.

--- command/e2etest/init_test.go
+++ command/e2etest/init_test.go
@@ -50,6 +50,37 @@ func TestInitProviders(t *testing.T) {
 
 }
 
+func TestInitProvidersInternal(t *testing.T) {
+	t.Parallel()
+
+	// This test should _not_ reach out anywhere because the "terraform"
+	// provider is internal to the core terraform binary.
+
+	fixturePath := filepath.Join("test-fixtures", "terraform-provider")
+	tf := e2e.NewBinary(terraformBin, fixturePath)
+	defer tf.Close()
+
+	stdout, stderr, err := tf.Run("init")
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if stderr != "" {
+		t.Errorf("unexpected stderr output:\n%s", stderr)
+	}
+
+	if !strings.Contains(stdout, "Terraform has been successfully initialized!") {
+		t.Errorf("success message is missing from output:\n%s", stdout)
+	}
+
+	if strings.Contains(stdout, "Downloading plugin for provider") {
+		// Shouldn't have downloaded anything with this config, because the
+		// provider is built in.
+		t.Errorf("provider download message appeared in output:\n%s", stdout)
+	}
+
+}
+
 func TestInitProviders_pluginCache(t *testing.T) {
 	t.Parallel()
 
