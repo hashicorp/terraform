8398e2ccf879335b0142ee831608fe81473d4083
SchreiverJ <SchreiverJ@gmail.com>
2018-01-03 17:50:53 -0500

    change from omnitruck-direct to omnitruck
    https://docs.chef.io/api_omnitruck.html

--- builtin/provisioners/chef/linux_provisioner_test.go
+++ builtin/provisioners/chef/linux_provisioner_test.go
@@ -25,7 +25,7 @@ func TestResourceProvider_linuxInstallChefClient(t *testing.T) {
 			},
 
 			Commands: map[string]bool{
-				"sudo curl -LO https://omnitruck-direct.chef.io/chef/install.sh": true,
+				"sudo curl -LO https://omnitruck.chef.io/chef/install.sh": true,
 				"sudo bash ./install.sh -v \"\"":                                 true,
 				"sudo rm -f install.sh":                                          true,
 			},
@@ -43,7 +43,7 @@ func TestResourceProvider_linuxInstallChefClient(t *testing.T) {
 			},
 
 			Commands: map[string]bool{
-				"curl -LO https://omnitruck-direct.chef.io/chef/install.sh": true,
+				"curl -LO https://omnitruck.chef.io/chef/install.sh": true,
 				"bash ./install.sh -v \"\"":                                 true,
 				"rm -f install.sh":                                          true,
 			},
@@ -61,7 +61,7 @@ func TestResourceProvider_linuxInstallChefClient(t *testing.T) {
 			},
 
 			Commands: map[string]bool{
-				"http_proxy='http://proxy.local' curl -LO https://omnitruck-direct.chef.io/chef/install.sh": true,
+				"http_proxy='http://proxy.local' curl -LO https://omnitruck.chef.io/chef/install.sh": true,
 				"http_proxy='http://proxy.local' bash ./install.sh -v \"\"":                                 true,
 				"http_proxy='http://proxy.local' rm -f install.sh":                                          true,
 			},
@@ -79,7 +79,7 @@ func TestResourceProvider_linuxInstallChefClient(t *testing.T) {
 			},
 
 			Commands: map[string]bool{
-				"https_proxy='https://proxy.local' curl -LO https://omnitruck-direct.chef.io/chef/install.sh": true,
+				"https_proxy='https://proxy.local' curl -LO https://omnitruck.chef.io/chef/install.sh": true,
 				"https_proxy='https://proxy.local' bash ./install.sh -v \"\"":                                 true,
 				"https_proxy='https://proxy.local' rm -f install.sh":                                          true,
 			},
