74b7d81898617ad229e53876fed0fd8c3c1d928f
James Bardin <j.bardin@gmail.com>
2018-01-30 10:42:49 -0500

    catch missing id attribute during interpolation
    The id attribute can be missing during the destroy operation.
    While the new destroy-time ordering of outputs and locals should prevent
    resources from having their id attributes set to an empty string,
    there's no reason to error out if we have the canonical ID field
    available.
    
    This still interrogates the attributes map first to retain any previous
    behavior, but in the future we should settle on a single ID location.

--- terraform/interpolate.go
+++ terraform/interpolate.go
@@ -518,6 +518,16 @@ func (i *Interpolater) computeResourceVariable(
 		return &v, err
 	}
 
+	// special case for the "id" field which is usually also an attribute
+	if v.Field == "id" && r.Primary.ID != "" {
+		// This is usually pulled from the attributes, but is sometimes missing
+		// during destroy. We can return the ID field in this case.
+		// FIXME: there should only be one ID to rule them all.
+		log.Printf("[WARN] resource %s missing 'id' attribute", v.ResourceId())
+		v, err := hil.InterfaceToVariable(r.Primary.ID)
+		return &v, err
+	}
+
 	// computed list or map attribute
 	_, isList = r.Primary.Attributes[v.Field+".#"]
 	_, isMap = r.Primary.Attributes[v.Field+".%"]
@@ -653,6 +663,11 @@ func (i *Interpolater) computeResourceMultiVariable(
 		if singleAttr, ok := r.Primary.Attributes[v.Field]; ok {
 			values = append(values, singleAttr)
 			continue
+		}
+
+		if v.Field == "id" && r.Primary.ID != "" {
+			log.Printf("[WARN] resource %s missing 'id' attribute", v.ResourceId())
+			values = append(values, r.Primary.ID)
 		}
 
 		// computed list or map attribute
