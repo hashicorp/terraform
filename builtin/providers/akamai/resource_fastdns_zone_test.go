448bbf2074a83e113725afc3e84e86c7f68aa19a
Johanna Cherry <johanna@johannacherry.com>
2018-02-03 00:33:22 -0600

    Implement the destroy and exists functions of the test

--- builtin/providers/akamai/resource_fastdns_zone_test.go
+++ builtin/providers/akamai/resource_fastdns_zone_test.go
@@ -2,10 +2,13 @@ package akamai
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/configdns-v1"
 )
 
 var testAccAkamaiFastDNSZoneConfig = fmt.Sprintf(`
@@ -63,9 +66,55 @@ func TestAccAkamaiFastDNSZone_basic(t *testing.T) {
 }
 
 func testAccCheckAkamaiFastDNSZoneDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "akamai_fastdns_zone" {
+			continue
+		}
+
+		hostname := strings.Split(rs.Primary.ID, "-")[2]
+		zone, err := dns.GetZone(hostname)
+		if err != nil {
+			return err
+		}
+		if len(zone.Zone.A) > 0 ||
+			len(zone.Zone.Aaaa) > 0 ||
+			len(zone.Zone.Afsdb) > 0 ||
+			len(zone.Zone.Cname) > 0 ||
+			len(zone.Zone.Dnskey) > 0 ||
+			len(zone.Zone.Ds) > 0 ||
+			len(zone.Zone.Hinfo) > 0 ||
+			len(zone.Zone.Loc) > 0 ||
+			len(zone.Zone.Mx) > 0 ||
+			len(zone.Zone.Naptr) > 0 ||
+			len(zone.Zone.Nsec3) > 0 ||
+			len(zone.Zone.Nsec3param) > 0 ||
+			len(zone.Zone.Ptr) > 0 ||
+			len(zone.Zone.Rp) > 0 ||
+			len(zone.Zone.Rrsig) > 0 ||
+			len(zone.Zone.Spf) > 0 ||
+			len(zone.Zone.Srv) > 0 ||
+			len(zone.Zone.Sshfp) > 0 ||
+			len(zone.Zone.Txt) > 0 {
+			// These never get deleted
+			// len(zone.Zone.Ns) > 0 ||
+			// len(zone.Zone.Soa) > 0 ||
+			return fmt.Errorf("Zone was not deleted %s", zone)
+		}
+	}
 	return nil
 }
 
 func testAccCheckAkamaiFastDNSZoneExists(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "akamai_fastdns_zone" {
+			continue
+		}
+
+		hostname := strings.Split(rs.Primary.ID, "-")[2]
+		_, err := dns.GetZone(hostname)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
