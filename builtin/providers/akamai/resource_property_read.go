66fcccffac3c0e2540f707212d5af233c0ae86b8
Johanna Cherry <johanna@johannacherry.com>
2018-02-04 20:08:12 -0600

    Never write to the state file except for in create/update functions, to prevent diff issues with server side added data

--- builtin/providers/akamai/resource_property_read.go
+++ builtin/providers/akamai/resource_property_read.go
@@ -1,8 +1,6 @@
 package akamai
 
 import (
-	"log"
-
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -19,40 +17,5 @@ func resourcePropertyExists(d *schema.ResourceData, meta interface{}) (bool, err
 }
 
 func resourcePropertyRead(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG] READING")
-
-	propertyId := d.Id()
-
-	if propertyId == "" {
-		results, err := papi.Search(papi.SearchByPropertyName, d.Get("name").(string))
-		if err != nil {
-			return err
-		}
-
-		if len(results.Versions.Items) > 0 {
-			propertyId = results.Versions.Items[0].PropertyID
-		}
-	}
-
-	if propertyId != "" {
-		property := papi.NewProperty(papi.NewProperties())
-		property.PropertyID = propertyId
-		e := property.GetProperty()
-		if e != nil {
-			return e
-		}
-
-		d.Set("account_id", property.AccountID)
-		d.Set("contract_id", property.ContractID)
-		d.Set("group_id", property.GroupID)
-		d.Set("product_id", property.ProductID)
-		d.Set("rule_format", property.RuleFormat)
-		// d.Set("clone_from", property.CloneFrom)
-		d.Set("name", property.PropertyName)
-		d.SetId(property.PropertyID)
-	}
-
-	log.Println("[DEBUG] Done")
-
 	return nil
 }
