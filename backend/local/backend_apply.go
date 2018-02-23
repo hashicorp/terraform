ee7756664d212299422c5ffc2052faeab3f4b667
James Bardin <j.bardin@gmail.com>
2018-02-23 11:31:10 -0500

    Revert "use the new clistate.Locker in the local backend"
    This reverts commit 0b804a9686b1d56446bd1cab2f201022d0f4ad5d.

--- backend/local/backend_apply.go
+++ backend/local/backend_apply.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform/backend"
+	"github.com/hashicorp/terraform/command/clistate"
 	"github.com/hashicorp/terraform/command/format"
 	"github.com/hashicorp/terraform/config/module"
 	"github.com/hashicorp/terraform/state"
@@ -52,6 +53,20 @@ func (b *Local) opApply(
 	if err != nil {
 		runningOp.Err = err
 		return
+	}
+
+	if op.LockState {
+		lockCtx, cancel := context.WithTimeout(stopCtx, op.StateLockTimeout)
+		defer cancel()
+
+		unlock, err := clistate.Lock(lockCtx, opState, op.Type.String(), "", b.CLI, b.Colorize())
+		if err != nil {
+			runningOp.Err = errwrap.Wrapf("Error locking state: {{err}}", err)
+			return
+		}
+		defer func() {
+			runningOp.Err = unlock(runningOp.Err)
+		}()
 	}
 
 	// Setup the state
--- backend/local/backend_plan.go
+++ backend/local/backend_plan.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/terraform/backend"
+	"github.com/hashicorp/terraform/command/clistate"
 	"github.com/hashicorp/terraform/command/format"
 	"github.com/hashicorp/terraform/config/module"
 	"github.com/hashicorp/terraform/terraform"
@@ -57,6 +58,20 @@ func (b *Local) opPlan(
 	if err != nil {
 		runningOp.Err = err
 		return
+	}
+
+	if op.LockState {
+		lockCtx, cancel := context.WithTimeout(stopCtx, op.StateLockTimeout)
+		defer cancel()
+
+		unlock, err := clistate.Lock(lockCtx, opState, op.Type.String(), "", b.CLI, b.Colorize())
+		if err != nil {
+			runningOp.Err = errwrap.Wrapf("Error locking state: {{err}}", err)
+			return
+		}
+		defer func() {
+			runningOp.Err = unlock(runningOp.Err)
+		}()
 	}
 
 	// Setup the state
