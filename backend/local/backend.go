e9a76808df4cc3eaa629969b1a2eb3d7448698dc
James Bardin <j.bardin@gmail.com>
2018-02-22 20:43:21 -0500

    create clistate.Locker interface
    Simplify the use of clistate.Lock by creating a clistate.Locker
    instance, which stores the context of locking a state, to allow unlock
    to be called without knowledge of how the state was locked.
    
    This alows the backend code to bring the needed UI methods to the point
    where the state is locked, and still unlock the state from an outer
    scope.
    
    Provide a NoopLocker as well, so that callers can always call Unlock
    without verifying the status of the lock.
    
    Add the StateLocker field to the backend.Operation, so that the state
    lock can be carried between the different function scopes of the backend
    code. This will allow the backend context to lock the state before it's
    read, while allowing the different operations to unlock the state when
    they complete.

--- backend/local/backend.go
+++ backend/local/backend.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 
 	"github.com/hashicorp/terraform/backend"
+	"github.com/hashicorp/terraform/command/clistate"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/terraform"
@@ -260,11 +261,23 @@ func (b *Local) Operation(ctx context.Context, op *backend.Operation) (*backend.
 	cancelCtx, cancel := context.WithCancel(context.Background())
 	runningOp.Cancel = cancel
 
+	if op.LockState {
+		op.StateLocker = clistate.NewLocker(stopCtx, op.StateLockTimeout, b.CLI, b.Colorize())
+	} else {
+		op.StateLocker = clistate.NewNoopLocker()
+	}
+
 	// Do it
 	go func() {
 		defer done()
 		defer stop()
 		defer cancel()
+
+		// the state was locked during context creation, unlock the state when
+		// the operation completes
+		defer func() {
+			runningOp.Err = op.StateLocker.Unlock(runningOp.Err)
+		}()
 
 		defer b.opLock.Unlock()
 		f(stopCtx, cancelCtx, op, runningOp)
