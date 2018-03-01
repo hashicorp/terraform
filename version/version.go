e25b31128e45aa17d915237bab9efc663e9dfa98
Martin Atkins <mart@degeneration.co.uk>
2018-03-01 10:55:57 -0800

    version: change to 0.12.0 to mark the beginning of the 0.12 dev branch

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 // The main version number that is being run at the moment.
-const Version = "0.11.9"
+const Version = "0.12.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
