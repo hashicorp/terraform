766e94b1900435478de004905f0619daf37477fb
John E Vincent <lusis.org+github.com@gmail.com>
2018-03-06 10:39:02 -0500

    updating artifactory remote state support

--- state/remote/artifactory.go
+++ state/remote/artifactory.go
@@ -6,27 +6,50 @@ import (
 	"os"
 	"strings"
 
-	artifactory "github.com/lusis/go-artifactory/src/artifactory.v401"
+	artifactory "github.com/lusis/go-artifactory/artifactory.v54"
 )
 
+// ARTIF_TFSTATE_NAME is the constant for the terraform state file stored in artifactory
 const ARTIF_TFSTATE_NAME = "terraform.tfstate"
 
 func artifactoryFactory(conf map[string]string) (Client, error) {
-	userName, ok := conf["username"]
-	if !ok {
-		userName = os.Getenv("ARTIFACTORY_USERNAME")
-		if userName == "" {
-			return nil, fmt.Errorf(
-				"missing 'username' configuration or ARTIFACTORY_USERNAME environment variable")
+	var (
+		userName   string
+		password   string
+		authmethod string
+		token      string
+	)
+	token, hasToken := conf["token"]
+	if !hasToken {
+		token = os.Getenv("ARTIFACTORY_TOKEN")
+		if token == "" {
+			authmethod = "basic"
+		} else {
+			authmethod = "token"
 		}
+	} else {
+		authmethod = "token"
 	}
-	password, ok := conf["password"]
-	if !ok {
-		password = os.Getenv("ARTIFACTORY_PASSWORD")
-		if password == "" {
-			return nil, fmt.Errorf(
-				"missing 'password' configuration or ARTIFACTORY_PASSWORD environment variable")
+	if authmethod == "basic" {
+		u, ok := conf["username"]
+		if !ok {
+			u = os.Getenv("ARTIFACTORY_USERNAME")
+			if u == "" {
+				return nil, fmt.Errorf(
+					"missing 'username' configuration or ARTIFACTORY_USERNAME environment variable")
+			}
+
 		}
+		p, ok := conf["password"]
+		if !ok {
+			p = os.Getenv("ARTIFACTORY_PASSWORD")
+			if p == "" {
+				return nil, fmt.Errorf(
+					"missing 'password' configuration or ARTIFACTORY_PASSWORD environment variable")
+			}
+		}
+		userName = u
+		password = p
 	}
 	url, ok := conf["url"]
 	if !ok {
@@ -47,17 +70,23 @@ func artifactoryFactory(conf map[string]string) (Client, error) {
 			"missing 'subpath' configuration")
 	}
 
-	clientConf := &artifactory.ClientConfig{
-		BaseURL:  url,
-		Username: userName,
-		Password: password,
+	clientConf := artifactory.ClientConfig{
+		BaseURL: url,
 	}
-	nativeClient := artifactory.NewClient(clientConf)
+	if authmethod == "token" {
+		clientConf.Token = token
+	} else {
+		clientConf.Username = userName
+		clientConf.Password = password
+	}
+	clientConf.AuthMethod = authmethod
+	nativeClient := artifactory.NewClient(&clientConf)
 
 	return &ArtifactoryClient{
 		nativeClient: &nativeClient,
 		userName:     userName,
 		password:     password,
+		token:        token,
 		url:          url,
 		repo:         repo,
 		subpath:      subpath,
@@ -65,15 +94,18 @@ func artifactoryFactory(conf map[string]string) (Client, error) {
 
 }
 
+// ArtifactoryClient is a wrapper around artifactory.Client for terraform to use
 type ArtifactoryClient struct {
-	nativeClient *artifactory.ArtifactoryClient
+	nativeClient *artifactory.Client
 	userName     string
 	password     string
+	token        string
 	url          string
 	repo         string
 	subpath      string
 }
 
+// Get gets a resource from Artifactory
 func (c *ArtifactoryClient) Get() (*Payload, error) {
 	p := fmt.Sprintf("%s/%s/%s", c.repo, c.subpath, ARTIF_TFSTATE_NAME)
 	output, err := c.nativeClient.Get(p, make(map[string]string))
@@ -101,15 +133,14 @@ func (c *ArtifactoryClient) Get() (*Payload, error) {
 	return payload, nil
 }
 
+// Put puts a resource in artifactory
 func (c *ArtifactoryClient) Put(data []byte) error {
 	p := fmt.Sprintf("%s/%s/%s", c.repo, c.subpath, ARTIF_TFSTATE_NAME)
-	if _, err := c.nativeClient.Put(p, string(data), make(map[string]string)); err == nil {
-		return nil
-	} else {
-		return fmt.Errorf("Failed to upload state: %v", err)
-	}
+	_, err := c.nativeClient.Put(p, data, make(map[string]string))
+	return fmt.Errorf("Failed to upload state: %v", err)
 }
 
+// Delete deletes a resource in artifactory
 func (c *ArtifactoryClient) Delete() error {
 	p := fmt.Sprintf("%s/%s/%s", c.repo, c.subpath, ARTIF_TFSTATE_NAME)
 	err := c.nativeClient.Delete(p)
--- state/remote/artifactory_test.go
+++ state/remote/artifactory_test.go
@@ -1,6 +1,7 @@
 package remote
 
 import (
+	"os"
 	"testing"
 )
 
@@ -9,47 +10,195 @@ func TestArtifactoryClient_impl(t *testing.T) {
 }
 
 func TestArtifactoryFactory(t *testing.T) {
+	for _, envVar := range []string{
+		"ARTIFACTORY_TOKEN",
+		"ARTIFACTORY_URL",
+		"ARTIFACTORY_USERNAME",
+		"ARTIFACTORY_PASSWORD",
+	} {
+		_ = os.Unsetenv(envVar)
+	}
+	testCases := map[string]struct {
+		config     map[string]string
+		shouldfail bool
+		authmethod string
+	}{
+		"valid with basic": {
+			shouldfail: false,
+			config: map[string]string{
+				"url":      "http://artifactory.local",
+				"repo":     "terraform-repo",
+				"subpath":  "myproject",
+				"username": "test",
+				"password": "testpass",
+			},
+			authmethod: "basic",
+		},
+		"valid with token": {
+			shouldfail: false,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+				"token":   "abcdefg",
+			},
+			authmethod: "token",
+		},
+		"invalid with no creds": {
+			shouldfail: true,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+			},
+		},
+		"invalid with no url": {
+			shouldfail: true,
+			config: map[string]string{
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+				"token":   "abcdefg",
+			},
+		},
+		"invalid with no repo": {
+			shouldfail: true,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"subpath": "myproject",
+				"token":   "abcdefg",
+			},
+		},
+		"invalid with no subpath": {
+			shouldfail: true,
+			config: map[string]string{
+				"url":   "http://artifactory.local",
+				"repo":  "terraform-repo",
+				"token": "abcdefg",
+			},
+		},
+	}
 	// This test just instantiates the client. Shouldn't make any actual
 	// requests nor incur any costs.
 
-	config := make(map[string]string)
-
-	// Empty config is an error
-	_, err := artifactoryFactory(config)
-	if err == nil {
-		t.Fatalf("Empty config should be error")
+	for k, v := range testCases {
+		t.Run(k, func(t *testing.T) {
+			client, err := artifactoryFactory(v.config)
+			if v.shouldfail {
+				if err == nil {
+					t.Fatalf("test should throw an error")
+				}
+				if client != nil {
+					t.Fatalf("client should be nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("error should be nil")
+			}
+			ac := client.(*ArtifactoryClient)
+			if ac.nativeClient == nil {
+				t.Fatalf("native client should not be nil")
+			}
+			if ac.nativeClient.Config.AuthMethod != v.authmethod {
+				t.Fatalf("authmethod should match")
+			}
+		})
 	}
+}
 
-	config["url"] = "http://artifactory.local:8081/artifactory"
-	config["repo"] = "terraform-repo"
-	config["subpath"] = "myproject"
+func TestArtifactoryFactoryFromEnv(t *testing.T) {
+	testCases := map[string]struct {
+		config     map[string]string
+		envVars    map[string]string
+		shouldfail bool
+		authmethod string
+	}{
+		"valid with basic from env": {
+			shouldfail: false,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+			},
+			envVars: map[string]string{
+				"ARTIFACTORY_USERNAME": "username",
+				"ARTIFACTORY_PASSWORD": "password",
+			},
+			authmethod: "basic",
+		},
+		"valid with token from env": {
+			shouldfail: false,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+			},
+			envVars: map[string]string{
+				"ARTIFACTORY_TOKEN": "abcdef",
+			},
+			authmethod: "token",
+		},
+		"valid with url from env": {
+			shouldfail: false,
+			config: map[string]string{
+				"repo":     "terraform-repo",
+				"subpath":  "myproject",
+				"username": "test",
+				"password": "testpassword",
+			},
+			envVars: map[string]string{
+				"ARTIFACTORY_URL": "http://artifactory.local",
+			},
+			authmethod: "basic",
+		},
+		"invalid with no creds in env": {
+			shouldfail: true,
+			config: map[string]string{
+				"url":     "http://artifactory.local",
+				"repo":    "terraform-repo",
+				"subpath": "myproject",
+			},
+		},
+	}
+	// This test just instantiates the client. Shouldn't make any actual
+	// requests nor incur any costs.
 
-	// For this test we'll provide the credentials as config. The
-	// acceptance tests implicitly test passing credentials as
-	// environment variables.
-	config["username"] = "test"
-	config["password"] = "testpass"
-
-	client, err := artifactoryFactory(config)
-	if err != nil {
-		t.Fatalf("Error for valid config")
-	}
-
-	artifactoryClient := client.(*ArtifactoryClient)
-
-	if artifactoryClient.nativeClient.Config.BaseURL != "http://artifactory.local:8081/artifactory" {
-		t.Fatalf("Incorrect url was populated")
-	}
-	if artifactoryClient.nativeClient.Config.Username != "test" {
-		t.Fatalf("Incorrect username was populated")
-	}
-	if artifactoryClient.nativeClient.Config.Password != "testpass" {
-		t.Fatalf("Incorrect password was populated")
-	}
-	if artifactoryClient.repo != "terraform-repo" {
-		t.Fatalf("Incorrect repo was populated")
-	}
-	if artifactoryClient.subpath != "myproject" {
-		t.Fatalf("Incorrect subpath was populated")
+	for k, v := range testCases {
+		t.Run(k, func(t *testing.T) {
+			for _, envVar := range []string{
+				"ARTIFACTORY_TOKEN",
+				"ARTIFACTORY_URL",
+				"ARTIFACTORY_USERNAME",
+				"ARTIFACTORY_PASSWORD",
+			} {
+				_ = os.Unsetenv(envVar)
+			}
+			for k, v := range v.envVars {
+				err := os.Setenv(k, v)
+				if err != nil {
+					t.Fatalf("should not throw an error setting env: %s", err.Error())
+				}
+			}
+			client, err := artifactoryFactory(v.config)
+			if v.shouldfail {
+				if err == nil {
+					t.Fatalf("test should throw an error")
+				}
+				if client != nil {
+					t.Fatalf("client should be nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("error should be nil but got %s", err.Error())
+			}
+			ac := client.(*ArtifactoryClient)
+			if ac.nativeClient == nil {
+				t.Fatalf("native client should not be nil")
+			}
+			if ac.nativeClient.Config.AuthMethod != v.authmethod {
+				t.Fatalf("authmethod should match")
+			}
+		})
 	}
 }
