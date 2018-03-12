653fb9f7d1df487fa6df2c91dd3cbaccba2ff4ab
Josh Soref <jsoref@users.noreply.github.com>
2018-03-12 01:34:13 +0000

    spelling: locations

--- registry/test/mock_registry.go
+++ registry/test/mock_registry.go
@@ -54,7 +54,7 @@ var (
 	})
 )
 
-// All the locationes from the mockRegistry start with a file:// scheme. If
+// All the locations from the mockRegistry start with a file:// scheme. If
 // the the location string here doesn't have a scheme, the mockRegistry will
 // find the absolute path and return a complete URL.
 var testMods = map[string][]testMod{
