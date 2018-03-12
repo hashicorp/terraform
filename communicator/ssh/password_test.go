4519b841d916dd8f1b2cf5d033e9a879b6ffce82
Josh Soref <jsoref@users.noreply.github.com>
2018-03-12 01:34:09 +0000

    spelling: keyboard

--- communicator/ssh/password_test.go
+++ communicator/ssh/password_test.go
@@ -15,7 +15,7 @@ func TestPasswordKeyboardInteractive_Impl(t *testing.T) {
 	}
 }
 
-func TestPasswordKeybardInteractive_Challenge(t *testing.T) {
+func TestPasswordKeyboardInteractive_Challenge(t *testing.T) {
 	p := PasswordKeyboardInteractive("foo")
 	result, err := p("foo", "bar", []string{"one", "two"}, nil)
 	if err != nil {
