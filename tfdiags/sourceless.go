b43690c01a239b4235d59edb3ec0a2b3ecb66db1
Martin Atkins <mart@degeneration.co.uk>
2018-03-20 18:44:44 -0700

    tfdiags: Simple helper for creating "sourceless" diagnostics
    While diagnostics are primarily designed for reporting problems in
    configuration, they can also be used for errors and warnings about the
    environment Terraform is running in, such as inability to reach a remote
    service, etc.
    
    Function Sourceless makes it easy to produce such diagnostics with the
    customary summary/detail structure. When these diagnostics are rendered
    they will have no source code snippet and will instead just include the
    English-language content.

---
+++ tfdiags/sourceless.go
@@ -0,0 +1,13 @@
+package tfdiags
+
+// Sourceless creates and returns a diagnostic with no source location
+// information. This is generally used for operational-type errors that are
+// caused by or relate to the environment where Terraform is running rather
+// than to the provided configuration.
+func Sourceless(severity Severity, summary, detail string) Diagnostic {
+	return diagnosticBase{
+		severity: severity,
+		summary:  summary,
+		detail:   detail,
+	}
+}
