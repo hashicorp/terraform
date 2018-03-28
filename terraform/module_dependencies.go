2a33f002edb6464bdb2f6d9b9061dea9e3336c93
Martin Atkins <mart@degeneration.co.uk>
2018-03-27 17:22:51 -0700

    terraform: HCL2-flavored module dependency resolver
    For the moment this is just a lightly-adapted copy of
    ModuleTreeDependencies named ConfigTreeDependencies, with the goal that
    the two can live concurrently for the moment while not all callers are yet
    updated and then we can drop ModuleTreeDependencies and its helper
    functions altogether in a later commit.
    
    This can then be used to make "terraform init" and "terraform providers"
    work properly with the HCL2-powered configuration loader.

--- terraform/module_dependencies.go
+++ terraform/module_dependencies.go
@@ -1,11 +1,206 @@
 package terraform
 
 import (
+	version "github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform/config"
 	"github.com/hashicorp/terraform/config/module"
+	"github.com/hashicorp/terraform/configs"
 	"github.com/hashicorp/terraform/moduledeps"
 	"github.com/hashicorp/terraform/plugin/discovery"
 )
+
+// ConfigTreeDependencies returns the dependencies of the tree of modules
+// described by the given configuration and state.
+//
+// Both configuration and state are required because there can be resources
+// implied by instances in the state that no longer exist in config.
+func ConfigTreeDependencies(root *configs.Config, state *State) *moduledeps.Module {
+	// First we walk the configuration tree to build the overall structure
+	// and capture the explicit/implicit/inherited provider dependencies.
+	deps := configTreeConfigDependencies(root, nil)
+
+	// Next we walk over the resources in the state to catch any additional
+	// dependencies created by existing resources that are no longer in config.
+	// Most things we find in state will already be present in 'deps', but
+	// we're interested in the rare thing that isn't.
+	configTreeMergeStateDependencies(deps, state)
+
+	return deps
+}
+
+func configTreeConfigDependencies(root *configs.Config, inheritProviders map[string]*configs.Provider) *moduledeps.Module {
+	if root == nil {
+		// If no config is provided, we'll make a synthetic root.
+		// This isn't necessarily correct if we're called with a nil that
+		// *isn't* at the root, but in practice that can never happen.
+		return &moduledeps.Module{
+			Name: "root",
+		}
+	}
+
+	name := "root"
+	if len(root.Path) != 0 {
+		name = root.Path[len(root.Path)-1]
+	}
+
+	ret := &moduledeps.Module{
+		Name: name,
+	}
+
+	module := root.Module
+
+	// Provider dependencies
+	{
+		providers := make(moduledeps.Providers)
+
+		// The main way to declare a provider dependency is explicitly inside
+		// the "terraform" block, which allows declaring a requirement without
+		// also creating a configuration.
+		for fullName, constraints := range module.ProviderRequirements {
+			inst := moduledeps.ProviderInstance(fullName)
+
+			// The handling here is a bit fiddly because the moduledeps package
+			// was designed around the legacy (pre-0.12) configuration model
+			// and hasn't yet been revised to handle the new model. As a result,
+			// we need to do some translation here.
+			// FIXME: Eventually we should adjust the underlying model so we
+			// can also retain the source location of each constraint, for
+			// more informative output from the "terraform providers" command.
+			var rawConstraints version.Constraints
+			for _, constraint := range constraints {
+				rawConstraints = append(rawConstraints, constraint.Required...)
+			}
+			discoConstraints := discovery.NewConstraints(rawConstraints)
+
+			providers[inst] = moduledeps.ProviderDependency{
+				Constraints: discoConstraints,
+				Reason:      moduledeps.ProviderDependencyExplicit,
+			}
+		}
+
+		// Provider configurations can also include version constraints,
+		// allowing for more terse declaration in situations where both a
+		// configuration and a constraint are defined in the same module.
+		for fullName, pCfg := range module.ProviderConfigs {
+			inst := moduledeps.ProviderInstance(fullName)
+			discoConstraints := discovery.AllVersions
+			if pCfg.Version.Required != nil {
+				discoConstraints = discovery.NewConstraints(pCfg.Version.Required)
+			}
+			if existing, exists := providers[inst]; exists {
+				existing.Constraints = existing.Constraints.Append(discoConstraints)
+			} else {
+				providers[inst] = moduledeps.ProviderDependency{
+					Constraints: discoConstraints,
+					Reason:      moduledeps.ProviderDependencyExplicit,
+				}
+			}
+		}
+
+		// Each resource in the configuration creates an *implicit* provider
+		// dependency, though we'll only record it if there isn't already
+		// an explicit dependency on the same provider.
+		for _, rc := range module.ManagedResources {
+			fullName := rc.ProviderConfigKey()
+			inst := moduledeps.ProviderInstance(fullName)
+			if _, exists := providers[inst]; exists {
+				// Explicit dependency already present
+				continue
+			}
+
+			reason := moduledeps.ProviderDependencyImplicit
+			if _, inherited := inheritProviders[fullName]; inherited {
+				reason = moduledeps.ProviderDependencyInherited
+			}
+
+			providers[inst] = moduledeps.ProviderDependency{
+				Constraints: discovery.AllVersions,
+				Reason:      reason,
+			}
+		}
+		for _, rc := range module.DataResources {
+			fullName := rc.ProviderConfigKey()
+			inst := moduledeps.ProviderInstance(fullName)
+			if _, exists := providers[inst]; exists {
+				// Explicit dependency already present
+				continue
+			}
+
+			reason := moduledeps.ProviderDependencyImplicit
+			if _, inherited := inheritProviders[fullName]; inherited {
+				reason = moduledeps.ProviderDependencyInherited
+			}
+
+			providers[inst] = moduledeps.ProviderDependency{
+				Constraints: discovery.AllVersions,
+				Reason:      reason,
+			}
+		}
+
+		ret.Providers = providers
+	}
+
+	childInherit := make(map[string]*configs.Provider)
+	for k, v := range inheritProviders {
+		childInherit[k] = v
+	}
+	for k, v := range module.ProviderConfigs {
+		childInherit[k] = v
+	}
+	for _, c := range root.Children {
+		ret.Children = append(ret.Children, configTreeConfigDependencies(c, childInherit))
+	}
+
+	return ret
+}
+
+func configTreeMergeStateDependencies(root *moduledeps.Module, state *State) {
+	if state == nil {
+		return
+	}
+
+	findModule := func(path []string) *moduledeps.Module {
+		module := root
+		for _, name := range path[1:] { // skip initial "root"
+			var next *moduledeps.Module
+			for _, cm := range module.Children {
+				if cm.Name == name {
+					next = cm
+					break
+				}
+			}
+
+			if next == nil {
+				// If we didn't find a next node, we'll need to make one
+				next = &moduledeps.Module{
+					Name: name,
+				}
+				module.Children = append(module.Children, next)
+			}
+
+			module = next
+		}
+		return module
+	}
+
+	for _, ms := range state.Modules {
+		module := findModule(ms.Path)
+
+		for _, is := range ms.Resources {
+			fullName := config.ResourceProviderFullName(is.Type, is.Provider)
+			inst := moduledeps.ProviderInstance(fullName)
+			if _, exists := module.Providers[inst]; !exists {
+				if module.Providers == nil {
+					module.Providers = make(moduledeps.Providers)
+				}
+				module.Providers[inst] = moduledeps.ProviderDependency{
+					Constraints: discovery.AllVersions,
+					Reason:      moduledeps.ProviderDependencyFromState,
+				}
+			}
+		}
+	}
+}
 
 // ModuleTreeDependencies returns the dependencies of the tree of modules
 // described by the given configuration tree and state.
@@ -106,50 +301,9 @@ func moduleTreeConfigDependencies(root *module.Tree, inheritProviders map[string
 }
 
 func moduleTreeMergeStateDependencies(root *moduledeps.Module, state *State) {
-	if state == nil {
-		return
-	}
-
-	findModule := func(path []string) *moduledeps.Module {
-		module := root
-		for _, name := range path[1:] { // skip initial "root"
-			var next *moduledeps.Module
-			for _, cm := range module.Children {
-				if cm.Name == name {
-					next = cm
-					break
-				}
-			}
-
-			if next == nil {
-				// If we didn't find a next node, we'll need to make one
-				next = &moduledeps.Module{
-					Name: name,
-				}
-				module.Children = append(module.Children, next)
-			}
-
-			module = next
-		}
-		return module
-	}
-
-	for _, ms := range state.Modules {
-		module := findModule(ms.Path)
-
-		for _, is := range ms.Resources {
-			fullName := config.ResourceProviderFullName(is.Type, is.Provider)
-			inst := moduledeps.ProviderInstance(fullName)
-			if _, exists := module.Providers[inst]; !exists {
-				if module.Providers == nil {
-					module.Providers = make(moduledeps.Providers)
-				}
-				module.Providers[inst] = moduledeps.ProviderDependency{
-					Constraints: discovery.AllVersions,
-					Reason:      moduledeps.ProviderDependencyFromState,
-				}
-			}
-		}
-	}
-
+	// This is really just the same logic as configTreeMergeStateDependencies
+	// but we retain this old name just to keep the symmetry until we've
+	// removed all of these "moduleTree..." versions that use the legacy
+	// configuration structs.
+	configTreeMergeStateDependencies(root, state)
 }
