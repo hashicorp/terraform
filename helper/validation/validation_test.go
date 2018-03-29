dfa623250afc47498cccdeaf23a3198e9d052be7
Adit Sarfaty <asarfaty@vmware.com>
2018-03-29 19:41:59 +0300

    helper/validation: IP address and IP address range validation helpers

--- helper/validation/validation_test.go
+++ helper/validation/validation_test.go
@@ -144,6 +144,49 @@ func TestValidationRegexp(t *testing.T) {
 	})
 }
 
+func TestValidationSingleIP(t *testing.T) {
+	runTestCases(t, []testCase{
+		{
+			val: "172.10.10.10",
+			f:   SingleIP(),
+		},
+		{
+			val:         "1.1.1",
+			f:           SingleIP(),
+			expectedErr: regexp.MustCompile(regexp.QuoteMeta("expected test_property to contain a valid IP, got:")),
+		},
+		{
+			val:         "1.1.1.0/20",
+			f:           SingleIP(),
+			expectedErr: regexp.MustCompile(regexp.QuoteMeta("expected test_property to contain a valid IP, got:")),
+		},
+		{
+			val:         "256.1.1.1",
+			f:           SingleIP(),
+			expectedErr: regexp.MustCompile(regexp.QuoteMeta("expected test_property to contain a valid IP, got:")),
+		},
+	})
+}
+
+func TestValidationIPRange(t *testing.T) {
+	runTestCases(t, []testCase{
+		{
+			val: "172.10.10.10-172.10.10.12",
+			f:   IPRange(),
+		},
+		{
+			val:         "172.10.10.20",
+			f:           IPRange(),
+			expectedErr: regexp.MustCompile(regexp.QuoteMeta("expected test_property to contain a valid IP range, got:")),
+		},
+		{
+			val:         "172.10.10.20-172.10.10.12",
+			f:           IPRange(),
+			expectedErr: regexp.MustCompile(regexp.QuoteMeta("expected test_property to contain a valid IP range, got:")),
+		},
+	})
+}
+
 func TestValidateRFC3339TimeString(t *testing.T) {
 	runTestCases(t, []testCase{
 		{
