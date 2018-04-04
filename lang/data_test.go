e1c134f67ae701aec55efbe22dc7080c7ae2082e
Martin Atkins <mart@degeneration.co.uk>
2018-04-03 18:11:28 -0700

    lang: new package for the runtime parts of the config language
    Whereas package "configs" deals with the static structure of the
    configuration language, this new package "lang" deals with the dynamic
    aspects such as expression evaluation.
    
    So far this mainly consists of populating a hcl.EvalContext that contains
    the values necessary to evaluate a block or an expression. There is also
    special handling here for dynamic block generation using the HCL
    "dynblock" extension, which is exposed in the public interface (rather
    than hiding it as an implementation detail of EvalBlock) so that the
    caller can then extract proper source locations for any result values
    using the expanded body.
    
    This also includes the beginnings of a replacement for the function table
    handling that currently lives in the old "config" package, but most of
    the functions are not yet ported and so this will expand in subsequent
    commits.

---
+++ lang/data_test.go
@@ -0,0 +1,58 @@
+package lang
+
+import (
+	"github.com/hashicorp/terraform/addrs"
+	"github.com/hashicorp/terraform/tfdiags"
+	"github.com/zclconf/go-cty/cty"
+)
+
+type dataForTests struct {
+	CountAttrs        map[string]cty.Value
+	ResourceInstances map[string]cty.Value
+	LocalValues       map[string]cty.Value
+	Modules           map[string]cty.Value
+	PathAttrs         map[string]cty.Value
+	Self              cty.Value
+	TerraformAttrs    map[string]cty.Value
+	InputVariables    map[string]cty.Value
+}
+
+var _ Data = &dataForTests{}
+
+func (d *dataForTests) GetCountAttr(addr addrs.CountAttr, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.CountAttrs[addr.Name], nil
+}
+
+func (d *dataForTests) GetResourceInstance(addr addrs.ResourceInstance, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.ResourceInstances[addr.String()], nil
+}
+
+func (d *dataForTests) GetInputVariable(addr addrs.InputVariable, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.InputVariables[addr.Name], nil
+}
+
+func (d *dataForTests) GetLocalValue(addr addrs.LocalValue, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.LocalValues[addr.Name], nil
+}
+
+func (d *dataForTests) GetModuleInstance(addr addrs.ModuleCallInstance, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.Modules[addr.String()], nil
+}
+
+func (d *dataForTests) GetModuleInstanceOutput(addr addrs.ModuleCallOutput, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	// This will panic if the module object does not have the requested attribute
+	obj := d.Modules[addr.Call.String()]
+	return obj.GetAttr(addr.Name), nil
+}
+
+func (d *dataForTests) GetPathAttr(addr addrs.PathAttr, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.PathAttrs[addr.Name], nil
+}
+
+func (d *dataForTests) GetSelf(rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.Self, nil
+}
+
+func (d *dataForTests) GetTerraformAttr(addr addrs.TerraformAttr, rng tfdiags.SourceRange) (cty.Value, tfdiags.Diagnostics) {
+	return d.TerraformAttrs[addr.Name], nil
+}
