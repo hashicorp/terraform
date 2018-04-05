6b99831f11796401fc9b8063eb6d4b331f5b7b17
tchary <t.chary@criteo.com>
2018-04-05 14:43:36 +0200

    copy chef provisioner

--- command/internal_plugin_list.go
+++ command/internal_plugin_list.go
@@ -5,6 +5,7 @@ package command
 
 import (
 	chefprovisioner "github.com/hashicorp/terraform/builtin/provisioners/chef"
+	chefzeroprovisioner "github.com/hashicorp/terraform/builtin/provisioners/chef-zero"
 	fileprovisioner "github.com/hashicorp/terraform/builtin/provisioners/file"
 	habitatprovisioner "github.com/hashicorp/terraform/builtin/provisioners/habitat"
 	localexecprovisioner "github.com/hashicorp/terraform/builtin/provisioners/local-exec"
@@ -18,6 +19,7 @@ var InternalProviders = map[string]plugin.ProviderFunc{}
 
 var InternalProvisioners = map[string]plugin.ProvisionerFunc{
 	"chef":            chefprovisioner.Provisioner,
+	"chef-zero":       chefzeroprovisioner.Provisioner,
 	"file":            fileprovisioner.Provisioner,
 	"habitat":         habitatprovisioner.Provisioner,
 	"local-exec":      localexecprovisioner.Provisioner,
