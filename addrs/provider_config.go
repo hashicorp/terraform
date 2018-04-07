7b13074c1226a1c091d121ca13c4173261ebd7d3
Martin Atkins <mart@degeneration.co.uk>
2018-04-06 18:45:16 -0700

    addrs: AbsProviderConfig type
    Fitting with the usual naming scheme, this is the combination of a
    module instance address and a provider config.

--- addrs/provider_config.go
+++ addrs/provider_config.go
@@ -1,5 +1,9 @@
 package addrs
 
+import (
+	"fmt"
+)
+
 // ProviderConfig is the address of a provider configuration.
 type ProviderConfig struct {
 	Type string
@@ -7,4 +11,32 @@ type ProviderConfig struct {
 	// If not empty, Alias identifies which non-default (aliased) provider
 	// configuration this address refers to.
 	Alias string
+}
+
+// Absolute returns an AbsProviderConfig from the receiver and the given module
+// instance address.
+func (pc ProviderConfig) Absolute(module ModuleInstance) AbsProviderConfig {
+	return AbsProviderConfig{
+		Module:         module,
+		ProviderConfig: pc,
+	}
+}
+
+func (pc ProviderConfig) String() string {
+	if pc.Alias != "" {
+		return fmt.Sprintf("provider.%s.%s", pc.Type, pc.Alias)
+	}
+
+	return "provider." + pc.Type
+}
+
+// AbsProviderConfig is the absolute address of a provider configuration
+// within a particular module instance.
+type AbsProviderConfig struct {
+	Module         ModuleInstance
+	ProviderConfig ProviderConfig
+}
+
+func (pc AbsProviderConfig) String() string {
+	return fmt.Sprintf("%s.%s", pc.Module.String(), pc.ProviderConfig.String())
 }
