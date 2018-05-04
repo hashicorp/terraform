4b5868f6539a86f6fb36cb16acda50d2e4fc6887
Martin Atkins <mart@degeneration.co.uk>
2018-05-03 17:24:49 -0700

    core: BuiltinEvalContext constructs evaluationStateData itself
    Previously our evaluationStateData object was constructed inside
    Evaluator.Scope, but this was awkward because all of the fields inside it
    need to be populated from BuiltinEvalContext fields, and so the signature
    of Evaluator.Scope kept growing new arguments over time.
    
    Instead, we reassign the responsibilities here so that Evaluator.Scope
    takes an already-constructed lang.Data, and then teach BuiltinEvalContext
    to build this object itself from its own internal values.

--- terraform/evaluate.go
+++ terraform/evaluate.go
@@ -42,12 +42,9 @@ type Evaluator struct {
 // If the "self" argument is nil then the "self" object is not available
 // in evaluated expressions. Otherwise, it behaves as an alias for the given
 // address.
-func (e *Evaluator) Scope(modulePath addrs.ModuleInstance, self addrs.Referenceable) *lang.Scope {
+func (e *Evaluator) Scope(data lang.Data, self addrs.Referenceable) *lang.Scope {
 	return &lang.Scope{
-		Data: &evaluationStateData{
-			Evaluator:  e,
-			ModulePath: modulePath,
-		},
+		Data:     data,
 		SelfAddr: self,
 		PureOnly: e.Operation != walkApply && e.Operation != walkDestroy,
 		BaseDir:  ".", // Always current working directory for now.
