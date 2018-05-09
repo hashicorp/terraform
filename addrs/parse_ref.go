77a5946c50f7e293243615edf404d1ea2a9ea275
Martin Atkins <mart@degeneration.co.uk>
2018-05-09 16:04:45 -0700

    addrs: helper wrappers for parsing addresses in strings
    Our "Parse..." functions all take hcl.Traversal objects rather than strings,
    assuming that in most cases we've already parsed a traversal out of some
    larger construct (usually a config file) before interpreting it as an
    address.
    
    However, there are some situations -- particularly tests -- where being
    able to easily parse a string directly is helpful. These new "Parse...Str"
    functions all wrap the function of the same name with no "Str" suffix and
    first parse the string with the HCL native syntax traversal parser.
    
    As noted in the function doc comments, this should not be used in "real"
    code except in exceptional circumstances, since it creates addresses and
    diagnostics that do not have useful source location information for
    reporting diagnostics.

--- addrs/parse_ref.go
+++ addrs/parse_ref.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/hashicorp/hcl2/hcl/hclsyntax"
 	"github.com/hashicorp/terraform/tfdiags"
 )
 
@@ -171,6 +172,35 @@ func ParseRef(traversal hcl.Traversal) (*Reference, tfdiags.Diagnostics) {
 	default:
 		return parseResourceRef(ManagedResourceMode, rootRange, traversal)
 	}
+}
+
+// ParseRefStr is a helper wrapper around ParseRef that takes a string
+// and parses it with the HCL native syntax traversal parser before
+// interpreting it.
+//
+// This should be used only in specialized situations since it will cause the
+// created references to not have any meaningful source location information.
+// If a reference string is coming from a source that should be identified in
+// error messages then the caller should instead parse it directly using a
+// suitable function from the HCL API and pass the traversal itself to
+// ParseRef.
+//
+// Error diagnostics are returned if either the parsing fails or the analysis
+// of the traversal fails. There is no way for the caller to distinguish the
+// two kinds of diagnostics programmatically. If error diagnostics are returned
+// the returned reference may be nil or incomplete.
+func ParseRefStr(str string) (*Reference, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+
+	traversal, parseDiags := hclsyntax.ParseTraversalAbs([]byte(str), "", hcl.Pos{Line: 1, Column: 1})
+	diags = diags.Append(parseDiags)
+	if parseDiags.HasErrors() {
+		return nil, diags
+	}
+
+	ref, targetDiags := ParseRef(traversal)
+	diags = diags.Append(targetDiags)
+	return ref, diags
 }
 
 func parseResourceRef(mode ResourceMode, startRange hcl.Range, traversal hcl.Traversal) (*Reference, tfdiags.Diagnostics) {
