5e8938de98b99574d240e401c3352c21efb1db54
Martin Atkins <mart@degeneration.co.uk>
2018-05-10 16:28:24 -0700

    core: Remove GraphWalkerPanicwrap, etc
    These are some remnants of the shadow graph functionality that was added
    to support the graph builder changes in v0.8, but that has since been
    removed and so there are no remaining callers for these types and
    functions.

--- terraform/graph_walk.go
+++ terraform/graph_walk.go
@@ -17,36 +17,6 @@ type GraphWalker interface {
 	ExitEvalTree(dag.Vertex, interface{}, error) tfdiags.Diagnostics
 }
 
-// GrpahWalkerPanicwrapper can be optionally implemented to catch panics
-// that occur while walking the graph. This is not generally recommended
-// since panics should crash Terraform and result in a bug report. However,
-// this is particularly useful for situations like the shadow graph where
-// you don't ever want to cause a panic.
-type GraphWalkerPanicwrapper interface {
-	GraphWalker
-
-	// Panic is called when a panic occurs. This will halt the panic from
-	// propogating so if the walker wants it to crash still it should panic
-	// again. This is called from within a defer so runtime/debug.Stack can
-	// be used to get the stack trace of the panic.
-	Panic(dag.Vertex, interface{})
-}
-
-// GraphWalkerPanicwrap wraps an existing Graphwalker to wrap and swallow
-// the panics. This doesn't lose the panics since the panics are still
-// returned as errors as part of a graph walk.
-func GraphWalkerPanicwrap(w GraphWalker) GraphWalkerPanicwrapper {
-	return &graphWalkerPanicwrapper{
-		GraphWalker: w,
-	}
-}
-
-type graphWalkerPanicwrapper struct {
-	GraphWalker
-}
-
-func (graphWalkerPanicwrapper) Panic(dag.Vertex, interface{}) {}
-
 // NullGraphWalker is a GraphWalker implementation that does nothing.
 // This can be embedded within other GraphWalker implementations for easily
 // implementing all the required functions.
