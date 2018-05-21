d8cfa978f55febe456eda416653429902d4c2b53
Martin Atkins <mart@degeneration.co.uk>
2018-05-21 11:32:43 -0700

    core: Misc. updates to the "plan" context tests
    Mostly this is about updating ctx.Plan callers to expect diags instead of
    err, but also includes a few light updates to test fixtures, and a fix to
    testModuleInline.

--- terraform/test-fixtures/plan-cdb-depends-datasource/main.tf
+++ terraform/test-fixtures/plan-cdb-depends-datasource/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "foo" {
   count   = 2
   num     = "2"
-  compute = "${element(data.aws_vpc.bar.*.id, count.index)}"
+  compute = data.aws_vpc.bar[count.index].id
 
   lifecycle {
     create_before_destroy = true
