639000e6a48155db78a6b296ac5c3d48839fb045
Martin Atkins <mart@degeneration.co.uk>
2018-05-23 17:38:51 -0700

    govendor fetch github.com/zclconf/go-cty/cty/...
    This includes upstream fixes, and in particular a converter for going
    between map types with different element types.

--- vendor/github.com/zclconf/go-cty/cty/convert/conversion_collection.go
+++ vendor/github.com/zclconf/go-cty/cty/convert/conversion_collection.go
@@ -84,6 +84,50 @@ func conversionCollectionToSet(ety cty.Type, conv conversion) conversion {
 	}
 }
 
+// conversionCollectionToMap returns a conversion that will apply the given
+// conversion to all of the elements of a collection (something that supports
+// ForEachElement and LengthInt) and then returns the result as a map.
+//
+// "conv" can be nil if the elements are expected to already be of the
+// correct type and just need to be re-wrapped into a map.
+func conversionCollectionToMap(ety cty.Type, conv conversion) conversion {
+	return func(val cty.Value, path cty.Path) (cty.Value, error) {
+		elems := make(map[string]cty.Value, 0)
+		path = append(path, nil)
+		it := val.ElementIterator()
+		for it.Next() {
+			key, val := it.Element()
+			var err error
+
+			path[len(path)-1] = cty.IndexStep{
+				Key: key,
+			}
+
+			keyStr, err := Convert(key, cty.String)
+			if err != nil {
+				// Should never happen, because keys can only be numbers or
+				// strings and both can convert to string.
+				return cty.DynamicVal, path.NewErrorf("cannot convert key type %s to string for map", key.Type().FriendlyName())
+			}
+
+			if conv != nil {
+				val, err = conv(val, path)
+				if err != nil {
+					return cty.NilVal, err
+				}
+			}
+
+			elems[keyStr.AsString()] = val
+		}
+
+		if len(elems) == 0 {
+			return cty.MapValEmpty(ety), nil
+		}
+
+		return cty.MapVal(elems), nil
+	}
+}
+
 // conversionTupleToList returns a conversion that will take a value of the
 // given tuple type and return a list of the given element type.
 //
