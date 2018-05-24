32987bd8cbf48ba91fa92edad978c5994b7e0b86
Martin Atkins <mart@degeneration.co.uk>
2018-05-24 12:14:06 -0700

    govendor fetch github.com/hashicorp/hcl2/...
    This includes an upstream fix to the hcldec.Variables function that fixes
    its behavior when dealing with specs that contain DefaultSpec, and other
    similar wrapper specs.

--- vendor/github.com/hashicorp/hcl2/hcldec/variables.go
+++ vendor/github.com/hashicorp/hcl2/hcldec/variables.go
@@ -15,20 +15,22 @@ import (
 // be incomplete, but that's assumed to be okay because the eventual call
 // to Decode will produce error diagnostics anyway.
 func Variables(body hcl.Body, spec Spec) []hcl.Traversal {
-	schema := ImpliedSchema(spec)
-
-	content, _, _ := body.PartialContent(schema)
-
 	var vars []hcl.Traversal
+	schema := ImpliedSchema(spec)
+	content, _, _ := body.PartialContent(schema)
 
 	if vs, ok := spec.(specNeedingVariables); ok {
 		vars = append(vars, vs.variablesNeeded(content)...)
 	}
-	spec.visitSameBodyChildren(func(s Spec) {
+
+	var visitFn visitFunc
+	visitFn = func(s Spec) {
 		if vs, ok := s.(specNeedingVariables); ok {
 			vars = append(vars, vs.variablesNeeded(content)...)
 		}
-	})
+		s.visitSameBodyChildren(visitFn)
+	}
+	spec.visitSameBodyChildren(visitFn)
 
 	return vars
 }
