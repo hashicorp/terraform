4a3bf51cb281611ccae1478c1fb2b3b9e00ab30c
James Bardin <j.bardin@gmail.com>
2018-05-25 19:27:11 -0400

    connect non-resources to providers they reference
    Any non-resource (outputs, variables, locals) that references a resource
    type must also be connected to that resources provider. This is required
    during apply, because the graph built from the diff may not include the
    referenced resources because they are being evaluated from the state.
    
    If the provider isn't present already, add a NodeEvalableProvider to
    fetch the provider schema.
    
    The provider transformers now need to happen after the outputs, locals,
    and variables are transformed.

--- terraform/test-fixtures/apply-targeted-module-unrelated-outputs/child2/main.tf
+++ terraform/test-fixtures/apply-targeted-module-unrelated-outputs/child2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "foo" {
 }
 
 output "instance_id" {
-  # Even though we're targeting just the resource a bove, this should still
+  # Even though we're targeting just the resource above, this should still
   # be populated because outputs are implicitly targeted when their
   # dependencies are
   value = "${aws_instance.foo.id}"
