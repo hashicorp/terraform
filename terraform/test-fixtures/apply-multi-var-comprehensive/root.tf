c25d7a0be7b78cced8ff2800c36387e46adaaae2
Martin Atkins <mart@degeneration.co.uk>
2018-05-25 13:33:04 -0700

    core: update various context apply tests for schema/fixtures
    Most of these changes are just adding schema to describe the expectations
    of the existing test fixtures. However, some of them require the fixtures
    themselves to be changed due to changing assumptions in the language.

--- terraform/test-fixtures/apply-multi-var-comprehensive/root.tf
+++ terraform/test-fixtures/apply-multi-var-comprehensive/root.tf
@@ -12,29 +12,29 @@ resource "test_thing" "multi_count_var" {
   count = "${var.num}"
 
   # Can pluck a single item out of a multi-var
-  source_id = "${test_thing.source.*.id[count.index]}"
+  source_id   = "${test_thing.source.*.id[count.index]}"
   source_name = "${test_thing.source.*.name[count.index]}"
 }
 
 resource "test_thing" "multi_count_derived" {
   # Can use the source to get the count
-  count = "${test_thing.source.count}"
+  count = "${length(test_thing.source)}"
 
-  source_id = "${test_thing.source.*.id[count.index]}"
+  source_id   = "${test_thing.source.*.id[count.index]}"
   source_name = "${test_thing.source.*.name[count.index]}"
 }
 
 resource "test_thing" "whole_splat" {
   # Can "splat" the ids directly into an attribute of type list.
-  source_ids = "${test_thing.source.*.id}"
+  source_ids   = "${test_thing.source.*.id}"
   source_names = "${test_thing.source.*.name}"
 
   # Accessing through a function should work.
-  source_ids_from_func = "${split(" ", join(" ", test_thing.source.*.id))}"
+  source_ids_from_func   = "${split(" ", join(" ", test_thing.source.*.id))}"
   source_names_from_func = "${split(" ", join(" ", test_thing.source.*.name))}"
 
   # A common pattern of selecting with a default.
-  first_source_id = "${element(concat(test_thing.source.*.id, list("default")), 0)}"
+  first_source_id   = "${element(concat(test_thing.source.*.id, list("default")), 0)}"
   first_source_name = "${element(concat(test_thing.source.*.name, list("default")), 0)}"
 
   # Legacy form: Prior to Terraform having comprehensive list support,
@@ -44,7 +44,7 @@ resource "test_thing" "whole_splat" {
   #
   # This should result in exactly the same result as the above, even
   # though it looks like it would result in a list of lists.
-  source_ids_wrapped = ["${test_thing.source.*.id}"]
+  source_ids_wrapped   = ["${test_thing.source.*.id}"]
   source_names_wrapped = ["${test_thing.source.*.name}"]
 
 }
@@ -52,8 +52,8 @@ resource "test_thing" "whole_splat" {
 module "child" {
   source = "./child"
 
-  count = "${var.num}"
-  source_ids = "${test_thing.source.*.id}"
+  num          = "${var.num}"
+  source_ids   = "${test_thing.source.*.id}"
   source_names = "${test_thing.source.*.name}"
 }
 
