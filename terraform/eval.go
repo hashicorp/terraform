5ffdb0005dc934fb3dc4dc44a54beb603cd132d3
Martin Atkins <mart@degeneration.co.uk>
2018-05-29 11:47:49 -0700

    core: EvalSequence must continue when only warnings are returned
    To avoid a massively-disruptive change to how EvalNode works, we're now
    "smuggling" warnings through the error return value for these, but this
    depends on all of the Eval machinery correctly handling this special case
    and continuing evaluation when only warnings are returned.
    
    Previous changes missed EvalSequence as a place where execution halts on
    error. Now it will accumulate diagnostics itself, aborting if any of
    them are error diagnostics, and then wrap its own result up in an error
    to be returned by the main Eval function, which already treats non-fatal
    errors as a special case, though now produces an explicit log message
    about that situation to make it easier to spot in trace logs.
    
    This also includes a more detailed warning message for the warning about
    provider input being disabled. While this warning should be removed before
    we release anyway, having this additional detail is helpful in debugging
    tests where it's being returned.

--- terraform/eval.go
+++ terraform/eval.go
@@ -2,6 +2,8 @@ package terraform
 
 import (
 	"log"
+
+	"github.com/hashicorp/terraform/tfdiags"
 )
 
 // EvalNode is the interface that must be implemented by graph nodes to
@@ -54,9 +56,12 @@ func EvalRaw(n EvalNode, ctx EvalContext) (interface{}, error) {
 	log.Printf("[TRACE] %s: eval: %T", path, n)
 	output, err := n.Eval(ctx)
 	if err != nil {
-		if _, ok := err.(EvalEarlyExitError); ok {
-			log.Printf("[TRACE] %s: eval: %T, err: %s", path, n, err)
-		} else {
+		switch err.(type) {
+		case EvalEarlyExitError:
+			log.Printf("[TRACE] %s: eval: %T, early exit err: %s", path, n, err)
+		case tfdiags.NonFatalError:
+			log.Printf("[WARN] %s: eval: %T, non-fatal err: %s", path, n, err)
+		default:
 			log.Printf("[ERROR] %s: eval: %T, err: %s", path, n, err)
 		}
 	}
