b42d12f9ff20106815c57835ae7bb6accae1988a
Martin Atkins <mart@degeneration.co.uk>
2018-05-30 16:07:52 -0700

    core: Update TestContext2Apply_multiVarComprehensive for new assumptions
    This comprehensive test was covering a few different behaviors that are
    intentionally different for v0.12:
    
    - Applying the splat operator to a list of resource instances that haven't
      been created yet produces a list of unknown values rather than a single
      unknown list as before. This is important because it allows that list
      to be passed into length().
    
    - Wrapping a splat expression in another round of brackets now produces
      a list of lists, whereas before we had a special case (for compatibility
      with prior to v0.10) that would flatten this away in the schema layer.

--- terraform/test-fixtures/apply-multi-var-comprehensive/root.tf
+++ terraform/test-fixtures/apply-multi-var-comprehensive/root.tf
@@ -2,48 +2,48 @@ variable "num" {
 }
 
 resource "test_thing" "source" {
-  count = "${var.num}"
+  count = var.num
 
   # The diffFunc in the test exports "name" here too, which we can use
   # to test values that are known during plan.
 }
 
 resource "test_thing" "multi_count_var" {
-  count = "${var.num}"
+  count = var.num
 
   # Can pluck a single item out of a multi-var
-  source_id   = "${test_thing.source.*.id[count.index]}"
-  source_name = "${test_thing.source.*.name[count.index]}"
+  source_id   = test_thing.source.*.id[count.index]
+  source_name = test_thing.source.*.name[count.index]
 }
 
 resource "test_thing" "multi_count_derived" {
   # Can use the source to get the count
-  count = "${length(test_thing.source)}"
+  count = length(test_thing.source)
 
-  source_id   = "${test_thing.source.*.id[count.index]}"
-  source_name = "${test_thing.source.*.name[count.index]}"
+  source_id   = test_thing.source.*.id[count.index]
+  source_name = test_thing.source.*.name[count.index]
 }
 
 resource "test_thing" "whole_splat" {
   # Can "splat" the ids directly into an attribute of type list.
-  source_ids   = "${test_thing.source.*.id}"
-  source_names = "${test_thing.source.*.name}"
+  source_ids   = test_thing.source.*.id
+  source_names = test_thing.source.*.name
 
   # Accessing through a function should work.
-  source_ids_from_func   = "${split(" ", join(" ", test_thing.source.*.id))}"
-  source_names_from_func = "${split(" ", join(" ", test_thing.source.*.name))}"
+  source_ids_from_func   = split(" ", join(" ", test_thing.source.*.id))
+  source_names_from_func = split(" ", join(" ", test_thing.source.*.name))
 
   # A common pattern of selecting with a default.
-  first_source_id   = "${element(concat(test_thing.source.*.id, list("default")), 0)}"
-  first_source_name = "${element(concat(test_thing.source.*.name, list("default")), 0)}"
+  first_source_id   = element(concat(test_thing.source.*.id, ["default"]), 0)
+  first_source_name = element(concat(test_thing.source.*.name, ["default"]), 0)
 
-  # Legacy form: Prior to Terraform having comprehensive list support,
-  # splats were treated as a special case and required to be presented
-  # in a wrapping list. This is no longer the suggested form, but we
-  # need it to keep working for compatibility.
-  #
-  # This should result in exactly the same result as the above, even
-  # though it looks like it would result in a list of lists.
+  # Prior to v0.12 we were handling lists containing list interpolations as
+  # a special case, flattening the result, for compatibility with behavior
+  # prior to v0.10. This deprecated handling is now removed, and so these
+  # each produce a list of lists. We're still using the interpolation syntax
+  # here, rather than the splat expression directly, to properly mimic how
+  # this would've looked prior to v0.12 to be explicit about what the new
+  # behavior is for this old syntax.
   source_ids_wrapped   = ["${test_thing.source.*.id}"]
   source_names_wrapped = ["${test_thing.source.*.name}"]
 
@@ -52,15 +52,15 @@ resource "test_thing" "whole_splat" {
 module "child" {
   source = "./child"
 
-  num          = "${var.num}"
-  source_ids   = "${test_thing.source.*.id}"
-  source_names = "${test_thing.source.*.name}"
+  num          = var.num
+  source_ids   = test_thing.source.*.id
+  source_names = test_thing.source.*.name
 }
 
 output "source_ids" {
-  value = "${test_thing.source.*.id}"
+  value = test_thing.source.*.id
 }
 
 output "source_names" {
-  value = "${test_thing.source.*.name}"
+  value = test_thing.source.*.name
 }
