c5a53561e54fd154eb23e14339682069040f1ffb
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2018-06-26 08:54:40 -0700

    builtin/providers: implement terraform remote state datasource as providers.Interface

--- builtin/providers/terraform/data_source_state.go
+++ builtin/providers/terraform/data_source_state.go
@@ -3,68 +3,21 @@ package terraform
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/hashicorp/terraform/backend"
 	backendinit "github.com/hashicorp/terraform/backend/init"
 	"github.com/hashicorp/terraform/config/hcl2shim"
-	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/tfdiags"
+	"github.com/zclconf/go-cty/cty"
 )
 
-func dataSourceRemoteState() *schema.Resource {
-	return &schema.Resource{
-		Read: dataSourceRemoteStateRead,
+func dataSourceRemoteStateRead(d *cty.Value) (cty.Value, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
 
-		Schema: map[string]*schema.Schema{
-			"backend": {
-				Type:     schema.TypeString,
-				Required: true,
-				ValidateFunc: func(v interface{}, k string) (ws []string, errors []error) {
-					if vStr, ok := v.(string); ok && vStr == "_local" {
-						ws = append(ws, "Use of the %q backend is now officially "+
-							"supported as %q. Please update your configuration to ensure "+
-							"compatibility with future versions of Terraform.",
-							"_local", "local")
-					}
+	newState := make(map[string]cty.Value)
+	newState["backend"] = d.GetAttr("backend")
 
-					return
-				},
-			},
-
-			"config": {
-				Type:     schema.TypeMap,
-				Optional: true,
-			},
-
-			"defaults": {
-				Type:     schema.TypeMap,
-				Optional: true,
-			},
-
-			"environment": {
-				Type:       schema.TypeString,
-				Optional:   true,
-				Default:    backend.DefaultStateName,
-				Deprecated: "Terraform environments are now called workspaces. Please use the workspace key instead.",
-			},
-
-			"workspace": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  backend.DefaultStateName,
-			},
-
-			"__has_dynamic_attributes": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-		},
-	}
-}
-
-func dataSourceRemoteStateRead(d *schema.ResourceData, meta interface{}) error {
-	backendType := d.Get("backend").(string)
+	backendType := d.GetAttr("backend").AsString()
 
 	// Don't break people using the old _local syntax - but note warning above
 	if backendType == "_local" {
@@ -76,67 +29,83 @@ func dataSourceRemoteStateRead(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] Initializing remote state backend: %s", backendType)
 	f := backendinit.Backend(backendType)
 	if f == nil {
-		return fmt.Errorf("Unknown backend type: %s", backendType)
+		return cty.NilVal, diags.Append(tfdiags.AttributeValue(
+			tfdiags.Error,
+			fmt.Sprintf("Unknown backend type: %s", backendType),
+			"",
+			cty.Path(nil).GetAttr("backend"),
+		))
 	}
 	b := f()
 
-	schema := b.ConfigSchema()
-	rawConfig := d.Get("config")
-	configVal := hcl2shim.HCL2ValueFromConfigValue(rawConfig)
+	config := d.GetAttr("config")
+	newState["config"] = config
 
+	schema := b.ConfigSchema()
 	// Try to coerce the provided value into the desired configuration type.
-	configVal, err := schema.CoerceValue(configVal)
+	configVal, err := schema.CoerceValue(config)
 	if err != nil {
-		return fmt.Errorf("invalid %s backend configuration: %s", backendType, tfdiags.FormatError(err))
+		return cty.NilVal, diags.Append(tfdiags.AttributeValue(
+			tfdiags.Error,
+			fmt.Sprintf("invalid %s backend configuration: %s", backendType, tfdiags.FormatError(err)),
+			"",
+			cty.Path(nil).GetAttr("backend"),
+		))
 	}
+
 	validateDiags := b.ValidateConfig(configVal)
 	if validateDiags.HasErrors() {
-		return validateDiags.Err()
+		return cty.NilVal, diags.Append(validateDiags.Err())
 	}
 	configureDiags := b.Configure(configVal)
 	if configureDiags.HasErrors() {
-		return configureDiags.Err()
+		return cty.NilVal, diags.Append(configureDiags.Err())
 	}
 
-	// environment is deprecated in favour of workspace.
-	// If both keys are set workspace should win.
-	name := d.Get("environment").(string)
-	if ws, ok := d.GetOk("workspace"); ok && ws != backend.DefaultStateName {
-		name = ws.(string)
+	var name string
+	if d.Type().HasAttribute("workspace") {
+		newState["workspace"] = d.GetAttr("workspace")
+		ws := d.GetAttr("workspace").AsString()
+		if ws != backend.DefaultStateName {
+			name = ws
+		}
 	}
 
 	state, err := b.State(name)
 	if err != nil {
-		return fmt.Errorf("error loading the remote state: %s", err)
+		return cty.NilVal, diags.Append(tfdiags.AttributeValue(
+			tfdiags.Error,
+			fmt.Sprintf("error loading the remote state: %s", err),
+			"",
+			cty.Path(nil).GetAttr("backend"),
+		))
 	}
 	if err := state.RefreshState(); err != nil {
-		return err
+		return cty.NilVal, diags.Append(err)
 	}
-	d.SetId(time.Now().UTC().String())
 
-	outputMap := make(map[string]interface{})
+	outputs := make(map[string]cty.Value)
 
-	defaults := d.Get("defaults").(map[string]interface{})
-	for key, val := range defaults {
-		outputMap[key] = val
+	if d.Type().HasAttribute("defaults") {
+		defaults := d.GetAttr("defaults")
+		newState["defaults"] = d.GetAttr("defaults")
+		it := defaults.ElementIterator()
+		for it.Next() {
+			k, v := it.Element()
+			outputs[k.AsString()] = v
+		}
 	}
 
 	remoteState := state.State()
 	if remoteState.Empty() {
 		log.Println("[DEBUG] empty remote state")
 	} else {
-		for key, val := range remoteState.RootModule().Outputs {
-			if val.Value != nil {
-				outputMap[key] = val.Value
-			}
+		for k, os := range remoteState.RootModule().Outputs {
+			outputs[k] = hcl2shim.HCL2ValueFromConfigValue(os.Value)
 		}
 	}
 
-	mappedOutputs := remoteStateFlatten(outputMap)
+	newState["outputs"] = cty.ObjectVal(outputs)
 
-	for key, val := range mappedOutputs {
-		d.UnsafeSetFieldRaw(key, val)
-	}
-
-	return nil
+	return cty.ObjectVal(newState), diags
 }
--- builtin/providers/terraform/provider.go
+++ builtin/providers/terraform/provider.go
@@ -1,21 +1,151 @@
 package terraform
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+	"fmt"
+
+	"github.com/hashicorp/terraform/config/configschema"
+	"github.com/hashicorp/terraform/providers"
+	"github.com/zclconf/go-cty/cty"
 )
 
-// Provider returns a terraform.ResourceProvider.
-func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{
-			"terraform_remote_state": schema.DataSourceResourceShim(
-				"terraform_remote_state",
-				dataSourceRemoteState(),
-			),
+// Provider is an implementation of providers.Interface
+type Provider struct {
+	// Provider is the schema for the provider itself.
+	Schema providers.Schema
+
+	// DataSources maps the data source name to that data source's schema.
+	DataSources map[string]providers.Schema
+}
+
+// NewProvider returns a new terraform provider
+func NewProvider() *Provider {
+	return &Provider{}
+}
+
+// GetSchema returns the complete schema for the provider.
+func (p *Provider) GetSchema() providers.GetSchemaResponse {
+	return providers.GetSchemaResponse{
+		Provider: providers.Schema{
+			Version: 1,
 		},
-		DataSourcesMap: map[string]*schema.Resource{
-			"terraform_remote_state": dataSourceRemoteState(),
+		DataSources: map[string]providers.Schema{
+			"terraform_remote_state": {
+				Block: &configschema.Block{
+					Attributes: map[string]*configschema.Attribute{
+						"backend": {
+							Type:     cty.String,
+							Required: true,
+						},
+						"config": {
+							Type:     cty.DynamicPseudoType,
+							Optional: true,
+						},
+						"defaults": {
+							Type:     cty.DynamicPseudoType,
+							Optional: true,
+						},
+						"outputs": {
+							Type:     cty.DynamicPseudoType,
+							Computed: true,
+						},
+						"workspace": {
+							Type:     cty.String,
+							Optional: true,
+						},
+					},
+				},
+			},
 		},
 	}
+}
+
+// ValidateProviderConfig is used to validate the configuration values.
+func (p *Provider) ValidateProviderConfig(providers.ValidateProviderConfigRequest) providers.ValidateProviderConfigResponse {
+	// At this moment there is nothing to configure for the terraform provider,
+	// so we will happily return without taking any action
+	var res providers.ValidateProviderConfigResponse
+	return res
+}
+
+// ValidateDataSourceConfig is used to validate the data source configuration values.
+func (p *Provider) ValidateDataSourceConfig(providers.ValidateDataSourceConfigRequest) providers.ValidateDataSourceConfigResponse {
+	// At this moment there is nothing to configure for the terraform provider,
+	// so we will happily return without taking any action
+	var res providers.ValidateDataSourceConfigResponse
+	return res
+}
+
+// Configure configures and initializes the provider.
+func (p *Provider) Configure(providers.ConfigureRequest) providers.ConfigureResponse {
+	// At this moment there is nothing to configure for the terraform provider,
+	// so we will happily return without taking any action
+	var res providers.ConfigureResponse
+	return res
+}
+
+// ReadDataSource returns the data source's current state.
+func (p *Provider) ReadDataSource(req providers.ReadDataSourceRequest) providers.ReadDataSourceResponse {
+	// call function
+	var res providers.ReadDataSourceResponse
+
+	// This should not happen
+	if req.TypeName != "terraform_remote_state" {
+		res.Diagnostics.Append(fmt.Errorf("Error: unsupported data source %s", req.TypeName))
+		return res
+	}
+
+	newState, diags := dataSourceRemoteStateRead(&req.Config)
+
+	res.State = newState
+	res.Diagnostics = diags
+
+	return res
+}
+
+// Stop is called when the provider should halt any in-flight actions.
+func (p *Provider) Stop() error {
+	panic("unimplemented")
+}
+
+// All the Resource-specific functions are below.
+// The terraform provider supplies a single data source, `terraform_remote_state`
+// and no resources.
+
+// UpgradeResourceState is called when the state loader encounters an
+// instance state whose schema version is less than the one reported by the
+// currently-used version of the corresponding provider, and the upgraded
+// result is used for any further processing.
+func (p *Provider) UpgradeResourceState(providers.UpgradeResourceStateRequest) providers.UpgradeResourceStateResponse {
+	panic("unimplemented - terraform_remote_state has no resources")
+}
+
+// ReadResource refreshes a resource and returns its current state.
+func (p *Provider) ReadResource(providers.ReadResourceRequest) providers.ReadResourceResponse {
+	panic("unimplemented - terraform_remote_state has no resources")
+}
+
+// PlanResourceChange takes the current state and proposed state of a
+// resource, and returns the planned final state.
+func (p *Provider) PlanResourceChange(providers.PlanResourceChangeRequest) providers.PlanResourceChangeResponse {
+	panic("unimplemented - terraform_remote_state has no resources")
+}
+
+// ApplyResourceChange takes the planned state for a resource, which may
+// yet contain unknown computed values, and applies the changes returning
+// the final state.
+func (p *Provider) ApplyResourceChange(providers.ApplyResourceChangeRequest) providers.ApplyResourceChangeResponse {
+	panic("unimplemented - terraform_remote_state has no resources")
+}
+
+// ImportResourceState requests that the given resource be imported.
+func (p *Provider) ImportResourceState(providers.ImportResourceStateRequest) providers.ImportResourceStateResponse {
+	panic("unimplemented - terraform_remote_state has no resources")
+}
+
+// ValidateResourceTypeConfig is used to to validate the resource configuration values.
+func (p *Provider) ValidateResourceTypeConfig(providers.ValidateResourceTypeConfigRequest) providers.ValidateResourceTypeConfigResponse {
+	// At this moment there is nothing to configure for the terraform provider,
+	// so we will happily return without taking any action
+	var res providers.ValidateResourceTypeConfigResponse
+	return res
 }
