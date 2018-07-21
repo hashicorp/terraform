44a4e2efdb958dc645fa02bd3cbad626ebe466bc
Martin Atkins <mart@degeneration.co.uk>
2018-07-20 17:15:03 -0700

    plans: separate types for encoded and decoded changes
    The types here were originally written to allow us to defer decoding of
    object values until schemas are available, but it turns out that this was
    forcing us to defer decoding longer than necessary and potentially decode
    the same value multiple times.
    
    To avoid this, we create pairs of types to represent the encoded and
    decoded versions and methods for moving between them. These types are
    identical to one another apart from how the dynamic values are
    represented.

--- plans/changes.go
+++ plans/changes.go
@@ -3,6 +3,7 @@ package plans
 import (
 	"github.com/hashicorp/terraform/addrs"
 	"github.com/hashicorp/terraform/states"
+	"github.com/zclconf/go-cty/cty"
 )
 
 // Changes describes various actions that Terraform will attempt to take if
@@ -11,8 +12,15 @@ import (
 // A Changes object can be rendered into a visual diff (by the caller, using
 // code in another package) for display to the user.
 type Changes struct {
-	Resources   []*ResourceInstanceChange
-	RootOutputs map[string]*OutputChange
+	Resources   []*ResourceInstanceChangeSrc
+	RootOutputs map[string]*OutputChangeSrc
+}
+
+// NewChanges returns a valid Changes object that describes no changes.
+func NewChanges() *Changes {
+	return &Changes{
+		RootOutputs: make(map[string]*OutputChangeSrc),
+	}
 }
 
 // ResourceInstanceChange describes a change to a particular resource instance
@@ -41,6 +49,22 @@ type ResourceInstanceChange struct {
 	Change
 }
 
+// Encode produces a variant of the reciever that has its change values
+// serialized so it can be written to a plan file. Pass the implied type of the
+// corresponding resource type schema for correct operation.
+func (rc *ResourceInstanceChange) Encode(ty cty.Type) (*ResourceInstanceChangeSrc, error) {
+	cs, err := rc.Change.Encode(ty)
+	if err != nil {
+		return nil, err
+	}
+	return &ResourceInstanceChangeSrc{
+		Addr:         rc.Addr,
+		DeposedKey:   rc.DeposedKey,
+		ProviderAddr: rc.ProviderAddr,
+		ChangeSrc:    *cs,
+	}, err
+}
+
 // OutputChange describes a change to an output value.
 type OutputChange struct {
 	// Change is an embedded description of the change.
@@ -54,6 +78,19 @@ type OutputChange struct {
 	// should elide the actual values while still indicating the action of the
 	// change.
 	Sensitive bool
+}
+
+// Encode produces a variant of the reciever that has its change values
+// serialized so it can be written to a plan file.
+func (oc *OutputChange) Encode() (*OutputChangeSrc, error) {
+	cs, err := oc.Change.Encode(cty.DynamicPseudoType)
+	if err != nil {
+		return nil, err
+	}
+	return &OutputChangeSrc{
+		ChangeSrc: *cs,
+		Sensitive: oc.Sensitive,
+	}, err
 }
 
 // Change describes a single change with a given action.
@@ -75,9 +112,30 @@ type Change struct {
 	// Unknown values may appear anywhere within the Before and After values,
 	// either as the values themselves or as nested elements within known
 	// collections/structures.
-	//
-	// A plan contains only raw (not yet decoded) values. The caller must use
-	// schema information obtained out-of-band to decode dynamic values before
-	// they can be used.
-	Before, After DynamicValue
+	Before, After cty.Value
+}
+
+// Encode produces a variant of the reciever that has its change values
+// serialized so it can be written to a plan file. Pass the type constraint
+// that the values are expected to conform to; to properly decode the values
+// later an identical type constraint must be provided at that time.
+//
+// Where a Change is embedded in some other struct, it's generally better
+// to call the corresponding Encode method of that struct rather than working
+// directly with its embedded Change.
+func (c *Change) Encode(ty cty.Type) (*ChangeSrc, error) {
+	beforeDV, err := NewDynamicValue(c.Before, ty)
+	if err != nil {
+		return nil, err
+	}
+	afterDV, err := NewDynamicValue(c.After, ty)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ChangeSrc{
+		Action: c.Action,
+		Before: beforeDV,
+		After:  afterDV,
+	}, nil
 }
