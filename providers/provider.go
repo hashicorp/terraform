4675e614e2a8f2906f23496907ad347d7b17567a
James Bardin <j.bardin@gmail.com>
2018-07-25 17:50:01 -0400

    removing connection data from the providers
    Very few resources need connection info, and rather than relying on
    providers to set default for some resources, we are going to require
    that connection blocks be explicitly set in the configuration.

--- providers/provider.go
+++ providers/provider.go
@@ -243,10 +243,6 @@ type ApplyResourceChangeResponse struct {
 	// known state of the resource, if it exists.
 	NewState cty.Value
 
-	// Connection is used to return any information provisioners might require
-	// to cty.Value
-	Connection cty.Value
-
 	// Private is an opaque blob that will be stored in state along with the
 	// resource. It is intended only for interpretation by the provider itself.
 	Private []byte
