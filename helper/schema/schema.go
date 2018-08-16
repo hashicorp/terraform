a11446ecef922c25e8a6157bb7b61b361387a586
Paul Tyng <paul@paultyng.net>
2018-08-16 16:29:29 -0400

    Fix typo in comment

--- helper/schema/schema.go
+++ helper/schema/schema.go
@@ -199,7 +199,7 @@ type Schema struct {
 	Sensitive bool
 }
 
-// SchemaDiffSuppresFunc is a function which can be used to determine
+// SchemaDiffSuppressFunc is a function which can be used to determine
 // whether a detected diff on a schema element is "valid" or not, and
 // suppress it from the plan if necessary.
 //
