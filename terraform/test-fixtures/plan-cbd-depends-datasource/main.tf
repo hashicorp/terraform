95ad735684833cdeb516a8b087a480117eae184e
James Bardin <j.bardin@gmail.com>
2018-09-19 21:31:59 -0400

    complete the context plan test conversions

--- terraform/test-fixtures/plan-cbd-depends-datasource/main.tf
+++ terraform/test-fixtures/plan-cbd-depends-datasource/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "foo" {
   count   = 2
   num     = "2"
-  compute = data.aws_vpc.bar[count.index].id
+  computed = data.aws_vpc.bar[count.index].id
 
   lifecycle {
     create_before_destroy = true
