06fd7b6f8697d3c875cff655cdea2f51b39c769a
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2018-09-25 10:12:56 -0700

    terraform.Schemas: export struct fields
    There does not appear to be any real reason that these Schemas fields are
    not exported, and exporting them makes it possible to directly construct
    Schemas for tests without pulling in an entire context.

--- terraform/schemas_test.go
+++ terraform/schemas_test.go
@@ -8,10 +8,10 @@ func simpleTestSchemas() *Schemas {
 	provider := simpleMockProvider()
 	provisioner := simpleMockProvisioner()
 	return &Schemas{
-		providers: map[string]*ProviderSchema{
+		Providers: map[string]*ProviderSchema{
 			"test": provider.GetSchemaReturn,
 		},
-		provisioners: map[string]*configschema.Block{
+		Provisioners: map[string]*configschema.Block{
 			"test": provisioner.GetSchemaResponse.Provisioner,
 		},
 	}
