930e012d941072e241fd8f627066c20c83e37c0e
Nicholas Anderson <nicholas.anderson@nike.com>
2018-09-24 23:05:48 -0700

    create the selected workspace if it does not exist

--- command/workspace_select.go
+++ command/workspace_select.go
@@ -1,9 +1,13 @@
 package command
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/hashicorp/terraform/command/clistate"
+	"github.com/hashicorp/terraform/terraform"
 	"github.com/mitchellh/cli"
 	"github.com/posener/complete"
 )
@@ -21,10 +25,12 @@ func (c *WorkspaceSelectCommand) Run(args []string) int {
 
 	envCommandShowWarning(c.Ui, c.LegacyName)
 
+	statePath := ""
 	var createWorkspace bool
 
 	cmdFlags := c.Meta.flagSet("workspace select")
 	cmdFlags.BoolVar(&createWorkspace, "create", false, "create workspace")
+	cmdFlags.StringVar(&statePath, "state", "", "terraform state file")
 	cmdFlags.Usage = func() { c.Ui.Error(c.Help()) }
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
@@ -88,8 +94,81 @@ func (c *WorkspaceSelectCommand) Run(args []string) int {
 	}
 
 	if !found {
-		c.Ui.Error(fmt.Sprintf(envDoesNotExist, name))
-		return 1
+
+		// we don't want to create a workspace if it does not exist
+		if !createWorkspace {
+			c.Ui.Error(fmt.Sprintf(envDoesNotExist, name))
+			return 1
+		} else {
+			// we do want to create a workspace if it does not exist
+
+			// new workspace name
+			_, err = b.State(name)
+			if err != nil {
+				c.Ui.Error(err.Error())
+			}
+
+			// now set the current workspace locally
+			if err := c.SetWorkspace(name); err != nil {
+				c.Ui.Error(fmt.Sprintf("Error selecting new workspace: %s", err))
+				return 1
+			}
+
+			// tell the client we've created the workspace
+			c.Ui.Output(
+				c.Colorize().Color(
+					fmt.Sprintf(strings.TrimSpace(envCreated), name),
+				),
+			)
+
+			// if we aren't loading a state file, bail early
+			if statePath == "" {
+				return 0
+			}
+
+			// load the new backend's state
+			sMgr, err := b.State(name)
+			if err != nil {
+				c.Ui.Error(err.Error())
+				return 1
+			}
+
+			if c.stateLock {
+				stateLocker := clistate.NewLocker(context.Background(), c.stateLockTimeout, c.Ui, c.Colorize())
+				if err := stateLocker.Lock(sMgr, "workspace_delete"); err != nil {
+					c.Ui.Error(fmt.Sprintf("Error locking state: %s", err))
+					return 1
+				}
+				defer stateLocker.Unlock(nil)
+			}
+
+			// read existing state file
+			stateFile, err := os.Open(statePath)
+			if err != nil {
+				c.Ui.Error(err.Error())
+				return 1
+			}
+
+			s, err := terraform.ReadState(stateFile)
+			if err != nil {
+				c.Ui.Error(err.Error())
+				return 1
+			}
+
+			// save the existing state in the new backend
+			if err = sMgr.WriteState(s); err != nil {
+				c.Ui.Error(err.Error())
+				return 1
+			}
+			if err = sMgr.PersistState(); err != nil {
+				c.Ui.Error(err.Error())
+				return 1
+			}
+
+			// we've successfully switched to a new workspace
+			return 0
+
+		}
 	}
 
 	err = c.SetWorkspace(name)
