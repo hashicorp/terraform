f13870b06b122880660db3dec37130122dc72fad
Martin Atkins <mart@degeneration.co.uk>
2018-09-27 17:57:08 -0700

    core: A "go fmt" catchup
    Since we started using experimental Go Modules our editor tooling hasn't
    been fully functional, apparently including format-on-save support. This
    is a catchup to get everything back straight again.

--- terraform/graph_builder_plan.go
+++ terraform/graph_builder_plan.go
@@ -72,7 +72,7 @@ func (b *PlanGraphBuilder) Steps() []GraphTransformer {
 	concreteResourceInstanceDeposed := func(a *NodeAbstractResourceInstance, key states.DeposedKey) dag.Vertex {
 		return &NodePlanDeposedResourceInstanceObject{
 			NodeAbstractResourceInstance: a,
-			DeposedKey: key,
+			DeposedKey:                   key,
 		}
 	}
 
