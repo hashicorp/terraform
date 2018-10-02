b28ea808f4d1cf7ec76e533331e6a0635d51d3e2
Martin Atkins <mart@degeneration.co.uk>
2018-10-02 14:56:29 -0700

    backend/remote-state/inmem: enable optional logging in tests

--- backend/remote-state/inmem/backend_test.go
+++ backend/remote-state/inmem/backend_test.go
@@ -1,13 +1,33 @@
 package inmem
 
 import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/hcl2/hcl"
+
 	"github.com/hashicorp/terraform/backend"
+	"github.com/hashicorp/terraform/helper/logging"
 	"github.com/hashicorp/terraform/state/remote"
 	statespkg "github.com/hashicorp/terraform/states"
 )
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	if testing.Verbose() {
+		// if we're verbose, use the logging requested by TF_LOG
+		logging.SetOutput()
+	} else {
+		// otherwise silence all logs
+		log.SetOutput(ioutil.Discard)
+	}
+
+	os.Exit(m.Run())
+}
 
 func TestBackend_impl(t *testing.T) {
 	var _ backend.Backend = new(Backend)
