cb82a1f5082423a62220e23ab09d5bf626056925
Martin Atkins <mart@degeneration.co.uk>
2018-10-14 08:29:52 -0700

    command: Make suitable provider schemas available for all "plan" tests

--- command/test-fixtures/plan/main.tf
+++ command/test-fixtures/plan/main.tf
@@ -1,9 +1,9 @@
 resource "test_instance" "foo" {
-    ami = "bar"
+  ami = "bar"
 
-    # This is here because at some point it caused a test failure
-    network_interface {
-      device_index = 0
-      description = "Main network interface"
-    }
+  # This is here because at some point it caused a test failure
+  network_interface {
+    device_index = 0
+    description = "Main network interface"
+  }
 }
