ca63d11965ceef31530eea8db933e96445b015c2
Martin Atkins <mart@degeneration.co.uk>
2018-10-14 09:21:31 -0700

    command: Fix most (but not all) "terraform plan" tests

--- command/test-fixtures/plan-invalid/main.tf
+++ command/test-fixtures/plan-invalid/main.tf
@@ -1,7 +1,10 @@
 resource "test_instance" "foo" {
-    count = 5
+  count = 5
 }
 
 resource "test_instance" "bar" {
-    count = "${length(test_instance.foo.*.id)}"
+  # This is invalid because timestamp() returns an unknown value during plan,
+  # but the "count" argument in particular must always be known during plan
+  # so we can predict how many instances we will operate on.
+  count = timestamp()
 }
