19c1241a500fa3b02d7f7af8b70fb99f48d58b3e
Sander van Harmelen <sander@xanzy.io>
2018-10-22 15:52:53 +0200

    command/state: update and fix the state rm command

--- command/state_meta.go
+++ command/state_meta.go
@@ -1,15 +1,14 @@
 package command
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/terraform/addrs"
 	backendlocal "github.com/hashicorp/terraform/backend/local"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/states"
 	"github.com/hashicorp/terraform/states/statemgr"
-	"github.com/hashicorp/terraform/terraform"
 )
 
 // StateMeta is the meta struct that should be embedded in state subcommands.
@@ -79,22 +78,40 @@ func (c *StateMeta) State() (state.State, error) {
 	return realState, nil
 }
 
-// filterInstance filters a single instance out of filter results.
-func (c *StateMeta) filterInstance(rs []*states.FilterResult) (*states.FilterResult, error) {
-	var result *states.FilterResult
-	for _, r := range rs {
-		if _, ok := r.Value.(*terraform.InstanceState); !ok {
-			continue
+func (c *StateMeta) filter(state *states.State, args []string) ([]*states.FilterResult, error) {
+	var results []*states.FilterResult
+
+	filter := &states.Filter{State: state}
+	for _, arg := range args {
+		filtered, err := filter.Filter(arg)
+		if err != nil {
+			return nil, err
 		}
 
-		if result != nil {
-			return nil, errors.New(errStateMultiple)
+	filtered:
+		for _, result := range filtered {
+			switch result.Address.(type) {
+			case addrs.ModuleInstance:
+				for _, result := range filtered {
+					if _, ok := result.Address.(addrs.ModuleInstance); ok {
+						results = append(results, result)
+					}
+				}
+				break filtered
+			case addrs.AbsResource:
+				for _, result := range filtered {
+					if _, ok := result.Address.(addrs.AbsResource); ok {
+						results = append(results, result)
+					}
+				}
+				break filtered
+			case addrs.AbsResourceInstance:
+				results = append(results, result)
+			}
 		}
-
-		result = r
 	}
 
-	return result, nil
+	return results, nil
 }
 
 const errStateMultiple = `Multiple instances found for the given pattern!
