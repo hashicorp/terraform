b316e4ab568cbcf1824473fb08112032ca75cace
Martin Atkins <mart@degeneration.co.uk>
2018-11-15 13:31:30 -0800

    command: Fix TestImport_remoteState
    The import command was imposing the default state path at the CLI level,
    rather than leaving that to be handled by the backend. As a result, the
    output state was always forced to be terraform.tfstate, regardless of
    the backend settings.

--- command/import.go
+++ command/import.go
@@ -39,7 +39,7 @@ func (c *ImportCommand) Run(args []string) int {
 
 	cmdFlags := c.Meta.flagSet("import")
 	cmdFlags.IntVar(&c.Meta.parallelism, "parallelism", 0, "parallelism")
-	cmdFlags.StringVar(&c.Meta.statePath, "state", DefaultStateFilename, "path")
+	cmdFlags.StringVar(&c.Meta.statePath, "state", "", "path")
 	cmdFlags.StringVar(&c.Meta.stateOutPath, "state-out", "", "path")
 	cmdFlags.StringVar(&c.Meta.backupPath, "backup", "", "path")
 	cmdFlags.StringVar(&configPath, "config", pwd, "path")
