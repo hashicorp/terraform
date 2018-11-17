96dfd4cb8f9bf39c80c311bb5194fc8cdcbfdad8
Martin Atkins <mart@degeneration.co.uk>
2018-11-16 18:01:29 -0800

    states/statemgr: Local locks meta is near output path, not input path
    This was a mistake while adapting this code from the old state.LocalState.
    Since the lock is held on the output file (s.path) the metadata should
    live adjacent to that rather than being built from the read path
    (s.readPath) that is used only as the initial snapshot on first
    instantiation.
    
    This also includes more logging, continuing the trend of other recent
    commits in these files. The local state behavior is sufficiently complex
    that these trace logs are a great help in debugging issues such as this
    one with the wrong files being used or actions being taken in the wrong
    order.

--- states/statemgr/filesystem_lock_unix.go
+++ states/statemgr/filesystem_lock_unix.go
@@ -3,6 +3,7 @@
 package statemgr
 
 import (
+	"log"
 	"os"
 	"syscall"
 )
@@ -10,6 +11,7 @@ import (
 // use fcntl POSIX locks for the most consistent behavior across platforms, and
 // hopefully some campatibility over NFS and CIFS.
 func (s *Filesystem) lock() error {
+	log.Printf("[TRACE] statemgr.Filesystem: locking %s using fcntl flock", s.path)
 	flock := &syscall.Flock_t{
 		Type:   syscall.F_RDLCK | syscall.F_WRLCK,
 		Whence: int16(os.SEEK_SET),
@@ -22,6 +24,7 @@ func (s *Filesystem) lock() error {
 }
 
 func (s *Filesystem) unlock() error {
+	log.Printf("[TRACE] statemgr.Filesystem: unlocking %s using fcntl flock", s.path)
 	flock := &syscall.Flock_t{
 		Type:   syscall.F_UNLCK,
 		Whence: int16(os.SEEK_SET),
