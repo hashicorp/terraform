3a756da72a3bf873a68403d283aa8eea3ea46533
tombuildsstuff <git@ibuildstuff.co.uk>
2018-11-21 17:58:43 +0100

    vendor updates
    - updating to v21.3.0 of github.com/Azure/azure-sdk-for-go
    - updating to v10.15.4 of github.com/Azure/go-autorest
    - vendoring github.com/hashicorp/go-azure-helpers @ 0.1.1

---
+++ vendor/github.com/hashicorp/go-azure-helpers/authentication/builder.go
@@ -0,0 +1,81 @@
+package authentication
+
+import (
+	"fmt"
+	"log"
+)
+
+// Builder supports all of the possible Authentication values and feature toggles
+// required to build a working Config for Authentication purposes.
+type Builder struct {
+	// Core
+	ClientID       string
+	SubscriptionID string
+	TenantID       string
+	Environment    string
+
+	// The custom Resource Manager Endpoint which should be used
+	// only applicable for Azure Stack at this time.
+	CustomResourceManagerEndpoint string
+
+	// Azure CLI Parsing / CloudShell Auth
+	SupportsAzureCliCloudShellParsing bool
+
+	// Managed Service Identity Auth
+	SupportsManagedServiceIdentity bool
+	MsiEndpoint                    string
+
+	// Service Principal (Client Cert) Auth
+	SupportsClientCertAuth bool
+	ClientCertPath         string
+	ClientCertPassword     string
+
+	// Service Principal (Client Secret) Auth
+	SupportsClientSecretAuth bool
+	ClientSecret             string
+}
+
+// Build takes the configuration from the Builder and builds up a validated Config
+// for authenticating with Azure
+func (b Builder) Build() (*Config, error) {
+	config := Config{
+		ClientID:                      b.ClientID,
+		SubscriptionID:                b.SubscriptionID,
+		TenantID:                      b.TenantID,
+		Environment:                   b.Environment,
+		CustomResourceManagerEndpoint: b.CustomResourceManagerEndpoint,
+	}
+
+	// NOTE: the ordering here is important
+	// since the Azure CLI Parsing should always be the last thing checked
+	supportedAuthenticationMethods := []authMethod{
+		servicePrincipalClientCertificateAuth{},
+		servicePrincipalClientSecretAuth{},
+		managedServiceIdentityAuth{},
+		azureCliParsingAuth{},
+	}
+
+	for _, method := range supportedAuthenticationMethods {
+		name := method.name()
+		log.Printf("Testing if %s is applicable for Authentication..", name)
+		if method.isApplicable(b) {
+			log.Printf("Using %s for Authentication", name)
+			auth, err := method.build(b)
+			if err != nil {
+				return nil, err
+			}
+
+			// populate authentication specific fields on the Config
+			// (e.g. is service principal, fields parsed from the azure cli)
+			err = auth.populateConfig(&config)
+			if err != nil {
+				return nil, err
+			}
+
+			config.authMethod = auth
+			return config.validate()
+		}
+	}
+
+	return nil, fmt.Errorf("No supported authentication methods were found!")
+}
