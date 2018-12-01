d5bb4cd82f3eb9f9feb6f535428ec73188bd26bd
Martin Atkins <mart@degeneration.co.uk>
2018-11-30 22:49:48 -0800

    configs/configupgrade: Upgrading of simple nested blocks

---
+++ configs/configupgrade/test-fixtures/valid/block-as-list-attr/input/block-as-list-attr.tf
@@ -0,0 +1,10 @@
+resource "test_instance" "foo" {
+  network = [
+    {
+      cidr_block = "10.1.0.0/16"
+    },
+    {
+      cidr_block = "10.2.0.0/16"
+    },
+  ]
+}
---
+++ configs/configupgrade/test-fixtures/valid/block-as-map-attr/want/block-as-map-attr.tf
@@ -0,0 +1,5 @@
+resource "test_instance" "foo" {
+  network {
+    cidr_block = "10.1.0.0/16"
+  }
+}
