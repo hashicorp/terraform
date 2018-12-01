c4cbf7c716a299d7f970f52d17bedbf4ada25c70
Martin Atkins <mart@degeneration.co.uk>
2018-12-01 10:28:34 -0800

    configs/configupgrade: Normalize and upgrade reference expressions
    The reference syntax is not significantly changed, but there are some
    minor additional restrictions on identifiers in HCL2 and as a special case
    we need to rewrite references to data.terraform_remote_state .
    
    Along with those mandatory upgrades, we will also switch references to
    using normal index syntax where it's safe to do so, as part of
    de-emphasizing the old strange integer attribute syntax (like foo.0.bar).

---
+++ configs/configupgrade/test-fixtures/valid/traversals/input/traversals.tf
@@ -0,0 +1,23 @@
+locals {
+  simple = "${test_instance.foo.bar}"
+  splat  = "${test_instance.foo.*.bar}"
+  index  = "${test_instance.foo.1.bar}"
+
+  after_simple = "${test_instance.foo.bar.0.baz}"
+  after_splat  = "${test_instance.foo.*.bar.0.baz}"
+  after_index  = "${test_instance.foo.1.bar.2.baz}"
+
+  non_ident_attr = "${test_instance.foo.bar.1baz}"
+
+  remote_state_output       = "${data.terraform_remote_state.foo.bar}"
+  remote_state_attr         = "${data.terraform_remote_state.foo.backend}"
+  remote_state_idx_output   = "${data.terraform_remote_state.foo.1.bar}"
+  remote_state_idx_attr     = "${data.terraform_remote_state.foo.1.backend}"
+  remote_state_splat_output = "${data.terraform_remote_state.foo.*.bar}"
+  remote_state_splat_attr   = "${data.terraform_remote_state.foo.*.backend}"
+}
+
+data "terraform_remote_state" "foo" {
+  # This is just here to make sure the schema for this gets loaded to
+  # support the remote_state_* checks above.
+}
