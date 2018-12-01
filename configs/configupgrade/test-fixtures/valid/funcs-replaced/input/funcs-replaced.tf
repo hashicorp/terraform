10969865d4109bc8f3ff85db2e59cb2aeccf0261
Martin Atkins <mart@degeneration.co.uk>
2018-11-30 18:23:09 -0800

    configs/configupgrade: Replace calls to list() and map()
    We now have native language features for declaring tuples and objects,
    which are the idiomatic way to construct sequence and mapping values that
    can then be converted to list, set, and map types as needed.

---
+++ configs/configupgrade/test-fixtures/valid/funcs-replaced/input/funcs-replaced.tf
@@ -0,0 +1,13 @@
+locals {
+  list        = "${list("a", "b", "c")}"
+  list_concat = "${concat(list("a", "b"), list("c"))}"
+  list_empty  = "${list()}"
+
+  map         = "${map("a", "b", "c", "d")}"
+  map_merge   = "${merge(map("a", "b"), map("c", "d"))}"
+  map_empty   = "${map()}"
+  map_invalid = "${map("a", "b", "c")}"
+
+  list_of_map = "${list(map("a", "b"))}"
+  map_of_list = "${map("a", list("b"))}"
+}
