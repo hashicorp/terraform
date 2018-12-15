1533f11c90cfcb7406cf3979e1a8d972fb51b96d
Martin Atkins <mart@degeneration.co.uk>
2018-12-15 04:33:40 +0000

    internal/lsp: Import langserver protocol stubs from the Go tools project
    This is a mechanical transliteration from the LSP specification and is
    thus not Go-specific even though it was created by and published within
    the Go tools project as part of the official Go LSP implementation.

---
+++ internal/lsp/client.go
@@ -0,0 +1,187 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lsp
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/hashicorp/terraform/internal/jsonrpc2"
+)
+
+type Client interface {
+	ShowMessage(context.Context, *ShowMessageParams) error
+	ShowMessageRequest(context.Context, *ShowMessageRequestParams) (*MessageActionItem, error)
+	LogMessage(context.Context, *LogMessageParams) error
+	Telemetry(context.Context, interface{}) error
+	RegisterCapability(context.Context, *RegistrationParams) error
+	UnregisterCapability(context.Context, *UnregistrationParams) error
+	WorkspaceFolders(context.Context) ([]WorkspaceFolder, error)
+	Configuration(context.Context, *ConfigurationParams) ([]interface{}, error)
+	ApplyEdit(context.Context, *ApplyWorkspaceEditParams) (bool, error)
+	PublishDiagnostics(context.Context, *PublishDiagnosticsParams) error
+}
+
+func clientHandler(client Client) jsonrpc2.Handler {
+	return func(ctx context.Context, conn *jsonrpc2.Conn, r *jsonrpc2.Request) {
+		switch r.Method {
+		case "$/cancelRequest":
+			var params CancelParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			conn.Cancel(params.ID)
+
+		case "window/showMessage":
+			var params ShowMessageParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.ShowMessage(ctx, &params))
+
+		case "window/showMessageRequest":
+			var params ShowMessageRequestParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			resp, err := client.ShowMessageRequest(ctx, &params)
+			unhandledError(conn.Reply(ctx, r, resp, err))
+
+		case "window/logMessage":
+			var params LogMessageParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.LogMessage(ctx, &params))
+
+		case "telemetry/event":
+			var params interface{}
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.Telemetry(ctx, &params))
+
+		case "client/registerCapability":
+			var params RegistrationParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.RegisterCapability(ctx, &params))
+
+		case "client/unregisterCapability":
+			var params UnregistrationParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.UnregisterCapability(ctx, &params))
+
+		case "workspace/workspaceFolders":
+			if r.Params != nil {
+				conn.Reply(ctx, r, nil, jsonrpc2.NewErrorf(jsonrpc2.CodeInvalidParams, "Expected no params"))
+				return
+			}
+			resp, err := client.WorkspaceFolders(ctx)
+			unhandledError(conn.Reply(ctx, r, resp, err))
+
+		case "workspace/configuration":
+			var params ConfigurationParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			resp, err := client.Configuration(ctx, &params)
+			unhandledError(conn.Reply(ctx, r, resp, err))
+
+		case "workspace/applyEdit":
+			var params ApplyWorkspaceEditParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			resp, err := client.ApplyEdit(ctx, &params)
+			unhandledError(conn.Reply(ctx, r, resp, err))
+
+		case "textDocument/publishDiagnostics":
+			var params PublishDiagnosticsParams
+			if err := json.Unmarshal(*r.Params, &params); err != nil {
+				sendParseError(ctx, conn, r, err)
+				return
+			}
+			unhandledError(client.PublishDiagnostics(ctx, &params))
+
+		default:
+			if r.IsNotify() {
+				conn.Reply(ctx, r, nil, jsonrpc2.NewErrorf(jsonrpc2.CodeMethodNotFound, "method %q not found", r.Method))
+			}
+		}
+	}
+}
+
+type clientDispatcher struct {
+	*jsonrpc2.Conn
+}
+
+func (c *clientDispatcher) ShowMessage(ctx context.Context, params *ShowMessageParams) error {
+	return c.Conn.Notify(ctx, "window/showMessage", params)
+}
+
+func (c *clientDispatcher) ShowMessageRequest(ctx context.Context, params *ShowMessageRequestParams) (*MessageActionItem, error) {
+	var result MessageActionItem
+	if err := c.Conn.Call(ctx, "window/showMessageRequest", params, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *clientDispatcher) LogMessage(ctx context.Context, params *LogMessageParams) error {
+	return c.Conn.Notify(ctx, "window/logMessage", params)
+}
+
+func (c *clientDispatcher) Telemetry(ctx context.Context, params interface{}) error {
+	return c.Conn.Notify(ctx, "telemetry/event", params)
+}
+
+func (c *clientDispatcher) RegisterCapability(ctx context.Context, params *RegistrationParams) error {
+	return c.Conn.Notify(ctx, "client/registerCapability", params)
+}
+
+func (c *clientDispatcher) UnregisterCapability(ctx context.Context, params *UnregistrationParams) error {
+	return c.Conn.Notify(ctx, "client/unregisterCapability", params)
+}
+
+func (c *clientDispatcher) WorkspaceFolders(ctx context.Context) ([]WorkspaceFolder, error) {
+	var result []WorkspaceFolder
+	if err := c.Conn.Call(ctx, "workspace/workspaceFolders", nil, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *clientDispatcher) Configuration(ctx context.Context, params *ConfigurationParams) ([]interface{}, error) {
+	var result []interface{}
+	if err := c.Conn.Call(ctx, "workspace/configuration", params, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *clientDispatcher) ApplyEdit(ctx context.Context, params *ApplyWorkspaceEditParams) (bool, error) {
+	var result bool
+	if err := c.Conn.Call(ctx, "workspace/applyEdit", params, &result); err != nil {
+		return false, err
+	}
+	return result, nil
+}
+
+func (c *clientDispatcher) PublishDiagnostics(ctx context.Context, params *PublishDiagnosticsParams) error {
+	return c.Conn.Notify(ctx, "textDocument/publishDiagnostics", params)
+}
---
+++ internal/lsp/printers.go
@@ -0,0 +1,83 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file contains formatting functions for types that
+// are commonly printed in debugging information.
+// They are separated from their types and gathered here as
+// they are hand written and not generated from the spec.
+// They should not be relied on for programmatic use (their
+// results should never be parsed for instance) but are meant
+// for temporary debugging and error messages.
+
+package lsp
+
+import (
+	"fmt"
+)
+
+func (p Position) Format(f fmt.State, c rune) {
+	fmt.Fprintf(f, "%d", int(p.Line)+1)
+	if p.Character >= 0 {
+		fmt.Fprintf(f, ":%d", int(p.Character)+1)
+	}
+}
+
+func (r Range) Format(f fmt.State, c rune) {
+	switch {
+	case r.Start == r.End || r.End.Line < 0:
+		fmt.Fprintf(f, "%v", r.Start)
+	case r.End.Line == r.Start.Line:
+		fmt.Fprintf(f, "%v¦%d", r.Start, int(r.End.Character)+1)
+	default:
+		fmt.Fprintf(f, "%v¦%v", r.Start, r.End)
+	}
+}
+
+func (l Location) Format(f fmt.State, c rune) {
+	fmt.Fprintf(f, "%s:%v", l.URI, l.Range)
+}
+
+func (s DiagnosticSeverity) Format(f fmt.State, c rune) {
+	switch s {
+	case SeverityError:
+		fmt.Fprint(f, "Error")
+	case SeverityWarning:
+		fmt.Fprint(f, "Warning")
+	case SeverityInformation:
+		fmt.Fprint(f, "Information")
+	case SeverityHint:
+		fmt.Fprint(f, "Hint")
+	}
+}
+
+func (d Diagnostic) Format(f fmt.State, c rune) {
+	fmt.Fprintf(f, "%v:%v from %v at %v: %v", d.Severity, d.Code, d.Source, d.Range, d.Message)
+}
+
+func (i CompletionItem) Format(f fmt.State, c rune) {
+	fmt.Fprintf(f, "%v %v %v", i.Label, i.Detail, CompletionItemKind(i.Kind))
+}
+
+func (k CompletionItemKind) Format(f fmt.State, c rune) {
+	switch k {
+	case StructCompletion:
+		fmt.Fprintf(f, "struct")
+	case FunctionCompletion:
+		fmt.Fprintf(f, "func")
+	case VariableCompletion:
+		fmt.Fprintf(f, "var")
+	case TypeParameterCompletion:
+		fmt.Fprintf(f, "type")
+	case FieldCompletion:
+		fmt.Fprintf(f, "field")
+	case InterfaceCompletion:
+		fmt.Fprintf(f, "interface")
+	case ConstantCompletion:
+		fmt.Fprintf(f, "const")
+	case MethodCompletion:
+		fmt.Fprintf(f, "method")
+	case ModuleCompletion:
+		fmt.Fprintf(f, "package")
+	}
+}
