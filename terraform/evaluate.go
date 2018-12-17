1ae46dcc3255645fdfcdc666f7ab6e1250c5eb35
James Bardin <j.bardin@gmail.com>
2018-12-16 21:59:44 -0500

    don't expand EachMode from state during validation
    Validate should not require state or changes to be present. Break out
    early when using evaluationStateData during walkValidate before checking
    state or changes, to prevent errors when indexing resources that haven't
    been expanded.

--- terraform/evaluate.go
+++ terraform/evaluate.go
@@ -91,6 +91,10 @@ type evaluationStateData struct {
 	// since the user specifies in that case which variable name to locally
 	// shadow.)
 	InstanceKeyData InstanceKeyEvalData
+
+	// Operation records the type of walk the evaluationStateData is being used
+	// for.
+	Operation walkOperation
 }
 
 // InstanceKeyEvalData is used during evaluation to specify which values,
@@ -503,6 +507,12 @@ func (d *evaluationStateData) GetResourceInstance(addr addrs.ResourceInstance, r
 		// (In practice we should only end up here during the validate walk,
 		// since later walks should have at least partial states populated
 		// for all resources in the configuration.)
+		return cty.DynamicVal, diags
+	}
+
+	// Break out early during validation, because resource may not be expanded
+	// yet and indexed references may show up as invalid.
+	if d.Operation == walkValidate {
 		return cty.DynamicVal, diags
 	}
 
