c63040c73729a4f9e607292ede5660dfe1eca522
James Bardin <j.bardin@gmail.com>
2019-01-09 13:01:17 -0500

    have TestCheckResourceAttr accept missing counts
    Missing containers were often erroneously kept in the state, but since
    the addition of the new provider shims, they can often be correctly
    eliminated. There are however many tests that check for a "0" count in
    the flatmap state when there shouldn't be a key at all. This addition
    looks for a container count key and "0" pair, and allows for the key to
    be missing.
    
    There may be some tests negatively effected by this which were
    legitimately checking for empty containers, but those were also not
    reliably detected, and there should be much fewer tests involved.

--- helper/resource/testing_test.go
+++ helper/resource/testing_test.go
@@ -1113,3 +1113,34 @@ resource "test_instance" "foo" {}
 const testConfigStrProvider = `
 provider "test" {}
 `
+
+func TestCheckResourceAttr_empty(t *testing.T) {
+	s := terraform.NewState()
+	s.AddModuleState(&terraform.ModuleState{
+		Path: []string{"root"},
+		Resources: map[string]*terraform.ResourceState{
+			"test_resource": &terraform.ResourceState{
+				Primary: &terraform.InstanceState{
+					Attributes: map[string]string{
+						"empty_list.#": "0",
+						"empty_map.%":  "0",
+					},
+				},
+			},
+		},
+	})
+
+	for _, key := range []string{
+		"empty_list.#",
+		"empty_map.%",
+		"missing_list.#",
+		"missing_map.%",
+	} {
+		t.Run(key, func(t *testing.T) {
+			check := TestCheckResourceAttr("test_resource", key, "0")
+			if err := check(s); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
