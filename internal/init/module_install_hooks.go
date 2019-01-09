84c7d4b9a3fbc30d36b498e4af38d2bfbdb415cd
Martin Atkins <mart@degeneration.co.uk>
2019-01-08 16:21:31 -0800

    internal/init: Module installation functionality
    This is an adaptation of the installation code from configs/configload,
    now using the "earlyconfig" package instead of the "configs" package.
    
    Module installation is an initialization-only process, with all other
    commands assuming an already-initialized directory. Having it here can
    therefore simplify the API of configs/configload, which can now focus only
    on the problem of loading modules that have already been installed.
    
    The old installer code in configs/configload is still in place for now
    because the caller in "terraform init" isn't yet updated to use this.

---
+++ internal/init/module_install_hooks.go
@@ -0,0 +1,36 @@
+package init
+
+import (
+	version "github.com/hashicorp/go-version"
+)
+
+// InstallHooks is an interface used to provide notifications about the
+// installation process being orchestrated by InstallModules.
+//
+// This interface may have new methods added in future, so implementers should
+// embed InstallHooksImpl to get no-op implementations of any unimplemented
+// methods.
+type InstallHooks interface {
+	// Download is called for modules that are retrieved from a remote source
+	// before that download begins, to allow a caller to give feedback
+	// on progress through a possibly-long sequence of downloads.
+	Download(moduleAddr, packageAddr string, version *version.Version)
+
+	// Install is called for each module that is installed, even if it did
+	// not need to be downloaded from a remote source.
+	Install(moduleAddr string, version *version.Version, localPath string)
+}
+
+// InstallHooksImpl is a do-nothing implementation of InstallHooks that
+// can be embedded in another implementation struct to allow only partial
+// implementation of the interface.
+type InstallHooksImpl struct {
+}
+
+func (h InstallHooksImpl) Download(moduleAddr, packageAddr string, version *version.Version) {
+}
+
+func (h InstallHooksImpl) Install(moduleAddr string, version *version.Version, localPath string) {
+}
+
+var _ InstallHooks = InstallHooksImpl{}
