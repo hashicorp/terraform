4439a7dcf4f1ec21da75062a10d73a3bb45b2471
James Bardin <j.bardin@gmail.com>
2019-01-16 16:54:02 -0500

    add tests for nested default values
    Don't lose default values set within a nested block.

--- builtin/providers/test/provider.go
+++ builtin/providers/test/provider.go
@@ -27,6 +27,7 @@ func Provider() terraform.ResourceProvider {
 			"test_resource_nested_set":       testResourceNestedSet(),
 			"test_resource_state_func":       testResourceStateFunc(),
 			"test_resource_deprecated":       testResourceDeprecated(),
+			"test_resource_defaults":         testResourceDefaults(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"test_data_source":    testDataSource(),
