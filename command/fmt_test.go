ecab3e055ead5586cb45e45ba2e5107658ba421c
Martin Atkins <mart@degeneration.co.uk>
2019-01-17 10:58:59 -0800

    command: Restore single-file support in "terraform fmt"
    This possibility was lost in the rewrite to use HCL2, but it's used by
    a number of external utilities and text editor integrations, so we'll
    restore it here.
    
    Using the stdin/stdout mode is generally preferable for text editor use
    since it allows formatting of the in-memory buffer rather than directly
    the file on disk, but for editors that don't have support for that sort of
    tooling it can be convenient to just launch a single command and directly
    modify the on-disk file.

--- command/fmt_test.go
+++ command/fmt_test.go
@@ -29,7 +29,7 @@ func TestFmt_nonexist(t *testing.T) {
 		t.Fatalf("wrong exit code. errors: \n%s", ui.ErrorWriter.String())
 	}
 
-	expected := "There is no configuration directory at"
+	expected := "No file or directory at"
 	if actual := ui.ErrorWriter.String(); !strings.Contains(actual, expected) {
 		t.Fatalf("expected:\n%s\n\nto include: %q", actual, expected)
 	}
@@ -133,6 +133,33 @@ func TestFmt_directoryArg(t *testing.T) {
 	}
 
 	args := []string{tempDir}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("wrong exit code. errors: \n%s", ui.ErrorWriter.String())
+	}
+
+	got, err := filepath.Abs(strings.TrimSpace(ui.OutputWriter.String()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := filepath.Join(tempDir, fmtFixture.filename)
+
+	if got != want {
+		t.Fatalf("wrong output\ngot:  %s\nwant: %s", got, want)
+	}
+}
+
+func TestFmt_fileArg(t *testing.T) {
+	tempDir := fmtFixtureWriteDir(t)
+
+	ui := new(cli.MockUi)
+	c := &FmtCommand{
+		Meta: Meta{
+			testingOverrides: metaOverridesForProvider(testProvider()),
+			Ui:               ui,
+		},
+	}
+
+	args := []string{filepath.Join(tempDir, fmtFixture.filename)}
 	if code := c.Run(args); code != 0 {
 		t.Fatalf("wrong exit code. errors: \n%s", ui.ErrorWriter.String())
 	}
