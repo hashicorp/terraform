2d290d7846d513a72b51dca470efccc28511fb2b
Martin Atkins <mart@degeneration.co.uk>
2019-01-24 15:12:57 -0800

    vendor: go get github.com/zclconf/go-cty@master
    This fixes some consistency problems with how number strings were parsed
    in the msgpack decoder vs other situations.
    
    This commit also includes an upgrade of HCL2 to use this new cty function,
    though there's no change in behavior here since the new function is
    functionally equivalent to what it replaced.

--- vendor/github.com/hashicorp/hcl2/hcl/json/parser.go
+++ vendor/github.com/hashicorp/hcl2/hcl/json/parser.go
@@ -3,9 +3,9 @@ package json
 import (
 	"encoding/json"
 	"fmt"
-	"math/big"
 
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func parseFileContent(buf []byte, filename string) (node, hcl.Diagnostics) {
@@ -370,10 +370,15 @@ func parseNumber(p *peeker) (node, hcl.Diagnostics) {
 		}
 	}
 
-	f, _, err := big.ParseFloat(string(num), 10, 512, big.ToNearestEven)
+	// We want to guarantee that we parse numbers the same way as cty (and thus
+	// native syntax HCL) would here, so we'll use the cty parser even though
+	// in most other cases we don't actually introduce cty concepts until
+	// decoding time. We'll unwrap the parsed float immediately afterwards, so
+	// the cty value is just a temporary helper.
+	nv, err := cty.ParseNumberVal(string(num))
 	if err != nil {
 		// Should never happen if above passed, since JSON numbers are a subset
-		// of what big.Float can parse...
+		// of what cty can parse...
 		return nil, hcl.Diagnostics{
 			{
 				Severity: hcl.DiagError,
@@ -385,7 +390,7 @@ func parseNumber(p *peeker) (node, hcl.Diagnostics) {
 	}
 
 	return &numberVal{
-		Value:    f,
+		Value:    nv.AsBigFloat(),
 		SrcRange: tok.Range,
 	}, nil
 }
--- vendor/github.com/zclconf/go-cty/cty/json/unmarshal.go
+++ vendor/github.com/zclconf/go-cty/cty/json/unmarshal.go
@@ -72,7 +72,7 @@ func unmarshalPrimitive(tok json.Token, t cty.Type, path cty.Path) (cty.Value, e
 		}
 		switch v := tok.(type) {
 		case string:
-			val, err := convert.Convert(cty.StringVal(v), t)
+			val, err := cty.ParseNumberVal(v)
 			if err != nil {
 				return cty.NilVal, path.NewError(err)
 			}
