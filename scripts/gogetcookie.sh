429bbcf14f1db5703ca12518f924ee31ecafa8c1
Martin Atkins <mart@degeneration.co.uk>
2019-02-01 15:41:28 -0800

    build: Periodic refresh of our go.googlesource.com cookie
    Just as with 336b352d6fe4, this refreshes our rate limit bypass cookie for
    go.googlesource.com since the old one has apparently expired.
    
    I did this by visiting https://go.googlesource.com/ and selecting
    "Generate Password" from the top navigation. This cookie belongs to a
    test account used by the Terraform team and should not be used by
    non-Terraform codebases; please generate your own!

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -4,5 +4,5 @@ chmod 0600 ~/.gitcookies
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-admin.hashicorptest.com=1/F-KiU2h0C3CsGR-W37nUzB2LOSfI24YXa71rjfd4qUI
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-admin.hashicorptest.com=1/ba6U_xcdflHTPlB4ScWE5O63YMMlvbYtHxP8M_yufDs5-YdA8pqbXQZAtKwT7ROb
 __END__
