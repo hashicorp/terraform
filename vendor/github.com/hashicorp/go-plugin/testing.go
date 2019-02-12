cf7a6b7bb826e0ea40e621446c4c75f0a9403dc5
James Bardin <j.bardin@gmail.com>
2019-02-12 18:27:56 -0500

    update go-plugin
    This unfortunately breaks alpha comaptibility at this point, due to
    changes in the grpc paths.

--- vendor/github.com/hashicorp/go-plugin/testing.go
+++ vendor/github.com/hashicorp/go-plugin/testing.go
@@ -7,9 +7,9 @@ import (
 	"net"
 	"net/rpc"
 
-	hclog "github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/go-plugin/internal/proto"
 	"github.com/mitchellh/go-testing-interface"
+	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin/internal/plugin"
 	"google.golang.org/grpc"
 )
 
@@ -173,7 +173,7 @@ func TestPluginGRPCConn(t testing.T, ps map[string]Plugin) (*GRPCClient, *GRPCSe
 		Plugins:    ps,
 		broker:     broker,
 		doneCtx:    context.Background(),
-		controller: proto.NewGRPCControllerClient(conn),
+		controller: plugin.NewGRPCControllerClient(conn),
 	}
 
 	return client, server
