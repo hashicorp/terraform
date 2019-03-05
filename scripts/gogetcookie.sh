6c896bc45671c9ce126cef7205797dbac9cee6ad
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2019-03-05 08:31:36 -0800

    build: refresh go.googlesource.com cookie (#20558)

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -4,5 +4,5 @@ chmod 0600 ~/.gitcookies
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-admin.hashicorptest.com=1/ba6U_xcdflHTPlB4ScWE5O63YMMlvbYtHxP8M_yufDs5-YdA8pqbXQZAtKwT7ROb
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-admin.hashicorptest.com=1/5dMSZVNdQscVq3on5V38iBrG9sP2TYRlbj3TDMJHKEvoBxl_QW-zl-L7a8lk-FU-
 __END__
