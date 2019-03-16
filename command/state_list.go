3d3bbdf1ce02fa5e59bce70da0e085911514a0df
Martin Atkins <mart@degeneration.co.uk>
2019-03-15 19:51:26 -0700

    command: Fix various issues in the "terraform state ..." subcommands
    In earlier refactoring we updated these commands to support the new
    address and state types, but attempted to partially retain the old-style
    "StateFilter" abstraction that originally lived in the Terraform package,
    even though that was no longer being used for any other functionality.
    
    Unfortunately the adaptation of the existing filtering to the new types
    wasn't exact and so these commands ended up having a few bugs that were
    not covered by the existing tests.
    
    Since the old StateFilter behavior was the source of various misbehavior
    anyway, here it's removed altogether and replaced with some simpler
    functions in the state_meta.go file that are tailored to the use-cases of
    these sub-commands.
    
    As well as just generally behaving more consistently with the other
    parts of Terraform that use the new resource address types, this commit
    fixes the following bugs:
    
    - A resource address of aws_instance.foo would previously match an
      resource of that type and name in any module, which disagreed with the
      expected interpretation elsewhere of meaning a single resource in the
      root module.
    
    - The "terraform state mv" command was not supporting moves from a single
      resource address to an indexed address and vice-versa, because the old
      logic didn't need to make that distinction while they are two separate
      address types in the new logic. Now we allow resources that do not have
      count/for_each to be treated as if they are instances for the purposes
      of this command, which is a better match for likely user intent and for
      the old behavior.
    
    Finally, we also clean up a little some of the usage output from these
    commands, which hasn't been updated for some time and so had both some
    stale information and some inaccurate terminology.

--- command/state_list.go
+++ command/state_list.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hashicorp/terraform/addrs"
 	"github.com/hashicorp/terraform/states"
+	"github.com/hashicorp/terraform/tfdiags"
 	"github.com/mitchellh/cli"
 )
 
@@ -54,49 +56,61 @@ func (c *StateListCommand) Run(args []string) int {
 		return 1
 	}
 
-	filter := &states.Filter{State: state}
-	results, err := filter.Filter(args...)
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(errStateFilter, err))
-		return cli.RunResultHelp
+	var addrs []addrs.AbsResourceInstance
+	var diags tfdiags.Diagnostics
+	if len(args) == 0 {
+		addrs, diags = c.lookupAllResourceInstanceAddrs(state)
+	} else {
+		addrs, diags = c.lookupResourceInstanceAddrs(state, args...)
+	}
+	if diags.HasErrors() {
+		c.showDiagnostics(diags)
+		return 1
 	}
 
-	for _, result := range results {
-		if is, ok := result.Value.(*states.ResourceInstance); ok {
+	for _, addr := range addrs {
+		if is := state.ResourceInstance(addr); is != nil {
 			if *lookupId == "" || *lookupId == states.LegacyInstanceObjectID(is.Current) {
-				c.Ui.Output(result.Address.String())
+				c.Ui.Output(addr.String())
 			}
 		}
 	}
+
+	c.showDiagnostics(diags)
 
 	return 0
 }
 
 func (c *StateListCommand) Help() string {
 	helpText := `
-Usage: terraform state list [options] [pattern...]
+Usage: terraform state list [options] [address...]
 
   List resources in the Terraform state.
 
-  This command lists resources in the Terraform state. The pattern argument
-  can be used to filter the resources by resource or module. If no pattern
-  is given, all resources are listed.
+  This command lists resource instances in the Terraform state. The address
+  argument can be used to filter the instances by resource or module. If
+  no pattern is given, all resource instances are listed.
 
-  The pattern argument is meant to provide very simple filtering. For
-  advanced filtering, please use tools such as "grep". The output of this
-  command is designed to be friendly for this usage.
+  The addresses must either be module addresses or absolute resource
+  addresses, such as:
+      aws_instance.example
+      module.example
+      module.example.module.child
+      module.example.aws_instance.example
 
-  The pattern argument accepts any resource targeting syntax. Please
-  refer to the documentation on resource targeting syntax for more
-  information.
+  An error will be returned if any of the resources or modules given as
+  filter addresses do not exist in the state.
 
 Options:
 
   -state=statefile    Path to a Terraform state file to use to look
-                      up Terraform-managed resources. By default it will
-                      use the state "terraform.tfstate" if it exists.
+                      up Terraform-managed resources. By default, Terraform
+                      will consult the state of the currently-selected
+                      workspace.
 
-  -id=ID              Restricts the output to objects whose id is ID.
+  -id=ID              Filters the results to include only instances whose
+                      resource types have an attribute named "id" whose value
+                      equals the given id string.
 
 `
 	return strings.TrimSpace(helpText)
