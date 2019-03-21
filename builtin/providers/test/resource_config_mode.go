4de1e6007eeb0dee5e8f744da84f6fa65ab7d21c
Martin Atkins <mart@degeneration.co.uk>
2019-03-21 11:59:53 -0700

    helper/plugin: Implement Schema.SkipCoreTypeCheck
    The previous commit added this flag but did not implement it. Here we
    implement it by adjusting the shape of schema we return to Terraform Core
    to mark the attribute as untyped and then ensure that gets handled
    correctly on the SDK side.

--- builtin/providers/test/resource_config_mode.go
+++ builtin/providers/test/resource_config_mode.go
@@ -27,6 +27,21 @@ func testResourceConfigMode() *schema.Resource {
 					},
 				},
 			},
+			"resource_as_attr_dynamic": {
+				Type:              schema.TypeList,
+				ConfigMode:        schema.SchemaConfigModeAttr,
+				SkipCoreTypeCheck: true,
+				Optional:          true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"foo": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "default",
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -37,13 +52,14 @@ func testResourceConfigModeCreate(d *schema.ResourceData, meta interface{}) erro
 }
 
 func testResourceConfigModeRead(d *schema.ResourceData, meta interface{}) error {
-	const k = "resource_as_attr"
-	if l, ok := d.Get(k).([]interface{}); !ok {
-		return fmt.Errorf("%s should appear as []interface{}, not %T", k, l)
-	} else {
-		for i, item := range l {
-			if _, ok := item.(map[string]interface{}); !ok {
-				return fmt.Errorf("%s[%d] should appear as map[string]interface{}, not %T", k, i, item)
+	for _, k := range []string{"resource_as_attr", "resource_as_attr_dynamic"} {
+		if l, ok := d.Get(k).([]interface{}); !ok {
+			return fmt.Errorf("%s should appear as []interface{}, not %T", k, l)
+		} else {
+			for i, item := range l {
+				if _, ok := item.(map[string]interface{}); !ok {
+					return fmt.Errorf("%s[%d] should appear as map[string]interface{}, not %T", k, i, item)
+				}
 			}
 		}
 	}
