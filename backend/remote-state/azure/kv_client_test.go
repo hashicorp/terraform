23e75dda7a2116ff8311ae648fb2e7c4a27dd1ad
Allan Targino <allan_targino@hotmail.com>
2019-06-01 12:16:37 -0300

    fix: removing unused dependencies from file

--- backend/remote-state/azure/kv_client_test.go
+++ backend/remote-state/azure/kv_client_test.go
@@ -2,15 +2,13 @@ package azure
 
 import (
 	"context"
-	"crypto/rsa"
-	"crypto/sha512"
 	"fmt"
 	"reflect"
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
+	"github.com/hashicorp/terraform/helper/acctest"
 )
 
 // Unit Tests
@@ -101,39 +99,39 @@ func TestCreateEncryptClientInvalid(t *testing.T) {
 	}
 }
 
-func TestGeneratePublicKeyFromKeyBundle(t *testing.T){
+func TestGeneratePublicKeyFromKeyBundle(t *testing.T) {
 	testAccAzureBackend(t)
 
 	cases := []struct {
-		n      string
-		e      string
+		n        string
+		e        string
 		sizeBits int
 	}{
 		{
-			n : "t8zB6yX6fIyloMqhfW07ihq0x22OdyW5h2gX2QjCN6O5cko21at_7f2tCjrBdO1bJs_Zr5nIjWKU3vJTGCJNijfrOMNIoamMxJumRpDraDiYD6e41CVw-oTHW4l8Yb_k81Dph9Ojfar9FlIJ-hTsIKkNb2laeLDgYqMKmtJIoXEsMUKKqzzC5-fDt_Q5Tj-ETJoNDiSgvmbfmwJDf2r2wIES_UJ8jY2sRcaa9YaHmgsA-NCHDH2Rs4ssU4f7TaEuyTsUQlNifKrME0AtKKv8do23g8_BTbIu1uFCGDxG-kBt83mCn27raT3lbo9P6ZKJmyGRAHIhJehA_2v3vpC30Q",
-			e : "AQAB",
-			sizeBits : 2048,
+			n:        "t8zB6yX6fIyloMqhfW07ihq0x22OdyW5h2gX2QjCN6O5cko21at_7f2tCjrBdO1bJs_Zr5nIjWKU3vJTGCJNijfrOMNIoamMxJumRpDraDiYD6e41CVw-oTHW4l8Yb_k81Dph9Ojfar9FlIJ-hTsIKkNb2laeLDgYqMKmtJIoXEsMUKKqzzC5-fDt_Q5Tj-ETJoNDiSgvmbfmwJDf2r2wIES_UJ8jY2sRcaa9YaHmgsA-NCHDH2Rs4ssU4f7TaEuyTsUQlNifKrME0AtKKv8do23g8_BTbIu1uFCGDxG-kBt83mCn27raT3lbo9P6ZKJmyGRAHIhJehA_2v3vpC30Q",
+			e:        "AQAB",
+			sizeBits: 2048,
 		},
 		{
-			n : "m9rXO6iYmBMx-57d-r1DlsNBnFLRPyifD2Ax7d6-W4AqyvTZ9Ac5aII4ds06eKH7bHg7fxIrnznKd3tiA4WhRctonPx2TJLQ95m_VbCArdkmIctfH-Rn_-Jg5ynj9kUgJUFNhRHw3DJEwI9aV6zogiuTtO47J5Nwg6I9G4BzRpOu7Hkp5SrA4XUwr9ZTy3wJJFee3O7AfCtoi196JsnvZxsNismIoQoO4R0eYzQQC-H1tjSwIlvfFJNQA2PRG5Ev3Tkk4rrAsO0rvSvjMC1am321P2acpW5gan1lXNJbChzY2fCLTysFtQVGQWgWSePNE1NA3jppsr5JA2moCq_g7hfEqAFuz4leqqbGZwVxz7rDwYFL-yQC7rffV4dy4qAIlTWLNRBXNUXgJXvJdABAbdWkxP7HSTvdWz92mpSpztcdC2Sx_wY_kZO5SCMdw6p3iCJOVKOuYqfOmACinc7Q-X190XVel3OGZdTTBGfPYS6vdRfKi1TZHr2FO69FNbdL",
-			e : "AQAB",
-			sizeBits : 3072,
+			n:        "m9rXO6iYmBMx-57d-r1DlsNBnFLRPyifD2Ax7d6-W4AqyvTZ9Ac5aII4ds06eKH7bHg7fxIrnznKd3tiA4WhRctonPx2TJLQ95m_VbCArdkmIctfH-Rn_-Jg5ynj9kUgJUFNhRHw3DJEwI9aV6zogiuTtO47J5Nwg6I9G4BzRpOu7Hkp5SrA4XUwr9ZTy3wJJFee3O7AfCtoi196JsnvZxsNismIoQoO4R0eYzQQC-H1tjSwIlvfFJNQA2PRG5Ev3Tkk4rrAsO0rvSvjMC1am321P2acpW5gan1lXNJbChzY2fCLTysFtQVGQWgWSePNE1NA3jppsr5JA2moCq_g7hfEqAFuz4leqqbGZwVxz7rDwYFL-yQC7rffV4dy4qAIlTWLNRBXNUXgJXvJdABAbdWkxP7HSTvdWz92mpSpztcdC2Sx_wY_kZO5SCMdw6p3iCJOVKOuYqfOmACinc7Q-X190XVel3OGZdTTBGfPYS6vdRfKi1TZHr2FO69FNbdL",
+			e:        "AQAB",
+			sizeBits: 3072,
 		},
 		{
-			n : "9aICHHp3OzKUU9ttEfKj14956PVhC_XWThJhFHVqILUxretGORdch13AeuhVf0qYMHedeVkDBehU24rn_9oc11rXxYFUEjAsrk-uhJBVmGr3ZCFwylle0ZQ-fHIigPhYauPz2g87fQsLenfyxl4r2i5xfQun2c4hHFlsaUanhX4P4m8pQTi3hRM-q1ZeP3dF9NmIAmrd6pSQ9MpCalFdp4Ic71kpHl45McHEV40DeivWJD4wnugF35LQcvOmTjnEiHkp_OyaYJ7hKhGiLd2NL0NlIerLe9-QjIgY4aqmgIzN4Er9d6sKaHS3QVW9ocj08WwRHVOhQTZrCGU6rUei_ygRZKESFZ_Jd8IDBoD9Vnza1xSerGntRsooOZvuexcFlRpvspGB0zPchygWiF7QTQvVwjTBqiUCP7DUyUTg9sgpNf6B3T-LxMzuIH4rQxlhnxiYjPwQtDAQrIFNjZLWqN_uor7mvaH9kuy01NElCa2Pijy4sLa9qxfq37g0L7y7jJLtcxWhOlhN2jfCWsmCMIGU5n4g08Tc-rf1bQBHCNyYohgPUDWdj0PYnas4nZ3fX1sijOwKpyZIpTgd-OoyTfjcrNVsnmVKT6xt6dBqS1bz53CeOgUahsaubcvflt68hAT09KyQTyG8aH5cb0nNICf5EZnVctAklP4x09Qv2tk",
-			e : "AQAB",
-			sizeBits : 4096,
+			n:        "9aICHHp3OzKUU9ttEfKj14956PVhC_XWThJhFHVqILUxretGORdch13AeuhVf0qYMHedeVkDBehU24rn_9oc11rXxYFUEjAsrk-uhJBVmGr3ZCFwylle0ZQ-fHIigPhYauPz2g87fQsLenfyxl4r2i5xfQun2c4hHFlsaUanhX4P4m8pQTi3hRM-q1ZeP3dF9NmIAmrd6pSQ9MpCalFdp4Ic71kpHl45McHEV40DeivWJD4wnugF35LQcvOmTjnEiHkp_OyaYJ7hKhGiLd2NL0NlIerLe9-QjIgY4aqmgIzN4Er9d6sKaHS3QVW9ocj08WwRHVOhQTZrCGU6rUei_ygRZKESFZ_Jd8IDBoD9Vnza1xSerGntRsooOZvuexcFlRpvspGB0zPchygWiF7QTQvVwjTBqiUCP7DUyUTg9sgpNf6B3T-LxMzuIH4rQxlhnxiYjPwQtDAQrIFNjZLWqN_uor7mvaH9kuy01NElCa2Pijy4sLa9qxfq37g0L7y7jJLtcxWhOlhN2jfCWsmCMIGU5n4g08Tc-rf1bQBHCNyYohgPUDWdj0PYnas4nZ3fX1sijOwKpyZIpTgd-OoyTfjcrNVsnmVKT6xt6dBqS1bz53CeOgUahsaubcvflt68hAT09KyQTyG8aH5cb0nNICf5EZnVctAklP4x09Qv2tk",
+			e:        "AQAB",
+			sizeBits: 4096,
 		},
 	}
 
 	for _, k := range cases {
 		keyBundle := &keyvault.KeyBundle{
 			Key: &keyvault.JSONWebKey{
-				N:      &k.n,
-				E:      &k.e,
+				N: &k.n,
+				E: &k.e,
 			},
 		}
-	
+
 		pubKey, err := generatePublicKeyFromKeyBundle(keyBundle)
 		if err != nil {
 			t.Errorf("Error when generating public key. Got an error: %v", err)
@@ -143,7 +141,7 @@ func TestGeneratePublicKeyFromKeyBundle(t *testing.T){
 			t.Errorf("Error when validating public key. Expected: %v Got: %v", 65537, pubKey.E)
 		}
 
-		size := pubKey.Size() *8
+		size := pubKey.Size() * 8
 		if size != k.sizeBits {
 			t.Errorf("Error when validating public key size. Expected: %v Got: %v", k.sizeBits, size)
 		}
@@ -369,15 +367,14 @@ func testGetKeyVaultAlgorithmParametersWithAlgorithm(t *testing.T, algorithm key
 	}
 }
 
-
 // Integration Tests
 
-func TestKeyVaultEncryption(t * testing.T){
+func TestKeyVaultEncryption(t *testing.T) {
 	testAccAzureBackend(t)
 
 	ctx := context.TODO()
 	rs := acctest.RandString(4)
-	
+
 	keyVaultName := fmt.Sprintf("keyvaultterraform%s", rs)
 	keyName := "myKey"
 	keyIdentifier := fmt.Sprintf("https://%s.vault.azure.net/keys/%s", keyVaultName, keyName)
@@ -404,8 +401,8 @@ func TestKeyVaultEncryption(t * testing.T){
 		if err != nil {
 			t.Fatalf("Error when getting key details: %v", err)
 		}
-	
-		if !strings.Contains(*details.Key.Kid, keyIdentifier){
+
+		if !strings.Contains(*details.Key.Kid, keyIdentifier) {
 			t.Fatalf("Key details was not correct. Expected: %v, Got: %v", keyIdentifier, *details.Key.Kid)
 		}
 	})
