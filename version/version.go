36d9a1e4e579eb11aab8fde0cc9b4f35f823c5f6
Pam Selle <pam@hashicorp.com>
2019-07-17 12:01:27 -0400

    Update version.go

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 // The main version number that is being run at the moment.
-var Version = "0.12.4"
+var Version = "0.12.5"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
