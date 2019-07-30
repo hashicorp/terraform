27335a2536c13bb994b9fb610c9c75c675a48d2e
Martin Atkins <mart@degeneration.co.uk>
2019-07-30 16:05:27 -0700

    svchost/auth: store and forget operations for helper programs
    This introduces two new verbs to the credentials helper protocol to store
    and forget credentials, and uses them to implement StoreForHost and
    ForgetForHost.

--- svchost/auth/helper_program.go
+++ svchost/auth/helper_program.go
@@ -7,6 +7,8 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	ctyjson "github.com/zclconf/go-cty/cty/json"
+
 	"github.com/hashicorp/terraform/svchost"
 )
 
@@ -80,9 +82,68 @@ func (s *helperProgramCredentialsSource) ForHost(host svchost.Hostname) (HostCre
 }
 
 func (s *helperProgramCredentialsSource) StoreForHost(host svchost.Hostname, credentials HostCredentialsWritable) error {
-	return fmt.Errorf("credentials helper cannot currently store new credentials")
+	args := make([]string, len(s.args), len(s.args)+2)
+	copy(args, s.args)
+	args = append(args, "store")
+	args = append(args, string(host))
+
+	toStore := credentials.ToStore()
+	toStoreRaw, err := ctyjson.Marshal(toStore, toStore.Type())
+	if err != nil {
+		return fmt.Errorf("can't serialize credentials to store: %s", err)
+	}
+
+	inReader := bytes.NewReader(toStoreRaw)
+	errBuf := bytes.Buffer{}
+
+	cmd := exec.Cmd{
+		Path:   s.executable,
+		Args:   args,
+		Stdin:  inReader,
+		Stderr: &errBuf,
+		Stdout: nil,
+	}
+	err = cmd.Run()
+	if _, isExitErr := err.(*exec.ExitError); isExitErr {
+		errText := errBuf.String()
+		if errText == "" {
+			// Shouldn't happen for a well-behaved helper program
+			return fmt.Errorf("error in %s, but it produced no error message", s.executable)
+		}
+		return fmt.Errorf("error in %s: %s", s.executable, errText)
+	} else if err != nil {
+		return fmt.Errorf("failed to run %s: %s", s.executable, err)
+	}
+
+	return nil
 }
 
 func (s *helperProgramCredentialsSource) ForgetForHost(host svchost.Hostname) error {
-	return fmt.Errorf("credentials helper cannot currently forget existing credentials")
+	args := make([]string, len(s.args), len(s.args)+2)
+	copy(args, s.args)
+	args = append(args, "forget")
+	args = append(args, string(host))
+
+	errBuf := bytes.Buffer{}
+
+	cmd := exec.Cmd{
+		Path:   s.executable,
+		Args:   args,
+		Stdin:  nil,
+		Stderr: &errBuf,
+		Stdout: nil,
+	}
+	err := cmd.Run()
+	if _, isExitErr := err.(*exec.ExitError); isExitErr {
+		errText := errBuf.String()
+		if errText == "" {
+			// Shouldn't happen for a well-behaved helper program
+			return fmt.Errorf("error in %s, but it produced no error message", s.executable)
+		}
+		return fmt.Errorf("error in %s: %s", s.executable, errText)
+	} else if err != nil {
+		return fmt.Errorf("failed to run %s: %s", s.executable, err)
+	}
+
+	return nil
 }
