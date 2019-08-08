07d62897014f912f6425bc5347f9473d07a90a3c
Alex Pilon <apilon@hashicorp.com>
2019-08-08 11:14:26 -0400

    add comment noting deprecated type

--- terraform/resource_mode.go
+++ terraform/resource_mode.go
@@ -1,6 +1,9 @@
 package terraform
 
 //go:generate stringer -type=ResourceMode -output=resource_mode_string.go resource_mode.go
+
+// ResourceMode is deprecated, use addrs.ResourceMode instead.
+// It has been preserved for backwards compatibility.
 type ResourceMode int
 
 const (
