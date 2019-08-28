fd784ea60b07e0bf49eb758be32057e57c822a30
Brian Flad <bflad417@gmail.com>
2019-08-28 11:54:04 -0400

    lang/funcs: Update fileset() function to include path as separate first argument, automatically trim the path argument from results, and ensure results are always canonical with forward slash path separators
    Reference: https://github.com/hashicorp/terraform/pull/22523#pullrequestreview-279694703
    
    These changes center around better function usability and consistency with other functions. The function has not yet been released, so these breaking changes can be applied safely.

--- lang/funcs/filesystem_test.go
+++ lang/funcs/filesystem_test.go
@@ -226,36 +226,43 @@ func TestFileExists(t *testing.T) {
 
 func TestFileSet(t *testing.T) {
 	tests := []struct {
+		Path    cty.Value
 		Pattern cty.Value
 		Want    cty.Value
 		Err     bool
 	}{
 		{
-			cty.StringVal("testdata/missing"),
-			cty.SetValEmpty(cty.String),
-			false,
-		},
-		{
-			cty.StringVal("testdata/missing*"),
-			cty.SetValEmpty(cty.String),
-			false,
-		},
-		{
-			cty.StringVal("*/missing"),
-			cty.SetValEmpty(cty.String),
-			false,
-		},
-		{
-			cty.StringVal("testdata"),
-			cty.SetValEmpty(cty.String),
-			false,
-		},
-		{
+			cty.StringVal("."),
 			cty.StringVal("testdata*"),
 			cty.SetValEmpty(cty.String),
 			false,
 		},
 		{
+			cty.StringVal("."),
+			cty.StringVal("testdata"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("."),
+			cty.StringVal("testdata/missing"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("."),
+			cty.StringVal("testdata/missing*"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("."),
+			cty.StringVal("*/missing"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("."),
 			cty.StringVal("testdata/*.txt"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.txt"),
@@ -263,6 +270,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("testdata/hello.txt"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.txt"),
@@ -270,6 +278,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("testdata/hello.???"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.txt"),
@@ -277,6 +286,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("testdata/hello*"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.tmpl"),
@@ -285,6 +295,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("*/hello.txt"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.txt"),
@@ -292,6 +303,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("*/*.txt"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.txt"),
@@ -299,6 +311,7 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("*/hello*"),
 			cty.SetVal([]cty.Value{
 				cty.StringVal("testdata/hello.tmpl"),
@@ -307,20 +320,67 @@ func TestFileSet(t *testing.T) {
 			false,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("["),
 			cty.SetValEmpty(cty.String),
 			true,
 		},
 		{
+			cty.StringVal("."),
 			cty.StringVal("\\"),
 			cty.SetValEmpty(cty.String),
 			true,
 		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("missing"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("missing*"),
+			cty.SetValEmpty(cty.String),
+			false,
+		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("*.txt"),
+			cty.SetVal([]cty.Value{
+				cty.StringVal("hello.txt"),
+			}),
+			false,
+		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("hello.txt"),
+			cty.SetVal([]cty.Value{
+				cty.StringVal("hello.txt"),
+			}),
+			false,
+		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("hello.???"),
+			cty.SetVal([]cty.Value{
+				cty.StringVal("hello.txt"),
+			}),
+			false,
+		},
+		{
+			cty.StringVal("testdata"),
+			cty.StringVal("hello*"),
+			cty.SetVal([]cty.Value{
+				cty.StringVal("hello.tmpl"),
+				cty.StringVal("hello.txt"),
+			}),
+			false,
+		},
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("FileSet(\".\", %#v)", test.Pattern), func(t *testing.T) {
-			got, err := FileSet(".", test.Pattern)
+		t.Run(fmt.Sprintf("FileSet(\".\", %#v, %#v)", test.Path, test.Pattern), func(t *testing.T) {
+			got, err := FileSet(".", test.Path, test.Pattern)
 
 			if test.Err {
 				if err == nil {
