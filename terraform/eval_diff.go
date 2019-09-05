96d84da0328c86d18d1c36364d0fe523e2949005
Pam Selle <204372+pselle@users.noreply.github.com>
2019-09-05 15:43:50 -0400

    Do ignore changes processing again after plan, in case strange things are done during plan that should not be

--- terraform/eval_diff.go
+++ terraform/eval_diff.go
@@ -214,6 +214,12 @@ func (n *EvalDiff) Eval(ctx EvalContext) (interface{}, error) {
 	plannedNewVal := resp.PlannedState
 	plannedPrivate := resp.PlannedPrivate
 
+	plannedNewVal, ignoreChangeDiags = n.processIgnoreChanges(priorVal, plannedNewVal)
+	diags = diags.Append(ignoreChangeDiags)
+	if ignoreChangeDiags.HasErrors() {
+		return nil, diags.Err()
+	}
+
 	if plannedNewVal == cty.NilVal {
 		// Should never happen. Since real-world providers return via RPC a nil
 		// is always a bug in the client-side stub. This is more likely caused
