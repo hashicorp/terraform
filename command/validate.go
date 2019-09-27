94d44888abd5f66a2ee5bcb86312c61977ff21f9
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-27 16:39:20 -0700

    website / help: reconcile 'validate' command docs

--- command/validate.go
+++ command/validate.go
@@ -230,10 +230,10 @@ Usage: terraform validate [options] [dir]
   configuration and not accessing any remote services such as remote state,
   provider APIs, etc.
 
-  Validate runs checks that verify whether a configuration is
-  internally-consistent, regardless of any provided variables or existing
-  state. It is thus primarily useful for general verification of reusable
-  modules, including correctness of attribute names and value types.
+  Validate runs checks that verify whether a configuration is syntactically
+  valid and internally consistent, regardless of any provided variables or
+  existing state. It is thus primarily useful for general verification of
+  reusable modules, including correctness of attribute names and value types.
 
   It is safe to run this command automatically, for example as a post-save
   check in a text editor or as a test step for a re-usable module in a CI
@@ -247,14 +247,16 @@ Usage: terraform validate [options] [dir]
   If dir is not specified, then the current directory will be used.
 
   To verify configuration in the context of a particular run (a particular
-  target workspace, operation variables, etc), use the terraform plan
-  subcommand instead, which includes an implied validation check.
+  target workspace, input variable values, etc), use the 'terraform plan'
+  command instead, which includes an implied validation check.
 
 Options:
 
   -json        Produce output in a machine-readable JSON format, suitable for
-               use in e.g. text editor integrations.
+               use in text editor integrations and other automated systems.
+               Always disables color.
 
+  -no-color    If specified, output won't contain any color.
 `
 	return strings.TrimSpace(helpText)
 }
