0235e3cd9aca15472f5c8f051f956d0a38cef51e
abejidefemi1@gmail.com <abejidefemi1@gmail.com>
2019-10-03 14:17:45 +0100

    refactor plugins file for conciseness

--- plugins.go
+++ plugins.go
@@ -13,16 +13,17 @@ import (
 // Earlier entries in this slice get priority over later when multiple copies
 // of the same plugin version are found, but newer versions always override
 // older versions where both satisfy the provider version constraints.
+const plugins = "plugins"
+
 func globalPluginDirs() []string {
 	var ret []string
 	// Look in ~/.terraform.d/plugins/ , or its equivalent on non-UNIX
-	dir, err := ConfigDir()
-	if err != nil {
+	if dir, err := ConfigDir(); err != nil {
 		log.Printf("[ERROR] Error finding global config directory: %s", err)
 	} else {
 		machineDir := fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH)
-		ret = append(ret, filepath.Join(dir, "plugins"))
-		ret = append(ret, filepath.Join(dir, "plugins", machineDir))
+		ret = append(ret, filepath.Join(dir, plugins))
+		ret = append(ret, filepath.Join(dir, plugins, machineDir))
 	}
 
 	return ret
