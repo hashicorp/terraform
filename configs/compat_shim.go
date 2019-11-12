134c67192bf25e8075a44e126153f842d51e060d
Martin Atkins <mart@degeneration.co.uk>
2019-11-12 12:10:29 -0800

    configs: Warn for deprecated interpolation and quoted type constraints
    Following on from de652e22a26b, this introduces deprecation warnings for
    when an attribute value expression is a template with only a single
    interpolation sequence, and for variable type constraints given in quotes.
    
    As with the previous commit, we allowed these deprecated forms with no
    warning for a few releases after v0.12.0 to ensure that folks who need to
    write cross-compatible modules for a while during upgrading would be able
    to do so, but we're now marking these as explicitly deprecated to guide
    users towards the new idiomatic forms.
    
    The "terraform 0.12upgrade" tool would've already updated configurations
    to not hit these warnings for those who had pre-existing configurations
    written for Terraform 0.11.
    
    The main target audience for these warnings are newcomers to Terraform who
    are learning from existing examples already published in various spots on
    the wider internet that may be showing older Terraform syntax, since those
    folks will not be running their configurations through the upgrade tool.
    These warnings will hopefully guide them towards modern Terraform usage
    during their initial experimentation, and thus reduce the chances of
    inadvertently adopting the less-readable legacy usage patterns in
    greenfield projects.

--- configs/compat_shim.go
+++ configs/compat_shim.go
@@ -107,3 +107,58 @@ func shimIsIgnoreChangesStar(expr hcl.Expression) bool {
 	}
 	return val.AsString() == "*"
 }
+
+// warnForDeprecatedInterpolations returns warning diagnostics if the given
+// body can be proven to contain attributes whose expressions are native
+// syntax expressions consisting entirely of a single template interpolation,
+// which is a deprecated way to include a non-literal value in configuration.
+//
+// This is a best-effort sort of thing which relies on the physical HCL native
+// syntax AST, so it might not catch everything. The main goal is to catch the
+// "obvious" cases in order to help spread awareness that this old form is
+// deprecated, when folks copy it from older examples they've found on the
+// internet that were written for Terraform 0.11 or earlier.
+func warnForDeprecatedInterpolationsInBody(body hcl.Body) hcl.Diagnostics {
+	var diags hcl.Diagnostics
+
+	nativeBody, ok := body.(*hclsyntax.Body)
+	if !ok {
+		// If it's not native syntax then we've nothing to do here.
+		return diags
+	}
+
+	for _, attr := range nativeBody.Attributes {
+		moreDiags := warnForDeprecatedInterpolationsInExpr(attr.Expr)
+		diags = append(diags, moreDiags...)
+	}
+
+	for _, block := range nativeBody.Blocks {
+		// We'll also go hunting in nested blocks
+		moreDiags := warnForDeprecatedInterpolationsInBody(block.Body)
+		diags = append(diags, moreDiags...)
+	}
+
+	return diags
+}
+
+func warnForDeprecatedInterpolationsInExpr(expr hcl.Expression) hcl.Diagnostics {
+	var diags hcl.Diagnostics
+
+	if _, ok := expr.(*hclsyntax.TemplateWrapExpr); !ok {
+		// We're only interested in TemplateWrapExpr, because that's how
+		// the HCL native syntax parser represents the case of a template
+		// that consists entirely of a single interpolation expression, which
+		// is therefore subject to the special case of passing through the
+		// inner value without conversion to string.
+		return diags
+	}
+
+	diags = append(diags, &hcl.Diagnostic{
+		Severity: hcl.DiagWarning,
+		Summary:  "Interpolation-only expressions are deprecated",
+		Detail:   "Terraform 0.11 and earlier required all non-constant expressions to be provided via interpolation syntax, but this pattern is now deprecated. To silence this warning, remove the \"${ sequence from the start and the }\" sequence from the end of this expression, leaving just the inner expression.\n\nTemplate interpolation syntax is still used to construct strings from expressions when the template includes multiple interpolation sequences or a mixture of literal strings and interpolations. This deprecation applies only to templates that consist entirely of a single interpolation sequence.",
+		Subject:  expr.Range().Ptr(),
+	})
+
+	return diags
+}
