c7d41a9e610ff5301a6b664823ba2777297b0b01
Martin Atkins <mart@degeneration.co.uk>
2019-11-12 12:10:29 -0800

    configs: Warn for deprecated interpolation and quoted type constraints
    Following on from de652e22a26b, this introduces deprecation warnings for
    when an attribute value expression is a template with only a single
    interpolation sequence, and for variable type constraints given in quotes.
    
    As with the previous commit, we allowed these deprecated forms with no
    warning for a few releases after v0.12.0 to ensure that folks who need to
    write cross-compatible modules for a while during upgrading would be able
    to do so, but we're now marking these as explicitly deprecated to guide
    users towards the new idiomatic forms.
    
    The "terraform 0.12upgrade" tool would've already updated configurations
    to not hit these warnings for those who had pre-existing configurations
    written for Terraform 0.11.
    
    The main target audience for these warnings are newcomers to Terraform who
    are learning from existing examples already published in various spots on
    the wider internet that may be showing older Terraform syntax, since those
    folks will not be running their configurations through the upgrade tool.
    These warnings will hopefully guide them towards modern Terraform usage
    during their initial experimentation, and thus reduce the chances of
    inadvertently adopting the less-readable legacy usage patterns in
    greenfield projects.

--- configs/named_values.go
+++ configs/named_values.go
@@ -138,10 +138,28 @@ func decodeVariableType(expr hcl.Expression) (cty.Type, VariableParsingMode, hcl
 		str := val.AsString()
 		switch str {
 		case "string":
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagWarning,
+				Summary:  "Quoted type constraints are deprecated",
+				Detail:   "Terraform 0.11 and earlier required type constraints to be given in quotes, but that form is now deprecated and will be removed in a future version of Terraform. To silence this warning, remove the quotes around \"string\".",
+				Subject:  expr.Range().Ptr(),
+			})
 			return cty.String, VariableParseLiteral, diags
 		case "list":
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagWarning,
+				Summary:  "Quoted type constraints are deprecated",
+				Detail:   "Terraform 0.11 and earlier required type constraints to be given in quotes, but that form is now deprecated and will be removed in a future version of Terraform. To silence this warning, remove the quotes around \"list\" and write list(string) instead to explicitly indicate that the list elements are strings.",
+				Subject:  expr.Range().Ptr(),
+			})
 			return cty.List(cty.DynamicPseudoType), VariableParseHCL, diags
 		case "map":
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagWarning,
+				Summary:  "Quoted type constraints are deprecated",
+				Detail:   "Terraform 0.11 and earlier required type constraints to be given in quotes, but that form is now deprecated and will be removed in a future version of Terraform. To silence this warning, remove the quotes around \"map\" and write map(string) instead to explicitly indicate that the map elements are strings.",
+				Subject:  expr.Range().Ptr(),
+			})
 			return cty.Map(cty.DynamicPseudoType), VariableParseHCL, diags
 		default:
 			return cty.DynamicPseudoType, VariableParseHCL, hcl.Diagnostics{{
