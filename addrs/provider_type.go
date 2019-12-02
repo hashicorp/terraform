6728e521c14fe42c554cb5e91abcd6603c585d2d
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2019-12-02 15:32:31 -0500

    addrs: rename Provider Name to more accurate Provider Type (#23449)

--- addrs/provider_type.go
+++ addrs/provider_type.go
@@ -3,5 +3,5 @@ package addrs
 // ProviderType encapsulates a single provider type. In the future this will be
 // extended to include additional fields including Namespace and SourceHost
 type ProviderType struct {
-	Name string
+	Type string
 }
