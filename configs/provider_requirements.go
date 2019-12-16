a2006f4187914b9b9490d9cb5ed17d22764e7ec9
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2019-12-16 09:38:30 -0500

    configs: add provider fqn to module.ProviderRequirements
    A configs.File includes all `provider` config blocks as well as any
    `required_providers` blocks. These are merged into
    `ProviderRequirements` in a configs.Module.
    
    This PR slightly renames types so it is very clear that the information
    in the struct previously named 'ProviderRequirements' is solely the
    contents of `required_providers`, which "providerRequirements" in a
    module contains the union of provider requirements from both "provider"
    and "required_providers" blocks.

--- configs/provider_requirements.go
+++ configs/provider_requirements.go
@@ -3,33 +3,36 @@ package configs
 import (
 	version "github.com/hashicorp/go-version"
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/terraform/addrs"
 )
 
-// ProviderRequirement represents a declaration of a dependency on a particular
+// RequiredProvider represents a declaration of a dependency on a particular
 // provider version without actually configuring that provider. This is used in
 // child modules that expect a provider to be passed in from their parent.
 //
 // TODO: "Source" is a placeholder for an attribute that is not yet supported.
-type ProviderRequirement struct {
+type RequiredProvider struct {
 	Name        string
 	Source      string // TODO
 	Requirement VersionConstraint
 }
 
-func decodeRequiredProvidersBlock(block *hcl.Block) ([]*ProviderRequirement, hcl.Diagnostics) {
-	attrs, diags := block.Body.JustAttributes()
-	var reqs []*ProviderRequirement
-	for name, attr := range attrs {
-		expr, err := attr.Expr.Value(nil)
-		if err != nil {
-			diags = append(diags, err...)
-		}
+// ProviderRequirements represents merged provider version constraints.
+// VersionConstraints come from terraform.require_providers blocks and provider
+// blocks.
+type ProviderRequirements struct {
+	Type               addrs.Provider
+	VersionConstraints []VersionConstraint
+}
 
-		switch {
-		case expr.Type().IsPrimitiveType():
-			vc, reqDiags := decodeVersionConstraint(attr)
-			diags = append(diags, reqDiags...)
-			reqs = append(reqs, &ProviderRequirement{
+func decodeRequiredProvidersBlock(block *hcl.Block) ([]*RequiredProvider, hcl.Diagnostics) {
+	attrs, diags := block.Body.JustAttributes()
+	var reqs []*RequiredProvider
+	for name, attr := range attrs {
+		req, reqDiags := decodeVersionConstraint(attr)
+		diags = append(diags, reqDiags...)
+		if !diags.HasErrors() {
+			reqs = append(reqs, &RequiredProvider{
 				Name:        name,
 				Requirement: vc,
 			})
