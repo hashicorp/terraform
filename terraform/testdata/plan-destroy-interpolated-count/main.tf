6bad4e3dc03bbf43a31977c1c33f7e94e5882813
James Bardin <j.bardin@gmail.com>
2019-12-18 15:37:08 -0500

    add an interp that fails during planned destroy
    Chain some values so that we can get an interpolation that fails if a
    module input is evaluated during destroy.

--- terraform/testdata/plan-destroy-interpolated-count/main.tf
+++ terraform/testdata/plan-destroy-interpolated-count/main.tf
@@ -3,9 +3,18 @@ variable "list" {
 }
 
 resource "aws_instance" "a" {
-  count = "${length(var.list)}"
+  count = length(var.list)
+}
+
+locals {
+  ids = aws_instance.a[*].id
+}
+
+module "empty" {
+  source = "./mod"
+  input = zipmap(var.list, local.ids)
 }
 
 output "out" {
-  value = "${aws_instance.a.*.id}"
+  value = aws_instance.a[*].id
 }
