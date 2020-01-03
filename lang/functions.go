52bcce7f5c8c751dec0a6e6f3a1d928b7157bd03
Martin Atkins <mart@degeneration.co.uk>
2020-01-03 14:01:14 -0800

    lang: "try" and "can" functions
    These are intended to make it easier to work with arbitrary data
    structures whose shape might not be known statically, such as the result
    of jsondecode(...) or yamldecode(...) of data from a separate system.
    
    For example, in an object value which has attributes that may or may not
    be set we can concisely provide a fallback value to use when the attribute
    isn't set:
    
        try(local.example.foo, "fallback-foo")
    
    Using a "try to evaluate" model rather than explicit testing fits better
    with the usual programming model of the Terraform language where values
    are normally automatically converted to the necessary type where possible:
    the given expression is subject to all of the same normal type conversions,
    which avoids inadvertently creating a more restrictive evaluation model
    as might happen if this were handled using checks like a hypothetical
    isobject(...) function, etc.

--- lang/functions.go
+++ lang/functions.go
@@ -3,6 +3,7 @@ package lang
 import (
 	"fmt"
 
+	"github.com/hashicorp/hcl/v2/ext/tryfunc"
 	ctyyaml "github.com/zclconf/go-cty-yaml"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
@@ -39,6 +40,7 @@ func (s *Scope) Functions() map[string]function.Function {
 			"base64sha256":     funcs.Base64Sha256Func,
 			"base64sha512":     funcs.Base64Sha512Func,
 			"bcrypt":           funcs.BcryptFunc,
+			"can":              tryfunc.CanFunc,
 			"ceil":             funcs.CeilFunc,
 			"chomp":            funcs.ChompFunc,
 			"cidrhost":         funcs.CidrHostFunc,
@@ -122,6 +124,7 @@ func (s *Scope) Functions() map[string]function.Function {
 			"trimprefix":       funcs.TrimPrefixFunc,
 			"trimspace":        funcs.TrimSpaceFunc,
 			"trimsuffix":       funcs.TrimSuffixFunc,
+			"try":              tryfunc.TryFunc,
 			"upper":            stdlib.UpperFunc,
 			"urlencode":        funcs.URLEncodeFunc,
 			"uuid":             funcs.UUIDFunc,
