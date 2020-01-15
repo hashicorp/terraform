35107acc468e0b04ad75caa079660774581a004d
James Bardin <j.bardin@gmail.com>
2020-01-15 11:39:01 -0500

    allow "terraform" in destroy provisioners
    The values supplied in terraform are static, and do not create any
    dependencies.

--- configs/testdata/warning-files/destroy-provisioners.tf
+++ configs/testdata/warning-files/destroy-provisioners.tf
@@ -12,7 +12,11 @@ resource "null_resource" "a" {
     when = destroy
     index = count.index
     key = each.key
+
+    // path and terraform values are static, and do not create any
+    // dependencies.
     dir = path.module
+    workspace = terraform.workspace
   }
 }
 
