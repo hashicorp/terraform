76e5b446ba8a670811db8b5fbe6522fa1f5a13a6
Li Kexian <i@likexian.com>
2020-02-14 00:37:11 +0800

    backend/cos: Add TencentCloud backend cos with lock (#22540)
    * add TencentCloud COS backend for remote state
    
    * add vendor of dependence
    
    * fixed error not handle and remove default value for prefix argument
    
    * get appid from TF_COS_APPID environment variables

--- backend/init/init.go
+++ backend/init/init.go
@@ -16,6 +16,7 @@ import (
 	backendArtifactory "github.com/hashicorp/terraform/backend/remote-state/artifactory"
 	backendAzure "github.com/hashicorp/terraform/backend/remote-state/azure"
 	backendConsul "github.com/hashicorp/terraform/backend/remote-state/consul"
+	backendCos "github.com/hashicorp/terraform/backend/remote-state/cos"
 	backendEtcdv2 "github.com/hashicorp/terraform/backend/remote-state/etcdv2"
 	backendEtcdv3 "github.com/hashicorp/terraform/backend/remote-state/etcdv3"
 	backendGCS "github.com/hashicorp/terraform/backend/remote-state/gcs"
@@ -57,6 +58,7 @@ func Init(services *disco.Disco) {
 		"atlas":       func() backend.Backend { return backendAtlas.New() },
 		"azurerm":     func() backend.Backend { return backendAzure.New() },
 		"consul":      func() backend.Backend { return backendConsul.New() },
+		"cos":         func() backend.Backend { return backendCos.New() },
 		"etcd":        func() backend.Backend { return backendEtcdv2.New() },
 		"etcdv3":      func() backend.Backend { return backendEtcdv3.New() },
 		"gcs":         func() backend.Backend { return backendGCS.New() },
