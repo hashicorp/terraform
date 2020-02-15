86f0b5191c9083569e75f572eefd0920f497dab1
Martin Atkins <mart@degeneration.co.uk>
2020-02-14 18:10:03 -0800

    addrs: Stronger validation and normalization of provider namespace/type
    The provider FQN is becoming our primary identifier for a provider, so
    it's important that we are clear about the equality rules for these
    addresses and what characters are valid within them.
    
    We previously had a basic regex permitting ASCII letters and digits for
    validation and no normalization at all. We need to do at least case
    folding and UTF-8 normalization because these names will appear in file
    and directory names in case-insensitive filesystems and in repository
    names such as on GitHub.
    
    Since we're already using DNS-style normalization and validation rules
    for the hostname part, rather than defining an entirely new set of rules
    here we'll just treat the provider namespace and type as if they were
    single labels in a DNS name. Aside from some internal consistency, that
    also works out nicely because systems like GitHub use organization and
    repository names as part of hostnames (e.g. with GitHub Pages) and so
    tend to apply comparable constraints themselves.
    
    This introduces the possibility of names containing letters from alphabets
    other than the latin alphabet, and for latin letters with diacritics.
    That's consistent with our introduction of similar support for identifiers
    in the language in Terraform 0.12, and is intended to be more friendly to
    Terraform users throughout the world that might prefer to name their
    products using a different alphabet. This is also a further justification
    for using the DNS normalization rules: modern companies tend to choose
    product names that make good domain names, and now such names will be
    usable as Terraform provider names too.

--- addrs/provider.go
+++ addrs/provider.go
@@ -2,8 +2,9 @@ package addrs
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
+
+	"golang.org/x/net/idna"
 
 	"github.com/hashicorp/hcl/v2"
 	svchost "github.com/hashicorp/terraform-svchost"
@@ -18,22 +19,56 @@ type Provider struct {
 	Hostname  svchost.Hostname
 }
 
-const DefaultRegistryHost = "registry.terraform.io"
+// DefaultRegistryHost is the hostname used for provider addresses that do
+// not have an explicit hostname.
+const DefaultRegistryHost = svchost.Hostname("registry.terraform.io")
 
-var (
-	ValidProviderName = regexp.MustCompile("^[a-zA-Z0-9_-]+$")
-)
+// LegacyProviderNamespace is the special string used in the Namespace field
+// of type Provider to mark a legacy provider address. This special namespace
+// value would normally be invalid, and can be used only when the hostname is
+// DefaultRegistryHost because that host owns the mapping from legacy name to
+// FQN.
+const LegacyProviderNamespace = "-"
 
 // String returns an FQN string, indended for use in output.
 func (pt Provider) String() string {
+	if pt.IsZero() {
+		panic("called String on zero-value addrs.Provider")
+	}
 	return pt.Hostname.ForDisplay() + "/" + pt.Namespace + "/" + pt.Type
+}
+
+// NewProvider constructs a provider address from its parts, and normalizes
+// the namespace and type parts to lowercase using unicode case folding rules
+// so that resulting addrs.Provider values can be compared using standard
+// Go equality rules (==).
+//
+// The hostname is given as a svchost.Hostname, which is required by the
+// contract of that type to have already been normalized for equality testing.
+//
+// This function will panic if the given namespace or type name are not valid.
+// When accepting namespace or type values from outside the program, use
+// ParseProviderPart first to check that the given value is valid.
+func NewProvider(hostname svchost.Hostname, namespace, typeName string) Provider {
+	if namespace == LegacyProviderNamespace {
+		// Legacy provider addresses must always be created via
+		// NewLegacyProvider so that we can use static analysis to find
+		// codepaths still working with those.
+		panic("attempt to create legacy provider address using NewProvider; use NewLegacyProvider instead")
+	}
+
+	return Provider{
+		Type:      MustParseProviderPart(typeName),
+		Namespace: MustParseProviderPart(namespace),
+		Hostname:  hostname,
+	}
 }
 
 // NewDefaultProvider returns the default address of a HashiCorp-maintained,
 // Registry-hosted provider.
 func NewDefaultProvider(name string) Provider {
 	return Provider{
-		Type:      name,
+		Type:      MustParseProviderPart(name),
 		Namespace: "hashicorp",
 		Hostname:  DefaultRegistryHost,
 	}
@@ -42,14 +77,12 @@ func NewDefaultProvider(name string) Provider {
 // NewLegacyProvider returns a mock address for a provider.
 // This will be removed when ProviderType is fully integrated.
 func NewLegacyProvider(name string) Provider {
-	// This is intended to catch provider names with aliases, such as "null.foo"
-	if !ValidProviderName.MatchString(name) {
-		panic("invalid provider name")
-	}
-
 	return Provider{
+		// We intentionally don't normalize and validate the legacy names,
+		// because existing code expects legacy provider names to pass through
+		// verbatim, even if not compliant with our new naming rules.
 		Type:      name,
-		Namespace: "-",
+		Namespace: LegacyProviderNamespace,
 		Hostname:  DefaultRegistryHost,
 	}
 }
@@ -59,10 +92,21 @@ func NewLegacyProvider(name string) Provider {
 // when provider type is fully integrated. As a safeguard for future
 // refactoring, this function panics if the Provider is not a legacy provider.
 func (pt Provider) LegacyString() string {
-	if pt.Namespace != "-" {
-		panic("not a legacy Provider")
+	if pt.IsZero() {
+		panic("called LegacyString on zero-value addrs.Provider")
+	}
+	if pt.Namespace != LegacyProviderNamespace {
+		panic(pt.String() + " is not a legacy addrs.Provider")
 	}
 	return pt.Type
+}
+
+// IsZero returns true if the receiver is the zero value of addrs.Provider.
+//
+// The zero value is not a valid addrs.Provider and calling other methods on
+// such a value is likely to either panic or otherwise misbehave.
+func (pt Provider) IsZero() bool {
+	return pt == Provider{}
 }
 
 // ParseProviderSourceString parses the source attribute and returns a provider.
@@ -101,12 +145,13 @@ func ParseProviderSourceString(str string) (Provider, tfdiags.Diagnostics) {
 	}
 
 	// check the 'name' portion, which is always the last part
-	name := parts[len(parts)-1]
-	if !ValidProviderName.MatchString(name) {
+	givenName := parts[len(parts)-1]
+	name, err := ParseProviderPart(givenName)
+	if err != nil {
 		diags = diags.Append(&hcl.Diagnostic{
 			Severity: hcl.DiagError,
 			Summary:  "Invalid provider type",
-			Detail:   fmt.Sprintf(`Invalid provider type %q in source %q: must be a provider type name"`, name, str),
+			Detail:   fmt.Sprintf(`Invalid provider type %q in source %q: %s"`, name, str, err),
 		})
 		return ret, diags
 	}
@@ -120,16 +165,24 @@ func ParseProviderSourceString(str string) (Provider, tfdiags.Diagnostics) {
 
 	if len(parts) >= 2 {
 		// the namespace is always the second-to-last part
-		namespace := parts[len(parts)-2]
-		if !ValidProviderName.MatchString(namespace) {
-			diags = diags.Append(&hcl.Diagnostic{
-				Severity: hcl.DiagError,
-				Summary:  "Invalid provider namespace",
-				Detail:   fmt.Sprintf(`Invalid provider namespace %q in source %q: must be a valid Registry Namespace"`, namespace, str),
-			})
-			return Provider{}, diags
+		givenNamespace := parts[len(parts)-2]
+		if givenNamespace == LegacyProviderNamespace {
+			// For now we're tolerating legacy provider addresses until we've
+			// finished updating the rest of the codebase to no longer use them,
+			// or else we'd get errors round-tripping through legacy subsystems.
+			ret.Namespace = LegacyProviderNamespace
+		} else {
+			namespace, err := ParseProviderPart(givenNamespace)
+			if err != nil {
+				diags = diags.Append(&hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Invalid provider namespace",
+					Detail:   fmt.Sprintf(`Invalid provider namespace %q in source %q: %s"`, namespace, str, err),
+				})
+				return Provider{}, diags
+			}
+			ret.Namespace = namespace
 		}
-		ret.Namespace = namespace
 	}
 
 	// Final Case: 3 parts
@@ -140,12 +193,93 @@ func ParseProviderSourceString(str string) (Provider, tfdiags.Diagnostics) {
 			diags = diags.Append(&hcl.Diagnostic{
 				Severity: hcl.DiagError,
 				Summary:  "Invalid provider source hostname",
-				Detail:   fmt.Sprintf(`Invalid provider source hostname namespace %q in source %q: must be a valid Registry Namespace"`, hn, str),
+				Detail:   fmt.Sprintf(`Invalid provider source hostname namespace %q in source %q: %s"`, hn, str, err),
 			})
 			return Provider{}, diags
 		}
 		ret.Hostname = hn
 	}
 
+	if ret.Namespace == LegacyProviderNamespace && ret.Hostname != DefaultRegistryHost {
+		// Legacy provider addresses must always be on the default registry
+		// host, because the default registry host decides what actual FQN
+		// each one maps to.
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Invalid provider namespace",
+			Detail:   "The legacy provider namespace \"-\" can be used only with hostname " + DefaultRegistryHost.ForDisplay() + ".",
+		})
+		return Provider{}, diags
+	}
+
 	return ret, diags
+}
+
+// ParseProviderPart processes an addrs.Provider namespace or type string
+// provided by an end-user, producing a normalized version if possible or
+// an error if the string contains invalid characters.
+//
+// A provider part is processed in the same way as an individual label in a DNS
+// domain name: it is transformed to lowercase per the usual DNS case mapping
+// and normalization rules and may contain only letters, digits, and dashes.
+// Additionally, dashes may not appear at the start or end of the string.
+//
+// These restrictions are intended to allow these names to appear in fussy
+// contexts such as directory/file names on case-insensitive filesystems,
+// repository names on GitHub, etc. We're using the DNS rules in particular,
+// rather than some similar rules defined locally, because the hostname part
+// of an addrs.Provider is already a hostname and it's ideal to use exactly
+// the same case folding and normalization rules for all of the parts.
+//
+// In practice a provider type string conventionally does not contain dashes
+// either. Such names are permitted, but providers with such type names will be
+// hard to use because their resource type names will not be able to contain
+// the provider type name and thus each resource will need an explicit provider
+// address specified. (A real-world example of such a provider is the
+// "google-beta" variant of the GCP provider, which has resource types that
+// start with the "google_" prefix instead.)
+//
+// It's valid to pass the result of this function as the argument to a
+// subsequent call, in which case the result will be identical.
+func ParseProviderPart(given string) (string, error) {
+	if len(given) == 0 {
+		return "", fmt.Errorf("must have at least one character")
+	}
+
+	// We're going to process the given name using the same "IDNA" library we
+	// use for the hostname portion, since it already implements the case
+	// folding rules we want.
+	//
+	// The idna library doesn't expose individual label parsing directly, but
+	// once we've verified it doesn't contain any dots we can just treat it
+	// like a top-level domain for this library's purposes.
+	if strings.ContainsRune(given, '.') {
+		return "", fmt.Errorf("dots are not allowed")
+	}
+
+	// We don't allow names containing multiple consecutive dashes, just as
+	// a matter of preference: they look weird, confusing, or incorrect.
+	// This also, as a side-effect, prevents the use of the "punycode"
+	// indicator prefix "xn--" that would cause the IDNA library to interpret
+	// the given name as punycode, because that would be weird and unexpected.
+	if strings.Contains(given, "--") {
+		return "", fmt.Errorf("cannot use multiple consecutive dashes")
+	}
+
+	result, err := idna.Lookup.ToUnicode(given)
+	if err != nil {
+		return "", fmt.Errorf("must contain only letters, digits, and dashes, and may not use leading or trailing dashes")
+	}
+
+	return result, nil
+}
+
+// MustParseProviderPart is a wrapper around ParseProviderPart that panics if
+// it returns an error.
+func MustParseProviderPart(given string) string {
+	result, err := ParseProviderPart(given)
+	if err != nil {
+		panic(err.Error())
+	}
+	return result
 }
--- configs/testdata/providers-explicit-fqn/root.tf
+++ configs/testdata/providers-explicit-fqn/root.tf
@@ -1,7 +1,7 @@
 
 terraform {
   required_providers {
-    foo_test = {
+    foo-test = {
       source = "foo/test"
     }
   }
