ba632bf68637c9fc5eef5d4717e544ab367a3bbd
James Bardin <j.bardin@gmail.com>
2020-03-06 18:27:11 -0500

    implement NodePlannableLocal
    Using this in the same manner as NodePlannableOutput, which expands the
    local values within modules. All thee output and local types are used in
    both plan and apply, we may rename these to better reflect their usage
    in expanding. That wait until we are certain that apply won't need any
    extra machinery for handling values that aren't stored in the plan.

--- terraform/node_local.go
+++ terraform/node_local.go
@@ -1,11 +1,72 @@
 package terraform
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform/addrs"
 	"github.com/hashicorp/terraform/configs"
 	"github.com/hashicorp/terraform/dag"
 	"github.com/hashicorp/terraform/lang"
 )
+
+// NodePlannableLocal represents a named local value in a configuration module,
+// which has not yet been expanded.
+type NodePlannableLocal struct {
+	Addr   addrs.LocalValue
+	Module addrs.Module
+	Config *configs.Local
+}
+
+var (
+	_ RemovableIfNotTargeted     = (*NodePlannableLocal)(nil)
+	_ GraphNodeReferenceable     = (*NodePlannableLocal)(nil)
+	_ GraphNodeReferencer        = (*NodePlannableLocal)(nil)
+	_ GraphNodeDynamicExpandable = (*NodePlannableLocal)(nil)
+)
+
+func (n *NodePlannableLocal) Name() string {
+	path := n.Module.String()
+	addr := n.Addr.String()
+	if path != "" {
+		return path + "." + addr
+	}
+	return addr
+}
+
+// GraphNodeModulePath
+func (n *NodePlannableLocal) ModulePath() addrs.Module {
+	return n.Module
+}
+
+// RemovableIfNotTargeted
+func (n *NodePlannableLocal) RemoveIfNotTargeted() bool {
+	return true
+}
+
+// GraphNodeReferenceable
+func (n *NodePlannableLocal) ReferenceableAddrs() []addrs.Referenceable {
+	return []addrs.Referenceable{n.Addr}
+}
+
+// GraphNodeReferencer
+func (n *NodePlannableLocal) References() []*addrs.Reference {
+	refs, _ := lang.ReferencesInExpr(n.Config.Expr)
+	return appendResourceDestroyReferences(refs)
+}
+
+func (n *NodePlannableLocal) DynamicExpand(ctx EvalContext) (*Graph, error) {
+	var g Graph
+	expander := ctx.InstanceExpander()
+	for _, module := range expander.ExpandModule(n.Module) {
+		o := &NodeLocal{
+			Addr:   n.Addr.Absolute(module),
+			Config: n.Config,
+		}
+		log.Printf("[TRACE] Expanding local: adding %s as %T", o.Addr.String(), o)
+		g.Add(o)
+	}
+	return &g, nil
+}
 
 // NodeLocal represents a named local value in a particular module.
 //
