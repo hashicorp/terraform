f5ce398b0011147e7b0ecd74216ba7c297585fca
Martin Atkins <mart@degeneration.co.uk>
2020-03-11 19:11:52 -0700

    internal/providercache: Start to stub Installer type
    The Installer type will encapsulate the logic for running an entire
    provider installation request: given a set of providers to install, it
    will determine a method to obtain each of them (or detect that they are
    already installed) and then take the necessary actions.
    
    So far it doesn't do anything, but this stubs out an interface by which
    the caller can request ongoing notifications during an installation
    operation.

---
+++ internal/providercache/installer.go
@@ -0,0 +1,7 @@
+package providercache
+
+// Installer is the main type in this package, representing a provider installer
+// with a particular configuration-specific cache directory and an optional
+// global cache directory.
+type Installer struct {
+}
