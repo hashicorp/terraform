137c529626b13fcfd42064fc6a307ad100719e18
Martin Atkins <mart@degeneration.co.uk>
2020-04-02 17:55:25 -0700

    vendor: go get github.com/apparentlymart/go-userdirs
    This library implements the user-specific directory layout specifications
    for various platforms (XDG on Unix, "Known Folders" on Windows, etc).
    
    We'll use this in a subsequent commit to add additional system-specific
    search directories for provider plugins, and perhaps later on also
    CLI configuration directories.

---
+++ vendor/github.com/apparentlymart/go-userdirs/windowsbase/doc.go
@@ -0,0 +1,6 @@
+// Package windowsbase contains helper functionality for accessing the
+// "Known Folders" shell API functions on Windows systems.
+//
+// This package calls into Windows system DLLs, so it cannot be used on any
+// other platform.
+package windowsbase
