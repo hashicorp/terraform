72a45641658405b78650754731189ba6cf28dd74
Troy Connor <troy0820@gmail.com>
2020-04-13 17:30:26 -0400

    🎨[output]Add no-color flag to output command. Add tests  Resolves #17453

--- command/output_test.go
+++ command/output_test.go
@@ -340,6 +340,40 @@ func TestOutput_noVars(t *testing.T) {
 	}
 }
 
+func TestOutput_nocolor(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &OutputCommand{
+		Meta: Meta{
+			testingOverrides: metaOverridesForProvider(testProvider()),
+			Ui:               ui,
+		},
+	}
+
+	args := []string{
+		"-no-color",
+	}
+
+	if code := c.Run(args); code != 0 && c.Meta.Colorize().Disable {
+		t.Fatalf("bad: \n%s", ui.ErrorWriter.String())
+	}
+}
+
+func TestOutput_color(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &OutputCommand{
+		Meta: Meta{
+			testingOverrides: metaOverridesForProvider(testProvider()),
+			Ui:               ui,
+		},
+	}
+
+	args := []string{}
+
+	if code := c.Run(args); code != 0 && !c.Meta.Colorize().Disable {
+		t.Fatalf("bad: \n%s", ui.ErrorWriter.String())
+	}
+}
+
 func TestOutput_stateDefault(t *testing.T) {
 	originalState := states.BuildState(func(s *states.SyncState) {
 		s.SetOutputValue(
