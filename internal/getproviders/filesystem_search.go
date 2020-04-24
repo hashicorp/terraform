954f5d234316841b112671ed5ecc6331c67c5cd6
Martin Atkins <mart@degeneration.co.uk>
2020-04-23 18:12:01 -0700

    internal/getproviders: PackageFilePathForPackage
    This is the equivalent of UnpackedDirectoryPathForPackage when working
    with the packed directory layout. It returns a path to a .zip file with
    a name that would be detected by SearchLocalDirectory as a
    PackageLocalArchive package.

--- internal/getproviders/filesystem_search.go
+++ internal/getproviders/filesystem_search.go
@@ -243,3 +243,16 @@ func UnpackedDirectoryPathForPackage(baseDir string, provider addrs.Provider, ve
 		platform.String(),
 	))
 }
+
+// PackedFilePathForPackage is similar to
+// PackageMeta.PackedFilePath but makes its decision based on
+// individually-passed provider address, version, and target platform so that
+// it can be used by callers outside this package that may have other
+// types that represent package identifiers.
+func PackedFilePathForPackage(baseDir string, provider addrs.Provider, version Version, platform Platform) string {
+	return filepath.ToSlash(filepath.Join(
+		baseDir,
+		provider.Hostname.ForDisplay(), provider.Namespace, provider.Type,
+		fmt.Sprintf("terraform-provider-%s_%s_%s.zip", provider.Type, version.String(), platform.String()),
+	))
+}
