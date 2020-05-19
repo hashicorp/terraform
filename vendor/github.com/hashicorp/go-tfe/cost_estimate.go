0b2a8a5f644a4569b4e38084055a3a0c932794ab
CJ Horton <17039873+radditude@users.noreply.github.com>
2020-05-19 09:35:49 -0700

    go get github.com/hashicorp/go-tfe@v0.8.1 + vendor

--- vendor/github.com/hashicorp/go-tfe/cost_estimate.go
+++ vendor/github.com/hashicorp/go-tfe/cost_estimate.go
@@ -33,13 +33,14 @@ type costEstimates struct {
 // CostEstimateStatus represents a costEstimate state.
 type CostEstimateStatus string
 
-//List all available costEstimate statuses.
+// List all available costEstimate statuses.
 const (
-	CostEstimateCanceled CostEstimateStatus = "canceled"
-	CostEstimateErrored  CostEstimateStatus = "errored"
-	CostEstimateFinished CostEstimateStatus = "finished"
-	CostEstimatePending  CostEstimateStatus = "pending"
-	CostEstimateQueued   CostEstimateStatus = "queued"
+	CostEstimateCanceled              CostEstimateStatus = "canceled"
+	CostEstimateErrored               CostEstimateStatus = "errored"
+	CostEstimateFinished              CostEstimateStatus = "finished"
+	CostEstimatePending               CostEstimateStatus = "pending"
+	CostEstimateQueued                CostEstimateStatus = "queued"
+	CostEstimateSkippedDueToTargeting CostEstimateStatus = "skipped_due_to_targeting"
 )
 
 // CostEstimate represents a Terraform Enterprise costEstimate.
@@ -58,11 +59,12 @@ type CostEstimate struct {
 
 // CostEstimateStatusTimestamps holds the timestamps for individual costEstimate statuses.
 type CostEstimateStatusTimestamps struct {
-	CanceledAt time.Time `json:"canceled-at"`
-	ErroredAt  time.Time `json:"errored-at"`
-	FinishedAt time.Time `json:"finished-at"`
-	PendingAt  time.Time `json:"pending-at"`
-	QueuedAt   time.Time `json:"queued-at"`
+	CanceledAt              time.Time `json:"canceled-at"`
+	ErroredAt               time.Time `json:"errored-at"`
+	FinishedAt              time.Time `json:"finished-at"`
+	PendingAt               time.Time `json:"pending-at"`
+	QueuedAt                time.Time `json:"queued-at"`
+	SkippedDueToTargetingAt time.Time `json:"skipped-due-to-targeting-at"`
 }
 
 // Read a costEstimate by its ID.
