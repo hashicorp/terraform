5127f1ef8b35fb9cd5f63a944d82b5d1ea800741
Paddy <paddy@hashicorp.com>
2020-05-26 17:48:57 -0700

    command: Unmanaged providers
    This adds supports for "unmanaged" providers, or providers with process
    lifecycles not controlled by Terraform. These providers are assumed to
    be started before Terraform is launched, and are assumed to shut
    themselves down after Terraform has finished running.
    
    To do this, we must update the go-plugin dependency to v1.3.0, which
    added support for the "test mode" plugin serving that powers all this.
    
    As a side-effect of not needing to manage the process lifecycle anymore,
    Terraform also no longer needs to worry about the provider's binary, as
    it won't be used for anything anymore. Because of this, we can disable
    the init behavior that concerns itself with downloading that provider's
    binary, checking its version, and otherwise managing the binary.
    
    This is all managed on a per-provider basis, so managed providers that
    Terraform downloads, starts, and stops can be used in the same commands
    as unmanaged providers. The TF_REATTACH_PROVIDERS environment variable
    is added, and is a JSON encoding of the provider's address to the
    information we need to connect to it.
    
    This change enables two benefits: first, delve and other debuggers can
    now be attached to provider server processes, and Terraform can connect.
    This allows for attaching debuggers to provider processes, which before
    was difficult to impossible. Second, it allows the SDK test framework to
    host the provider in the same process as the test driver, while running
    a production Terraform binary against the provider. This allows for Go's
    built-in race detector and test coverage tooling to work as expected in
    provider tests.
    
    Unmanaged providers are expected to work in the exact same way as
    managed providers, with one caveat: Terraform kills provider processes
    and restarts them once per graph walk, meaning multiple times during
    most Terraform CLI commands. As unmanaged providers can't be killed by
    Terraform, and have no visibility into graph walks, unmanaged providers
    are likely to have differences in how their global mutable state behaves
    when compared to managed providers. Namely, unmanaged providers are
    likely to retain global state when managed providers would have reset
    it. Developers relying on global state should be aware of this.

--- vendor/github.com/hashicorp/go-plugin/server.go
+++ vendor/github.com/hashicorp/go-plugin/server.go
@@ -1,11 +1,13 @@
 package plugin
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -15,10 +17,8 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync/atomic"
 
-	"github.com/hashicorp/go-hclog"
-
+	hclog "github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc"
 )
 
@@ -85,6 +85,51 @@ type ServeConfig struct {
 	// Logger is used to pass a logger into the server. If none is provided the
 	// server will create a default logger.
 	Logger hclog.Logger
+
+	// Test, if non-nil, will put plugin serving into "test mode". This is
+	// meant to be used as part of `go test` within a plugin's codebase to
+	// launch the plugin in-process and output a ReattachConfig.
+	//
+	// This changes the behavior of the server in a number of ways to
+	// accomodate the expectation of running in-process:
+	//
+	//   * The handshake cookie is not validated.
+	//   * Stdout/stderr will receive plugin reads and writes
+	//   * Connection information will not be sent to stdout
+	//
+	Test *ServeTestConfig
+}
+
+// ServeTestConfig configures plugin serving for test mode. See ServeConfig.Test.
+type ServeTestConfig struct {
+	// Context, if set, will force the plugin serving to end when cancelled.
+	// This is only a test configuration because the non-test configuration
+	// expects to take over the process and therefore end on an interrupt or
+	// kill signal. For tests, we need to kill the plugin serving routinely
+	// and this provides a way to do so.
+	//
+	// If you want to wait for the plugin process to close before moving on,
+	// you can wait on CloseCh.
+	Context context.Context
+
+	// If this channel is non-nil, we will send the ReattachConfig via
+	// this channel. This can be encoded (via JSON recommended) to the
+	// plugin client to attach to this plugin.
+	ReattachConfigCh chan<- *ReattachConfig
+
+	// CloseCh, if non-nil, will be closed when serving exits. This can be
+	// used along with Context to determine when the server is fully shut down.
+	// If this is not set, you can still use Context on its own, but note there
+	// may be a period of time between canceling the context and the plugin
+	// server being shut down.
+	CloseCh chan<- struct{}
+
+	// SyncStdio, if true, will enable the client side "SyncStdout/Stderr"
+	// functionality to work. This defaults to false because the implementation
+	// of making this work within test environments is particularly messy
+	// and SyncStdio functionality is fairly rare, so we default to the simple
+	// scenario.
+	SyncStdio bool
 }
 
 // protocolVersion determines the protocol version and plugin set to be used by
@@ -169,26 +214,46 @@ func protocolVersion(opts *ServeConfig) (int, Protocol, PluginSet) {
 // Serve serves the plugins given by ServeConfig.
 //
 // Serve doesn't return until the plugin is done being executed. Any
-// errors will be outputted to os.Stderr.
+// fixable errors will be output to os.Stderr and the process will
+// exit with a status code of 1. Serve will panic for unexpected
+// conditions where a user's fix is unknown.
 //
 // This is the method that plugins should call in their main() functions.
 func Serve(opts *ServeConfig) {
-	// Validate the handshake config
-	if opts.MagicCookieKey == "" || opts.MagicCookieValue == "" {
-		fmt.Fprintf(os.Stderr,
-			"Misconfigured ServeConfig given to serve this plugin: no magic cookie\n"+
-				"key or value was set. Please notify the plugin author and report\n"+
-				"this as a bug.\n")
-		os.Exit(1)
-	}
+	exitCode := -1
+	// We use this to trigger an `os.Exit` so that we can execute our other
+	// deferred functions. In test mode, we just output the err to stderr
+	// and return.
+	defer func() {
+		if opts.Test == nil && exitCode >= 0 {
+			os.Exit(exitCode)
+		}
 
-	// First check the cookie
-	if os.Getenv(opts.MagicCookieKey) != opts.MagicCookieValue {
-		fmt.Fprintf(os.Stderr,
-			"This binary is a plugin. These are not meant to be executed directly.\n"+
-				"Please execute the program that consumes these plugins, which will\n"+
-				"load any plugins automatically\n")
-		os.Exit(1)
+		if opts.Test != nil && opts.Test.CloseCh != nil {
+			close(opts.Test.CloseCh)
+		}
+	}()
+
+	if opts.Test == nil {
+		// Validate the handshake config
+		if opts.MagicCookieKey == "" || opts.MagicCookieValue == "" {
+			fmt.Fprintf(os.Stderr,
+				"Misconfigured ServeConfig given to serve this plugin: no magic cookie\n"+
+					"key or value was set. Please notify the plugin author and report\n"+
+					"this as a bug.\n")
+			exitCode = 1
+			return
+		}
+
+		// First check the cookie
+		if os.Getenv(opts.MagicCookieKey) != opts.MagicCookieValue {
+			fmt.Fprintf(os.Stderr,
+				"This binary is a plugin. These are not meant to be executed directly.\n"+
+					"Please execute the program that consumes these plugins, which will\n"+
+					"load any plugins automatically\n")
+			exitCode = 1
+			return
+		}
 	}
 
 	// negotiate the version and plugins
@@ -206,19 +271,6 @@ func Serve(opts *ServeConfig) {
 			Output:     os.Stderr,
 			JSONFormat: true,
 		})
-	}
-
-	// Create our new stdout, stderr files. These will override our built-in
-	// stdout/stderr so that it works across the stream boundary.
-	stdout_r, stdout_w, err := os.Pipe()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error preparing plugin: %s\n", err)
-		os.Exit(1)
-	}
-	stderr_r, stderr_w, err := os.Pipe()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error preparing plugin: %s\n", err)
-		os.Exit(1)
 	}
 
 	// Register a listener so we can accept a connection
@@ -281,6 +333,33 @@ func Serve(opts *ServeConfig) {
 	// Create the channel to tell us when we're done
 	doneCh := make(chan struct{})
 
+	// Create our new stdout, stderr files. These will override our built-in
+	// stdout/stderr so that it works across the stream boundary.
+	var stdout_r, stderr_r io.Reader
+	stdout_r, stdout_w, err := os.Pipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error preparing plugin: %s\n", err)
+		os.Exit(1)
+	}
+	stderr_r, stderr_w, err := os.Pipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error preparing plugin: %s\n", err)
+		os.Exit(1)
+	}
+
+	// If we're in test mode, we tee off the reader and write the data
+	// as-is to our normal Stdout and Stderr so that they continue working
+	// while stdio works. This is because in test mode, we assume we're running
+	// in `go test` or some equivalent and we want output to go to standard
+	// locations.
+	if opts.Test != nil {
+		// TODO(mitchellh): This isn't super ideal because a TeeReader
+		// only works if the reader side is actively read. If we never
+		// connect via a plugin client, the output still gets swallowed.
+		stdout_r = io.TeeReader(stdout_r, os.Stdout)
+		stderr_r = io.TeeReader(stderr_r, os.Stderr)
+	}
+
 	// Build the server type
 	var server ServerProtocol
 	switch protoType {
@@ -323,35 +402,96 @@ func Serve(opts *ServeConfig) {
 
 	logger.Debug("plugin address", "network", listener.Addr().Network(), "address", listener.Addr().String())
 
-	// Output the address and service name to stdout so that the client can bring it up.
-	fmt.Printf("%d|%d|%s|%s|%s|%s\n",
-		CoreProtocolVersion,
-		protoVersion,
-		listener.Addr().Network(),
-		listener.Addr().String(),
-		protoType,
-		serverCert)
-	os.Stdout.Sync()
-
-	// Eat the interrupts
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt)
-	go func() {
-		var count int32 = 0
-		for {
-			<-ch
-			newCount := atomic.AddInt32(&count, 1)
-			logger.Debug("plugin received interrupt signal, ignoring", "count", newCount)
+	// Output the address and service name to stdout so that the client can
+	// bring it up. In test mode, we don't do this because clients will
+	// attach via a reattach config.
+	if opts.Test == nil {
+		fmt.Printf("%d|%d|%s|%s|%s|%s\n",
+			CoreProtocolVersion,
+			protoVersion,
+			listener.Addr().Network(),
+			listener.Addr().String(),
+			protoType,
+			serverCert)
+		os.Stdout.Sync()
+	} else if ch := opts.Test.ReattachConfigCh; ch != nil {
+		// Send back the reattach config that can be used. This isn't
+		// quite ready if they connect immediately but the client should
+		// retry a few times.
+		ch <- &ReattachConfig{
+			Protocol: protoType,
+			Addr:     listener.Addr(),
+			Pid:      os.Getpid(),
+			Test:     true,
 		}
-	}()
+	}
 
-	// Set our new out, err
-	os.Stdout = stdout_w
-	os.Stderr = stderr_w
+	// Eat the interrupts. In test mode we disable this so that go test
+	// can be cancelled properly.
+	if opts.Test == nil {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, os.Interrupt)
+		go func() {
+			count := 0
+			for {
+				<-ch
+				count++
+				logger.Trace("plugin received interrupt signal, ignoring", "count", count)
+			}
+		}()
+	}
+
+	// Set our stdout, stderr to the stdio stream that clients can retrieve
+	// using ClientConfig.SyncStdout/err. We only do this for non-test mode
+	// or if the test mode explicitly requests it.
+	//
+	// In test mode, we use a multiwriter so that the data continues going
+	// to the normal stdout/stderr so output can show up in test logs. We
+	// also send to the stdio stream so that clients can continue working
+	// if they depend on that.
+	if opts.Test == nil || opts.Test.SyncStdio {
+		if opts.Test != nil {
+			// In test mode we need to maintain the original values so we can
+			// reset it.
+			defer func(out, err *os.File) {
+				os.Stdout = out
+				os.Stderr = err
+			}(os.Stdout, os.Stderr)
+		}
+		os.Stdout = stdout_w
+		os.Stderr = stderr_w
+	}
 
 	// Accept connections and wait for completion
 	go server.Serve(listener)
-	<-doneCh
+
+	ctx := context.Background()
+	if opts.Test != nil && opts.Test.Context != nil {
+		ctx = opts.Test.Context
+	}
+	select {
+	case <-ctx.Done():
+		// Cancellation. We can stop the server by closing the listener.
+		// This isn't graceful at all but this is currently only used by
+		// tests and its our only way to stop.
+		listener.Close()
+
+		// If this is a grpc server, then we also ask the server itself to
+		// end which will kill all connections. There isn't an easy way to do
+		// this for net/rpc currently but net/rpc is more and more unused.
+		if s, ok := server.(*GRPCServer); ok {
+			s.Stop()
+		}
+
+		// Wait for the server itself to shut down
+		<-doneCh
+
+	case <-doneCh:
+		// Note that given the documentation of Serve we should probably be
+		// setting exitCode = 0 and using os.Exit here. That's how it used to
+		// work before extracting this library. However, for years we've done
+		// this so we'll keep this functionality.
+	}
 }
 
 func serverListener() (net.Listener, error) {
@@ -390,7 +530,7 @@ func serverListener_tcp() (net.Listener, error) {
 	}
 
 	if minPort > maxPort {
-		return nil, fmt.Errorf("ENV_MIN_PORT value of %d is greater than PLUGIN_MAX_PORT value of %d", minPort, maxPort)
+		return nil, fmt.Errorf("PLUGIN_MIN_PORT value of %d is greater than PLUGIN_MAX_PORT value of %d", minPort, maxPort)
 	}
 
 	for port := minPort; port <= maxPort; port++ {
