54fb4b63bfa689147148d7622d6c9555cdb2e6da
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2020-06-17 11:04:43 -0400

    gofmt tools/tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@ package tools
 
 import (
 	_ "github.com/golang/mock/mockgen"
+	_ "github.com/mitchellh/gox"
 	_ "golang.org/x/tools/cmd/cover"
 	_ "golang.org/x/tools/cmd/stringer"
-	_ "github.com/mitchellh/gox"
 )
