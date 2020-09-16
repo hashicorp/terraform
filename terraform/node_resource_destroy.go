9032a4acfc65a9c53d790fba6503abf0b8578b8c
James Bardin <j.bardin@gmail.com>
2020-09-16 09:52:48 -0400

    destroy nodes must rely on the state cbd status
    When there is only a destroy node, there is no descendent to check for
    "forced CBD", so we can only rely on the state to verify.

--- terraform/node_resource_destroy.go
+++ terraform/node_resource_destroy.go
@@ -53,20 +53,22 @@ func (n *NodeDestroyResourceInstance) DestroyAddr() *addrs.AbsResourceInstance {
 
 // GraphNodeDestroyerCBD
 func (n *NodeDestroyResourceInstance) CreateBeforeDestroy() bool {
-	if n.CreateBeforeDestroyOverride != nil {
-		return *n.CreateBeforeDestroyOverride
-	}
-
-	// Config takes precedence
-	if n.Config != nil && n.Config.Managed != nil {
-		return n.Config.Managed.CreateBeforeDestroy
-	}
-
-	// Otherwise check the state for a stored destroy order
+	// State takes precedence during destroy.
+	// If the resource was removed, there is no config to check.
+	// If CBD was forced from descendent, it should be saved in the state
+	// already.
 	if s := n.instanceState; s != nil {
 		if s.Current != nil {
 			return s.Current.CreateBeforeDestroy
 		}
+	}
+
+	if n.CreateBeforeDestroyOverride != nil {
+		return *n.CreateBeforeDestroyOverride
+	}
+
+	if n.Config != nil && n.Config.Managed != nil {
+		return n.Config.Managed.CreateBeforeDestroy
 	}
 
 	return false
