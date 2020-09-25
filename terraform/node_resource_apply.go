549fba5cfa537542e513599eeedb01cba5618cfa
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2020-09-25 11:15:53 -0400

    terraform: improved refactor of EvalWriteResourceState
    EvalWriteResourceState finds new life as a method called WriteResourceState on NodeAbstractResource.

--- terraform/node_resource_apply.go
+++ terraform/node_resource_apply.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hashicorp/terraform/addrs"
 	"github.com/hashicorp/terraform/dag"
 	"github.com/hashicorp/terraform/lang"
-	"github.com/hashicorp/terraform/tfdiags"
 )
 
 // nodeExpandApplyableResource handles the first layer of resource
@@ -109,41 +108,6 @@ func (n *NodeApplyableResource) Execute(ctx EvalContext, op walkOperation) error
 		return nil
 	}
 
-	var diags tfdiags.Diagnostics
-	state := ctx.State()
-
-	// We'll record our expansion decision in the shared "expander" object
-	// so that later operations (i.e. DynamicExpand and expression evaluation)
-	// can refer to it. Since this node represents the abstract module, we need
-	// to expand the module here to create all resources.
-	expander := ctx.InstanceExpander()
-
-	switch {
-	case n.Config.Count != nil:
-		count, countDiags := evaluateCountExpression(n.Config.Count, ctx)
-		diags = diags.Append(countDiags)
-		if countDiags.HasErrors() {
-			return diags.Err()
-		}
-
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceCount(n.Addr.Module, n.Addr.Resource, count)
-
-	case n.Config.ForEach != nil:
-		forEach, forEachDiags := evaluateForEachExpression(n.Config.ForEach, ctx)
-		diags = diags.Append(forEachDiags)
-		if forEachDiags.HasErrors() {
-			return diags.Err()
-		}
-
-		// This method takes care of all of the business logic of updating this
-		// while ensuring that any existing instances are preserved, etc.
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceForEach(n.Addr.Module, n.Addr.Resource, forEach)
-
-	default:
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceSingle(n.Addr.Module, n.Addr.Resource)
-	}
-	return nil
+	err := n.WriteResourceState(ctx, n.Addr)
+	return err
 }
--- terraform/node_resource_plan.go
+++ terraform/node_resource_plan.go
@@ -185,43 +185,8 @@ func (n *NodePlannableResource) Execute(ctx EvalContext, op walkOperation) error
 		return nil
 	}
 
-	var diags tfdiags.Diagnostics
-	state := ctx.State()
-
-	// We'll record our expansion decision in the shared "expander" object
-	// so that later operations (i.e. DynamicExpand and expression evaluation)
-	// can refer to it. Since this node represents the abstract module, we need
-	// to expand the module here to create all resources.
-	expander := ctx.InstanceExpander()
-
-	switch {
-	case n.Config.Count != nil:
-		count, countDiags := evaluateCountExpression(n.Config.Count, ctx)
-		diags = diags.Append(countDiags)
-		if countDiags.HasErrors() {
-			return diags.Err()
-		}
-
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceCount(n.Addr.Module, n.Addr.Resource, count)
-
-	case n.Config.ForEach != nil:
-		forEach, forEachDiags := evaluateForEachExpression(n.Config.ForEach, ctx)
-		diags = diags.Append(forEachDiags)
-		if forEachDiags.HasErrors() {
-			return diags.Err()
-		}
-
-		// This method takes care of all of the business logic of updating this
-		// while ensuring that any existing instances are preserved, etc.
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceForEach(n.Addr.Module, n.Addr.Resource, forEach)
-
-	default:
-		state.SetResourceProvider(n.Addr, n.ResolvedProvider)
-		expander.SetResourceSingle(n.Addr.Module, n.Addr.Resource)
-	}
-	return nil
+	err := n.WriteResourceState(ctx, n.Addr)
+	return err
 }
 
 // GraphNodeDestroyerCBD
