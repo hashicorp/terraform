4f53234d8cf6c9f964ac8bfa8bce3a624a119bf1
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2020-10-16 15:29:44 -0400

    terraform: Hide maybe-sensitive provisioner output
    If the provisioner configuration includes sensitive values, it's a
    reasonable assumption that we should suppress its log output. Obvious
    examples where this makes sense include echoing a secret to a file using
    local-exec or remote-exec.
    
    This commit adds tests for both logging output from provisioners with
    non-sensitive configuration, and suppressing logs for provisioners with
    sensitive values in configuration.
    
    Note that we do not suppress logs if connection info contains sensitive
    information, as provisioners should not be logging connection
    information under any circumstances.

--- terraform/eval_apply.go
+++ terraform/eval_apply.go
@@ -687,8 +687,21 @@ func (n *EvalApplyProvisioners) apply(ctx EvalContext, provs []*configs.Provisio
 		// those are stripped out before sending to the provisioner. Unlike
 		// resources, we have no need to capture the marked paths and reapply
 		// later.
-		unmarkedConfig, _ := config.UnmarkDeep()
+		unmarkedConfig, configMarks := config.UnmarkDeep()
 		unmarkedConnInfo, _ := connInfo.UnmarkDeep()
+
+		// Marks on the config might result in leaking sensitive values through
+		// provisioner logging, so we conservatively suppress all output in
+		// this case. This should not apply to connection info values, which
+		// provisioners ought not to be logging anyway.
+		if len(configMarks) > 0 {
+			outputFn = func(msg string) {
+				ctx.Hook(func(h Hook) (HookAction, error) {
+					h.ProvisionOutput(absAddr, prov.Type, "(output suppressed due to sensitive value in config)")
+					return HookActionContinue, nil
+				})
+			}
+		}
 
 		output := CallbackUIOutput{OutputFn: outputFn}
 		resp := provisioner.ProvisionResource(provisioners.ProvisionResourceRequest{
