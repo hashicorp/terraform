29c342b020f9bf5fae11c62380ace326c954a51b
James Bardin <j.bardin@gmail.com>
2020-11-18 10:08:50 -0500

    remove unused helper packages
    helper/schema has been moved into internal/legacy/helper.
    helper/resource and helper/validation are no longer used.

--- helper/schema/equal.go
+++
@@ -1,6 +0,0 @@
-package schema
-
-// Equal is an interface that checks for deep equality between two objects.
-type Equal interface {
-	Equal(interface{}) bool
-}
--- helper/schema/field_writer.go
+++
@@ -1,8 +0,0 @@
-package schema
-
-// FieldWriters are responsible for writing fields by address into
-// a proper typed representation. ResourceData uses this to write new data
-// into existing sources.
-type FieldWriter interface {
-	WriteField([]string, interface{}) error
-}
