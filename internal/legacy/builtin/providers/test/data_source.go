b32362da9ded7c7cca758a264abf6f782158d0c8
James Bardin <j.bardin@gmail.com>
2020-11-18 10:00:55 -0500

    internal/legacy/builtin/providers/test
    Move the test provider into the interal/legacy directory, until we can
    factor it out of the e2e test.

---
+++ internal/legacy/builtin/providers/test/data_source.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"time"
+
+	"github.com/hashicorp/terraform/internal/legacy/helper/schema"
+)
+
+func testDataSource() *schema.Resource {
+	return &schema.Resource{
+		Read: testDataSourceRead,
+
+		Schema: map[string]*schema.Schema{
+			"list": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"input": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"output": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			// this attribute is computed, but never set by the provider
+			"nil": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"input_map": {
+				Type:     schema.TypeMap,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+			"output_map": {
+				Type:     schema.TypeMap,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+		},
+	}
+}
+
+func testDataSourceRead(d *schema.ResourceData, meta interface{}) error {
+	d.SetId(time.Now().UTC().String())
+	d.Set("list", []interface{}{"one", "two", "three"})
+
+	if input, hasInput := d.GetOk("input"); hasInput {
+		d.Set("output", input)
+	} else {
+		d.Set("output", "some output")
+	}
+
+	if inputMap, hasInput := d.GetOk("input_map"); hasInput {
+		d.Set("output_map", inputMap)
+	}
+	return nil
+}
---
+++ internal/legacy/builtin/providers/test/resource_config_mode.go
@@ -0,0 +1,78 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/internal/legacy/helper/schema"
+)
+
+func testResourceConfigMode() *schema.Resource {
+	return &schema.Resource{
+		Create: testResourceConfigModeCreate,
+		Read:   testResourceConfigModeRead,
+		Delete: testResourceConfigModeDelete,
+		Update: testResourceConfigModeUpdate,
+
+		Schema: map[string]*schema.Schema{
+			"resource_as_attr": {
+				Type:       schema.TypeList,
+				ConfigMode: schema.SchemaConfigModeAttr,
+				Optional:   true,
+				Computed:   true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"foo": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"nested_set": {
+				Type:       schema.TypeSet,
+				Optional:   true,
+				ConfigMode: schema.SchemaConfigModeAttr,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"value": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"set": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func testResourceConfigModeCreate(d *schema.ResourceData, meta interface{}) error {
+	d.SetId("placeholder")
+	return testResourceConfigModeRead(d, meta)
+}
+
+func testResourceConfigModeRead(d *schema.ResourceData, meta interface{}) error {
+	if l, ok := d.Get("resource_as_attr").([]interface{}); !ok {
+		return fmt.Errorf("resource_as_attr should appear as []interface{}, not %T", l)
+	} else {
+		for i, item := range l {
+			if _, ok := item.(map[string]interface{}); !ok {
+				return fmt.Errorf("resource_as_attr[%d] should appear as map[string]interface{}, not %T", i, item)
+			}
+		}
+	}
+	return nil
+}
+
+func testResourceConfigModeUpdate(d *schema.ResourceData, meta interface{}) error {
+	return testResourceConfigModeRead(d, meta)
+}
+
+func testResourceConfigModeDelete(d *schema.ResourceData, meta interface{}) error {
+	d.SetId("")
+	return nil
+}
