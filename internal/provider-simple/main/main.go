eee581ae54731c0540e8500cf2080e467229fb39
James Bardin <j.bardin@gmail.com>
2020-11-20 11:30:12 -0500

    simple provider for internal testing
    Simple is a minimal provider implementation using the grpcwrap package,
    intended for use in internal tests.

---
+++ internal/provider-simple/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/internal/grpcwrap"
+	simple "github.com/hashicorp/terraform/internal/provider-simple"
+	"github.com/hashicorp/terraform/internal/tfplugin5"
+	"github.com/hashicorp/terraform/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		GRPCProviderFunc: func() tfplugin5.ProviderServer {
+			return grpcwrap.New(simple.Provider())
+		},
+	})
+}
