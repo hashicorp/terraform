7cae76383a4c9c12c8a67609556034616b82a430
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2021-02-05 14:01:58 -0500

    cli: Fix for provider requirements in JSON plan
    The JSON plan output format includes a serialized, simplified version of
    the configuration. One component of this config is a map of provider
    configurations, which includes version constraints.
    
    Until now, only version constraints specified in the provider config
    blocks were exposed in the JSON plan output. This is a deprecated method
    of specifying provider versions, and the recommended use of a
    required_providers block resulted in the version constraints being
    omitted.
    
    This commit fixes this with two changes:
    
    - When processing the provider configurations from a module, output the
      fully-merged version constraints for the entire module, instead of any
      constraints set in the provider configuration block itself;
    - After all provider configurations are processed, iterate over the
      required_providers entries to ensure that any configuration-less
      providers are output to the JSON plan too.
    
    No changes are necessary to the structure of the JSON plan output, so
    this is effectively a semantic level bug fix.

---
+++ command/testdata/show-json/provider-version-no-config/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    test = {
+      source = "hashicorp/test"
+      version = ">= 1.2.3"
+    }
+  }
+}
+
+variable "test_var" {
+  default = "bar"
+}
+
+resource "test_instance" "test" {
+  ami   = var.test_var
+  count = 3
+}
+
+output "test" {
+  value = var.test_var
+}
