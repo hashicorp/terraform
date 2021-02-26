a61ece4af0847e07e43ba985382e63af3915f898
Omar Ismail <oismail91@gmail.com>
2021-02-26 11:24:02 -0500

    Fix state issues

--- backend/remote/backend_common.go
+++ backend/remote/backend_common.go
@@ -414,19 +414,16 @@ func (b *Remote) checkPolicy(stopCtx, cancelCtx context.Context, op *backend.Ope
 		case tfe.PolicyHardFailed:
 			return fmt.Errorf(msgPrefix + " hard failed.")
 		case tfe.PolicySoftFailed:
+			runUrl := fmt.Sprintf(runHeader, b.hostname, b.organization, op.Workspace, r.ID)
 
-			runOverrideOuputMsg := fmt.Sprintf(
-				"The run needs to be manually overridden or discarded.\n%s\n",
-				fmt.Sprintf(runHeader, b.hostname, b.organization, op.Workspace, r.ID),
-			)
 			if op.Type == backend.OperationTypePlan || op.UIOut == nil || op.UIIn == nil ||
 				!pc.Actions.IsOverridable || !pc.Permissions.CanOverride {
-				return fmt.Errorf(msgPrefix + " soft failed.\n" + runOverrideOuputMsg)
+				return fmt.Errorf(msgPrefix + " soft failed.\n" + runUrl)
 			}
 
 			if op.AutoApprove {
 				if _, err = b.client.PolicyChecks.Override(stopCtx, pc.ID); err != nil {
-					return generalError(fmt.Sprintf("Failed to override policy check.\n%s", runOverrideOuputMsg), err)
+					return generalError(fmt.Sprintf("Failed to override policy check.\n%s", runUrl), err)
 				}
 			} else {
 				opts := &terraform.InputOpts{
@@ -436,7 +433,17 @@ func (b *Remote) checkPolicy(stopCtx, cancelCtx context.Context, op *backend.Ope
 				}
 				err = b.confirm(stopCtx, op, opts, r, "override")
 				if err != nil && err != errRunOverridden {
-					return fmt.Errorf(fmt.Sprintf("Failed to override: %s.\n%s", err.Error(), runOverrideOuputMsg))
+					return fmt.Errorf(
+						fmt.Sprintf("Failed to override: %s\n%s\n", err.Error(), runUrl),
+					)
+				}
+
+				if err != errRunOverridden {
+					if _, err = b.client.PolicyChecks.Override(stopCtx, pc.ID); err != nil {
+						return generalError(fmt.Sprintf("Failed to override policy check.\n%s", runUrl), err)
+					}
+				} else {
+					b.CLI.Output(fmt.Sprintf("The run needs to be manually overridden or discarded.\n%s\n", runUrl))
 				}
 			}
 
