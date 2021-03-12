0bbe583eb84035146f9dabc7ed616c8ee0416f65
Alisdair McDiarmid <alisdair@hashicorp.com>
2021-03-12 08:19:55 -0500

    functions: Fix defaults for null objects/tuples
    When using defaults with a value which contains null objects or tuples,
    we cannot continue to traverse the value and apply defaults. Instead,
    when we find an attribute which is null, we return early and stop
    processing this branch.

--- lang/funcs/defaults.go
+++ lang/funcs/defaults.go
@@ -94,6 +94,12 @@ func defaultsApply(input, fallback cty.Value) cty.Value {
 		return v
 
 	case wantTy.IsObjectType():
+		// For structural types, a null input value must be passed through. We
+		// do not apply default values for missing optional structural values,
+		// only their contents.
+		if input.IsNull() {
+			return input
+		}
 		atys := wantTy.AttributeTypes()
 		ret := map[string]cty.Value{}
 		for attr, aty := range atys {
@@ -107,6 +113,13 @@ func defaultsApply(input, fallback cty.Value) cty.Value {
 		return cty.ObjectVal(ret)
 
 	case wantTy.IsTupleType():
+		// For structural types, a null input value must be passed through. We
+		// do not apply default values for missing optional structural values,
+		// only their contents.
+		if input.IsNull() {
+			return input
+		}
+
 		l := wantTy.Length()
 		ret := make([]cty.Value, l)
 		for i := 0; i < l; i++ {
