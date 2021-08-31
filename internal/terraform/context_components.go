dcfa077adf68efdcf0083280dff554720f7ba468
Martin Atkins <mart@degeneration.co.uk>
2021-08-31 10:22:25 -0700

    core: contextComponentFactory doesn't need to enumerate components
    In earlier Terraform versions we used the set of all available plugins of
    each type to make graph-building decisions, but in modern Terraform we
    make those decisions based entirely on the configuration.
    
    Consequently, we no longer need the methods which can enumerate all of the
    known plugin components of a given type. Instead, we just try to
    instantiate each of the plugins that the configuration refers to and then
    handle the error when that fails, which typically means that the user
    needs to run "terraform init" to install some new plugins.

--- internal/terraform/context_components.go
+++ internal/terraform/context_components.go
@@ -15,35 +15,16 @@ import (
 type contextComponentFactory interface {
 	// ResourceProvider creates a new ResourceProvider with the given type.
 	ResourceProvider(typ addrs.Provider) (providers.Interface, error)
-	ResourceProviders() []string
 
 	// ResourceProvisioner creates a new ResourceProvisioner with the given
 	// type.
 	ResourceProvisioner(typ string) (provisioners.Interface, error)
-	ResourceProvisioners() []string
 }
 
 // basicComponentFactory just calls a factory from a map directly.
 type basicComponentFactory struct {
 	providers    map[addrs.Provider]providers.Factory
 	provisioners map[string]provisioners.Factory
-}
-
-func (c *basicComponentFactory) ResourceProviders() []string {
-	var result []string
-	for k := range c.providers {
-		result = append(result, k.String())
-	}
-	return result
-}
-
-func (c *basicComponentFactory) ResourceProvisioners() []string {
-	var result []string
-	for k := range c.provisioners {
-		result = append(result, k)
-	}
-
-	return result
 }
 
 func (c *basicComponentFactory) ResourceProvider(typ addrs.Provider) (providers.Interface, error) {
