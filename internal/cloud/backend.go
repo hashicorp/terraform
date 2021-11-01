0c99b305a4ac5cafaa82047d3db0c087b5bba406
uturunku1 <luces.huayhuaca@gmail.com>
2021-11-01 08:55:59 -0700

    convert uses of worspaces.operations into workspaces.executionMode

--- internal/cloud/backend.go
+++ internal/cloud/backend.go
@@ -631,7 +631,7 @@ func (b *Cloud) Operation(ctx context.Context, op *backend.Operation) (*backend.
 	b.IgnoreVersionConflict()
 
 	// Check if we need to use the local backend to run the operation.
-	if b.forceLocal || !w.Operations {
+	if b.forceLocal || w.ExecutionMode == "local" {
 		// Record that we're forced to run operations locally to allow the
 		// command package UI to operate correctly
 		b.forceLocal = true
@@ -820,9 +820,9 @@ func (b *Cloud) VerifyWorkspaceTerraformVersion(workspaceName string) tfdiags.Di
 		return nil
 	}
 
-	// If the workspace has remote operations disabled, the remote Terraform
+	// If the workspace has execution-mode set to local, the remote Terraform
 	// version is effectively meaningless, so we'll skip version verification.
-	if !workspace.Operations {
+	if workspace.ExecutionMode == "local" {
 		return nil
 	}
 
