d196d2870a2e77f4ec65efe2359ed0d6919b10f7
Barrett Clark <barrett.clark@hashicorp.com>
2021-12-16 14:23:42 -0600

    Refactor cloud table test runs
    As the cloud e2e tests evolved some common patters became apparent. This
    standardizes and consolidates the patterns into a common test runner
    that takes the table tests and runs them in parallel. Some tests also
    needed to be converted to utilize table tests.

--- internal/cloud/e2e/migrate_state_tfc_to_other_test.go
+++ internal/cloud/e2e/migrate_state_tfc_to_other_test.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
-
-	expect "github.com/Netflix/go-expect"
-	"github.com/hashicorp/terraform/internal/e2e"
 )
 
 func Test_migrate_tfc_to_other(t *testing.T) {
 	t.Parallel()
 	skipIfMissingEnvVar(t)
 
-	cases := map[string]struct {
-		operations []operationSets
-	}{
+	cases := testCases{
 		"migrate from cloud to local backend": {
 			operations: []operationSets{
 				{
@@ -48,71 +41,5 @@ func Test_migrate_tfc_to_other(t *testing.T) {
 		},
 	}
 
-	for name, tc := range cases {
-		tc := tc // rebind tc into this lexical scope
-		t.Run(name, func(subtest *testing.T) {
-			subtest.Parallel()
-			organization, cleanup := createOrganization(t)
-			defer cleanup()
-			exp, err := expect.NewConsole(defaultOpts()...)
-			if err != nil {
-				subtest.Fatal(err)
-			}
-			defer exp.Close()
-
-			tmpDir, err := ioutil.TempDir("", "terraform-test")
-			if err != nil {
-				subtest.Fatal(err)
-			}
-			defer os.RemoveAll(tmpDir)
-
-			tf := e2e.NewBinary(terraformBin, tmpDir)
-			tf.AddEnv(cliConfigFileEnv)
-			defer tf.Close()
-
-			for _, op := range tc.operations {
-				op.prep(t, organization.Name, tf.WorkDir())
-				for _, tfCmd := range op.commands {
-					cmd := tf.Cmd(tfCmd.command...)
-					cmd.Stdin = exp.Tty()
-					cmd.Stdout = exp.Tty()
-					cmd.Stderr = exp.Tty()
-
-					err = cmd.Start()
-					if err != nil {
-						subtest.Fatal(err)
-					}
-
-					if tfCmd.expectedCmdOutput != "" {
-						got, err := exp.ExpectString(tfCmd.expectedCmdOutput)
-						if err != nil {
-							subtest.Fatalf("error while waiting for output\nwant: %s\nerror: %s\noutput\n%s", tfCmd.expectedCmdOutput, err, got)
-						}
-					}
-
-					lenInput := len(tfCmd.userInput)
-					lenInputOutput := len(tfCmd.postInputOutput)
-					if lenInput > 0 {
-						for i := 0; i < lenInput; i++ {
-							input := tfCmd.userInput[i]
-							exp.SendLine(input)
-							// use the index to find the corresponding
-							// output that matches the input.
-							if lenInputOutput-1 >= i {
-								output := tfCmd.postInputOutput[i]
-								_, err := exp.ExpectString(output)
-								if err != nil {
-									subtest.Fatal(err)
-								}
-							}
-						}
-					}
-					err = cmd.Wait()
-					if err != nil && !tfCmd.expectError {
-						subtest.Fatal(err)
-					}
-				}
-			}
-		})
-	}
+	testRunner(t, cases, 1)
 }
