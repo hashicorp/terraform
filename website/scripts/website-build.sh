c8a018d8225904d66ec1aabf46d624fe348d9a36
Kevin Wang <kwangsan@gmail.com>
2022-04-11 11:47:07 -0400

    feat: deploy preview

---
+++ website/scripts/website-build.sh
@@ -0,0 +1,28 @@
+# Repo which we are cloning and executing npm run build:deploy-preview within
+REPO_TO_CLONE=terraform-website
+# Set the subdirectory name for the terraform-website app
+PREVIEW_DIR=website-preview
+CLONE_DIR=website-preview
+
+from_cache=false
+
+if [ -d "$PREVIEW_DIR" ]; then
+    echo "$PREVIEW_DIR found"
+    CLONE_DIR="$PREVIEW_DIR-tmp"
+    from_cache=true
+fi
+
+# Clone the terraform-website project, if needed
+echo "⏳ Cloning the terraform-website repo, this might take a while..."
+git clone --depth=1 --branch=master https://github.com/hashicorp/$REPO_TO_CLONE.git "$CLONE_DIR"
+
+if [ "$from_cache" = true ]; then
+    echo "Setting up $PREVIEW_DIR"
+    cp -R "./$CLONE_DIR/." "./$PREVIEW_DIR"
+fi
+
+# cd into the preview directory project
+cd "$PREVIEW_DIR"
+
+# Run the terraform-website content-repo start script
+REPO=terraform IS_CONTENT_PREVIEW=true npm run build:deploy-preview
