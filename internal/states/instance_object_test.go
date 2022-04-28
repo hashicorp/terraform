897c7f4cc46129e106540bbdaae61b2a453c6b3e
James Bardin <j.bardin@gmail.com>
2022-04-28 15:16:27 +0000

    backport of commit 34a01c92d639b60848a0d844447797be23f39680

--- internal/states/instance_object_test.go
+++ internal/states/instance_object_test.go
@@ -1,6 +1,7 @@
 package states
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -24,27 +25,59 @@ func TestResourceInstanceObject_encode(t *testing.T) {
 		addrs.RootModule.Resource(addrs.ManagedResourceMode, "test", "boop"),
 		addrs.RootModule.Resource(addrs.ManagedResourceMode, "test", "honk"),
 	}
-	rioOne := &ResourceInstanceObject{
-		Value:        value,
-		Status:       ObjectPlanned,
-		Dependencies: depsOne,
+
+	// multiple instances may have been assigned the same deps slice
+	objs := []*ResourceInstanceObject{
+		&ResourceInstanceObject{
+			Value:        value,
+			Status:       ObjectPlanned,
+			Dependencies: depsOne,
+		},
+		&ResourceInstanceObject{
+			Value:        value,
+			Status:       ObjectPlanned,
+			Dependencies: depsTwo,
+		},
+		&ResourceInstanceObject{
+			Value:        value,
+			Status:       ObjectPlanned,
+			Dependencies: depsOne,
+		},
+		&ResourceInstanceObject{
+			Value:        value,
+			Status:       ObjectPlanned,
+			Dependencies: depsOne,
+		},
 	}
-	rioTwo := &ResourceInstanceObject{
-		Value:        value,
-		Status:       ObjectPlanned,
-		Dependencies: depsTwo,
+
+	var encoded []*ResourceInstanceObjectSrc
+
+	// Encoding can happen concurrently, so we need to make sure the shared
+	// Dependencies are safely handled
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for _, obj := range objs {
+		obj := obj
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			rios, err := obj.Encode(value.Type(), 0)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+			mu.Lock()
+			encoded = append(encoded, rios)
+			mu.Unlock()
+		}()
 	}
-	riosOne, err := rioOne.Encode(value.Type(), 0)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	riosTwo, err := rioTwo.Encode(value.Type(), 0)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
+	wg.Wait()
+
 	// However, identical sets of dependencies should always be written to state
 	// in an identical order, so we don't do meaningless state updates on refresh.
-	if diff := cmp.Diff(riosOne.Dependencies, riosTwo.Dependencies); diff != "" {
-		t.Errorf("identical dependencies got encoded in different orders:\n%s", diff)
+	for i := 0; i < len(encoded)-1; i++ {
+		if diff := cmp.Diff(encoded[i].Dependencies, encoded[i+1].Dependencies); diff != "" {
+			t.Errorf("identical dependencies got encoded in different orders:\n%s", diff)
+		}
 	}
 }
