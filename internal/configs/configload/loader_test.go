d1e35a3f7c1b4fcad7593f9d5136e2cf51d7e112
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2022-05-16 16:45:16 -0400

    configs: Fix module loader nil pointer panic
    In configurations which have already been initialized, updating the
    source of a non-root module call to an invalid value could cause a nil
    pointer panic. This commit fixes the bug and adds test coverage.

--- internal/configs/configload/loader_test.go
+++ internal/configs/configload/loader_test.go
@@ -14,7 +14,7 @@ func assertNoDiagnostics(t *testing.T, diags hcl.Diagnostics) bool {
 
 func assertDiagnosticCount(t *testing.T, diags hcl.Diagnostics, want int) bool {
 	t.Helper()
-	if len(diags) != 0 {
+	if len(diags) != want {
 		t.Errorf("wrong number of diagnostics %d; want %d", len(diags), want)
 		for _, diag := range diags {
 			t.Logf("- %s", diag)
