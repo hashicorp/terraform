1f8cdcab0e8b14df90c2f0f7bc0f2e2cb04313bb
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2022-05-16 20:45:16 +0000

    backport of commit d1e35a3f7c1b4fcad7593f9d5136e2cf51d7e112

--- internal/configs/configload/loader_load.go
+++ internal/configs/configload/loader_load.go
@@ -60,8 +60,11 @@ func (l *Loader) moduleWalkerLoad(req *configs.ModuleRequest) (*configs.Module, 
 
 	var diags hcl.Diagnostics
 
-	// Check for inconsistencies between manifest and config
-	if req.SourceAddr.String() != record.SourceAddr {
+	// Check for inconsistencies between manifest and config.
+
+	// We ignore a nil SourceAddr here, which represents a failure during
+	// configuration parsing, and will be reported in a diagnostic elsewhere.
+	if req.SourceAddr != nil && req.SourceAddr.String() != record.SourceAddr {
 		diags = append(diags, &hcl.Diagnostic{
 			Severity: hcl.DiagError,
 			Summary:  "Module source has changed",
