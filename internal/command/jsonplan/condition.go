a82631ae2439123db89c6ad9761b1a128c94b753
Martin Atkins <mart@degeneration.co.uk>
2022-07-22 18:56:11 -0700

    command/jsonplan: Include new-style check results in JSON plan output
    This is a new-shaped representation of check results which follows the
    two-tiered structure of static objects and dynamic instances of objects,
    thereby allowing consumers to see which checkable objects exist in the
    configuration even if a dynamic evaluation error prevented actually
    expanding them all to determine their declared instances.
    
    Eventually we'll include this in the state too, but this initially adds it
    only to the plan in order to replace the now-deprecated experimental
    conditions result that was present but undocumented in Terraform v1.2.

--- internal/command/jsonplan/condition.go
+++ internal/command/jsonplan/condition.go
@@ -5,10 +5,17 @@ package jsonplan
 // This no longer really fits how Terraform is modelling checks -- we're now
 // treating check status as a whole-object thing rather than an individual
 // condition thing -- but we've preserved this for now to remain as compatible
-// as possible with the interface we'd documented as part of the Terraform v1.2
-// release, before we'd really solidified the use-cases for checks outside
-// of just making a single plan and apply operation fail with an error.
+// as possible with the interface we'd experimentally-implemented but not
+// documented in the Terraform v1.2 release, before we'd really solidified the
+// use-cases for checks outside of just making a single plan and apply
+// operation fail with an error.
 type conditionResult struct {
+	// This is a weird "pseudo-comment" noting that we're deprecating this
+	// not-previously-documented, experimental representation of conditions
+	// in favor of the "checks" property which better fits Terraform Core's
+	// modelling of checks.
+	DeprecationNotice conditionResultDeprecationNotice `json:"//"`
+
 	// Address is the absolute address of the condition's containing object.
 	Address string `json:"address,omitempty"`
 
@@ -27,3 +34,11 @@ type conditionResult struct {
 	// present if the condition fails.
 	ErrorMessage string `json:"error_message,omitempty"`
 }
+
+type conditionResultDeprecationNotice struct{}
+
+func (n conditionResultDeprecationNotice) MarshalJSON() ([]byte, error) {
+	return conditionResultDeprecationNoticeJSON, nil
+}
+
+var conditionResultDeprecationNoticeJSON = []byte(`"This previously-experimental representation of conditions is deprecated and will be removed in Terraform v1.4. Use the 'checks' property instead."`)
