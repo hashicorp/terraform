6487100c9a9f8a726f0e50dce771695624c6e058
Liam Cervante <liam.cervante@hashicorp.com>
2022-08-02 09:29:20 +0100

    Remove -local flag from goimports check

--- scripts/goimportscheck.sh
+++ scripts/goimportscheck.sh
@@ -8,11 +8,11 @@ if [[ -z ${target_files}  ]]; then
   exit 0
 fi
 
-goimports_files=$(goimports -w -l -local github.com/hashicorp/terraform/ ${target_files})
+goimports_files=$(goimports -w -l "${target_files}")
 if [[ -n ${goimports_files} ]]; then
   echo 'goimports needs running on the following files:'
   echo "${goimports_files}"
-  echo "You can use the command and flags \`goimports -w -l -local github.com/hashicorp/terraform/\` to reformat the code"
+  echo "You can use the command and flags \`goimports -w -l\` to reformat the code"
   exit 1
 fi
 
