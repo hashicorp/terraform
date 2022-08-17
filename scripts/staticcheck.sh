2fcb8f7a6a8b0cd75c112c93c325b265762cd7b7
Martin Atkins <mart@degeneration.co.uk>
2022-08-17 13:49:33 -0700

    build: Disable errors for deprecated functions in staticcheck
    Because we maintain multiple versions of Terraform across different
    release branches, we aim to avoid creating needless differences between
    the branches to maximize the chance of successful automatic backporting.
    
    Part of that policy is that we don't make cross-cutting changes to respond
    to deprecation of functions in upstream packages and instead we respond
    to them gradually over time when we'd be changing the nearby code anyway,
    or when new work requires using the replacement APIs.
    
    In recognition of that, this turns of the staticcheck rule that would
    otherwise force us to resolve all deprecations before moving forward with
    any other change.

--- scripts/staticcheck.sh
+++ scripts/staticcheck.sh
@@ -12,5 +12,7 @@ packages=$(go list ./... | egrep -v ${skip})
 
 # We are skipping style-related checks, since terraform intentionally breaks
 # some of these. The goal here is to find issues that reduce code clarity, or
-# may result in bugs.
-go run honnef.co/go/tools/cmd/staticcheck -checks 'all,-ST*' ${packages}
+# may result in bugs. We also disable fucntion deprecation checks (SA1019)
+# because our policy is to update deprecated calls locally while making other
+# nearby changes, rather than to make cross-cutting changes to update them all.
+go run honnef.co/go/tools/cmd/staticcheck -checks 'all,-SA1019,-ST*' ${packages}
