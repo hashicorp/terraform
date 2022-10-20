6a5540c9f7ce2a53b033c147c2e688bd3d896b75
The Terraform Team <52939924+teamterraform@users.noreply.github.com>
2022-10-20 14:26:36 -0400

    Backport of chore: Update Digital Team Files into v1.1 (#32052)
    * backport of commit a2bcc4ed057aad771d3319639f45ec8911c3d139
    
    * backport of commit e0dcc9e828027dc8f8846c8d25e03c3fe78566db
    
    * backport of commit b68c925c67985959fa02ea9c789e32ea80a0c745
    
    * backport of commit 06537b0944624f328ad3b0b1ed8ee74a32742eda
    
    * chroe: update root `Makefile`
    
    Co-authored-by: HashiBot <62622282+hashibot-web@users.noreply.github.com>
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

--- website/scripts/website-start.sh
+++ website/scripts/website-start.sh
@@ -5,10 +5,8 @@
 
 # Repo which we are cloning and executing npm run build:deploy-preview within
 REPO_TO_CLONE=dev-portal
-# Set the subdirectory name for the base project
+# Set the subdirectory name for the dev-portal app
 PREVIEW_DIR=website-preview
-# The directory we want to clone the project into
-CLONE_DIR=website-preview
 # The product for which we are building the deploy preview
 PRODUCT=terraform
 # Preview mode, controls the UI rendered (either the product site or developer). Can be `io` or `developer`
@@ -16,37 +14,31 @@ PREVIEW_MODE=developer
 
 # Get the git branch of the commit that triggered the deploy preview
 # This will power remote image assets in local and deploy previews
-CURRENT_GIT_BRANCH=$VERCEL_GIT_COMMIT_REF
+CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # This is where content files live, relative to the website-preview dir. If omitted, "../content" will be used
 LOCAL_CONTENT_DIR=../docs
 
-from_cache=false
+should_pull=true
 
-if [ -d "$PREVIEW_DIR" ]; then
-  echo "$PREVIEW_DIR found"
-  CLONE_DIR="$PREVIEW_DIR-tmp"
-  from_cache=true
+# Clone the dev-portal project, if needed
+if [ ! -d "$PREVIEW_DIR" ]; then
+    echo "⏳ Cloning the $REPO_TO_CLONE repo, this might take a while..."
+    git clone --depth=1 https://github.com/hashicorp/$REPO_TO_CLONE.git "$PREVIEW_DIR"
+    should_pull=false
 fi
 
-# Clone the base project, if needed
-echo "⏳ Cloning the $REPO_TO_CLONE repo, this might take a while..."
-git clone --depth=1 "https://github.com/hashicorp/$REPO_TO_CLONE.git" "$CLONE_DIR"
-
-if [ "$from_cache" = true ]; then
-  echo "Setting up $PREVIEW_DIR"
-  cp -R "./$CLONE_DIR/." "./$PREVIEW_DIR"
-fi
-
-# cd into the preview directory project
 cd "$PREVIEW_DIR"
 
-# Run the build:deploy-preview start script
-PREVIEW_FROM_REPO=$PRODUCT \
-IS_CONTENT_PREVIEW=true \
-PREVIEW_MODE=$PREVIEW_MODE \
+# If the directory already existed, pull to ensure the clone is fresh
+if [ "$should_pull" = true ]; then
+    git pull origin main
+fi
+
+# Run the dev-portal content-repo start script
 REPO=$PRODUCT \
-HASHI_ENV=project-preview \
+PREVIEW_FROM_REPO=$PRODUCT \
 LOCAL_CONTENT_DIR=$LOCAL_CONTENT_DIR \
 CURRENT_GIT_BRANCH=$CURRENT_GIT_BRANCH \
-npm run build:deploy-preview
+PREVIEW_MODE=$PREVIEW_MODE \
+npm run start:local-preview
