8881418c992804ae7b8eb86c9fd9111f033c4390
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2022-12-16 15:03:46 -0500

    Update internal/command/workspace_select.go
    Co-authored-by: James Bardin <j.bardin@gmail.com>

--- internal/command/workspace_select.go
+++ internal/command/workspace_select.go
@@ -20,7 +20,7 @@ func (c *WorkspaceSelectCommand) Run(args []string) int {
 
 	var orCreate bool
 	cmdFlags := c.Meta.defaultFlagSet("workspace select")
-	cmdFlags.BoolVar(&orCreate, "or-create", false, "create workspace if it doesnt exist")
+	cmdFlags.BoolVar(&orCreate, "or-create", false, "create workspace if it does not exist")
 	cmdFlags.Usage = func() { c.Ui.Error(c.Help()) }
 	if err := cmdFlags.Parse(args); err != nil {
 		c.Ui.Error(fmt.Sprintf("Error parsing command-line flags: %s\n", err.Error()))
