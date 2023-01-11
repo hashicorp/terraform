a5a586e89baf5ad48e71e3578276690bb5e6236c
Liam Cervante <liam.cervante@hashicorp.com>
2023-01-11 17:08:47 +0100

    add missing plan metadata into the renderer

--- internal/command/jsonformat/renderer.go
+++ internal/command/jsonformat/renderer.go
@@ -11,16 +11,20 @@ import (
 
 	"github.com/hashicorp/terraform/internal/command/format"
 	"github.com/hashicorp/terraform/internal/command/jsonformat/computed"
-	"github.com/hashicorp/terraform/internal/command/jsonformat/differ"
 	"github.com/hashicorp/terraform/internal/command/jsonplan"
 	"github.com/hashicorp/terraform/internal/command/jsonprovider"
 	"github.com/hashicorp/terraform/internal/plans"
 	"github.com/hashicorp/terraform/internal/terminal"
 )
 
+type RendererOpt int
+
 const (
 	detectedDrift  string = "drift"
 	proposedChange string = "change"
+
+	Errored RendererOpt = iota
+	CanNotApply
 )
 
 type Plan struct {
@@ -36,96 +40,163 @@ type Plan struct {
 type Renderer struct {
 	Streams  *terminal.Streams
 	Colorize *colorstring.Colorize
+
+	RunningInAutomation bool
 }
 
-type diff struct {
-	change jsonplan.ResourceChange
-	diff   computed.Diff
-}
-
-type diffs struct {
-	drift   []diff
-	changes []diff
-	outputs map[string]computed.Diff
-}
-
-func (r Renderer) RenderHumanPlan(plan Plan) {
+func (r Renderer) RenderHumanPlan(plan Plan, mode plans.Mode, opts ...RendererOpt) {
 	// TODO(liamcervante): Tidy up this detection of version differences, we
 	// should only report warnings when the plan is generated using a newer
 	// version then we are executing. We could also look into major vs minor
 	// version differences. This should work for alpha testing in the meantime.
 	if plan.PlanFormatVersion != jsonplan.FormatVersion || plan.ProviderFormatVersion != jsonprovider.FormatVersion {
-		fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color("\n[bold][red]Warning:[reset][bold] This plan was generated using a different version of Terraform, the diff presented here maybe missing representations of recent features."))
+		r.Streams.Println(r.Colorize.Color("\n[bold][red]Warning:[reset][bold] This plan was generated using a different version of Terraform, the diff presented here maybe missing representations of recent features."))
+	}
+
+	checkOpts := func(target RendererOpt) bool {
+		for _, opt := range opts {
+			if opt == target {
+				return true
+			}
+		}
+		return false
 	}
 
 	diffs := precomputeDiffs(plan)
-
-	willPrintResourceDrift := false
-	for _, drift := range diffs.drift {
-		diff, render := r.renderHumanDiff(drift, detectedDrift)
-		if render {
-			if !willPrintResourceDrift {
-				fmt.Fprint(r.Streams.Stdout.File, r.Colorize.Color("\n[bold][cyan]Note:[reset][bold] Objects have changed outside of Terraform\n"))
-				fmt.Fprintln(r.Streams.Stdout.File)
-				fmt.Fprint(r.Streams.Stdout.File, "Terraform detected the following changes made outside of Terraform since the last \"terraform apply\" which may have affected this plan:\n")
-			}
-			willPrintResourceDrift = true
-
-			fmt.Fprintln(r.Streams.Stdout.File)
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(diff))
-		}
-	}
+	haveRefreshChanges := r.renderHumanDiffDrift(diffs, mode)
 
 	willPrintResourceChanges := false
 	counts := make(map[plans.Action]int)
-	for _, resource := range plan.ResourceChanges {
-		action := jsonplan.UnmarshalActions(resource.Change.Actions)
-		if action == plans.NoOp {
+	var changes []diff
+	for _, diff := range diffs.changes {
+		action := jsonplan.UnmarshalActions(diff.change.Change.Actions)
+		if action == plans.NoOp && !diff.Moved() {
 			// Don't show anything for NoOp changes.
 			continue
 		}
-		if action == plans.Delete && resource.Mode != "managed" {
+		if action == plans.Delete && diff.change.Mode != "managed" {
 			// Don't render anything for deleted data sources.
 			continue
 		}
 
+		changes = append(changes, diff)
 		willPrintResourceChanges = true
-		counts[action]++
+
+		// Don't count move-only changes
+		if action != plans.NoOp {
+			counts[action]++
+		}
 	}
+
+	if len(changes) == 0 && len(diffs.outputs) == 0 {
+		if checkOpts(Errored) {
+			if haveRefreshChanges {
+				r.Streams.Print(format.HorizontalRule(r.Colorize, r.Streams.Stdout.Columns()))
+				r.Streams.Println("")
+			}
+			r.Streams.Print(
+				r.Colorize.Color("\n[reset][bold][red]Planning failed.[reset][bold] Terraform encountered an error while generating this plan.[reset]\n\n"),
+			)
+		} else {
+			switch mode {
+			case plans.RefreshOnlyMode:
+				if haveRefreshChanges {
+					return
+				}
+
+				r.Streams.Print(r.Colorize.Color("\n[reset][bold][green]No changes.[reset][bold] Your infrastructure still matches the configuration.[reset]\n\n"))
+				r.Streams.Println(format.WordWrap(
+					"Terraform has checked that the real remote objects still match the result of your most recent changes, and found no differences.",
+					r.Streams.Stdout.Columns()))
+			case plans.DestroyMode:
+				if haveRefreshChanges {
+					r.Streams.Print(format.HorizontalRule(r.Colorize, r.Streams.Stdout.Columns()))
+					fmt.Fprintln(r.Streams.Stdout.File)
+				}
+				r.Streams.Print(r.Colorize.Color("\n[reset][bold][green]No changes.[reset][bold] No objects need to be destroyed.[reset]\n\n"))
+				r.Streams.Println(format.WordWrap(
+					"Either you have not created any objects yet or the existing objects were already deleted outside of Terraform.",
+					r.Streams.Stdout.Columns()))
+			default:
+				if haveRefreshChanges {
+					r.Streams.Print(format.HorizontalRule(r.Colorize, r.Streams.Stdout.Columns()))
+					r.Streams.Println("")
+				}
+				r.Streams.Print(
+					r.Colorize.Color("\n[reset][bold][green]No changes.[reset][bold] Your infrastructure matches the configuration.[reset]\n\n"),
+				)
+
+				if haveRefreshChanges {
+					if !checkOpts(CanNotApply) {
+						// In this case, applying this plan will not change any
+						// remote objects but _will_ update the state to match what
+						// we detected during refresh, so we'll reassure the user
+						// about that.
+						r.Streams.Println(format.WordWrap(
+							"Your configuration already matches the changes detected above, so applying this plan will only update the state to include the changes detected above and won't change any real infrastructure.",
+							r.Streams.Stdout.Columns(),
+						))
+					} else {
+						// In this case we detected changes during refresh but this isn't
+						// a planning mode where we consider those to be applyable. The
+						// user must re-run in refresh-only mode in order to update the
+						// state to match the upstream changes.
+						suggestion := "."
+						if !r.RunningInAutomation {
+							// The normal message includes a specific command line to run.
+							suggestion = ":\n  terraform apply -refresh-only"
+						}
+						r.Streams.Println(format.WordWrap(
+							"Your configuration already matches the changes detected above. If you'd like to update the Terraform state to match, create and apply a refresh-only plan"+suggestion,
+							r.Streams.Stdout.Columns(),
+						))
+					}
+					return
+				}
+
+				// If we get down here then we're just in the simple situation where
+				// the plan isn't applyable at all.
+				r.Streams.Println(format.WordWrap(
+					"Terraform has compared your real infrastructure against your configuration and found no differences, so no changes are needed.",
+					r.Streams.Stdout.Columns(),
+				))
+			}
+		}
+	}
+
 	if willPrintResourceChanges {
-		fmt.Fprintln(r.Streams.Stdout.File, "\nTerraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:")
+		r.Streams.Println("\nTerraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:")
 		if counts[plans.Create] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.Create)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.Create)))
 		}
 		if counts[plans.Update] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.Update)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.Update)))
 		}
 		if counts[plans.Delete] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.Delete)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.Delete)))
 		}
 		if counts[plans.DeleteThenCreate] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.DeleteThenCreate)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.DeleteThenCreate)))
 		}
 		if counts[plans.CreateThenDelete] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.CreateThenDelete)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.CreateThenDelete)))
 		}
 		if counts[plans.Read] > 0 {
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(actionDescription(plans.Read)))
+			r.Streams.Println(r.Colorize.Color(actionDescription(plans.Read)))
 		}
 
-		fmt.Fprint(r.Streams.Stdout.File, "\nTerraform will perform the following actions:\n")
+		r.Streams.Print("\nTerraform will perform the following actions:\n")
 	}
 
-	for _, change := range diffs.changes {
+	for _, change := range changes {
 		diff, render := r.renderHumanDiff(change, proposedChange)
 		if render {
 			fmt.Fprintln(r.Streams.Stdout.File)
-			fmt.Fprintln(r.Streams.Stdout.File, r.Colorize.Color(diff))
+			r.Streams.Println(r.Colorize.Color(diff))
 		}
 	}
 
-	fmt.Fprintf(
-		r.Streams.Stdout.File,
+	r.Streams.Printf(
 		"\nPlan: %d to add, %d to change, %d to destroy.\n",
 		counts[plans.Create]+counts[plans.DeleteThenCreate]+counts[plans.CreateThenDelete],
 		counts[plans.Update],
@@ -133,8 +204,20 @@ func (r Renderer) RenderHumanPlan(plan Plan) {
 
 	diff := r.renderHumanDiffOutputs(diffs.outputs)
 	if len(diff) > 0 {
-		fmt.Fprint(r.Streams.Stdout.File, "\nChanges to Outputs:\n")
-		fmt.Fprintf(r.Streams.Stdout.File, "%s\n", r.Colorize.Color(diff))
+		r.Streams.Print("\nChanges to Outputs:\n")
+		r.Streams.Printf("%s\n", r.Colorize.Color(diff))
+
+		// TODO: check what happens if you have modules with outputs.
+
+		if len(counts) == 0 {
+			// If we have output changes but not resource changes then we
+			// won't have output any indication about the changes at all yet,
+			// so we need some extra context about what it would mean to
+			// apply a change that _only_ includes output changes.
+			r.Streams.Println(format.WordWrap(
+				"\nYou can apply this plan to save these new output values to the Terraform state, without changing any real infrastructure.",
+				r.Streams.Stdout.Columns()))
+		}
 	}
 }
 
@@ -154,6 +237,46 @@ func (r Renderer) renderHumanDiffOutputs(outputs map[string]computed.Diff) strin
 		}
 	}
 	return strings.Join(rendered, "\n")
+}
+
+func (r Renderer) renderHumanDiffDrift(diffs diffs, mode plans.Mode) bool {
+	var drs []diff
+	if mode == plans.RefreshOnlyMode {
+		drs = diffs.drift
+	} else {
+		for _, dr := range diffs.drift {
+			// TODO(liamcervante): Look into if we have to keep filtering resource changes.
+			// For now we still want to remove the moved resources from here as
+			// they will show up in the regular changes.
+			if dr.diff.Action != plans.NoOp {
+				drs = append(drs, dr)
+			}
+		}
+	}
+
+	if len(drs) == 0 {
+		return false
+	}
+
+	if diffs.Empty() && mode != plans.RefreshOnlyMode {
+		return false
+	}
+
+	r.Streams.Print(r.Colorize.Color("\n[bold][cyan]Note:[reset][bold] Objects have changed outside of Terraform\n"))
+	r.Streams.Println()
+	r.Streams.Print(format.WordWrap(
+		"Terraform detected the following changes made outside of Terraform since the last \"terraform apply\" which may have affected this plan:\n",
+		r.Streams.Stdout.Columns()))
+
+	for _, drift := range drs {
+		diff, render := r.renderHumanDiff(drift, detectedDrift)
+		if render {
+			r.Streams.Println()
+			r.Streams.Println(r.Colorize.Color(diff))
+		}
+	}
+
+	return true
 }
 
 func (r Renderer) renderHumanDiff(diff diff, cause string) (string, bool) {
@@ -179,34 +302,6 @@ func (r Renderer) renderHumanDiff(diff diff, cause string) (string, bool) {
 
 func (r Renderer) RenderLog(message map[string]interface{}) {
 	panic("not implemented")
-}
-
-func precomputeDiffs(plan Plan) diffs {
-	ctx := diffs{
-		outputs: make(map[string]computed.Diff),
-	}
-
-	for _, drift := range plan.ResourceDrift {
-		schema := plan.ProviderSchemas[drift.ProviderName].ResourceSchemas[drift.Type]
-		ctx.drift = append(ctx.drift, diff{
-			change: drift,
-			diff:   differ.FromJsonChange(drift.Change).ComputeDiffForBlock(schema.Block),
-		})
-	}
-
-	for _, change := range plan.ResourceChanges {
-		schema := plan.ProviderSchemas[change.ProviderName].ResourceSchemas[change.Type]
-		ctx.changes = append(ctx.changes, diff{
-			change: change,
-			diff:   differ.FromJsonChange(change.Change).ComputeDiffForBlock(schema.Block),
-		})
-	}
-
-	for key, output := range plan.OutputChanges {
-		ctx.outputs[key] = differ.FromJsonChange(output).ComputeDiffForOutput()
-	}
-
-	return ctx
 }
 
 func resourceChangeComment(resource jsonplan.ResourceChange, action plans.Action, changeCause string) string {
