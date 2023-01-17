d0df33d547cf73b431c61fcc0fd1cf482b9ed49d
Jan Mueller <jan.mueller@adfinis.com>
2023-01-17 13:47:38 +0100

    Add support for PG_CONN_STR ENV var

--- internal/backend/remote-state/pg/backend.go
+++ internal/backend/remote-state/pg/backend.go
@@ -23,6 +23,7 @@ func New() backend.Backend {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "Postgres connection string; a `postgres://` URL",
+				DefaultFunc: schema.EnvDefaultFunc("PG_CONN_STR", ""),
 			},
 
 			"schema_name": {
