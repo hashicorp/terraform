deb888e3c3841c5434465de9fd3f73dba1e2908b
Daniel Banck <daniel@dbanck.de>
2023-01-18 12:01:07 +0100

    Simplify type marshaling by using cty.Type

--- internal/command/jsonfunction/return_type.go
+++ internal/command/jsonfunction/return_type.go
@@ -3,10 +3,9 @@ package jsonfunction
 import (
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
-	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
-func getReturnType(f function.Function) ([]byte, error) {
+func getReturnType(f function.Function) (cty.Type, error) {
 	args := make([]cty.Type, 0)
 	for _, param := range f.Params() {
 		args = append(args, param.Type)
@@ -15,9 +14,5 @@ func getReturnType(f function.Function) ([]byte, error) {
 		args = append(args, f.VarParam().Type)
 	}
 
-	returnType, err := f.ReturnType(args)
-	if err != nil {
-		return nil, err
-	}
-	return ctyjson.MarshalType(returnType)
+	return f.ReturnType(args)
 }
