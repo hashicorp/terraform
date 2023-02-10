135f14275b62d76f337651a88e7f02390d50d240
Martin Atkins <mart@degeneration.co.uk>
2023-02-09 17:44:23 -0800

    core: Graph.ResourceGraph
    This helper method derives from an execution graph a typically-smaller
    graph which describes only the relationships between resources. This could
    then be used for analyses that only care about the relationships between
    resources and don't need to consider all of the other objects that might
    indirectly glue the resources together.

--- internal/terraform/graph.go
+++ internal/terraform/graph.go
@@ -239,3 +239,87 @@ func (g *Graph) checkAndApplyOverrides(overrides *mocking.Overrides, target dag.
 
 	return false
 }
+
+// ResourceGraph derives a graph containing addresses of only the nodes in the
+// receiver which implement [GraphNodeConfigResource], describing the
+// relationships between all of their [addrs.ConfigResource] addresses.
+//
+// Nodes that do not have resource addresses are discarded but the
+// result preserves correct dependency relationships for the nodes that are
+// left, still taking into account any indirect dependencies through nodes
+// that were discarded.
+func (g *Graph) ResourceGraph() addrs.DirectedGraph[addrs.ConfigResource] {
+	// For now we're doing this in a kinda-janky way, by first constructing
+	// a reduced graph containing only GraphNodeConfigResource implementations
+	// and then using that temporary graph to construct the final graph to
+	// return.
+
+	tmpG := Graph{}
+	tmpG.Subsume(&g.Graph)
+	tmpG.reducePreservingRelationships(func(n dag.Vertex) bool {
+		_, ret := n.(GraphNodeConfigResource)
+		return ret
+	})
+	tmpG.TransitiveReduction()
+
+	ret := addrs.NewDirectedGraph[addrs.ConfigResource]()
+	for _, n := range tmpG.Vertices() {
+		sourceR := n.(GraphNodeConfigResource)
+		sourceAddr := sourceR.ResourceAddr()
+		ret.Add(sourceAddr)
+		for _, dn := range tmpG.DownEdges(n) {
+			targetR := dn.(GraphNodeConfigResource)
+			ret.AddDependency(sourceAddr, targetR.ResourceAddr())
+		}
+	}
+	return ret
+}
+
+// reducePreservingRelationships modifies the receiver in-place so that it only
+// contains the nodes for which keepNode returns true, but also adds new
+// edges to preserve the dependency relationships for all of the nodes
+// that still remain.
+func (g *Graph) reducePreservingRelationships(keepNode func(dag.Vertex) bool) {
+	// This is a naive algorithm for now. Maybe we'll improve it later.
+
+	// We'll keep iterating as long as we find new edges to add because we
+	// might need to bridge across multiple nodes that we're going to remove
+	// in order to retain the relationships, and one iteration can only
+	// bridge a single node at a time.
+	changed := true
+	for changed {
+		changed = false
+		for _, n := range g.Vertices() {
+			if keepNode(n) {
+				continue
+			}
+
+			// If we're not going to keep this node then we need to connect
+			// all of its dependents to all of its dependencies so that the
+			// ordering is still preserved for those nodes that remain.
+			// However, this will often generate more edges than are strictly
+			// required and so it could be productive to run a transitive
+			// reduction afterwards.
+			dependents := g.UpEdges(n)
+			dependencies := g.DownEdges(n)
+			for dependent := range dependents {
+				for dependency := range dependencies {
+					edge := dag.BasicEdge(dependent, dependency)
+					if !g.HasEdge(edge) {
+						g.Connect(edge)
+						changed = true
+					}
+				}
+			}
+		}
+	}
+
+	// With all of the extra supporting edges in place, we can now safely
+	// remove the nodes we aren't going to keep without changing the
+	// relationships of the remaining nodes.
+	for _, n := range g.Vertices() {
+		if !keepNode(n) {
+			g.Remove(n)
+		}
+	}
+}
