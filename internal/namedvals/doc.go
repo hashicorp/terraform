ab14d24fb509c877774fec4642adca4f7aa44128
Martin Atkins <mart@degeneration.co.uk>
2023-03-08 18:19:08 -0800

    namedvals: A new package to encapsulate the tracking of named values
    Currently we have a slightly different treatment for each of the different
    kinds of named values, which makes it hard to generalize the
    implementations of their very similar behaviors.
    
    This package should over time take ownership of the problem of tracking
    all three kinds of named value during a graph walk, with result data
    structures that use this information (such as state and plan objects)
    copying the relevant values out of here only once the graph walk is
    complete.
    
    Nothing is using this yet, though. Some retrofitting of this into
    Terraform Core will follow in subsequent commits.

---
+++ internal/namedvals/doc.go
@@ -0,0 +1,4 @@
+// Package namedvals has container types to help with modelling the gradual
+// evaluation of local values (input variable values, local values, output
+// values) during a graph walk.
+package namedvals
