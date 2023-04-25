d278350bf14f6f3678286725a112e76d93b1de18
Martin Atkins <mart@degeneration.co.uk>
2023-04-25 10:18:06 -0700

    sourcebundle: even more WIP

--- internal/getmodules/sourcebundle/builder.go
+++ internal/getmodules/sourcebundle/builder.go
@@ -95,137 +95,17 @@ func (b *Builder) Manifest() *Manifest {
 // returns error diagnostics then the [Builder] may be left in an inconsistent
 // state and so should not be used any further.
 func (b *Builder) AddRemoteSource(ctx context.Context, src addrs.ModuleSourceRemote, srcRng *tfdiags.SourceRange, depFinder DependencyFinder) tfdiags.Diagnostics {
-	// We retrieve remote module packages using go-getter internally, since
-	// Terraform's remote module package addresses are really just go-getter
-	// addresses.
-
-	var diags tfdiags.Diagnostics
-	var hclRng *hcl.Range
-	if srcRng != nil {
-		hclRng = srcRng.ToHCL().Ptr()
+	// we preallocate some queue buffers so that in many cases we can avoid
+	// reallocating these slices throughout, unless one of the downstream
+	// modules has a large number of dependencies itself.
+	remoteQueue := make([]remoteQueueItem, 1, 8)
+	regQueue := make([]registryQueueItem, 0, 8)
+	remoteQueue[0] = remoteQueueItem{
+		sourceAddr: src,
+		depFinder:  depFinder,
+		srcRng:     srcRng,
 	}
-
-	// TODO: Check if we already have a local copy of this source's module
-	// package.
-
-	// We'll first retrieve each package into a temporary directory so that we
-	// can tidy it and analyze it a bit before moving it to its final directory
-	// name. We only want to keep the files that are actually relevant to
-	// Terraform, and want to detect if we end up retrieving the same source
-	// package twice (due to slightly-different source addresses that are
-	// actually equivalent) so that in situations like a big monorepo we can
-	// minimize the chance of ending up storing the whole monorepo multiple
-	// times.
-	tmpDir, err := ioutil.TempDir(b.workDir, "installtemp-")
-	if err != nil {
-		diags = diags.Append(tfdiags.Sourceless(
-			tfdiags.Error,
-			"Failed to create temporary directory",
-			fmt.Sprintf("Failed to create directory under %q for module package installation: %s.", b.workDir, err),
-		))
-		return diags
-	}
-
-	err = b.fetcher.FetchPackage(ctx, tmpDir, src.Package.String())
-	if err != nil {
-		// FIXME: go-getter generates a poor error for an invalid relative
-		// path, so we should generate a better error if err is of type
-		// *getmodules.MaybeRelativePathErr. See package initwd for
-		// an example.
-
-		// Errors returned by go-getter have very inconsistent quality as
-		// end-user error messages, but for now we're accepting that because
-		// we have no way to recognize any specific errors to improve them
-		// and masking the error entirely would hide valuable diagnostic
-		// information from the user.
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Failed to fetch module package",
-			Detail:   fmt.Sprintf("Could not download module package %q: %s.", src, err),
-			Subject:  hclRng,
-		})
-		return diags
-	}
-
-	// We'll delete from the package any paths that are matched by the
-	// .terraformignore file, if present, or by our default ignore rules.
-	// This also validates the contents to make sure we only have regular
-	// files, directories, and symlinks only within the package root.
-	ignoreRules, err := parseIgnoreFile(tmpDir)
-	if err != nil {
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Invalid remote module package",
-			Detail:   fmt.Sprintf("Could not load the .terraformignore rules from the remote module package: %s.", err),
-			Subject:  hclRng,
-		})
-		return diags
-	}
-	// NOTE: The checks in packagePrepareWalkFn are safe only if we are sure
-	// that no other process is concurrently modifying our temporary directory.
-	// Source bundle building should only occur on hosts that are trusted by
-	// whoever will ultimately be using the generated bundle.
-	err = filepath.Walk(tmpDir, packagePrepareWalkFn(tmpDir, ignoreRules))
-	if err != nil {
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Invalid remote module package",
-			Detail:   fmt.Sprintf("Module package from %q has invalid contents: %s.", src.Package, err),
-			Subject:  hclRng,
-		})
-		return diags
-	}
-
-	// If we got here then our tmpDir contains the final source code of a valid
-	// module package. We'll compute a hash of its contents so we can notice
-	// if it is identical to some other package we already installed, and then
-	// if not rename it into its final directory name.
-	// For this purpose we reuse the same directory tree hashing scheme that
-	// Go uses for its own modules, although that's an implementation detail
-	// subject to change in future versions: callers should always resolve
-	// paths through the source bundle's manifest rather than assuming a path.
-	hash, err := dirhash.HashDir(tmpDir, "", dirhash.Hash1)
-	if err != nil {
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Failed to calculate module package checksum",
-			Detail:   fmt.Sprintf("Cannot calculate checksum for contents of package %q: %s.", src.Package, err),
-			Subject:  hclRng,
-		})
-		return diags
-	}
-
-	finalPath := filepath.Join(b.workDir, hash)
-
-	if _, exists := b.knownPackageHashes[hash]; exists {
-		// We already fetched the same package content from another location,
-		// so we'll just discard this directory and use the earlier one.
-		err := os.RemoveAll(tmpDir)
-		if err != nil {
-			diags = diags.Append(tfdiags.Sourceless(
-				tfdiags.Error,
-				"Failed to clean up temporary directory",
-				fmt.Sprintf("Failed to clean up module package working directory %q: %s.", tmpDir, err),
-			))
-			return diags
-		}
-	} else {
-		// We've not encountered this package yet, so we'll rename the
-		// temporary directory into its final location within the bundle.
-		err := os.Rename(tmpDir, finalPath)
-		if err != nil {
-			diags = diags.Append(tfdiags.Sourceless(
-				tfdiags.Error,
-				"Failed to prepare module package directory",
-				fmt.Sprintf("Failed to move module package %q to its final bundle location: %s.", src.Package, err),
-			))
-			return diags
-		}
-	}
-
-	b.manifest.saveModulePackageBundleDir(src.Package, hash)
-
-	return diags
+	return b.addPackages(ctx, regQueue, remoteQueue)
 }
 
 // AddRegistrySource adds a new remote source to the builder, retrieving its
@@ -254,6 +134,71 @@ func (b *Builder) AddRegistrySource(ctx context.Context, src addrs.ModuleSourceR
 
 	moreDiags = b.AddRemoteSource(ctx, remoteSrc, srcRng, depFinder)
 	diags = diags.Append(moreDiags)
+	return diags
+}
+
+// addPackages is the main mechanism of the builder, which deals with
+// everything in the given "queue" slices, but also appends new items to the
+// queues as they are discovered and then depletes them to empty.
+//
+// After calling this method it exclusively owns the backing arrays of the
+// two "queue" slices and will make arbitrary modifications to them.
+func (b *Builder) addPackages(ctx context.Context, regQueue []registryQueueItem, remoteQueue []remoteQueueItem) tfdiags.Diagnostics {
+	var diags tfdiags.Diagnostics
+
+	// We'll keep iterating until both of our queues are empty.
+	for len(regQueue) > 0 && len(remoteQueue) > 0 {
+
+		// First we'll deal with registry queue items, because each iteration
+		// of this will generate another entry in pkgQueue which we can then
+		// process all together afterwards.
+		for len(regQueue) > 0 {
+			// Although we've called it a "queue", the processing order doesn't
+			// actually matter here so we're going to consume it in LIFO order
+			// (like a stack) to make more efficient use of the slice.
+			item := regQueue[len(regQueue)-1]
+			regQueue = regQueue[:len(regQueue)-1]
+
+			remoteSrc, moreDiags := b.findRegistryModulePackage(ctx, item.sourceAddr.Package, item.allowedVersions, item.srcRng)
+			diags = diags.Append(moreDiags)
+			if diags.HasErrors() {
+				return diags
+			}
+
+			// We need to adjust the remote source address to take into account any
+			// additional subdirectory component that was present in "src". This
+			// is important because the registry is allowed to return a subdir part
+			// itself, and so the two subdir portions must be combined together if so.
+			remoteSrc = remoteSrc.FromRegistry(item.sourceAddr)
+
+			// The underlying real remote source address now transfers directly
+			// into our "remote queue" for processing in the other loop below.
+			remoteQueue = append(remoteQueue, remoteQueueItem{
+				sourceAddr: remoteSrc,
+				depFinder:  item.depFinder,
+				srcRng:     item.srcRng,
+			})
+		}
+
+		// By the time we get here we probably have a mixture of both
+		// directly-detected remote sources and those we detected indirectly
+		// through a module registry. We deal with them both in the same way.
+		for len(remoteQueue) > 0 {
+			// Although we've called it a "queue", the processing order doesn't
+			// actually matter here so we're going to consume it in LIFO order
+			// (like a stack) to make more efficient use of the slice.
+			item := remoteQueue[len(remoteQueue)-1]
+			remoteQueue = remoteQueue[:len(remoteQueue)-1]
+
+			localDir, moreDiags := b.fetchRemotePackage(ctx, item.sourceAddr.Package, item.srcRng, item.depFinder)
+			diags = diags.Append(moreDiags)
+			if diags.HasErrors() {
+				return diags
+			}
+		}
+
+	}
+
 	return diags
 }
 
@@ -373,4 +318,161 @@ func (b *Builder) findRegistryModulePackage(ctx context.Context, pkgAddr addrs.M
 		})
 		return addrs.ModuleSourceRemote{}, diags
 	}
+}
+
+// fetchRemotePackage ensures that the given remote module package is present
+// in the builder's work directory, and returns the name of the bundle
+// subdirectory it's been installed to.
+//
+// If the given package address has been requested before, or if another
+// package address yielding identical source code was requested before, then
+// the returned subdirectory will be shared with those other requests. This
+// in particular means that if everything is coming from the same monorepo
+// then we should end up storing only one copy of its contents, no matter
+// how many times it is mentioned with different sub-paths.
+func (b *Builder) fetchRemotePackage(ctx context.Context, pkgAddr addrs.ModulePackage, srcRng *tfdiags.SourceRange, depFinder DependencyFinder) (string, tfdiags.Diagnostics) {
+	// We retrieve remote module packages using go-getter internally, since
+	// Terraform's remote module package addresses are really just go-getter
+	// addresses.
+
+	var diags tfdiags.Diagnostics
+	var hclRng *hcl.Range
+	if srcRng != nil {
+		hclRng = srcRng.ToHCL().Ptr()
+	}
+
+	// TODO: Check if we already have a local copy of this source's module
+	// package.
+
+	// We'll first retrieve each package into a temporary directory so that we
+	// can tidy it and analyze it a bit before moving it to its final directory
+	// name. We only want to keep the files that are actually relevant to
+	// Terraform, and want to detect if we end up retrieving the same source
+	// package twice (due to slightly-different source addresses that are
+	// actually equivalent) so that in situations like a big monorepo we can
+	// minimize the chance of ending up storing the whole monorepo multiple
+	// times.
+	tmpDir, err := ioutil.TempDir(b.workDir, "installtemp-")
+	if err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Failed to create temporary directory",
+			fmt.Sprintf("Failed to create directory under %q for module package installation: %s.", b.workDir, err),
+		))
+		return "", diags
+	}
+
+	err = b.fetcher.FetchPackage(ctx, tmpDir, pkgAddr.String())
+	if err != nil {
+		// FIXME: go-getter generates a poor error for an invalid relative
+		// path, so we should generate a better error if err is of type
+		// *getmodules.MaybeRelativePathErr. See package initwd for
+		// an example.
+
+		// Errors returned by go-getter have very inconsistent quality as
+		// end-user error messages, but for now we're accepting that because
+		// we have no way to recognize any specific errors to improve them
+		// and masking the error entirely would hide valuable diagnostic
+		// information from the user.
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Failed to fetch module package",
+			Detail:   fmt.Sprintf("Could not download module package %q: %s.", pkgAddr, err),
+			Subject:  hclRng,
+		})
+		return "", diags
+	}
+
+	// We'll delete from the package any paths that are matched by the
+	// .terraformignore file, if present, or by our default ignore rules.
+	// This also validates the contents to make sure we only have regular
+	// files, directories, and symlinks only within the package root.
+	ignoreRules, err := parseIgnoreFile(tmpDir)
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Invalid remote module package",
+			Detail:   fmt.Sprintf("Could not load the .terraformignore rules from the remote module package: %s.", err),
+			Subject:  hclRng,
+		})
+		return "", diags
+	}
+	// NOTE: The checks in packagePrepareWalkFn are safe only if we are sure
+	// that no other process is concurrently modifying our temporary directory.
+	// Source bundle building should only occur on hosts that are trusted by
+	// whoever will ultimately be using the generated bundle.
+	err = filepath.Walk(tmpDir, packagePrepareWalkFn(tmpDir, ignoreRules))
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Invalid remote module package",
+			Detail:   fmt.Sprintf("Module package from %q has invalid contents: %s.", pkgAddr, err),
+			Subject:  hclRng,
+		})
+		return "", diags
+	}
+
+	// If we got here then our tmpDir contains the final source code of a valid
+	// module package. We'll compute a hash of its contents so we can notice
+	// if it is identical to some other package we already installed, and then
+	// if not rename it into its final directory name.
+	// For this purpose we reuse the same directory tree hashing scheme that
+	// Go uses for its own modules, although that's an implementation detail
+	// subject to change in future versions: callers should always resolve
+	// paths through the source bundle's manifest rather than assuming a path.
+	hash, err := dirhash.HashDir(tmpDir, "", dirhash.Hash1)
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Failed to calculate module package checksum",
+			Detail:   fmt.Sprintf("Cannot calculate checksum for contents of package %q: %s.", pkgAddr, err),
+			Subject:  hclRng,
+		})
+		return "", diags
+	}
+
+	finalPath := filepath.Join(b.workDir, hash)
+
+	if _, exists := b.knownPackageHashes[hash]; exists {
+		// We already fetched the same package content from another location,
+		// so we'll just discard this directory and use the earlier one.
+		err := os.RemoveAll(tmpDir)
+		if err != nil {
+			diags = diags.Append(tfdiags.Sourceless(
+				tfdiags.Error,
+				"Failed to clean up temporary directory",
+				fmt.Sprintf("Failed to clean up module package working directory %q: %s.", tmpDir, err),
+			))
+			return "", diags
+		}
+	} else {
+		// We've not encountered this package yet, so we'll rename the
+		// temporary directory into its final location within the bundle.
+		err := os.Rename(tmpDir, finalPath)
+		if err != nil {
+			diags = diags.Append(tfdiags.Sourceless(
+				tfdiags.Error,
+				"Failed to prepare module package directory",
+				fmt.Sprintf("Failed to move module package %q to its final bundle location: %s.", pkgAddr, err),
+			))
+			return "", diags
+		}
+	}
+
+	b.manifest.saveModulePackageBundleDir(pkgAddr, hash)
+
+	return hash, diags
+}
+
+type registryQueueItem struct {
+	sourceAddr      addrs.ModuleSourceRegistry
+	allowedVersions versions.Set
+	depFinder       DependencyFinder
+	srcRng          *tfdiags.SourceRange
+}
+
+type remoteQueueItem struct {
+	sourceAddr addrs.ModuleSourceRemote
+	depFinder  DependencyFinder
+	srcRng     *tfdiags.SourceRange
 }
--- internal/getmodules/sourcebundle/dependency_finder.go
+++ internal/getmodules/sourcebundle/dependency_finder.go
@@ -22,5 +22,38 @@ type DependencyFinder interface {
 	// analyzing the _new_ source location, which does not necessarily need to
 	// be the same as the receiver of this call, for situtions where one kind
 	// of artifact calls into another kind of artifact.
-	FindDependencies(src addrs.ModuleSourceRemote, localPath string, announce func(newSrc addrs.ModuleSource, srcRng *tfdiags.SourceRange, depFinder DependencyFinder)) tfdiags.Diagnostics
+	FindDependencies(src addrs.ModuleSourceRemote, localPath string, deps Dependencies) tfdiags.Diagnostics
+}
+
+// Dependencies is the interface used by a [DependencyFinder] to report the
+// dependencies it has discovered back to the requesting [Builder].
+type Dependencies interface {
+	// AddRemoteSource announces a dependency on a module from a remote
+	// module package, that should be analyzed using the given dependency
+	// finder.
+	//
+	// srcRng is optional and if set should be a source range that a user
+	// would recognize as them configuring the source address given in addr.
+	AddRemoteSource(addr addrs.ModuleSourceRemote, depFinder DependencyFinder, srcRng *tfdiags.SourceRange)
+
+	// AddRegistrySource announces a dependency on a module from a module
+	// package delivered indirectly through a module registry, that should be
+	// analyzed using the given dependency finder.
+	//
+	// srcRng is optional and if set should be a source range that a user
+	// would recognize as them configuring the source address given in addr.
+	AddRegistrySource(addr addrs.ModuleSourceRegistry, depFinder DependencyFinder, srcRng *tfdiags.SourceRange)
+
+	// AddLocalSource announces a dependency on a module at a local path
+	// relative to the module being analyzed, which must therefore belong to
+	// the same module package as that calling module. It will be analyzed
+	// using the given dependency finder.
+	//
+	// If the given relative path traverses out of the module package root
+	// then the calling [Builder] will reject it by returning an error to
+	// the user which blames the module package as being invalid.
+	//
+	// srcRng is optional and if set should be a source range that a user
+	// would recognize as them configuring the source address given in addr.
+	AddLocalSource(addr addrs.ModuleSourceLocal, depFinder DependencyFinder, srcRng *tfdiags.SourceRange)
 }
