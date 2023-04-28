c5b440f78dad5222890f6eda531b856dc4b38c8e
Katy Moe <katy@katy.moe>
2023-04-29 00:24:12 +0100

    add config generation package
    The genconfig package implements HCL config generation from provider state values.
    
    Thanks to @mildwonkey whose implementation of terraform add is the basis for this package.

--- internal/configs/configschema/none_required.go
+++ internal/configs/configschema/none_required.go
@@ -39,3 +39,57 @@ func (a *Attribute) forceOptional() *Attribute {
 	ret.Required = false
 	return &ret
 }
+
+// OmitAttr returns a deep copy of the receiver removing any top-level attribute
+// with the supplied name.
+func (b *Block) OmitAttr(name string) *Block {
+	ret := &Block{}
+
+	if b.Attributes != nil {
+		ret.Attributes = make(map[string]*Attribute, len(b.Attributes))
+	}
+	for attrN, attrS := range b.Attributes {
+		if attrN != name {
+			ret.Attributes[attrN] = attrS
+		}
+	}
+
+	if b.BlockTypes != nil {
+		ret.BlockTypes = make(map[string]*NestedBlock, len(b.BlockTypes))
+	}
+	for name, blockS := range b.BlockTypes {
+		ret.BlockTypes[name] = blockS
+	}
+
+	return ret
+}
+
+// OmitReadOnly returns a deep copy of the receiver filtering out any attributes
+// that are both computed and non-optional.
+func (b *Block) OmitReadOnly() *Block {
+	ret := &Block{}
+
+	if b.Attributes != nil {
+		ret.Attributes = make(map[string]*Attribute, len(b.Attributes))
+	}
+	for name, attrS := range b.Attributes {
+		if !(attrS.Computed && !attrS.Optional) {
+			ret.Attributes[name] = attrS
+		}
+	}
+
+	if b.BlockTypes != nil {
+		ret.BlockTypes = make(map[string]*NestedBlock, len(b.BlockTypes))
+	}
+	for name, blockS := range b.BlockTypes {
+		ret.BlockTypes[name] = blockS.omitReadOnly()
+	}
+
+	return ret
+}
+
+func (b *NestedBlock) omitReadOnly() *NestedBlock {
+	ret := *b
+	ret.Block = *(ret.Block.OmitReadOnly())
+	return &ret
+}
