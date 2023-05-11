60239e1a45be0ab7d65e369485591b7854925698
Lauren <lauren.burwell@hashicorp.com>
2023-05-11 17:04:50 -0400

    add load fn logic to read, unmarshal

--- internal/cloud/backend_saved_plan.go
+++ internal/cloud/backend_saved_plan.go
@@ -3,8 +3,37 @@
 
 package cloud
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 type SavedPlanBookmark struct {
 	RemotePlanFormat int    `json:"remote_plan_format"`
 	RunID            string `json:"run_id"`
 	Hostname         string `json:"hostname"`
+}
+
+func (s *SavedPlanBookmark) load(filepath string) (map[string]interface{}, error) {
+	fmt.Println("Are we only accepting a filepath?")
+
+	path := filepath
+	bookmark := SavedPlanBookmark{}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal([]byte(data), &bookmark); err != nil {
+		panic(err)
+	}
+	return bookmark, err
+}
+
+func (s *SavedPlanBookmark) save() error {
+	fmt.Println("this verifies we can save what to what?")
+
+	return nil
 }
