091c5d1d5e5cfe6e6c664c2dbd1c50ab624b821a
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2023-05-15 10:37:27 -0400

    Update main branch version to 1.6.0

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 // The main version number that is being run at the moment.
-var Version = "1.5.0"
+var Version = "1.6.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
