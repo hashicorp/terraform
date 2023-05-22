c9588e427a7e82570f6133355532eec56c30670a
Lauren <lauren.burwell@hashicorp.com>
2023-05-22 17:42:03 -0400

    add test

---
+++ internal/cloud/backend_saved_plan_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package cloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestCloud_loadandSaveBasic(t *testing.T) {
+	bookmark := &SavedPlanBookmark{
+		RemotePlanFormat: 1,
+		RunID:            "run-GXfuHMkbyHccAGUg",
+		Hostname:         "app.terraform.io",
+	}
+
+	data, _ := json.Marshal(bookmark)
+	err := os.WriteFile("/tmp/test-load-bookmark", data, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	readBookmark, err := os.ReadFile("/tmp/test-load-bookmark")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("Read Ok!", readBookmark)
+}
