be2ad69eda9c66ca2c3ee793720d79faee997334
kmoe <5575356+kmoe@users.noreply.github.com>
2023-05-24 11:16:05 +0100

    plannable import: safer config generation and schema filters (#33232)
    * genconfig: fix nil nested block panic
    
    * genconfig: null NestingSingle blocks should be absent
    
    A NestingSingle block that is null in state should be completely absent from config.
    
    * configschema: make FilterOr variadic
    
    * configschema: apply filters to nested types
    
    * configschema: filter helper/schema id attribute
    
    The legacy SDK adds an Optional+Computed "id" attribute to the
    resource schema even if not defined in provider code.
    During validation, however, the presence of an extraneous "id"
    attribute in config will cause an error.
    Remove this attribute so we do not generate an "id" attribute
    where there is a risk that it is not in the real resource schema.
    
    * configschema: filter test
    
    * terraform: do not pre-validate generated config
    
    Config generated from a resource's import state may fail validation in
    the case of schema behaviours such as ExactlyOneOf and ConflictsWith.
    We don't want to fail the plan now, because that would give the user no
    way to proceed and fix the config to make it valid. We allow the plan to
    complete and output the generated config.
    
    * generate config alongside import process
    
    Rather than waiting until we call `plan()`, generate the configuration
    at the point of the import call, so we have the necessary data to return
    in case planning fails later.
    
    The `plan` and `state` predeclared variables in the plan() method were
    obfuscating the actual return of nil throughout, so those identifiers
    were removed for clarity.
    
    * move generateHCLStringAttributes closer to caller
    
    * store generated config in plan on error
    
    * test for config gen with error
    
    * add simple warning when generating config
    
    ---------
    
    Co-authored-by: James Bardin <j.bardin@gmail.com>

--- internal/configs/configschema/filter.go
+++ internal/configs/configschema/filter.go
@@ -3,8 +3,15 @@ package configschema
 type FilterT[T any] func(string, T) bool
 
 var (
-	FilterReadOnlyAttributes = func(name string, attribute *Attribute) bool {
+	FilterReadOnlyAttribute = func(name string, attribute *Attribute) bool {
 		return attribute.Computed && !attribute.Optional
+	}
+
+	FilterHelperSchemaIdAttribute = func(name string, attribute *Attribute) bool {
+		if name == "id" && attribute.Computed && attribute.Optional {
+			return true
+		}
+		return false
 	}
 
 	FilterDeprecatedAttribute = func(name string, attribute *Attribute) bool {
@@ -16,9 +23,14 @@ var (
 	}
 )
 
-func FilterOr[T any](one, two FilterT[T]) FilterT[T] {
+func FilterOr[T any](filters ...FilterT[T]) FilterT[T] {
 	return func(name string, value T) bool {
-		return one(name, value) || two(name, value)
+		for _, f := range filters {
+			if f(name, value) {
+				return true
+			}
+		}
+		return false
 	}
 }
 
@@ -36,6 +48,10 @@ func (b *Block) Filter(filterAttribute FilterT[*Attribute], filterBlock FilterT[
 		if filterAttribute == nil || !filterAttribute(name, attrS) {
 			ret.Attributes[name] = attrS
 		}
+
+		if attrS.NestedType != nil {
+			ret.Attributes[name].NestedType = filterNestedType(attrS.NestedType, filterAttribute)
+		}
 	}
 
 	if b.BlockTypes != nil {
@@ -49,6 +65,28 @@ func (b *Block) Filter(filterAttribute FilterT[*Attribute], filterBlock FilterT[
 				Nesting:  blockS.Nesting,
 				MinItems: blockS.MinItems,
 				MaxItems: blockS.MaxItems,
+			}
+		}
+	}
+
+	return ret
+}
+
+func filterNestedType(obj *Object, filterAttribute FilterT[*Attribute]) *Object {
+	if obj == nil {
+		return nil
+	}
+
+	ret := &Object{
+		Attributes: map[string]*Attribute{},
+		Nesting:    obj.Nesting,
+	}
+
+	for name, attrS := range obj.Attributes {
+		if filterAttribute == nil || !filterAttribute(name, attrS) {
+			ret.Attributes[name] = attrS
+			if attrS.NestedType != nil {
+				ret.Attributes[name].NestedType = filterNestedType(attrS.NestedType, filterAttribute)
 			}
 		}
 	}
