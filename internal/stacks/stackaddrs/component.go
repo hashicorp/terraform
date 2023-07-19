ebab67b468792c888da066dab3eb4ed455f176d1
Martin Atkins <mart@degeneration.co.uk>
2023-07-18 17:26:21 -0700

    stackaddrs: AbsComponentInstance address parsing
    Addresses of this type will appear in some of our serialization formats
    and so we need a parser function which provides the inverse of the
    "String" method of this type.

--- internal/stacks/stackaddrs/component.go
+++ internal/stacks/stackaddrs/component.go
@@ -1,8 +1,13 @@
 package stackaddrs
 
 import (
+	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/terraform/internal/addrs"
 	"github.com/hashicorp/terraform/internal/collections"
+	"github.com/hashicorp/terraform/internal/tfdiags"
 )
 
 // Component is the address of a "component" block within a stack config.
@@ -61,3 +66,101 @@ type ConfigComponentInstance = InStackConfig[ComponentInstance]
 // AbsComponentInstance places a [ComponentInstance] in the context of a
 // particular [StackInstance].
 type AbsComponentInstance = InStackInstance[ComponentInstance]
+
+func ParseAbsComponentInstance(traversal hcl.Traversal) (AbsComponentInstance, tfdiags.Diagnostics) {
+	if traversal.IsRelative() {
+		// This is always a caller bug: caller must only pass absolute
+		// traversals in here.
+		panic("ParseAbsComponentInstance with relative traversal")
+	}
+
+	stackInst, remain, diags := parseInStackInstancePrefix(traversal)
+	if diags.HasErrors() {
+		return AbsComponentInstance{}, diags
+	}
+
+	// "remain" should now be the keyword "component" followed by a valid
+	// component name, optionally followed by an instance key, and nothing
+	// else.
+	const diagSummary = "Invalid component instance address"
+	if len(remain) < 2 && len(remain) > 3 {
+		rng := remain.SourceRange()
+		// if "remain" is empty then the source range would be zero length,
+		// and so we'll use the original traversal instead.
+		if len(remain) == 0 {
+			rng = traversal.SourceRange()
+		}
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  diagSummary,
+			Detail:   "The component instance address must include the keyword \"component\" followed by a component name.",
+			Subject:  &rng,
+		})
+		return AbsComponentInstance{}, diags
+	}
+
+	if kwStep, ok := remain[0].(hcl.TraverseAttr); !ok || kwStep.Name != "component" {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  diagSummary,
+			Detail:   "The component instance address must include the keyword \"component\" followed by a component name.",
+			Subject:  remain[0].SourceRange().Ptr(),
+		})
+		return AbsComponentInstance{}, diags
+	}
+
+	nameStep, ok := remain[1].(hcl.TraverseAttr)
+	if !ok {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  diagSummary,
+			Detail:   "The component instance address must include the keyword \"component\" followed by a component name.",
+			Subject:  remain[1].SourceRange().Ptr(),
+		})
+		return AbsComponentInstance{}, diags
+	}
+	componentAddr := ComponentInstance{
+		Component: Component{Name: nameStep.Name},
+	}
+
+	if len(remain) == 3 {
+		instStep, ok := remain[2].(hcl.TraverseIndex)
+		if !ok {
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  diagSummary,
+				Detail:   "The final part of a component instance address must be the instance key.",
+				Subject:  remain[2].SourceRange().Ptr(),
+			})
+		}
+		var err error
+		componentAddr.Key, err = addrs.ParseInstanceKey(instStep.Key)
+		if err != nil {
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  diagSummary,
+				Detail:   fmt.Sprintf("Invalid instance key: %s.", err),
+				Subject:  instStep.SourceRange().Ptr(),
+			})
+			return AbsComponentInstance{}, diags
+		}
+	}
+
+	return AbsComponentInstance{
+		Stack: stackInst,
+		Item:  componentAddr,
+	}, diags
+}
+
+func ParseAbsComponentInstanceStr(s string) (AbsComponentInstance, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+	traversal, hclDiags := hclsyntax.ParseTraversalAbs([]byte(s), "", hcl.InitialPos)
+	diags = diags.Append(hclDiags)
+	if diags.HasErrors() {
+		return AbsComponentInstance{}, diags
+	}
+
+	ret, moreDiags := ParseAbsComponentInstance(traversal)
+	diags = diags.Append(moreDiags)
+	return ret, diags
+}
--- internal/stacks/stackaddrs/in_stack.go
+++ internal/stacks/stackaddrs/in_stack.go
@@ -1,6 +1,14 @@
 package stackaddrs
 
-import "github.com/hashicorp/terraform/internal/collections"
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/terraform/internal/addrs"
+	"github.com/hashicorp/terraform/internal/collections"
+	"github.com/hashicorp/terraform/internal/tfdiags"
+)
 
 // StackItemConfig is a type set containing all of the address types that make
 // sense to consider as belonging statically to a [Stack].
@@ -99,4 +107,119 @@ func ConfigForAbs[T interface {
 	StackItemConfig[T]
 }](absAddr InStackInstance[T]) InStackConfig[T] {
 	return Config(absAddr.Stack.ConfigAddr(), absAddr.Item)
+}
+
+// parseInStackInstancePrefix parses as many nested stack traversal steps
+// as possible from the start of the given traversal, and then returns
+// the resulting StackInstance address along with a relative traversal
+// covering all of the remaining traversal steps, if any.
+func parseInStackInstancePrefix(traversal hcl.Traversal) (StackInstance, hcl.Traversal, tfdiags.Diagnostics) {
+	if len(traversal) == 0 {
+		return RootStackInstance, nil, nil
+	}
+
+	const errSummary = "Invalid stack instance address"
+	var diags tfdiags.Diagnostics
+	var stackInst StackInstance
+	for len(traversal) > 0 {
+		switch step := traversal[0].(type) {
+		case hcl.TraverseRoot:
+			if step.Name != "stack" {
+				break
+			}
+		case hcl.TraverseAttr:
+			if step.Name != "stack" {
+				break
+			}
+		default:
+			break
+		}
+
+		// If we get here then we know that we're expecting a valid
+		// stack instance step prefix, which always consists of the
+		// literal step "stack" (which we found above) followed
+		// by an embedded stack name. That might then be followed
+		// by one optional index step for a multi-instance embedded stack.
+		if len(traversal) < 2 {
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  errSummary,
+				Detail:   "The \"stack\" keyword must be followed by an attribute specifying the name of the embedded stack.",
+				Subject:  traversal.SourceRange().Ptr(),
+			})
+			return nil, nil, diags
+		}
+		nameStep, ok := traversal[1].(hcl.TraverseAttr)
+		if !ok {
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  errSummary,
+				Detail:   "The \"stack\" keyword must be followed by an attribute specifying the name of the embedded stack.",
+				Subject:  traversal[1].SourceRange().Ptr(),
+			})
+			return nil, nil, diags
+		}
+		if !hclsyntax.ValidIdentifier(nameStep.Name) {
+			// This check is redundant since the HCL parser should've caught
+			// an invalid identifier while parsing this traversal, but this
+			// is here for robustness in case we obtained this traversal
+			// value in an unusual way.
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  errSummary,
+				Detail:   "A stack name must be a valid identifier.",
+				Subject:  nameStep.SourceRange().Ptr(),
+			})
+			return nil, nil, diags
+		}
+		addrStep := StackInstanceStep{
+			Name: nameStep.Name,
+			Key:  addrs.NoKey,
+		}
+		traversal = traversal[2:] // consume the first two steps that we already dealt with
+		if len(traversal) > 2 {
+			idxStep, ok := traversal[2].(hcl.TraverseIndex)
+			if ok {
+				var err error
+				addrStep.Key, err = addrs.ParseInstanceKey(idxStep.Key)
+				if err != nil {
+					diags = diags.Append(&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  errSummary,
+						Detail:   fmt.Sprintf("Invalid instance key: %s.", err),
+						Subject:  idxStep.SourceRange().Ptr(),
+					})
+					return nil, nil, diags
+				}
+				traversal = traversal[1:] // consume the step we just dealt with
+			}
+		}
+		stackInst = append(stackInst, addrStep)
+	}
+	return stackInst, forceTraversalRelative(traversal), diags
+}
+
+// forceTraversalRelative takes any traversal and if it's absolute transforms
+// it into a relative one by changing the first step from a TraverseRoot
+// to an equivalent TraverseAttr.
+func forceTraversalRelative(given hcl.Traversal) hcl.Traversal {
+	if len(given) == 0 {
+		return nil
+	}
+	firstStep, ok := given[0].(hcl.TraverseRoot)
+	if !ok {
+		return given
+	}
+
+	// If we get here then we have an absolute traversal. We shouldn't
+	// mutate the backing array of the traversal because others might
+	// still be using it, so we'll allocate a new traversal and copy
+	// the steps into it.
+	ret := make(hcl.Traversal, len(given))
+	ret[0] = hcl.TraverseAttr{
+		Name:     firstStep.Name,
+		SrcRange: firstStep.SrcRange,
+	}
+	copy(ret[1:], given[1:])
+	return ret
 }
