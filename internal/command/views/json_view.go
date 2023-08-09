f1ca74f09fbec614d4c4a02d19cc9e640e14ed6b
Liam Cervante <liam.cervante@hashicorp.com>
2023-08-09 15:47:01 +0200

    Update JSON_UI_VERSION after test framework changes

--- internal/command/views/json_view.go
+++ internal/command/views/json_view.go
@@ -17,7 +17,7 @@ import (
 // This version describes the schema of JSON UI messages. This version must be
 // updated after making any changes to this view, the jsonHook, or any of the
 // command/views/json package.
-const JSON_UI_VERSION = "1.1"
+const JSON_UI_VERSION = "1.2"
 
 func NewJSONView(view *View) *JSONView {
 	log := hclog.New(&hclog.LoggerOptions{
