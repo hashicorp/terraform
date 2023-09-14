1bd17ffd80facb97c481740e35564fce3298eb25
The Terraform Team <52939924+teamterraform@users.noreply.github.com>
2023-09-14 00:13:45 -0700

    Backport of Implement TFC integration for private module registry test runs into v1.6 (#33893)
    * backport of commit 812c3a7223fb35f67088f25b806b3ad75fb8176d
    
    * backport of commit 464717a2c49920ae140554d95cfb3b1a2339f46d
    
    * backport of commit 8a6447cdca6ca988ce15561b16ce419d147d42f5
    
    * backport of commit 7c3fa4248074236112e543dba5a74bba7aeac07c
    
    ---------
    
    Co-authored-by: Liam Cervante <liam.cervante@hashicorp.com>

--- internal/command/jsonformat/renderer.go
+++ internal/command/jsonformat/renderer.go
@@ -30,6 +30,19 @@ type JSONLog struct {
 	Diagnostic *viewsjson.Diagnostic  `json:"diagnostic"`
 	Outputs    viewsjson.Outputs      `json:"outputs"`
 	Hook       map[string]interface{} `json:"hook"`
+
+	// Special fields for test messages.
+
+	TestRun  string `json:"@testrun,omitempty"`
+	TestFile string `json:"@testfile,omitempty"`
+
+	TestFileStatus     *viewsjson.TestFileStatus     `json:"test_file,omitempty"`
+	TestRunStatus      *viewsjson.TestRunStatus      `json:"test_run,omitempty"`
+	TestFileCleanup    *viewsjson.TestFileCleanup    `json:"test_cleanup,omitempty"`
+	TestSuiteSummary   *viewsjson.TestSuiteSummary   `json:"test_summary,omitempty"`
+	TestFatalInterrupt *viewsjson.TestFatalInterrupt `json:"test_interrupt,omitempty"`
+	TestState          *State                        `json:"test_state,omitempty"`
+	TestPlan           *Plan                         `json:"test_plan,omitempty"`
 }
 
 const (
@@ -48,6 +61,19 @@ const (
 	LogRefreshStart      JSONLogType = "refresh_start"
 	LogResourceDrift     JSONLogType = "resource_drift"
 	LogVersion           JSONLogType = "version"
+
+	// Test Messages
+
+	LogTestAbstract  JSONLogType = "test_abstract"
+	LogTestFile      JSONLogType = "test_file"
+	LogTestRun       JSONLogType = "test_run"
+	LogTestPlan      JSONLogType = "test_plan"
+	LogTestState     JSONLogType = "test_state"
+	LogTestSummary   JSONLogType = "test_summary"
+	LogTestCleanup   JSONLogType = "test_cleanup"
+	LogTestInterrupt JSONLogType = "test_interrupt"
+	LogTestStatus    JSONLogType = "test_status"
+	LogTestRetry     JSONLogType = "test_retry"
 )
 
 func incompatibleVersions(localVersion, remoteVersion string) bool {
@@ -119,7 +145,13 @@ func (renderer Renderer) RenderLog(log *JSONLog) error {
 		LogPlannedChange,
 		LogProvisionComplete,
 		LogProvisionErrored,
-		LogApplyErrored:
+		LogApplyErrored,
+		LogTestAbstract,
+		LogTestStatus,
+		LogTestRetry,
+		LogTestPlan,
+		LogTestState,
+		LogTestInterrupt:
 		// We won't display these types of logs
 		return nil
 
@@ -167,6 +199,82 @@ func (renderer Renderer) RenderLog(log *JSONLog) error {
 		// generates a plan change summary for us
 		msg := fmt.Sprintf(renderer.Colorize.Color("[bold][green]%s[reset]"), log.Message)
 		renderer.Streams.Println("\n" + msg + "\n")
+
+	case LogTestFile:
+		status := log.TestFileStatus
+
+		var msg string
+		switch status.Progress {
+		case "starting":
+			msg = fmt.Sprintf(renderer.Colorize.Color("%s... [light_gray]in progress[reset]"), status.Path)
+		case "teardown":
+			msg = fmt.Sprintf(renderer.Colorize.Color("%s... [light_gray]tearing down[reset]"), status.Path)
+		case "complete":
+			switch status.Status {
+			case "error", "fail":
+				msg = fmt.Sprintf(renderer.Colorize.Color("%s... [red]fail[reset]"), status.Path)
+			case "pass":
+				msg = fmt.Sprintf(renderer.Colorize.Color("%s... [green]pass[reset]"), status.Path)
+			case "skip", "pending":
+				msg = fmt.Sprintf(renderer.Colorize.Color("%s... [light_gray]%s[reset]"), status.Path, string(status.Status))
+			}
+		}
+
+		renderer.Streams.Println(msg)
+
+	case LogTestRun:
+		status := log.TestRunStatus
+
+		var msg string
+		switch status.Status {
+		case "error", "fail":
+			msg = fmt.Sprintf(renderer.Colorize.Color("  %s... [red]fail[reset]"), status.Run)
+		case "pass":
+			msg = fmt.Sprintf(renderer.Colorize.Color("  %s... [green]pass[reset]"), status.Run)
+		case "skip", "pending":
+			msg = fmt.Sprintf(renderer.Colorize.Color("  %s... [light_gray]%s[reset]"), status.Run, string(status.Status))
+		}
+
+		renderer.Streams.Println(msg)
+
+	case LogTestSummary:
+		renderer.Streams.Println() // We start our summary with a line break.
+
+		summary := log.TestSuiteSummary
+
+		switch summary.Status {
+		case "pending", "skip":
+			renderer.Streams.Print("Executed 0 tests")
+			if summary.Skipped > 0 {
+				renderer.Streams.Printf(", %d skipped.\n", summary.Skipped)
+			} else {
+				renderer.Streams.Println(".")
+			}
+			return nil
+		case "pass":
+			renderer.Streams.Print(renderer.Colorize.Color("[green]Success![reset] "))
+		case "fail", "error":
+			renderer.Streams.Print(renderer.Colorize.Color("[red]Failure![reset] "))
+		}
+
+		renderer.Streams.Printf("%d passed, %d failed", summary.Passed, summary.Failed+summary.Errored)
+		if summary.Skipped > 0 {
+			renderer.Streams.Printf(", %d skipped.\n", summary.Skipped)
+		} else {
+			renderer.Streams.Println(".")
+		}
+
+	case LogTestCleanup:
+		cleanup := log.TestFileCleanup
+
+		renderer.Streams.Eprintln(format.WordWrap(log.Message, renderer.Streams.Stderr.Columns()))
+		for _, resource := range cleanup.FailedResources {
+			if len(resource.DeposedKey) > 0 {
+				renderer.Streams.Eprintf(" - %s (%s)\n", resource.Instance, resource.DeposedKey)
+			} else {
+				renderer.Streams.Eprintf(" - %s\n", resource.Instance)
+			}
+		}
 
 	default:
 		// If the log type is not a known log type, we will just print the log message
