54622c68a54094f8463e998c7a2f4e9ec8dd912f
Martin Atkins <mart@degeneration.co.uk>
2023-09-29 17:07:42 -0700

    stackstate: Generate "raw state" objects for resource instance objects
    Previously we just had a stub that only generated basic external
    description objects. Now we'll also emit "raw state" objects for these,
    and have some initial support for deposed objects too.

--- internal/stacks/stackstate/applied_change.go
+++ internal/stacks/stackstate/applied_change.go
@@ -8,10 +8,14 @@ import (
 
 	"github.com/hashicorp/terraform/internal/configs/configschema"
 	"github.com/hashicorp/terraform/internal/plans"
+	"github.com/hashicorp/terraform/internal/plans/planproto"
 	"github.com/hashicorp/terraform/internal/rpcapi/terraform1"
 	"github.com/hashicorp/terraform/internal/stacks/stackaddrs"
 	"github.com/hashicorp/terraform/internal/stacks/stackstate/statekeys"
+	"github.com/hashicorp/terraform/internal/stacks/tfstackdata1"
 	"github.com/hashicorp/terraform/internal/states"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 // AppliedChange represents a single isolated change, emitted as
@@ -52,57 +56,110 @@ func (ac *AppliedChangeResourceInstance) AppliedChangeProto() (*terraform1.Appli
 	// representation of the state maps become malformed.
 
 	var descs []*terraform1.AppliedChange_ChangeDescription
-	currentObjKey := statekeys.ResourceInstanceObject{
-		ResourceInstance: ac.ResourceInstanceAddr,
-		DeposedKey:       states.NotDeposed,
+	var raws []*terraform1.AppliedChange_RawChange
+
+	moreDescs, moreRaws, err := ac.protosForObject(ac.NewStateSrc.Current, states.NotDeposed)
+	if err != nil {
+		return nil, fmt.Errorf("encoding current object for %s: %w", ac.ResourceInstanceAddr, err)
 	}
-	currentObjKeyRaw := statekeys.String(currentObjKey)
-	if currentObjSrc := ac.NewStateSrc.Current; currentObjSrc != nil {
+	descs = append(descs, moreDescs...)
+	raws = append(raws, moreRaws...)
 
-		// TRICKY: For historical reasons, a states.ResourceInstance
-		// contains pre-JSON-encoded dynamic data ready to be
-		// inserted verbatim into Terraform CLI's traditional
-		// JSON-based state file format. However, our RPC API
-		// exclusively uses MessagePack encoding for dynamic
-		// values, and so we will need to use the ac.Schema to
-		// transcode the data.
-		ty := ac.Schema.ImpliedType()
-		currentObj, err := currentObjSrc.Decode(ty)
+	for dk, objSrc := range ac.NewStateSrc.Deposed {
+		moreDescs, moreRaws, err := ac.protosForObject(objSrc, dk)
 		if err != nil {
-			// It would be _very_ strange to get here because we should just
-			// be reversing the same encoding operation done earlier to
-			// produce this object, using exactly the same schema.
-			return nil, fmt.Errorf("cannot decode new state for %s in preparation for saving it: %w", ac.ResourceInstanceAddr, err)
+			return nil, fmt.Errorf("encoding deposed object %s for %s: %w", dk, ac.ResourceInstanceAddr, err)
 		}
-		encValue, err := plans.NewDynamicValue(currentObj.Value, ty)
-		if err != nil {
-			return nil, fmt.Errorf("cannot encode new state for %s in preparation for saving it: %w", ac.ResourceInstanceAddr, err)
-		}
-		protoValue := terraform1.NewDynamicValue(encValue, currentObjSrc.AttrSensitivePaths)
+		descs = append(descs, moreDescs...)
+		raws = append(raws, moreRaws...)
+	}
+	// FIXME: We also need to emit "deletion" entries for any deposed keys
+	// that were present in the prior state but not present in the new state,
+	// but we'll need stackeval to provide us with the prior state deposed key
+	// information in order to achieve that.
 
+	return &terraform1.AppliedChange{
+		Raw:          raws,
+		Descriptions: descs,
+	}, nil
+}
+
+func (ac *AppliedChangeResourceInstance) protosForObject(objSrc *states.ResourceInstanceObjectSrc, deposedKey states.DeposedKey) ([]*terraform1.AppliedChange_ChangeDescription, []*terraform1.AppliedChange_RawChange, error) {
+	var descs []*terraform1.AppliedChange_ChangeDescription
+	var raws []*terraform1.AppliedChange_RawChange
+
+	// For resource instance objects we use the same key format for both the
+	// raw and description representations, but callers MUST NOT rely on this.
+	objKey := statekeys.ResourceInstanceObject{
+		ResourceInstance: ac.ResourceInstanceAddr,
+		DeposedKey:       deposedKey,
+	}
+	objKeyRaw := statekeys.String(objKey)
+
+	if objSrc == nil {
+		// If the new object is nil then we'll emit a "deleted" description
+		// to ensure that any existing prior state value gets removed.
 		descs = append(descs, &terraform1.AppliedChange_ChangeDescription{
-			Key: currentObjKeyRaw,
-			Description: &terraform1.AppliedChange_ChangeDescription_ResourceInstance{
-				ResourceInstance: &terraform1.AppliedChange_ResourceInstance{
-					Addr: &terraform1.ResourceInstanceInStackAddr{
-						ComponentInstanceAddr: ac.ResourceInstanceAddr.Component.String(),
-						ResourceInstanceAddr:  ac.ResourceInstanceAddr.Item.String(),
-					},
-					NewValue: protoValue,
-				},
-			},
-		})
-	} else {
-		descs = append(descs, &terraform1.AppliedChange_ChangeDescription{
-			Key: currentObjKeyRaw,
+			Key: objKeyRaw,
 			Description: &terraform1.AppliedChange_ChangeDescription_Deleted{
 				Deleted: &terraform1.AppliedChange_Nothing{},
 			},
 		})
+		raws = append(raws, &terraform1.AppliedChange_RawChange{
+			Key:   objKeyRaw,
+			Value: nil, // unset Value field represents "delete" for raw changes
+		})
+		return descs, raws, nil
 	}
-	// TODO: Also deposed objects
 
-	return &terraform1.AppliedChange{
-		Descriptions: descs,
-	}, nil
+	// TRICKY: For historical reasons, a states.ResourceInstance
+	// contains pre-JSON-encoded dynamic data ready to be
+	// inserted verbatim into Terraform CLI's traditional
+	// JSON-based state file format. However, our RPC API
+	// exclusively uses MessagePack encoding for dynamic
+	// values, and so we will need to use the ac.Schema to
+	// transcode the data.
+	ty := ac.Schema.ImpliedType()
+	obj, err := objSrc.Decode(ty)
+	if err != nil {
+		// It would be _very_ strange to get here because we should just
+		// be reversing the same encoding operation done earlier to
+		// produce this object, using exactly the same schema.
+		return nil, nil, fmt.Errorf("cannot decode new state for %s in preparation for saving it: %w", ac.ResourceInstanceAddr, err)
+	}
+	encValue, err := plans.NewDynamicValue(obj.Value, ty)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot encode new state for %s in preparation for saving it: %w", ac.ResourceInstanceAddr, err)
+	}
+	protoValue := terraform1.NewDynamicValue(encValue, objSrc.AttrSensitivePaths)
+
+	descs = append(descs, &terraform1.AppliedChange_ChangeDescription{
+		Key: objKeyRaw,
+		Description: &terraform1.AppliedChange_ChangeDescription_ResourceInstance{
+			ResourceInstance: &terraform1.AppliedChange_ResourceInstance{
+				Addr: &terraform1.ResourceInstanceInStackAddr{
+					ComponentInstanceAddr: ac.ResourceInstanceAddr.Component.String(),
+					ResourceInstanceAddr:  ac.ResourceInstanceAddr.Item.String(),
+				},
+				NewValue: protoValue,
+			},
+		},
+	})
+
+	var raw anypb.Any
+	err = anypb.MarshalFrom(&raw, &tfstackdata1.StateResourceInstanceObjectV1{
+		Value: &planproto.DynamicValue{
+			Msgpack: protoValue.Msgpack,
+		},
+		SensitivePaths: tfstackdata1.Terraform1ToPlanProtoAttributePaths(protoValue.Sensitive),
+	}, proto.MarshalOptions{})
+	if err != nil {
+		return nil, nil, fmt.Errorf("encoding raw state object: %w", err)
+	}
+	raws = append(raws, &terraform1.AppliedChange_RawChange{
+		Key:   objKeyRaw,
+		Value: &raw,
+	})
+
+	return descs, raws, nil
 }
