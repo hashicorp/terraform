992b80e20c600994163b2aa8fa7158e3c1fc1ecb
Liam Cervante <liam.cervante@hashicorp.com>
2023-10-13 11:36:52 +0200

    remove unused function

--- internal/moduletest/file.go
+++ internal/moduletest/file.go
@@ -18,28 +18,3 @@ type File struct {
 
 	Diagnostics tfdiags.Diagnostics
 }
-
-// GetAllVariableDefinitions returns all the definitions we have for named
-// variables in any root config files required by this test file.
-func (file *File) GetAllVariableDefinitions(config *configs.Config) map[string]*configs.Variable {
-	variables := make(map[string]*configs.Variable)
-	for name, variable := range config.Module.Variables {
-		variables[name] = variable
-	}
-
-	for _, run := range file.Runs {
-		if config := run.Config.ConfigUnderTest; config != nil {
-			for name, variable := range config.Module.Variables {
-				if _, exists := variables[name]; exists {
-					// It might be that the same variable definition is shared
-					// between different modules. That's fine, but they could
-					// be different so we'll default to the definition we found
-					// first.
-					continue
-				}
-				variables[name] = variable
-			}
-		}
-	}
-	return variables
-}
