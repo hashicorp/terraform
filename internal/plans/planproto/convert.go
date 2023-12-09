a06db2e1a3fe73f02ae2ebe2e786f1dcf2e1668f
Martin Atkins <mart@degeneration.co.uk>
2023-12-08 19:03:21 -0800

    stackplan: Include extra info in raw component instance plan
    We'll now track the planned action, the component addresses we depend on,
    and the planned output values.

--- internal/plans/planproto/convert.go
+++ internal/plans/planproto/convert.go
@@ -58,3 +58,46 @@ func NewPlanDynamicValue(dv plans.DynamicValue) *DynamicValue {
 		Msgpack: []byte(dv),
 	}
 }
+
+func NewAction(action plans.Action) Action {
+	switch action {
+	case plans.NoOp:
+		return Action_NOOP
+	case plans.Create:
+		return Action_CREATE
+	case plans.Read:
+		return Action_READ
+	case plans.Update:
+		return Action_UPDATE
+	case plans.Delete:
+		return Action_DELETE
+	case plans.DeleteThenCreate:
+		return Action_DELETE_THEN_CREATE
+	case plans.CreateThenDelete:
+		return Action_CREATE_THEN_DELETE
+	default:
+		// The above should be exhaustive for all possible actions
+		panic(fmt.Sprintf("unsupported change action %s", action))
+	}
+}
+
+func FromAction(protoAction Action) (plans.Action, error) {
+	switch protoAction {
+	case Action_NOOP:
+		return plans.NoOp, nil
+	case Action_CREATE:
+		return plans.Create, nil
+	case Action_READ:
+		return plans.Read, nil
+	case Action_UPDATE:
+		return plans.Update, nil
+	case Action_DELETE:
+		return plans.Delete, nil
+	case Action_DELETE_THEN_CREATE:
+		return plans.DeleteThenCreate, nil
+	case Action_CREATE_THEN_DELETE:
+		return plans.CreateThenDelete, nil
+	default:
+		return plans.NoOp, fmt.Errorf("unsupported action %s", protoAction)
+	}
+}
