9246b3a9942999338f5f826eff292186b68e64a4
Liam Cervante <liam.cervante@hashicorp.com>
2024-01-15 17:58:31 +0100

    stacks: on module load convert relative paths to absolute (#34523)

--- internal/stacks/stackruntime/internal/stackeval/testdata/sourcebundle/validating/modules_with_provider_configs/module-a/modules-with-provider-configs-a.tf
+++ internal/stacks/stackruntime/internal/stackeval/testdata/sourcebundle/validating/modules_with_provider_configs/module-a/modules-with-provider-configs-a.tf
@@ -11,10 +11,5 @@ provider "test" {
 }
 
 module "b" {
-  # FIXME: The following is an absolute remote address only because at the
-  # time of writing this test the stacks runtime's module loader can't deal
-  # with relative paths in this location.
-  source = "https://testing.invalid/validating.tar.gz//modules_with_provider_configs/module-b"
-  # Once that's been fixed, this should instead be:
-  # source = "../module-b"
+  source = "../module-b"
 }
