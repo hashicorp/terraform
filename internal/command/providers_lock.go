472a07ad6eaf7ddffc543daa3596b86008db86fd
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-02-08 17:21:58 +0100

    fix typo
    Co-authored-by: Liam Cervante <liam.cervante@hashicorp.com>

--- internal/command/providers_lock.go
+++ internal/command/providers_lock.go
@@ -415,7 +415,7 @@ Options:
 
   -enable-plugin-cache   Enable the usage of the globally configured plugin cache.
                          This will speed up the locking process, but the providers
-                         wont be loaded from am authoritative source.
+                         wont be loaded from an authoritative source.
 `
 }
 
