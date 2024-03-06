49f0d6dd7109424a04745d7ca51fd3744d935698
Martin Atkins <mart@degeneration.co.uk>
2024-03-05 18:36:11 -0800

    backend/http: Use backendbase instead of helper/schema
    The legacy helper/schema package is really just a frozen copy of
    Terraform's old plugin SDK, which is a complicated heap of code that
    nobody at HashiCorp really understands anymore.
    
    This backend will now use the lightweight "backendbase" helper package
    instead, which is at a slightly lower level of abstraction and therefore
    requires more explicit logic during config decoding, but these extra
    calls to helper functions are still considerably less code than the whole
    helper/schema package and its various external dependencies that Terraform
    imports purely to keep it working.

--- internal/backend/remote-state/http/backend.go
+++ internal/backend/remote-state/http/backend.go
@@ -4,7 +4,6 @@
 package http
 
 import (
-	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
@@ -15,128 +14,261 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-retryablehttp"
+	"github.com/zclconf/go-cty/cty"
 
 	"github.com/hashicorp/terraform/internal/backend"
-	"github.com/hashicorp/terraform/internal/legacy/helper/schema"
+	"github.com/hashicorp/terraform/internal/backend/backendbase"
+	"github.com/hashicorp/terraform/internal/configs/configschema"
 	"github.com/hashicorp/terraform/internal/logging"
 	"github.com/hashicorp/terraform/internal/states/remote"
 	"github.com/hashicorp/terraform/internal/states/statemgr"
+	"github.com/hashicorp/terraform/internal/tfdiags"
 )
 
 func New() backend.Backend {
-	s := &schema.Backend{
-		Schema: map[string]*schema.Schema{
-			"address": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_ADDRESS", nil),
-				Description: "The address of the REST endpoint",
-			},
-			"update_method": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_UPDATE_METHOD", "POST"),
-				Description: "HTTP method to use when updating state",
-			},
-			"lock_address": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_LOCK_ADDRESS", nil),
-				Description: "The address of the lock REST endpoint",
-			},
-			"unlock_address": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_UNLOCK_ADDRESS", nil),
-				Description: "The address of the unlock REST endpoint",
-			},
-			"lock_method": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_LOCK_METHOD", "LOCK"),
-				Description: "The HTTP method to use when locking",
-			},
-			"unlock_method": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_UNLOCK_METHOD", "UNLOCK"),
-				Description: "The HTTP method to use when unlocking",
-			},
-			"username": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_USERNAME", nil),
-				Description: "The username for HTTP basic authentication",
-			},
-			"password": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_PASSWORD", nil),
-				Description: "The password for HTTP basic authentication",
-			},
-			"skip_cert_verification": &schema.Schema{
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Default:     false,
-				Description: "Whether to skip TLS verification.",
-			},
-			"retry_max": &schema.Schema{
-				Type:        schema.TypeInt,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_RETRY_MAX", 2),
-				Description: "The number of HTTP request retries.",
-			},
-			"retry_wait_min": &schema.Schema{
-				Type:        schema.TypeInt,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_RETRY_WAIT_MIN", 1),
-				Description: "The minimum time in seconds to wait between HTTP request attempts.",
-			},
-			"retry_wait_max": &schema.Schema{
-				Type:        schema.TypeInt,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_RETRY_WAIT_MAX", 30),
-				Description: "The maximum time in seconds to wait between HTTP request attempts.",
-			},
-			"client_ca_certificate_pem": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_CLIENT_CA_CERTIFICATE_PEM", ""),
-				Description: "A PEM-encoded CA certificate chain used by the client to verify server certificates during TLS authentication.",
-			},
-			"client_certificate_pem": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_CLIENT_CERTIFICATE_PEM", ""),
-				Description: "A PEM-encoded certificate used by the server to verify the client during mutual TLS (mTLS) authentication.",
-			},
-			"client_private_key_pem": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("TF_HTTP_CLIENT_PRIVATE_KEY_PEM", ""),
-				Description: "A PEM-encoded private key, required if client_certificate_pem is specified.",
+	return &Backend{
+		Base: backendbase.Base{
+			Schema: &configschema.Block{
+				Attributes: map[string]*configschema.Attribute{
+					"address": {
+						Type:        cty.String,
+						Optional:    true, // Must be set but can be set using the TF_HTTP_ADDRESS environment variable
+						Description: "The address of the REST endpoint",
+					},
+					"update_method": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "HTTP method to use when updating state",
+					},
+					"lock_address": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The address of the lock REST endpoint",
+					},
+					"unlock_address": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The address of the unlock REST endpoint",
+					},
+					"lock_method": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The HTTP method to use when locking",
+					},
+					"unlock_method": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The HTTP method to use when unlocking",
+					},
+					"username": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The username for HTTP basic authentication",
+					},
+					"password": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "The password for HTTP basic authentication",
+					},
+					"skip_cert_verification": {
+						Type:        cty.Bool,
+						Optional:    true,
+						Description: "Whether to skip TLS verification",
+					},
+					"retry_max": {
+						Type:        cty.Number,
+						Optional:    true,
+						Description: "The number of HTTP request retries",
+					},
+					"retry_wait_min": {
+						Type:        cty.Number,
+						Optional:    true,
+						Description: "The minimum time in seconds to wait between HTTP request attempts",
+					},
+					"retry_wait_max": {
+						Type:        cty.Number,
+						Optional:    true,
+						Description: "The maximum time in seconds to wait between HTTP request attempts",
+					},
+					"client_ca_certificate_pem": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "A PEM-encoded CA certificate chain used by the client to verify server certificates during TLS authentication",
+					},
+					"client_certificate_pem": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "A PEM-encoded certificate used by the server to verify the client during mutual TLS (mTLS) authentication",
+					},
+					"client_private_key_pem": {
+						Type:        cty.String,
+						Optional:    true,
+						Description: "A PEM-encoded private key, required if client_certificate_pem is specified",
+					},
+				},
 			},
 		},
 	}
-
-	b := &Backend{Backend: s}
-	b.Backend.ConfigureFunc = b.configure
-	return b
 }
 
 type Backend struct {
-	*schema.Backend
+	backendbase.Base
 
 	client *httpClient
 }
 
+func (b *Backend) Configure(configVal cty.Value) tfdiags.Diagnostics {
+	address := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "address",
+		"TF_HTTP_ADDRESS", cty.StringVal(""),
+	).AsString()
+	if address == "" {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("address argument is required"),
+		)
+	}
+	updateURL, err := url.Parse(address)
+	if err != nil {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("failed to parse address URL: %s", err),
+		)
+	}
+	if updateURL.Scheme != "http" && updateURL.Scheme != "https" {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("address must be HTTP or HTTPS"),
+		)
+	}
+
+	updateMethod := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "update_method",
+		"TF_HTTP_UPDATE_METHOD", cty.StringVal("POST"),
+	).AsString()
+
+	var lockURL *url.URL
+	if v := backendbase.GetAttrEnvDefault(configVal, "lock_address", "TF_HTTP_LOCK_ADDRESS"); !v.IsNull() {
+		var err error
+		lockURL, err = url.Parse(v.AsString())
+		if err != nil {
+			return backendbase.ErrorAsDiagnostics(
+				fmt.Errorf("failed to parse lock_address URL: %s", err),
+			)
+		}
+		if lockURL.Scheme != "http" && lockURL.Scheme != "https" {
+			return backendbase.ErrorAsDiagnostics(
+				fmt.Errorf("lock_address must be HTTP or HTTPS"),
+			)
+		}
+	}
+	lockMethod := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "lock_method",
+		"TF_HTTP_LOCK_METHOD", cty.StringVal("LOCK"),
+	).AsString()
+
+	var unlockURL *url.URL
+	if v := backendbase.GetAttrEnvDefault(configVal, "unlock_address", "TF_HTTP_UNLOCK_ADDRESS"); !v.IsNull() {
+		var err error
+		unlockURL, err = url.Parse(v.AsString())
+		if err != nil {
+			return backendbase.ErrorAsDiagnostics(
+				fmt.Errorf("failed to parse unlock_address URL: %s", err),
+			)
+		}
+		if unlockURL.Scheme != "http" && unlockURL.Scheme != "https" {
+			return backendbase.ErrorAsDiagnostics(
+				fmt.Errorf("unlock_address must be HTTP or HTTPS"),
+			)
+		}
+	}
+	unlockMethod := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "unlock_method",
+		"TF_HTTP_UNLOCK_METHOD", cty.StringVal("UNLOCK"),
+	).AsString()
+
+	retryMax, err := backendbase.IntValue(
+		backendbase.GetAttrEnvDefaultFallback(
+			configVal, "retry_max",
+			"TF_HTTP_RETRY_MAX", cty.NumberIntVal(2),
+		),
+	)
+	if err != nil {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("invalid retry_max: %s", err),
+		)
+	}
+	retryWaitMin, err := backendbase.IntValue(
+		backendbase.GetAttrEnvDefaultFallback(
+			configVal, "retry_wait_min",
+			"TF_HTTP_RETRY_WAIT_MIN", cty.NumberIntVal(1),
+		),
+	)
+	if err != nil {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("invalid retry_wait_min: %s", err),
+		)
+	}
+	retryWaitMax, err := backendbase.IntValue(
+		backendbase.GetAttrEnvDefaultFallback(
+			configVal, "retry_wait_max",
+			"TF_HTTP_RETRY_WAIT_MAX", cty.NumberIntVal(30),
+		),
+	)
+	if err != nil {
+		return backendbase.ErrorAsDiagnostics(
+			fmt.Errorf("invalid retry_wait_max: %s", err),
+		)
+	}
+
+	rClient := retryablehttp.NewClient()
+	rClient.RetryMax = int(retryMax)
+	rClient.RetryWaitMin = time.Duration(retryWaitMin) * time.Second
+	rClient.RetryWaitMax = time.Duration(retryWaitMax) * time.Second
+	rClient.Logger = log.New(logging.LogOutput(), "", log.Flags())
+	if err = b.configureTLS(rClient, configVal); err != nil {
+		return backendbase.ErrorAsDiagnostics(err)
+	}
+
+	b.client = &httpClient{
+		URL:          updateURL,
+		UpdateMethod: updateMethod,
+
+		LockURL:      lockURL,
+		LockMethod:   lockMethod,
+		UnlockURL:    unlockURL,
+		UnlockMethod: unlockMethod,
+
+		Username: backendbase.GetAttrEnvDefaultFallback(
+			configVal, "username",
+			"TF_HTTP_USERNAME", cty.StringVal(""),
+		).AsString(),
+		Password: backendbase.GetAttrEnvDefaultFallback(
+			configVal, "password",
+			"TF_HTTP_PASSWORD", cty.StringVal(""),
+		).AsString(),
+
+		// accessible only for testing use
+		Client: rClient,
+	}
+	return nil
+}
+
 // configureTLS configures TLS when needed; if there are no conditions requiring TLS, no change is made.
-func (b *Backend) configureTLS(client *retryablehttp.Client, data *schema.ResourceData) error {
+func (b *Backend) configureTLS(client *retryablehttp.Client, configVal cty.Value) error {
 	// If there are no conditions needing to configure TLS, leave the client untouched
-	skipCertVerification := data.Get("skip_cert_verification").(bool)
-	clientCACertificatePem := data.Get("client_ca_certificate_pem").(string)
-	clientCertificatePem := data.Get("client_certificate_pem").(string)
-	clientPrivateKeyPem := data.Get("client_private_key_pem").(string)
+	skipCertVerification := backendbase.MustBoolValue(
+		backendbase.GetAttrDefault(configVal, "skip_cert_verification", cty.False),
+	)
+	clientCACertificatePem := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "client_ca_certificate_pem",
+		"TF_HTTP_CLIENT_CA_CERTIFICATE_PEM", cty.StringVal(""),
+	).AsString()
+	clientCertificatePem := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "client_certificate_pem",
+		"TF_HTTP_CLIENT_CERTIFICATE_PEM", cty.StringVal(""),
+	).AsString()
+	clientPrivateKeyPem := backendbase.GetAttrEnvDefaultFallback(
+		configVal, "client_private_key_pem",
+		"TF_HTTP_CLIENT_PRIVATE_KEY_PEM", cty.StringVal(""),
+	).AsString()
 	if !skipCertVerification && clientCACertificatePem == "" && clientCertificatePem == "" && clientPrivateKeyPem == "" {
 		return nil
 	}
@@ -171,75 +303,6 @@ func (b *Backend) configureTLS(client *retryablehttp.Client, data *schema.Resour
 		tlsConfig.Certificates = []tls.Certificate{certificate}
 	}
 
-	return nil
-}
-
-func (b *Backend) configure(ctx context.Context) error {
-	data := schema.FromContextBackendConfig(ctx)
-
-	address := data.Get("address").(string)
-	updateURL, err := url.Parse(address)
-	if err != nil {
-		return fmt.Errorf("failed to parse address URL: %s", err)
-	}
-	if updateURL.Scheme != "http" && updateURL.Scheme != "https" {
-		return fmt.Errorf("address must be HTTP or HTTPS")
-	}
-
-	updateMethod := data.Get("update_method").(string)
-
-	var lockURL *url.URL
-	if v, ok := data.GetOk("lock_address"); ok && v.(string) != "" {
-		var err error
-		lockURL, err = url.Parse(v.(string))
-		if err != nil {
-			return fmt.Errorf("failed to parse lockAddress URL: %s", err)
-		}
-		if lockURL.Scheme != "http" && lockURL.Scheme != "https" {
-			return fmt.Errorf("lockAddress must be HTTP or HTTPS")
-		}
-	}
-
-	lockMethod := data.Get("lock_method").(string)
-
-	var unlockURL *url.URL
-	if v, ok := data.GetOk("unlock_address"); ok && v.(string) != "" {
-		var err error
-		unlockURL, err = url.Parse(v.(string))
-		if err != nil {
-			return fmt.Errorf("failed to parse unlockAddress URL: %s", err)
-		}
-		if unlockURL.Scheme != "http" && unlockURL.Scheme != "https" {
-			return fmt.Errorf("unlockAddress must be HTTP or HTTPS")
-		}
-	}
-
-	unlockMethod := data.Get("unlock_method").(string)
-
-	rClient := retryablehttp.NewClient()
-	rClient.RetryMax = data.Get("retry_max").(int)
-	rClient.RetryWaitMin = time.Duration(data.Get("retry_wait_min").(int)) * time.Second
-	rClient.RetryWaitMax = time.Duration(data.Get("retry_wait_max").(int)) * time.Second
-	rClient.Logger = log.New(logging.LogOutput(), "", log.Flags())
-	if err = b.configureTLS(rClient, data); err != nil {
-		return err
-	}
-
-	b.client = &httpClient{
-		URL:          updateURL,
-		UpdateMethod: updateMethod,
-
-		LockURL:      lockURL,
-		LockMethod:   lockMethod,
-		UnlockURL:    unlockURL,
-		UnlockMethod: unlockMethod,
-
-		Username: data.Get("username").(string),
-		Password: data.Get("password").(string),
-
-		// accessible only for testing use
-		Client: rClient,
-	}
 	return nil
 }
 
