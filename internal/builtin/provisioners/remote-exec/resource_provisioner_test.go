154f9ada90a93daed69ad045c98b2b8a2472a298
James Bardin <j.bardin@gmail.com>
2024-03-11 12:11:37 -0400

    disconnect remote-exec immediately
    The remote-exec provisioner was not disconnecting immediately after
    provisioning. The remote exec context was tired to the lifetime of the
    provisioner, but the provisioner node used to not live long after the
    remote execution had completed so it was not noticed. Some more recent
    refactoring extends the lifetime of the node, and causes the provisioner
    connections to hang around longer when they're not needed.
    
    Ensure we disconnect immediately after the remote execution is complete.

--- internal/builtin/provisioners/remote-exec/resource_provisioner_test.go
+++ internal/builtin/provisioners/remote-exec/resource_provisioner_test.go
@@ -257,6 +257,13 @@ func TestProvisionerTimeout(t *testing.T) {
 	if runErr != nil {
 		t.Fatal(err)
 	}
+
+	// make sure provisioner disconnected after the commands were run
+	select {
+	case <-disconnected:
+	case <-time.After(2 * time.Second):
+		t.Fatal("communicator did not disconnect")
+	}
 }
 
 // Validate that Stop can Close can be called even when not provisioning.
