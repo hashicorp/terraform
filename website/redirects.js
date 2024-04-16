1fd45d83d8aa375d9e37e89f3ecac543b2f706c3
Rose <rose.koron@hashicorp.com>
2024-04-16 16:08:49 -0700

    Rename cloud file and add redirects

---
+++ website/redirects.js
@@ -0,0 +1,11 @@
+/**
+ * Redirects in this file are intended to be for documentation content only. The redirects will be applied to developer.hashicorp.com.
+ */
+module.exports = [
+  // HCP Terraform rebranding
+  {
+    source: "/terraform/language/settings/terraform-cloud",
+    destination: "/terraform/language/settings/cloud",
+    permanent: true,
+  }
+];
