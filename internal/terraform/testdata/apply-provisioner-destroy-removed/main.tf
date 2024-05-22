ddd7ddd1ed2c38d84f6f70eff52dd0b650c953b1
James Bardin <j.bardin@gmail.com>
2024-05-22 11:20:51 -0400

    use try() in removed provisioner test
    This records in a test that both `count.index` and `each.key` will
    validate properly within a `removed` block provisioner.

--- internal/terraform/testdata/apply-provisioner-destroy-removed/main.tf
+++ internal/terraform/testdata/apply-provisioner-destroy-removed/main.tf
@@ -3,6 +3,10 @@ removed {
 
   provisioner "shell" {
     when     = "destroy"
-    command  = "destroy ${each.key} ${self.foo}"
+
+    // Capture that we can reference either count.index or each.key from a
+    // removed block, and it's up to the user to ensure the provisioner is
+    // correct for the now removed resources.
+    command  = "destroy ${try(count.index, each.key)} ${self.foo}"
   }
 }
