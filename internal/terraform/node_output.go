3cc01159312525fd7120a800c010c5200d65064c
Nick Fagerlund <nick.fagerlund@gmail.com>
2024-06-10 18:33:11 -0700

    PR feedback: Go straight to null instead of detouring through unknown

--- internal/terraform/node_output.go
+++ internal/terraform/node_output.go
@@ -782,14 +782,13 @@ func (n *NodeApplyableOutput) setValue(namedVals *namedvals.State, state *states
 		if n.Addr.Module.IsRoot() {
 			val, _ = val.UnmarkDeep()
 			if val.IsKnown() && !val.IsWhollyKnown() {
-				// For the version of the value that ends up in state, treat
-				// partially-unknown values as wholly unknown. The goal is to avoid
-				// confusion with outputs that reference deferred values; since the
-				// referenced resource is still unready, we don't want to store
-				// planned-but-not-verified speculations in the source of truth.
-				val = cty.UnknownVal(val.Type())
+				// If the value is partially unknown, act like it's fully
+				// unknown and store a null value in state. This can happen if
+				// an output references a deferred value.
+				val = cty.NullVal(val.Type())
+			} else {
+				val = cty.UnknownAsNull(val)
 			}
-			val = cty.UnknownAsNull(val)
 		}
 		state.SetOutputValue(n.Addr, val, n.Config.Sensitive)
 	}
