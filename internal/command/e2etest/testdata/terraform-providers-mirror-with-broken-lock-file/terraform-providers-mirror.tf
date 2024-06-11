3875ea36560d0383541f48db3f4e863e0e2f7c70
James Bardin <j.bardin@gmail.com>
2024-06-11 16:33:28 -0400

    add -lock-file flag to providers mirror command
    The providers mirror command was updated to inspect the lock file,
    however that was not part of the original intent for the command, and
    it's possible that the command needs to be run without a lock file.
    
    Since we have been honoring the lock file for the past few releases,
    let's keep that consistent and allow disabling the file with
    `-lock-file=false`.

--- internal/command/e2etest/testdata/terraform-providers-mirror-with-broken-lock-file/terraform-providers-mirror.tf
+++ internal/command/e2etest/testdata/terraform-providers-mirror-with-broken-lock-file/terraform-providers-mirror.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    template  = { source = "hashicorp/template" }
-    null      = { source = "hashicorp/null" }
+    template  = { version = "2.1.1" }
+    null      = { source = "hashicorp/null", version = "2.1.0" }
     terraform = { source = "terraform.io/builtin/terraform" }
   }
 }
