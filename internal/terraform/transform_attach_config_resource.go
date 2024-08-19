87b6f3ff348c8e5a7ae04cfa44e1c4c4d9bc5c89
James Bardin <j.bardin@gmail.com>
2024-08-19 15:43:19 +0000

    backport of commit c6202df069c24bcf66e9a25e0ea9eaef1e1deb0b

--- internal/terraform/transform_attach_config_resource.go
+++ internal/terraform/transform_attach_config_resource.go
@@ -34,6 +34,22 @@ type AttachResourceConfigTransformer struct {
 }
 
 func (t *AttachResourceConfigTransformer) Transform(g *Graph) error {
+	// Collect removed blocks to attach to any resources. These are collected
+	// independently because removed blocks may live in a parent module of the
+	// resource referenced.
+	removed := addrs.MakeMap[addrs.ConfigResource, *configs.Removed]()
+
+	t.Config.DeepEach(func(c *configs.Config) {
+		for _, rem := range c.Module.Removed {
+			resAddr, ok := rem.From.RelSubject.(addrs.ConfigResource)
+			if !ok {
+				// Not for a resource at all, so can't possibly match.
+				// Non-resource removed targets have nothing to attach.
+				continue
+			}
+			removed.Put(resAddr, rem)
+		}
+	})
 
 	// Go through and find GraphNodeAttachResource
 	for _, v := range g.Vertices() {
@@ -45,6 +61,12 @@ func (t *AttachResourceConfigTransformer) Transform(g *Graph) error {
 
 		// Determine what we're looking for
 		addr := arn.ResourceAddr()
+
+		// Check for a removed block first, since that would preclude any resource config.
+		if remCfg, ok := removed.GetOk(addr); ok {
+			log.Printf("[TRACE] AttachResourceConfigTransformer: attaching to %q (%T) removed block from %#v", dag.VertexName(v), v, remCfg.DeclRange)
+			arn.AttachResourceConfig(nil, remCfg)
+		}
 
 		// Get the configuration.
 		config := t.Config.Descendent(addr.Module)
@@ -64,27 +86,6 @@ func (t *AttachResourceConfigTransformer) Transform(g *Graph) error {
 					log.Printf("[TRACE] AttachResourceConfigTransformer: no provider meta configs available to attach to %s", dag.VertexName(v))
 				}
 			}
-		}
-
-		for _, r := range config.Module.Removed {
-			crAddr, ok := r.From.RelSubject.(addrs.ConfigResource)
-			if !ok {
-				// Not for a resource at all, so can't possibly match
-				continue
-			}
-			rAddr := crAddr.Resource
-			if rAddr != addr.Resource {
-				// Not the same resource
-				continue
-			}
-
-			log.Printf("[TRACE] AttachResourceConfigTransformer: attaching to %q (%T) removed block from %#v", dag.VertexName(v), v, r.DeclRange)
-
-			// Validation ensures that there can't be both a resource/data block
-			// and a removed block referring to the same configuration, so
-			// we can assume that this isn't clobbering a non-removed resource
-			// configuration we already attached above.
-			arn.AttachResourceConfig(nil, r)
 		}
 	}
 
