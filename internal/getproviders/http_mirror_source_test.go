3b6790fd7bbc75338fd7f13966555ad01d9bbfa6
Andrei <55236384+416e64726579@users.noreply.github.com>
2024-08-30 19:15:40 +0200

    Add support for mirror registries behind mutual TLS

--- internal/getproviders/http_mirror_source_test.go
+++ internal/getproviders/http_mirror_source_test.go
@@ -5,11 +5,21 @@ package getproviders
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
+	"math/big"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	svchost "github.com/hashicorp/terraform-svchost"
@@ -324,4 +334,185 @@ func testHTTPMirrorSourceHandler(resp http.ResponseWriter, req *http.Request) {
 		resp.WriteHeader(404)
 		fmt.Fprintln(resp, "not found")
 	}
+}
+
+func TestHTTPMirrorSource_mTLS(t *testing.T) {
+	caCert, caPriv, err := generateCA()
+	if err != nil {
+		t.Fatalf("failed to generate CA: %v", err)
+	}
+
+	// Generate a server certificate signed by the test CA
+	serverCert, _, err := generateCertSignedByCAWithKey(caCert, caPriv, true) // true indicates server certificate
+	if err != nil {
+		t.Fatalf("failed to generate server certificate: %v", err)
+	}
+
+	serverTLSConfig := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    x509.NewCertPool(),
+		ServerName:   "127.0.0.1",
+	}
+	serverTLSConfig.ClientCAs.AddCert(caCert)
+
+	httpServer := httptest.NewUnstartedServer(http.HandlerFunc(testHTTPMirrorSourceHandler))
+	httpServer.TLS = serverTLSConfig
+	httpServer.StartTLS()
+	defer httpServer.Close()
+
+	clientCert, clientKey, err := generateCertSignedByCAWithKey(caCert, caPriv, false)
+	if err != nil {
+		t.Fatalf("failed to generate client certificate: %v", err)
+	}
+
+	tempCertFile, err := os.CreateTemp("", "cert.pem")
+	if err != nil {
+		t.Fatalf("failed to create temp cert file: %v", err)
+	}
+	defer os.Remove(tempCertFile.Name())
+
+	tempKeyFile, err := os.CreateTemp("", "key.pem")
+	if err != nil {
+		t.Fatalf("failed to create temp key file: %v", err)
+	}
+	defer os.Remove(tempKeyFile.Name())
+
+	tempCACertFile, err := os.CreateTemp("", "ca_cert.pem")
+	if err != nil {
+		t.Fatalf("failed to create temp ca cert file: %v", err)
+	}
+	defer os.Remove(tempCACertFile.Name())
+
+	if err := savePEMFile(tempCertFile.Name(), "CERTIFICATE", clientCert.Certificate[0]); err != nil {
+		t.Fatalf("failed to save client cert: %v", err)
+	}
+	if err := savePEMFile(tempKeyFile.Name(), "RSA PRIVATE KEY", x509.MarshalPKCS1PrivateKey(clientKey)); err != nil {
+		t.Fatalf("failed to save client key: %v", err)
+	}
+	if err := savePEMFile(tempCACertFile.Name(), "CERTIFICATE", caCert.Raw); err != nil {
+		t.Fatalf("failed to save CA cert: %v", err)
+	}
+
+	creds := svcauth.StaticCredentialsSource(map[svchost.Hostname]map[string]interface{}{
+		svchost.Hostname(httpServer.Listener.Addr().String()): {
+			"token":       "placeholder-token", // this is required by the handler but not required by actual HTTPMirrorSource
+			"ca_cert":     tempCACertFile.Name(),
+			"client_cert": tempCertFile.Name(),
+			"client_key":  tempKeyFile.Name(),
+		},
+	})
+
+	httpClient := httpServer.Client()
+	baseURL, err := url.Parse(httpServer.URL)
+	if err != nil {
+		t.Fatalf("httptest.StartTLS returned a server with an invalid URL")
+	}
+	source := newHTTPMirrorSourceWithHTTPClient(baseURL, creds, httpClient)
+
+	existingProvider := addrs.MustParseProviderSourceString("terraform.io/test/exists")
+
+	t.Run("AvailableVersions for provider that exists with mTLS and token", func(t *testing.T) {
+		got, _, err := source.AvailableVersions(context.Background(), existingProvider)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		want := VersionList{
+			MustParseVersion("1.0.0"),
+			MustParseVersion("1.0.1"),
+			MustParseVersion("1.0.2-beta.1"),
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("wrong result\n%s", diff)
+		}
+	})
+
+	t.Run("AvailableVersions without required mTLS credentials", func(t *testing.T) {
+		unauthSource := newHTTPMirrorSourceWithHTTPClient(baseURL, nil, httpClient)
+		_, _, err := unauthSource.AvailableVersions(context.Background(), existingProvider)
+		switch err := err.(type) {
+		case ErrUnauthorized:
+			if got, want := string(err.Hostname), baseURL.Host; got != want {
+				t.Errorf("wrong hostname in error\ngot:  %s\nwant: %s", got, want)
+			}
+		default:
+			t.Fatalf("wrong error type %T; want ErrUnauthorized", err)
+		}
+	})
+}
+
+func generateCertSignedByCAWithKey(caCert *x509.Certificate, caPriv *rsa.PrivateKey, isServer bool) (tls.Certificate, *rsa.PrivateKey, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return tls.Certificate{}, nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber:          big.NewInt(2),
+		Subject:               pkix.Name{Organization: []string{"Test Org"}, CommonName: "127.0.0.1"},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+
+	if isServer {
+		template.IPAddresses = []net.IP{net.ParseIP("127.0.0.1")}
+		template.DNSNames = []string{"127.0.0.1"}
+	} else {
+		template.DNSNames = []string{"127.0.0.1"}
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, caCert, &priv.PublicKey, caPriv)
+	if err != nil {
+		return tls.Certificate{}, nil, err
+	}
+
+	cert := tls.Certificate{
+		Certificate: [][]byte{certDER},
+		PrivateKey:  priv,
+		Leaf:        &template,
+	}
+
+	return cert, priv, nil
+}
+
+func generateCA() (*x509.Certificate, *rsa.PrivateKey, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber:          big.NewInt(1),
+		Subject:               pkix.Name{Organization: []string{"Test CA"}},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(24 * time.Hour),
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cert, err := x509.ParseCertificate(certDER)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cert, priv, nil
+}
+
+func savePEMFile(filename, pemType string, pemBytes []byte) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return pem.Encode(file, &pem.Block{Type: pemType, Bytes: pemBytes})
 }
