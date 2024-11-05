e84c6c3c6b933b1c22056ba8d71cf5302ef38a2b
Liam Cervante <liam.cervante@hashicorp.com>
2024-11-05 13:23:06 +0100

    stacks: load credentials from config file on startup

--- internal/command/cliconfig/plugins.go
+++ internal/command/cliconfig/plugins.go
@@ -1,27 +1,25 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: BUSL-1.1
 
-package main
+package cliconfig
 
 import (
 	"fmt"
 	"log"
 	"path/filepath"
 	"runtime"
-
-	"github.com/hashicorp/terraform/internal/command/cliconfig"
 )
 
-// globalPluginDirs returns directories that should be searched for
+// GlobalPluginDirs returns directories that should be searched for
 // globally-installed plugins (not specific to the current configuration).
 //
 // Earlier entries in this slice get priority over later when multiple copies
 // of the same plugin version are found, but newer versions always override
 // older versions where both satisfy the provider version constraints.
-func globalPluginDirs() []string {
+func GlobalPluginDirs() []string {
 	var ret []string
 	// Look in ~/.terraform.d/plugins/ , or its equivalent on non-UNIX
-	dir, err := cliconfig.ConfigDir()
+	dir, err := ConfigDir()
 	if err != nil {
 		log.Printf("[ERROR] Error finding global config directory: %s", err)
 	} else {
