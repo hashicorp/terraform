f25e37e91fad43a0f0225ee8bd84795b6de20785
Liam Cervante <liam.cervante@hashicorp.com>
2024-11-26 08:53:31 +0000

    backport of commit e9b3a544db1012af8a50d20c7183c9616d7d73d2

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -4,15 +4,27 @@ set -uo pipefail
 
 function usage {
   cat <<-'EOF'
-Usage: ./changelog.sh <version> <date>
+Usage: ./changelog.sh <command> [<options>]
 
 Description:
-  This script will update the first line in the CHANGELOG.md file with the given
-  version and date.
+  This script will update CHANGELOG.md with the given version and date.
+
+Commands:
+  prepare <version> <date>
+    prepare updates the first line in the CHANGELOG.md file with the
+    given version and date.
+
+    ./changelog.sh prepare 1.0.0 "November 1, 2021"
+
+  cleanup <released-version> <next-version>
+    cleanup prepends a new section to the CHANGELOG.md file with the given
+    version and (Unreleased) as the date. If the released version contains a
+    pre-release tag, the next version will replace the top line instead of
+    inserting a new section.
 EOF
 }
 
-function update_changelog {
+function prepare {
   VERSION="${1:-}"
   DATE="${2:-}"
 
@@ -25,5 +37,41 @@ function update_changelog {
   sed -i '' -e "1s/.*/## $VERSION ($DATE)/" CHANGELOG.md
 }
 
-update_changelog "$@"
+function cleanup {
+  RELEASED_VERSION="${1:-}"
+  NEXT_VERSION="${2:-}"
+
+  if [[ -z "$RELEASED_VERSION" || -z "$NEXT_VERSION" ]]; then
+    echo "missing at least one of [<released-version>, <next-version>] arguments"
+    usage
+    exit 1
+  fi
+
+  if [[ "$RELEASED_VERSION" == *-* ]]; then
+    # then we have a pre-release version, so we should replace the top line
+    sed -i '' -e "1s/.*/## $NEXT_VERSION (Unreleased)/" CHANGELOG.md
+  else
+    sed -i '' -e "1s/^/## $NEXT_VERSION (Unreleased)\n\n/" CHANGELOG.md
+  fi
+}
+
+function main {
+  case "$1" in
+    prepare)
+      prepare "${@:2}"
+
+      ;;
+    cleanup)
+      cleanup "${@:2}"
+
+      ;;
+    *)
+      usage
+      exit 1
+
+      ;;
+  esac
+}
+
+main "$@"
 exit $?
