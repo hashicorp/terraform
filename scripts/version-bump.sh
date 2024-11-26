9fe58c4166f8ac6390e492bec64b22e0d98ce128
Liam Cervante <liam.cervante@hashicorp.com>
2024-11-26 08:23:17 +0000

    backport of commit e60aae3b0e99ba88cfc23c46eb2fc7336aa2d223

---
+++ scripts/version-bump.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+
+function usage {
+  cat <<-'EOF'
+Usage: ./version-bump.sh <version>
+
+Description:
+  This script will update the version/VERSION file with the given version.
+EOF
+}
+
+function update_version {
+  VERSION="${1:-}"
+
+  if [[ -z "$VERSION" ]]; then
+    echo "missing at least one of [<version>] arguments"
+    usage
+    exit 1
+  fi
+
+  echo "$VERSION" > version/VERSION
+}
+
+update_version "$@"
+exit $?
