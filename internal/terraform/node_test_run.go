0b5f5dcfab6c867445c7ca2403d914e16cde2cb3
Samsondeen Dare <samsondeen.dare@hashicorp.com>
2025-01-08 16:34:15 +0100

    Build and execute tf test file runs in a graph

---
+++ internal/terraform/node_test_run.go
@@ -0,0 +1,27 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package terraform
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/internal/moduletest"
+)
+
+type NodeTestRun struct {
+	file *moduletest.File
+	run  *moduletest.Run
+}
+
+func (n *NodeTestRun) Run() *moduletest.Run {
+	return n.run
+}
+
+func (n *NodeTestRun) File() *moduletest.File {
+	return n.file
+}
+
+func (n *NodeTestRun) Name() string {
+	return fmt.Sprintf("%s.%s", n.file.Name, n.run.Name)
+}
