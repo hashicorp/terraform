ab6e4f2299ff073667a025ab83a5a4ee29b8fd2e
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2025-01-13 13:25:58 +0000

    Refactor JUnit XML output of `terraform test` into a new `junit` package (#36304)
    * Refactor JUnit XML output to use new concept of an Artifact
    
    * Move JUnit-related code into new `artifact` package
    
    * Refactor Artifact's Save method to return diagnostics, update comments
    
    Previously TestJUnitXMLFile implemented the View interface, which cannot return errors. Now it's not a View any more we can simplify things.
    
    * Make junitXMLTestReport output deterministic by iterating over a slice instead of a map, add test
    
    * Provide sources to junitXMLTestReport, allowing complete error messages in the XML
    
    We need to ensure that artifact.NewTestJUnitXMLFile is called once the config Loader is available as a non-nil pointer
    
    * Whitespace
    
    * Add some test coverage for JUnit XML output for `terraform test`
    
    * Refactor how file is saved, add tests
    
    * Move XML structs definitions outside of `junitXMLTestReport`
    
    * Fix nil pointer bug
    
    * Add missing file headers
    
    * Refactor comparison of byte slices
    
    * Rename package to `junit`, rename structs to match
    
    * Add a test showing JUnit output when a test is skipped by the user

--- internal/backend/local/test.go
+++ internal/backend/local/test.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/hashicorp/terraform/internal/addrs"
 	"github.com/hashicorp/terraform/internal/backend/backendrun"
+	"github.com/hashicorp/terraform/internal/command/junit"
 	"github.com/hashicorp/terraform/internal/command/views"
 	"github.com/hashicorp/terraform/internal/configs"
 	"github.com/hashicorp/terraform/internal/lang"
@@ -48,7 +49,8 @@ type TestSuiteRunner struct {
 
 	Opts *terraform.ContextOpts
 
-	View views.Test
+	View  views.Test
+	JUnit junit.JUnit
 
 	// Stopped and Cancelled track whether the user requested the testing
 	// process to be interrupted. Stopped is a nice graceful exit, we'll still
@@ -170,6 +172,14 @@ func (runner *TestSuiteRunner) Test() (moduletest.Status, tfdiags.Diagnostics) {
 	}
 
 	runner.View.Conclusion(suite)
+
+	if runner.JUnit != nil {
+		artifactDiags := runner.JUnit.Save(suite)
+		diags = diags.Append(artifactDiags)
+		if artifactDiags.HasErrors() {
+			return moduletest.Error, diags
+		}
+	}
 
 	return suite.Status, diags
 }
