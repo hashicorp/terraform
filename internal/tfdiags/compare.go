4b18e3af0dd1cfe2bb40e3ba52d39b6ed579c30d
Sarah French <sarah.french@hashicorp.com>
2025-02-06 19:02:15 +0000

    Add comparer that uses sources (Subject or Context) when comparing diags

--- internal/tfdiags/compare.go
+++ internal/tfdiags/compare.go
@@ -35,3 +35,70 @@ func diagnosticComparerSimple(l, r Diagnostic) bool {
 	}
 	return lp.Equals(rp)
 }
+
+// DiagnosticComparerWithSource returns a cmp.Option that can be used with
+// the package github.com/google/go-cmp/cmp.
+//
+// The comparer expands on the DiagnosticComparer by additionally
+// comparing based on:
+// 1) Source.Subject
+// 2) Source.Context
+//
+// Example usage:
+//
+//	cmp.Diff(diag1, diag2, tfdiags.DiagnosticComparerWithSource)
+var DiagnosticComparerWithSource cmp.Option = cmp.Comparer(diagnosticComparerWithSource)
+
+// diagnosticComparerWithSource returns false when a difference is identified between
+// the two Diagnostic arguments.
+func diagnosticComparerWithSource(l, r Diagnostic) bool {
+	// Compare diagnostics using high-level fields
+	same := diagnosticComparerSimple(l, r)
+	if !same {
+		return false
+	}
+
+	// Is there a Subject mismatch?
+	lHasSource := l.Source().Subject != nil
+	rHasSource := r.Source().Subject != nil
+	if lHasSource && !rHasSource {
+		return false
+	}
+	if !lHasSource && rHasSource {
+		return false
+	}
+	if lHasSource && rHasSource {
+		if l.Source().Subject.Filename != r.Source().Subject.Filename {
+			return false
+		}
+		if l.Source().Subject.Start != r.Source().Subject.Start {
+			return false
+		}
+		if l.Source().Subject.End != r.Source().Subject.End {
+			return false
+		}
+	}
+
+	// Is there a Context mismatch?
+	lHasContext := l.Source().Context != nil
+	rHasContext := r.Source().Context != nil
+	if lHasContext && !rHasContext {
+		return false
+	}
+	if !lHasContext && rHasContext {
+		return false
+	}
+	if lHasContext && rHasContext {
+		if l.Source().Context.Filename != r.Source().Context.Filename {
+			return false
+		}
+		if l.Source().Context.Start != r.Source().Context.Start {
+			return false
+		}
+		if l.Source().Context.End != r.Source().Context.End {
+			return false
+		}
+	}
+
+	return true
+}
