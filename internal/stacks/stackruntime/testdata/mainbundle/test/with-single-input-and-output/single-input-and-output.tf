45336179b60f12e9dd2b155450469cfe45793a12
Samsondeen Dare <samsondeen.dare@hashicorp.com>
2025-02-12 13:33:35 +0100

    remove unneeded testdata

--- internal/stacks/stackruntime/testdata/mainbundle/test/with-single-input-and-output/single-input-and-output.tf
+++ internal/stacks/stackruntime/testdata/mainbundle/test/with-single-input-and-output/single-input-and-output.tf
@@ -22,12 +22,6 @@ resource "testing_resource" "data" {
   value = var.input
 }
 
-resource "testing_resource" "another" {
-  count = 2
-  id    = var.id
-  value = var.input
-}
-
 output "id" {
   value = testing_resource.data.id
 }
