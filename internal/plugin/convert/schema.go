851b7c998953b582e89bd9899c7857121a01f6b4
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-02-12 14:43:13 +0100

    validate resource identity schema

--- internal/plugin/convert/schema.go
+++ internal/plugin/convert/schema.go
@@ -5,11 +5,13 @@ package convert
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
 	"sort"
 
 	"github.com/hashicorp/terraform/internal/configs/configschema"
 	"github.com/hashicorp/terraform/internal/providers"
+	"github.com/hashicorp/terraform/internal/tfdiags"
 	proto "github.com/hashicorp/terraform/internal/tfplugin5"
 )
 
@@ -189,7 +191,8 @@ func sortedKeys(m interface{}) []string {
 	return keys
 }
 
-func ProtoToResourceIdentitySchema(s *proto.ResourceIdentitySchema) providers.IdentitySchema {
+func ProtoToResourceIdentitySchema(s *proto.ResourceIdentitySchema) (providers.IdentitySchema, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
 	schema := providers.IdentitySchema{
 		Version:    s.Version,
 		Attributes: make(configschema.IdentityAttributes),
@@ -204,12 +207,21 @@ func ProtoToResourceIdentitySchema(s *proto.ResourceIdentitySchema) providers.Id
 
 		if a.Type != nil {
 			if err := json.Unmarshal(a.Type, &attr.Type); err != nil {
-				panic(err)
+				diags = diags.Append(fmt.Errorf("Could not unmarshal type for attribute %q: %w", a.Name, err))
 			}
+		} else {
+			diags = diags.Append(fmt.Errorf("Attribute %q is missing a type definition", a.Name))
+		}
+
+		if attr.RequiredForImport && attr.OptionalForImport {
+			diags = diags.Append(fmt.Errorf("Attribute %q cannot be both required and optional for import", a.Name))
+		}
+		if !attr.RequiredForImport && !attr.OptionalForImport {
+			diags = diags.Append(fmt.Errorf("Attribute %q must be either required or optional for import", a.Name))
 		}
 
 		schema.Attributes[a.Name] = attr
 	}
 
-	return schema
+	return schema, diags
 }
