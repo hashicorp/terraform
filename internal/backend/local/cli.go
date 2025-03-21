7e1247a3083702e05a55b5e3d4a889ab9d90af2d
Sarah French <sarah.french@hashicorp.com>
2025-03-21 11:29:25 +0000

    Make CLI opts be used to configure the local-state backend, if present

--- internal/backend/local/cli.go
+++ internal/backend/local/cli.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform/internal/backend/backendrun"
+	localState "github.com/hashicorp/terraform/internal/backend/local-state"
 )
 
 // backendrun.CLI impl.
@@ -15,20 +16,23 @@ func (b *Local) CLIInit(opts *backendrun.CLIOpts) error {
 	b.OpInput = opts.Input
 	b.OpValidation = opts.Validation
 
-	// configure any new cli options
-	if opts.StatePath != "" {
-		log.Printf("[TRACE] backend/local: CLI option -state is overriding state path to %s", opts.StatePath)
-		b.OverrideStatePath = opts.StatePath
-	}
+	// If CLI options affect how local state is stored, set on
+	// the internal Backend
+	if be, ok := b.Backend.(*localState.Local); ok {
+		if opts.StatePath != "" {
+			log.Printf("[TRACE] backend/local: CLI option -state is overriding state path to %s", opts.StatePath)
+			be.OverrideStatePath = opts.StatePath
+		}
 
-	if opts.StateOutPath != "" {
-		log.Printf("[TRACE] backend/local: CLI option -state-out is overriding state output path to %s", opts.StateOutPath)
-		b.OverrideStateOutPath = opts.StateOutPath
-	}
+		if opts.StateOutPath != "" {
+			log.Printf("[TRACE] backend/local: CLI option -state-out is overriding state output path to %s", opts.StateOutPath)
+			be.OverrideStateOutPath = opts.StateOutPath
+		}
 
-	if opts.StateBackupPath != "" {
-		log.Printf("[TRACE] backend/local: CLI option -backup is overriding state backup path to %s", opts.StateBackupPath)
-		b.OverrideStateBackupPath = opts.StateBackupPath
+		if opts.StateBackupPath != "" {
+			log.Printf("[TRACE] backend/local: CLI option -backup is overriding state backup path to %s", opts.StateBackupPath)
+			be.OverrideStateBackupPath = opts.StateBackupPath
+		}
 	}
 
 	return nil
