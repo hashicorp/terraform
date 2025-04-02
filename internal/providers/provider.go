86a4ad78d8fd6cbf2d81f35e1d185441eeabba7d
Sarah French <sarah.french@hashicorp.com>
2025-04-02 19:25:06 +0100

    Update GetProviderSchemaResponse struct

--- internal/providers/provider.go
+++ internal/providers/provider.go
@@ -142,9 +142,8 @@ type GetProviderSchemaResponse struct {
 	// prefix) to the declaration of a function.
 	Functions map[string]FunctionDecl
 
-	// TODO
 	// Backends maps the backend type name to that type's schema.
-	// Backends map[string]Schema
+	StateStores map[string]Schema
 
 	// Diagnostics contains any warnings or errors from the method call.
 	Diagnostics tfdiags.Diagnostics
