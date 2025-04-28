91b1dd3cc35a06ae4620657e8a2c921eda20cf6d
UKEME BASSEY <ukeme.bassey@hashicorp.com>
2025-04-28 12:54:23 -0400

    extend existing cloud plugin to accomodate new plugin

---
+++ internal/pluginshared/cloudclient.go
@@ -0,0 +1,40 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package pluginshared
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/hashicorp/terraform/internal/httpclient"
+	"github.com/hashicorp/terraform/internal/logging"
+)
+
+// type CloudPluginClient struct {
+// 	BasePluginClient
+// }
+
+// NewCloudPluginClient creates a new client for downloading and verifying
+// terraform-cloudplugin archives
+func NewCloudPluginClient(ctx context.Context, serviceURL *url.URL) (*BasePluginClient, error) {
+	httpClient := httpclient.New()
+	httpClient.Timeout = defaultRequestTimeout
+
+	retryableClient := retryablehttp.NewClient()
+	retryableClient.HTTPClient = httpClient
+	retryableClient.RetryMax = 3
+	retryableClient.RequestLogHook = requestLogHook
+	retryableClient.Logger = logging.HCLogger()
+
+	client := BasePluginClient{
+		ctx:        ctx,
+		serviceURL: serviceURL,
+		httpClient: retryableClient,
+		pluginName: "cloudplugin",
+	}
+
+	// return &CloudPluginClient{BasePluginClient: client}, nil
+	return &client, nil
+}
