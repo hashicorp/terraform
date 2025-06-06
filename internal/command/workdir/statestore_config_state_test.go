0d74aad3e3ea5116833c9778bf90d6a4139951f8
Sarah French <sarah.french@hashicorp.com>
2025-06-06 20:32:19 +0100

    Remove direct testing of (s *Source) MarshalText() and UnmarshalText() methods

--- internal/command/workdir/statestore_config_state_test.go
+++ internal/command/workdir/statestore_config_state_test.go
@@ -5,12 +5,9 @@ package workdir
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	tfaddr "github.com/hashicorp/terraform-registry-address"
-	svchost "github.com/hashicorp/terraform-svchost"
 	"github.com/hashicorp/terraform/internal/configs/configschema"
 	"github.com/zclconf/go-cty-debug/ctydebug"
 	"github.com/zclconf/go-cty/cty"
@@ -104,134 +101,5 @@ func TestParseStateStoreConfigState_Empty(t *testing.T) {
 	isEmpty = s.Empty()
 	if isEmpty != true {
 		t.Fatalf("expected config to be reported as empty, but got empty=%v", isEmpty)
-	}
-}
-
-func TestProviderSource_MarshalText(t *testing.T) {
-
-	cases := map[string]struct {
-		host           svchost.Hostname
-		namespace      string
-		typeName       string
-		expectedString string
-	}{
-		"marshals complete ProviderSource to an expected FQN": {
-			host:           tfaddr.DefaultProviderRegistryHost,
-			namespace:      "hashicorp",
-			typeName:       "random",
-			expectedString: "registry.terraform.io/hashicorp/random",
-		},
-		// Unhappy path cases
-		"when host is unset, marshaling succeeds with an incomplete FQN": {
-			namespace:      "hashicorp",
-			typeName:       "random",
-			expectedString: "/hashicorp/random",
-		},
-		"when namespace is unset, marshaling succeeds with an incomplete FQN": {
-			host:           tfaddr.DefaultProviderRegistryHost,
-			typeName:       "random",
-			expectedString: "registry.terraform.io//random",
-		},
-		"when type is unset, marshaling succeeds with an incomplete FQN": {
-			host:           tfaddr.DefaultProviderRegistryHost,
-			namespace:      "hashicorp",
-			expectedString: "registry.terraform.io/hashicorp/",
-		},
-	}
-
-	for tn, tc := range cases {
-		t.Run(tn, func(t *testing.T) {
-
-			ps := Source{
-				// Provider: tfaddr.NewProvider(tc.host, tc.namespace, tc.typeName),
-				Provider: tfaddr.Provider{
-					Type:      tc.typeName,
-					Namespace: tc.namespace,
-					Hostname:  tc.host,
-				},
-			}
-
-			txt, err := ps.MarshalText()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if string(txt) != tc.expectedString {
-				t.Fatalf("expected marshalled text %q but got %q", tc.expectedString, txt)
-			}
-		})
-	}
-}
-
-func TestProviderSource_UnmarshalText(t *testing.T) {
-
-	cases := map[string]struct {
-		host          svchost.Hostname
-		namespace     string
-		typeName      string
-		inputText     string
-		expectedError string
-	}{
-		"With a complete FQN string, it unmarshals to expected values": {
-			host:      tfaddr.DefaultProviderRegistryHost,
-			namespace: "hashicorp",
-			typeName:  "random",
-			inputText: "registry.terraform.io/hashicorp/random",
-		},
-		"When hostname is missing from the FQN string, it supplied the default hostname": {
-			host:      tfaddr.DefaultProviderRegistryHost,
-			namespace: "hashicorp",
-			typeName:  "random",
-			inputText: "hashicorp/random",
-		},
-		"When hostname and namespace are missing from the FQN string, it supplied the default hostname and hashicorp namespace": {
-			host:      tfaddr.DefaultProviderRegistryHost,
-			namespace: "hashicorp",
-			typeName:  "random",
-			inputText: "random",
-		},
-		"An error is returned when unmarshaling an empty string": {
-			inputText:     "",
-			expectedError: "error unmarshaling provider source from backend state file",
-		},
-		"An error is returned when unmarshaling a malformed FQN: missing host with leading slash": {
-			inputText:     "/hashicorp/random",
-			expectedError: "error unmarshaling provider source from backend state file",
-		},
-		"An error is returned when unmarshaling a malformed FQN: missing namespace": {
-			inputText:     "registry.terraform.io//random",
-			expectedError: "error unmarshaling provider source from backend state file",
-		},
-		"An error is returned when unmarshaling a malformed FQN: missing type": {
-			inputText:     "registry.terraform.io/hashicorp/",
-			expectedError: "error unmarshaling provider source from backend state file",
-		},
-	}
-
-	for tn, tc := range cases {
-		t.Run(tn, func(t *testing.T) {
-
-			ps := Source{}
-
-			err := ps.UnmarshalText([]byte(tc.inputText))
-			if err != nil {
-				if tc.expectedError != "" {
-					if !strings.Contains(err.Error(), tc.expectedError) {
-						t.Fatalf("expected error to contain the string %q, but got %q", tc.expectedError, err)
-					}
-					return // stop early in error cases
-				}
-
-				t.Fatalf("unexpected error: %q", err)
-			}
-			if ps.Provider.Hostname != tc.host {
-				t.Fatalf("expected host to be %q but got %q", tc.host, ps.Provider.Hostname)
-			}
-			if ps.Provider.Namespace != tc.namespace {
-				t.Fatalf("expected namespace to be %q but got %q", tc.namespace, ps.Provider.Namespace)
-			}
-			if ps.Provider.Type != tc.typeName {
-				t.Fatalf("expected type to be %q but got %q", tc.typeName, ps.Provider.Type)
-			}
-		})
 	}
 }
