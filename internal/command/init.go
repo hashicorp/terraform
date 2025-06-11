79443544714520ff056dbe771dc53d3a44b8b2d8
Sarah French <sarah.french@hashicorp.com>
2025-06-11 15:39:52 +0100

    WIP - add PSS to init code

--- internal/command/init.go
+++ internal/command/init.go
@@ -8,11 +8,14 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"maps"
 	"reflect"
+	"slices"
 	"sort"
 	"strings"
 
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hcldec"
 	svchost "github.com/hashicorp/terraform-svchost"
 	"github.com/posener/complete"
 	"github.com/zclconf/go-cty/cty"
@@ -28,8 +31,10 @@ import (
 	"github.com/hashicorp/terraform/internal/command/views"
 	"github.com/hashicorp/terraform/internal/configs"
 	"github.com/hashicorp/terraform/internal/configs/configschema"
+	"github.com/hashicorp/terraform/internal/didyoumean"
 	"github.com/hashicorp/terraform/internal/getproviders"
 	"github.com/hashicorp/terraform/internal/providercache"
+	"github.com/hashicorp/terraform/internal/providers"
 	"github.com/hashicorp/terraform/internal/states"
 	"github.com/hashicorp/terraform/internal/terraform"
 	"github.com/hashicorp/terraform/internal/tfdiags"
@@ -213,7 +218,10 @@ func (c *InitCommand) Run(args []string) int {
 	case initArgs.Cloud && rootModEarly.CloudConfig != nil:
 		back, backendOutput, backDiags = c.initCloud(ctx, rootModEarly, initArgs.BackendConfig, initArgs.ViewType, view)
 	case initArgs.Backend:
-		back, backendOutput, backDiags = c.initBackend(ctx, rootModEarly, initArgs.BackendConfig, initArgs.ViewType, view)
+		// This handles case when config contains either backend or state_store blocks.
+		// This is valid as either can be implementations of backend.Backend, which is what we
+		// obtain here.
+		back, backendOutput, backDiags = c.initBackend(ctx, rootModEarly, initArgs, view, config)
 	default:
 		// load the previously-stored backend config
 		back, backDiags = c.Meta.backendFromState(ctx)
@@ -417,16 +425,132 @@ func (c *InitCommand) initCloud(ctx context.Context, root *configs.Module, extra
 	return back, true, diags
 }
 
-func (c *InitCommand) initBackend(ctx context.Context, root *configs.Module, extraConfig arguments.FlagNameValueSlice, viewType arguments.ViewType, view views.Init) (be backend.Backend, output bool, diags tfdiags.Diagnostics) {
+func (c *InitCommand) initBackend(ctx context.Context, root *configs.Module, initArgs *arguments.Init, view views.Init, config *configs.Config) (be backend.Backend, output bool, diags tfdiags.Diagnostics) {
 	ctx, span := tracer.Start(ctx, "initialize backend")
 	_ = ctx // prevent staticcheck from complaining to avoid a maintenence hazard of having the wrong ctx in scope here
 	defer span.End()
 
+	// Vars to account for changed method parameter names
+	extraConfig := initArgs.BackendConfig
+	viewType := initArgs.ViewType
+
+	// TODO - we need appropriate messages for backend vs state_store
 	view.Output(views.InitializingBackendMessage)
 
 	var backendConfig *configs.Backend
 	var backendConfigOverride hcl.Body
-	if root.Backend != nil {
+	// TODO: expected mutual exclusive validation to have happened by now
+	if root.StateStore != nil {
+		// TODO
+
+		// 1) Download provider
+		providersOutput, providersAbort, providerDiags := c.getProviders(ctx, config, nil, initArgs.Upgrade, initArgs.PluginPath, initArgs.Lockfile, view)
+		diags = diags.Append(providerDiags)
+		if providersAbort || providerDiags.HasErrors() {
+			view.Diagnostics(diags)
+			return nil, providersOutput, diags
+		}
+
+		// 2) Get provider as var
+		opts, err := c.contextOpts()
+		if err != nil {
+			diags = diags.Append(err)
+			return nil, true, diags
+		}
+
+		factory, exists := opts.Providers[root.StateStore.ProviderAddr]
+		if !exists {
+			panic("oh no")
+		}
+
+		provider, err := factory()
+		if err != nil {
+			diags = diags.Append(err)
+			return nil, true, diags
+		}
+
+		fmt.Println(provider)
+		// TODO - investigate using GetMetadata here to check provider contains a state store with the correct name
+
+		// Schema needed to convert 'provider' body to cty.Value
+		resp := provider.GetProviderSchema()
+
+		// Check provider can do PSS
+		if len(resp.StateStores) == 0 {
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Provider does not support pluggable state storage",
+				Detail: fmt.Sprintf("There are no state stores implemented by provider %s (%q)",
+					root.StateStore.Provider.Name,
+					root.StateStore.ProviderAddr),
+				Subject: &root.StateStore.DeclRange,
+			})
+			return nil, true, diags
+		}
+		// Check provider includes state store named in config
+		storeSchema, exists := resp.StateStores[root.StateStore.Type]
+		if !exists {
+			suggestions := slices.Sorted(maps.Keys(resp.StateStores))
+			suggestion := didyoumean.NameSuggestion(root.StateStore.Type, suggestions)
+			if suggestion != "" {
+				suggestion = fmt.Sprintf(" Did you mean %q?", suggestion)
+			}
+			diags = diags.Append(&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "State store not implemented by the provider",
+				Detail: fmt.Sprintf("State store %q is not implemented by provider %s (%q)%s",
+					root.StateStore.Type, root.StateStore.Provider.Name,
+					root.StateStore.ProviderAddr, suggestion),
+				Subject: &root.StateStore.DeclRange,
+			})
+			return nil, true, diags
+		}
+
+		// TODO - here we're using the nested provider block inside state_store
+		// We haven't added any assertions or usage of a paired required_providers
+		// entry yet.
+		spec := resp.Provider.Body.DecoderSpec()
+		cfg := root.StateStore.Provider.Config
+		providerConfigVal, decDiags := hcldec.Decode(cfg, spec, nil)
+		diags = diags.Append(decDiags)
+
+		// Validate provider
+		// TODO: deal with marks
+		validateResp := provider.ValidateProviderConfig(providers.ValidateProviderConfigRequest{
+			Config: providerConfigVal,
+		})
+		diags = diags.Append(validateResp.Diagnostics)
+
+		// Configure provider
+		configureResp := provider.ConfigureProvider(providers.ConfigureProviderRequest{
+			// TODO TerraformVersion: ,
+			Config: validateResp.PreparedConfig,
+			// TODO ClientCapabilities: ,
+		})
+		diags = diags.Append(configureResp.Diagnostics)
+
+		// Validate Store Config
+		storeSpec := storeSchema.Body.DecoderSpec()
+		storeCfg := root.StateStore.Config
+		storeConfig, decDiags := hcldec.Decode(storeCfg, storeSpec, nil)
+		// TODO: deal with marks
+		validateStoreResp := provider.ValidateStateStoreConfig(providers.ValidateStateStoreConfigRequest{
+			TypeName: root.StateStore.Type,
+			Config:   storeConfig,
+		})
+		diags = diags.Append(validateStoreResp.Diagnostics)
+
+		// Configure State Store
+		cfgStoreResp := provider.ConfigureStateStore(providers.ConfigureStateStoreRequest{
+			TypeName: root.StateStore.Type,
+			Config:   storeConfig,
+		})
+		diags = diags.Append(cfgStoreResp.Diagnostics)
+
+		// 8) Save backend state file
+
+		// 9) Convert provider into backend.Backend
+	} else if root.Backend != nil {
 		backendType := root.Backend.Type
 		if backendType == "cloud" {
 			diags = diags.Append(&hcl.Diagnostic{
