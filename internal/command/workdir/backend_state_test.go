2559f0a3dbaf97ee51affc2543d9804dc62f7023
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2025-06-11 15:10:26 +0100

    Update backend state file so it can describe PSS state (#37179)
    * Split code for backend state file vs backend state
    
    * Rename BackendState to BackendConfigState
    
    * Spelling error
    
    * Add `StateStorageConfigState` struct as new implementation of new `ConfigState[T any]` interface.
    
    * Split tests for backend state file vs backend config state structs
    
    * Rename StateStorageConfigState to StateStoreConfigState
    
    * Clarify test name, add comments
    
    * Add tests for StateStoreConfigState methods
    
    * Add test showing state_store in JSON is parsed correctly
    
    * Add detection of malformed backend state files that contain both backend and state_store fields
    
    * Add validation that stops a backend state file being written if it will contain state for both backend and state_store blocks
    
    * Rename `state_storage` to `state_store`
    
    * Rename `state_storage` to `state_store` in filenames
    
    * Move`ConfigState` to its own file
    
    * Fix test name, remove whitespace
    
    * Update `StateStoreConfigState` comment using review suggestion
    
    * Update error message to no longer allude to the environment TF is being run in
    
    * Update the state_store state to use `version.Version` and an adapted version of `tfaddr.Provider` for marshalling version and source data
    
    * Update test helper so it doesn't accidentally supply validation in tests
    
    * Add protection against saving an empty backend state file
    
    * Remove direct testing of (s *Source) MarshalText() and UnmarshalText() methods
    
    * Add Validate method to StateStoreConfigState, use in backend state encoding logic
    
    * Refactor to use new features in registry dependency

--- internal/command/workdir/backend_state_test.go
+++ internal/command/workdir/backend_state_test.go
@@ -4,15 +4,11 @@
 package workdir
 
 import (
-	"bytes"
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/zclconf/go-cty-debug/ctydebug"
-	"github.com/zclconf/go-cty/cty"
-
-	"github.com/hashicorp/terraform/internal/configs/configschema"
 )
 
 func TestParseBackendStateFile(t *testing.T) {
@@ -65,11 +61,62 @@ func TestParseBackendStateFile(t *testing.T) {
 			Want: &BackendStateFile{
 				Version:   3,
 				TFVersion: "0.8.0",
-				Backend: &BackendState{
+				Backend: &BackendConfigState{
 					Type:      "treasure_chest_buried_on_a_remote_island",
 					ConfigRaw: json.RawMessage("{}"),
 				},
 			},
+		},
+		"active state_store": {
+			Input: `{
+				"version": 3,
+				"terraform_version": "9.9.9",
+				"state_store": {
+					"type": "foobar_baz",
+					"config": {
+						"provider": "foobar",
+						"bucket": "my-bucket"
+					},
+					"provider": {
+						"version": "1.2.3",
+						"source": "registry.terraform.io/my-org/foobar"
+					}
+				}
+			}`,
+			Want: &BackendStateFile{
+				Version:   3,
+				TFVersion: "9.9.9",
+				StateStore: &StateStoreConfigState{
+					Type:     "foobar_baz",
+					Provider: getTestProviderState(t, "1.2.3", "registry.terraform.io", "my-org", "foobar"),
+					ConfigRaw: json.RawMessage(`{
+						"provider": "foobar",
+						"bucket": "my-bucket"
+					}`),
+				},
+			},
+		},
+		"detection of malformed state: conflicting 'backend' and 'state_store' sections": {
+			Input: `{
+				"version": 3,
+				"terraform_version": "9.9.9",
+				"backend": {
+					"type": "treasure_chest_buried_on_a_remote_island",
+					"config": {}
+				},
+				"state_store": {
+					"type": "foobar_baz",
+					"config": {
+						"provider": "foobar",
+						"bucket": "my-bucket"
+					},
+					"provider": {
+						"version": "1.2.3",
+						"source": "registry.terraform.io/my-org/foobar"
+					}
+				}
+			}`,
+			WantErr: `encountered a malformed backend state file that contains state for both a 'backend' and a 'state_store' block`,
 		},
 	}
 
@@ -97,47 +144,89 @@ func TestParseBackendStateFile(t *testing.T) {
 	}
 }
 
-func ParseBackendStateConfig(t *testing.T) {
-	// This test only really covers the happy path because Config/SetConfig is
-	// largely just a thin wrapper around configschema's "ImpliedType" and
-	// cty's json unmarshal/marshal and both of those are well-tested elsewhere.
+func TestEncodeBackendStateFile(t *testing.T) {
 
-	s := &BackendState{
-		Type: "whatever",
-		ConfigRaw: []byte(`{
-			"foo": "bar"
-		}`),
-	}
-
-	schema := &configschema.Block{
-		Attributes: map[string]*configschema.Attribute{
-			"foo": {
-				Type:     cty.String,
-				Optional: true,
+	tests := map[string]struct {
+		Input   *BackendStateFile
+		Want    []byte
+		WantErr string
+	}{
+		"it returns an error when neither backend nor state_store config state are present": {
+			Input: &BackendStateFile{},
+			Want:  []byte("{\n  \"version\": 3,\n  \"terraform_version\": \"1.13.0\"\n}"),
+		},
+		"it returns an error when the provider source's hostname is missing": {
+			Input: &BackendStateFile{
+				StateStore: &StateStoreConfigState{
+					Type:      "foobar_baz",
+					Provider:  getTestProviderState(t, "1.2.3", "", "my-org", "foobar"),
+					ConfigRaw: json.RawMessage([]byte(`{"foo":"bar"}`)),
+					Hash:      123,
+				},
 			},
+			WantErr: `state store is not valid: Unknown hostname: Expected hostname in the provider address to be set`,
+		},
+		"it returns an error when the provider source's hostname and namespace are missing ": {
+			Input: &BackendStateFile{
+				StateStore: &StateStoreConfigState{
+					Type:      "foobar_baz",
+					Provider:  getTestProviderState(t, "1.2.3", "", "", "foobar"),
+					ConfigRaw: json.RawMessage([]byte(`{"foo":"bar"}`)),
+					Hash:      123,
+				},
+			},
+			WantErr: `state store is not valid: Unknown hostname: Expected hostname in the provider address to be set`,
+		},
+		"it returns an error when the provider source is completely missing ": {
+			Input: &BackendStateFile{
+				StateStore: &StateStoreConfigState{
+					Type:      "foobar_baz",
+					Provider:  getTestProviderState(t, "1.2.3", "", "", ""),
+					ConfigRaw: json.RawMessage([]byte(`{"foo":"bar"}`)),
+					Hash:      123,
+				},
+			},
+			WantErr: `state store is not valid: Empty provider address: Expected address composed of hostname, provider namespace and name`,
+		},
+		"it returns an error when both backend and state_store config state are present": {
+			Input: &BackendStateFile{
+				Backend: &BackendConfigState{
+					Type:      "foobar",
+					ConfigRaw: json.RawMessage([]byte(`{"foo":"bar"}`)),
+					Hash:      123,
+				},
+				StateStore: &StateStoreConfigState{
+					Type:      "foobar_baz",
+					Provider:  getTestProviderState(t, "1.2.3", "registry.terraform.io", "my-org", "foobar"),
+					ConfigRaw: json.RawMessage([]byte(`{"foo":"bar"}`)),
+					Hash:      123,
+				},
+			},
+			WantErr: `attempted to encode a malformed backend state file; it contains state for both a 'backend' and a 'state_store' block`,
 		},
 	}
-	got, err := s.Config(schema)
-	want := cty.ObjectVal(map[string]cty.Value{
-		"foo": cty.StringVal("bar"),
-	})
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	if diff := cmp.Diff(want, got, ctydebug.CmpOptions); diff != "" {
-		t.Errorf("wrong result\n%s", diff)
-	}
 
-	err = s.SetConfig(cty.ObjectVal(map[string]cty.Value{
-		"foo": cty.StringVal("baz"),
-	}), schema)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := EncodeBackendStateFile(test.Input)
 
-	gotRaw := s.ConfigRaw
-	wantRaw := []byte(`{"foo":"baz"}`)
-	if !bytes.Equal(wantRaw, gotRaw) {
-		t.Errorf("wrong raw config after encode\ngot:  %s\nwant: %s", gotRaw, wantRaw)
+			if test.WantErr != "" {
+				if err == nil {
+					t.Fatalf("unexpected success\nwant error: %s", test.WantErr)
+				}
+				if !strings.Contains(err.Error(), test.WantErr) {
+					t.Errorf("wrong error\ngot:  %s\nwant: %s", err.Error(), test.WantErr)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+			if diff := cmp.Diff(test.Want, got); diff != "" {
+				t.Errorf("wrong result\n%s", diff)
+			}
+		})
+
 	}
 }
