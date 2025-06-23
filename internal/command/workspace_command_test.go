403bcefdf264a364c9af2c2c8496fb6eedf150ad
Sarah French <sarah.french@hashicorp.com>
2025-06-23 12:44:38 +0100

    Add tests defining what are valid workspace names

--- internal/command/workspace_command_test.go
+++ internal/command/workspace_command_test.go
@@ -476,3 +476,31 @@ func TestWorkspace_selectWithOrCreate(t *testing.T) {
 	}
 
 }
+
+func TestValidWorkspaceName(t *testing.T) {
+	cases := map[string]struct {
+		input string
+		valid bool
+	}{
+		"foobar": {
+			input: "foobar",
+			valid: true,
+		},
+		"valid symbols": {
+			input: "-._~@:",
+			valid: true,
+		},
+		"includes space": {
+			input: "two words",
+			valid: false,
+		},
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			valid := validWorkspaceName(tc.input)
+			if valid != tc.valid {
+				t.Fatalf("unexpected output when processing input %q. Wanted %v got %v", tc.input, tc.valid, valid)
+			}
+		})
+	}
+}
