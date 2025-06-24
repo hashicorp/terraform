f8f52cf966453299b7720fc2e2c505e2f0c01c20
Sarah French <sarah.french@hashicorp.com>
2025-06-24 17:20:07 +0100

    Remove changes to test, now that the plan doesn't report the provider used for PSS anymore.

--- internal/plans/plan_test.go
+++ internal/plans/plan_test.go
@@ -7,14 +7,11 @@ import (
 	"testing"
 
 	"github.com/go-test/deep"
-	"github.com/zclconf/go-cty/cty"
 
 	"github.com/hashicorp/terraform/internal/addrs"
 )
 
-// TestProviderAddrs_basic tests sourcing providers from a plan file that only uses providers
-// for resource management only.
-func TestProviderAddrs_basic(t *testing.T) {
+func TestProviderAddrs(t *testing.T) {
 
 	// Prepare plan
 	plan := &Plan{
@@ -69,100 +66,6 @@ func TestProviderAddrs_basic(t *testing.T) {
 		{
 			Module:   addrs.RootModule,
 			Provider: addrs.NewDefaultProvider("test"),
-		},
-	}
-
-	for _, problem := range deep.Equal(got, want) {
-		t.Error(problem)
-	}
-}
-
-// TestProviderAddrs_withStateStore tests sourcing providers from a plan file that uses providers
-// both for resource management and state storage.
-func TestProviderAddrs_withStateStore(t *testing.T) {
-	// Inputs for plan
-	provider := &Provider{}
-	err := provider.SetSource("registry.terraform.io/hashicorp/pluggable")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = provider.SetVersion("9.9.9")
-	if err != nil {
-		t.Fatal(err)
-	}
-	config, err := NewDynamicValue(cty.ObjectVal(map[string]cty.Value{
-		"foo": cty.StringVal("bar"),
-	}), cty.Object(map[string]cty.Type{
-		"foo": cty.String,
-	}))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Prepare plan
-	plan := &Plan{
-		StateStore: StateStore{
-			Type:      "pluggable_foobar",
-			Provider:  provider,
-			Config:    config,
-			Workspace: "default",
-		},
-		VariableValues: map[string]DynamicValue{},
-		Changes: &ChangesSrc{
-			Resources: []*ResourceInstanceChangeSrc{
-				{
-					Addr: addrs.Resource{
-						Mode: addrs.ManagedResourceMode,
-						Type: "test_thing",
-						Name: "woot",
-					}.Instance(addrs.IntKey(0)).Absolute(addrs.RootModuleInstance),
-					ProviderAddr: addrs.AbsProviderConfig{
-						Module:   addrs.RootModule,
-						Provider: addrs.NewDefaultProvider("test"),
-					},
-				},
-				{
-					Addr: addrs.Resource{
-						Mode: addrs.ManagedResourceMode,
-						Type: "test_thing",
-						Name: "woot",
-					}.Instance(addrs.IntKey(0)).Absolute(addrs.RootModuleInstance),
-					DeposedKey: "foodface",
-					ProviderAddr: addrs.AbsProviderConfig{
-						Module:   addrs.RootModule,
-						Provider: addrs.NewDefaultProvider("test"),
-					},
-				},
-				{
-					Addr: addrs.Resource{
-						Mode: addrs.ManagedResourceMode,
-						Type: "test_thing",
-						Name: "what",
-					}.Instance(addrs.IntKey(0)).Absolute(addrs.RootModuleInstance),
-					ProviderAddr: addrs.AbsProviderConfig{
-						Module:   addrs.RootModule.Child("foo"),
-						Provider: addrs.NewDefaultProvider("test"),
-					},
-				},
-			},
-		},
-	}
-
-	got := plan.ProviderAddrs()
-	want := []addrs.AbsProviderConfig{
-		// Providers used for managed resources
-		{
-			Module:   addrs.RootModule.Child("foo"),
-			Provider: addrs.NewDefaultProvider("test"),
-		},
-		{
-			Module:   addrs.RootModule,
-			Provider: addrs.NewDefaultProvider("test"),
-		},
-		// Provider used for pluggable state storage
-		{
-			Module:   addrs.RootModule,
-			Provider: addrs.NewDefaultProvider("pluggable"),
 		},
 	}
 
