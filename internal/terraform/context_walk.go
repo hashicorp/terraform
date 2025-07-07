66001080e6439d0d33241bcec0593a98fad52279
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-07-07 16:19:48 +0200

    actions: implement expansion and planning actions
    This implementation is very barebones, but builds the skelleton for
    all upcoming features in actions. It includes the graph implementation
    for planning actions including the expansion of actions and dealing
    with multiple action triggers.
    
    We plan all actions as part of their triggering resource for now.
    All action_triggers depend on the triggering resources plan to
    determine if an action should run or not, therefore we can plan
    the actions as part of the triggering resource.
    
    This implementation ignores conditions, deferrals, provider arguments
    and it does not add the actions to the planfile; mostly to enable more
    people to work on these issues in parallel.

--- internal/terraform/context_walk.go
+++ internal/terraform/context_walk.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/hashicorp/terraform/internal/actions"
 	"github.com/hashicorp/terraform/internal/addrs"
 	"github.com/hashicorp/terraform/internal/checks"
 	"github.com/hashicorp/terraform/internal/configs"
@@ -199,5 +200,6 @@ func (c *Context) graphWalker(graph *Graph, operation walkOperation, opts *graph
 		PlanTimestamp:           opts.PlanTimeTimestamp,
 		functionResults:         opts.FunctionResults,
 		Forget:                  opts.Forget,
+		Actions:                 actions.NewActions(),
 	}
 }
