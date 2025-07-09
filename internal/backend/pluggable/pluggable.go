39f79203870058cfb8f2ddfcbcf6e31e5ba30527
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2025-07-09 18:22:12 +0100

    PSS: Add packages for presenting a provider as an instance of backend.Backend (#37291)
    * Add code to 'wrap' a provider implementation so that it can behave like a backend.Backend
    
    * Add code for creating a state manager that will interact with a provider via grpc to interact with state
    
    * Remove prototyping code
    
    * Update old implementation of PrepareConfig on Pluggable to match new RPC names
    
    * Implement Configure method on Pluggable
    
    * Implement Workspaces and DeleteWorkspace methods on Pluggable
    
    * Prevent construction of a pluggable with missing data, add godoc comment to NewPluggable
    
    * Add godoc comment to ConfigSchema
    
    * Refactor how we create a state manager for interacting with PSS
    
    Now we reuse the remote.State struct and all the pre-existing logic there. We still wrap the provider interface to enable use of gRPC methods, but that's now done using the remote.Client interface.
    
    * Rename file
    
    * Move file into the remote package
    
    * Rename file and package to `pluggable`
    
    * Add test for the only method implemented fully on `grpcClient` : Delete
    
    * Add tests for `NewPluggable`
    
    * Add tests for (Pluggable).ConfigSchema method, make minor fixes
    
    * Change mocks: hardcoded response or logic should return before default logic in method.
    
    * Add tests for (Pluggable).PrepareConfig method
    
    * Add tests for (Pluggable).Configure method
    
    * Add tests for (Pluggable).Workspaces method
    
    * Add tests for (Pluggable).DeleteWorkspace method
    
    * Fix rebase
    
    * Run `make syncdeps`
    
    * Add headers
    
    * Add missing comments
    
    * No need to implement ClientForcePusher
    
    * Apply feedback from review, make small tweaks to test failure messages

---
+++ internal/backend/pluggable/pluggable.go
@@ -0,0 +1,140 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package pluggable
+
+import (
+	"errors"
+
+	"github.com/hashicorp/terraform/internal/backend"
+	"github.com/hashicorp/terraform/internal/configs/configschema"
+	"github.com/hashicorp/terraform/internal/providers"
+	"github.com/hashicorp/terraform/internal/states/remote"
+	"github.com/hashicorp/terraform/internal/states/statemgr"
+	"github.com/hashicorp/terraform/internal/tfdiags"
+	"github.com/zclconf/go-cty/cty"
+)
+
+// NewPluggable returns an instance of the backend.Backend interface that
+// contains a provider interface. These are the assumptions about that
+// provider:
+//
+// * The provider implements at least one state store.
+// * The provider has already been configured before using NewPluggable.
+//
+// The state store could also be configured prior to using NewPluggable,
+// or it could be configured using the relevant backend.Backend methods.
+//
+// By wrapping a configured provider in a Pluggable we allow calling code
+// to use the provider's gRPC methods when interacting with state.
+func NewPluggable(p providers.Interface, typeName string) (backend.Backend, error) {
+	if p == nil {
+		return nil, errors.New("Attempted to initialize pluggable state with a nil provider interface. This is a bug in Terraform and should be reported")
+	}
+	if typeName == "" {
+		return nil, errors.New("Attempted to initialize pluggable state with an empty string identifier for the state store. This is a bug in Terraform and should be reported")
+	}
+
+	return &Pluggable{
+		provider: p,
+		typeName: typeName,
+	}, nil
+}
+
+var _ backend.Backend = &Pluggable{}
+
+type Pluggable struct {
+	provider providers.Interface
+	typeName string
+}
+
+// ConfigSchema returns the schema for the state store implementation
+// name provided when the Pluggable was constructed.
+//
+// ConfigSchema implements backend.Backend
+func (p *Pluggable) ConfigSchema() *configschema.Block {
+	schemaResp := p.provider.GetProviderSchema()
+	if len(schemaResp.StateStores) == 0 {
+		// No state stores
+		return nil
+	}
+	val, ok := schemaResp.StateStores[p.typeName]
+	if !ok {
+		// Cannot find state store with that type
+		return nil
+	}
+
+	// State store type exists
+	return val.Body
+}
+
+// PrepareConfig validates configuration for the state store in
+// the state storage provider. The configuration sent from Terraform core
+// will not include any values from environment variables; it is the
+// provider's responsibility to access any environment variables
+// to get the complete set of configuration prior to validating it.
+//
+// PrepareConfig implements backend.Backend
+func (p *Pluggable) PrepareConfig(config cty.Value) (cty.Value, tfdiags.Diagnostics) {
+	req := providers.ValidateStateStoreConfigRequest{
+		TypeName: p.typeName,
+		Config:   config,
+	}
+	resp := p.provider.ValidateStateStoreConfig(req)
+	return config, resp.Diagnostics
+}
+
+// Configure configures the state store in the state storage provider.
+// Calling code is expected to have already validated the config using
+// the PrepareConfig method.
+//
+// It is the provider's responsibility to access any environment variables
+// set by the user to get the complete set of configuration.
+//
+// Configure implements backend.Backend
+func (p *Pluggable) Configure(config cty.Value) tfdiags.Diagnostics {
+	req := providers.ConfigureStateStoreRequest{
+		TypeName: p.typeName,
+		Config:   config,
+	}
+	resp := p.provider.ConfigureStateStore(req)
+	return resp.Diagnostics
+}
+
+// Workspaces returns a list of all states/CE workspaces that the backend.Backend
+// can find, given how it is configured. For example returning a list of differently
+// -named files in a blob storage service.
+//
+// Workspace implements backend.Backend
+func (p *Pluggable) Workspaces() ([]string, error) {
+	req := providers.GetStatesRequest{
+		TypeName: p.typeName,
+	}
+	resp := p.provider.GetStates(req)
+
+	return resp.States, resp.Diagnostics.Err()
+}
+
+// DeleteWorkspace deletes the state file for the named workspace.
+// The state storage provider is expected to return error diagnostics
+// if the workspace doesn't exist or it is unable to be deleted.
+//
+// DeleteWorkspace implements backend.Backend
+func (p *Pluggable) DeleteWorkspace(workspace string, force bool) error {
+	req := providers.DeleteStateRequest{
+		TypeName: p.typeName,
+		StateId:  workspace,
+	}
+	resp := p.provider.DeleteState(req)
+	return resp.Diagnostics.Err()
+}
+
+// StateMgr returns a state manager that uses gRPC to communicate with the
+// state storage provider to interact with state.
+//
+// StateMgr implements backend.Backend
+func (p *Pluggable) StateMgr(workspace string) (statemgr.Full, error) {
+	// repackages the provider's methods inside a state manager,
+	// to be passed to the calling code that expects a statemgr.Full
+	return remote.NewRemoteGRPC(p.provider, p.typeName, workspace), nil
+}
