42fd55b12c5fc79b2d1b78c434294193449b1dc4
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-07-14 12:02:05 +0200

    refactor: parse references outside of config

--- internal/terraform/node_resource_abstract.go
+++ internal/terraform/node_resource_abstract.go
@@ -218,7 +218,13 @@ func (n *NodeAbstractResource) References() []*addrs.Reference {
 
 			// All actions referenced in the action triggeres should be evaluated prior.
 			for _, at := range c.Managed.ActionTriggers {
-				result = append(result, at.Actions...)
+				for _, actionRef := range at.Actions {
+					// This should have been caught during validation
+					ref, _ := addrs.ParseRef(actionRef.Traversal)
+					if ref != nil {
+						result = append(result, ref)
+					}
+				}
 			}
 		}
 
