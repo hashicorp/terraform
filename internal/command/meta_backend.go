858c10619cb05bd9d51fdf21522dfaf7a3bd613d
Sarah French <sarah.french@hashicorp.com>
2025-08-08 12:42:39 +0100

    Fix missing }

--- internal/command/meta_backend.go
+++ internal/command/meta_backend.go
@@ -1687,6 +1687,7 @@ func (m *Meta) stateStore_C_s(c *configs.StateStore, cHash int, backendSMgr *cli
 				}
 			}
 			// TODO: handle if input is not enabled
+		}
 	}
 	if diags.HasErrors() {
 		return nil, diags
