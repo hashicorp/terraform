fc5af644b0b1c121a2d80a21df841bcdcaf5f5fd
Ricard Bejarano <ricard@bejarano.io>
2025-08-21 09:13:15 +0200

    terraform test: fix regression when running with no tests (#37475)

--- internal/backend/local/test.go
+++ internal/backend/local/test.go
@@ -99,7 +99,7 @@ func (runner *TestSuiteRunner) Test() (moduletest.Status, tfdiags.Diagnostics) {
 	// collisions, as the test directory variables should take precedence.
 	maps.Copy(testDirectoryGlobalVariables, runner.GlobalTestVariables)
 
-	suite.Status = moduletest.Pending
+	suite.Status = moduletest.Pass
 	for _, name := range slices.Sorted(maps.Keys(suite.Files)) {
 		if runner.Cancelled {
 			return suite.Status, diags
