fa0e95c22288d99889d76838df26086abc0fd018
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-09-01 13:46:36 +0200

    remove duplicate methods implemented on abstract action already

--- internal/terraform/node_action.go
+++ internal/terraform/node_action.go
@@ -5,9 +5,6 @@ package terraform
 
 import (
 	"github.com/hashicorp/terraform/internal/addrs"
-	"github.com/hashicorp/terraform/internal/dag"
-	"github.com/hashicorp/terraform/internal/lang/langrefs"
-	"github.com/hashicorp/terraform/internal/providers"
 	"github.com/hashicorp/terraform/internal/tfdiags"
 )
 
@@ -35,48 +32,6 @@ var (
 
 func (n *nodeExpandActionDeclaration) Name() string {
 	return n.Addr.String() + " (expand)"
-}
-
-func (n *nodeExpandActionDeclaration) ActionAddr() addrs.ConfigAction {
-	return n.Addr
-}
-
-func (n *nodeExpandActionDeclaration) ReferenceableAddrs() []addrs.Referenceable {
-	return []addrs.Referenceable{n.Addr.Action}
-}
-
-// GraphNodeModulePath
-func (n *nodeExpandActionDeclaration) ModulePath() addrs.Module {
-	return n.Addr.Module
-}
-
-// GraphNodeAttachActionSchema
-func (n *nodeExpandActionDeclaration) AttachActionSchema(schema *providers.ActionSchema) {
-	n.Schema = schema
-}
-
-func (n *nodeExpandActionDeclaration) DotNode(string, *dag.DotOpts) *dag.DotNode {
-	return &dag.DotNode{
-		Name: n.Name(),
-	}
-}
-
-// GraphNodeReferencer
-func (n *nodeExpandActionDeclaration) References() []*addrs.Reference {
-	var result []*addrs.Reference
-	c := n.Config
-
-	refs, _ := langrefs.ReferencesInExpr(addrs.ParseRef, c.Count)
-	result = append(result, refs...)
-	refs, _ = langrefs.ReferencesInExpr(addrs.ParseRef, c.ForEach)
-	result = append(result, refs...)
-
-	if n.Schema != nil {
-		refs, _ = langrefs.ReferencesInBlock(addrs.ParseRef, c.Config, n.Schema.ConfigSchema)
-		result = append(result, refs...)
-	}
-
-	return result
 }
 
 func (n *nodeExpandActionDeclaration) DynamicExpand(ctx EvalContext) (*Graph, tfdiags.Diagnostics) {
@@ -202,32 +157,4 @@ func (n *nodeExpandActionDeclaration) recordActionData(ctx EvalContext, addr add
 	}
 
 	return diags
-}
-
-// GraphNodeProviderConsumer
-func (n *nodeExpandActionDeclaration) ProvidedBy() (addrs.ProviderConfig, bool) {
-	// Once the provider is fully resolved, we can return the known value.
-	if n.ResolvedProvider.Provider.Type != "" {
-		return n.ResolvedProvider, true
-	}
-
-	// Since we always have a config, we can use it
-	relAddr := n.Config.ProviderConfigAddr()
-	return addrs.LocalProviderConfig{
-		LocalName: relAddr.LocalName,
-		Alias:     relAddr.Alias,
-	}, false
-}
-
-// GraphNodeProviderConsumer
-func (n *nodeExpandActionDeclaration) Provider() addrs.Provider {
-	if n.ResolvedProvider.Provider.Type != "" {
-		return n.ResolvedProvider.Provider
-	}
-	return n.Config.Provider
-}
-
-// GraphNodeProviderConsumer
-func (n *nodeExpandActionDeclaration) SetProvider(p addrs.AbsProviderConfig) {
-	n.ResolvedProvider = p
 }
