80a0f369a71711e2c4b214c64027429cd09e5b5f
Sarah French <sarah.french@hashicorp.com>
2025-09-03 13:10:58 +0100

    Update other test fixtures for consistency
    The related tests pass before and after this change

--- internal/configs/testdata/valid-modules/override-state-store/main.tf
+++ internal/configs/testdata/valid-modules/override-state-store/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
-    foo = {
-      source = "my-org/foo"
+    test = {
+      source = "hashicorp/test"
     }
   }
-  state_store "foo_bar" {
-    provider "foo" {}
+  state_store "test_store" {
+    provider "test" {}
 
-    custom_attr = "foobar"
+    value = "foobar"
   }
 }
 
