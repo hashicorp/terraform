61a7914dd9b09a732b7e274b470792afc2f9eb6c
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2025-09-04 10:51:49 +0100

    Add test helper to create mock provider that contains a state store (#37556)
    * Add a helper for mocking a provider with a state store
    
    * Update tests to use the new helper
    
    * Update other test fixtures for consistency
    
    The related tests pass before and after this change
    
    * Update testing helper to return the underlying type, not the interface

--- internal/configs/testdata/invalid-modules/conflict-cloud-statestore/main.tf
+++ internal/configs/testdata/invalid-modules/conflict-cloud-statestore/main.tf
@@ -6,9 +6,14 @@ terraform {
     }
   }
 
-  state_store "foo_bar" {
-    provider "foo" {}
+  required_providers {
+    test = {
+      source = "hashicorp/test"
+    }
+  }
+  state_store "test_store" {
+    provider "test" {}
 
-    custom_attr = "override"
+    value = "override"
   }
 }
--- internal/configs/testdata/invalid-modules/conflict-statestore-backend/main.tf
+++ internal/configs/testdata/invalid-modules/conflict-statestore-backend/main.tf
@@ -1,9 +1,14 @@
 terraform {
   backend "foo" {}
 
-  state_store "foo_bar" {
-    provider "foo" {}
+  required_providers {
+    test = {
+      source = "hashicorp/test"
+    }
+  }
+  state_store "test_store" {
+    provider "test" {}
 
-    custom_attr = "override"
+    value = "override"
   }
 }
