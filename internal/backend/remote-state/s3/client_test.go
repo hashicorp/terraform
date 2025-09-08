0d7bd8e5487623814ddc0c9708a9ed56d24c0720
Sarah French <sarah.french@hashicorp.com>
2025-09-08 16:37:25 +0100

    Fix tests after changing the interface to return diagnostics

--- internal/backend/remote-state/s3/client_test.go
+++ internal/backend/remote-state/s3/client_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform/internal/states/remote"
 	"github.com/hashicorp/terraform/internal/states/statefile"
 	"github.com/hashicorp/terraform/internal/states/statemgr"
+	"github.com/hashicorp/terraform/internal/tfdiags"
 )
 
 func TestRemoteClient_impl(t *testing.T) {
@@ -385,38 +386,54 @@ func TestRemoteClient_stateChecksum(t *testing.T) {
 
 	// fetching an empty state through client1 should now error out due to a
 	// mismatched checksum.
-	if _, diags := client1.Get(); !IsA[badChecksumError](diags.Err()) {
-		t.Fatalf("expected state checksum error: got %s", diags.Err())
-	} else if bse, ok := As[badChecksumError](diags.Err()); ok && len(bse.digest) != 0 {
-		t.Fatalf("expected empty checksum, got %x", bse.digest)
-	}
+	if _, diags := client1.Get(); diags.HasErrors() {
+		var comparableErr error
+		if errValue, isDiag := diags.Err().(tfdiags.DiagnosticsAsError); isDiag {
+			// To use `As` below we need to extract the error that's wrapped
+			// in a diagnostic.
+			comparableErr = errValue.WrappedErrors()[0]
+		}
+		if !IsA[badChecksumError](comparableErr) {
+			t.Fatalf("expected state checksum error: got %s", diags.Err())
+		} else if bse, ok := As[badChecksumError](comparableErr); ok && len(bse.digest) != 0 {
+			t.Fatalf("expected empty checksum, got %x", bse.digest)
+		}
 
-	// put the old state in place of the new, without updating the checksum
-	if diags := client2.Put(oldState.Bytes()); diags.HasErrors() {
-		t.Fatal(diags.Err())
-	}
-
-	// fetching the wrong state through client1 should now error out due to a
-	// mismatched checksum.
-	if _, diags := client1.Get(); !IsA[badChecksumError](diags.Err()) {
-		t.Fatalf("expected state checksum error: got %s", diags.Err())
-	}
-
-	// update the state with the correct one after we Get again
-	testChecksumHook = func() {
-		if diags := client2.Put(newState.Bytes()); diags.HasErrors() {
+		// put the old state in place of the new, without updating the checksum
+		if diags := client2.Put(oldState.Bytes()); diags.HasErrors() {
 			t.Fatal(diags.Err())
 		}
-		testChecksumHook = nil
-	}
 
-	consistencyRetryTimeout = origTimeout
+		// fetching the wrong state through client1 should now error out due to a
+		// mismatched checksum.
+		if _, diags := client1.Get(); diags.HasErrors() {
+			var comparableErr error
+			if errValue, isDiag := diags.Err().(tfdiags.DiagnosticsAsError); isDiag {
+				// To use `As` below we need to extract the error that's wrapped
+				// in a diagnostic.
+				comparableErr = errValue.WrappedErrors()[0]
+			}
+			if !IsA[badChecksumError](comparableErr) {
+				t.Fatalf("expected state checksum error: got %s", diags.Err())
+			}
+		}
 
-	// this final Get will fail to fail the checksum verification, the above
-	// callback will update the state with the correct version, and Get should
-	// retry automatically.
-	if _, diags := client1.Get(); diags.HasErrors() {
-		t.Fatal(diags.Err())
+		// update the state with the correct one after we Get again
+		testChecksumHook = func() {
+			if diags := client2.Put(newState.Bytes()); diags.HasErrors() {
+				t.Fatal(diags.Err())
+			}
+			testChecksumHook = nil
+		}
+
+		consistencyRetryTimeout = origTimeout
+
+		// this final Get will fail to fail the checksum verification, the above
+		// callback will update the state with the correct version, and Get should
+		// retry automatically.
+		if _, diags := client1.Get(); diags.HasErrors() {
+			t.Fatal(diags.Err())
+		}
 	}
 }
 
