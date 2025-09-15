d9f2ef39f7a87c0587d8cac15e2e524caa44e490
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2025-09-15 13:39:43 -0400

    remove commented out bits too

--- internal/configs/module_merge.go
+++ internal/configs/module_merge.go
@@ -306,18 +306,5 @@ func (a *Action) merge(oa *Action, rps map[string]*RequiredProvider) hcl.Diagnos
 
 	a.Config = MergeBodies(a.Config, oa.Config)
 
-	/* depends_on: not yet supported in Actions
-	// We don't allow depends_on to be overridden because that is likely to
-	// cause confusing misbehavior.
-	if len(oa.DependsOn) != 0 {
-		diags = append(diags, &hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Unsupported override",
-			Detail:   "The depends_on argument may not be overridden.",
-			Subject:  oa.DependsOn[0].SourceRange().Ptr(), // the first item is the closest range we have
-		})
-	}
-	*/
-
 	return diags
 }
