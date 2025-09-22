8b11c1d9225773c0cd965a467db8953378ed1c4c
Sarah French <sarah.french@hashicorp.com>
2025-09-22 18:40:01 +0100

    Add test for getStateStoreProviderFactory

--- internal/command/init.go
+++ internal/command/init.go
@@ -247,7 +247,7 @@ func (c *InitCommand) initBackend(ctx context.Context, root *configs.Module, ext
 
 		opts = &BackendOpts{
 			StateStoreConfig:       root.StateStore,
-			Locks:                  locks,
+			Locks:                  configLocks,
 			ProviderFactory:        factory,
 			CreateDefaultWorkspace: initArgs.CreateDefaultWorkspace,
 			ConfigOverride:         configOverride,
