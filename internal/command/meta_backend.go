9304b20641369d93dfa4d2f427c312623b396620
Sarah French <sarah.french@hashicorp.com>
2025-10-04 14:09:48 +0100

    Update chunk size negotiation code in `stateStoreInitFromConfig` to error if the provider doesn't return a chunk size > 0

--- internal/command/meta_backend.go
+++ internal/command/meta_backend.go
@@ -1969,9 +1969,9 @@ func (m *Meta) stateStoreInitFromConfig(c *configs.StateStore, factory providers
 	}
 
 	chunkSize := cfgStoreResp.Capabilities.ChunkSize
-	if chunkSize > maxStateStoreChunkSize {
+	if chunkSize == 0 || chunkSize > maxStateStoreChunkSize {
 		diags = diags.Append(fmt.Errorf("Failed to negotiate acceptable chunk size. "+
-			"Expected size <= %d bytes, provider wants %d bytes",
+			"Expected size >0, <= %d bytes, provider wants %d bytes",
 			maxStateStoreChunkSize, chunkSize,
 		))
 		return nil, cty.NilVal, cty.NilVal, diags
