3b5b67f488617b5562940f0b0b3404f2b7f72211
Sarah French <sarah.french@hashicorp.com>
2025-10-06 16:09:42 +0100

    Rename var in test

--- internal/command/meta_backend_test.go
+++ internal/command/meta_backend_test.go
@@ -2808,10 +2808,10 @@ func TestMetaBackend_stateStoreInitFromConfig(t *testing.T) {
 				diags.Err(),
 			)
 		}
-		expectedStateStore := `Did you mean "test_bore"?`
-		if !strings.Contains(diags.Err().Error(), expectedStateStore) {
+		expectedMsg := `Did you mean "test_bore"?`
+		if !strings.Contains(diags.Err().Error(), expectedMsg) {
 			t.Fatalf("expected the returned error to include %q, got: %s",
-				expectedStateStore,
+				expectedMsg,
 				diags.Err(),
 			)
 		}
