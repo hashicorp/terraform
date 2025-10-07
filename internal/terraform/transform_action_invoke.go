4b461595ca7a15701108f541b0be052e94bc3919
Daniel Schmidt <danielmschmidt92@gmail.com>
2025-10-07 11:34:23 +0200

    refactor: separate action triggers from action invocations through --invoke

---
+++ internal/terraform/transform_action_invoke.go
@@ -0,0 +1,55 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package terraform
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/internal/addrs"
+	"github.com/hashicorp/terraform/internal/configs"
+)
+
+type ActionInvokeTransformer struct {
+	Config    *configs.Config
+	Targets   []addrs.Targetable
+	Operation walkOperation
+
+	queryPlanMode bool
+}
+
+func (t *ActionInvokeTransformer) Transform(g *Graph) error {
+	if t.Operation != walkPlan || t.queryPlanMode || len(t.Targets) == 0 {
+		return nil
+	}
+
+	// Then we're invoking and we're just going to include the actions that
+	// have been specifically asked for.
+	for _, target := range t.Targets {
+		var config *configs.Action
+		switch target := target.(type) {
+		case addrs.AbsAction:
+			module := t.Config.DescendantForInstance(target.Module)
+			if module != nil {
+				config = module.Module.Actions[target.Action.String()]
+			}
+		case addrs.AbsActionInstance:
+			module := t.Config.DescendantForInstance(target.Module)
+			if module != nil {
+				config = module.Module.Actions[target.Action.Action.String()]
+			}
+		}
+
+		if config == nil {
+			return fmt.Errorf("action %s does not exist in the configuration", target.String())
+		}
+
+		g.Add(&nodeActionInvokeExpand{
+			Target: target,
+			Config: config,
+		})
+	}
+
+	return nil
+
+}
--- internal/terraform/transform_action_trigger_config.go
+++ internal/terraform/transform_action_trigger_config.go
@@ -21,43 +21,10 @@ type ActionTriggerConfigTransformer struct {
 }
 
 func (t *ActionTriggerConfigTransformer) Transform(g *Graph) error {
-	if t.Operation != walkPlan || t.queryPlanMode {
+	// We don't want to run if we are using the query plan mode or have targets in place
+	if t.Operation != walkPlan || t.queryPlanMode || len(t.Targets) > 0 {
 		return nil
 	}
-
-	if len(t.Targets) > 0 {
-		// Then we're invoking and we're just going to include the actions that
-		// have been specifically asked for.
-
-		for _, target := range t.Targets {
-			var config *configs.Action
-			switch target := target.(type) {
-			case addrs.AbsAction:
-				module := t.Config.DescendantForInstance(target.Module)
-				if module != nil {
-					config = module.Module.Actions[target.Action.String()]
-				}
-			case addrs.AbsActionInstance:
-				module := t.Config.DescendantForInstance(target.Module)
-				if module != nil {
-					config = module.Module.Actions[target.Action.Action.String()]
-				}
-			}
-
-			if config == nil {
-				return fmt.Errorf("action %s does not exist in the configuration", target.String())
-			}
-
-			g.Add(&nodeActionInvokeExpand{
-				Target: target,
-				Config: config,
-			})
-		}
-
-		return nil
-	}
-
-	// otherwise, add all the action triggers from the config.
 
 	return t.transform(g, t.Config)
 }
