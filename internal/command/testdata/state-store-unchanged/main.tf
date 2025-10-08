93d1a1ef3f32f7e8784dbee0607c66344cb1ffd4
Sarah French <sarah.french@hashicorp.com>
2025-10-08 23:03:39 +0100

    Add test showing successful init when no config changes are detected.

---
+++ internal/command/testdata/state-store-unchanged/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    test = {
+      source = "hashicorp/test"
+    }
+  }
+  state_store "test_store" {
+    provider "test" {}
+
+    value = "foobar" # matches backend state file
+  }
+}
