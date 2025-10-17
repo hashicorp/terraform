7972812b69bd667a139cdfbc7f3eb1c3fb777de7
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2025-10-17 11:50:10 +0200

    Send over action type in planned change

--- internal/stacks/stackplan/planned_change.go
+++ internal/stacks/stackplan/planned_change.go
@@ -913,6 +913,7 @@ func (pc *PlannedChangeActionInvocationInstancePlanned) ChangeDescription() (*st
 	invoke := stacks.PlannedChange_ActionInvocationInstance{
 		Addr:         stacks.NewActionInvocationInStackAddr(addr),
 		ProviderAddr: pc.Invocation.ProviderAddr.Provider.String(),
+		ActionType:   pc.Invocation.Addr.Action.Action.Name,
 
 		ConfigValue: stacks.NewDynamicValue(
 			pc.Invocation.ConfigValue,
