7f6431f992e03ee476ba9896aead39c5c74bb508
James Bardin <j.bardin@gmail.com>
2025-12-10 13:36:18 -0500

    make protobuf

--- internal/tfplugin6/tfplugin6_grpc.pb.go
+++ internal/tfplugin6/tfplugin6_grpc.pb.go
@@ -1,9 +1,9 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-// Terraform Plugin RPC protocol version 6.10
+// Terraform Plugin RPC protocol version 6.11
 //
-// This file defines version 6.10 of the RPC protocol. To implement a plugin
+// This file defines version 6.11 of the RPC protocol. To implement a plugin
 // against this protocol, copy this definition into your own codebase and
 // use protoc to generate stubs for your target language.
 //
