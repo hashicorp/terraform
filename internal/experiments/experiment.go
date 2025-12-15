08afe0494cad6af756024ca666d56fa126176085
Sarah French <sarah.french@hashicorp.com>
2025-12-15 15:55:05 +0000

    Add an experiment called `current` that can be used for testing behaviours of the experiments list in the terraform block.

--- internal/experiments/experiment.go
+++ internal/experiments/experiment.go
@@ -25,11 +25,14 @@ const (
 	PreconditionsPostconditions    = Experiment("preconditions_postconditions")
 	EphemeralValues                = Experiment("ephemeral_values")
 	UnknownInstances               = Experiment("unknown_instances")
+	Current                        = Experiment("current")
 )
 
 func init() {
 	// Each experiment constant defined above must be registered here as either
 	// a current or a concluded experiment.
+	registerCurrentExperiment(Current)
+
 	registerConcludedExperiment(UnknownInstances, "Unknown instances are being rolled into a larger feature for deferring unready resources and modules.")
 	registerConcludedExperiment(VariableValidation, "Custom variable validation can now be used by default, without enabling an experiment.")
 	registerConcludedExperiment(VariableValidationCrossRef, "Input variable validation rules may now refer to other objects in the same module without enabling any experiment.")
