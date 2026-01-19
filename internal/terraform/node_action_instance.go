1f5c492407c586bb82284a39c7440c19e6b3f824
Daniel Schmidt <danielmschmidt92@gmail.com>
2026-01-19 17:38:01 +0100

    ElaborateFromConfigBody handles deeply nested paths more precisely

--- internal/terraform/node_action_instance.go
+++ internal/terraform/node_action_instance.go
@@ -71,7 +71,7 @@ func (n *NodeActionDeclarationInstance) Execute(ctx EvalContext, _ walkOperation
 		valDiags := validateResourceForbiddenEphemeralValues(ctx, configVal, n.Schema.ConfigSchema)
 		diags = diags.Append(valDiags.InConfigBody(n.Config.Config, n.Addr.String()))
 
-		deprecationDiags := ctx.Deprecations().ValidateAsConfig(configVal, n.ModulePath())
+		deprecationDiags := ctx.Deprecations().ValidateAsConfig(configVal, n.Schema.ConfigSchema, n.ModulePath())
 		diags = diags.Append(deprecationDiags.InConfigBody(n.Config.Config, n.Addr.String()))
 
 		if diags.HasErrors() {
