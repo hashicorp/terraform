82984539df691510c2e798eba6c0c7e5789fe097
Daniel Schmidt <danielmschmidt92@gmail.com>
2026-02-13 14:43:54 +0100

    refactor state-pull command argument parsing

---
+++ internal/command/arguments/state_pull.go
@@ -0,0 +1,32 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package arguments
+
+import (
+	"github.com/hashicorp/terraform/internal/tfdiags"
+)
+
+// StatePull represents the command-line arguments for the state pull command.
+type StatePull struct {
+}
+
+// ParseStatePull processes CLI arguments, returning a StatePull value and
+// diagnostics. If errors are encountered, a StatePull value is still returned
+// representing the best effort interpretation of the arguments.
+func ParseStatePull(args []string) (*StatePull, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+	pull := &StatePull{}
+
+	cmdFlags := defaultFlagSet("state pull")
+
+	if err := cmdFlags.Parse(args); err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Failed to parse command-line flags",
+			err.Error(),
+		))
+	}
+
+	return pull, diags
+}
