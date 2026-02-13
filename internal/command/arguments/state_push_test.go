e4ec3fe9b27017e1d52e51871d72e96b0487336d
Daniel Schmidt <danielmschmidt92@gmail.com>
2026-02-13 14:43:54 +0100

    refactor state-push command argument parsing

---
+++ internal/command/arguments/state_push_test.go
@@ -0,0 +1,155 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package arguments
+
+import (
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform/internal/tfdiags"
+)
+
+func TestParseStatePush_valid(t *testing.T) {
+	testCases := map[string]struct {
+		args []string
+		want *StatePush
+	}{
+		"path only": {
+			[]string{"replace.tfstate"},
+			&StatePush{
+				Force:               false,
+				StateLock:           true,
+				StateLockTimeout:    0,
+				IgnoreRemoteVersion: false,
+				Path:                "replace.tfstate",
+			},
+		},
+		"stdin": {
+			[]string{"-"},
+			&StatePush{
+				Force:               false,
+				StateLock:           true,
+				StateLockTimeout:    0,
+				IgnoreRemoteVersion: false,
+				Path:                "-",
+			},
+		},
+		"force": {
+			[]string{"-force", "replace.tfstate"},
+			&StatePush{
+				Force:               true,
+				StateLock:           true,
+				StateLockTimeout:    0,
+				IgnoreRemoteVersion: false,
+				Path:                "replace.tfstate",
+			},
+		},
+		"lock disabled": {
+			[]string{"-lock=false", "replace.tfstate"},
+			&StatePush{
+				Force:               false,
+				StateLock:           false,
+				StateLockTimeout:    0,
+				IgnoreRemoteVersion: false,
+				Path:                "replace.tfstate",
+			},
+		},
+		"lock timeout": {
+			[]string{"-lock-timeout=5s", "replace.tfstate"},
+			&StatePush{
+				Force:               false,
+				StateLock:           true,
+				StateLockTimeout:    5 * time.Second,
+				IgnoreRemoteVersion: false,
+				Path:                "replace.tfstate",
+			},
+		},
+		"ignore remote version": {
+			[]string{"-ignore-remote-version", "replace.tfstate"},
+			&StatePush{
+				Force:               false,
+				StateLock:           true,
+				StateLockTimeout:    0,
+				IgnoreRemoteVersion: true,
+				Path:                "replace.tfstate",
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, diags := ParseStatePush(tc.args)
+			if len(diags) > 0 {
+				t.Fatalf("unexpected diags: %v", diags)
+			}
+			if *got != *tc.want {
+				t.Fatalf("unexpected result\n got: %#v\nwant: %#v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestParseStatePush_invalid(t *testing.T) {
+	testCases := map[string]struct {
+		args      []string
+		want      *StatePush
+		wantDiags tfdiags.Diagnostics
+	}{
+		"no arguments": {
+			nil,
+			&StatePush{
+				StateLock: true,
+			},
+			tfdiags.Diagnostics{
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Required argument missing",
+					"Exactly one argument expected: the path to a Terraform state file.",
+				),
+			},
+		},
+		"too many arguments": {
+			[]string{"foo.tfstate", "bar.tfstate"},
+			&StatePush{
+				StateLock: true,
+				Path:      "",
+			},
+			tfdiags.Diagnostics{
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Required argument missing",
+					"Exactly one argument expected: the path to a Terraform state file.",
+				),
+			},
+		},
+		"unknown flag": {
+			[]string{"-boop"},
+			&StatePush{
+				StateLock: true,
+			},
+			tfdiags.Diagnostics{
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Failed to parse command-line flags",
+					"flag provided but not defined: -boop",
+				),
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Required argument missing",
+					"Exactly one argument expected: the path to a Terraform state file.",
+				),
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, gotDiags := ParseStatePush(tc.args)
+			if *got != *tc.want {
+				t.Fatalf("unexpected result\n got: %#v\nwant: %#v", got, tc.want)
+			}
+			tfdiags.AssertDiagnosticsMatch(t, gotDiags, tc.wantDiags)
+		})
+	}
+}
