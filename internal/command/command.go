62964c5f2daac942fb252266316a14c6db0f21c8
Daniel Schmidt <danielmschmidt92@gmail.com>
2026-02-18 10:27:17 +0100

    remove copied default value

--- internal/command/command.go
+++ internal/command/command.go
@@ -34,10 +34,6 @@ const DefaultStateFilename = "terraform.tfstate"
 // Terraform state, if applicable. Backends can set their own custom defaults.
 const DefaultStatePersistInterval = 20
 
-// DefaultVarsFilename is the default filename used for vars
-// TODO: Remove here
-const DefaultVarsFilename = "terraform.tfvars"
-
 // DefaultBackupExtension is added to the state file to form the path
 const DefaultBackupExtension = ".backup"
 
