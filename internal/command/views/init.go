a491544f6bb7094ece48d425dbf1d908994daa02
Sarah French <sarah.french@hashicorp.com>
2026-02-23 19:23:20 +0000

    feat: implement safe init security features when PSS is used while input is enabled
    Terraform prompts users when installing state storage providers via HTTP. If a provider would be sourced via HTTP and the -safe-init flag is missing an error is returned. If a provider is downloaded via other sources then nothing has changed (no new output, no need for new flags).

--- internal/command/views/init.go
+++ internal/command/views/init.go
@@ -199,6 +199,14 @@ var MessageRegistry map[InitMessageCode]InitMessage = map[InitMessageCode]InitMe
 		HumanValue: "\n[reset][bold]Initializing the state store...",
 		JSONValue:  "Initializing the state store...",
 	},
+	"user_approved_state_store_provider_message": {
+		HumanValue: "\n[reset][bold]User approved the state storage provider.",
+		JSONValue:  "User approved the state storage provider.",
+	},
+	"user_rejected_state_store_provider_message": {
+		HumanValue: "\n[reset][bold]User rejected the state storage provider.",
+		JSONValue:  "User rejected the state storage provider.",
+	},
 	"default_workspace_created_message": {
 		HumanValue: defaultWorkspaceCreatedInfo,
 		JSONValue:  defaultWorkspaceCreatedInfo,
@@ -343,6 +351,8 @@ const (
 	InitializingModulesMessage                  InitMessageCode = "initializing_modules_message"
 	InitializingBackendMessage                  InitMessageCode = "initializing_backend_message"
 	InitializingStateStoreMessage               InitMessageCode = "initializing_state_store_message"
+	UserApprovedStateStoreProviderMessage       InitMessageCode = "user_approved_state_store_provider_message"
+	UserRejectedStateStoreProviderMessage       InitMessageCode = "user_rejected_state_store_provider_message"
 	InitializingProviderPluginFromConfigMessage InitMessageCode = "initializing_provider_plugin_from_config_message"
 	InitializingProviderPluginFromStateMessage  InitMessageCode = "initializing_provider_plugin_from_state_message"
 	ReusingVersionIdentifiedFromConfig          InitMessageCode = "reusing_version_during_state_provider_init"
