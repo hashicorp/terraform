5b87f2159808a0c6f047aeeab4346d16d4f6ec6e
Samsondeen Dare <samsondeen.dare@hashicorp.com>
2026-03-06 16:57:43 +0100

    Cleanup in appropriate order when running parallel execution

--- internal/moduletest/graph/node_state_cleanup.go
+++ internal/moduletest/graph/node_state_cleanup.go
@@ -36,6 +36,7 @@ func (n *NodeStateCleanup) Name() string {
 
 // Execute destroys the resources created in the state file.
 func (n *NodeStateCleanup) Execute(evalCtx *EvalContext) {
+	fmt.Printf("Cleaning up state for %s\n", n.stateKey)
 	file := n.opts.File
 	state := evalCtx.GetState(n.stateKey)
 	log.Printf("[TRACE] TestStateManager: cleaning up state for %s", file.Name)
