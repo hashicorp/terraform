8345e1d3d9c1c2d321f59cffc88c7a250220763e
Daniel Banck <daniel@dbanck.de>
2026-03-11 15:07:01 +0100

    command: Add vars to providers command

--- internal/command/arguments/providers_test.go
+++ internal/command/arguments/providers_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/hashicorp/terraform/internal/tfdiags"
 )
 
@@ -19,15 +20,19 @@ func TestParseProviders_valid(t *testing.T) {
 			nil,
 			&Providers{
 				TestsDirectory: "tests",
+				Vars:           &Vars{},
 			},
 		},
 		"test directory": {
 			[]string{"-test-directory=integration-tests"},
 			&Providers{
 				TestsDirectory: "integration-tests",
+				Vars:           &Vars{},
 			},
 		},
 	}
+
+	cmpOpts := cmpopts.IgnoreUnexported(Vars{})
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
@@ -35,7 +40,7 @@ func TestParseProviders_valid(t *testing.T) {
 			if len(diags) > 0 {
 				t.Fatalf("unexpected diags: %v", diags)
 			}
-			if diff := cmp.Diff(tc.want, got); diff != "" {
+			if diff := cmp.Diff(tc.want, got, cmpOpts); diff != "" {
 				t.Fatalf("unexpected result\n%s", diff)
 			}
 		})
@@ -52,6 +57,7 @@ func TestParseProviders_invalid(t *testing.T) {
 			[]string{"-wat"},
 			&Providers{
 				TestsDirectory: "tests",
+				Vars:           &Vars{},
 			},
 			tfdiags.Diagnostics{
 				tfdiags.Sourceless(
@@ -65,6 +71,7 @@ func TestParseProviders_invalid(t *testing.T) {
 			[]string{"foo"},
 			&Providers{
 				TestsDirectory: "tests",
+				Vars:           &Vars{},
 			},
 			tfdiags.Diagnostics{
 				tfdiags.Sourceless(
@@ -76,13 +83,59 @@ func TestParseProviders_invalid(t *testing.T) {
 		},
 	}
 
+	cmpOpts := cmpopts.IgnoreUnexported(Vars{})
+
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			got, gotDiags := ParseProviders(tc.args)
-			if diff := cmp.Diff(tc.want, got); diff != "" {
+			if diff := cmp.Diff(tc.want, got, cmpOpts); diff != "" {
 				t.Fatalf("unexpected result\n%s", diff)
 			}
 			tfdiags.AssertDiagnosticsMatch(t, gotDiags, tc.wantDiags)
+		})
+	}
+}
+
+func TestParseProviders_vars(t *testing.T) {
+	testCases := map[string]struct {
+		args []string
+		want []FlagNameValue
+	}{
+		"var": {
+			args: []string{"-var", "foo=bar"},
+			want: []FlagNameValue{
+				{Name: "-var", Value: "foo=bar"},
+			},
+		},
+		"var-file": {
+			args: []string{"-var-file", "cool.tfvars"},
+			want: []FlagNameValue{
+				{Name: "-var-file", Value: "cool.tfvars"},
+			},
+		},
+		"both": {
+			args: []string{
+				"-var", "foo=bar",
+				"-var-file", "cool.tfvars",
+				"-var", "boop=beep",
+			},
+			want: []FlagNameValue{
+				{Name: "-var", Value: "foo=bar"},
+				{Name: "-var-file", Value: "cool.tfvars"},
+				{Name: "-var", Value: "boop=beep"},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, diags := ParseProviders(tc.args)
+			if len(diags) > 0 {
+				t.Fatalf("unexpected diags: %v", diags)
+			}
+			if vars := got.Vars.All(); !cmp.Equal(vars, tc.want) {
+				t.Fatalf("unexpected vars: %#v", vars)
+			}
 		})
 	}
 }
