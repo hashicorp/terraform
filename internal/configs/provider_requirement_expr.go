92c07b3cc1f6e7aa4d2493167588c41139b2793d
Matej Risek <matej.risek@hashicorp.com>
2026-04-13 12:01:46 +0200

    Add ProviderRequirementExpr
    This type will hold expressions found in the source and version
    variables of the required provider block.

---
+++ internal/configs/provider_requirement_expr.go
@@ -0,0 +1,21 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: BUSL-1.1
+
+package configs
+
+import (
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/terraform/internal/addrs"
+)
+
+// ProviderRequirementExpr is a container for deferred HCL expressions for a
+// required provider's source and/or version.
+type ProviderRequirementExpr struct {
+	Name        string
+	SourceExpr  hcl.Expression
+	VersionExpr hcl.Expression
+
+	ConfigAliases []addrs.LocalProviderConfig
+
+	DeclRange hcl.Range
+}
