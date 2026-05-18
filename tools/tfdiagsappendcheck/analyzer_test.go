e81e765913b4e82d6a68b7aa4f84f2e0bde3471c
Sarah French <sarah.french@hashicorp.com>
2026-05-18 20:24:45 +0100

    Add code comments about how testing works using `tools/go/analysis`

--- tools/tfdiagsappendcheck/analyzer_test.go
+++ tools/tfdiagsappendcheck/analyzer_test.go
@@ -13,5 +13,8 @@ func TestAnalyzer(t *testing.T) {
 	t.Parallel()
 
 	testdata := analysistest.TestData()
+	// Note, assertions are made in comments in the testdata files.
+	// See comments in ./testpkg/testpkg.go
+	// See docs for analysistest.Run for more info: https://pkg.go.dev/golang.org/x/tools/go/analysis/analysistest#Run
 	analysistest.Run(t, testdata, DiagsAppendAnalyzer, "github.com/hashicorp/terraform/tools/tfdiagsappendcheck/testpkg")
 }
