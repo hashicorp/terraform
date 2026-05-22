3097f8bf9b19e743282f0b700a1ce6bc443b9fa8
Radek Simko <radek.simko@gmail.com>
2026-05-22 16:40:35 +0100

    command/state_migrate: Wire up configuration parsing

--- internal/command/state_migrate_test.go
+++ internal/command/state_migrate_test.go
@@ -4,56 +4,223 @@
 package command
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/hashicorp/cli"
+	"github.com/hashicorp/terraform/internal/command/workdir"
 )
 
-func TestStateMigrate_basic(t *testing.T) {
+func TestStateMigrate_fromBackendToBackend(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-backend-to-backend")
+	t.Chdir(wd.RootModuleDir())
+
 	ui := cli.NewMockUi()
 	view, done := testView(t)
 	c := &StateMigrateCommand{
 		Meta: Meta{
-			Ui:   ui,
-			View: view,
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
 		},
 	}
 
-	tmpDir := t.TempDir()
-	_, err := os.Create(filepath.Join(tmpDir, ".terraform.lock.hcl"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Chdir(tmpDir)
-
-	args := []string{}
+	args := []string{"-no-color"}
 	code := c.Run(args)
 	if code != 1 {
 		t.Fatalf("expected exit code 1, got %d", code)
 	}
 	out := done(t)
 
-	expectedMsg := "Not implemented yet"
+	expectedMsg := `Migrating state from backend "local" to backend "local"...`
+	if !strings.Contains(out.Stdout(), expectedMsg) {
+		t.Fatalf("expected output %q, got %q", expectedMsg, out.Stdout())
+	}
+
+	expectedErr := "Not implemented yet"
+	if !strings.Contains(out.Stderr(), expectedErr) {
+		t.Fatalf("expected output %q, got %q", expectedErr, out.Stderr())
+	}
+}
+
+func TestStateMigrate_fromBackendToStateStore(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-backend-to-state-store")
+	t.Chdir(wd.RootModuleDir())
+
+	ui := cli.NewMockUi()
+	view, done := testView(t)
+	c := &StateMigrateCommand{
+		Meta: Meta{
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
+		},
+	}
+
+	args := []string{"-no-color"}
+	code := c.Run(args)
+	if code != 1 {
+		t.Fatalf("expected exit code 1, got %d", code)
+	}
+	out := done(t)
+
+	expectedMsg := `Migrating state from backend "local" to state store "test_store" (registry.terraform.io/hashicorp/test)...`
+	if !strings.Contains(out.Stdout(), expectedMsg) {
+		t.Fatalf("expected output %q, got %q", expectedMsg, out.Stdout())
+	}
+
+	expectedErr := "Not implemented yet"
+	if !strings.Contains(out.Stderr(), expectedErr) {
+		t.Fatalf("expected output %q, got %q", expectedErr, out.All())
+	}
+}
+
+func TestStateMigrate_fromStateStoreToStateStore(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-state-store-to-state-store")
+	t.Chdir(wd.RootModuleDir())
+
+	ui := cli.NewMockUi()
+	view, done := testView(t)
+	c := &StateMigrateCommand{
+		Meta: Meta{
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
+		},
+	}
+
+	args := []string{"-no-color"}
+	code := c.Run(args)
+	if code != 1 {
+		t.Fatalf("expected exit code 1, got %d", code)
+	}
+	out := done(t)
+
+	expectedMsg := `Migrating state from state store "test_store" (registry.terraform.io/hashicorp/test) to state store "test_store" (registry.terraform.io/hashicorp/test)...`
+	if !strings.Contains(out.Stdout(), expectedMsg) {
+		t.Fatalf("expected output %q, got %q", expectedMsg, out.Stdout())
+	}
+
+	expectedErr := "Not implemented yet"
+	if !strings.Contains(out.Stderr(), expectedErr) {
+		t.Fatalf("expected output %q, got %q", expectedErr, out.All())
+	}
+}
+
+func TestStateMigrate_fromStateStoreToBackend(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-state-store-to-backend")
+	t.Chdir(wd.RootModuleDir())
+
+	ui := cli.NewMockUi()
+	view, done := testView(t)
+	c := &StateMigrateCommand{
+		Meta: Meta{
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
+		},
+	}
+
+	args := []string{"-no-color"}
+	code := c.Run(args)
+	if code != 1 {
+		t.Fatalf("expected exit code 1, got %d", code)
+	}
+	out := done(t)
+
+	expectedMsg := `Migrating state from state store "test_store" (registry.terraform.io/hashicorp/test) to backend "local"...`
+	if !strings.Contains(out.Stdout(), expectedMsg) {
+		t.Fatalf("expected output %q, got %q", expectedMsg, out.Stdout())
+	}
+
+	expectedErr := "Not implemented yet"
+	if !strings.Contains(out.Stderr(), expectedErr) {
+		t.Fatalf("expected output %q, got %q", expectedErr, out.All())
+	}
+}
+
+func TestStateMigrate_missingModuleFiles(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-missing-mod-files")
+	t.Chdir(wd.RootModuleDir())
+
+	ui := cli.NewMockUi()
+	view, done := testView(t)
+	c := &StateMigrateCommand{
+		Meta: Meta{
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
+		},
+	}
+
+	args := []string{
+		"-input=false",
+		"-no-color",
+	}
+	code := c.Run(args)
+	if code != 1 {
+		t.Fatalf("expected exit code 1, got %d", code)
+	}
+	out := done(t)
+
+	expectedMsg := "Error: Unknown migration destination"
+	if !strings.Contains(out.Stderr(), expectedMsg) {
+		t.Fatalf("expected output %q, got %q", expectedMsg, out.All())
+	}
+}
+
+func TestStateMigrate_missingMigrationFiles(t *testing.T) {
+	wd := tempWorkingDirFixture(t, "state-migrate-missing-migrate-files")
+	t.Chdir(wd.RootModuleDir())
+
+	ui := cli.NewMockUi()
+	view, done := testView(t)
+	c := &StateMigrateCommand{
+		Meta: Meta{
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                wd,
+			AllowExperimentalFeatures: true,
+		},
+	}
+
+	args := []string{
+		"-input=false",
+		"-no-color",
+	}
+	code := c.Run(args)
+	if code != 1 {
+		t.Fatalf("expected exit code 1, got %d", code)
+	}
+	out := done(t)
+
+	expectedMsg := "Error: No state migration instructions found"
 	if !strings.Contains(out.Stderr(), expectedMsg) {
 		t.Fatalf("expected output %q, got %q", expectedMsg, out.All())
 	}
 }
 
 func TestStateMigrate_nonExistentLockFiles(t *testing.T) {
+	// use temporary directory to ensure the lock files certainly do not exist
+	tmpDir := t.TempDir()
+	t.Chdir(tmpDir)
+
 	ui := cli.NewMockUi()
 	view, done := testView(t)
 	c := &StateMigrateCommand{
 		Meta: Meta{
-			Ui:   ui,
-			View: view,
+			Ui:                        ui,
+			View:                      view,
+			WorkingDir:                workdir.NewDir(tmpDir),
+			AllowExperimentalFeatures: true,
 		},
 	}
-
-	// use temporary directory to ensure the lock files certainly do not exist
-	tmpDir := t.TempDir()
 
 	args := []string{
 		"-input=false",
