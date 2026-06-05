54640034c9e45417831fc51ac59157976676f459
Sarah French <sarah.french@hashicorp.com>
2026-06-05 12:17:01 +0100

    refactor: Make `state show` command use the Meta's internal details about the path to the configuration directory.
    When Terraform CLI starts running the working directory is changed, using `os.Chdir`, if the -chdir global option is set. This means when downstream code uses os.Getwd it will get the directory intended by the user. There currently isn't a bug in the `state show` command, so this change is just a refactor. Also, if the local backend is in use then the `Filesystem` state manager implementation that's used relies on the CLI running `os.Chdir` previously if no explicit -state flag is supplied by the user.

--- internal/command/state_show.go
+++ internal/command/state_show.go
@@ -6,7 +6,6 @@ package command
 import (
 	"errors"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/hashicorp/terraform/internal/addrs"
@@ -75,11 +74,7 @@ func (c *StateShowCommand) Run(args []string) int {
 	}
 
 	// We expect the config dir to always be the cwd
-	cwd, err := os.Getwd()
-	if err != nil {
-		diags = diags.Append(fmt.Sprintf("Error getting cwd: %s\n", err))
-		return view.DisplayResourceInstanceState(jsonformat.State{}, diags)
-	}
+	cwd := c.WorkingDir.RootModuleDir()
 
 	// Build the operation (required to get the schemas)
 	opReq := c.Operation(b, c.viewType)
