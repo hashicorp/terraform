065b5c55b07223d7b09617b5078ead2c285e6f1a
Sarah French <sarah.french@hashicorp.com>
2026-06-26 13:27:18 +0100

    fix: Update misleading log. We detect matching providers but not matching provider versions.

--- internal/command/meta_dependencies.go
+++ internal/command/meta_dependencies.go
@@ -87,7 +87,7 @@ func (m *Meta) mergeLockedDependencies(baseLocks, additionalLocks *depsfile.Lock
 	for _, lock := range additionalLocks.AllProviders() {
 		match := mergedLocks.Provider(lock.Provider())
 		if match != nil {
-			log.Printf("[TRACE] Ignoring provider %s version %s in mergeLockedDependencies; lock file contains %s version %s already",
+			log.Printf("[TRACE] Ignoring provider %s version %s in mergeLockedDependencies; lock file contains %s provider already, at version %s",
 				lock.Provider(),
 				lock.Version(),
 				match.Provider(),
