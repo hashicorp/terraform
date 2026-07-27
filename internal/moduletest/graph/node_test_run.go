0e16f6cfe1d62469e5b315eb60367c94e0fb0f71
Chpag <christophe.paget@hashicorp.com>
2026-07-27 15:47:25 +0200

    Add SkipActions in Validate Graph

--- internal/moduletest/graph/node_test_run.go
+++ internal/moduletest/graph/node_test_run.go
@@ -214,6 +214,7 @@ func (n *NodeTestRun) testValidate(providers map[addrs.RootProviderConfig]provid
 	validateDiags := tfCtx.Validate(config, &terraform.ValidateOpts{
 		ExternalProviders:         providers,
 		AllowRootEphemeralOutputs: true,
+		SkipActions:               true,
 	})
 	run.Diagnostics = run.Diagnostics.Append(validateDiags)
 	if validateDiags.HasErrors() {
