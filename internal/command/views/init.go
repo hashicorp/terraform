1088568fdf1b0ecd0aeb3c905cf829a07d31f1e7
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2026-08-03 19:11:26 +0100

    refactor(init,state_migrate): Introduce new `DependencyLockingLogger` interface, use new methods for logging about events involving dependency lock files (#38963)
    This refactoring helps break up larger interfaces into smaller ones, supports code reuse, and also helps with the future removal of the multi-pupose `Output` method from the init view implementation.

--- internal/command/views/init.go
+++ internal/command/views/init.go
@@ -24,6 +24,7 @@ type Init interface {
 	Log(message string, params ...any)
 
 	ProviderInstallationLogger
+	DependencyLockingLogger
 
 	prepareMessage(messageCode InitMessageCode, params ...any) string
 
@@ -134,6 +135,16 @@ func (v *InitHuman) LogProviderVersionSuccessWithKeyID(providerAddr addrs.Provid
 
 func (v *InitHuman) LogPartnerAndCommunityProviders() {
 	v.view.streams.Println(v.prepareMessage(PartnerAndCommunityProvidersMessage))
+}
+
+func (v *InitHuman) LogDependencyLockfileCreated() {
+	params := []any{}
+	v.view.streams.Println(v.prepareMessage(LockInfo, params...))
+}
+
+func (v *InitHuman) LogDependencyLockfileUpdated() {
+	params := []any{}
+	v.view.streams.Println(v.prepareMessage(DependenciesLockChangesInfo, params...))
 }
 
 // this implements log method for use by interfaces that need to log generic string messages, e.g used for logging in hook_module_install.go
@@ -316,6 +327,20 @@ func (v *InitJSON) LogPartnerAndCommunityProviders() {
 	// This was previously logged via LogInitMessage, so we need to match implementation of that method
 	// to ensure the same JSON log is produced.
 	v.logInitMessage(PartnerAndCommunityProvidersMessage)
+}
+
+func (v *InitJSON) LogDependencyLockfileCreated() {
+	// This was previously logged via Output, so we need to match implementation of that method
+	// to ensure the same JSON log is produced.
+	params := []any{}
+	v.Output(LockInfo, params...)
+}
+
+func (v *InitJSON) LogDependencyLockfileUpdated() {
+	// This was previously logged via Output, so we need to match implementation of that method
+	// to ensure the same JSON log is produced.
+	params := []any{}
+	v.Output(DependenciesLockChangesInfo, params...)
 }
 
 func (v *InitJSON) prepareMessage(messageCode InitMessageCode, params ...any) string {
@@ -623,23 +648,6 @@ see any changes that are required for your infrastructure.
 If you ever set or change modules or Terraform Settings, run "terraform init"
 again to reinitialize your working directory.
 `
-
-const previousLockInfoHuman = `
-Terraform has created a lock file [bold].terraform.lock.hcl[reset] to record the provider
-selections it made above. Include this file in your version control repository
-so that Terraform can guarantee to make the same selections by default when
-you run "terraform init" in the future.`
-
-const previousLockInfoJSON = `
-Terraform has created a lock file .terraform.lock.hcl to record the provider
-selections it made above. Include this file in your version control repository
-so that Terraform can guarantee to make the same selections by default when
-you run "terraform init" in the future.`
-
-const dependenciesLockChangesInfo = `
-Terraform has made some changes to the provider dependency selections recorded
-in the .terraform.lock.hcl file. Review those changes and commit them to your
-version control system if they represent changes you intended to make.`
 
 const partnerAndCommunityProvidersInfo = "\nPartner and community providers are signed by their developers.\n" +
 	"If you'd like to know more about provider signing, you can read about it here:\n" +
--- internal/command/views/init_test.go
+++ internal/command/views/init_test.go
@@ -907,6 +907,54 @@ func TestNewInit_LogInitializingStateStoreProviderPlugin_json(t *testing.T) {
 	}
 }
 
+func TestNewInit_LogDependencyLockfileCreated_json(t *testing.T) {
+	streams, done := terminal.StreamsForTesting(t)
+	view := NewView(streams)
+	initView := NewInit(arguments.ViewJSON, view)
+
+	initView.LogDependencyLockfileCreated()
+
+	// Assert output
+	output := done(t)
+	expectedOutputFields := []string{
+		`"@level":"info"`,
+		`"@message":"Terraform has created a lock file .terraform.lock.hcl to `, // ... incomplete but sufficient for test
+		`"@module":"terraform.ui"`,
+		//@timestamp is dynamic
+		`"message_code":"lock_info"`,
+		`"type":"init_output"`,
+	}
+	for _, snippet := range expectedOutputFields {
+		if !strings.Contains(output.Stdout(), snippet) {
+			t.Fatalf("output didn't include expected snippet:\n expected: %s\n got:\n %s", snippet, output.Stdout())
+		}
+	}
+}
+
+func TestNewInit_LogDependencyLockfileUpdated_json(t *testing.T) {
+	streams, done := terminal.StreamsForTesting(t)
+	view := NewView(streams)
+	initView := NewInit(arguments.ViewJSON, view)
+
+	initView.LogDependencyLockfileUpdated()
+
+	// Assert output
+	output := done(t)
+	expectedOutputFields := []string{
+		`"@level":"info"`,
+		`"@message":"Terraform has made some changes to the provider dependency selections `, // ... incomplete but sufficient for test
+		`"@module":"terraform.ui"`,
+		//@timestamp is dynamic
+		`"message_code":"dependencies_lock_changes_info"`,
+		`"type":"init_output"`,
+	}
+	for _, snippet := range expectedOutputFields {
+		if !strings.Contains(output.Stdout(), snippet) {
+			t.Fatalf("output didn't include expected snippet:\n expected: %s\n got:\n %s", snippet, output.Stdout())
+		}
+	}
+}
+
 func TestNewInit_Spacer_json(t *testing.T) {
 	streams, done := terminal.StreamsForTesting(t)
 	view := NewView(streams)
