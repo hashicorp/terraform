9eec77ea438984486640591866582ea9225a188a
Sarah French <sarah.french@hashicorp.com>
2026-08-06 18:54:02 +0100

    feat: Add `StateMigrateJSON` view, implementing `Spacer` interface and with `Diagnostics` method.

--- internal/command/views/state_migrate.go
+++ internal/command/views/state_migrate.go
@@ -60,7 +60,9 @@ type StateMigrate interface {
 func NewStateMigrate(viewType arguments.ViewType, view *View) StateMigrate {
 	switch viewType {
 	case arguments.ViewHuman:
-		return &StateMigrateHuman{view: view}
+		return &StateMigrateHuman{
+			view: view,
+		}
 	default:
 		panic(fmt.Sprintf("unsupported view type: %s", viewType))
 	}
@@ -224,4 +226,19 @@ func (s *StateMigrateHuman) prepareMessage(code InitMessageCode, params ...any) 
 	}
 
 	return s.view.colorize.Color(strings.TrimSpace(fmt.Sprintf(message.HumanValue, params...)))
+}
+
+var _ Spacer = (*StateMigrateJSON)(nil)
+
+type StateMigrateJSON struct {
+	view *JSONView
+}
+
+func (s *StateMigrateJSON) Diagnostics(diags tfdiags.Diagnostics) {
+	s.view.Diagnostics(diags)
+}
+
+// Implements Spacer
+func (s *StateMigrateJSON) Spacer() {
+	// no-op for JSON output, since we don't want to log empty messages in JSON
 }
