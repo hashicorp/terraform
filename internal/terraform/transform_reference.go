311cb0b63359f99cf05a22f7454a2d4408a7b0e9
Austin Valle <austinvalle@gmail.com>
2026-08-12 09:44:58 -0400

    import: Fix `id` and `identity` evaluation for child modules
    This change fixes a bug where child modules would evaluate the `id` and `identity` attributes using the root module scope (which previously you could only define import blocks in the root module). This change also adjusts the reference transformer to ensure the proper graph edges are created for child module imports.

--- internal/terraform/transform_reference.go
+++ internal/terraform/transform_reference.go
@@ -344,8 +344,7 @@ func (m ReferenceMap) References(v dag.Vertex) []dag.Vertex {
 
 	if rn, ok := v.(GraphNodeImportReferencer); ok {
 		for _, ref := range rn.ImportReferences() {
-			// import block references are always in the root module scope
-			referenceKeys = append(referenceKeys, m.referenceMapKey(addrs.RootModule, ref.Subject))
+			referenceKeys = append(referenceKeys, m.referenceMapKey(vertexReferencePath(v), ref.Subject))
 		}
 	}
 
