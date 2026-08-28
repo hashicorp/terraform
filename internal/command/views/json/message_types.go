6be4828d62b8f360f8cd1fea00aaa3e146edace9
Sarah French <sarah.french@hashicorp.com>
2026-08-28 19:40:37 +0100

    refactor: Introduce a `LogInitializingBackendStart` method to the init's view interface, use it

--- internal/command/views/json/message_types.go
+++ internal/command/views/json/message_types.go
@@ -111,6 +111,7 @@ const (
 	MessageUpgradingModulesMessage           MessageType = "upgrading_modules_message"
 	MessageInitializingModulesMessage        MessageType = "initializing_modules_message"
 	MessageInitializingStateStoreMessage     MessageType = "initializing_state_store_message"
+	MessageInitializingBackendMessage        MessageType = "initializing_backend_message"
 	MessageInitializingProviderPluginMessage MessageType = "initializing_provider_plugin_message"
 	MessageLockInfo                          MessageType = "lock_info"
 	MessageDependenciesLockChangesInfo       MessageType = "dependencies_lock_changes_info"
