cc83f296c500daf6795478ebd163ea4748617dca
Sarah French <sarah.french@hashicorp.com>
2026-08-28 19:52:44 +0100

    refactor: Introduce a `LogInitializingHCPTerraformStart` method to the init's view interface, use it

--- internal/command/views/init.go
+++ internal/command/views/init.go
@@ -36,6 +36,9 @@ type Init interface {
 	// LogInitializingBackendStart indicates progress initializing a backend.
 	LogInitializingBackendStart()
 
+	// LogInitializingHCPTerraformStart indicates progress initializing the `cloud` backend.
+	LogInitializingHCPTerraformStart()
+
 	ModuleInstallationLogger
 	ProviderInstallationLogger
 	ProviderLockingLogger
@@ -106,6 +109,11 @@ func (v *InitHuman) LogInitializingStateStoreStart(storeType string) {
 
 func (v *InitHuman) LogInitializingBackendStart() {
 	msg := "\n[reset][bold]Initializing the backend..."
+	v.print(msg)
+}
+
+func (v *InitHuman) LogInitializingHCPTerraformStart() {
+	msg := "\n[reset][bold]Initializing HCP Terraform..."
 	v.print(msg)
 }
 
@@ -321,6 +329,11 @@ func (v *InitJSON) LogInitializingStateStoreStart(storeType string) {
 func (v *InitJSON) LogInitializingBackendStart() {
 	msg := "Initializing the backend..."
 	v.initOutputLog(msg, json.MessageInitializingBackendMessage)
+}
+
+func (v *InitJSON) LogInitializingHCPTerraformStart() {
+	msg := "Initializing HCP Terraform..."
+	v.initOutputLog(msg, json.MessageInitializingTerraformCloudMessage)
 }
 
 // logInitMessage is an internalised version of an old method `LogInitMessage`.
@@ -548,10 +561,6 @@ var MessageRegistry map[InitMessageCode]InitMessage = map[InitMessageCode]InitMe
 		HumanValue: outputInitSuccessCLICloud,
 		JSONValue:  outputInitSuccessCLICloudJSON,
 	},
-	"initializing_terraform_cloud_message": {
-		HumanValue: "\n[reset][bold]Initializing HCP Terraform...",
-		JSONValue:  "Initializing HCP Terraform...",
-	},
 	"provider_already_installed_message": {
 		HumanValue: logProviderVersionAlreadyInstalledHuman,
 		JSONValue:  logProviderVersionAlreadyInstalledJSON,
@@ -640,12 +649,11 @@ const (
 	// Following message codes are used and documented EXTERNALLY
 	// Keep docs/internals/machine-readable-ui.mdx up to date with
 	// this list when making changes here.
-	OutputInitEmptyMessage            InitMessageCode = "output_init_empty_message"
-	OutputInitSuccessMessage          InitMessageCode = "output_init_success_message"
-	OutputInitSuccessCloudMessage     InitMessageCode = "output_init_success_cloud_message"
-	OutputInitSuccessCLIMessage       InitMessageCode = "output_init_success_cli_message"
-	OutputInitSuccessCLICloudMessage  InitMessageCode = "output_init_success_cli_cloud_message"
-	InitializingTerraformCloudMessage InitMessageCode = "initializing_terraform_cloud_message"
+	OutputInitEmptyMessage           InitMessageCode = "output_init_empty_message"
+	OutputInitSuccessMessage         InitMessageCode = "output_init_success_message"
+	OutputInitSuccessCloudMessage    InitMessageCode = "output_init_success_cloud_message"
+	OutputInitSuccessCLIMessage      InitMessageCode = "output_init_success_cli_message"
+	OutputInitSuccessCLICloudMessage InitMessageCode = "output_init_success_cli_cloud_message"
 
 	//// Message codes below are ONLY used INTERNALLY (for now)
 
