97e3cfdd69aa54a1589147015fe0b0e3685ed12d
nao-kichi <98793084+nao-kichi@users.noreply.github.com>
2026-09-09 17:15:23 +0900

    Add provider meta schema to JSON output

--- internal/command/jsonprovider/provider_test.go
+++ internal/command/jsonprovider/provider_test.go
@@ -27,6 +27,7 @@ func TestMarshalProvider(t *testing.T) {
 			providers.ProviderSchema{},
 			&Provider{
 				Provider:                 &Schema{},
+				ProviderMeta:             &Schema{},
 				ResourceSchemas:          map[string]*Schema{},
 				DataSourceSchemas:        map[string]*Schema{},
 				EphemeralResourceSchemas: map[string]*Schema{},
@@ -45,6 +46,18 @@ func TestMarshalProvider(t *testing.T) {
 							"region": {
 								AttributeType:   json.RawMessage(`"string"`),
 								Required:        true,
+								DescriptionKind: "plain",
+							},
+						},
+						DescriptionKind: "plain",
+					},
+				},
+				ProviderMeta: &Schema{
+					Block: &Block{
+						Attributes: map[string]*Attribute{
+							"module_name": {
+								AttributeType:   json.RawMessage(`"string"`),
+								Optional:        true,
 								DescriptionKind: "plain",
 							},
 						},
@@ -272,6 +285,13 @@ func testProvider() providers.ProviderSchema {
 			Body: &configschema.Block{
 				Attributes: map[string]*configschema.Attribute{
 					"region": {Type: cty.String, Required: true},
+				},
+			},
+		},
+		ProviderMeta: providers.Schema{
+			Body: &configschema.Block{
+				Attributes: map[string]*configschema.Attribute{
+					"module_name": {Type: cty.String, Optional: true},
 				},
 			},
 		},
