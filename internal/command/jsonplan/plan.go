75f0466a83dab1ba1c1f35d6be71a964c3f0d5b4
James Bardin <j.bardin@gmail.com>
2026-09-09 19:06:05 +0000

    backport of commit 2dce9e423fec6e49665dbf7837531eba7fc712fd

--- internal/command/jsonplan/plan.go
+++ internal/command/jsonplan/plan.go
@@ -991,6 +991,8 @@ func actionString(action string) []string {
 		return []string{"delete", "create"}
 	case action == "Forget":
 		return []string{"forget"}
+	case action == "ForgetThenCreate":
+		return []string{"forget", "create"}
 	case action == "CreateThenForget":
 		return []string{"create", "forget"}
 	default:
@@ -1011,6 +1013,10 @@ func UnmarshalActions(actions []string) plans.Action {
 
 		if actions[0] == "create" && actions[1] == "forget" {
 			return plans.CreateThenForget
+		}
+
+		if actions[0] == "forget" && actions[1] == "create" {
+			return plans.ForgetThenCreate
 		}
 	}
 
