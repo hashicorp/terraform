845f51465e180f74be297d048059003f82eeb633
Sarah French <sarah.french@hashicorp.com>
2026-09-11 13:55:39 +0100

    feat: Add a method for logging when a new workspace was created but its state wasn't updated as expected

--- internal/command/views/workspace_new_test.go
+++ internal/command/views/workspace_new_test.go
@@ -75,6 +75,56 @@ for this configuration.`, workspaceName)
 	}
 }
 
+func TestWorkspaceNewHuman_LogWorkspaceCreationFromStateFailure(t *testing.T) {
+	workspaceName := "my-workspace"
+
+	testCases := map[string]struct {
+		workspace         string
+		diags             tfdiags.Diagnostics
+		wantStdoutSnippet string
+		wantStderr        string
+	}{
+		"error": {
+			workspaceName,
+			tfdiags.Diagnostics{
+				tfdiags.Sourceless(
+					tfdiags.Error,
+					"Example error",
+					"This is an example error message.",
+				),
+			},
+			fmt.Sprintf("Created and switched to workspace %q, but failed to initialize the state.", workspaceName),
+			"Error: Example error\n\nThis is an example error message.\n\n",
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			streams, done := terminal.StreamsForTesting(t)
+			view := NewView(streams)
+			view.Configure(&arguments.View{NoColor: true})
+			v := NewWorkspaceNew(arguments.ViewHuman, view)
+
+			v.LogWorkspaceCreationFromStateFailure(tc.workspace, tc.diags)
+
+			output := done(t)
+
+			// Assert contents
+			// This must be done separately for stdout and stderr due to
+			// the interleaving of output caused in tests by (TestOutput).All()
+			gotStdout := strings.TrimSpace(output.Stdout())
+			wantStdoutSnippet := strings.TrimSpace(tc.wantStdoutSnippet)
+			if !strings.Contains(gotStdout, wantStdoutSnippet) {
+				t.Fatalf("expected stdout to contain:\n%s\nbut got:\n%s", wantStdoutSnippet, gotStdout)
+			}
+			gotStderr := strings.TrimSpace(output.Stderr())
+			wantStderr := strings.TrimSpace(tc.wantStderr)
+			if diff := cmp.Diff(wantStderr, gotStderr); diff != "" {
+				t.Fatalf("unexpected diff in human output:\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestWorkspaceNewHuman_Diagnostics(t *testing.T) {
 	testCases := map[string]struct {
 		diags      tfdiags.Diagnostics
